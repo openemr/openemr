625096bc86b0d39e53ec977792d6bde326213e4b
tekknogenius <tekknogenius>
2005-03-20 04:58:36 +0000

    added Problem List section to patitent's summary page

--- interface/patient_file/summary/stats_full.php
+++ interface/patient_file/summary/stats_full.php
@@ -9,11 +9,12 @@ include_once("../../globals.php");
 OpenEMR
 </TITLE>
 </HEAD>
-<frameset rows="*" cols="33%,33%,33%" frameborder="NO" border="0" framespacing="0">
+<frameset rows="*" cols="25%,25%,25%,25%" frameborder="NO" border="0" framespacing="0">
 
   <frame src="medications.php?active=<?echo $active;?>" name="Medications" scrolling="auto" noresize frameborder="NO">
   <frame src="allergies.php?active=<?echo $active;?>" name="Allergies" scrolling="auto" noresize frameborder="NO">
   <frame src="immunizations.php?active=<?echo $active;?>" name="Immunizations" scrolling="auto" noresize frameborder="NO">
+  <frame src="problems.php?active=<?echo $active;?>" name="Problem List" scrolling="auto" noresize frameborder="NO">
   
 </frameset>
 
