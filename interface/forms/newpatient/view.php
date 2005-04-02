74ee93e19f715f4beab3d422668caf6326cdfd89
sunsetsystems <sunsetsystems>
2005-04-02 01:32:15 +0000

    make it easy to modify the code so that the encounter date is changeable

--- interface/forms/newpatient/view.php
+++ interface/forms/newpatient/view.php
@@ -1,6 +1,13 @@
 <?
 include_once("../../globals.php");
 
+$disabled = "disabled";
+
+// Uncomment this if you want to allow changing encounter dates.
+// Eventually this needs to be subject to access control:
+//
+//$disabled = "";
+
 $months = array("01","02","03","04","05","06","07","08","09","10","11","12");
 $days = array("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17",
 	"18","19","20","21","22","23","24","25","26","27","28","29","30","31");
@@ -55,7 +62,7 @@ echo $result{"reason"};
 </td></td></tr>
 
 <tr><td><span class='text'>Date Of Service:</span></td>
-<td><select name='month' disabled>
+<td><select name='month' <? echo $disabled ?>>
 <?
 foreach($months as $month){
 ?>
@@ -64,7 +71,7 @@ foreach($months as $month){
 }
 ?>
 </select>
-<select name='day' disabled>
+<select name='day' <? echo $disabled ?>>
 <?
 foreach($days as $day){
 ?>
@@ -73,7 +80,7 @@ foreach($days as $day){
 }
 ?>
 </select>
-<select name='year' disabled>
+<select name='year' <? echo $disabled ?>>
 <?
 foreach($years as $year){
 ?>
