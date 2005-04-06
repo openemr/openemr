f9ec4c48612a575b44c568708461024b0d491f8c
vitsyn <vitsyn>
2005-04-06 22:20:24 +0000

    *** empty log message ***

--- interface/patient_file/summary/patient_summary.php
+++ interface/patient_file/summary/patient_summary.php
@@ -9,14 +9,17 @@ include_once("../../globals.php");
 OpenEMR
 </TITLE>
 </HEAD>
-<frameset rows="50%,50%" cols="*">
 
-  <frame src="demographics.php" name="Demographics" scrolling="auto">
-  <frameset rows="*" cols="175,*">
-	<frame src="stats.php" name="Stats" scrolling="auto">
-	<frame src="pnotes.php" name="Notes" scrolling="auto">
-  </frameset>
+
+<frameset rows="50%,50%" cols="*" frameborder="NO" border="0" framespacing="0">
+         <frame src="demographics.php" name="Demographics" scrolling="auto" noresize frameborder="NO">
+      <frameset rows="*" cols="80%,20%" frameborder="NO" border="0" framespacing="0">
+        <frame src="stats.php" name="Stats" scrolling="auto" frameborder="NO">
+	<frame src="pnotes.php" name="Notes" scrolling="auto" frameborder="NO">
+     </frameset>
 </frameset>
+
+
 
 
 <noframes><body bgcolor="#FFFFFF">
--- interface/patient_file/summary/stats_full.php
+++ interface/patient_file/summary/stats_full.php
@@ -9,20 +9,28 @@ include_once("../../globals.php");
 OpenEMR
 </TITLE>
 </HEAD>
-<frameset rows="*" cols="25%,25%,25%,25%" frameborder="NO" border="0" framespacing="0">
 
+
+<frameset rows="80%,20%" cols="*" frameborder="NO" border="0" framespacing="0">
+
+ <frameset rows="*" cols="20%,20%,20%,20%" frameborder="NO" border="0" framespacing="0">
+
+  <frame src="medical_problems.php?active=<?echo $active;?>" name="Medical Problems" scrolling="auto" noresize rameborder="NO">
   <frame src="medications.php?active=<?echo $active;?>" name="Medications" scrolling="auto" noresize frameborder="NO">
   <frame src="allergies.php?active=<?echo $active;?>" name="Allergies" scrolling="auto" noresize frameborder="NO">
-  <frame src="immunizations.php?active=<?echo $active;?>" name="Immunizations" scrolling="auto" noresize frameborder="NO">
-  <frame src="problems.php?active=<?echo $active;?>" name="Problem List" scrolling="auto" noresize frameborder="NO">
+  <frame src="surgeries.php?active=<?echo $active;?>" name="Surgery" scrolling="auto" noresize frameborder="NO">
+  </frameset>
   
-</frameset>
+  <frameset rows="*" cols="*" frameborder="NO" border="0" framespacing="0">
+ 
+  <frame src="immunizations.php?active=<?echo $active;?>" name="Immunizations" scrolling="auto" noresize frameborder="NO">
+  
+  </frameset>
 
+  </frameset>
 
 <noframes><body bgcolor="#FFFFFF">
 
 </body></noframes>
-
-
 
 </HTML
