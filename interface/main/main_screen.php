6130804ae1d5b3c07169e649b60ab46d1e778a5a
sunsetsystems <sunsetsystems>
2005-07-03 07:27:16 +0000

    for sports team use show the player roster initially instead of the calendar

--- interface/main/main_screen.php
+++ interface/main/main_screen.php
@@ -3,12 +3,8 @@ include_once("../globals.php");
 
 $_SESSION["encounter"] = "";
 
-
 if (isset($_GET[mode]) && $_GET{mode} == "loadcalendar") {
-
 ?>
-
-
 <HTML>
 <HEAD>
 <TITLE>
@@ -18,27 +14,26 @@ OpenEMR
 <frameset rows="<?echo "$GLOBALS[navBarHeight],$GLOBALS[titleBarHeight]" ?>,*" cols="*" frameborder="NO" border="0" framespacing="0">
   <frame src="main_navigation.php" name="Navigation" scrolling="no" noresize frameborder="NO">
   <frame src="main_title.php" name="Title" scrolling="no" noresize frameborder="NO">
-  <frame src="calendar/index.php?pid=<?echo $_GET{pid};?><?if (isset($_GET{date})) {echo "&date=".$_GET{date};};?>" name="Main" scrolling="auto" noresize frameborder="NO">
-
-
+<?
+ if ($GLOBALS['athletic_team']) {
+  echo "  <frame src='../reports/players_report.php?embed=1' " .
+   "name='Main' scrolling='auto' noresize frameborder='no'>\n";
+ } else {
+  echo "  <frame src='calendar/index.php?pid=" . $_GET['pid'];
+  if (isset($_GET['date'])) echo "&date=" . $_GET['date'];
+  echo "' name='Main' scrolling='auto' noresize frameborder='NO'>\n";
+ }
+?>
 </frameset>
-
 
 <noframes><body bgcolor="#FFFFFF">
 
 </body></noframes>
 
-
-
 </HTML>
-
-
-
 <?
-
 } else {
 ?>
-
 <HTML>
 <HEAD>
 <TITLE>
@@ -48,18 +43,22 @@ OpenEMR
 <frameset rows="<?echo "$GLOBALS[navBarHeight],$GLOBALS[titleBarHeight]" ?>,*" cols="*" frameborder="NO" border="0" framespacing="0">
   <frame src="main_navigation.php" name="Navigation" scrolling="no" noresize frameborder="NO">
   <frame src="main_title.php" name="Title" scrolling="no" noresize frameborder="NO">
-  <frame src="main.php?mode=<?=$_GET['mode']?>" name="Main" scrolling="auto" noresize frameborder="NO">
+<?
+ if ($GLOBALS['athletic_team']) {
+  echo "  <frame src='../reports/players_report.php?embed=1' " .
+   "name='Main' scrolling='auto' noresize frameborder='no'>\n";
+ } else {
+  echo "  <frame src='main.php?mode=" . $_GET['mode'];
+  echo "' name='Main' scrolling='auto' noresize frameborder='NO'>\n";
+ }
+?>
 </frameset>
-
 
 <noframes><body bgcolor="#FFFFFF">
 
 </body></noframes>
 
-
-
 </HTML>
-
 <?
 }
 ?>
