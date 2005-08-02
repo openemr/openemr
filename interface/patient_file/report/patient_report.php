43d3c0fb95027458ccea13563c2ee913cc429acd
sunsetsystems <sunsetsystems>
2005-08-02 23:46:54 +0000

    improved layout and ordering

--- interface/patient_file/report/patient_report.php
+++ interface/patient_file/report/patient_report.php
@@ -1,105 +1,191 @@
 <?
-include_once("../../globals.php");
-
+ include_once("../../globals.php");
 ?>
-
 <html>
 <head>
-
-
 <link rel=stylesheet href="<?echo $css_header;?>" type="text/css">
-
-
+<script language='JavaScript'>
+ // When an issue is checked, auto-check all the related encounters.
+ function issueClick(icb) {
+  if (! icb.checked) return;
+  var f = document.forms[0];
+  var nel = f.elements.length;
+  for (var i = 0; i < nel; ++i) {
+   var ecb = f.elements[i];
+   if (ecb.name == 'documents[]' ||
+    ecb.name.indexOf('include_') == 0 ||
+    ecb.name.indexOf('issue_'  ) == 0)
+    continue;
+   if (icb.value.indexOf('/' + ecb.value + '/') >= 0) {
+    ecb.checked = true;
+   }
+  }
+ }
+</script>
 </head>
-<body <?echo $top_bg_line;?> topmargin=0 rightmargin=0 leftmargin=2 bottommargin=0 marginwidth=2 marginheight=0>
 
+<body <?echo $top_bg_line;?> topmargin='0' rightmargin='0' leftmargin='2'
+ bottommargin='0' marginwidth='2' marginheight='0'>
 
-<font class=title>Patient Report</font><br>
+<font class='title'>Patient Report</font><br>
 
 <a class="link_submit" href="full_report.php">[View Comprehensive Patient Report]</a>
 
-<form name=report_form method=post action=custom_report.php>
+<form name='report_form' method='post' action='custom_report.php'>
 
-<table><tr><td>
-<input type=checkbox name=include_demographics value="demographics" checked><span class=text>Demographics</span><br>
-<input type=checkbox name=include_history value="history"><span class=text>History</span><br>
-<input type=checkbox name=include_employer value="employer"><span class=text>Employer</span><br>
-<input type=checkbox name=include_insurance value="insurance"><span class=text>Insurance</span><br>
-<input type=checkbox name=include_billing value="billing" checked><span class=text>Billing</span><br>
+<table>
+ <tr>
+  <td class='text' valign='top'>
+   <input type='checkbox' name='include_demographics' value="demographics" checked>Demographics<br>
+   <input type='checkbox' name='include_history' value="history">History<br>
+   <input type='checkbox' name='include_employer' value="employer">Employer<br>
+   <input type='checkbox' name='include_insurance' value="insurance">Insurance<br>
+   <input type='checkbox' name='include_billing' value="billing" checked>Billing<br>
+  </td>
+  <td class='text' valign='top'>
+   <!--
+   <input type='checkbox' name='include_allergies' value="allergies">Allergies<br>
+   <input type='checkbox' name='include_medications' value="medications">Medications<br>
+   -->
+   <input type='checkbox' name='include_immunizations' value="immunizations">Immunizations<br>
+   <!--
+   <input type='checkbox' name='include_medical_problems' value="medical_problems">Medical Problems<br>
+   -->
+   <input type='checkbox' name='include_notes' value="notes">Patient Notes<br>
+   <input type='checkbox' name='include_transactions' value="transactions">Transactions<br>
+  </td>
+ </tr>
+</table>
+
 <br>
-</td><td>
-<input type=checkbox name=include_allergies value="allergies"><span class=text>Allergies</span><br>
-<input type=checkbox name=include_medications value="medications"><span class=text>Medications</span><br>
-<input type=checkbox name=include_immunizations value="immunizations"><span class=text>Immunizations</span><br>
-<input type=checkbox name=include_medical_problems value="medical_problems"><span class=text>Medical Problems</span><br>
-<input type=checkbox name=include_notes value="notes"><span class=text>Patient Notes</span><br>
-<input type=checkbox name=include_transactions value="transactions"><span class=text>Transactions</span><br>
-</td></tr></table>
-<br>
-<a href="javascript:document.report_form.submit();" class=link_submit>Generate Report</a>
+<a href='javascript:document.report_form.submit()' class='link_submit'>Generate Report</a>
 <hr>
-<span class=bold>Select the Encounter Forms to Include in this Report:</span>
-<br>
+
+<table>
+ <tr>
+
+  <td valign='top' class='text'>
+   <span class='bold'>Issues to Include in this Report: &nbsp; &nbsp;</span>
+   <br>&nbsp;
+   <table cellpadding='1' cellspacing='2'>
+    <!--
+    <tr class='bold'>
+     <td>Type</td>
+     <td>Title</td>
+     <td>Begin</td>
+     <td>End &nbsp; &nbsp; &nbsp;</td>
+    </tr>
+    -->
 <?
-$last_encounter = 1;
-$in_last_encounter = 0;
-$isfirst=1;
-$res = sqlStatement("select * from forms where pid='$pid' order by date DESC");
-while($result = sqlFetchArray($res)) {
-	if ($result{"form_name"} == "New Patient Encounter") {
-		if ($isfirst==0) {
-		print "</blockquote>\n\n";
-		}
-		$isfirst=0;
-		print "<br><input type=checkbox name='".$result{"formdir"}."[]' value='".$result{"form_id"}.":".$result{"encounter"}."'";
-		if($last_encounter === 1) {
-			print " checked";
-			$in_last_encounter = 1;
-			$last_encounter = 0;
-		}
-		else {
-			$in_last_encounter = 0;
-		}
-		print " ><span class=bold>".$result{"form_name"}." </span><span class=text>(".date("Y-m-d",strtotime($result{"date"})).")</span><br><blockquote>\n";
-	} else {
-		print "<input type=checkbox name='".$result{"formdir"}."[]' value='".$result{"form_id"}.":".$result{"encounter"}."'";
-		/*******
-		**	Comment this out if you want the indented sub encounters checked by default
-		** as well.
-		if($in_last_encounter === 1) {
-			print "checked";
-		}
-		*/
-		print "><span class=bold>".$result{"form_name"}." </span><span class=text>(".date("Y-m-d",strtotime($result{"date"})).")</span><br>\n";
-	}
-	//call_user_func($result{"formdir"} . "_report", $pid, $result{"encounter"}, $cols, $result{"form_id"});
+ // get issues
+ $pres = sqlStatement("SELECT * FROM lists WHERE pid = $pid " .
+  "ORDER BY type, begdate");
+ $lasttype = "";
+ while ($prow = sqlFetchArray($pres)) {
+  if ($lasttype != $prow['type']) {
+   $lasttype = $prow['type'];
+   $disptype = $lasttype;
+   switch ($lasttype) {
+    case "allergy"        : $disptype = "Allergies"       ; break;
+    case "problem"        :
+    case "medical_problem": $disptype = "Medical Problems"; break;
+    case "medication"     : $disptype = "Medications"     ; break;
+    case "surgery"        : $disptype = "Surgeries"       ; break;
+   }
+   echo " <tr>\n";
+   echo "  <td valign='top' colspan='4' class='bold'><b>$disptype</b></td>\n";
+   echo " </tr>\n";
+  }
+  $rowid = $prow['id'];
+  $disptitle = trim($prow['title']) ? $prow['title'] : "[Missing Title]";
+
+  $ieres = sqlStatement("SELECT encounter FROM issue_encounter WHERE " .
+   "pid = '$pid' AND list_id = '$rowid'");
+
+  echo "    <tr class='text'>\n";
+  echo "     <td valign='top'>&nbsp;</td>\n";
+  echo "     <td valign='top'>";
+  echo "<input type='checkbox' name='issue_$rowid' onclick='issueClick(this)' value='/";
+  while ($ierow = sqlFetchArray($ieres)) {
+   echo $ierow['encounter'] . "/";
+  }
+  echo "' />$disptitle</td>\n";
+  echo "     <td valign='top'>" . $prow['begdate'];
+  if ($prow['enddate']) {
+   echo " - " . $prow['enddate'];
+  } else {
+   echo " Active";
+  }
+  echo " &nbsp; &nbsp; </td>\n";
+  echo "    </tr>\n";
+ }
+?>
+   </table>
+   <br>
+  </td>
+
+  <td valign='top' class='text'>
+<span class='bold'>Encounter Forms to Include in this Report:</span>
+<br><br>
+<?
+ $isfirst = 1;
+
+ $res = sqlStatement("SELECT forms.encounter, forms.form_id, forms.form_name, " .
+  "forms.formdir, forms.date AS fdate, form_encounter.date " .
+  "FROM forms, form_encounter WHERE " .
+  "forms.pid = '$pid' AND form_encounter.encounter = forms.encounter " .
+  "ORDER BY form_encounter.date DESC, fdate ASC");
+
+ while($result = sqlFetchArray($res)) {
+  if ($result{"form_name"} == "New Patient Encounter") {
+   if ($isfirst == 0) {
+    print "</blockquote>\n\n";
+   }
+   $isfirst = 0;
+
+   print "<input type=checkbox name='" . $result{"formdir"} . "_" .
+    $result{"form_id"} . "' value='" . $result{"encounter"} . "'";
+   print " >New Encounter" .
+    " (" . date("Y-m-d",strtotime($result{"date"})) .
+    ")<blockquote>\n";
+  } else {
+   print "<input type='checkbox' name='" . $result{"formdir"} . "_" .
+    $result{"form_id"} . "' value='" . $result{"encounter"} . "'";
+   print ">" . $result{"form_name"} . "<br>\n";
+  }
+  //call_user_func($result{"formdir"} . "_report", $pid, $result{"encounter"}, $cols, $result{"form_id"});
 }
 ?>
 </blockquote>
+
+  </td>
+ </tr>
+</table>
+
 <span class="bold">Documents</span>:<br>
 <ul>
 <?
-
-//code lists available images
-$db = $GLOBALS['adodb']['db'];
-$sql = "SELECT d.id,d.url,c.name from documents as d LEFT JOIN categories_to_documents as ctd on d.id=ctd.document_id LEFT JOIN categories as c on c.id = ctd.category_id where d.foreign_id = " . $db->qstr($pid);
-$result = $db->Execute($sql);
-echo $db->ErrorMsg();
-while ($result && !$result->EOF) {
-	echo '<span class="bold"><input type="checkbox" name="documents[]" value="' . $result->fields['id'] . '">';
-	echo '&nbsp;&nbsp;<i>' . $result->fields['name'] . "</i>";
-	echo '&nbsp;&nbsp;Name: <i>' . basename($result->fields['url']) . "</i>";
-	echo '</span><br>';
-	$result->MoveNext();	
-}
-
+ //code lists available images
+ $db = $GLOBALS['adodb']['db'];
+ $sql = "SELECT d.id, d.url, c.name FROM documents AS d " .
+  "LEFT JOIN categories_to_documents AS ctd ON d.id=ctd.document_id " .
+  "LEFT JOIN categories AS c ON c.id = ctd.category_id WHERE " .
+  "d.foreign_id = " . $db->qstr($pid);
+ $result = $db->Execute($sql);
+ echo $db->ErrorMsg();
+ while ($result && !$result->EOF) {
+  echo '<span class="bold"><input type="checkbox" name="documents[]" value="' .
+   $result->fields['id'] . '">';
+  echo '&nbsp;&nbsp;<i>' . $result->fields['name'] . "</i>";
+  echo '&nbsp;&nbsp;Name: <i>' . basename($result->fields['url']) . "</i>";
+  echo '</span><br>';
+  $result->MoveNext();	
+ }
 ?>
 </ul>
 </form>
 
-<a href="javascript:document.report_form.submit();" class=link_submit>Generate Report</a>
-
-
+<a href='javascript:document.report_form.submit()' class='link_submit'>Generate Report</a>
 
 </body>
 </html>
--- interface/patient_file/report/print_custom_report.php
+++ interface/patient_file/report/print_custom_report.php
@@ -1,162 +1,178 @@
 <?
-include_once("../../globals.php");
+ include_once("../../globals.php");
+ include_once("$srcdir/forms.inc");
+ include_once("$srcdir/billing.inc");
+ include_once("$srcdir/pnotes.inc");
+ include_once("$srcdir/patient.inc");
+ include_once("$srcdir/report.inc");
+ include_once(dirname(__file__) . "/../../../library/classes/Document.class.php");
+ include_once(dirname(__file__) . "/../../../library/classes/Note.class.php");
 
+ $N = 6;
+ $first_issue = 1;
 
-include_once("$srcdir/forms.inc");
-include_once("$srcdir/billing.inc");
-include_once("$srcdir/pnotes.inc");
-include_once("$srcdir/patient.inc");
-include_once("$srcdir/report.inc");
-include_once(dirname(__file__) . "/../../../library/classes/Document.class.php");
-include_once(dirname(__file__) . "/../../../library/classes/Note.class.php");
-
-$N = 6;
-function postToGet($arin) {
-$getstring="";
-foreach ($arin as $key => $val) {
-$getstring.=urlencode($key)."=".urlencode($val)."&";
-}
-return $getstring;
-}
+ function postToGet($arin) {
+  $getstring="";
+  foreach ($arin as $key => $val) {
+   $getstring.=urlencode($key)."=".urlencode($val)."&";
+  }
+  return $getstring;
+ }
 ?>
-
 <html>
 <head>
-
-
 <link rel=stylesheet href="<?echo $css_header;?>" type="text/css">
-
-
 </head>
+
 <body bgcolor="#ffffff" topmargin=0 rightmargin=0 leftmargin=2 bottommargin=0 marginwidth=2 marginheight=0>
-
-<?
-if (sizeof($_GET) > 0) {
-$ar = $_GET;
-} else {
-$ar = $_POST;
-}
-
-$titleres = getPatientData($pid, "fname,lname,providerID");
-$sql = "select * from facility where billing_location = 1";
-$db = $GLOBALS['adodb']['db'];
-$results = $db->Execute($sql);
-$facility = array();
-if (!$results->EOF) {
-	$facility = $results->fields; 	
-} 
-
-?>
 <p>
+<?
+ if (sizeof($_GET) > 0) {
+  $ar = $_GET;
+ } else {
+  $ar = $_POST;
+ }
+
+ $titleres = getPatientData($pid, "fname,lname,providerID");
+ $sql = "select * from facility where billing_location = 1";
+ $db = $GLOBALS['adodb']['db'];
+ $results = $db->Execute($sql);
+ $facility = array();
+ if (!$results->EOF) {
+  $facility = $results->fields;
+ }
+
+ $practice_logo = "../../../custom/practice_logo.gif";
+ if (file_exists($practice_logo)) {
+  echo "<img src='$practice_logo' align='left'>\n";
+ }
+?>
 <h2><?=$facility['name']?></h2>
 <?=$facility['street']?><br>
-<?=$facility['city']?>, <?=$facility['state']?> <?=$facility['postal_code']?><br>
+<?=$facility['city']?>, <?=$facility['state']?> <?=$facility['postal_code']?><br clear='all'>
 
 </p>
 
 <a href="javascript:window.close();"><font class=title><?print $titleres{"fname"} . " " . $titleres{"lname"};?></font></a><br>
 <span class=text>Generated on: <?print date("Y-m-d");?></span>
-<br>
+<br><br>
 
 <?
-//$provider = getProviderName($titleres['providerID']);
+ //$provider = getProviderName($titleres['providerID']);
 
-//print "Provider: " . $provider  . "</br>";
+ //print "Provider: " . $provider  . "</br>";
 
-$inclookupres = sqlStatement("select distinct formdir from forms where pid='$pid'");
-while($result = sqlFetchArray($inclookupres)) {
-	include_once("{$GLOBALS['incdir']}/forms/" . $result{"formdir"} . "/report.php");
-}
+ $inclookupres = sqlStatement("select distinct formdir from forms where pid='$pid'");
+ while($result = sqlFetchArray($inclookupres)) {
+  include_once("{$GLOBALS['incdir']}/forms/" . $result{"formdir"} . "/report.php");
+ }
 
-$printed = false;
+ $printed = false;
 
-foreach ($ar as $key => $val) {
-if(!empty($ar['newpatient'])){
-	foreach ($ar['newpatient'] as $be) {	
-						
-		$ta = split(":",$be);
-		$billing = getPatientBillingEncounter($pid,$ta[1]);
+ foreach ($ar as $key => $val) {
+  if (!$printed && strpos($key, "newpatient_") === 0) {
+   $billing = getPatientBillingEncounter($pid, $val);
+   foreach ($billing as $b) {
+    if(!empty($b['provider_name'])) {
+     echo "Provider: " . $b['provider_name'] . "<br>";
+     $printed = true;
+     break;
+    }
+   }
+  }
+ }
 
-		if(!$printed){		
-			foreach ($billing as $b) {
-				if(!empty($b['provider_name'])){
-					echo "Provider: " . $b['provider_name'] . "<br>";
-					$printed = true;
-					break;
-				}
-			}
-		}
-	}
-}
+ foreach ($ar as $key => $val) {
 
-if (stristr($key,"include_")) {
-	//print "include: $val<br>\n";
+  /****
+  // WTF??  Redo this.
+  if (!empty($ar['newpatient'])){
+   foreach ($ar['newpatient'] as $be) {
+    $ta = split(":", $be);
+    $billing = getPatientBillingEncounter($pid, $ta[1]);
+    if(!$printed) {
+     foreach ($billing as $b) {
+      if(!empty($b['provider_name'])) {
+       echo "Provider: " . $b['provider_name'] . "<br>";
+       $printed = true;
+       break;
+      }
+     }
+    }
+   }
+  }
+  ****/
 
-	if ($val == "demographics") {
-		
-		print "<font class=bold>Patient Data:</font><br>";
-		printRecDataOne($patient_data_array, getRecPatientData ($pid), $N);
-	
-		} elseif ($val == "history") {
-	
-			print "<font class=bold>History Data:</font><br>";
-			printRecDataOne($history_data_array, getRecHistoryData ($pid), $N);
-		
-		} elseif ($val == "employer") {
-	
-			print "<font class=bold>Employer Data:</font><br>";
-			printRecDataOne($employer_data_array, getRecEmployerData ($pid), $N);
-	
-		} elseif ($val == "insurance") {
-		
-			print "<font class=bold>Primary Insurance Data:</font><br>";
-			printRecDataOne($insurance_data_array, getRecInsuranceData ($pid,"primary"), $N);		
-			print "<font class=bold>Secondary Insurance Data:</font><br>";	
-			printRecDataOne($insurance_data_array, getRecInsuranceData ($pid,"secondary"), $N);
-			
-			print "<font class=bold>Tertiary Insurance Data:</font><br>";
-			printRecDataOne($insurance_data_array, getRecInsuranceData ($pid,"tertiary"), $N);
-			
-		} elseif ($val == "billing") {
-			print "<font class=bold>Billing Information:</font><br>";
-			if (count($ar['newpatient']) > 0) {
-				$billings = array();
-				echo "<table>";
-				echo "<tr><td width=\"400\" class=bold>Code</td><td class=bold>Fee</td></tr>\n";
-				$total = 0.00;
-				$copays = 0.00;
-				foreach ($ar['newpatient'] as $be) {
-				  $ta = split(":",$be);
-				  $billing = getPatientBillingEncounter($pid,$ta[1]);
-				  $billings[] = $billing;
-				  foreach ($billing as $b) {
-				    echo "<tr>\n";
-				    echo "<td class=text>";
-				    echo $b['code_type'] . ":\t" . $b['code'] . "&nbsp;&nbsp;&nbsp;" . $b['code_text'] . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-				    echo "</td>\n";
-				    echo "<td class=text>";
-				    echo $b['fee'];
-				    echo "</td>\n";
-				    echo "</tr>\n";
-				    $total += $b['fee'];
-				    if ($b['code_type'] == "COPAY") {
-				      $copays += $b['fee'];
-				    }
-				  }
-				}
-											    echo "<tr><td>&nbsp;</td></tr>";
-											    echo "<tr><td class=bold>Sub-Total</td><td class=text>" . sprintf("%0.2f",$total) . "</td></tr>";
-											    echo "<tr><td class=bold>Paid</td><td class=text>" . sprintf("%0.2f",$copays) . "</td></tr>";
-				echo "<tr><td class=bold>Total</td><td class=text>" . sprintf("%0.2f",($total - $copays)) . "</td></tr>";
-				echo "</table>";
-				echo "<pre>";
-				//print_r($billings);
-				echo "</pre>";
-			}
-			else {
-				printPatientBilling($pid);
-			}
-			
+  if (stristr($key,"include_")) {
+  //print "include: $val<br>\n";
+
+   if ($val == "demographics") {
+
+    print "<br><font class=bold>Patient Data:</font><br>";
+    printRecDataOne($patient_data_array, getRecPatientData ($pid), $N);
+
+   } elseif ($val == "history") {
+
+    print "<br><font class=bold>History Data:</font><br>";
+    printRecDataOne($history_data_array, getRecHistoryData ($pid), $N);
+
+   } elseif ($val == "employer") {
+
+    print "<br><font class=bold>Employer Data:</font><br>";
+    printRecDataOne($employer_data_array, getRecEmployerData ($pid), $N);
+
+   } elseif ($val == "insurance") {
+
+    print "<br><font class=bold>Primary Insurance Data:</font><br>";
+    printRecDataOne($insurance_data_array, getRecInsuranceData ($pid,"primary"), $N);		
+    print "<font class=bold>Secondary Insurance Data:</font><br>";	
+    printRecDataOne($insurance_data_array, getRecInsuranceData ($pid,"secondary"), $N);
+    print "<font class=bold>Tertiary Insurance Data:</font><br>";
+    printRecDataOne($insurance_data_array, getRecInsuranceData ($pid,"tertiary"), $N);
+
+   } elseif ($val == "billing") {
+
+    print "<br><font class=bold>Billing Information:</font><br>";
+    if (count($ar['newpatient']) > 0) {
+     $billings = array();
+     echo "<table>";
+     echo "<tr><td width=\"400\" class=bold>Code</td><td class=bold>Fee</td></tr>\n";
+     $total = 0.00;
+     $copays = 0.00;
+     foreach ($ar['newpatient'] as $be) {
+      $ta = split(":",$be);
+      $billing = getPatientBillingEncounter($pid,$ta[1]);
+      $billings[] = $billing;
+      foreach ($billing as $b) {
+       echo "<tr>\n";
+       echo "<td class=text>";
+       echo $b['code_type'] . ":\t" . $b['code'] . "&nbsp;&nbsp;&nbsp;" . $b['code_text'] . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+       echo "</td>\n";
+       echo "<td class=text>";
+       echo $b['fee'];
+       echo "</td>\n";
+       echo "</tr>\n";
+       $total += $b['fee'];
+       if ($b['code_type'] == "COPAY") {
+        $copays += $b['fee'];
+       }
+      }
+     }
+     echo "<tr><td>&nbsp;</td></tr>";
+     echo "<tr><td class=bold>Sub-Total</td><td class=text>" . sprintf("%0.2f",$total) . "</td></tr>";
+     echo "<tr><td class=bold>Paid</td><td class=text>" . sprintf("%0.2f",$copays) . "</td></tr>";
+     echo "<tr><td class=bold>Total</td><td class=text>" . sprintf("%0.2f",($total - $copays)) . "</td></tr>";
+     echo "</table>";
+     echo "<pre>";
+     //print_r($billings);
+     echo "</pre>";
+    }
+    else {
+     printPatientBilling($pid);
+    }
+
+
+    /****
 		} elseif ($val == "allergies") {
 			print "<font class=bold>Patient Allergies:</font><br>";
 			printListData($pid, "allergy", "1");
@@ -166,79 +182,120 @@ if (stristr($key,"include_")) {
 		} elseif ($val == "medical_problems") {
 				print "<font class=bold>Patient Medical Problems:</font><br>";
 				printListData($pid, "medical_problem", "1");
-		} elseif ($val == "immunizations") {
-				print "<font class=bold>Patient Immunization:</font><br>";
-                                $sql = "select if(i1.administered_date,concat(i1.administered_date,' - ',i2.name) ,substring(i1.note,1,20) ) as immunization_data from immunizations i1 left join immunization i2 on i1.immunization_id = i2.id where i1.patient_id = $pid order by administered_date desc";
+    ****/
 
-                                $result = sqlStatement($sql);
+   } elseif ($val == "immunizations") {
+    print "<font class=bold>Patient Immunization:</font><br>";
+    $sql = "select if(i1.administered_date,concat(i1.administered_date,' - ',i2.name) ,substring(i1.note,1,20) ) as immunization_data from immunizations i1 left join immunization i2 on i1.immunization_id = i2.id where i1.patient_id = $pid order by administered_date desc";
+    $result = sqlStatement($sql);
+    while ($row=sqlFetchArray($result)) {
+     echo "<span class=text> " . $row{'immunization_data'} . "</span><br>\n";
+    }
 
-                                while ($row=sqlFetchArray($result)){
-                                        echo "<span class=text> " . $row{'immunization_data'} . "</span>
-<br>\n";
-                                }
-		} elseif ($val == "notes") {
-				print "<font class=bold>Patient Notes:</font><br>";
-				printPatientNotes($pid);
-		} elseif ($val == "transactions") {
-			print "<font class=bold>Patient Transactions:</font><br>";
-			printPatientTransactions($pid);
-		}	
-	} else {
-		if ($key == "documents") {
-			echo "<br><br>";
-			foreach($val as $valkey => $valvalue) {
-				$document_id = $valvalue;
-				if (!is_numeric($document_id)) continue;
-				$d = new Document($document_id);
-				$fname = basename($d->get_url());
-				$extension = substr($fname, strrpos($fname,"."));
-				echo "Document '" . $fname ."'<br>";
-				$notes = Note::notes_factory($d->get_id());
-				echo "<table>";
-				foreach ($notes as $note) {
-					
-					echo '<tr>';
-	      			echo '<td>Note #' . $note->get_id() . '</td>';
-	    			echo '</tr>';
-	    			echo '<tr>';
-	      			echo '<td>Date: '.$note->get_date().'</td>';
-	    			echo '</tr>';
-	    			echo '<tr>';
-	      			echo '<td>'.$note->get_note().'<br><br></td>';
-	    			echo '</tr>';
-						
-				}
-				echo "</table>";
-				if ($extension == ".png" || $extension == ".jpg" || $extension == ".jpeg" || $extension == ".gif") {
-					echo '<img src="' . $GLOBALS['webroot'] . "/controller.php?document&retrieve&patient_id=&document_id=" . $document_id . '"><br><br>';
-				}
-				else {
-					echo "<b>NOTE</b>: Document '" . $fname ."' cannot be displayed inline becuase its type is not supported by the browser.<br><br>";	
-				}
-			}
-		}
-		else {
-		//print "$key:$val<br>\n";
-		foreach ($val as $valkey => $valvalue ) {
-			preg_match_all('/^(\d+?)\:(\d+?)$/',$valvalue,$res);
-			
-			$form_id = $res[1][0];
-			$form_encounter = $res[2][0];
-			//print $res[0][0] . " " . $res[1][0] . " " . $res[2][0] . "<br>\n";
-		
-			$formres = getFormNameByFormdir($key);
-			$dateres = getEncounterDateByEncounter($form_encounter);
-			print "<span class=bold>" . $formres{"form_name"} . "</span><span class=text>(" . date("Y-m-d",strtotime($dateres{"date"})) . ")" . "</span><br>\n";
-			call_user_func($key . "_report", $pid, $form_encounter, $N, $form_id);
-		}
-		}
-	}
-}
+   } elseif ($val == "notes") {
 
-print "</br></br>Signature: _______________________________</br>";
+    print "<font class=bold>Patient Notes:</font><br>";
+    printPatientNotes($pid);
+
+   } elseif ($val == "transactions") {
+
+    print "<font class=bold>Patient Transactions:</font><br>";
+    printPatientTransactions($pid);
+
+   }
+
+  } else {
+
+   if ($key == "documents") {
+    echo "<br><br>";
+    foreach($val as $valkey => $valvalue) {
+     $document_id = $valvalue;
+     if (!is_numeric($document_id)) continue;
+     $d = new Document($document_id);
+     $fname = basename($d->get_url());
+     $extension = substr($fname, strrpos($fname,"."));
+     echo "Document '" . $fname ."'<br>";
+     $notes = Note::notes_factory($d->get_id());
+     echo "<table>";
+     foreach ($notes as $note) {
+      echo '<tr>';
+      echo '<td>Note #' . $note->get_id() . '</td>';
+      echo '</tr>';
+      echo '<tr>';
+      echo '<td>Date: '.$note->get_date().'</td>';
+      echo '</tr>';
+      echo '<tr>';
+      echo '<td>'.$note->get_note().'<br><br></td>';
+      echo '</tr>';
+     }
+     echo "</table>";
+     if ($extension == ".png" || $extension == ".jpg" || $extension == ".jpeg" || $extension == ".gif") {
+      echo '<img src="' . $GLOBALS['webroot'] . "/controller.php?document&retrieve&patient_id=&document_id=" . $document_id . '"><br><br>';
+     }
+     else {
+      echo "<b>NOTE</b>: Document '" . $fname ."' cannot be displayed inline becuase its type is not supported by the browser.<br><br>";	
+     }
+    }
+   }
+
+   else if (strpos($key, "issue_") === 0) {
+
+    if ($first_issue) {
+     $first_issue = 0;
+     echo "<br>\n";
+    }
+    preg_match('/^(.*)_(\d+)$/', $key, $res);
+    $rowid = $res[2];
+    $irow = sqlQuery("SELECT type, title, comments, diagnosis " .
+     "FROM lists WHERE id = '$rowid'");
+    $diagnosis = $irow['diagnosis'];
+    echo "<span class='bold'>" . $irow['title'] . ":</span><span class='text'> " .
+     $irow['comments'] . "</span><br>\n";
+    // Show issue's chief diagnosis and its description:
+    if ($diagnosis) {
+     $crow = sqlQuery("SELECT code_text FROM codes WHERE " .
+      "code = '$diagnosis' AND " .
+      "(code_type = 2 OR code_type = 4 OR code_type = 5)" .
+      "LIMIT 1");
+     echo "<span class='bold'>&nbsp;Diagnosis: </span><span class='text'>" .
+      $irow['diagnosis'] . " " . $crow['code_text'] . "</span><br>\n";
+    }
+
+   }
+
+   // Otherwise we have an "encounter form" form field whose name is like
+   // dirname_formid, with a value which is the encounter ID.
+   //
+   else {
+
+    $form_encounter = $val;
+    preg_match('/^(.*)_(\d+)$/', $key, $res);
+    $form_id = $res[2];
+    $formres = getFormNameByFormdir($res[1]);
+    $dateres = getEncounterDateByEncounter($form_encounter);
+    if ($res[1] == 'newpatient') print "<br>\n";
+    print "<span class='bold'>" . $formres{"form_name"} .
+     "</span><span class=text>(" . date("Y-m-d",strtotime($dateres{"date"})) .
+     ")" . "</span><br>\n";
+    call_user_func($res[1] . "_report", $pid, $form_encounter, $N, $form_id);
+    if ($res[1] == 'newpatient') {
+     $bres = sqlStatement("SELECT date, code, code_text FROM billing WHERE " .
+      "encounter = '$form_encounter' AND activity = 1 AND " .
+      "(code_type = 'CPT4' OR code_type = 'OPCS') " .
+      "ORDER BY date");
+     while ($brow=sqlFetchArray($bres)) {
+      echo "<span class='bold'>&nbsp;Procedure: </span><span class='text'>" .
+        $brow['code'] . " " . $brow['code_text'] . "</span><br>\n";
+     }
+    }
+
+   }
+  }
+ }
+
+ print "</br></br>Signature: _______________________________</br>";
 
 ?>
-
 
 </body>
 </html>
