5a746dd5872b40861a1dff0af13ea747e6d47ef2
sunsetsystems <sunsetsystems>
2005-08-26 17:10:06 +0000

    cosmetics

--- interface/new/new_patient_save.php
+++ interface/new/new_patient_save.php
@@ -1,9 +1,8 @@
 <?
-include_once("../globals.php");
-
-include_once("$srcdir/sql.inc");
-include_once("$srcdir/pid.inc");
-include_once("$srcdir/patient.inc");
+ include_once("../globals.php");
+ include_once("$srcdir/sql.inc");
+ include_once("$srcdir/pid.inc");
+ include_once("$srcdir/patient.inc");
 
 //here, we lock the patient data table while we find the most recent max PID
 //other interfaces can still read the data during this lock, however
@@ -11,92 +10,98 @@ sqlStatement("lock tables patient_data read");
 
 $result = sqlQuery("select max(pid)+1 as pid from patient_data");
 
+// TBD: This looks wrong to unlock the table before we have added our
+// patient with its newly allocated pid!
+//
 sqlStatement("unlock tables");
 //end table lock
 $newpid = 1;
 
 if ($result['pid'] > 1)
-	$newpid = $result['pid'];
+  $newpid = $result['pid'];
 
 setpid($newpid);
 
 if($pid == NULL) {
-	$pid = 0;
+  $pid = 0;
 }
 
 //what do we set for the public pid?
 if (isset($_POST["pubpid"]) && ($_POST["pubpid"] != "")) {
-	$mypubpid = $_POST["pubpid"];
+  $mypubpid = $_POST["pubpid"];
 } else {
-	$mypubpid = $pid;
+  $mypubpid = $pid;
 }
 
+if ($_POST['form_create']) {
 
+ $form_fname = ucwords(trim($_POST["fname"]));
+ $form_lname = ucwords(trim($_POST["lname"]));
+ $form_mname = ucwords(trim($_POST["mname"]));
 
-newPatientData(         $_POST["db_id"],
-			$_POST["title"],
-                        ucwords($_POST["fname"]),
-                        ucwords($_POST["lname"]),
-                        ucwords($_POST["mname"]),
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-			"",
-			"",
-			"",
-			"",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "$mypubpid",
-                        $pid
-                );
+ newPatientData(
+  $_POST["db_id"],
+  $_POST["title"],
+  $form_fname,
+  $form_lname,
+  $form_mname,
+  "", // sex
+  "", // dob
+  "", // street
+  "", // postal_code
+  "", // city
+  "", // state
+  "", // country_code
+  "", // ss
+  "", // occupation
+  "", // phone_home
+  "", // phone_biz
+  "", // phone_contact
+  "", // status
+  "", // contact_relationship
+  "", // referrer
+  "", // referrerID
+  "", // email
+  "", // language
+  "", // ethnoracial
+  "", // interpreter
+  "", // migrantseasonal
+  "", // family_size
+  "", // monthly_income
+  "", // homeless
+  "", // financial_review
+  "$mypubpid",
+  $pid
+      // providerID
+      // genericname1
+      // genericval1
+      // genericname2
+      // genericval2
+      // phone_cell
+      // hipaa_mail
+      // hipaa_voice
+      // squad
+ );
 
-
-newEmployerData( $pid);
-
-newHistoryData( $pid);
-
-
-newInsuranceData( $pid, "primary");
-
-
-
-
-newInsuranceData( $pid, "secondary");
-
-
-newInsuranceData( $pid, "tertiary");
-
-
-
-
-
-
+ newEmployerData($pid);
+ newHistoryData($pid);
+ newInsuranceData($pid, "primary");
+ newInsuranceData($pid, "secondary");
+ newInsuranceData($pid, "tertiary");
+}
 ?>
 <html>
 <body>
 <script language="Javascript">
 <!--
+<?
+ if ($alertmsg) { 
+  echo "alert('$alertmsg');\n";
+ }
+?>
 window.location="<?echo "$rootdir/patient_file/patient_file.php?set_pid=$pid";?>";
 -->
 </script>
-
 
 </body>
 </html>
