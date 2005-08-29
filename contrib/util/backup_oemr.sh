869655955dc6802da69e0c9b409e1b3e41e052c0
andres_paglayan <andres_paglayan>
2005-08-29 22:20:10 +0000

    added bzip and newer-than date to the tar part of the script.

--- contrib/util/backup_oemr.sh
+++ contrib/util/backup_oemr.sh
@@ -10,6 +10,9 @@ pguser='sql-ledger'
 workdir='/var/tmp/backups/'
 tempdir='/var/tmp/isos/'
 installdir='/var/www/openemr'
+newerthan='2005-01-01'
+#newerthan is a workaround for when your media gets full,
+#re-edit this file and change the date to the last succesfull cd copy
 
 # a name for the file that is unique of that day
 file=`date +%F`
@@ -27,7 +30,8 @@ pg_dumpall -U ${pguser} > ${workdir}${file}postgres.bak
 bzip2 ${workdir}${file}postgres.bak
 
 # get all the files on the site
-tar -zcf ${workdir}${file}openemr.tar.gz ${installdir}
+# we are using bzip2 here
+tar -N ${newerthan} -jcf ${workdir}${file}openemr.tar.bzip2 ${installdir}
 
 # create a disk image
 label=`date +%y%m%d`
