d5e7db99dec2e0fd15478d729184dd093f9ed672
andres_paglayan <andres_paglayan>
2005-10-21 19:49:26 +0000

    batchcom table added

--- sql/2_7_2-to-2_7_3_upgrade.sql
+++ sql/2_7_2-to-2_7_3_upgrade.sql
@@ -56,3 +56,19 @@ ALTER TABLE openemr_postcalendar_events
 
 ALTER TABLE lists
   ADD diagnosis varchar(255) NOT NULL DEFAULT '';
+
+## 
+## Table structure for table `batchcom`
+## 
+
+CREATE TABLE IF NOT EXISTS `batchcom` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `patient_id` int(11) NOT NULL default '0',
+  `sent_by` bigint(20) NOT NULL default '0',
+  `msg_type` varchar(60) NOT NULL default '',
+  `msg_subject` varchar(255) NOT NULL default '',
+  `msg_text` mediumtext NOT NULL,
+  `msg_date_sent` datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM ;
+
