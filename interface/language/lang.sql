774461378cefb649ec1543ac9c71253f36fbe1a4
andres_paglayan <andres_paglayan>
2005-11-16 17:45:16 +0000

    table definition for information only

---
+++ interface/language/lang.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 2.6.1
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Nov 16, 2005 at 10:28 AM
+-- Server version: 4.1.11
+-- PHP Version: 4.3.9-1
+-- 
+-- Database: `openemr1018`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `lang_constants`
+-- 
+
+CREATE TABLE IF NOT EXISTS `lang_constants` (
+  `cons_id` int(11) NOT NULL auto_increment,
+  `constant_name` varchar(255) NOT NULL default '',
+  UNIQUE KEY `cons_id` (`cons_id`),
+  KEY `cons_name` (`constant_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+-- 
+-- Dumping data for table `lang_constants`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `lang_definitions`
+-- 
+
+CREATE TABLE IF NOT EXISTS `lang_definitions` (
+  `def_id` int(11) NOT NULL auto_increment,
+  `cons_id` int(11) NOT NULL default '0',
+  `lang_id` int(11) NOT NULL default '0',
+  `definition` mediumtext NOT NULL,
+  UNIQUE KEY `def_id` (`def_id`),
+  KEY `definition` (`definition`(100))
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
+
+-- 
+-- Dumping data for table `lang_definitions`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `lang_languages`
+-- 
+
+CREATE TABLE IF NOT EXISTS `lang_languages` (
+  `lang_id` int(11) NOT NULL auto_increment,
+  `lang_code` char(2) NOT NULL default '',
+  `lang_description` varchar(100) NOT NULL default '',
+  UNIQUE KEY `lang_id` (`lang_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
+
+-- 
+-- Dumping data for table `lang_languages`
+-- 
+
+INSERT INTO `lang_languages` (`lang_id`, `lang_code`, `lang_description`) VALUES (1, 'en', 'English');
+INSERT INTO `lang_languages` (`lang_id`, `lang_code`, `lang_description`) VALUES (2, 'se', 'Swedish');
+INSERT INTO `lang_languages` (`lang_id`, `lang_code`, `lang_description`) VALUES (3, 'es', 'Spanish');
