8af8e0f0ef5bfab49be4dce39a733ef87514eba9
andres_paglayan <andres_paglayan>
2005-12-07 17:40:58 +0000

    echo corrected

--- interface/batchcom/batchPhoneList.php
+++ interface/batchcom/batchPhoneList.php
@@ -13,19 +13,19 @@
 <link rel=stylesheet href="batchcom.css" type="text/css">
 </head>
 <body <?echo $top_bg_line;?> topmargin=0 rightmargin=0 leftmargin=2 bottommargin=0 marginwidth=2 marginheight=0>
-<span class="title"><?xl('Batch Communication Tool','e')?></span>
+<span class="title"><? xl('Batch Communication Tool','e')?></span>
 <br><br>
 
 <?
 
 echo ("<table>"); //will do css
 
-echo ("<tr><td>xl('Name') </td>");
-echo ("<td>xl('DOB') </td>");
-echo ("<td>xl('Home')</td>");
-echo ("<td>xl('Work') </td>");
-echo ("<td>xl('Contact') </td>");
-echo ("<td>$Cell </td></tr>\n");
+echo ("<tr><td>".xl('Name') ."</td>");
+echo ("<td>".xl('DOB') ."</td>");
+echo ("<td>".xl('Home')."</td>");
+echo ("<td>".xl('Work') ."</td>");
+echo ("<td>".xl('Contact') ."</td>");
+echo ("<td>".$Cell."</td></tr>\n");
 
 
 while ($row=sqlFetchArray($res)) {
