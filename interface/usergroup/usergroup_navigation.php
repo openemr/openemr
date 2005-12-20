863731e08e9e41e010dea24bfb03c72de4ba8b11
sunsetsystems <sunsetsystems>
2005-12-20 20:47:04 +0000

    was getting too wide, reworded to fit better

--- interface/usergroup/usergroup_navigation.php
+++ interface/usergroup/usergroup_navigation.php
@@ -19,56 +19,65 @@ include_once("../../library/acl.inc");
 
 <? if (acl_check('admin', 'users')) { ?>
 <td valign="middle" nowrap>
-&nbsp;&nbsp;<a class=menu target=Main href="usergroup_admin.php">Users & Groups</a>&nbsp;
+&nbsp;&nbsp;<a class=menu target=Main href="usergroup_admin.php"
+ title="Add or Edit Users, Groups and Facilities">Users & Groups</a>&nbsp;
 </td>
 <? } ?>
 
 <? if (acl_check('admin', 'forms')) { ?>
 <td valign="middle" nowrap>
-&nbsp;<a class=menu target=Main href="../forms_admin/forms_admin.php">Forms Settings</a>&nbsp;
+&nbsp;<a class=menu target=Main href="../forms_admin/forms_admin.php"
+ title="Activate New Forms">Forms</a>&nbsp;
 </td>
 <? } ?>
 
 <? if (acl_check('admin', 'practice')) { ?>
 <td valign="middle" nowrap>
-&nbsp;<a class=menu target=Main href="<?=$GLOBALS['webroot']?>/controller.php?practice_settings">Practice Settings</a>&nbsp;
+&nbsp;<a class=menu target=Main href="<?=$GLOBALS['webroot']?>/controller.php?practice_settings"
+ title="Practice Settings">Practice</a>&nbsp;
 </td>
 <? } ?>
 
 <? if (acl_check('admin', 'calendar')) { ?>
 <td valign="middle" nowrap>
-&nbsp;<a class=menu target=Main href="../main/calendar/index.php?module=PostCalendar&type=admin&func=modifyconfig">Calendar Settings</a>&nbsp;
+&nbsp;<a class=menu target=Main href="../main/calendar/index.php?module=PostCalendar&type=admin&func=modifyconfig"
+ title="Calendar Settings">Calendar</a>&nbsp;
 </td>
 <? } ?>
 
 <? if (acl_check('admin', 'database')) { ?>
 <td valign="middle" nowrap>
-&nbsp;<a class=menu target=Main href="../main/myadmin/index.php">Database&nbsp;Reporting</a>&nbsp;
+&nbsp;<a class=menu target=Main href="../main/myadmin/index.php"
+ title="Database Reporting">Database</a>&nbsp;
 </td>
 <? } ?>
 
 <? if (acl_check('admin', 'batchcom')) { ?>
 <td valign="middle" nowrap>
-&nbsp;<a class=menu target=Main href="../batchcom/batchcom.php">Batch Com</a>&nbsp;
+&nbsp;<a class=menu target=Main href="../batchcom/batchcom.php"
+ title="Batch Communication and Export">BatchCom</a>&nbsp;
 </td>
 <? } ?>
 
 <? if (acl_check('admin', 'language')) { ?>
 <td valign="middle" nowrap>
-&nbsp;<a class=menu target=Main href="../language/language.php">Language</a>&nbsp;
+&nbsp;<a class=menu target=Main href="../language/language.php"
+ title="Language Management">Language</a>&nbsp;
 </td>
 <? } ?>
 
 
 <? if (acl_check('admin', 'users')) { ?>
 <td valign="middle" nowrap>
-&nbsp;<a class=menu target=Main href="<?echo $rootdir?>/logview/logview.php">View Logs</a>&nbsp;
+&nbsp;<a class=menu target=Main href="<?echo $rootdir?>/logview/logview.php"
+ title="View Logs">Logs</a>&nbsp;
 </td>
 <? } ?>
 
 <td valign="middle" align="right" nowrap>
 <?//<a href="../logout.php?auth=logout" target="_top" class="logout">Logout</a>?>
-&nbsp;<a class=menu href="../main/main_screen.php" target="_top" class="menu">Back</a>&nbsp;&nbsp;
+&nbsp;<a class=menu href="../main/main_screen.php" target="_top" class="menu"
+ title="Exit from Administration">Back</a>&nbsp;&nbsp;
 </td>
 </tr>
 </table>
