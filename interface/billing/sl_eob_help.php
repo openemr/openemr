86b7779037a1d2845e5dd0b6e72312786a70d61a
sunsetsystems <sunsetsystems>
2006-01-15 21:29:56 +0000

    fix another translation bug

--- interface/billing/sl_eob_help.php
+++ interface/billing/sl_eob_help.php
@@ -1,3 +1,13 @@
+<?php
+ // Copyright (C) 2005 Rod Roark <rod@sunsetsystems.com>
+ //
+ // This program is free software; you can redistribute it and/or
+ // modify it under the terms of the GNU General Public License
+ // as published by the Free Software Foundation; either version 2
+ // of the License, or (at your option) any later version.
+
+ include_once("../globals.php");
+?>
 <html>
 <head>
 <link rel=stylesheet href="<?echo $css_header;?>" type="text/css">
