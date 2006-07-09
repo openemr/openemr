6ac772ae9e68247c560fbfb83c93b0deb643928a
markleeds <markleeds>
2006-07-09 13:52:15 +0000

    cleanup

--- interface/forms/CAMOS/report.php
+++ interface/forms/CAMOS/report.php
@@ -6,12 +6,10 @@ include_once("content_parser.php");
 function CAMOS_report( $pid, $encounter, $cols, $id) {
   $data = formFetch("form_CAMOS", $id);
   if ($data) {
-    echo "(category) ".stripslashes($data['category'])." | ";
-    echo "(subcategory) ".stripslashes($data['subcategory'])." | ";
-    echo "(item) ".stripslashes($data['item']);
-
-
-   echo "<pre>".stripslashes(content_parser($data['content']))."</pre><hr>\n";
+    //echo "(category) ".stripslashes($data['category'])." | ";
+    //echo "(subcategory) ".stripslashes($data['subcategory'])." | ";
+    //echo "(item) ".stripslashes($data['item']);
+    echo "<pre>".wordwrap(stripslashes(content_parser($data['content'])))."</pre><hr>\n";
   }
 }
 ?> 
--- interface/forms/CAMOS/save.php
+++ interface/forms/CAMOS/save.php
@@ -17,19 +17,29 @@ $field_names[$k] = mysql_real_escape_string($var);
 echo "$var\n";
 }
 
-process_commands($field_names['content']);
+$embedded_camos = process_commands($field_names['content']);
 $field_names['content'] = remove_comments($field_names['content']);
 
 if ($encounter == "")
 $encounter = date("Ymd");
 if ($_GET["mode"] == "new"){
-$newid = formSubmit("form_CAMOS", $field_names, $_GET["id"], $userauthorized);
-addForm($encounter, "CAMOS", $newid, "CAMOS", $pid, $userauthorized);
-}elseif ($_GET["mode"] == "update") {
-sqlInsert("update form_CAMOS set pid = {$_SESSION["pid"]},groupname='".$_SESSION["authProvider"]."',user='".$_SESSION["authUser"]."',authorized=$userauthorized,activity=1, date = NOW(), category='".$field_names["category"]."',subcategory='".$field_names["subcategory"]."',item='".$field_names["item"]."',content='".$field_names['content']."' where id=$id");
-}elseif ($_GET["mode"] == "delete") {
-sqlInsert("delete from form_CAMOS where id=$id and date(date) = date(now())");
-sqlInsert("delete from forms where form_name='CAMOS' and form_id=$id and pid='".$_SESSION["pid"]."' and date(date) = date(now())");
+  $newid = formSubmit("form_CAMOS", $field_names, $_GET["id"], $userauthorized);
+  addForm($encounter, "CAMOS", $newid, "CAMOS", $pid, $userauthorized);
+  //deal with embedded camos submissions here
+  foreach($embedded_camos as $val) {
+    foreach($val as $k => $v) {
+      $val[$k] = trim($v);  
+    } 
+    $newid = formSubmit("form_CAMOS", $val, $_GET["id"], $userauthorized);
+    addForm($encounter, "CAMOS", $newid, "CAMOS", $pid, $userauthorized);
+  }
+}
+elseif ($_GET["mode"] == "update") {
+  sqlInsert("update form_CAMOS set pid = {$_SESSION["pid"]},groupname='".$_SESSION["authProvider"]."',user='".$_SESSION["authUser"]."',authorized=$userauthorized,activity=1, date = NOW(), category='".$field_names["category"]."',subcategory='".$field_names["subcategory"]."',item='".$field_names["item"]."',content='".$field_names['content']."' where id=$id");
+}
+elseif ($_GET["mode"] == "delete") {
+  sqlInsert("delete from form_CAMOS where id=$id and date(date) = date(now())");
+  sqlInsert("delete from forms where form_name='CAMOS' and form_id=$id and pid='".$_SESSION["pid"]."' and date(date) = date(now())");
 }
 $_SESSION["encounter"] = $encounter;
 formHeader("Redirecting....");
