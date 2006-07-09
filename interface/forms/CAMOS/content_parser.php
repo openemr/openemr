cd07584b4dc8b198f4b6fdcf799a7f6012bf9a21
markleeds <markleeds>
2006-07-09 13:51:11 +0000

    support for CAMOS calling camos function added and support for 'justify' added.

--- interface/forms/CAMOS/content_parser.php
+++ interface/forms/CAMOS/content_parser.php
@@ -1,6 +1,20 @@
 <?
 include_once("../../globals.php");
-include_once("../../../library/billing.inc");	
+include_once("../../../library/sql.inc");	
+
+//This function was copied from billing.inc and altered to support 'justify'
+function addBilling2($encounter, $code_type, $code, $code_text, $modifier="",$units="",$fee="0.00",$justify)
+{
+  $justify_string = '';
+  if ($justify)
+  {
+    $justify_string = implode(":",$justify).":";
+  }
+  $sql = "insert into billing (date, encounter, code_type, code, code_text, pid, authorized, user, groupname,activity,billed,provider_id,modifier,units,fee,justify) values (NOW(), '".$_SESSION['encounter']."', '$code_type', '$code', '$code_text', '".$_SESSION['pid']."', '$authorized', '" . $_SESSION['authId'] . "', '" . $_SESSION['authProvider'] . "',1,0,".$_SESSION['authUserID'].",'$modifier','$units','$fee','$justify_string')";
+	
+  return sqlInsert($sql);
+	
+}
 
 function content_parser($input) {
 
@@ -34,26 +48,39 @@ function remove_dangling_comments($string_to_process) {
 
 function process_commands($string_to_process) {
 
+  $camos_return_data = array(); // to be filled with additional camos form submissions if any embedded
   $command_array = array();  //to be filled with potential commands
   $matches= array();  //to be filled with potential commands
-  if (!preg_match_all("/\/\*.*?\*\//",$string_to_process, $matches)) {return 0;}
+  if (!preg_match_all("/\/\*.*?\*\//",$string_to_process, $matches)) {return $camos_return_data;}
   $command_array = $matches[0];
   foreach($command_array as $val) {
     //process each command 
     $comm = preg_replace("/(\/\*)|(\*\/)/","",$val);
     $comm_array = split('::', $comm); //array where first element is command and rest are args
     //Here is where we process particular commands
-    if (trim($comm_array[0]) == 'billing') {
+    if (trim($comm_array[0])== 'billing') {
+      array_shift($comm_array); //couldn't do it in 'if' or would lose element 0 for next if
       //insert data into the billing table, see, easy!
-      $type = trim($comm_array[1]);  
-      $code = trim($comm_array[2]);  
-      $text = trim($comm_array[3]);  
-      $modifier = trim($comm_array[4]);  
-      $units = trim($comm_array[5]);  
-      $fee = trim($comm_array[6]);  
-      $encounter = $_SESSION["encounter"];
-      $pid = $_SESSION["pid"];
-      addBilling($encounter, $type, $code, $text, $pid, $userauthorized,$_SESSION['authUserID'],$modifier,$units,sprintf("%01.2f", $fee));
+      $type = trim(array_shift($comm_array));  
+      $code = trim(array_shift($comm_array));  
+      $text = trim(array_shift($comm_array));  
+      $modifier = trim(array_shift($comm_array));  
+      $units = trim(array_shift($comm_array));  
+      $fee = sprintf("%01.2f",trim(array_shift($comm_array)));  
+      //in function call 'addBilling' note last param is the remainder of the array.  we will look for justifications here...
+      addBilling2($encounter, $type, $code, $text, $modifier,$units,$fee,$comm_array);
+    }
+    if (trim($comm_array[0]) == 'camos') {
+      //data to be submitted as separate camos forms
+      //this is for embedded prescriptions, test orders etc... usually within a soap note or something  
+      //data collected here will be returned so that save.php can give it special treatment and insert
+      //into the database after the main form data is submitted so it will be in a sensible order 
+      array_push($camos_return_data, 
+        array("category" => trim($comm_array[1]), 
+        "subcategory" => trim($comm_array[2]), 
+        "item" => trim($comm_array[3]), 
+        "content" => trim($comm_array[4]))); 
     }
   }
+  return $camos_return_data;
 } 
