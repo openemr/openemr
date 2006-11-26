27479a9702bf796c1743c7029c8d584661f85aee
sunsetsystems <sunsetsystems>
2006-11-26 23:48:03 +0000

    revert experiemental code accidentally checked in

--- interface/patient_file/summary/patient_summary.php
+++ interface/patient_file/summary/patient_summary.php
@@ -9,17 +9,13 @@ include_once("../../globals.php");
 <frameset rows="50%,50%">
  <frame src="demographics.php" name="Demographics" scrolling="auto">
 <?php if ($GLOBALS['athletic_team']) { ?>
- <frameset cols="50%,*,50%">
+ <frameset cols="50%,50%">
 <?php } else { ?>
- <frameset cols="20%,*,80%">
+ <frameset cols="20%,80%">
 <?php } ?>
   <frame src="stats.php" name="Stats" scrolling="auto">
-  <frame src="<?php echo $GLOBALS['webroot'] ?>/controller.php?prescription&edit&id=&pid=<?php echo $pid ?>"
-   name="Prescription" scrolling="auto">
   <frame src="pnotes.php" name="Notes" scrolling="auto">
  </frameset>
 </frameset>
 
-<noframes><body bgcolor="#FFFFFF">
-</body></noframes>
 </html>
