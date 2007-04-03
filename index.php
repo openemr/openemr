818cea47c2454e24a202e6a1a6e08051bd85072d
lemonsoftwarero <lemonsoftwarero>
2007-04-03 05:46:42 +0000

    remove duplicate include; fix short php tags

--- index.php
+++ index.php
@@ -1,27 +1,20 @@
-<?php
-include_once("library/sqlconf.php");
-
-?>
-
 <html>
-<head>
+<head></head>
 
-
-</head>
 <?php
+
 include_once("library/sqlconf.php");
 
 if ($config == 1) {
 ?>                                                                                        
-<body ONLOAD="javascript:top.location.href='<?echo "interface/login/login_frame.php"?>';">
+<body ONLOAD="javascript:top.location.href='<?php echo "interface/login/login_frame.php"?>';">
 OpenEMR requires Javascript and a GUI browser. We can't promise you                       
 anything but try following <a href="interface/login/login.php"                            
 >this link</a> to continue.                                                               
 <?php                                                                                     
-}                                                                                         
-else {
+} else {
 ?>                                                                       
-<body ONLOAD="javascript:top.location.href='<?echo "setup.php"?>';">     
+<body ONLOAD="javascript:top.location.href='<?php echo "setup.php"?>';">     
  OpenEMR requires Javascript and a GUI browser. We can't promise you     
 anything but try following <a href="setup.php">this link</a> to continue.
 <?php                                                                    
