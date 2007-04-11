8226db7b33c6487cf9c99701a08ea899931fedd2
sunsetsystems <sunsetsystems>
2007-04-11 17:58:16 +0000

    new cricket injury audit form

---
+++ contrib/forms/cricket_injury_audit/cia.inc.php
@@ -0,0 +1,109 @@
+<?php
+ $arr_county = array(
+   '1' => 'Derbyshire',
+   '2' => 'Durham',
+   '3' => 'Essex',
+   '4' => 'Glamorgan',
+   '5' => 'Gloucestershire',
+   '6' => 'Hampshire',
+   '7' => 'Kent',
+   '8' => 'Lancashire',
+   '9' => 'Leicestershire',
+  '10' => 'Middlesex',
+  '11' => 'Northamptonshire',
+  '12' => 'Nottinghamshire',
+  '13' => 'Somerset',
+  '14' => 'Surrey',
+  '15' => 'Sussex',
+  '16' => 'Warwickshire',
+  '17' => 'Worcestershire',
+  '18' => 'Yorkshire',
+ );
+
+ $arr_team = array(
+   '1' => 'County 1st',
+   '2' => 'County 2nd',
+   '3' => 'County U19',
+   '4' => 'Test',
+   '5' => 'ODI',
+   '6' => 'Other',
+   '7' => 'Club Cricket',
+ );
+
+ $arr_duration = array(
+   '1' => '1',
+   '2' => '2',
+   '3' => '3',
+   '4' => '4',
+   '5' => '5',
+   '6' => '20/20',
+ );
+
+ $arr_role = array(
+   '1' => 'Batsman',
+   '2' => 'Bowler',
+   '3' => 'Wicket Keeper',
+ );
+
+ $arr_matchtype = array(
+   '1' => 'Game',
+   '2' => 'Warm Up',
+   '3' => 'Warm Down/Recovery',
+   '4' => 'Training',
+   '5' => 'Other',
+   '6' => 'Non Cricket',
+   '7' => 'Cross Training',
+   '8' => 'Practice',
+   '9' => 'Previous Season',
+ );
+
+ $arr_cause = array(
+   '1' => 'Contact',
+   '2' => 'Torsion',
+   '3' => 'Repetition',
+   '4' => 'Compression',
+ );
+
+ $arr_activity = array(
+   '1' => 'Batting',
+   '2' => 'Bowling',
+   '3' => 'Circuit Training',
+   '4' => 'Fielding',
+   '5' => 'Frisbee',
+   '6' => 'Golf',
+   '7' => 'N/S',
+   '8' => 'N/A',
+   '9' => 'Other',
+  '10' => 'Plyometrics',
+  '11' => 'Rugby',
+  '12' => 'Running',
+  '13' => 'SAQ',
+  '14' => 'Soccer',
+  '15' => 'Sprints',
+  '16' => 'Spontaneous',
+  '17' => 'Team Building',
+  '18' => 'Warm Up Activity',
+  '19' => 'Wt Training',
+  '20' => 'Wicket Keeping',
+  '21' => 'Recovery Activity',
+ );
+
+ $arr_batside = array(
+   '1' => 'Right',
+   '2' => 'Left',
+   '3' => 'N/A',
+ );
+
+ $arr_bowlside = array(
+   '1' => 'Right',
+   '2' => 'Left',
+   '3' => 'N/A',
+ );
+
+ $arr_bowltype = array(
+   '1' => 'Fast',
+   '2' => 'Medium',
+   '3' => 'Slow',
+   '4' => 'N/A',
+ );
+?>
