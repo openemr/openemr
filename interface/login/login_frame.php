46546c5d79fd7efbc4847a52bbc12165cf0c6cb0
lemonsoftwarero <lemonsoftwarero>
2007-04-17 15:39:48 +0000

    Patch for XSS vulnerability

--- interface/login/login_frame.php
+++ interface/login/login_frame.php
@@ -1,22 +1,23 @@
-<?
-$ignoreAuth=true;
-include_once("../globals.php");
-
+<?php
+$ignoreAuth = true;
+include_once ("../globals.php");
+include_once ("$srcdir/classes/Filtreatment_class.php");
 ?>
-
-
-
 <HTML>
 <HEAD>
-<TITLE>
-Login
-</TITLE>
+    <TITLE><?php xl ('Login','e'); ?></TITLE>
 </HEAD>
+
+<?php
+$ob         = new Filtreatment();
+$_rootdir = $ob->doTreatment($rootdir, 'XSS');
+?>
+
 <frameset rows="<?echo "$GLOBALS[logoBarHeight],$GLOBALS[titleBarHeight]" ?>,*" cols="*" frameborder="NO" border="0" framespacing="0">
-  <frame src="<?echo $rootdir;?>/login/filler.php" name="Filler Top" scrolling="no" noresize frameborder="NO">
-  <frame src="<?echo $rootdir;?>/login/login_title.php" name="Title" scrolling="no" noresize frameborder="NO">
-  <frame src="<?echo $rootdir;?>/login/login.php" name="Login" scrolling="auto" frameborder="NO">
-  <!--<frame src="<?echo $rootdir;?>/login/filler.php" name="Filler Bottom" scrolling="no" noresize frameborder="NO">-->
+  <frame src="<?echo $_rootdir;?>/login/filler.php" name="Filler Top" scrolling="no" noresize frameborder="NO">
+  <frame src="<?echo $_rootdir;?>/login/login_title.php" name="Title" scrolling="no" noresize frameborder="NO">
+  <frame src="<?echo $_rootdir;?>/login/login.php" name="Login" scrolling="auto" frameborder="NO">
+  <!--<frame src="<?echo $_rootdir;?>/login/filler.php" name="Filler Bottom" scrolling="no" noresize frameborder="NO">-->
 </frameset>
 
 <noframes><body bgcolor="#FFFFFF">
