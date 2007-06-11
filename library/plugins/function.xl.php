b89cd349c66baa2c3cc0d9105a545ffb546d5a57
lemonsoftwarero <lemonsoftwarero>
2007-06-11 17:24:04 +0000

    Fix sql statement

--- library/plugins/function.xl.php
+++ library/plugins/function.xl.php
@@ -36,7 +36,7 @@ function smarty_function_xl($params, &$smarty)
 	$sql = "SELECT * FROM lang_definitions JOIN lang_constants ON " .
     	"lang_definitions.cons_id = lang_constants.cons_id WHERE " .
     	"lang_id='$lang_id' AND constant_name = '" .
-    	addslashes($constant) . "' LIMIT 1";
+    	addslashes($translate) . "' LIMIT 1";
 	
 	$res = SqlStatement($sql);
 	$row = SqlFetchArray($res);
