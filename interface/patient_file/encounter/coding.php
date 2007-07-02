524d8622f673013ff31b0c1ab42ddd254fcf940f
lemonsoftwarero <lemonsoftwarero>
2007-07-02 06:26:51 +0000

    Update for dutch prescription

--- interface/patient_file/encounter/coding.php
+++ interface/patient_file/encounter/coding.php
@@ -20,11 +20,16 @@ include_once("../../../custom/code_types.inc.php");
  target="Codes"><?php echo $key; ?> <?php xl('Search','e'); ?></a></dd>
 <?php } ?>
 
+<?php
+if ( LANGUAGE == 5 ) $pres = "prescriptiondutch";
+else $pres = "prescription";
+?>
+
 <dd><a class="text" href="copay.php" target="Codes"><?php xl('Copay','e'); ?></a></dd>
 <dd><a class="text" href="other.php" target="Codes"><?php xl('Other','e'); ?></a></dd><br />
 <dt><span href="coding.php" class="title"><?php xl('Prescriptions','e'); ?></span></dt>
-<dd><a class="text" href="<?=$GLOBALS['webroot']?>/controller.php?prescription&list&id=<?=$pid?>" target="Codes"><?php xl('List Prescriptions','e'); ?></a></dd>
-<dd><a class="text" href="<?=$GLOBALS['webroot']?>/controller.php?prescription&edit&id=&pid=<?=$pid?>" target="Codes"><?php xl('Add Prescription','e'); ?></a></dd>
+<dd><a class="text" href="<?=$GLOBALS['webroot']?>/controller.php?<?=$pres?>&list&id=<?=$pid?>" target="Codes"><?php xl('List Prescriptions','e'); ?></a></dd>
+<dd><a class="text" href="<?=$GLOBALS['webroot']?>/controller.php?<?=$pres?>&edit&id=&pid=<?=$pid?>" target="Codes"><?php xl('Add Prescription','e'); ?></a></dd>
 </dl>
 
 </body>
