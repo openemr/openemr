927a66a9b6f8f0beb5d218c4831b170e2b3b028f
markleeds <markleeds>
2007-10-05 19:48:29 +0000

    fixed sql date comparison to be pre-4.1 compatible

--- interface/forms/CAMOS/save.php
+++ interface/forms/CAMOS/save.php
@@ -24,24 +24,28 @@ $CAMOS_form_name = "CAMOS-".$field_names['category'].'-'.$field_names['subcatego
 if ($encounter == "")
 $encounter = date("Ymd");
 if ($_GET["mode"] == "new"){
-  $newid = formSubmit("form_CAMOS", $field_names, $_GET["id"], $userauthorized);
-  addForm($encounter, $CAMOS_form_name, $newid, "CAMOS", $pid, $userauthorized);
+  if (preg_match("/^[\s\\r\\n\\\\r\\\\n]*$/",$field_names['content']) == 0) { //make sure blanks do not get submitted
+    $newid = formSubmit("form_CAMOS", $field_names, $_GET["id"], $userauthorized);
+    addForm($encounter, $CAMOS_form_name, $newid, "CAMOS", $pid, $userauthorized);
+  }
   //deal with embedded camos submissions here
   foreach($embedded_camos as $val) {
-    foreach($val as $k => $v) {
-      $val[$k] = trim($v);  
-    } 
-    $CAMOS_form_name = "CAMOS-".$val['category'].'-'.$val['subcategory'].'-'.$val['item'];
-    $newid = formSubmit("form_CAMOS", $val, $_GET["id"], $userauthorized);
-    addForm($encounter, $CAMOS_form_name, $newid, "CAMOS", $pid, $userauthorized);
+    if (preg_match("/^[\s\\r\\n\\\\r\\\\n]*$/",$val['content']) == 0) { //make sure blanks not submitted
+      foreach($val as $k => $v) {
+        $val[$k] = trim($v);  
+      } 
+      $CAMOS_form_name = "CAMOS-".$val['category'].'-'.$val['subcategory'].'-'.$val['item'];
+      $newid = formSubmit("form_CAMOS", $val, $_GET["id"], $userauthorized);
+      addForm($encounter, $CAMOS_form_name, $newid, "CAMOS", $pid, $userauthorized);
+    }
   }
 }
 elseif ($_GET["mode"] == "update") {
   sqlInsert("update form_CAMOS set pid = {$_SESSION["pid"]},groupname='".$_SESSION["authProvider"]."',user='".$_SESSION["authUser"]."',authorized=$userauthorized,activity=1, date = NOW(), category='".$field_names["category"]."',subcategory='".$field_names["subcategory"]."',item='".$field_names["item"]."',content='".$field_names['content']."' where id=$id");
 }
 elseif ($_GET["mode"] == "delete") {
-  sqlInsert("delete from form_CAMOS where id=$id and date(date) = date(now())");
-  sqlInsert("delete from forms where form_name like 'CAMOS%' and form_id=$id and pid='".$_SESSION["pid"]."' and date(date) = date(now())");
+  sqlInsert("delete from form_CAMOS where id=$id and DATE_FORMAT(date,'%Y-%m-%d') like current_date()");
+  sqlInsert("delete from forms where form_name like 'CAMOS%' and form_id=$id and pid='".$_SESSION["pid"]."' and DATE_FORMAT(date,'%Y-%m-%d') like current_date()");
 }
 $_SESSION["encounter"] = $encounter;
 formHeader("Redirecting....");
