acb7c73855ab68474430b11366aeb4d741a93065
markleeds <markleeds>
2007-10-09 15:19:06 +0000

    Fixed parsing of multibox to deal with embedded functions in multibox data.

--- interface/forms/CAMOS/content_parser.php
+++ interface/forms/CAMOS/content_parser.php
@@ -10,6 +10,7 @@ function addBilling2($encounter, $code_type, $code, $code_text, $modifier="",$un
   {
     $justify_string = implode(":",$justify).":";
   }
+  $authorized = 1;
   $sql = "insert into billing (date, encounter, code_type, code, code_text, pid, authorized, user, groupname,activity,billed,provider_id,modifier,units,fee,justify) values (NOW(), '".$_SESSION['encounter']."', '$code_type', '$code', '$code_text', '".$_SESSION['pid']."', '$authorized', '" . $_SESSION['authId'] . "', '" . $_SESSION['authProvider'] . "',1,0,".$_SESSION['authUserID'].",'$modifier','$units','$fee','$justify_string')";
 	
   return sqlInsert($sql);
@@ -69,7 +70,6 @@ function process_commands(&$string_to_process, &$camos_return_data) {
 
   //end of special case of replace function
   $return_value = 0;
-  $camos_return_data = array(); // to be filled with additional camos form submissions if any embedded
   $command_array = array();  //to be filled with potential commands
   $matches= array();  //to be filled with potential commands
   if (!preg_match_all("/\/\*.*?\*\//",$string_to_process, $matches)) {return $return_value;}
@@ -107,3 +107,36 @@ function process_commands(&$string_to_process, &$camos_return_data) {
   $string_to_process = remove_comments($string_to_process);
   return $return_value;
 } 
+
+
+function clean_multibox_array (&$multibox_array, &$camos_array) {
+  foreach($multibox_array as $key => $var) {
+    $head = '';
+    $body = '';
+    $tail = '';
+    $all = '';
+    if (preg_match('/^(\/\* camos :: .*? :: .*? :: .*? :: )(.*?)(\*\/)$/s', $var, $matches) > 0) {
+      $head = $matches[1];
+      $body = $matches[2];
+      $tail = $matches[3];
+      process_commands($body, $camos_array);
+    } 
+    $all = $head.$body.$tail;
+    if (preg_match('/^\/\* camos :: (.*?) :: (.*?) :: (.*?) :: (.*?) \*\/$/s', 
+      $all, $matches)) {
+      array_push($camos_array, array('category' => $matches[1], 'subcategory' => $matches[2],
+        'item' => $matches[3], 'content' => $matches[4])); 
+    }
+  }
+}
+function create_multibox_array (&$string_to_parse, &$multibox_array) {
+  if (preg_match_all('/\/\*\[begin.+?\]\*\/\s*(\/\* camos.+?)\s*\/\*\[end.+?\]/s', 
+    $string_to_parse, $matches,PREG_SET_ORDER)) {
+    foreach($matches as $match) {
+      array_push($multibox_array, $match[1]); 
+    }
+  }
+}
+function remove_multibox_data (&$string_to_parse) {
+  $string_to_parse = preg_replace('/\/\*\[begin.+?\].*?\[end.+?\]\*\//s', '', $string_to_parse);
+}
