e363249ac58143205589dd954e6dc015edb9756a
sunsetsystems <sunsetsystems>
2007-12-22 15:53:19 +0000

    include patient name

--- interface/patient_file/summary/pnotes_print.php
+++ interface/patient_file/summary/pnotes_print.php
@@ -4,26 +4,30 @@
  include_once("$srcdir/patient.inc");
  include_once("$srcdir/acl.inc");
 
+ $prow = getPatientData($pid, "squad, title, fname, mname, lname");
+
  // Check authorization.
  $thisauth = acl_check('patients', 'notes');
  if (!$thisauth)
   die("Not authorized.");
- $tmp = getPatientData($pid, "squad");
- if ($tmp['squad'] && ! acl_check('squads', $tmp['squad']))
+ if ($prow['squad'] && ! acl_check('squads', $prow['squad']))
   die("Not authorized for this squad.");
 
 $noteid = $_REQUEST['noteid'];
+
+$ptname = $prow['title'] . ' ' . $prow['fname'] . ' ' . $prow['mname'] .
+  ' ' . $prow['lname'];
 
 $title       = '';
 $assigned_to = '';
 $body        = '';
 $activity    = 0;
 if ($noteid) {
-  $prow = getPnoteById($noteid, 'title,assigned_to,activity,body');
-  $title = $prow['title'];
-  $assigned_to = $prow['assigned_to'];
-  $activity = $prow['activity'];
-  $body = $prow['body'];
+  $nrow = getPnoteById($noteid, 'title,assigned_to,activity,body');
+  $title = $nrow['title'];
+  $assigned_to = $nrow['assigned_to'];
+  $activity = $nrow['activity'];
+  $body = $nrow['body'];
 }
 ?>
 <html>
@@ -34,7 +38,7 @@ if ($noteid) {
 <body <?echo $top_bg_line;?> topmargin='0' rightmargin='0' leftmargin='2'
  bottommargin='0' marginwidth='2' marginheight='0'>
 
-<p><b><?php echo $title; ?></b></p>
+<p><?php echo "<b>$title</b> for <b>$ptname</b>"; ?></p>
 
 <p>Assigned To: <?php echo $assigned_to; ?></p>
 
