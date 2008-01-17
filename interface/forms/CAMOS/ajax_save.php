65994c8fa68cac3426fe455add2afeefaff99b56
markleeds <markleeds>
2008-01-17 15:22:46 +0000

    Strange problem apparently due to mysql_escape_string function which causes category value to not be submitted to database.  If I add a dummy field to the array before category, which is the first field in the array, it works ok.  For now, I am disabling mysql_escape_string here.

--- interface/forms/CAMOS/ajax_save.php
+++ interface/forms/CAMOS/ajax_save.php
@@ -7,22 +7,22 @@ include_once("../../../library/sql.inc");
 include_once("content_parser.php");
 $field_names = array('category' => $_POST['category'], 'subcategory' => $_POST['subcategory'], 'item' => $_POST['item'], 'content' => $_POST['content']);
 
-
-$multibox_array = array();
+//$multibox_array = array();
 $camos_array = array();
-create_multibox_array($field_names['content'],$multibox_array);
-clean_multibox_array($multibox_array, $camos_array);
-remove_multibox_data($field_names['content']);
+//create_multibox_array($field_names['content'],$multibox_array);
+//clean_multibox_array($multibox_array, $camos_array);
+//remove_multibox_data($field_names['content']);
 
-foreach ($field_names as $k => $var) {
-  $field_names[$k] = mysql_real_escape_string($var);
-}
+//foreach ($field_names as $k => $var) {
+//  $field_names[$k] = mysql_real_escape_string($var);
+//}
 process_commands($field_names['content'],$camos_array); 
 
 $CAMOS_form_name = "CAMOS-".$field_names['category'].'-'.$field_names['subcategory'].'-'.$field_names['item'];
 
-if ($encounter == "")
-$encounter = date("Ymd");
+if ($encounter == "") {
+  $encounter = date("Ymd");
+}
 
   if (preg_match("/^[\s\\r\\n\\\\r\\\\n]*$/",$field_names['content']) == 0) { //make sure blanks do not get submitted
     $newid = formSubmit("form_CAMOS", $field_names, $_GET["id"], $userauthorized);
