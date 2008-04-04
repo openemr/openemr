0cb37b8127abaedd5c2eaa513fbeeaa35bbd01ff
cfapress <cfapress>
2008-04-04 16:10:24 +0000

    fixed <? tags

--- interface/main/calendar/findfirstapp.php
+++ interface/main/calendar/findfirstapp.php
@@ -5,12 +5,12 @@ include_once("$srcdir/patient.inc");
 ?>
 <html>
 <head>
-<? html_header_show();?>
+<?php html_header_show();?>
 
-<link rel=stylesheet href="<?php echo $css_header;?>" type="text/css">
+<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
 
 </head>
-<body bgcolor="#FFFFFF" topmargin=0 rightmargin=0 leftmargin=2 bottommargin=0 marginwidth=2 marginheight=0 >
+<body bgcolor="#FFFFFF" >
 <form>
 
 <input type=submit value="<?php xl('Find','e'); ?>" />
