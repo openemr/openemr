5b23502262ea2b36ad37b8d06caa9311fcbbccbf
cfapress <cfapress>
2008-04-04 15:32:59 +0000

    cleaned <?, ^M

--- interface/login/filler.php
+++ interface/login/filler.php
@@ -1,15 +1,13 @@
-<?
+<?php
 $ignoreAuth=true;
 include_once("../globals.php");
 ?>
 <html>
 <head>
-<? html_header_show();?>
 <title>Navigation</title>
-<link rel=stylesheet href="<?echo $css_header;?>" type="text/css">
+<link rel=stylesheet href="<?php echo $css_header;?>" type="text/css">
 </head>
-<body  topmargin='0' rightmargin='0' leftmargin='5' marginheight='0' bottommargin='0'
- link='#000000' vlink='#000000' alink='#000000' bgcolor='#24262c'>
-<img style="position:absolute;top:0;left:0;"src=" <?=$GLOBALS['webroot']?>/interface/pic/logo.gif" />
+<body>
+<img style="position:absolute;top:0;left:0;"src=" <?php echo $GLOBALS['webroot']?>/interface/pic/logo.gif" />
 </body>
 </html>
