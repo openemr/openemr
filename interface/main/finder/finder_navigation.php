1c3dd2a5c67dfd1c6a38c2707e0256f0f889d6c5
cfapress <cfapress>
2008-04-08 20:21:32 +0000

    changes to styles and table structure

--- interface/main/finder/finder_navigation.php
+++ interface/main/finder/finder_navigation.php
@@ -12,33 +12,29 @@ include_once("../../globals.php");
 </head>
 <body class="body_nav">
 
-<form border='0' method='post' target="_top"
- name="find_patient" action="<?php echo $rootdir?>/main/finder/patient_finder.php"
- onsubmit="return top.restoreSession()">
+<div id="nav_topmenu">
+<form method='post' target="_top" name="find_patient" action="<?php echo $rootdir?>/main/finder/patient_finder.php" onsubmit="return top.restoreSession()">
 
 <table border="0" cellspacing="0" cellpadding="0" width="100%" height="100%">
 <tr>
 
-<td width="10%" nowrap>
-<input type=entry size=10 name=patient> <select name="findBy" size=1> 
-	<option value="ID"><?php xl('ID','e');?></option>
-	<option value="Last" selected><?php xl('Name','e');?></option>
-	<option value="SSN"><?php xl('SSN','e');?></option>
-	<option value="DOB"><?php xl('DOB','e');?></option>
+<td style="text-align:left; width: 250px; white-space: nowrap;">
+<input type="textbox" size="10" name="patient">
+<select name="findBy">
+<option value="ID"><?php xl('ID','e');?></option>
+<option value="Last" selected><?php xl('Name','e');?></option>
+<option value="SSN"><?php xl('SSN','e');?></option>
+<option value="DOB"><?php xl('DOB','e');?></option>
 </select>
-
-</td>
-
-<td width="5%" nowrap>
 <a href="javascript:top.restoreSession();document.find_patient.action='<?php echo $rootdir?>/main/finder/patient_finder.php';document.find_patient.submit();" class="link">&nbsp;<?php xl('Find Patient','e');?></a>
 </td>
 
-<td width="48%" nowrap>
+<td style="text-align:left">
 &nbsp;&nbsp;&nbsp;<a class="menu" target="_top" href="../../new/new_patient.php" onclick="top.restoreSession()">
 <?php xl('New Patient','e');?></a>&nbsp;
 </td>
 
-<td align="right" nowrap>
+<td style="text-align:right">
 &nbsp;<a href="../main_screen.php" target="_top" class="logout" onclick="top.restoreSession()">
 <?php xl('Back','e');?></a>&nbsp;&nbsp;
 </td>
@@ -46,6 +42,7 @@ include_once("../../globals.php");
 </table>
 
 </form>
+</div>
 
 </body>
 </html>
