bca0030447228d0288d08a1fd958d3bbe3e22c6a
cfapress <cfapress>
2008-04-14 17:39:32 +0000

    added URL parameters to finder_navigation

--- interface/main/finder/patient_finder.php
+++ interface/main/finder/patient_finder.php
@@ -15,7 +15,7 @@ include_once("../../globals.php");
 
 </HEAD>
 <frameset rows="<?php echo "$GLOBALS[navBarHeight],$GLOBALS[titleBarHeight]" ?>,*" cols="*" frameborder="0" border="0" framespacing="0">
-  <frame src="finder_navigation.php" name="Navigation" scrolling="no" noresize frameborder="0">
+  <frame src="finder_navigation.php?patient=<?php echo $patient;?>&findBy=<?php echo $findBy;?>" name="Navigation" scrolling="no" noresize frameborder="0">
   <frame src="../main_title.php" name="Title" scrolling="no" noresize frameborder="0">
   <frame src="patient_select.php?patient=<?php echo $patient;?>&findBy=<?php echo $findBy;?>" name="Comment" scrolling="auto" noresize frameborder="0">
 </frameset>
