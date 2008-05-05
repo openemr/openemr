485c0e84a70587f5ec0f092b23213ad15145016c
lemonsoftwarero <lemonsoftwarero>
2008-05-05 12:14:04 +0000

    Fix the session_start warnings.

--- interface/patient_file/summary/insurance_history.php
+++ interface/patient_file/summary/insurance_history.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
 <?php
 include_once("../../../interface/globals.php");
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
 <html>  
 <head>
     <title>Overzicht zorgverzekering</title>
