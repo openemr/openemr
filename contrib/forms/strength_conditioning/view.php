708300ba1d8fc65eb85bcf28f4677163375ea6f1
sunsetsystems <sunsetsystems>
2008-05-09 23:37:19 +0000

    new form for sports teams

---
+++ contrib/forms/strength_conditioning/view.php
@@ -0,0 +1,25 @@
+<?php
+//////////////////////////////////////////////////////////////////////
+// ------------------ DO NOT MODIFY VIEW.PHP !!! ---------------------
+// View.php is an exact duplicate of new.php.  If you wish to make
+// any changes, then change new.php and either (recommended) make
+// view.php a symbolic link to new.php, or copy new.php to view.php.
+//
+// And if you check in a change to either module, be sure to check
+// in the other (identical) module also.
+//
+// This nonsense will go away if we ever move to subversion.
+//////////////////////////////////////////////////////////////////////
+
+// Copyright (C) 2006-2008 Rod Roark <rod@sunsetsystems.com>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+$spreadsheet_form_name = 'strength_conditioning';
+$spreadsheet_title = 'Strength and Conditioning';
+include_once("../../globals.php");
+include_once("$srcdir/spreadsheet.inc.php");
+?>
