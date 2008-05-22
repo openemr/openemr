896fa96e452ee0a18f06469c68e00103565268b9
sunsetsystems <sunsetsystems>
2008-05-22 03:37:27 +0000

    added fitness status frame for sports teams

--- interface/patient_file/summary/patient_summary.php
+++ interface/patient_file/summary/patient_summary.php
@@ -3,29 +3,31 @@ include_once("../../globals.php");
 ?>
 <html>
 <head>
-<? html_header_show();?>
+<?php html_header_show();?>
 <title><? xl('Patient Summary','e'); ?></title>
 </head>
 
 <frameset rows="50%,50%">
 
-<?php if( $GLOBALS['dutchpc'] ) 
-{ ?>
+<?php if($GLOBALS['dutchpc']) { ?>
  <frame src="demographics_dutch.php" name="Demographics" scrolling="auto">
-<?php } else
-{ ?>
+<?php } else { ?>
  <frame src="demographics.php" name="Demographics" scrolling="auto">
 <?php } ?>
 
-
 <?php if ($GLOBALS['athletic_team']) { ?>
- <frameset cols="50%,50%">
+ <frameset cols="25%,50%,*">
+  <frame src="stats.php" name="Stats" scrolling="auto">
+  <frame src="pnotes.php" name="Notes" scrolling="auto">
+  <frame src="fitness_status.php" name="Fitness" scrolling="auto">
+ </frameset>
 <?php } else { ?>
  <frameset cols="20%,80%">
-<?php } ?>
   <frame src="stats.php" name="Stats" scrolling="auto">
   <frame src="pnotes.php" name="Notes" scrolling="auto">
  </frameset>
+<?php } ?>
+
 </frameset>
 
 </html>
--- interface/patient_file/summary/summary_bottom.php
+++ interface/patient_file/summary/summary_bottom.php
@@ -7,9 +7,17 @@ include_once("../../globals.php");
 <title><? xl('Patient Summary','e'); ?></title>
 </head>
 
+<?php if ($GLOBALS['athletic_team']) { ?>
+<frameset cols="25%,50%,*">
+ <frame src="stats.php" name="Stats" scrolling="auto">
+ <frame src="pnotes.php" name="Notes" scrolling="auto">
+ <frame src="fitness_status.php" name="Fitness" scrolling="auto">
+</frameset>
+<?php } else { ?>
 <frameset cols="25%,*">
  <frame src="stats.php" name="Stats" scrolling="auto">
  <frame src="pnotes.php" name="Notes" scrolling="auto">
 </frameset>
+<?php } ?>
 
 </html>
