e07bef5eef4eabb2934f5f758fa5f73f4cbdacf4
lemonsoftwarero <lemonsoftwarero>
2008-05-22 14:35:53 +0000

    Make it utf8 compliant.

--- library/translation.inc.php
+++ library/translation.inc.php
@@ -6,7 +6,10 @@ include_once(dirname(__FILE__) . '/sql.inc'); // fixes vulnerability with regist
 
 function xl($constant,$mode='r',$prepend='',$append='') {
 	$lang_id=LANGUAGE;
-	$sql="SELECT * FROM lang_definitions JOIN lang_constants ON " .
+	
+	// utf8 compliant
+    sqlQuery("SET NAMES utf8");
+    	$sql="SELECT * FROM lang_definitions JOIN lang_constants ON " .
     "lang_definitions.cons_id = lang_constants.cons_id WHERE " .
     "lang_id='$lang_id' AND constant_name = '" .
     addslashes($constant) . "' LIMIT 1";
