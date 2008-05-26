db8a736e3a1989c8125398051d6c0438b7d90d37
lemonsoftwarero <lemonsoftwarero>
2008-05-26 13:44:06 +0000

    New link for DBC Dutch System.

--- interface/new/new_patient.php
+++ interface/new/new_patient.php
@@ -17,7 +17,12 @@ $_SESSION["encounter"] = "";
 <frameset rows="<?php echo "$GLOBALS[navBarHeight],$GLOBALS[titleBarHeight]"; ?>,*" cols="*" frameborder="0" border="0" framespacing="0">
   <frame src="new_navigation.php" name="Navigation" scrolling="no" noresize frameborder="NO">
   <frame src="new_title.php" name="Title" scrolling="no" noresize frameborder="NO">
-  <frame src="new.php" name="Main" scrolling="auto" noresize frameborder="NO">
+
+  <?php if ( $GLOBALS['dutchpc'] ) { ?>
+    <frame src="new_dutch.php" name="Main" scrolling="auto" noresize frameborder="NO">
+<?php } else { ?>c
+    <frame src="new.php" name="Main" scrolling="auto" noresize frameborder="NO">
+<?php } ?>
 
 </frameset>
 
