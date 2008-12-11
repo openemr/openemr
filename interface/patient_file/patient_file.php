671284ebd44232b1bfb032ec60a5a5736140894c
cfapress <cfapress>
2008-12-11 18:43:33 +0000

    cleaned up php tags

--- interface/patient_file/patient_file.php
+++ interface/patient_file/patient_file.php
@@ -5,7 +5,7 @@ setpid($_GET["set_pid"]);
 ?>
 <HTML>
 <head>
-<? html_header_show();?>
+<?php html_header_show();?>
 <TITLE>
 <?php echo $openemr_name ?>
 </TITLE>
@@ -16,35 +16,28 @@ setpid($_GET["set_pid"]);
 </script>
 
 </HEAD>
-<frameset rows="<?echo "$GLOBALS[navBarHeight],$GLOBALS[titleBarHeight]" ?>,*"
- cols="*" frameborder="0" border="0" framespacing="0" onunload="imclosing()">
-<?
-if (isset($_GET["calenc"])) {
-?>
+<frameset rows="<?php echo "$GLOBALS[navBarHeight],$GLOBALS[titleBarHeight]" ?>,*" cols="*" frameborder="0" border="0" framespacing="0" onunload="imclosing()">
+
+<?php if (isset($_GET["calenc"])) { ?>
   <frame src="navigation.php" name="Navigation" scrolling="NO" noresize frameborder="0">
   <frame src="encounter/encounter_title.php" name="Title" scrolling="no" noresize frameborder="0">
-  <frame src="encounter/patient_encounter.php?mode=new&calenc=<?echo $_GET["calenc"];?>" name="Main" scrolling="AUTO" noresize frameborder="0">
-<?
-} elseif ($_GET['go'] == "encounter"){
-?>
-  <frame src="navigation.php?pid=<?=$_GET['pid']?>&set_pid=<?=$_GET['pid']?>" name="Navigation" scrolling="NO" noresize frameborder="0">
-  <frame src="encounter/encounter_title.php?pid=<?=$_GET['pid']?>&set_pid=<?=$_GET['pid']?>" name="Title" scrolling="no" noresize frameborder="0">
-  <frame src="encounter/patient_encounter.php?mode=new&pid=<?=$_GET['pid']?>&set_pid=<?=$_GET['pid']?>" name="Main" scrolling="AUTO" noresize frameborder="0">
-<?
-} elseif ($_GET['noteid']){
-?>
+  <frame src="encounter/patient_encounter.php?mode=new&calenc=<?php echo $_GET["calenc"];?>" name="Main" scrolling="AUTO" noresize frameborder="0">
+
+<?php } elseif ($_GET['go'] == "encounter"){ ?>
+  <frame src="navigation.php?pid=<?php echo $_GET['pid']?>&set_pid=<?php echo $_GET['pid']?>" name="Navigation" scrolling="NO" noresize frameborder="0">
+  <frame src="encounter/encounter_title.php?pid=<?php echo $_GET['pid']?>&set_pid=<?php echo $_GET['pid']?>" name="Title" scrolling="no" noresize frameborder="0">
+  <frame src="encounter/patient_encounter.php?mode=new&pid=<?php echo $_GET['pid']?>&set_pid=<?php echo $_GET['pid']?>" name="Main" scrolling="AUTO" noresize frameborder="0">
+
+<?php } elseif ($_GET['noteid']){ ?>
   <frame src="navigation.php" name="Navigation" scrolling="NO" noresize frameborder="0">
   <frame src="summary/summary_title.php" name="Title" scrolling="no" noresize frameborder="0">
   <frame src="summary/pnotes_full.php?noteid=<?php echo $_GET['noteid'] ?>&active=1" name="Main" scrolling="AUTO" noresize frameborder="0">
-<?
-} else {
-?>
+
+<?php } else { ?>
   <frame src="navigation.php" name="Navigation" scrolling="NO" noresize frameborder="0">
   <frame src="summary/summary_title.php" name="Title" scrolling="no" noresize frameborder="0">
   <frame src="summary/patient_summary.php" name="Main" scrolling="AUTO" noresize frameborder="0">
-<?
-}
-?>
+<?php } ?>
 </frameset>
 
 <noframes><body bgcolor="#FFFFFF">
