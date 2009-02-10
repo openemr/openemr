626d1a3e18428903e42cc3f3fe556c0dba84bb75
bradymiller <bradymiller>
2009-02-10 13:10:49 +0000

    reorganize and error checking

--- setup.php
+++ setup.php
@@ -12,15 +12,18 @@ $conffile = "library/sqlconf.php";
 $upgrade = 0;
 $defhost = 'localhost';
 $state = $_POST["state"];
+$conffile2 = "interface/globals.php";
 $gaclConfigFile1 = "gacl/gacl.ini.php";
 $gaclConfigFile2 = "gacl/gacl.class.php";
+$docsDirectory = "documents";
+$billingDirectory = "edi";
 $gaclWritableDirectory = "gacl/admin/templates_c";
-$gaclSetupScript1 = "./gacl/setup.php";
-$gaclSetupScript2 = "./acl_setup.php";
 $requiredDirectory1 = "interface/main/calendar/modules/PostCalendar/pntemplates/compiled";
 $requiredDirectory2 = "interface/main/calendar/modules/PostCalendar/pntemplates/cache";
-$writableFileList = array($conffile, $gaclConfigFile1, $gaclConfigFile2);
-$writableDirList = array($gaclWritableDirectory, $requiredDirectory1, $requiredDirectory2);
+$gaclSetupScript1 = "./gacl/setup.php";
+$gaclSetupScript2 = "./acl_setup.php";
+$writableFileList = array($conffile, $conffile2, $gaclConfigFile1, $gaclConfigFile2);
+$writableDirList = array($docsDirectory, $billingDirectory, $gaclWritableDirectory, $requiredDirectory1, $requiredDirectory2);
 
 include_once($conffile);
 ?>
@@ -51,39 +54,37 @@ include_once($conffile);
 <p>Congratulations! OpenEMR is now successfully installed.
 
 <ul>
- <li>Please Edit the 'interface/globals.php' file now to specify the correct
-  URL paths, and to select a theme.</li>
-  <li>Please restore secure permissions on the configuration files: /openemr/library/sqlconf.php, /openemr/gacl/gacl.ini.php, and /openemr/gacl/gacl.class.php files.</li> 
+ <li>Please restore secure permissions on the four configuration files: /openemr/interface/globals.php, 
+     /openemr/library/sqlconf.php, /openemr/gacl/gacl.ini.php, and /openemr/gacl/gacl.class.php files.  
+     In linux, recommend changing file permissions with the 'chmod 644 filename' command.</li>
+ <li>In order to take full advantage of the documents capability you 
+     must give your web server permissions on the document storage 
+     directory. Try "chown apache:apache -R openemrwebroot/documents" 
+     and then "chmod g+w openemrwebroot/documents". You must also make sure 
+     your PHP installation (normally set in your php.ini file) has 
+     "file_uploads enabled", that "upload_max_filesize" is appropriate for your 
+     use and that "upload_tmp_dir" is set to a correct value if the default of "/tmp" 
+     won't work on your system.</li>
+ <li>Access controls (php-GACL) are installed for fine-grained security, and can be administered in
+     OpenEMR's admin->acl menu.</li>
+ <li>Reading openemr/includes/config.php and openemr/interface/globals.php is a good idea. These files
+     contain many options to choose from including themes.</li>
+ <li>There's much information and many extra tools bundled within the OpenEMR installation directory. 
+     Please refer to openemr/Documentation. Many forms and other useful scripts can be found at openemr/contrib.</li>
+ <li>To ensure a consistent look and feel through out the application using 
+     <a href='http://www.mozilla.org/products/firefox/'>Firefox</a> is recommended.</li> 
 </ul>
-<p>
- In order to take full advantage of the documents capability you
- must give your web server permissions on the document storage
- directory. Try "chown apache:apache -R openemrwebroot/documents"
- and then "chmod g+w openemrwebroot/documents".
- You must also make sure your PHP installation (normally set in
- your php.ini file) has "file_uploads enabled", that
- "upload_max_filesize" is appropriate for your use and that
- "upload_tmp_dir" is set to a correct value if the default of
- "/tmp" won't work on your system.
-</p>
-<p>
-Access controls (php-GACL) are installed for fine-grained security,
-and can be administered in OpenEMR's admin->acl menu.
-</p>
-<p>
-There's much information and many extra tools bundled within the OpenEMR
-installation directory. Please refer to openemr/Documentation.
-<br>Many forms and other useful scripts can be found at openemr/contrib.
-</p>
-<p>
-Reading openemr/includes/config.php and openemr/interface/globals.php is a good
-idea.
-</p>
-<p>
-To ensure a consistent look and feel through out the application
-using <a href='http://www.mozilla.org/products/firefox/'>Firefox</a>
-is recommended.
-</p>
+<p>The "openemrwebroot/documents" and "openemrwebroot/edi" contain patient information, and
+   it is important to secure these directories. This can be done by placing pertinent .htaccess 
+   files in these directories or by pasting the below in your apache configuration file:<br>
+&lt;Directory "<?php echo realpath($docsDirectory);?>"&gt;<br>
+order deny,allow<br>
+Deny from all<br>
+&lt;/Directory&gt;<br>
+&lt;Directory "<?php echo realpath($billingDirectory);?>"&gt;<br>
+order deny,allow<br>
+Deny from all<br>
+&lt;/Directory&gt;<br>
 <p>
 <b>The initial OpenEMR user is "<?php echo $iuser; ?>" and the password is "pass".</b>
 You should change this password!
@@ -109,7 +110,7 @@ You should change this password!
 	$rootpass = $_POST["rootpass"];
 
 
-if ($config == 1) {
+if (($config == 1) && ($state != 4)) {
 	echo "OpenEMR is already configured.  If you wish to re-configure the SQL server, edit $conffile, or change the 'config' variable to 0, and re-run this script.<br>\n";
 }
 else {
@@ -130,7 +131,7 @@ break;
 
 	case 2:
 echo "<b>Step $state</b><br><br>\n";
-echo "Now you need to supply the MySQL server information.
+echo "Now you need to supply the MySQL server information and path information.
 <br><br>
 <FORM METHOD='POST'>
 <INPUT TYPE='HIDDEN' NAME='state' VALUE='3'>
@@ -154,6 +155,11 @@ echo "<TR><TD COLSPAN=2></TD></TR>
 <TR><TD><span class='text'>Initial User's Name:</span></TD><TD><INPUT SIZE='30' TYPE='TEXT' NAME='iuname' VALUE='Administrator'><span class='text'>(This is the real name of the initial user.)</span></TD></TR>
 <TR><TD><span class='text'>Initial Group:</span></TD><TD><INPUT SIZE='30' TYPE='TEXT' NAME='igroup' VALUE='Default'><span class='text'>(This is the group that will be created for your users.  This should be the name of your practice.)</span></TD></TR>
 ";
+echo "<TR><TD><font color='red'>PATHS:</font></TD></TR>";
+echo "<TR><TD COLSPAN=2></TD></TR>
+<TR><TD><span class='text'>Absolute Path:</span></TD><TD><INPUT SIZE='30' TYPE='TEXT' NAME='openemrBasePath' VALUE='".realpath('./')."'><span class='text'>(This is the full absolute directory path to openemr. The value here is automatically created, and should not need to be modified.)</span></TD></TR>
+<TR><TD><span class='text'>Relative HTML Path:</span></TD><TD><INPUT SIZE='30' TYPE='TEXT' NAME='openemrWebPath' VALUE='/openemr'><span class='text'>(Set this to the relative html path, ie. what you would type into the web browser after the server address to get to OpenEMR. For example, if you type 'http://127.0.0.1/clinic/openemr/ to load OpenEMR, set this to '/clinic/openemr' without the trailing slash.)</span></TD></TR>
+";
 echo "</TABLE>
 <br>
 <INPUT TYPE='SUBMIT' VALUE='Continue'><br></FORM><br>";
@@ -170,6 +176,9 @@ echo "<b>Step $state</b><br><br>\n";
 		break;
 	}
 
+echo "Configuring OpenEMR...<br><br>\n";
+	
+	
 if ($inst != 2) {
 	echo "Connecting to MySQL Server...\n";
 	flush();
@@ -314,30 +323,9 @@ if ($upgrade != 1) {
 	echo "OK\n";
 	fclose($fd);*/
 	flush();
-}
-echo "\n<br>Next step will finalize SQL setup and install/configure access controls (php-GACL).<br>\n";	
+}	
 
-echo "
-<FORM METHOD='POST'>\n
-<INPUT TYPE='HIDDEN' NAME='state' VALUE='4'>
-<INPUT TYPE='HIDDEN' NAME='host' VALUE='$server'>
-<INPUT TYPE='HIDDEN' NAME='dbname' VALUE='$dbname'>
-<INPUT TYPE='HIDDEN' NAME='port' VALUE='$port'>
-<INPUT TYPE='HIDDEN' NAME='login' VALUE='$login'>
-<INPUT TYPE='HIDDEN' NAME='pass' VALUE='$pass'>
-<INPUT TYPE='HIDDEN' NAME='iuser' VALUE='$iuser'>
-<INPUT TYPE='HIDDEN' NAME='iuname' VALUE='$iuname'>
-<br>\n
-<INPUT TYPE='SUBMIT' VALUE='Continue'><br></FORM><br>\n";
-
-
-break;
-
-	case 4:
-echo "<b>Step $state</b><br><br>\n";
-echo "Writing SQL configuration file and setting up access controls(php-GACL)...<br><br>";	
-
-echo "Writing SQL Configuration...<br>";	
+echo "<br>Writing SQL Configuration...<br>";	
 @touch($conffile); // php bug
 $fd = @fopen($conffile, 'w');
 $string = "<?
@@ -350,7 +338,7 @@ $string = "<?
 $it_died = 0;   //fmg: variable keeps running track of any errors
 
 fwrite($fd,$string) or $it_died++;
-fwrite($fd,"\$host\t= '$host';\n") or $it_died++;
+fwrite($fd,"\$host\t= '$server';\n") or $it_died++;
 fwrite($fd,"\$port\t= '$port';\n") or $it_died++;
 fwrite($fd,"\$login\t= '$login';\n") or $it_died++;
 fwrite($fd,"\$pass\t= '$pass';\n") or $it_died++;
@@ -388,8 +376,63 @@ if ($it_died != 0) {
 fclose($fd);
 
 echo "Successfully wrote SQL configuration.<BR><br>";
+
+echo "Writing OpenEMR webserver paths to config file...<br>";
+//edit interface/globals.php
+$data = file($conffile2) or die("Could not read ".$conffile2." file.");
+$finalData = "";
+$isCount = 0;
+foreach ($data as $line) {
+	$isHit = 0;
+        if ((strpos($line,"\$webserver_root = \"")) === false) {
+	}
+	else {
+	        $isHit = 1;
+	        $isCount += 1;
+	        $finalData .= "\$webserver_root = \"$openemrBasePath\";\n";
+	}
+        if ((strpos($line,"\$web_root = \"")) === false) {
+	}
+	else {
+	        $isHit = 1;
+	        $isCount += 1;
+	        $finalData .= "\$web_root = \"$openemrWebPath\";\n";
+	}
+        if (!$isHit) {
+	        $finalData .= $line;
+	}
+}
+$fd = @fopen($conffile2, 'w') or die("Could not open ".$conffile2." file.");
+fwrite($fd, $finalData);
+fclose($fd);
+if ($isCount == 2) {
+	echo "Successfully wrote OpenEMR webserver paths to config file<br><br>";
+}
+else {
+	echo "<FONT COLOR='red'>ERROR</FONT> writing openemr webserver root paths to config file ($conffile2). ($isCount)<br><br>\n";
+}
 	
-echo "Installing and Configuring Access Controls (php-GACL)<br>";
+echo "\n<br>Next step will install and configure access controls (php-GACL).<br>\n";
+	
+echo "
+<FORM METHOD='POST'>\n
+<INPUT TYPE='HIDDEN' NAME='state' VALUE='4'>
+<INPUT TYPE='HIDDEN' NAME='host' VALUE='$server'>
+<INPUT TYPE='HIDDEN' NAME='dbname' VALUE='$dbname'>
+<INPUT TYPE='HIDDEN' NAME='port' VALUE='$port'>
+<INPUT TYPE='HIDDEN' NAME='login' VALUE='$login'>
+<INPUT TYPE='HIDDEN' NAME='pass' VALUE='$pass'>
+<INPUT TYPE='HIDDEN' NAME='iuser' VALUE='$iuser'>
+<INPUT TYPE='HIDDEN' NAME='iuname' VALUE='$iuname'>
+<br>\n
+<INPUT TYPE='SUBMIT' VALUE='Continue'><br></FORM><br>\n";
+	
+	
+break;	
+
+        case 4:
+echo "<b>Step $state</b><br><br>\n";
+echo "Installing and Configuring Access Controls (php-GACL)...<br><br>";
 
 //first, edit two gacl config files
 // edit gacl.ini.php
@@ -474,11 +517,13 @@ echo "<br>";
 //third, give the administrator user admin priviledges
 $groupArray = array("Administrators");
 set_user_aro($groupArray,$iuser,$iuname,"","");	
-echo "Gave the '$iuser' user (password is 'pass') administrator access.<br>";
-	
+echo "Gave the '$iuser' user (password is 'pass') administrator access.<br><br>";
+
+echo "Done installing and configuring access controls (php-GACL).  Click 'continue' to see further instructions.";
+
 echo "<br><FORM METHOD='POST'>\n
 <INPUT TYPE='HIDDEN' NAME='state' VALUE='5'>\n
-<INPUT TYPE='HIDDEN' NAME='iuser' VALUE='$iuser'>\n
+<INPUT TYPE='HIDDEN' NAME='iuser' VALUE='$iuser'>\n	
 <br>\n
 <INPUT TYPE='SUBMIT' VALUE='Continue'><br></FORM><br>\n";
 
@@ -495,18 +540,18 @@ echo "<FONT COLOR='blue'>Ensuring following files are world-writable...</FONT><b
 $errorWritable = 0;
 foreach ($writableFileList as $tempFile) {
 	if (is_writable($tempFile)) {
-	        echo "'openemr/$tempFile' file is <FONT COLOR='blue'>ready</FONT>.<br>";
+	        echo "'".realpath($tempFile)."' file is <FONT COLOR='blue'>ready</FONT>.<br>";
 	}
 	else {
-	        echo "<br><FONT COLOR='red'>UNABLE</FONT> to open file 'openemr/$tempFile' for writing.<br>";
-	        echo "(ensure 'openemr/$tempFile' file is world-writeable)<br>";
+	        echo "<br><FONT COLOR='red'>UNABLE</FONT> to open file '".realpath($tempFile)."' for writing.<br>";
+	        echo "(configure file permissions; see below for further instructions)<br>";
 	        $errorWritable = 1;
 	}
 }
 if ($errorWritable) {
-	echo "<br><FONT COLOR='red'>You can't proceed until all above files are ready.</FONT><br>";
-        echo "Fix above file permissions and then click the 'Check Again' button to re-check files.<br>";
-	echo "In linux, recommend changing file permissions temporarily with the 'chmod 666 filename' command.<br>";
+	echo "<br><FONT COLOR='red'>You can't proceed until all above files are ready (world-writable).</FONT><br>";	
+	echo "In linux, recommend changing file permissions with the 'chmod 666 filename' command.<br>";
+	echo "Fix above file permissions and then click the 'Check Again' button to re-check files.<br>";
 	echo "<FORM METHOD='POST'><INPUT TYPE='SUBMIT' VALUE='Check Again'><br></FORM><br>\n";
 	break;
 }
@@ -515,18 +560,18 @@ echo "<br><FONT COLOR='blue'>Ensuring following directories exist...</FONT><br>\
 $errorWritable = 0;
 foreach ($writableDirList as $tempDir) {
 	if (file_exists($tempDir)) {
-	        echo "'openemr/$tempDir' directory <FONT COLOR='blue'>exists</FONT>.<br>";
+	        echo "'".realpath($tempDir)."' directory <FONT COLOR='blue'>exists</FONT>.<br>";
 	}
 	else {
-	        echo "<br><FONT COLOR='red'>UNABLE</FONT> to find directory 'openemr/$tempDir'.<br>";
-	        echo "(please create 'openemr/$tempDir' directory)<br>";
+	        echo "<br><FONT COLOR='red'>UNABLE</FONT> to find directory '".realpath($tempDir)."'.<br>";
+	        echo "(please create directory; see below for further instructions)<br>";
 	        $errorWritable = 1;
         }
 }
 if ($errorWritable) {
 	echo "<br><FONT COLOR='red'>You can't proceed until all above directories exist.</FONT><br>";
+	echo "In linux, recommend using the 'mkdir directory_name' command<br>\n";
 	echo "Add above marked directories and then click the 'Check Again' button to re-check for directories.<br>";
-	echo "In linux, recommend using the 'mkdir directory_name' command<br><br>\n";
 	echo "<FORM METHOD='POST'><INPUT TYPE='SUBMIT' VALUE='Check Again'><br></FORM><br>\n";
 	break;
 }	
@@ -535,19 +580,19 @@ echo "<br><FONT COLOR='blue'>Ensuring following directories have proper permissi
 $errorWritable = 0;
 foreach ($writableDirList as $tempDir) {
 	if (is_writable($tempDir)) {
-	        echo "'openemr/$tempDir' directory is <FONT COLOR='blue'>ready</FONT>.<br>";
+	        echo "'".realpath($tempDir)."' directory is <FONT COLOR='blue'>ready</FONT>.<br>";
 	}
 	else {
-	        echo "<br><FONT COLOR='red'>UNABLE</FONT> to open directory 'openemr/$tempDir' for writing.<br>";
-	        echo "(ensure 'openemr/$tempDir' directory is world-writeable)<br>";
+	        echo "<br><FONT COLOR='red'>UNABLE</FONT> to open directory '".realpath($tempDir)."' for writing by web server.<br>";
+	        echo "(configure directory permissions; see below for further instructions)<br>";
 	        $errorWritable = 1;
 	}
 }
 if ($errorWritable) {
 	echo "<br><FONT COLOR='red'>You can't proceed until all directories are ready.</FONT><br>";
-	echo "Fix above directory permissions and then click the 'Check Again' button to re-check directories.<br>";
-	echo "In linux, recommend changing owners of directories permanently to the web server. For example, in many linux OS's the web server user is 'apache', 'nobody', or 'www-data'. So if 'apache' were the web server user name, could use the command 'chown -R apache:apache directory_name' command.<br><br>\n";
-	echo "<FORM METHOD='POST'><INPUT TYPE='SUBMIT' VALUE='Check Again'><br></FORM><br>\n";
+	echo "In linux, recommend changing owners of these directories to the web server. For example, in many linux OS's the web server user is 'apache', 'nobody', or 'www-data'. So if 'apache' were the web server user name, could use the command 'chown -R apache:apache directory_name' command.<br>\n";
+        echo "Fix above directory permissions and then click the 'Check Again' button to re-check directories.<br>";
+       	echo "<FORM METHOD='POST'><INPUT TYPE='SUBMIT' VALUE='Check Again'><br></FORM><br>\n";
 	break;
 }
 
