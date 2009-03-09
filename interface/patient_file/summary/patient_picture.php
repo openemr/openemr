df58f4caa400366aa97a14ac00128572e5450080
markleeds <markleeds>
2009-03-09 18:37:48 +0000

    Removed unnecessary variable.

--- interface/patient_file/summary/patient_picture.php
+++ interface/patient_file/summary/patient_picture.php
@@ -6,7 +6,6 @@ function pic_array() {
     $pics = array();
     $sql_query = "select documents.url from documents join categories_to_documents on documents.id = categories_to_documents.document_id join categories on categories.id = categories_to_documents.category_id where categories.name like '$picture_directory' and documents.foreign_id = ".$_SESSION['pid'];
     if ($query = sqlStatement($sql_query)) {
-      $filename = '';
       while ($results = mysql_fetch_array($query)) {
         $tmp = explode("documents",$results['url']);
         if (isset($tmp[1])) {
