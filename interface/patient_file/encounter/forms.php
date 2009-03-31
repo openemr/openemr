5c20e2a7b8507a3c728b6ee1bddab6462ba22896
markleeds <markleeds>
2009-03-31 12:14:00 +0000

    Created an exception to the rule that clicking on form content goes to view.php for editing. This exception is for CAMOS, which has html links in it's content generated by report.php, to prevent unexpected behavior when these links are clicked.

--- interface/patient_file/encounter/forms.php
+++ interface/patient_file/encounter/forms.php
@@ -86,8 +86,21 @@ if ($result = getFormByEncounter($pid, $encounter, "id, date, form_id, form_name
         else continue;
 
         // $form_info = getFormInfoById($iter['id']);
-        echo '<tr title="Edit form" '.
-             'id="'.$formdir.'~'.$iter['form_id'].'" class="text onerow">';
+        if (strtolower(substr($iter['form_name'],0,5)) == 'camos') {
+	//CAMOS generates links from report.php and these links should
+	//be clickable without causing view.php to come up unexpectedly.
+	//I feel that the JQuery code in this file leading to a click
+	//on the report.php content to bring up view.php steps on a
+	//form's autonomy to generate it's own html content in it's report
+	//but until any other form has a problem with this, I will just
+	//make an exception here for CAMOS and allow it to carry out this
+	//functionality for all other forms.  --Mark
+	        echo '<tr title="Edit form" '.
+       		      'id="'.$formdir.'~'.$iter['form_id'].'">';
+	} else {
+	        echo '<tr title="Edit form" '.
+       		      'id="'.$formdir.'~'.$iter['form_id'].'" class="text onerow">';
+	}
         $user = getNameFromUsername($iter['user']);
 
         $form_name = ($formdir == 'newpatient') ? "Patient Encounter" : $iter['form_name'];
