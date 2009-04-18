56afd2e08eb80a029095429c94800a59aa195615
bradymiller <bradymiller>
2009-04-18 17:23:55 +0000

    added check to ensure english constants not modified

--- contrib/util/language_translations/buildLanguageDatabase.pl
+++ contrib/util/language_translations/buildLanguageDatabase.pl
@@ -20,13 +20,26 @@
 #   language spreadsheet:
 #  ./buildLanguageDatabase.pl openemr_language_table.tsv
 #
+#  -Below command will build the sql dumpfile from given tsv
+#   language spreadsheet and compare with a constants list to
+#   ensure constants didn't get edited (output will go to
+#   the log file:
+#  ./buildLanguageDatabase.pl openemr_language_table.tsv constants.txt
 #
 
 use strict;
 
+# Put current known constant mismatches here, which are basically
+# constants that get modified during the pipeline and don't look
+# like originals in the end. If this number increases, a english constant
+# was likely modified in the spreadsheet, and can then use log output
+# to localize and fix the problem.  As of list of 3.0.1 constants
+# the known number of mismatched constants is 57 .
+my $mismatchesKnown = 57;
+
 my $de = "\t";
 my $filenameOut = "languageTranslations.sql";
-my $logFile = "errorLog.txt";
+my $logFile = "log.txt";
 my $constantIdColumn = 0; # 0 is lowest
 my $constantColumn = 1; # 0 is lowest 
 my $constantRow = 5; # 0 is lowest
@@ -34,36 +47,54 @@ my $languageNumRow = 0; # 0 is lowest
 my $languageIdRow = 1; # 0 is lowest
 my $languageNameRow = 2; # 0 is lowest
 my $inputFilename;
+my $checkFilename; # holds list of constants if checking
+my $flagCheck = 0;
+my @previousConstants;
 my @inputFile;
 my @inputFileProcessed;
 
+# open output file
+open(LOGFILE, ">$logFile") or die "unable to open log file";
+
 # ensure only one parameter given (for now)
-if (@ARGV > 1) {
+if (@ARGV > 2) {
  die "\nERROR: Too many parameters. Follow instructions found in buildLanguageDatabase.pl file.\n\n";
  }
 elsif (@ARGV < 1) {
- die "\nERROR: Need a parameter. Follow instructions found in buildLanguageDatabase.pl file.\n\n";
+ die "\nERROR: Need a parameter(s). Follow instructions found in buildLanguageDatabase.pl file.\n\n";
+}
+elsif (@ARGV == 2) {
+ $flagCheck = 1;
+ $inputFilename = $ARGV[0];
+ $checkFilename = $ARGV[1];
+}
+elsif (@ARGV == 1) {
+ $inputFilename = $ARGV[0];
 }
 else {
+ print LOGFILE "ERROR: with parameters\n\n";
 }
-$inputFilename = $ARGV[0];
 
-# open log file and output file
-open(LOGFILE, ">$logFile") or die "unable to open log file";
+# open output file
 open(OUTPUTFILE, ">$filenameOut") or die "unable to open output file";
 
-# if comparing, then open comparison file and store in array (after set this up
-# if ($compareFlag) {
-# open(MYINPUTFILE, "<$comparisonFile") or die "unable to open file";
-# @previousConstants = <MYINPUTFILE>;
-# close(MYINPUTFILE);
-# }
-
+# if checking, then open check file and store in array (after set this up
+if ($flagCheck) {
+ open(MYINPUTFILE, "<$checkFilename") or die "unable to open file";
+ @previousConstants = <MYINPUTFILE>;
+ close(MYINPUTFILE);
+}
 
 # place spreadsheet into array 
 open(MYINPUTFILE2, "<$inputFilename") or die "unable to open file";
 @inputFile = <MYINPUTFILE2>;
 close(MYINPUTFILE2);
+
+# do check first if list of constants was given
+if ($flagCheck) {
+ checkConstants();
+}
+
 
 # FIRST, remove newlines, blank lines, escape characters, and windows returns
 # SECOND, place the escape characters in all required sql characters
@@ -97,6 +128,45 @@ $outputString .= createDefinitions();
 print OUTPUTFILE $outputString;
 
 
+    
+#
+# function to compare to original constants
+# globals - @previousConstants, @inputFile, $constantRow, $de, LOGFILE,
+#           $constantIdColumn, $constantColumn
+# return - none
+#
+sub checkConstants () {
+
+ print LOGFILE "Checking constants:\n\n";
+ my $counter = $constantRow;
+ my $badCount = 0;
+ foreach my $var (@previousConstants) {
+  chomp($var);
+  my @tempRow = split($de,@inputFile[$counter]);
+  my $tempId = $tempRow[$constantIdColumn];
+  my $tempConstant = $tempRow[$constantColumn];
+  if ($var ne $tempConstant) {
+   print LOGFILE "Following constant not same:\n";
+   print LOGFILE "\toriginal- val:$var\n";
+   print LOGFILE "\tspreadsheet- ID:$tempId val:$tempConstant\n";
+   $badCount += 1;
+  }
+
+  $counter += 1;     
+ }
+
+ print LOGFILE "\nDone checking constants:\n";
+ print LOGFILE "\t".$badCount." mismatches found (known is ".$mismatchesKnown.")\n";
+ if ($badCount == $mismatchesKnown) {
+  print LOGFILE "Good, constants weren't modified by translators\n\n";
+ }
+ else {
+  print LOGFILE "ERROR: Constant(s) have been modified by translators\n\n";
+ }
+    
+ return;
+}
+
 #
 # function to build lang_languages dumpfile
 # globals - @inputFileProcessed, $constantColumn, $constantRow,
@@ -126,7 +196,7 @@ sub createLanguages() {
 DROP TABLE IF EXISTS `lang_languages`;
 CREATE TABLE `lang_languages` (
   `lang_id` int(11) NOT NULL auto_increment,
-  `lang_code` char(2) character set latin1 NOT NULL default '',
+  `lang_code` char(2) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `lang_description` varchar(100) character set utf8 collate utf8_unicode_ci default NULL,
   UNIQUE KEY `lang_id` (`lang_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=".$tempCounter." ;
