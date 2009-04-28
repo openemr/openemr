6cb9c55e7b3cb512768c4d4acf8750e3ff937377
bradymiller <bradymiller>
2009-04-28 05:37:07 +0000

    added more language table sanity checks

--- contrib/util/language_translations/buildLanguageDatabase.pl
+++ contrib/util/language_translations/buildLanguageDatabase.pl
@@ -23,7 +23,7 @@
 #  -Below command will build the sql dumpfile from given tsv
 #   language spreadsheet and compare with a constants list to
 #   ensure constants didn't get edited (output will go to
-#   the log file:
+#   the log file), and will also fix limited issues:
 #  ./buildLanguageDatabase.pl openemr_language_table.tsv constants.txt
 #
 
@@ -35,7 +35,7 @@ use strict;
 # was likely modified in the spreadsheet, and can then use log output
 # to localize and fix the problem.  As of list of 3.0.1 constants
 # the known number of mismatched constants is 57 .
-my $mismatchesKnown = 57;
+my $mismatchesKnown = 95;
 
 # Hold variables to calculate language database statistics
 my $totalConstants;
@@ -62,7 +62,7 @@ my @inputFileProcessed;
 # open output file
 open(LOGFILE, ">$logFile") or die "unable to open log file";
 
-# ensure only one parameter given (for now)
+# collect parameters
 if (@ARGV > 2) {
  die "\nERROR: Too many parameters. Follow instructions found in buildLanguageDatabase.pl file.\n\n";
  }
@@ -96,11 +96,6 @@ open(MYINPUTFILE2, "<$inputFilename") or die "unable to open file";
 @inputFile = <MYINPUTFILE2>;
 close(MYINPUTFILE2);
 
-# do check first if list of constants was given
-if ($flagCheck) {
- checkConstants();
-}
-
 # FIRST, remove newlines, blank lines, escape characters, and windows returns
 # SECOND, place the escape characters in all required sql characters
 foreach my $tempLine (@inputFile) {
@@ -121,14 +116,19 @@ foreach my $tempLine (@inputFile) {
  }
 }
 
+# check and fix modified constants (and constant id's)
+if ($flagCheck) {
+ @inputFileProcessed = checkConstants(@inputFileProcessed);
+}
+
 # parse lang_languages
-my $outputString = createLanguages();
+my $outputString = createLanguages(@inputFileProcessed);
 
 # parse lang_constants
-$outputString .= createConstants();
+$outputString .= createConstants(@inputFileProcessed);
 
 # parse lang_definitions
-$outputString .= createDefinitions();
+$outputString .= createDefinitions(@inputFileProcessed);
 
 print OUTPUTFILE $outputString;
 
@@ -149,31 +149,61 @@ my @sorted_tempArray = sort { lc($a) cmp lc($b) } @tempArray;
 foreach my $var (@sorted_tempArray) {
  print LOGFILE $var;
 }
+
+# close files
+close(OUTPUTFILE);
+close(LOGFILE);
     
 #
 # function to compare to original constants
-# globals - @previousConstants, @inputFile, $constantRow, $de, LOGFILE,
+# param - array of processed file
+# globals - @previousConstants, $constantRow, $de, LOGFILE,
 #           $constantIdColumn, $constantColumn
 # return - none
 #
 sub checkConstants () {
-
+ my (@page) = @_;
+    
  print LOGFILE "Checking constants:\n\n";
  my $counter = $constantRow;
  my $badCount = 0;
+ my $idErrorFlag = 0;
  foreach my $var (@previousConstants) {
   chomp($var);
-  my @tempRow = split($de,@inputFile[$counter]);
+  my @tempRow = split($de,$page[$counter]);
   my $tempId = $tempRow[$constantIdColumn];
   my $tempConstant = $tempRow[$constantColumn];
+    
+  # ensure constant has not been altered
   if ($var ne $tempConstant) {
    print LOGFILE "Following constant not same:\n";
    print LOGFILE "\toriginal- val:$var\n";
    print LOGFILE "\tspreadsheet- ID:$tempId val:$tempConstant\n";
    $badCount += 1;
-  }
 
-  $counter += 1;     
+   # apply fix (replace with original after reset escape characters)
+   my $fixedVar = $var;
+   $fixedVar =~ s/\\//g;
+   $fixedVar =~ s/\'/\\\'/g;
+   $fixedVar =~ s/\"/\\\"/g;
+   $tempRow[$constantColumn] = $fixedVar;
+   $page[$counter] = join($de,@tempRow);
+      
+  }
+     
+  # ensure constant id number has not been altered
+  my $realID = ($counter - $constantRow + 1);
+  if ($realID != $tempId) {
+   $idErrorFlag = 1;
+   print LOGFILE "\nERROR: Constant ID number ".$realID." has been modified to ".$tempId."!!!\n\n";
+
+   # apply fix (replace with original after reset escape characters)
+   $tempRow[$constantIdColumn] = $realID;
+   $page[$counter] = join($de,@tempRow);
+  }     
+
+  # increment counter
+  $counter += 1;
  }
 
  print LOGFILE "\nDone checking constants:\n";
@@ -184,25 +214,30 @@ sub checkConstants () {
  else {
   print LOGFILE "ERROR: Constant(s) have been modified by translators\n\n";
  }
+ if ($idErrorFlag) {
+  print LOGFILE "ERROR: Constant ID number(s) have been modified by translators\n\n";
+ }
     
- return;
+ return @page;
 }
 
 #
 # function to build lang_languages dumpfile
-# globals - @inputFileProcessed, $constantColumn, $constantRow,
+# param - array of processed file
+# globals - $constantColumn, $constantRow,
 #           $languageNumRow, $languageIdRow, $languageNameRow,
 #           @numberConstantsLanguages, @languages
 # return - output string
 #
 sub createLanguages() {
-
+ my (@page) = @_;
+    
  # create table input
  my $tempReturn;
  my $tempCounter;
- my @numberRow = split($de,$inputFileProcessed[$languageNumRow]);
- my @idRow = split($de,$inputFileProcessed[$languageIdRow]);
- my @nameRow = split($de,$inputFileProcessed[$languageNameRow]);
+ my @numberRow = split($de,$page[$languageNumRow]);
+ my @idRow = split($de,$page[$languageIdRow]);
+ my @nameRow = split($de,$page[$languageNameRow]);
  for (my $i = $constantColumn; $i < @numberRow; $i++) {
   $tempReturn .= "INSERT INTO `lang_languages` VALUES (".$numberRow[$i].", '".$idRow[$i]."', '".$nameRow[$i]."');\n";
   $tempCounter = $numberRow[$i];
@@ -243,16 +278,18 @@ CREATE TABLE `lang_languages` (
 
 #
 # function to build lang_constants dumpfile
-# globals - @inputFileProcessed, $constantColumn, $constantRow, $constantIdColumn, $totalConstants
+# param - array of processed file
+# globals - $constantColumn, $constantRow, $constantIdColumn, $totalConstants
 # return - nothing
 #
 sub createConstants() {
-
+ my (@page) = @_;
+    
  # create table input
  my $tempReturn;
  my $tempCounter; 
- for (my $i = $constantRow; $i < @inputFileProcessed; $i++) {
-  my @tempRow = split($de,@inputFileProcessed[$i]);
+ for (my $i = $constantRow; $i < @page; $i++) {
+  my @tempRow = split($de,$page[$i]);
   my $tempId = $tempRow[$constantIdColumn];
   my $tempConstant = $tempRow[$constantColumn];
   $tempReturn .= "INSERT INTO `lang_constants` VALUES (".$tempId.", '".$tempConstant."');\n";
@@ -293,21 +330,23 @@ CREATE TABLE `lang_constants` (
 
 #
 # function to build lang_definitions dumpfile
-# globals - @inputFileProcessed, $constantColumn, $constantRow,
+# param - array of processed file
+# globals - $constantColumn, $constantRow,
 #           $languageNumRow, $constantIdColumn, @numberConstantsLanguages, 
 #           $totalDefinitions
 # return - nothing
 #
 sub createDefinitions() {
-
+ my (@page) = @_;
+    
  # create table input
  my $tempReturn;
  my $tempCounter; 
- my @numberRow = split($de,$inputFileProcessed[$languageNumRow]);
+ my @numberRow = split($de,$page[$languageNumRow]);
  my $counter = 1;
  for (my $i = $constantColumn + 1; $i < @numberRow; $i++) {
-  for (my $j = $constantRow; $j < @inputFileProcessed; $j++) {
-   my @tempRow = split($de,@inputFileProcessed[$j]);
+  for (my $j = $constantRow; $j < @page; $j++) {
+   my @tempRow = split($de,$page[$j]);
    my $tempId = $tempRow[$constantIdColumn];
    my $tempDefinition = $tempRow[$i];
    my $tempLangNumber = $numberRow[$i];
