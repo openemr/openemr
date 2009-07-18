054d553b812b686b4631453ec3cbef825cd0aba4
cfapress <cfapress>
2009-07-18 12:59:30 +0000

    corrected omission of translation code

--- interface/forms/vitals/report.php
+++ interface/forms/vitals/report.php
@@ -27,13 +27,19 @@ function vitals_report( $pid, $encounter, $cols, $id) {
             if ($value == "on") { $value = "yes"; } 
 
             $key=ucwords(str_replace("_"," ",$key));
-            if ($key == "BMI Status") {
-	        if ($patient_age <= 20 || (preg_match('/month/', $patient_age))) { 
-		    $value = "See Growth-Chart"; 
-		}
-	    }
 
-            echo "<td><span class=bold>".xl($key).": </span><span class=text>".$value."</span></td>";
+            //modified by BM 06-2009 for required translation
+            if ($key == "Temp Method" || $key == "BMI Status") { 
+                if ($key == "BMI Status") {
+	            if ($patient_age <= 20 || (preg_match('/month/', $patient_age))) { 
+		        $value = "See Growth-Chart"; 
+		    }
+	        }
+                print "<td><span class=bold>" . xl($key) . ": </span><span class=text>" . xl($value) . "</span></td>"; 
+            } 
+            else { 
+                print "<td><span class=bold>" . xl($key) . ": </span><span class=text>$value</span></td>"; 
+            } 
 
             $count++;
 
