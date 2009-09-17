cfa084c084abbee22a1a7d56749c3f1a0288292b
bradymiller <bradymiller>
2009-09-17 07:55:38 +0000

    bug fix by visolveemr - clearing copay entry field after save value

--- interface/patient_file/encounter/copay.php
+++ interface/patient_file/encounter/copay.php
@@ -22,6 +22,15 @@ $N=10
 ?>
 <html>
 <head>
+<script type="text/javascript">
+function cleartext(atrib)
+{
+document.copay_form.code.value=document.copay_form.codeH.value;
+document.copay_form.codeH.value="";
+}
+</script>
+
+
 <?php html_header_show();?>
 <link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
 </head>
@@ -40,9 +49,11 @@ $N=10
 <dt><span class=title><?php xl('Copay','e'); ?></span></dt>
 
 <br>
-<span class='text'><?php xl('$','e'); ?> </span><input type='entry' name='code' value='' size='5' />
+<input type=hidden name=code>
+<span class='text'><?php xl('$','e'); ?> </span><input type='entry' name='codeH' value='' size='5' />
 
-<input type="SUBMIT" value="<?php xl('Save','e'); ?>"><br><br>
+<input type="SUBMIT" value="<?php xl('Save','e');?>" onclick="cleartext('clear')"><br><br>
+
 
 <div<?php if ($GLOBALS['simplified_copay']) echo " style='display:none;'"; ?>>
 <input type="RADIO" name="payment_method" value="cash" checked><?php xl('cash','e'); ?>
