00b780a583ffbee0d1d1e97b68be151b3bdd58a8
bradymiller <bradymiller>
2009-10-09 07:22:20 +0000

    Bug fix by visolveemr to allow selection of units and clear values when save

--- interface/patient_file/encounter/other.php
+++ interface/patient_file/encounter/other.php
@@ -11,7 +11,6 @@ $N=10
 <?php html_header_show();?>
 
 <link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
-
 </head>
 <body class="body_bottom">
 
@@ -24,7 +23,26 @@ $N=10
 
 <form method='post' name='other_form' action="diagnosis.php?mode=add&type=OTHER"
  target='Diagnosis' onsubmit='return top.restoreSession()'>
-
+<script type="text/javascript">
+function clearform(atrib){
+document.other_form.text.value=document.other_form.textH.value;
+document.other_form.code.value=document.other_form.codeH.value;
+document.other_form.fee.value=document.other_form.feeH.value*document.other_form.noofunits.value;
+document.other_form.units.value=document.other_form.noofunits.value;
+document.other_form.textH.value='';
+document.other_form.codeH.value='';
+document.other_form.feeH.value='';
+document.other_form.noofunits.value='1';
+}
+function isNumberKey(evt)
+      {var charCode = (evt.which) ? evt.which : event.keyCode
+         if (charCode > 31 && (charCode < 48 || charCode > 57)) 
+         {
+         alert("<?php xl('Units must be a number','e'); ?>");
+         return false;
+         }
+return true;  }
+</script>
 <dt><span class=title><?php xl('Other','e'); ?></span></dt>
 
 <br>
@@ -33,18 +51,23 @@ $N=10
 <td class="text"><?php xl('code','e'); ?></td>
 <td class="text"><?php xl('description','e'); ?></td>
 <td class="text">&nbsp;&nbsp;&nbsp;<?php xl('fee','e'); ?></td>
+<td class="text"><?php xl('units','e'); ?></td>
 <td></td>
 </tr>
 <tr>
-  <td class="text"><input type=entry name=code size=4>&nbsp;&nbsp;</td>
-  <td class="text"><input type=entry name=text size=15>&nbsp;&nbsp;</td>
-  <td class="text"><?php xl('$','e'); ?> </span><input type=entry name=fee size=5></td>
-  <td>&nbsp;<a class='text' href="javascript:top.restoreSession();document.other_form.submit();">
-   <?php xl('Save','e'); ?></a>
+<input type=hidden name=code>
+<input type=hidden name=text>
+<input type=hidden name=fee>
+<input type=hidden name=units>
+  <td class="text"><input type=entry name=codeH size=4>&nbsp;&nbsp;</td>
+  <td class="text"><input type=entry name=textH size=13 value="">&nbsp;&nbsp;</td>
+  <td class="text"><?php xl('$','e'); ?> </span><input type=entry name=feeH size=5></td>
+  <td> <input type=text name="noofunits" onkeypress="return isNumberKey(event)" size=3 value=1></td>
+  <td>&nbsp;<a class='text' onclick="clearform('clear')" href="javascript:top.restoreSession();document.other_form.submit();">
+   <?php xl('Save','e'); ?> </a>
   </td>
 </tr>
 </table>
-
 </form>
 
 </dl>
