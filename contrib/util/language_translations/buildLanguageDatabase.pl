38ecebb3ac0be0062be2b5cd9f586156752421cd
bradymiller <bradymiller>
2009-10-24 18:30:55 +0000

    updated language constants and tables

--- contrib/util/language_translations/buildLanguageDatabase.pl
+++ contrib/util/language_translations/buildLanguageDatabase.pl
@@ -43,7 +43,7 @@ use strict;
 # was likely modified in the spreadsheet, and can then use log output
 # to localize and fix the problem.  As of list of 3.0.1 constants
 # the known number of mismatched constants is 57 .
-my $mismatchesKnown = 82;
+my $mismatchesKnown = 83;
 
 # Hold variables to calculate language database statistics
 my $totalConstants;
