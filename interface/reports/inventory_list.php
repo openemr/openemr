968f6725f20222b69d5beae49f179e03ac2c8871
sunsetsystems <sunsetsystems>
2010-02-24 01:12:41 +0000

    fixed broken reporting of sales and stock months

--- interface/reports/inventory_list.php
+++ interface/reports/inventory_list.php
@@ -15,14 +15,18 @@
  $thisauth = acl_check('admin', 'drugs');
  if (!$thisauth) die(xl('Not authorized'));
 
- // get drugs
- $res = sqlStatement("SELECT d.*, SUM(di.on_hand) AS on_hand, " .
-  "SUM(ds.quantity) AS sale_quantity, MIN(ds.sale_date) AS min_sale_date " .
+if (!empty($_POST['form_days'])) {
+  $form_days = $_POST['form_days'] + 0;
+}
+else {
+  $form_days = sprintf('%d', (strtotime(date('Y-m-d')) - strtotime(date('Y-01-01'))) / (60 * 60 * 24) + 1);
+}
+
+// get drugs
+$res = sqlStatement("SELECT d.*, SUM(di.on_hand) AS on_hand " .
   "FROM drugs AS d " .
   "LEFT JOIN drug_inventory AS di ON di.drug_id = d.drug_id " .
   "AND di.on_hand != 0 AND di.destroy_date IS NULL " .
-  "LEFT JOIN drug_sales AS ds ON ds.sale_date > DATE_SUB(NOW(), INTERVAL 1 YEAR) " .
-  "AND ds.pid != 0 " .
   "WHERE d.active = 1 " .
   "GROUP BY d.name, d.drug_id ORDER BY d.name, d.drug_id");
 ?>
@@ -57,7 +61,11 @@ a, a:visited, a:hover { color:#0000cc; }
    <?php xl('Inventory List','e'); ?>
   </td>
   <td class='text' align='right'>
-   <input type="button" value="Print" onclick="window.print()">
+   <?php xl('For the past','e'); ?>
+   <input type="input" name="form_days" size='3' value="<?php echo $form_days; ?>" />
+   <?php xl('days','e'); ?>&nbsp;
+   <input type="submit" value="Refresh" />&nbsp;
+   <input type="button" value="Print" onclick="window.print()" />
   </td>
  </tr>
 </table>
@@ -70,20 +78,29 @@ a, a:visited, a:hover { color:#0000cc; }
    <th><?php  xl('NDC','e'); ?></th>
    <th><?php  xl('Form','e'); ?></th>
    <th align='right'><?php  xl('QOH','e'); ?></th>
-   <th><?php  xl('Stock Months','e'); ?></th>
+   <th align='right'><?php  xl('Avg Monthly','e'); ?></th>
+   <th align='right'><?php  xl('Stock Months','e'); ?></th>
   </tr>
  </thead>
  <tbody>
 <?php 
 $encount = 0;
 while ($row = sqlFetchArray($res)) {
+  $srow = sqlQuery("SELECT " .
+    "SUM(quantity) AS sale_quantity " .
+    "FROM drug_sales WHERE " .
+    "drug_id = '" . $row['drug_id'] . "' AND " .
+    "sale_date > DATE_SUB(NOW(), INTERVAL $form_days DAY) " .
+    "AND pid != 0");
+
   ++$encount;
   $bgcolor = "#" . (($encount & 1) ? "ddddff" : "ffdddd");
 
-  $sale_quantity = $row['sale_quantity'];
-  $msd = $row['min_sale_date'];
-  $months = (time() - mktime(0, 0, 0, substr($msd,5,2),
-    substr($msd,8,2), substr($msd,0,4))) / (60 * 60 * 24 * 30.5);
+  $sale_quantity = $srow['sale_quantity'];
+  $months = $form_days / 30.5;
+
+  $monthly = ($months && $sale_quantity) ?
+    sprintf('%0.1f', $sale_quantity / $months) : '&nbsp;';
 
   $stock_months = '&nbsp;';
   if ($sale_quantity != 0) $stock_months = sprintf('%0.1f',
@@ -96,6 +113,7 @@ while ($row = sqlFetchArray($res)) {
        generate_display_field(array('data_type'=>'1','list_id'=>'drug_form'), $row['form']) .
        "</td>\n";
   echo "  <td align='right'>" . $row['on_hand'] . "</td>\n";
+  echo "  <td align='right'>$monthly</td>\n";
   echo "  <td align='right'>$stock_months</td>\n";
   echo " </tr>\n";
  }
