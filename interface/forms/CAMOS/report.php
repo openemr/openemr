f520a9ad521ad5b7c322d43a52e90c753ae5fef7
bradymiller <bradymiller>
2010-03-21 08:20:38 +0000

    added mechanism to not print links in form reports, ie CAMOS and the custom report

--- interface/forms/CAMOS/report.php
+++ interface/forms/CAMOS/report.php
@@ -9,7 +9,7 @@ function CAMOS_report( $pid, $encounter, $cols, $id) {
     //echo "(category) ".stripslashes($data['category'])." | ";
     //echo "(subcategory) ".stripslashes($data['subcategory'])." | ";
     //echo "(item) ".stripslashes($data['item']);
-    echo "<a href='" . $GLOBALS['webroot'] .
+    echo "<div class='navigateLink'><a href='" . $GLOBALS['webroot'] .
       "/interface/forms/CAMOS/rx_print.php?sigline=embossed' target=_new>" . xl('Rx') . "</a>\n";
     echo " | ";
     echo "<a href='" . $GLOBALS['webroot'] .
@@ -25,7 +25,7 @@ function CAMOS_report( $pid, $encounter, $cols, $id) {
       "/interface/forms/CAMOS/notegen.php?pid=".$GLOBALS['pid']."&encounter=".$GLOBALS['encounter']."' target=_new>" . xl('Print This Encounter') . "</a>\n";
     echo " | ";
     echo "<a href='" . $GLOBALS['webroot'] .
-      "/interface/forms/CAMOS/notegen.php' target=_new>" . xl('Print Any Encounter') . "</a>\n";
+      "/interface/forms/CAMOS/notegen.php' target=_new>" . xl('Print Any Encounter') . "</a></div>\n";
 //    echo "<pre>".wordwrap(stripslashes(content_parser($data['content'])))."</pre><hr>\n";
     echo "<pre>".wordwrap(stripslashes(replace($GLOBALS['pid'],$GLOBALS['encounter'],$data['content'])))."</pre><hr>\n";
   }
