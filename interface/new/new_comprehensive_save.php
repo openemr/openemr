ded7c4d5debe117972b0202f65028ca7d129d41f
bradymiller <bradymiller>
2010-05-16 00:56:30 +0000

    place insurance form into the new patient demographics collection, mostly contributed by Ed

--- interface/new/new_comprehensive_save.php
+++ interface/new/new_comprehensive_save.php
@@ -71,13 +71,114 @@ while ($frow = sqlFetchArray($fres)) {
 updatePatientData($pid, $newdata['patient_data'], true);
 updateEmployerData($pid, $newdata['employer_data'], true);
 
+$i1dob = fixDate(formData("i1subscriber_DOB"));
+$i1date = fixDate(formData("i1effective_date"));
+
 // sqlStatement("unlock tables");
 // end table lock
 
 newHistoryData($pid);
-newInsuranceData($pid, "primary");
-newInsuranceData($pid, "secondary");
-newInsuranceData($pid, "tertiary");
+newInsuranceData(
+  $pid,
+  "primary",
+  formData("i1provider"),
+  formData("i1policy_number"),
+  formData("i1group_number"),
+  formData("i1plan_name"),
+  formData("i1subscriber_lname"),
+  formData("i1subscriber_mname"),
+  formData("i1subscriber_fname"),
+  formData("form_i1subscriber_relationship"),
+  formData("i1subscriber_ss"),
+  $i1dob,
+  formData("i1subscriber_street"),
+  formData("i1subscriber_postal_code"),
+  formData("i1subscriber_city"),
+  formData("form_i1subscriber_state"),
+  formData("form_i1subscriber_country"),
+  formData("i1subscriber_phone"),
+  formData("i1subscriber_employer"),
+  formData("i1subscriber_employer_street"),
+  formData("i1subscriber_employer_city"),
+  formData("i1subscriber_employer_postal_code"),
+  formData("form_i1subscriber_employer_state"),
+  formData("form_i1subscriber_employer_country"),
+  formData('i1copay'),
+  formData('form_i1subscriber_sex'),
+  $i1date,
+  formData('i1accept_assignment')
+);
+
+
+$i2dob = fixDate(formData("i2subscriber_DOB"));
+$i2date = fixDate(formData("i2effective_date"));
+
+
+
+newInsuranceData(
+  $pid,
+  "secondary",
+  formData("i2provider"),
+  formData("i2policy_number"),
+  formData("i2group_number"),
+  formData("i2plan_name"),
+  formData("i2subscriber_lname"),
+  formData("i2subscriber_mname"),
+  formData("i2subscriber_fname"),
+  formData("form_i2subscriber_relationship"),
+  formData("i2subscriber_ss"),
+  $i2dob,
+  formData("i2subscriber_street"),
+  formData("i2subscriber_postal_code"),
+  formData("i2subscriber_city"),
+  formData("form_i2subscriber_state"),
+  formData("form_i2subscriber_country"),
+  formData("i2subscriber_phone"),
+  formData("i2subscriber_employer"),
+  formData("i2subscriber_employer_street"),
+  formData("i2subscriber_employer_city"),
+  formData("i2subscriber_employer_postal_code"),
+  formData("form_i2subscriber_employer_state"),
+  formData("form_i2subscriber_employer_country"),
+  formData('i2copay'),
+  formData('form_i2subscriber_sex'),
+  $i2date,
+  formData('i2accept_assignment')
+);
+
+$i3dob  = fixDate(formData("i3subscriber_DOB"));
+$i3date = fixDate(formData("i3effective_date"));
+
+newInsuranceData(
+  $pid,
+  "tertiary",
+  formData("i3provider"),
+  formData("i3policy_number"),
+  formData("i3group_number"),
+  formData("i3plan_name"),
+  formData("i3subscriber_lname"),
+  formData("i3subscriber_mname"),
+  formData("i3subscriber_fname"),
+  formData("form_i3subscriber_relationship"),
+  formData("i3subscriber_ss"),
+  $i3dob,
+  formData("i3subscriber_street"),
+  formData("i3subscriber_postal_code"),
+  formData("i3subscriber_city"),
+  formData("form_i3subscriber_state"),
+  formData("form_i3subscriber_country"),
+  formData("i3subscriber_phone"),
+  formData("i3subscriber_employer"),
+  formData("i3subscriber_employer_street"),
+  formData("i3subscriber_employer_city"),
+  formData("i3subscriber_employer_postal_code"),
+  formData("form_i3subscriber_employer_state"),
+  formData("form_i3subscriber_employer_country"),
+  formData('i3copay'),
+  formData('form_i3subscriber_sex'),
+  $i3date,
+  formData('i3accept_assignment')
+);
 ?>
 <html>
 <body>
