7aaff71b609ea169eec8d551ab2fd401e6f68791
rachoac <rachoac>
2010-07-13 03:34:49 +0000

    Bug 2995293: Fixed

--- interface/logview/logview.php
+++ interface/logview/logview.php
@@ -8,7 +8,7 @@ require_once("$srcdir/formatting.inc.php");
 <head>
 <?php html_header_show();?>
 <link rel="stylesheet" href='<?php echo $GLOBALS['webroot'] ?>/library/dynarch_calendar.css' type='text/css'>
-<script type="text/javascript" src="../../../library/dialog.js"></script>
+<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/dialog.js"></script>
 <script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/dynarch_calendar.js"></script>
 <?php include_once("{$GLOBALS['srcdir']}/dynarch_calendar_en.inc.php"); ?>
 <script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/dynarch_calendar_setup.js"></script>
