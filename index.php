6d7f92c673e2c4fc87055cd4fb547f48ea9aa1ed
sunsetsystems <sunsetsystems>
2010-09-04 18:22:01 +0000

    better handling of site ID

--- index.php
+++ index.php
@@ -4,27 +4,24 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
-$ignoreAuth = true;
-require_once("interface/globals.php");
-?>                                                                                        
-<html>
-<head></head>
+// Set the site ID if required.  This must be done before any database
+// access is attempted.
 
-<?php
-if ($config == 1) {
-?>                                                                                        
-<body ONLOAD="javascript:top.location.href='<?php echo "interface/login/login_frame.php"?>';">
-OpenEMR requires Javascript and a GUI browser. We can't promise you                       
-anything but try following <a href="interface/login/login.php"                            
->this link</a> to continue.                                                               
-<?php                                                                                     
-} else {
-?>                                                                       
-<body ONLOAD="javascript:top.location.href='<?php echo "setup.php"?>';">     
- OpenEMR requires Javascript and a GUI browser. We can't promise you     
-anything but try following <a href="setup.php">this link</a> to continue.
-<?php                                                                    
-}
+if (!empty($_GET['site']))
+  $site_id = $_GET['site'];
+else if (is_dir("sites/" . $_SERVER['HTTP_HOST']))
+  $site_id = $_SERVER['HTTP_HOST'];
+else
+  $site_id = 'default';
+
+require_once("sites/$site_id/sqlconf.php");
 ?>
+<html>
+<?php if ($config == 1) { ?>
+<body ONLOAD="javascript:top.location.href='<?php echo "interface/login/login_frame.php?site=$site_id" ?>';">
+<?php } else { ?>
+<body ONLOAD="javascript:top.location.href='<?php echo "setup.php?site=$site_id" ?>';">     
+<?php } ?>
+Redirecting...
 </body>
 </html>
