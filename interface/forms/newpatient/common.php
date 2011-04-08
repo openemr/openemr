0ffb96274e44c11914159d0fa92b943698c3865e
Paul Simon <paul@zhservices.com>
2011-04-08 17:38:02 +0530

    The Refresh button on the calendar & blank for Onset/hosp. date is corrected.
    Signed-off-by: Paul Simon <paul@zhservices.com>

--- interface/forms/newpatient/common.php
+++ interface/forms/newpatient/common.php
@@ -277,7 +277,7 @@ if ($fres) {
      <td class='bold' nowrap><?php xl('Onset/hosp. date:','e'); ?></td>
      <td class='text' nowrap><!-- default is blank so that while generating claim the date is blank. -->
       <input type='text' size='10' name='form_onset_date' id='form_onset_date'
-       value='<?php echo $viewmode ? substr($result['onset_date'], 0, 10) : ''; ?>' 
+       value='<?php echo $viewmode && $result['onset_date']!='0000-00-00 00:00:00' ? substr($result['onset_date'], 0, 10) : ''; ?>' 
        title='<?php xl('yyyy-mm-dd Date of onset or hospitalization','e'); ?>'
        onkeyup='datekeyup(this,mypcc)' onblur='dateblur(this,mypcc)' />
         <img src='../../pic/show_calendar.gif' align='absbottom' width='24' height='22'
