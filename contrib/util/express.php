b2eb213d329ffbabcf041fe543d894e8d213b529
bradymiller <bradymiller@users.sourceforge.net>
2011-04-14 23:55:00 -0700

    Security bug fix: Fix for arbitrary database creation/database enumeration

--- contrib/util/express.php
+++ contrib/util/express.php
@@ -5,6 +5,12 @@
 </head>
 <body>
 <?php
+
+// This exit is to avoid malicious use of this script.
+//  (before running this script, comment out the exit command)
+//  (when you are done running the script, remember to comment it out)
+exit;
+
 if (!$_POST['submit']) {
 ?>
 <form method=post>
