7e527eae0ee84356edec5bacd679f60245f53b5f
bradymiller <bradymiller@users.sourceforge.net>
2011-05-12 01:19:33 -0700

    Minor CQM/AMC report engine bug fix

--- library/classes/rulesets/ReportTypes.php
+++ library/classes/rulesets/ReportTypes.php
@@ -30,6 +30,7 @@ class ReportTypes
         "rule_dm_foot_cqm" => array( ReportTypes::CQM, "NFQ_Unimplemented" ),
         "rule_dm_bp_control_cqm" => array( ReportTypes::CQM, "NFQ_Unimplemented" ),
         "rule_dm_a1c_cqm" => array( ReportTypes::CQM, "NFQ_Unimplemented" ),
+        "rule_dm_ldl_cqm" => array( ReportTypes::CQM, "NFQ_Unimplemented" ),
     	"problem_list_amc" => array( ReportTypes::AMC, "ProblemList" ),
     	"med_list_amc" => array( ReportTypes::AMC, "AMC_Unimplemented" ),
     	"med_allergy_list_amc" => array( ReportTypes::AMC, "AMC_Unimplemented" ),
