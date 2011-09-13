873110104daa4ddd6233317b08141e44bed54ef3
Rod Roark <rod@sunsetsystems.com>
2011-09-13 15:42:19 -0700

    Fix saving of quotes in letter generator.

--- interface/patient_file/letter.php
+++ interface/patient_file/letter.php
@@ -1,10 +1,14 @@
 <?php
-// Copyright (C) 2007-2010 Rod Roark <rod@sunsetsystems.com>
+// Copyright (C) 2007-2011 Rod Roark <rod@sunsetsystems.com>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
+
+// Undo magic quotes and do not allow fake register globals.
+$sanitize_all_escapes  = true;
+$fake_register_globals = false;
 
 include_once("../globals.php");
 include_once($GLOBALS['srcdir'] . "/patient.inc");
