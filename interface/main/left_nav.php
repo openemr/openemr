a5825142d82101dca7dd0e7770c3851dbae9cada
Kevin Yeh <kevinyeh@alum.mit.edu>
2011-10-31 09:14:05 -0400

    updating jquery treeview version to 1.4.1.

--- interface/main/left_nav.php
+++ interface/main/left_nav.php
@@ -352,9 +352,9 @@ function genFindBlock() {
 }
 </style>
 
-<link rel="stylesheet" href="../../library/js/jquery.treeview-1.3/jquery.treeview.css" />
+<link rel="stylesheet" href="../../library/js/jquery.treeview-1.4.1/jquery.treeview.css" />
 <script src="../../library/js/jquery-1.6.4.min.js" type="text/javascript"></script>
-<script src="../../library/js/jquery.treeview-1.3/jquery.treeview.min.js" type="text/javascript"></script>
+<script src="../../library/js/jquery.treeview-1.4.1/jquery.treeview.js" type="text/javascript"></script>
 
 <script type="text/javascript" src="../../library/dialog.js"></script>
 
