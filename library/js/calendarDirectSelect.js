a7d500b76716703e612b710f3bb86d5b5d5901ab
yehster <kevinyeh@alum.mit.edu>
2011-11-30 12:44:19 -0500

    Copyright notice for new script file

--- library/js/calendarDirectSelect.js
+++ library/js/calendarDirectSelect.js
@@ -1,4 +1,28 @@
-    function updateApptTime(marker,index,y,date,provider)
+// +-----------------------------------------------------------------------------+
+// Copyright (C) 2011 IntegralEMR LLC <kevin.y@integralemr.com>
+//
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//
+// A copy of the GNU General Public License is included along with this program:
+// openemr/interface/login/GnuGPL.html
+// For more information write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// Author:   Kevin Yeh <kevin.y@integralemr.com>
+//
+// +------------------------------------------------------------------------------+
+function updateApptTime(marker,index,y,date,provider)
     {
         row=$("#times table tr").eq(index);
         timeSlot=row.find("td a");
