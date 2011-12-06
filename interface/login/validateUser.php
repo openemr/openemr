7e3c2c1c736b19a66946f72974e3574eb6c3789c
bradymiller <bradymiller@users.sourceforge.net>
2011-12-06 14:52:33 -0800

    high priority sql-injection security fix

--- interface/login/validateUser.php
+++ interface/login/validateUser.php
@@ -1,13 +1,17 @@
 <?php
 //VicarePlus :: This file checks the hashing algorithm used for the password in the initial login.
 //VicarePlus :: This file is called by a jquery function in login.php
+
+// Use new security method
+$fake_register_globals=false;
+$sanitize_all_escapes=true;
+
 $ignoreAuth=true;
 include_once("../globals.php");
 include_once("$srcdir/sql.inc");
 $user = $_GET['u'];
-$authDB = sqlQuery("select password,length(password) as passlength from users where username = '$user'");
+$authDB = sqlQuery("select length(password) as passlength from users where username = ?", array($user) );
 $passlength = $authDB['passlength'];
-$pw = $authDB['password'];
 //VicarePlus :: If the length of the password is 32, 'md5' hashing algorithm is used.
 if ($passlength == 32)
 {
