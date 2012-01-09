952e8bb389c26a868a3f85bec80213e95b81d74d
Devender Singh <devender.singh@mmf.com>
2012-01-09 16:34:40 +0530

    Committed addlistitem.php after including json_encode function so as slashes and quotes could be handled automatically

--- library/ajax/addlistitem.php
+++ library/ajax/addlistitem.php
@@ -32,8 +32,8 @@ $exists_title = sqlQuery("SELECT * FROM list_options WHERE ".
                     " list_id='".$list_id."'".
                     " and title='".trim($title). "'" 
                     );
-if ($exists_title) {
-	echo '{"error":"' . addslashes( xl('Record already exist')) . '"}'; 
+if ($exists_title) { 
+	echo json_encode(array("error"=> xl('Record already exist') ));
 	exit; 
 }
 
@@ -42,7 +42,7 @@ $exists_id = sqlQuery("SELECT * FROM list_options WHERE ".
                     " and option_id='".trim($option_id)."'"
                     );
 if ($exists_id) { 
-	echo '{"error":"' . addslashes( xl('Record already exist')) . '"}';
+	echo json_encode(array("error"=> xl('Record already exist') ));
 	exit; 
 }
 
