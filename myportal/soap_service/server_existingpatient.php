8789fcf041deb4f8b8f9d46a468d12126f6969a7
Jacob T.Paul <jacob@zhservices.com>
2012-04-25 13:57:02 +0530

    Modification made to fix a bug in patient portal
    Signed-off-by: Jacob T.Paul <jacob@zhservices.com>

--- myportal/soap_service/server_existingpatient.php
+++ myportal/soap_service/server_existingpatient.php
@@ -164,7 +164,7 @@ class existingpatient {
                 return array($query,array($pid,'pre_payment'));
             break;
             case 'A10':
-                $query = "SELECT sum(pay_total)  as pay_amount FROM ar_session,ar_activity WHERE patient_id=? AND adjustment_code=?
+                $query = "SELECT sum(pay_amount)  as pay_amount FROM ar_session,ar_activity WHERE patient_id=? AND adjustment_code=?
                           AND pid=? AND ar_session.session_id=ar_activity.session_id  and pay_amount>0";
                 return array($query,array($pid,'pre_payment',$pid));
             break;
@@ -173,7 +173,7 @@ class existingpatient {
                 return array($query,array($pid,'pre_payment'));
             break;
             case 'A12':
-                $query = "SELECT sum(pay_total)  as pay_amount FROM ar_session,ar_activity WHERE patient_id=? AND adjustment_code!=?
+                $query = "SELECT sum(pay_amount)  as pay_amount FROM ar_session,ar_activity WHERE patient_id=? AND adjustment_code!=?
                           AND pid=? AND ar_session.session_id=ar_activity.session_id  and pay_amount>0";
                 return array($query,array($pid,'pre_payment',$pid));
             break;
