443f4e958e687c846f5760219fdfec7d9c560e8e
bradymiller <bradymiller@users.sourceforge.net>
2012-06-05 00:09:31 -0700

    Added back copyright/license information to library/gen_x12_837.inc.php script

--- library/gen_x12_837.inc.php
+++ library/gen_x12_837.inc.php
@@ -1,5 +1,10 @@
 <?php
-//various changes, tested working with multiple carriers
+// Copyright (C) 2007-2011 Rod Roark <rod@sunsetsystems.com>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 
 require_once("Claim.class.php");
 function stripZipCode($zip)
