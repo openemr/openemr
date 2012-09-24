820bf18448e7ee8a0ffa71356d0985c79ae5394d
bradymiller <bradymiller@users.sourceforge.net>
2012-09-24 08:51:45 -0700

    billing bug fix

--- interface/reports/report.inc.php
+++ interface/reports/report.inc.php
@@ -1,5 +1,8 @@
 <?php
-// +-----------------------------------------------------------------------------+ 
+// +-----------------------------------------------------------------------------+
+//
+// Common php functions are stored in this page.
+// 
 // Copyright (C) 2011 Z&H Consultancy Services Private Limited <sam@zhservices.com>
 //
 //
@@ -24,16 +27,16 @@
 //           Paul Simon K <paul@zhservices.com> 
 //
 // +------------------------------------------------------------------------------+
-?>
-<!-- Common php functions are stored in this page.  -->
-<?php
+
 function stripslashes_deep($value)
 {
     $value = is_array($value) ? array_map('stripslashes_deep', $value) : strip_escape_custom($value);
     return $value;
 }
+
+//Parses the search value part of the criteria and prepares for sql.
 function PrepareSearchItem($SearchItem)
- {//Parses the search value part of the criteria and prepares for sql.
+ {
   $SplitArray=split(' like ',$SearchItem);
   if(isset($SplitArray[1]))
    {
@@ -53,8 +56,10 @@ function PrepareSearchItem($SearchItem)
    }
   return($SearchItem);
  }
+
+//Parses the database value and prepares for display.
 function BuildArrayForReport($Query)
- {//Parses the database value and prepares for display.
+ {
   $array_data=array();
   $res = sqlStatement($Query);
   while($row=sqlFetchArray($res))
@@ -63,37 +68,38 @@ function BuildArrayForReport($Query)
    }
   return $array_data;
  }
+
+//The criteria  "Insurance Company" is coded here.The ajax one
 function InsuranceCompanyDisplay()
- {//The criteria  "Insurance Company" is coded here.The ajax one
+ {
   global $ThisPageSearchCriteriaDisplay,$ThisPageSearchCriteriaKey,$ThisPageSearchCriteriaIndex,$web_root;
-?>
-<table width="140" border="0" cellspacing="0" cellpadding="0">
-      <tr>
-        <td width="140" colspan="2">
-            <iframe id="frame_to_hide" style="position:absolute;display:none; width:240px; height:100px" frameborder=0 
-                scrolling=no marginwidth=0 src="" marginheight=0>hello</iframe>
-        <input type="hidden" id="hidden_ajax_close_value" value="<?php echo formData('type_code') ?>" /><input name='type_code'  id='type_code' class="text "
-        style=" width:140px;"  title="<?php echo htmlspecialchars(xl("Type Id or Name.3 characters minimum (including spaces)."), ENT_QUOTES) ?>"
-        onfocus="hide_frame_to_hide();appendOptionTextCriteria('<?php echo $ThisPageSearchCriteriaDisplay[$ThisPageSearchCriteriaIndex] ?>',
-                                    '<?php echo $ThisPageSearchCriteriaKey[$ThisPageSearchCriteriaIndex] ?>',
-                                    document.getElementById('type_code').value,document.getElementById('div_insurance_or_patient').innerHTML,
-                                    ' = ',
-                                    'text')" onblur="show_frame_to_hide()" onKeyDown="PreventIt(event)" value="<?php echo formData('type_code') ?>"  autocomplete="off"   /><br> 
-        <!--onKeyUp="ajaxFunction(event,'non','search_payments.php');"-->
-            <div id='ajax_div_insurance_section'>
-            <div id='ajax_div_insurance_error'>            </div>
-            <div id="ajax_div_insurance" style="display:none;"></div>
-            </div>
-            </div>        </td>
-      </tr>
-      <tr height="5"><td colspan="2"></td></tr>
-      <tr>
-        <td><div  name="div_insurance_or_patient" id="div_insurance_or_patient" class="text"  style="border:1px solid black; padding-left:5px; width:50px; height:17px;"><?php echo formData('hidden_type_code') ?></div><input type="hidden" name="description"  id="description" /></td>
-        <td><a href="#" onClick="CleanUpAjax('<?php echo $ThisPageSearchCriteriaDisplay[$ThisPageSearchCriteriaIndex] ?>',
-                                    '<?php echo $ThisPageSearchCriteriaKey[$ThisPageSearchCriteriaIndex] ?>',' = ')"><img src="<?php echo $web_root ?>/interface/pic/Clear.gif" border="0" /></a></td>
-      </tr>
-    </table>
-<input type="hidden" name="hidden_type_code" id="hidden_type_code" value="<?php echo formData('hidden_type_code') ?>"/>
-<?php
+
+  echo '<table width="140" border="0" cellspacing="0" cellpadding="0">'.
+      '<tr>'.
+        '<td width="140" colspan="2">'.
+            '<iframe id="frame_to_hide" style="position:absolute;display:none; width:240px; height:100px" frameborder=0'.
+                'scrolling=no marginwidth=0 src="" marginheight=0>hello</iframe>'.
+        '<input type="hidden" id="hidden_ajax_close_value" value="'.attr($_POST['type_code']).'" /><input name="type_code"  id="type_code" class="text "'.
+        'style=" width:140px;"  title="'.xla("Type Id or Name.3 characters minimum (including spaces).").'"'.
+        'onfocus="hide_frame_to_hide();appendOptionTextCriteria(\''.$ThisPageSearchCriteriaDisplay[$ThisPageSearchCriteriaIndex].'\','.
+                                    '\''.$ThisPageSearchCriteriaKey[$ThisPageSearchCriteriaIndex].'\','.
+                                    'document.getElementById(\'type_code\').value,document.getElementById(\'div_insurance_or_patient\').innerHTML,'.
+                                    '\' = \','.
+                                    '\'text\')" onblur="show_frame_to_hide()" onKeyDown="PreventIt(event)" value="'.attr($_POST['type_code']).'"  autocomplete="off"   /><br>'.
+        '<!--onKeyUp="ajaxFunction(event,\'non\',\'search_payments.php\');"-->'.
+            '<div id="ajax_div_insurance_section">'.
+            '<div id="ajax_div_insurance_error">            </div>'.
+            '<div id="ajax_div_insurance" style="display:none;"></div>'.
+            '</div>'.
+            '</div>        </td>'.
+      '</tr>'.
+      '<tr height="5"><td colspan="2"></td></tr>'.
+      '<tr>'.
+        '<td><div  name="div_insurance_or_patient" id="div_insurance_or_patient" class="text"  style="border:1px solid black; padding-left:5px; width:50px; height:17px;">'.attr($_POST['hidden_type_code']).'</div><input type="hidden" name="description"  id="description" /></td>'.
+        '<td><a href="#" onClick="CleanUpAjax(\''.$ThisPageSearchCriteriaDisplay[$ThisPageSearchCriteriaIndex].'\','.
+                                    '\''.$ThisPageSearchCriteriaKey[$ThisPageSearchCriteriaIndex].'\',\' = \')"><img src="'.$web_root.'/interface/pic/Clear.gif" border="0" /></a></td>'.
+      '</tr>'.
+    '</table>'.
+'<input type="hidden" name="hidden_type_code" id="hidden_type_code" value="'.attr($_POST['hidden_type_code']).'"/>';
  }
 ?>
