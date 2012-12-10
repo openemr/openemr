cbe8796b44017d0862fc375467c1416cd523a865
bradymiller <bradymiller@users.sourceforge.net>
2012-12-10 10:59:17 -0800

    Fix the dated reminders script headers.
     -Permission was obtained from the author/copyright holder
      of these scripts. These changes ensure the code is compatible
      with the GPL license.

--- interface/main/dated_reminders/dated_reminders_add.php
+++ interface/main/dated_reminders/dated_reminders_add.php
@@ -1,31 +1,24 @@
   <?php          
-//  ------------------------------------------------------------------------ //
-//                OpenEMR Electronic Medical Records System                  //
-//                 Copyright (c) 2012 tajemo.co.za                      //
-//                     <http://www.tajemo.co.za/>                            //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA // 
-// --------------------------------------------------------------------------//
-// Original Author of this file: Craig Bezuidenhout (Tajemo Enterprises)     //
-// Purpose of this file: Used for adding dated reminders                     //
-// --------------------------------------------------------------------------// 
+/**
+ * Used for adding dated reminders.
+ *
+ * Copyright (C) 2012 tajemo.co.za <http://www.tajemo.co.za/>
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Craig Bezuidenhout <http://www.tajemo.co.za/>
+ * @link    http://www.open-emr.org
+ */
                                   
   $fake_register_globals=false;
   $sanitize_all_escapes=true;     
