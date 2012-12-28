8561a06b948840fd2bb3083d2855957e8ceed17c
bradymiller <bradymiller@users.sourceforge.net>
2012-12-28 10:48:43 -0800

    Fixed some GPL script headers that contained text that was not compliant with GPL.
     -Permission was granted from John Williams at Garden State Health Systems
      to make these changes.

--- ccr/createCCRAlerts.php
+++ ccr/createCCRAlerts.php
@@ -1,28 +1,24 @@
 <?php
-//  ------------------------------------------------------------------------ //
-//                     Garden State Health Systems                           //
-//                    Copyright (c) 2010 gshsys.com                          //
-//                      <http://www.gshsys.com/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
+/**
+ * CCR Script.
+ *
+ * Copyright (C) 2010 Garden State Health Systems <http://www.gshsys.com/>
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Garden State Health Systems <http://www.gshsys.com/>
+ * @link    http://www.open-emr.org
+ */
 
 
 $result = getAlertData();
