52b87d3121353428501aff0705177a2211e99390
bradymiller <bradymiller@users.sourceforge.net>
2012-12-28 10:35:11 -0800

    Removed some erroneous copyright headers.
     -discussed on this forum thread:
      http://sourceforge.net/projects/openemr/forums/forum/202506/topic/6405748

--- sites/default/config.php
+++ sites/default/config.php
@@ -1,29 +1,4 @@
 <?php
-/* $Id$ */
-//  ------------------------------------------------------------------------ //
-//                OpenEMR Electronic Medical Records System                  //
-//                    Copyright (c) 2005-2010 oemr.org                       //
-//                       <http://www.oemr.org/>                              //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
 
 $GLOBALS['oer_config']['freeb']['claim_file_dir'] 	= "/usr/share/freeb/public/";
 //currently can be pdf or txt
