54a2be49be3f470f9fbe6cf3ba884e02eaf00ec0
Kevin Yeh <kevinyeh@alum.mit.edu>
2013-01-09 10:52:23 -0500

    Minor typo fix for button table cells in main_title

--- interface/main/main_title.php
+++ interface/main/main_title.php
@@ -79,11 +79,12 @@ $res = sqlQuery("select * from users where username='".$_SESSION{"authUser"}."'"
 <tr><td style="vertical-align:text-bottom;">
 		<a href='' class="css_button_small" style="margin:0px;vertical-align:top;" id='new0' onClick=" return top.window.parent.left_nav.loadFrame2('new0','RTop','new/new.php')">
 		<span><?php echo htmlspecialchars( xl('NEW PATIENT'), ENT_QUOTES); ?></span></a>
-	<td style="vertical-align:text-bottom;">
-		<a href='' class="css_button_small" style="margin:0px;vertical-align:top;display:none;" id='clear_active' onClick="javascript:parent.left_nav.clearactive();return false;">
-		<span><?php echo htmlspecialchars( xl('CLEAR ACTIVE PATIENT'), ENT_QUOTES); ?></span></a>
-        </td>
-	</td></tr>
+    </td>
+    <td style="vertical-align:text-bottom;">
+            <a href='' class="css_button_small" style="margin:0px;vertical-align:top;display:none;" id='clear_active' onClick="javascript:parent.left_nav.clearactive();return false;">
+            <span><?php echo htmlspecialchars( xl('CLEAR ACTIVE PATIENT'), ENT_QUOTES); ?></span></a>
+    </td>
+</tr>
 <?php } //end of acl_check('patients','demo','',array('write','addonly') if ?>
 
 	<tr><td valign="baseline"><B>
