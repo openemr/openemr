2a7ac4ee42536065e1db002efaff5a6a6344b976
Julia Longtin <julialongtin@diasp.org>
2013-02-07 22:22:30 +0000

    Bring initial comment block up to documentation standard, and document the text() function.

--- library/htmlspecialchars.inc.php
+++ library/htmlspecialchars.inc.php
@@ -1,24 +1,31 @@
 <?php
-/*
-Copyright © 2011 Boyd Stephen Smith Jr.
+/**
+ * library/htmlspecialchars.inc.php Escaping Functions
+ *
+ * Copyright © 2011 Boyd Stephen Smith Jr.
+ * Copyright license terms appear at the end of this file.
+ *
+ * @package OpenEMR
+ * @author Boyd Stephen Smith Jr.
+ */
 
-Copyright license terms appear at the end of this file.
-*/
-
-/*
-This function uses htmlspecialchars() to escape a PHP string for use as
-(part of) an HTML / XML text node (in DOM terms).
-
-It only escapes a few special chars: the ampersand (&) and both the left-
-pointing angle bracket (<) and the right-pointing angle bracket (>), since
-these are the only characters that are special in a text node.  Minimal quoting
-is preferred because it produces smaller and more easily human-readable output.
-
-Some characters simply cannot appear in valid XML documents, even
-as entities but, this function does not attempt to handle them.
-
-NOTE: Attribute values are NOT text nodes, and require additional escaping.
-*/
+/**
+ * Escape a PHP string for use as (part of) an HTML / XML text node.
+ *
+ * It only escapes a few special chars: the ampersand (&) and both the left-
+ * pointing angle bracket (<) and the right-pointing angle bracket (>), since
+ * these are the only characters that are special in a text node.  Minimal
+ * quoting is preferred because it produces smaller and more easily human-
+ * readable output.
+ *
+ * Some characters simply cannot appear in valid XML documents, even
+ * as entities but, this function does not attempt to handle them.
+ *
+ * NOTE: Attribute values are NOT text nodes, and require additional escaping.
+ *
+ * @param string The string to escape, possibly including "&", "<", or ">".
+ * @return string The string, with "&", "<", and ">" escaped.
+ */
 function text($text) {
 	return htmlspecialchars($text, ENT_NOQUOTES);
 }
