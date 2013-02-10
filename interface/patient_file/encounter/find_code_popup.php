5624f7c0fa8c73ae4c46bb32a88798a3b3f0c91b
Julia Longtin <julialongtin@diasp.org>
2013-02-10 04:57:17 +0000

    Change format on initial comment block to comply with documentation standards.

--- interface/patient_file/encounter/find_code_popup.php
+++ interface/patient_file/encounter/find_code_popup.php
@@ -1,10 +1,11 @@
 <?php
-// Copyright (C) 2008 Rod Roark <rod@sunsetsystems.com>
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+/* Copyright (C) 2008 Rod Roark <rod@sunsetsystems.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
 
 $fake_register_globals=false;
 $sanitize_all_escapes=true;
