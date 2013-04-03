e4ace1cee12c26fcebd66936fbbde7c72f07159d
Julia Longtin <julialongtin@diasp.org>
2013-04-03 21:23:21 +0100

    Change initial comment block to comply further with documentation standards.

--- interface/patient_file/summary/stats_full.php
+++ interface/patient_file/summary/stats_full.php
@@ -1,10 +1,12 @@
 <?php
-// Copyright (C) 2005-2009 Rod Roark <rod@sunsetsystems.com>
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+/**
+ * Copyright (C) 2005-2009 Rod Roark <rod@sunsetsystems.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
 
 //SANITIZE ALL ESCAPES
 $sanitize_all_escapes=true;
