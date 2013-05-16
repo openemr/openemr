7387acde83bce514617b1ee75cbcac49ad9ebd62
medmasterpro <iqbal@medmastermobile.com>
2013-05-16 17:13:16 +0500

    Used frontPayment() function from library/payment.inc.php. Also, placed parameters for the timestamp and auth.

--- api/addpayment.php
+++ api/addpayment.php
@@ -25,6 +25,7 @@
 header("Content-Type:text/xml");
 $ignoreAuth = true;
 require_once 'classes.php';
+require_once("$srcdir/payment.inc.php");
 
 $xml_string = "";
 $xml_string = "<payment>";
@@ -46,29 +47,6 @@ $NameNew = $_POST['fname'] . " " . $_POST['lname'] . " " . $_POST['mname'];
 $now = time();
 $today = date('Y-m-d', $now);
 $timestamp = date('Y-m-d H:i:s', $now);
-
-// Insertion in Payment table
-function frontPayment($patient_id, $visit_id, $auth, $payment_method, $check_ref_number, $amount1, $amount2) {
-    global $timestamp;
-    $tmprow = sqlQuery("SELECT date FROM form_encounter WHERE " .
-            "encounter=? and pid=?", array($visit_id, $patient_id));
-    $tmprowArray = explode(' ', $tmprow['date']);
-    if (date('Y-m-d') == $tmprowArray[0]) {
-        if ($amount1 == 0) {
-            $amount1 = $amount2;
-            $amount2 = 0;
-        }
-    } else {
-        if ($amount2 == 0) {
-            $amount2 = $amount1;
-            $amount1 = 0;
-        }
-    }
-    $payid = sqlInsert("INSERT INTO payments ( " .
-            "pid, encounter, dtime, user, method, source, amount1, amount2 " .
-            ") VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)", array($patient_id, $visit_id, $timestamp, $auth, $payment_method, $check_ref_number, $amount1, $amount2));
-    return $payid;
-}
 
 if ($userId = validateToken($token)) {
     $user = getUsername($userId);
@@ -92,12 +70,11 @@ if ($userId = validateToken($token)) {
                     ", adjustment_code = 'pre_payment'" .
                     ", post_to_date = now() " .
                     ", payment_method = ?";
-            $payment_id = idSqlStatement($strQuery, array(0, $patient_id, $userId, 0, $check_ref_number, $prepayment, $NameNew, $payment_method));
+            $payment_id = idSqlStatement($strQuery, array(0, $patient_id, $userId, 0, $check_ref_number, $prepayment, $NameNew, $payment_type));
 
-            $result = frontPayment($patient_id, $visit_id, $user, $payment_method, $check_ref_number, $amount, 0);
-        }
-
-        if ($payment_type == 'COPAY') {//copay saving to ar_session and ar_activity tables
+            $result = frontPayment($patient_id, 0, $payment_type, $payment_method, $prepayment, 0, $timestamp, $user);
+        }             
+        if ($payment_type == 'COPAY') {
             $ResultSearchNew = sqlStatement("SELECT * FROM billing LEFT JOIN code_types ON billing.code_type=code_types.ct_key " .
                     "WHERE code_types.ct_fee=1 AND billing.activity!=0 AND billing.pid =? AND encounter=? ORDER BY billing.code,billing.modifier", array($patient_id, $visit_id));
 
@@ -114,23 +91,20 @@ if ($userId = validateToken($token)) {
             $strQuery1 = "INSERT INTO ar_session (payer_id,user_id,reference,check_date,deposit_date,pay_total," .
                     " global_amount,payment_type,description,patient_id,payment_method,adjustment_code,post_to_date) " .
                     " VALUES ('0',?,?,now(),now(),?,'','patient','COPAY',?,?,'patient_payment',now())";
-            $session_id = idSqlStatement($strQuery1, array($userId, $check_ref_number, $amount, $patient_id, $payment_method));
+            $session_id = idSqlStatement($strQuery1, array($userId, $check_ref_number, $amount, $patient_id, $payment_type));
 
 
-            $insrt_id = idSqlStatement("INSERT INTO ar_activity (pid,encounter,code_type,code,modifier,payer_type,post_time,post_user,session_id,pay_amount,account_code)" .
+            $insert_id = idSqlStatement("INSERT INTO ar_activity (pid,encounter,code_type,code,modifier,payer_type,post_time,post_user,session_id,pay_amount,account_code)" .
                     " VALUES (?,?,?,?,?,0,now(),?,?,?,'PCP')", array($patient_id, $visit_id, $Codetype, $Code, $Modifier, $userId, $session_id, $amount));
 
-            $result = frontPayment($patient_id, $visit_id, $user, $payment_method, $check_ref_number, $amount, 0);
-        }
-
+            $result = frontPayment($patient_id, $visit_id, $payment_type, $payment_method, $amount, 0, $timestamp, $user);
+        } 
         if ($payment_type == 'invoice_balance' || $payment_type == 'cash') {
 
             if ($payment_type == 'cash') {
                 sqlStatement("update form_encounter set last_level_closed=? where encounter=? and pid=? ", array(4, $visit_id, $patient_id));
                 sqlStatement("update billing set billed=? where encounter=? and pid=?", array(1, $visit_id, $patient_id));
             }
-
-
 
             $adjustment_code = 'patient_payment';
             $strQuery2 = "insert into ar_session set " .
@@ -146,17 +120,17 @@ if ($userId = validateToken($token)) {
                     ", adjustment_code = ?" .
                     ", post_to_date = now() " .
                     ", payment_method = ?";
-            $payment_id = idSqlStatement($strQuery2, array(0, $patient_id, $userId, 0, $check_ref_number, $amount, $NameNew, $adjustment_code, $payment_method));
+            $payment_id = idSqlStatement($strQuery2, array(0, $patient_id, $userId, 0, $check_ref_number, $amount, $NameNew, $adjustment_code, $payment_type));
 
-            $result = frontPayment($patient_id, $visit_id, $user, $payment_method, $check_ref_number, 0, $amount); //insertion to 'payments' table.
+            $result = frontPayment($patient_id, $visit_id, $payment_type, $payment_method, 0, $amount, $timestamp, $user); //insertion to 'payments' table.
 
             $resMoneyGot = sqlStatement("SELECT sum(pay_amount) as PatientPay FROM ar_activity where pid =? and " .
                     "encounter =? and payer_type=0 and account_code='PCP'", array($patient_id, $visit_id)); //new fees screen copay gives account_code='PCP'
             $rowMoneyGot = sqlFetchArray($resMoneyGot);
             $Copay = $rowMoneyGot['PatientPay'];
-            //Looping the existing code and modifier
+
             $ResultSearchNew = sqlStatement("SELECT * FROM billing LEFT JOIN code_types ON billing.code_type=code_types.ct_key WHERE code_types.ct_fee=1 " .
-                    "AND billing.activity!=0 AND billing.pid =? AND encounter=? ORDER BY billing.code,billing.modifier", array($form_pid, $enc));
+                    "AND billing.activity!=0 AND billing.pid =? AND encounter=? ORDER BY billing.code,billing.modifier", array($patient_id, $visit_id));
             while ($RowSearch = sqlFetchArray($ResultSearchNew)) {
                 $Codetype = $RowSearch['code_type'];
                 $Code = $RowSearch['code'];
@@ -164,13 +138,13 @@ if ($userId = validateToken($token)) {
                 $Fee = $RowSearch['fee'];
 
                 $resMoneyGot = sqlStatement("SELECT sum(pay_amount) as MoneyGot FROM ar_activity where pid =? " .
-                        "and code_type=? and code=? and modifier=? and encounter =? and !(payer_type=0 and account_code='PCP')", array($form_pid, $Codetype, $Code, $Modifier, $enc));
-                //new fees screen copay gives account_code='PCP'
+                        "and code_type=? and code=? and modifier=? and encounter =? and !(payer_type=0 and account_code='PCP')", array($patient_id, $Codetype, $Code, $Modifier, $visit_id));
+
                 $rowMoneyGot = sqlFetchArray($resMoneyGot);
                 $MoneyGot = $rowMoneyGot['MoneyGot'];
 
                 $resMoneyAdjusted = sqlStatement("SELECT sum(adj_amount) as MoneyAdjusted FROM ar_activity where " .
-                        "pid =? and code_type=? and code=? and modifier=? and encounter =?", array($form_pid, $Codetype, $Code, $Modifier, $enc));
+                        "pid =? and code_type=? and code=? and modifier=? and encounter =?", array($patient_id, $Codetype, $Code, $Modifier, $visit_id));
                 $rowMoneyAdjusted = sqlFetchArray($resMoneyAdjusted);
                 $MoneyAdjusted = $rowMoneyAdjusted['MoneyAdjusted'];
 
@@ -198,8 +172,8 @@ if ($userId = validateToken($token)) {
                             ", pay_amount = ?" .
                             ", adj_amount = ?" .
                             ", account_code = 'PP'", array($patient_id, $visit_id, $Codetype, $Code, $Modifier, 0, $userId, $payment_id, $insert_value, 0));
-                }//if
-            }//while
+                }
+            }
         }
         if ($result) {
             $xml_string .= "<status>0</status>";
