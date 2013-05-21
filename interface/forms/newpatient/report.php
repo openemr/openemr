5b98e46303ba54e3686dd005f9d3fd7dfde8ac61
mdsupport <md.support@accretics.com>
2013-05-21 09:10:27 -0700

    Fixes display of encounter reason containing multiple lines.

--- interface/forms/newpatient/report.php
+++ interface/forms/newpatient/report.php
@@ -6,9 +6,8 @@ function newpatient_report( $pid, $encounter, $cols, $id) {
 	$res = sqlStatement("select * from form_encounter where pid='$pid' and id='$id'");
 	print "<table><tr><td>\n";
 	while($result = sqlFetchArray($res)) {
-		print "<span class=bold>" . xl('Reason') . ": </span><span class=text>" . $result{"reason"} . "</span><br>\n";
 		print "<span class=bold>" . xl('Facility') . ": </span><span class=text>" . $result{"facility"} . "</span><br>\n";
-
+		print "<span class=bold>" . xl('Reason') . ": </span><span class=text>" . nl2br(htmlspecialchars($result{"reason"})) . "</span><br>\n";
 	}
 	print "</td></tr></table>\n";
 }
