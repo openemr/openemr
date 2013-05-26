e1b0edb3d8067d42376d65435bd97ff5cde373c5
bradymiller <bradymiller@users.sourceforge.net>
2013-05-26 00:38:35 -0700

    Converted encounter form to new security model

--- interface/forms/newpatient/new.php
+++ interface/forms/newpatient/new.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Encounter form new script.
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Brady Miller <brady@sparmy.com>
+ * @link    http://www.open-emr.org
+ */
+
+$fake_register_globals=false;
+$sanitize_all_escapes=true;
+
 include_once("../../globals.php");
 include_once("$srcdir/acl.inc");
 include_once("$srcdir/lists.inc");
@@ -13,7 +35,7 @@ if (($tmp['squad'] && ! acl_check('squads', $tmp['squad'])) ||
         acl_check('encounters', 'relaxed' )))
 {
   echo "<body>\n<html>\n";
-  echo "<p>(" . xl('New encounters not authorized'). ")</p>\n";
+  echo "<p>(" . xlt('New encounters not authorized'). ")</p>\n";
   echo "</body>\n</html>\n";
   exit();
 }
