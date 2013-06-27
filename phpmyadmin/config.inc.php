4ed7c7b5763538fab5090d3145194cfdd6120752
bradymiller <bradymiller@users.sourceforge.net>
2013-06-27 00:18:52 -0700

    Upgrade phpmyadmin project (commit 3 of 3; This commits integrates phpmyadmin into OpenEMR)

---
+++ phpmyadmin/config.inc.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Customized for OpenEMR.
+ *
+ */
+
+// Access control is dealt with by the ACL check
+$ignoreAuth = true;
+require_once("../interface/globals.php");
+require_once("../library/acl.inc");
+if ($GLOBALS['disable_phpmyadmin_link']) {
+  echo "You do not have access to this resource<br>";
+  exit;
+}
+if (! acl_check('admin', 'database')) {
+  echo "You do not have access to this resource<br>";
+  exit;
+}
+
+/* Servers configuration */
+$i = 0;
+
+/* Server localhost (config:openemr) [1] */
+$i++;
+
+/* For standard OpenEMR database access */
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['host'] = $sqlconf['host'];
+$cfg['Servers'][$i]['port'] = $sqlconf['port'];
+$cfg['Servers'][$i]['user'] = $sqlconf['login'];
+$cfg['Servers'][$i]['password'] = $sqlconf['pass'];
+$cfg['Servers'][$i]['only_db'] = $sqlconf['dbase'];
+
+/* Other mods for OpenEMR */
+$cfg['AllowThirdPartyFraming'] = TRUE;
+$cfg['ShowCreateDb'] = false;
+$cfg['ShowPhpInfo'] = TRUE;
+?>
