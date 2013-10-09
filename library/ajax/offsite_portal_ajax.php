c46df7876db2bed9837c1e61a81906870eddc9bb
bradymiller <bradymiller@users.sourceforge.net>
2013-10-08 19:20:10 -0700

    fixed headers of prior commit

--- library/ajax/offsite_portal_ajax.php
+++ library/ajax/offsite_portal_ajax.php
@@ -1,29 +1,29 @@
 <?php
-// +-----------------------------------------------------------------------------+ 
-// Copyright (C) 2013 Z&H Consultancy Services Private Limited <sam@zhservices.com>
-//
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-//
-// A copy of the GNU General Public License is included along with this program:
-// openemr/interface/login/GnuGPL.html
-// For more information write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-// 
-// Author:   Eldho Chacko <eldho@zhservices.com>
-//           Vinish K     <vinish@zhservices.com>
-//
-// +------------------------------------------------------------------------------+
+/**
+ * Ajax script to connect to offsite patient portal.
+ *
+ * Currently supports collecting the vpn connection package and
+ * can be expanded to support other features in the future.
+ *
+ * Copyright (C) 2013 Z&H Consultancy Services Private Limited <sam@zhservices.com>
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Eldho Chacko <eldho@zhservices.com>
+ * @author  Vinish K <vinish@zhservices.com>
+ * @link    http://www.open-emr.org
+ */
+
 //SANITIZE ALL ESCAPES
 $sanitize_all_escapes=true;
 //
