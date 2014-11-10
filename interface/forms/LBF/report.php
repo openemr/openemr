831fa9bb18288d2df98000fc396de9597474d9a7
Arnab Naha <superarnab@gmail.com>
2014-11-10 21:51:51 +0530

    PDF patient report fixes - author is Terry Hill.
    Signed-off-by: Arnab Naha <superarnab@gmail.com>

--- interface/forms/LBF/report.php
+++ interface/forms/LBF/report.php
@@ -12,18 +12,17 @@ include_once($GLOBALS["srcdir"] . "/api.inc");
 // This function is invoked from printPatientForms in report.inc
 // when viewing a "comprehensive patient report".  Also from
 // interface/patient_file/encounter/forms.php.
-//
+
 function lbf_report($pid, $encounter, $cols, $id, $formname) {
   require_once($GLOBALS["srcdir"] . "/options.inc.php");
   echo "<table>\n";
-
+  
   $arr = array();
   $fres = sqlStatement("SELECT field_id, field_value FROM lbf_data WHERE form_id = ?", array($id) );
   while ($frow = sqlFetchArray($fres)) {
-    $arr[$frow['field_id']] = $frow['field_value'];
+	 $arr[$frow['field_id']] = wordwrap($frow['field_value'], 30, "\n", true);
   }
   display_layout_rows($formname, $arr);
-
   echo "</table>\n";
-}
+ }
 ?>
