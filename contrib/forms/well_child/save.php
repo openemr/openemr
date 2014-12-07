81ec1b0bf8ce02aa31ceeb1e251f238f7df69158
aethelwulffe <art@starfrontiers.org>
2014-12-07 01:28:29 -0500

    Review/remove Contrib Forms
    Removed forms from contrib folder on the following list of criteria:
    1.  Bad install/excessive table footprint
    2.  Single Text Field form or was particularly low quality
    3.  Replicates other function (i.e. family history, insurance data,
    vitals form type data).
    4.  Was part of set that had others that did not function and was low
    quality.
    NOTE:  some forms kept due to useful code examples.

--- contrib/forms/well_child/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../../globals.php");
-include_once("$srcdir/api.inc");
-
-require ("C_FormWellChild.class.php");
-$c = new C_FormWellChild();
-echo $c->default_action_process($_POST);
-@formJump();
-?>
