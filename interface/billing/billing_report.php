e565efb0699dad093adced46f7c17ec58f0a89e4
aethelwulffe <art@starfrontiers.org>
2014-12-18 17:54:07 -0500

    Added usage note to BM
    Added usage notes to Billing Manager criterion selection.  Sorting by
    insurance and x12 partner only works against AR tables, not unbilled
    items.  Causes confusion.

--- interface/billing/billing_report.php
+++ interface/billing/billing_report.php
@@ -365,7 +365,7 @@ $ThisPageSearchCriteriaQueryDropDownMasterDefault=array();
 $ThisPageSearchCriteriaQueryDropDownMasterDefaultKey=array();
 $ThisPageSearchCriteriaIncludeMaster=array();
 
-$ThisPageSearchCriteriaDisplayMaster= array( xl("Date of Service"),xl("Date of Entry"),xl("Date of Billing"),xl("Claim Type"),xl("Patient Name"),xl("Patient Id"),xl("Insurance Company"),xl("Encounter"),xl("Whether Insured"),xl("Charge Coded"),xl("Billing Status"),xl("Authorization Status"),xl("Last Level Billed"),xl("X12 Partner"),xl("Code"),xl("Modifier") );
+$ThisPageSearchCriteriaDisplayMaster= array( xl("Date of Service"),xl("Date of Entry"),xl("Date of Billing"),xl("Claim Type"),xl("Patient Name"),xl("Patient Id"),xl("Insurance Company (Billed Only!)"),xl("Encounter"),xl("Whether Insured"),xl("Charge Coded"),xl("Billing Status"),xl("Authorization Status"),xl("Last Level Billed"),xl("X12 Partner (Billed Only!)"),xl("Code"),xl("Modifier") );
 $ThisPageSearchCriteriaKeyMaster="form_encounter.date,billing.date,claims.process_time,claims.target,patient_data.fname,".
                                  "form_encounter.pid,claims.payer_id,form_encounter.encounter,insurance_data.provider,billing.id,billing.billed,".
                                  "billing.authorized,form_encounter.last_level_billed,billing.x12_partner_id,billing.code,billing.modifier";
