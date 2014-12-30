076c8750176300ff9d04b77f3e14e6ad5ba92465
ophthal <magauran@ophthal.org>
2014-12-30 18:44:26 -0500

    Symbolic links

--- interface/forms/eye_mag/view.php
+++ interface/forms/eye_mag/view.php
@@ -163,7 +163,7 @@ formHeader("Chart: ".$pat_data['fname']." ".$pat_data['lname']." ".$visit_date);
     <!-- Add Font stuff for the look and feel.  -->
     <link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
     <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
-    <link rel="stylesheet" href="style.css" type="text/css">    
+    <link rel="stylesheet" href="../../forms/<?php echo $form_folder; ?>/style.css" type="text/css">    
     <link rel="stylesheet" href="<?php echo $GLOBALS['webroot'] ?>/library/css/font-awesome-4.2.0/css/font-awesome.min.css">
 
     <?php 
