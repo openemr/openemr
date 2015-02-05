171932ede477f533c6b28ef9a4902b1e19b5b06c
Seethal V P <seethalvp@zhservices.com>
2015-02-05 15:01:34 +0530

    Added module installed checking in patient portal soap functions. Also modified query in server_existingpatient to fix issue with encounter listing in patient portal medical records page.
    Signed-off-by: Seethal V P <seethalvp@zhservices.com>

--- myportal/soap_service/server_existingpatient.php
+++ myportal/soap_service/server_existingpatient.php
@@ -463,20 +463,19 @@ class existingpatient {
                             break;
 												
 						case 'D5':
-                    $query = "SELECT po.procedure_order_id, po.date_ordered,po.procedure_type_id AS order_type_id, pt1.name AS procedure_name,
-									  ptrc.name AS result_category_name, pt2.procedure_type AS result_type, pt2.procedure_type_id AS result_type_id, pt2.name
-										AS result_name, pt2.units AS result_def_units, pt2.range AS result_def_range, pt2.description AS result_description,
-										lo.title AS units_name, pr.procedure_report_id, pr.date_report, pr.date_collected, pr.specimen_num, pr.report_status,
-										pr.review_status, ps.procedure_result_id, ps.abnormal, ps.result, ps.range, ps.result_status, ps.facility, ps.comments
-										FROM procedure_order AS po LEFT JOIN procedure_type AS pt1 ON pt1.procedure_type_id = po.procedure_type_id LEFT JOIN
-										procedure_type AS ptrc ON ptrc.procedure_type_id = pt1.parent AND ptrc.procedure_type LIKE 'grp%' LEFT JOIN procedure_type
-										AS pt2 ON ((ptrc.procedure_type_id IS NULL AND (pt2.parent = po.procedure_type_id OR pt2.procedure_type_id = po.procedure_type_id))
-										OR (pt2.procedure_type_id IS NOT NULL AND pt2.parent = pt1.procedure_type_id)) AND (pt2.procedure_type LIKE 'res%' OR
-										pt2.procedure_type LIKE 'rec%') LEFT JOIN list_options AS lo ON list_id = 'proc_unit' AND option_id = pt2.units LEFT JOIN
-										procedure_report AS pr ON pr.procedure_order_id = po.procedure_order_id LEFT JOIN procedure_result AS ps ON
-										ps.procedure_report_id = pr.procedure_report_id AND ps.procedure_type_id = pt2.procedure_type_id WHERE po.patient_id = ? 
-										ORDER BY po.date_ordered, po.procedure_order_id, pr.procedure_report_id, ptrc.seq, ptrc.name, ptrc.procedure_type_id,
-									  pt2.seq, pt2.name, pt2.procedure_type_id";
+                    $query = " SELECT po.procedure_order_id,po.date_ordered,pt1.procedure_type_id AS order_type_id,pt1.name AS procedure_name,ptrc.name AS result_category_name,
+                               pt2.procedure_type AS result_type,pt2.procedure_type_id AS result_type_id,pt2.name AS result_name,pt2.units AS result_def_units,
+                               pt2.range AS result_def_range,pt2.description AS result_description,lo.title AS units_name,pr.procedure_report_id,pr.date_report,
+                               pr.date_collected,pr.specimen_num,pr.report_status,pr.review_status,ps.procedure_result_id,ps.abnormal,ps.result,ps.range,ps.result_status,
+                               ps.facility,ps.comments FROM procedure_order AS po LEFT JOIN procedure_order_code pc ON pc.procedure_order_id = po.`procedure_order_id`
+                               LEFT JOIN procedure_type AS pt1 ON pt1.lab_id = po.`lab_id` AND pt1.procedure_code = pc.procedure_code LEFT JOIN procedure_type AS ptrc 
+                               ON ptrc.procedure_type_id = pt1.parent AND ptrc.procedure_type LIKE 'grp%' LEFT JOIN procedure_type AS pt2 ON ((ptrc.procedure_type_id IS NULL 
+                               AND (pt2.parent = pt1.procedure_type_id OR pt2.procedure_type_id = pt1.procedure_type_id)) OR (pt2.procedure_type_id IS NOT NULL 
+                               AND pt2.parent = pt1.procedure_type_id )) AND (pt2.procedure_type LIKE 'res%' OR pt2.procedure_type LIKE 'rec%') LEFT JOIN list_options AS lo 
+                               ON list_id = 'proc_unit' AND option_id = pt2.units LEFT JOIN procedure_report AS pr ON pr.procedure_order_id = po.procedure_order_id 
+                               LEFT JOIN procedure_result AS ps ON ps.procedure_report_id = pr.procedure_report_id WHERE po.patient_id = ? 
+                               ORDER BY po.date_ordered,po.procedure_order_id,pr.procedure_report_id,ptrc.seq,ptrc.name,ptrc.procedure_type_id,pt2.seq,pt2.name,
+                               pt2.procedure_type_id ";
                             return array($query,array($pid));
                             break;
             
