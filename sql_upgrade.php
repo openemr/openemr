934b1f86653c905a06b62c32e052b8f541341534
bradymiller <bradymiller@users.sourceforge.net>
2015-03-27 19:01:09 -0700

    incremented default version in upgrade script

--- sql_upgrade.php
+++ sql_upgrade.php
@@ -109,8 +109,8 @@ if (!empty($_POST['form_submit'])) {
 <?php
 foreach ($versions as $version => $filename) {
   echo " <option value='$version'";
-  // Defaulting to most recent version, which is now 4.1.2.
-  if ($version === '4.1.2') echo " selected";
+  // Defaulting to most recent version, which is now 4.2.0.
+  if ($version === '4.2.0') echo " selected";
   echo ">$version</option>\n";
 }
 ?>
