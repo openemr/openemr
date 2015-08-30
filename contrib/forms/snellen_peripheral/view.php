6c43e60d560d6d9f1f6458f20dda18a546ec8b96
JerryP <sjpadgett@gmail.com>
2015-08-30 11:56:24 -0400

    Updated snellen eye exam with peripheral vision asnd security fixes (Does not work)

---
+++ contrib/forms/snellen_peripheral/view.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright Medical Information Integration,LLC info@mi-squared.com
+ *
+ *
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Aron Racho <aron@mi-squared.com>
+ * @author  Jerry Padgett <sjpadgett@gmail.com>
+ * @link    http://www.open-emr.org
+ */
+
+$sanitize_all_escapes=true;
+$fake_register_globals=false;
+
+require_once(__DIR__.'/../../globals.php');
+require_once($GLOBALS['srcdir'].'/api.inc');
+
+require("C_FormSnellen.class.php");
+
+$c = new C_FormSnellen();
+echo $c->view_action($_GET['id']);
