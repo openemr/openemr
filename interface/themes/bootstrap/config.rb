a419c93cbe83d77b8cc04a0e6d3bd430d9d33452
John W. Carbone <john@qlcorp.net>
2015-10-31 12:24:48 -0400

    Fix css path issue
    Chang screen.scss to style_bootstrap.css and modified css_dir.

--- interface/themes/bootstrap/config.rb
+++ interface/themes/bootstrap/config.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "../"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
