ff52324a71eea14658c83884830f4ddb16e8aadc
Oleg Sverdlov <olegsv@matrix.co.il>
2016-03-16 11:30:51 +0200

    reverted sqlconf.php

---
+++ sqlconf.php
@@ -0,0 +1,31 @@
+<?php
+//  OpenEMR
+//  MySQL Config
+//  Referenced from /library/sqlconf.php.
+
+global $disable_utf8_flag;
+$disable_utf8_flag = false;
+
+$host	= 'localhost';
+$port	= '3306';
+$login	= 'openemr';
+$pass	= 'openemr';
+$dbase	= 'openemr';
+
+$sqlconf = array();
+global $sqlconf;
+$sqlconf["host"]= $host;
+$sqlconf["port"] = $port;
+$sqlconf["login"] = $login;
+$sqlconf["pass"] = $pass;
+$sqlconf["dbase"] = $dbase;
+
+//////////////////////////
+//////////////////////////
+//////////////////////////
+//////DO NOT TOUCH THIS///
+$config = 0; /////////////
+//////////////////////////
+//////////////////////////
+//////////////////////////
+?>
