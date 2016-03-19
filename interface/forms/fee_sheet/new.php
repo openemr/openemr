b1fb69bd4f925be6d03a29dd0893d209fac12be1
bradymiller <bradymiller@users.sourceforge.net>
2016-03-18 23:25:53 -0700

    minor fix to prior commit

--- interface/forms/fee_sheet/new.php
+++ interface/forms/fee_sheet/new.php
@@ -586,7 +586,7 @@ if (!$alertmsg && ($_POST['bn_save'] || $_POST['bn_save_close'])) {
          $autojustify =  $justify;
       }
     }
-    if ($GLOBALS['replicate_justification']=='1' && (check_is_code_type_justify($code_type) && $justify == '')) {
+    if ( ($GLOBALS['replicate_justification']=='1') && ($justify == '') && check_is_code_type_justify($code_type) ) {
         $justify =  $autojustify; 
     }
 
