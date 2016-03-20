b35c652e838735d94986d303f6b6ccb97dcd7fe3
roberto <robertogagliotta@gmail.com>
2016-03-20 09:44:18 -0700

    Converted delete_form.php to standard security model

--- interface/patient_file/encounter/delete_form.php
+++ interface/patient_file/encounter/delete_form.php
@@ -77,10 +77,10 @@ foreach ($_GET as $key => $value) {
 ?>
 <input type="hidden" id="confirm" name="confirm" value="1"/>
 <p>
-<?php echo xlt('You are about to delete the following form from this encounter') . ': ' . attr($_GET['formname']); ?>
+<?php echo xlt('You are about to delete the following form from this encounter') . ': ' . text($_GET['formname']); ?>
 </p>
 <input type="button" id="confirmbtn" name="confirmbtn" value='<?php echo xla('Yes, Delete this form'); ?>'>
-<input type="button" id="cancel" name="cancel" value="Cancel">
+<input type="button" id="cancel" name="cancel" value='<?php echo xla('Cancel'); ?>'>
 </form>
 
 </body>
