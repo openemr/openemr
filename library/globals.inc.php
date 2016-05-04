656690028e69df34f5922c0bd1f2996c97da518a
teryhill <teryhill@yahoo.com>
2016-05-03 22:40:29 -0400

    Added option to Cash Receipts Report to show Patient Name instead of invoice number. Also converted the Cash Receipts Report to standard security model.

--- library/globals.inc.php
+++ library/globals.inc.php
@@ -816,6 +816,17 @@ $GLOBALS_METADATA = array(
       '2',                              // default = 2
       xl('This will Display the Invoice Number in the Sales Report or the Patient Name and ID or Patient Name and Invoice Number.')
     ), 
+	
+    'cash_receipts_report_invoice' => array(
+      xl('Display Invoice Number or Patient Name in the Cash Receipt Report'),
+      array(
+        '0' => xl('Invoice Number'),
+        '1' => xl('Patient Name'),  
+      ),   
+      '0',                              // default = 0
+      xl('Display Invoice Number or Patient Name in the Cash Receipt Report')
+    ),
+	
   ),
     
   // Billing Tab
