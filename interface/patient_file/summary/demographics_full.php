7aae290c086bddc90c4e03b7162f64afc76c3050
sharonco <sharonco@matric.co.il>
2016-06-05 15:06:15 +0300

    Extracted javascript to an external field to be used in other forms

--- interface/patient_file/summary/demographics_full.php
+++ interface/patient_file/summary/demographics_full.php
@@ -754,38 +754,8 @@ $group_seq=0; // this gives the DIV blocks unique IDs
         checkSkipConditions();
     });
 </script>
-<script language='JavaScript'>
-    function submitme(validation_type,e) {
-        //Use the old validation script if no parameter sent (backward compatibility)
-        if (typeof validation_type === 'undefined') {
-            var f = document.forms[0];
-            if (validate_old(f)) {
-                top.restoreSession();
-                f.submit();
-            }
-        } else {
-            <?php $constraints = LBF_Validation::generate_validate_constraints('DEM');?>
-            var constraints = <?=$constraints?>;
-            var error_msg ='<?=xl('is not valid')?>';
-            var form = document.querySelector("form#demographics_form");
-            var errors = validate(form, constraints);
-            if (typeof  errors !== 'undefined') {
-                e.preventDefault();
-                 showErrors(form, errors);
-            }
 
-            function showErrors(form, errors) {
-                for (var key in errors) {
-                    if (errors.hasOwnProperty(key)) {
-                        var title= form.elements.namedItem(key).title;
-                        document.getElementById("error_" + key).innerHTML =title +' '+error_msg;
-                    }
-                }
-            }
-
-        }
-    }
-</script>    
-
+<?/*Include the validation script and rules for this form*/?>
+<?include_once("$srcdir/validation/validation_script.js.php");?>
 
 </html>
