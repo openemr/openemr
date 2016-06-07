5d80441e9f52f90489f77e64859a9d1a900c0682
Amiel Elboim <amielel@matrix.co.il>
2016-06-07 12:01:51 +0300

    multiple select support

--- library/options.inc.php
+++ library/options.inc.php
@@ -207,6 +207,13 @@ function generate_select_list($tag_name, $list_id, $currvalue, $title, $empty_na
 	else {
 		$s .= "</select>";
 	}
+    // add default selected to 'Unassigned' for multiple select
+    if(!$got_selected && !$got_selected_backup && $multiple) {
+
+        $s = str_replace("value=''", "value='' selected ", $s);
+    }
+
+
 	return $s;
 	
 }
--- library/validation/validation_script.js.php
+++ library/validation/validation_script.js.php
@@ -11,6 +11,7 @@ if($GLOBALS['new_validate']) {
 <script language='JavaScript'>
 
     function submitme(new_validate,e,form_id) {
+
     //Use the old validation script if no parameter sent (backward compatibility)
         if (new_validate !== 1) {
             var f = document.forms[0];
@@ -31,7 +32,23 @@ if($GLOBALS['new_validate']) {
             var constraints = <?=$constraints?>;
             var error_msg ='<?=xl('is not valid')?>';
             var form = document.querySelector("form#"+form_id);
-            var errors = validate(form, constraints);
+            var elements = validate.collectFormValues(form);
+
+            //custom validate for multiple select(failed validate.js)
+            for(key in elements){
+                if(key.slice(-2) == '[]'){
+                    new_key = key.substring(0, key.length - 2);
+                    elements[new_key] = elements[key];
+
+                    if(validate.isObject(constraints[new_key]) && elements[key] == null){
+                        var select = $('[name="'+ key + '"]');
+                        appendError(select, new_key);
+                    }
+                    delete elements[key];
+                }
+            }
+
+            var errors = validate(elements, constraints);
             if (typeof  errors !== 'undefined') {
                 //prevent default if trigger is submit button
                 if(typeof (e) !== 'undefined') {
@@ -46,47 +63,52 @@ if($GLOBALS['new_validate']) {
                 for (var key in errors) {
                     if (errors.hasOwnProperty(key)) {
 
-                        var input = $('#'+key);
+                        appendError($('#'+key), key)
+                    }
+                }
+            }
+            /**
+            * append 'span' with error message
+            */
+            function appendError(input, id){
 
-                        //append 'span' tag for error massages if not exist
-                        if($("#error_" + key).length == 0) {
-                            //If have another element after the input
-                            if($(input).next().length > 0) {
+                //append 'span' tag for error massages if not exist
+                if($("#error_" + id).length == 0) {
+                    //If have another element after the input
+                    if($(input).next().length > 0) {
 
-                                $(input).next().after("<span id='error_" + key +"' class='error-message' '></span>");
+                        $(input).next().after("<span id='error_" + id +"' class='error-message' '></span>");
 
-                            } else {
-                                $(input).after("<span id='error_" + key +"' class='error-message'></span>");
-
-                            }
-                        }
-                        //show error message
-                        var title= form.elements.namedItem(key).title;
-                        $("#error_" + key).text(title +' '+error_msg);
-
-                        $(input).addClass('error-border');
-                        //bind hide function on focus/select again
-                        $(input).on('click focus select', function(){
-                             hideErrors(this);
-                        });
-                        //for datepicker button
-                        if($(input).next().is('img')){
-                            $(input).next().click(function(){
-
-                                hideErrors($(this).prev());
-                            });
-                        }
+                    } else {
+                        $(input).after("<span id='error_" + id +"' class='error-message'></span>");
 
                     }
                 }
+                //show error message
+                var title= $(input).attr('title');
+                if(title == undefined) { title = "" }
+                $("#error_" + id).text(title +' '+error_msg);
+
+                $(input).addClass('error-border');
+                //bind hide function on focus/select again
+                $(input).on('click focus select', function(){
+                    hideErrors(this, id);
+                });
+                //for datepicker button
+                if($(input).next().is('img')){
+                    $(input).next().click(function(){
+
+                        hideErrors($(this).prev(), id);
+                    });
+                }
+
             }
             /*
             * hide error message
             * @param element
             * */
-            function hideErrors(input){
+            function hideErrors(input, id){
                 $(input).removeClass('error-border');
-                var id = $(input).attr('id');
                 $("#error_" + id).text('');
             }
             return valid;
