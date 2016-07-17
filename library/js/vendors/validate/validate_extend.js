103d3fbee92ac74e2bec9ca2a5d36874f8b9e252
bradymiller <bradymiller@users.sourceforge.net>
2016-07-17 00:09:46 -0700

    added missing license

--- library/js/vendors/validate/validate_extend.js
+++ library/js/vendors/validate/validate_extend.js
@@ -1,7 +1,21 @@
 /**
- * Created by amiel on 02/06/16.
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see
+ * http://www.gnu.org/licenses/licenses.html#GPL .
+ *
+ * @package OpenEMR
+ * @author  Sharon Cohen <sharonco@matrix.co.il>
+ * @author  Amiel Elboim <amielel@matrix.co.il>
+ * @link    http://www.open-emr.org
  */
-
 
 // Extend for date roll from  https://validatejs.org/#validators-datetime
 
