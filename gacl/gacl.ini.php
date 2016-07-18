d348259e5dda79f6a2b6058942c398bcdc8fe707
Wakie87 <scott@npclinics.com.au>
2016-07-18 11:00:03 +0800

    Migrated Smarty from library into vendor via composer (and using autoloader).

--- gacl/gacl.ini.php
+++ gacl/gacl.ini.php
@@ -10,7 +10,7 @@ max_select_box_items 	= 100
 max_search_return_items = 200
 
 ;NO Trailing slashes
-smarty_dir 		= "../../library/Smarty"
+smarty_dir 		= "../vendor/smarty/smarty/lib"
 smarty_template_dir 	= "templates"
 smarty_compile_dir 	= "templates_c"
 
