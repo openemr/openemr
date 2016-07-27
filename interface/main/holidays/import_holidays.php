2727a52dc07521ba1d8ebb700ec3725293b74cb2
Sharon Cohen <sharonco@matrix.co.il>
2016-07-27 09:52:10 +0300

    Removed br

--- interface/main/holidays/import_holidays.php
+++ interface/main/holidays/import_holidays.php
@@ -167,7 +167,7 @@ if ($saved){
 
             <td>
                     <?php echo xla('CSV to calendar_external table'); ?></br>
-                <?php echo xla('If  the csv has been uploaded please click on "parse and insert" button</br>
+                <?php echo xla('If  the csv has been uploaded please click on "parse and insert" button
                     **NOTE: clicking on the button will remove all the existing rows in the calendar external table')?>
                 </td>
         </tr>
@@ -179,7 +179,7 @@ if ($saved){
                 </td>
                 <td >
                     <?php echo xla('calendar_External to events'); ?></br>
-                    <?php echo xla('If you have already filled the calendar external table please click on create events to have the holidays n the calendar view </br>
+                    <?php echo xla('If you have already filled the calendar external table please click on create events to have the holidays n the calendar view 
                     **NOTE: clicking on the button will remove all the existing rows in the events table related to holidays')?>
                 </td>
             </tr>
