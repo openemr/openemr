ade03d0ffdeba65b3c823d201ee4fc3a384df48a
Dror Golan <drorgo@matrix.co.il>
2016-08-01 15:09:42 +0300

    fix in theupgrade sql file

--- sql/4_2_2-to-4_3_1_upgrade.sql
+++ sql/4_2_2-to-4_3_1_upgrade.sql
@@ -609,7 +609,7 @@ INSERT INTO `list_options` (`list_id`,`option_id`,`title`,`notes`,`seq`) VALUES 
 
 #EndIf
 
-#IfMissingColumn list_options list_id option_id PageValidation
+#IfNotRow2D list_options list_id lists option_id PageValidation
 INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`) VALUES
 ('lists', 'PageValidation', 'PageValidation', 298, 1, 0, '', '', '', 0, 0, 1, ''),
 ('PageValidation', 'add_edit_issue', 'add_edit_issue.php', 0, 0, 0, '', '{form_title:{presence: true}}', '', 0, 0, 1, '');
