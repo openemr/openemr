99ad4b6af358bbd4d186e121e4131f7d56143de4
Dror Golan <drorgo@matrix.co.il>
2016-08-03 18:03:17 +0300

    note constants should not have trailing/leading spaces or using symbols fix

--- interface/super/edit_list.php
+++ interface/super/edit_list.php
@@ -953,10 +953,9 @@ while ($row = sqlFetchArray($res)) {
                   }else if ( $list_id == 'next_of_kin_relationship' || $list_id == 'immunization_administered_site') {
                         xl('HL7 Code','e');
                   }else if ( $list_id == 'immunization_observation' ) {
-                        xl('LOINC Code','e'); 
-                  
+                        xl('LOINC Code','e');
                   }else if ( $list_id == 'page_validation' ) {
-                              xl('Page Validation - ','e');
+                              xl('Page Validation','e');
           } else {
 		  	xl('Notes','e');
 		  }
