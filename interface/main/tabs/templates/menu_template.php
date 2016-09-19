7c1c1168a2aa09cff6f49ed392cddbe66fe2f79c
Robert Down <robertdown@live.com>
2016-09-19 04:35:08 -0400

    Improved tabs layout styling (#254)

--- interface/main/tabs/templates/menu_template.php
+++ interface/main/tabs/templates/menu_template.php
@@ -21,13 +21,13 @@
 
 <script type="text/html" id="menu-action">
     <i data-bind="css: icon,text:helperText" class="fa closeButton"></i>
-    <div class='menuLabel' data-bind="text:label,click: menuActionClick,css: {menuDisabled: ! enabled()}"></div>
-    
+    <a class='menuLabel' href="#" data-bind="text:label,click: menuActionClick,css: {menuDisabled: ! enabled()}"></a>
+
 </script>
 <script type="text/html" id="menu-header">
     <i data-bind="css: icon" class="fa closeButton"></i>
     <div class="menuSection">
-        <div class='menuLabel' data-bind="text:label"></div>
+        <div class='menuLa-bel' data-bind="text:label"></div>
         <ul class="menuEntries" name="menuEntries" data-bind="foreach: children">
            <li data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }"></li>
         </ul>
