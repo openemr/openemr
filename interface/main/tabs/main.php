b8e5dbfa3814da8299e360354a0eeafdf30b1df6
Brady Miller <brady.g.miller@gmail.com>
2016-09-19 01:26:44 -0700

    Support multiple tab themes (#255)

--- interface/main/tabs/main.php
+++ interface/main/tabs/main.php
@@ -69,8 +69,7 @@ function isEncounterLocked( encounterId ) {
 var webroot_url="<?php echo $web_root; ?>";
 </script>
 <link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
-<link rel="stylesheet" type="text/css" href="css/tabs.css?v=<?php echo $v_js_includes; ?>"/>
-<link rel="stylesheet" type="text/css" href="css/menu.css?v=<?php echo $v_js_includes; ?>"/>
+<link rel="stylesheet" type="text/css" href="<?php echo $webroot; ?>/interface/themes/<?php echo $GLOBALS['theme_tabs_layout']; ?>?v=<?php echo $v_js_includes; ?>"/>
 <link rel="shortcut icon" href="<?php echo $webroot; ?>/interface/pic/favicon.ico" />
 <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/knockout-3-4-0/dist/knockout.js"></script>
 <script type="text/JavaScript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/jquery-min-2-2-0/index.js"></script>
