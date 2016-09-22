84706807f46182bef254367462d012c759b37c97
roberto <robertogagliotta@gmail.com>
2016-09-22 14:13:44 -0700

    Bug fix to support more recent SNOMED packages

--- interface/code_systems/list_staged.php
+++ interface/code_systems/list_staged.php
@@ -149,6 +149,17 @@ if (is_dir($mainPATH)) {
                     array_push($revisions,$temp_date);
 	    	    $supported_file = 1;
                 }
+                else if (preg_match("/SnomedCT_RF1Release_INT_([0-9]{8}).zip/",$file,$matches)) {
+    
+		    // Hard code the version SNOMED feed to be International:English
+                    //  (if add different SNOMED types/versions/languages, then can use this)
+                    //
+                    $version = "International:English";
+                    $date_release = substr($matches[1],0,4)."-".substr($matches[1],4,-2)."-".substr($matches[1],6);
+                    $temp_date = array('date'=>$date_release, 'version'=>$version, 'path'=>$mainPATH."/".$matches[0]);
+                    array_push($revisions,$temp_date);
+	    	    $supported_file = 1;
+                }
                 else if (preg_match("/SnomedCT_Release_US[0-9]*_([0-9]{8}).zip/",$file,$matches)) {
 
                     // This is the SNOMED US extension pack which can only be installed on top
