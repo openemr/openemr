27319eb15d4acabe565ae89e9da4f1a79dc6801f
Brady Miller <brady.g.miller@gmail.com>
2016-10-22 23:28:01 -0700

    standardize formatting of about script code

--- interface/main/about_page.php
+++ interface/main/about_page.php
@@ -6,6 +6,7 @@
  * If it have been entered in Globals along with the Manual and On Line Support Links
  *
  * Copyright (C) 2016 Terry Hill <terry@lillysystems.com>
+ * Copyright (C) 2016 Brady Miller <brady.g.miller@gmail.com>
  *
  * LICENSE: This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,6 +21,7 @@
  *
  * @package OpenEMR
  * @author Terry Hill <terry@lilysystems.com>
+ * @author Brady Miller <brady.g.miller@gmail.com>
  * @link http://www.open-emr.org
  *
  * Please help the overall project by sending changes you make to the author and to the OpenEMR community.
@@ -31,46 +33,45 @@ $sanitize_all_escapes=true;
 
 require_once("../globals.php");
 ?>
- <html>
-  <head>
+<html>
+<head>
+    <link rel="stylesheet" href="<?php echo $GLOBALS['assets_static_relative']; ?>/bootstrap-3-3-4/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo $GLOBALS['assets_static_relative'] ?>/jquery-ui-1-11-4/themes/ui-darkness/jquery-ui.min.css" />
+    <link rel="stylesheet" href="<?php echo $GLOBALS['assets_static_relative']; ?>/font-awesome-4-6-3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo $css_header; ?>" type="text/css">
 
-   <link rel="stylesheet" href="<?php echo $GLOBALS['assets_static_relative']; ?>/bootstrap-3-3-4/dist/css/bootstrap.min.css">
-   <link rel="stylesheet" href="<?php echo $GLOBALS['assets_static_relative'] ?>/jquery-ui-1-11-4/themes/ui-darkness/jquery-ui.min.css" />
-   <link rel="stylesheet" href="<?php echo $GLOBALS['assets_static_relative']; ?>/font-awesome-4-6-3/css/font-awesome.min.css">
-   <link rel="stylesheet" href="<?php echo $css_header; ?>" type="text/css">
+    <style>
+        .donations-needed {
+            margin-top: 25px;
+            margin-bottom: 25px;
+            color: #c9302c;
+        }
+        .donations-needed a, .donations-needed a:visited,
+        .donations-needed a:active {
+            color: #c9302c;
+        }
+        .donations-needed a.btn {
+            color: #c9302c;
+            text-align: center;
+            font-size: 1.5em;
+            font-weight: bold;
+            animation: all 2s;
+        }
+        .donations-needed a.btn:hover {
+            background-color: #c9302c;
+            color: #fff;
+        }
+        .donations-needed .btn {
+            border-radius: 8px;
+            border: 2px solid #c9302c;
+            color: #c9302c;
+            background-color: transparent;
+        }
+    </style>
 
-   <style>
-    .donations-needed {
-     margin-top: 25px;
-     margin-bottom: 25px;
-     color: #c9302c;
-    }
-    .donations-needed a, .donations-needed a:visited,
-    .donations-needed a:active {
-     color: #c9302c;
-    }
-    .donations-needed a.btn {
-     color: #c9302c;
-     text-align: center;
-     font-size: 1.5em;
-     font-weight: bold;
-     animation: all 2s;
-    }
-    .donations-needed a.btn:hover {
-     background-color: #c9302c;
-     color: #fff;
-    }
-    .donations-needed .btn {
-     border-radius: 8px;
-     border: 2px solid #c9302c;
-     color: #c9302c;
-     background-color: transparent;
-    }
-   </style>
-
-   <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative'] ?>/jquery-min-2-2-0/index.js"></script>
-   <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/bootstrap-3-3-4/dist/js/bootstrap.min.js"></script>
-   <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']  ?>/jquery-ui-1-11-4/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative'] ?>/jquery-min-2-2-0/index.js"></script>
+    <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/bootstrap-3-3-4/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']  ?>/jquery-ui-1-11-4/jquery-ui.min.js"></script>
 
     <script type="text/javascript">
         var registrationTranslations = <?php echo json_encode(array(
@@ -87,7 +88,7 @@ require_once("../globals.php");
         ?>;
 
         var registrationConstants = <?php echo json_encode(array(
-        'webroot' => $GLOBALS['webroot']
+            'webroot' => $GLOBALS['webroot']
         ))
         ?>;
     </script>
@@ -109,33 +110,31 @@ require_once("../globals.php");
             });
         });
     </script>
+</head>
 
-  </head>
-
-  <body class="body_top">
-   <div style="text-align: center;">
-    <span class="title"><?php  echo xlt('About OpenEMR'); ?> </span><br><br>
-    <span class="text"><?php  echo xlt('Version Number'); ?>: <?php echo "v".text($openemr_version) ?></span><br><br>
-    <span class="text product-registration"><span class="email"></span> <span class="id"></span></span><br><br>
-    <?php if (!empty($GLOBALS['support_phone_number'])) { ?>
-     <span class="text"><?php  echo xlt('Support Phone Number'); ?>: <?php echo $GLOBALS['support_phone_number'] ?></span><br><br>
+<body class="body_top">
+    <div style="text-align: center;">
+        <span class="title"><?php  echo xlt('About OpenEMR'); ?> </span><br><br>
+        <span class="text"><?php  echo xlt('Version Number'); ?>: <?php echo "v".text($openemr_version) ?></span><br><br>
+        <span class="text product-registration"><span class="email"></span> <span class="id"></span></span><br><br>
+        <?php if (!empty($GLOBALS['support_phone_number'])) { ?>
+            <span class="text"><?php  echo xlt('Support Phone Number'); ?>: <?php echo $GLOBALS['support_phone_number'] ?></span><br><br>
+        <?php } ?>
+    </div>
+    <a href="<?php echo "http://open-emr.org/wiki/index.php/OpenEMR_".attr($v_major).".".attr($v_minor).".".attr($v_patch)."_Users_Guide"; ?>" target="_blank" class="css_button"><span><?php echo xlt('User Manual'); ?></span></a><br><br>
+    <?php if (!empty($GLOBALS['online_support_link'])) { ?>
+        <a href='<?php echo $GLOBALS["online_support_link"]; ?>' target="_blank" class="css_button"><span><?php echo xlt('Online Support'); ?></span></a><br><br>
     <?php } ?>
-   </div>
-   <a href="<?php echo "http://open-emr.org/wiki/index.php/OpenEMR_".attr($v_major).".".attr($v_minor).".".attr($v_patch)."_Users_Guide"; ?>" target="_blank" class="css_button"><span><?php echo xlt('User Manual'); ?></span></a><br><br>
-   <?php if (!empty($GLOBALS['online_support_link'])) { ?>
-    <a href='<?php echo $GLOBALS["online_support_link"]; ?>' target="_blank" class="css_button"><span><?php echo xlt('Online Support'); ?></span></a><br><br>
-   <?php } ?>
-   <a href="../../acknowledge_license_cert.html" target="_blank" class="css_button"><span><?php echo xlt('Acknowledgments, Licensing and Certification'); ?></span></a><br>
-   <div class="donations-needed">
-    <span class="text"><?php echo xlt("Please consider sending in a donation to OpenEMR"); ?>:</span><br>
-    <a href="http://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=V6EVVTYYK264C" target="_blank" class="btn btn-lg btn-block"><i class="fa fa-2x fa-heart"></i><br/><?php echo xlt("DONATE NOW!"); ?></a>
-   </div>
+    <a href="../../acknowledge_license_cert.html" target="_blank" class="css_button"><span><?php echo xlt('Acknowledgments, Licensing and Certification'); ?></span></a><br>
+    <div class="donations-needed">
+        <span class="text"><?php echo xlt("Please consider sending in a donation to OpenEMR"); ?>:</span><br>
+        <a href="http://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=V6EVVTYYK264C" target="_blank" class="btn btn-lg btn-block"><i class="fa fa-2x fa-heart"></i><br/><?php echo xlt("DONATE NOW!"); ?></a>
+    </div>
 
-   <div class="product-registration-modal" style="display: none">
-    <p class="context"><?php echo xlt("Register your installation with OEMR 501(c)(3) to receive important notifications, such as security fixes and new release announcements."); ?></p>
-    <input placeholder="<?php echo xlt('email'); ?>" type="email" class="email" style="width: 100%; color: black" />
-    <p class="message" style="font-style: italic"></p>
-   </div>
-
-  </body>
+    <div class="product-registration-modal" style="display: none">
+        <p class="context"><?php echo xlt("Register your installation with OEMR 501(c)(3) to receive important notifications, such as security fixes and new release announcements."); ?></p>
+        <input placeholder="<?php echo xlt('email'); ?>" type="email" class="email" style="width: 100%; color: black" />
+        <p class="message" style="font-style: italic"></p>
+    </div>
+</body>
 </html>
