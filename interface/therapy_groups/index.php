c97c7fb664183cb86b2a215c3b5c810bb8829370
Amiel Elboim <amielel@matrix.co.il>
2016-11-07 14:58:52 +0200

    adding globals and sql.inc to group therapy system

--- interface/therapy_groups/index.php
+++ interface/therapy_groups/index.php
@@ -6,7 +6,9 @@
  * Time: 12:01
  */
 
-require dirname(__FILE__) . '/therapy_groups_controllers/therapy_groups_controller.php';
+require_once dirname(__FILE__) . '/../globals.php';
+require_once(dirname(__FILE__) . '/../../library/sql.inc');
+require_once dirname(__FILE__) . '/therapy_groups_controllers/therapy_groups_controller.php';
 
 $method = $_GET['method'];
 
