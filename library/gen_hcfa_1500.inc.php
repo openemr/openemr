57438ae67eb3f11cae725bf0f70c75a174cda3f4
Brady Miller <brady.g.miller@gmail.com>
2016-11-18 21:27:17 -0500

    Misc_Billing_Form Changes.
    These changes allow the EPSDT options, The box 17 options, The
    electronic resubmission of claims.
    
    Original author is Terry Hill.

--- library/gen_hcfa_1500.inc.php
+++ library/gen_hcfa_1500.inc.php
@@ -312,6 +312,12 @@ function gen_hcfa_1500_page($pid, $encounter, &$log, &$claim) {
       put_hcfa(26, 1, 28, $claim->planName(1));
     }
   }
+  
+  # Box 10d. Claim Codes  medicaid_referral_code
+  
+  if($claim->epsdtFlag()) {
+      put_hcfa(26, 34, 2, $claim->medicaidReferralCode());
+    }
 
   // Box 11d. Is There Another Health Benefit Plan
   if (!$new_medicare_logic) {
@@ -367,7 +373,7 @@ function gen_hcfa_1500_page($pid, $encounter, &$log, &$claim) {
   // Medicare forbids an entry here and other payers require one.
   // There is still confusion over this.
   //
-  if ($claim->referrerLastName() &&
+  if ($claim->referrerLastName() || $claim->billingProviderLastName() &&
     (empty($GLOBALS['MedicareReferrerIsRenderer']) || $claim->claimType() != 'MB'))
   {
     // Box 17a. Referring Provider Alternate Identifier
@@ -380,16 +386,31 @@ function gen_hcfa_1500_page($pid, $encounter, &$log, &$claim) {
     }
     *****************************************************************/
 
-    // Box 17. Name of Referring Provider or Other Source
+
+
+    // Box 17. Name of Referring Provider or Other Source leave it like it is just check if there is info in misc_billing the use it provider_qualifier_code
+    # Changed to look first at the misc hcfa billing form to complete this box if nothing on misc hcfa form use referrer
+    if (strlen($claim->billingProviderLastName()) !=0) {
+      $tmp2 = $claim->billingProviderLastName() . ', ' . $claim->billingProviderFirstName();
+      if ($claim->billingProviderMiddleName())
+        $tmp2 .= ', ' . substr($claim->billingProviderMiddleName(),0,1);
+      put_hcfa(34, 1, 3, $claim->billing_options['provider_qualifier_code']);
+      put_hcfa(34, 4, 25, $tmp2);
+      if ($claim->billingProviderNPI()) {
+        put_hcfa(34, 33, 15, $claim->billingProviderNPI());
+      }
+    }
+    else
+    {
     $tmp = $claim->referrerLastName() . ', ' . $claim->referrerFirstName();
     if ($claim->referrerMiddleName())
       $tmp .= ', ' . substr($claim->referrerMiddleName(),0,1);
-    put_hcfa(34, 1, 25, $tmp);
-
-    // Box 17b. Referring Provider NPI
+    put_hcfa(34, 1, 3, 'DN');
+    put_hcfa(34, 4, 25, $tmp);
     if ($claim->referrerNPI()) {
       put_hcfa(34, 33, 15, $claim->referrerNPI());
     }
+  }
   }
 
   // Box 18. Hospitalization Dates Related to Current Services
@@ -436,7 +457,7 @@ function gen_hcfa_1500_page($pid, $encounter, &$log, &$claim) {
 
         // Box 22. Medicaid Resubmission Code and Original Ref. No.
         put_hcfa(38, 50, 10, $claim->medicaidResubmissionCode());
-        put_hcfa(38, 62, 10, $claim->medicaidOriginalReference());
+        put_hcfa(38, 62, 15, $claim->medicaidOriginalReference());
 
         // Box 21 continued. Diagnoses
         if (!empty($diags[1])) {
@@ -567,7 +588,10 @@ function gen_hcfa_1500_page($pid, $encounter, &$log, &$claim) {
     put_hcfa($lino, 59, 3, $claim->cptUnits($hcfa_proc_index));
 
     // 24h. EPSDT Family Plan
-    // Not currently supported.
+    // 
+    if($claim->epsdtFlag()) {
+      put_hcfa($lino, 63, 2, '03');
+    }
 
     // 24j. Rendering Provider NPI
     put_hcfa($lino, 68, 10, $claim->providerNPI($hcfa_proc_index));
--- sql/4_2_2-to-5_0_0_upgrade.sql
+++ sql/4_2_2-to-5_0_0_upgrade.sql
@@ -2157,3 +2157,29 @@ INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`,`is_default`)
 #IfMissingColumn insurance_companies inactive
 ALTER TABLE `insurance_companies` ADD `inactive` INT(1) NOT NULL DEFAULT '0' ;
 #EndIf
+
+#IfMissingColumn form_misc_billing_options medicaid_referral_code
+  ALTER TABLE form_misc_billing_options ADD COLUMN medicaid_referral_code varchar(2) default NULL;
+#EndIf
+
+#IfMissingColumn form_misc_billing_options epsdt_flag
+  ALTER TABLE form_misc_billing_options ADD COLUMN epsdt_flag tinyint(1) default NULL;
+#EndIf
+
+#IfMissingColumn form_misc_billing_options provider_qualifier_code
+  ALTER TABLE form_misc_billing_options ADD COLUMN provider_qualifier_code varchar(2) default NULL;
+#EndIf
+
+#IfMissingColumn form_misc_billing_options provider_id
+  ALTER TABLE form_misc_billing_options ADD COLUMN provider_id int(11) default NULL;
+#EndIf
+
+#IfMissingColumn form_misc_billing_options icn_resubmission_number
+  ALTER TABLE form_misc_billing_options ADD COLUMN icn_resubmission_number int(35) default NULL;
+#EndIf
+
+#IfNotRow2D list_options list_id lists option_id provider_qualifier_code
+        INSERT INTO list_options ( list_id, option_id, title, seq, is_default ) VALUES ('lists','provider_qualifier_code','Provider Qualifier Code', 1,0);
+        INSERT INTO list_options ( list_id, option_id, title, seq, is_default ) VALUES ('provider_qualifier_code','dk','DK',10,0);
+        INSERT INTO list_options ( list_id, option_id, title, seq, is_default ) VALUES ('provider_qualifier_code','dn','DN',20,0);
+#EndIf
