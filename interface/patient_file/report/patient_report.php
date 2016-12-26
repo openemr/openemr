c262dc0e9807c5532a367ded23de101c2cf0fdb7
Dror Golan <drorgo@matrix.co.il>
2016-12-26 09:44:26 +0200

    add new feature to the report : view reccuring days.

--- interface/patient_file/report/patient_report.php
+++ interface/patient_file/report/patient_report.php
@@ -218,6 +218,9 @@ function show_date_fun(){
    <input type='checkbox' name='include_transactions' id='include_transactions' value="transactions"><?php xl('Transactions','e'); ?><br>
    <input type='checkbox' name='include_batchcom' id='include_batchcom' value="batchcom"><?php xl('Communications','e'); ?><br>
   </td>
+     <td class="text">
+         <input type='checkbox' name='include_recurring_days' id='include_recurring_days' value="recurring_days" ><?php xl('Recurrent Appointments','e'); ?><br>
+     </td>
  </tr>
 </table>
 
