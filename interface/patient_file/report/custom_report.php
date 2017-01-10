06e733f4ec185d614232cf93541db3d1f4bca1c5
Brady Miller <brady.g.miller@gmail.com>
2017-01-09 23:24:58 -0800

    also brought prior fix into the patient report

--- interface/patient_file/report/custom_report.php
+++ interface/patient_file/report/custom_report.php
@@ -329,7 +329,7 @@ foreach ($ar as $key => $val) {
                     if ($row == false || !recurrence_is_current($row['pc_endDate']))
                         continue;
                     echo "<div class='text' >";
-                    echo "<span>" . xlt('Appointment Category') . ': ' . text($row['pc_title']) . "</span>";
+                    echo "<span>" . xlt('Appointment Category') . ': ' . xlt($row['pc_catname']) . "</span>";
                     echo "<br>";
                     echo "<span>" . xlt('Recurrence') . ': ' .text($row['pc_recurrspec']) . "</span>";
                     echo "<br>";
