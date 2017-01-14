9f3db6a1cfe3f72ba3004126e39931175cf8b326
JP-DEV\sjpad <sjpadgett@gmail.com>
2017-01-14 12:05:21 -0500

    Some path changes for clarity

--- patients/patient/scripts/app/patientdata.js
+++ patients/patient/scripts/app/patientdata.js
@@ -433,7 +433,7 @@ page.patient.save({
 			success: function(){
 				if( live != 1){
 					setTimeout("app.appendAlert('Patient was sucessfully " + (isNew ? "inserted" : "updated") + "','alert-success',2000,'collectionAlert')",200);
-					setTimeout("window.location.href = './../home.php'",2500);
+					setTimeout("window.location.href ="+webRoot+" '/home.php'",2500);
 					}
 				else
 					eModal.close(true)
