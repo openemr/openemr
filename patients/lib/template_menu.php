d90d0b6bda16ee5122eef9b6ba7cecaca0326fb2
JP-DEV\sjpad <sjpadgett@gmail.com>
2017-01-16 19:42:46 -0500

    Stage 1 of notify and send invoice to portal Patient Documents from Postings. Stage 2 of allow specific patient template management

--- patients/lib/template_menu.php
+++ patients/lib/template_menu.php
@@ -29,5 +29,12 @@ foreach (glob($GLOBALS['OE_SITE_DIR'] . "/onsite_portal_documents/templates/*.tp
 
     echo '<li class="bg-success"><a id="' . $basefile . '"' . 'href="#" onclick="page.newDocument(' . "<%= cpid %>,'<%= cuser %>','$btnfile')".'"'.">$btnname</a></li>";
 }
+foreach (glob($GLOBALS['OE_SITE_DIR'] . "/onsite_portal_documents/templates/" . $pid . "/*.tpl") as $filename) {
+	$basefile = basename($filename,".tpl");
+	$btnname = str_replace('_', ' ',$basefile);
+	$btnfile = $basefile . '.tpl';
+
+	echo '<li class="bg-success"><a id="' . $basefile . '"' . 'href="#" onclick="page.newDocument(' . "<%= cpid %>,'<%= cuser %>','$btnfile')".'"'.">$btnname</a></li>";
+}
 
 ?>
