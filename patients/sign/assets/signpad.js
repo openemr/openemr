0e648af9ea2725dba98e5bd81dad38f81d8d8a67
Brady Miller <brady.g.miller@gmail.com>
2017-01-26 03:55:28 -0800

    header fixes to prior commit

--- patients/sign/assets/signpad.js
+++ patients/sign/assets/signpad.js
@@ -23,6 +23,10 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Thomas J Bradley
+ * @author Jerry Padgett <sjpadgett@gmail.com>
+ *
  */
 /**
  * Usage for accepting signatures:
