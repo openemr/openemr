00d2a91d59bc02b54f503fe6f51c7c3b78b6f8d5
shahar zi <shaharzi@matrix.co.il>
2017-02-06 11:39:44 +0200

    fixed sql

--- sql/5_0_0-to-5_0_1_upgrade.sql
+++ sql/5_0_0-to-5_0_1_upgrade.sql
@@ -233,11 +233,11 @@ INSERT INTO list_options (`list_id`, `option_id`, `title`, `seq`, `is_default`, 
 INSERT INTO list_options (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `notes`, `toggle_setting_1`) VALUES ('attendstat', 'x', 'x Cancelled', '50', '0', '0', 'FEFDCF|0', '0');
 #EndIf
 
-#IfNotRow2D registry directory group_attendance id 22
+#IfNotRow2D registry directory group_attendance category Clinical
 INSERT INTO registry (name, state, directory, id, sql_run, unpackaged, date, priority, category, nickname, patient_encounter, therapy_group_encounter) VALUES ('Group Attendance Form', 1, 'group_attendance', 22, 1, 1, '2015-10-15 00:00:00', 0, 'Clinical', '',0,1);
 #EndIf
 
-#IfNotRow2D registry directory newGroupEncounter id 23
+#IfNotRow2D registry directory newGroupEncounter category Clinical
 INSERT INTO registry (name, state, directory, id, sql_run, unpackaged, date, priority, category, nickname, patient_encounter, therapy_group_encounter) VALUES ('New Group Encounter Form', 1, 'newGroupEncounter', 23, 1, 1, '2015-10-15 00:00:00', 0, 'Clinical', '',0,1);
 #EndIf
 
