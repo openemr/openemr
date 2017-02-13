899837464b57471b0cac85f4db833c4a4d7b6cf9
oshri <oshriro@matrix.co.il>
2017-02-13 17:13:54 +0200

    fix bug at zend global

--- interface/modules/zend_modules/config/autoload/global.php
+++ interface/modules/zend_modules/config/autoload/global.php
@@ -85,7 +85,7 @@ return array(
  */
 function my_encrypt($data) {
     // Remove the base64 encoding from our key
-    $encryption_key = base64_decode($GLOBALS['safe_key']);
+    $encryption_key = base64_decode($GLOBALS['safe_key_database']);
     // Generate an initialization vector
     $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
     // Encrypt the data using AES 256 encryption in CBC mode using our encryption key and initialization vector.
@@ -102,7 +102,7 @@ function my_encrypt($data) {
 
 function my_decrypt($data) {
     // Remove the base64 encoding from our key
-    $encryption_key = base64_decode($GLOBALS['safe_key']);
+    $encryption_key = base64_decode($GLOBALS['safe_key_database']);
     // To decrypt, split the encrypted data from our IV - our unique separator used was "::"
     list($encrypted_data, $iv) = explode('::', base64_decode($data), 2);
     return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
