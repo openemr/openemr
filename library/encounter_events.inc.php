a40fdbe42dafb1c98e500974fa3313b3ba145ccc
Brady Miller <brady.g.miller@gmail.com>
2017-02-21 17:35:11 -0800

    bug fix for the patient PDF report

--- library/encounter_events.inc.php
+++ library/encounter_events.inc.php
@@ -1,4 +1,4 @@
-	<?php
+<?php
 // +-----------------------------------------------------------------------------+ 
 // Copyright (C) 2010 Z&H Consultancy Services Private Limited <sam@zhservices.com>
 //
