b8e8758c4064c4288e64fb8e8849d5d4d3f8df84
Sharon Cohen <sharonco@matrix.co.il>
2017-03-15 16:41:34 +0200

    Fixed not translated word

--- interface/super/rules/controllers/browse/controller.php
+++ interface/super/rules/controllers/browse/controller.php
@@ -24,7 +24,7 @@ class Controller_browse extends BaseController {
         $rules = resolve_rules_sql('','0',TRUE);
         foreach( $rules as $rowRule ) {
             $title = getLabel($rowRule['id'],'clinical_rules');
-            $type = "Reminder";
+            $type = xl("Reminder");
 
             $row = array(
                 "title" => $title,
