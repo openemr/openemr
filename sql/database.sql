2906f8a3aaa60071a842a79fc3ed19eca1f8ff76
Rod Roark <rod@sunsetsystems.com>
2017-03-17 08:53:36 -0700

    Some fixes for drawable images with larger image sizes. (#566)

--- sql/database.sql
+++ sql/database.sql
@@ -6957,7 +6957,7 @@ CREATE TABLE `users_facility` (
 CREATE TABLE `lbf_data` (
   `form_id`     int(11)      NOT NULL AUTO_INCREMENT COMMENT 'references forms.form_id',
   `field_id`    varchar(31)  NOT NULL COMMENT 'references layout_options.field_id',
-  `field_value` TEXT,
+  `field_value` LONGTEXT,
   PRIMARY KEY (`form_id`,`field_id`)
 ) ENGINE=InnoDB COMMENT='contains all data from layout-based forms';
 
