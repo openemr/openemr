77488f13fab672b79101ce16aa605bf2418c352c
Sherwin Gaddis <sherwingaddis@gmail.com>
2017-03-25 00:47:31 -0400

    minor change in the bottom layout to return it to proper order

--- interface/forms/procedure_order/new.php
+++ interface/forms/procedure_order/new.php
@@ -607,14 +607,16 @@ if ($qoe_init_javascript)
 ?>
 
 </table>
-
-<p>
+<div style="text-align: left">
+<div style="display:inline-block">
 <?php $procedure_order_type = getListOptions('order_type' , array('option_id', 'title')); ?>
 <select name="procedure_type_names" id="procedure_type_names" class='form-control'>
 	<?php foreach($procedure_order_type as $ordered_types){?>
 	<option value="<?php echo attr($ordered_types['option_id']); ?>" ><?php echo text(xl_list_label($ordered_types['title'])) ; ?></option>
 	<?php } ?>
 </select>
+</div>
+<div style="display:inline-block">
 <input type='button' value='<?php echo xla('Add Procedure'); ?>' onclick="addProcLine()" />
 &nbsp;
 <input type='submit' name='bn_save' value='<?php echo xla('Save'); ?>' onclick='transmitting = false;' />
@@ -622,6 +624,8 @@ if ($qoe_init_javascript)
 <input type='submit' name='bn_xmit' value='<?php echo xla('Save and Transmit'); ?>' onclick='transmitting = true;' />
 &nbsp;
 <input type='button' value='<?php echo xla('Cancel'); ?>' onclick="top.restoreSession();location='<?php echo $GLOBALS['form_exit_url']; ?>'" />
+</div>
+</div>
 </p>
 
 
