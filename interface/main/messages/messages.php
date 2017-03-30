397f3f28a81dca48df183ee4e3e9974192268b59
Brady Miller <brady.g.miller@gmail.com>
2017-03-30 04:27:10 -0700

    Update messages.php

--- interface/main/messages/messages.php
+++ interface/main/messages/messages.php
@@ -275,7 +275,7 @@ $ures = sqlStatement("SELECT username, fname, lname FROM users " .
   if ($urow['fname']) echo ", " . htmlspecialchars( $urow['fname'], ENT_NOQUOTES);
   echo "</option>\n";
  }
- if($GLOBALS['enable_patient_messaging']){
+ if($GLOBALS['enable_offsite_patient_messaging']){
       echo "<option value='" . htmlspecialchars( '-patient-', ENT_QUOTES) . "'";
       echo ">" . htmlspecialchars( '-Patient-', ENT_NOQUOTES);
       echo "</option>\n";
