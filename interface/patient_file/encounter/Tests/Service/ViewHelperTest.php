86b9bbb34c6be8bcd54f78f682529d2f162a5336
Robert Down <robertdown@live.com>
2017-04-28 12:07:54 -0400

    Correcting syntax error

--- interface/patient_file/encounter/Tests/Service/ViewHelperTest.php
+++ interface/patient_file/encounter/Tests/Service/ViewHelperTest.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: rdown
- * Date: 4/23/17
- * Time: 18:03
+ * ViewHelperTest
+ *
+ * @author Robert Down <robertdown@live.com>
+ * @copyright Copyright (C) 2017 Robert Down
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html GNU Public License
  */
 
 namespace OpenEMR\Encounter\Tests;
 
-require_once '../../../vendor/autoload.php';
-//require_once '../../../../globals.php';
+require_once '../../../../globals.php';
 
-use OpenEMR\Encounter\Service\ViewHelper;
+use OpenEMR\Encounter\Services\ViewHelper;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -24,9 +24,5 @@ final class ViewHelperTest extends TestCase
 
     public function testCanGetListOfRegisteredMenuItems()
     {
-        $this->assertInstanceOf(
-            ViewHelper::class,
-            ViewHelper::myGetRegistered()
-        );
     }
 }
