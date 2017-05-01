ed36e691f05fe37873fca56bd8fad45c77a6c764
Robert Down <robertdown@live.com>
2017-05-01 14:59:37 -0400

    Changing file-level docblock license
    Per request made by Brady

--- interface/patient_file/encounter/Tests/Service/ViewHelperTest.php
+++ interface/patient_file/encounter/Tests/Service/ViewHelperTest.php
@@ -2,9 +2,21 @@
 /**
  * Helper functions tests for an encounter view files
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * @package OpenEMR
  * @subpackage Encounter
- * @license http://opensource.org/licenses/gpl-license.php General Public License 3
  * @author Robert Down <robertdown@live.com>
  * @copyright Copyright (C) 2017 Robert Down
  */
