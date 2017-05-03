73231f112d0cffffdc96a03580c9d5318d202290
stephen waite <stephen.waite@cmsvt.com>
2017-05-03 17:48:55 -0400

    fix to prior

--- library/gen_x12_837.inc.php
+++ library/gen_x12_837.inc.php
@@ -816,8 +816,8 @@ function gen_x12_837($pid, $encounter, &$log, $encounter_claim=false) {
   // Loop 2310D is omitted in the case of home visits (POS=12).
   if ($claim->facilityPOS() != 12 &&
       (!$CMS_5010 || 
-      $claim->facilityNPI() != $claim->billingFacilityNPI()) ||
-      $claim->facilityStreet() != $claim->billingFacilityStreet())
+      ($claim->facilityNPI() != $claim->billingFacilityNPI() &&
+      $claim->facilityStreet() != $claim->billingFacilityStreet())))
     {
     ++$edicount;
     $out .= "NM1" .       // Loop 2310D Service Location
