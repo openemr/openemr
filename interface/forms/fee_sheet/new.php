f81ce61d497a37f991f71debb159418a080ceb8c
Ranganath Pathak <pathak01@hotmail.com>
2017-05-02 23:17:42 -0700

    06_styling_feesheet_master
    The fee sheet styling changed to accomodate more than 9 categories
    
    /interface/forms/fee_sheet/new.php -  substr($fs_category, 2)), (substr($fs_option, 2))
    /interface/forms/fee_sheet/code_choice/css/code_choices.css - 1px margin added
    /interface/forms/fee_sheet/code_choice/templates/code_choices.php
    - 10px padding for OK button

--- interface/forms/fee_sheet/new.php
+++ interface/forms/fee_sheet/new.php
@@ -721,9 +721,9 @@ while ($row = sqlFetchArray($res)) {
     echo ($i <= 1) ? " <tr>\n" : "";
     echo "  <td width='50%' align='center' nowrap>\n";
     echo "   <select style='width:96%' onchange='codeselect(this)'>\n";
-    echo "    <option value=''> " . xlt(substr($fs_category, 1)) . "</option>\n";
+    echo "    <option value=''> " . xlt(substr($fs_category, 2)) . "</option>\n";
   }
-  echo "    <option value='" . attr($fs_codes) . "'>" . xlt(substr($fs_option, 1)) . "</option>\n";
+  echo "    <option value='" . attr($fs_codes) . "'>" . xlt(substr($fs_option, 2)) . "</option>\n";
 }
 endFSCategory();
 
