d55f9b3f8c8d55110ac81a825c3bf67729af8823
Ranganath Pathak <pathak01@hotmail.com>
2017-05-21 23:59:44 -0700

    023_styling_find_order_popup_master styled the header of find_order_popup.php in Encounter > Administrative > Procedure Order
    /var/www/html/openemr/interface/orders/find_order_popup.php -
     <tr bgcolor="#ddddff" class="head">

--- interface/orders/find_order_popup.php
+++ interface/orders/find_order_popup.php
@@ -19,8 +19,8 @@
 * @author    Rod Roark <rod@sunsetsystems.com>
 */
 
-
-
+$fake_register_globals = false;
+$sanitize_all_escapes = true;
 
 require_once("../globals.php");
 
@@ -115,7 +115,7 @@ if (isset($_GET['formseq'])) echo '&formseq=' . $_GET['formseq'];
   </td>
  </tr>
 
- <tr bgcolor='#ddddff'>
+ <tr bgcolor='#ddddff' class="head">
   <td>
    <b>
 
