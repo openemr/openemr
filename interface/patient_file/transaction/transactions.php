cd6b9cd1d9154c68a108e1c272d2974eabbd082d
Brady Miller <brady.g.miller@gmail.com>
2017-05-24 03:36:06 -0700

    transaction scripts consolidation

--- interface/patient_file/transaction/transactions.php
+++ interface/patient_file/transaction/transactions.php
@@ -4,6 +4,7 @@
 
 include_once("../../globals.php");
 include_once("$srcdir/transactions.inc");
+require_once("$srcdir/options.inc.php");
 ?>
 <html>
 <head>
@@ -13,26 +14,11 @@ include_once("$srcdir/transactions.inc");
     ?>
 
 <script type="text/javascript">
-    function toggle( target, div ) {
-        $mode = $(target).find(".indicator").text();
-        if ( $mode == "collapse" ) {
-            $(target).find(".indicator").text( "expand" );
-            $(div).hide();
-        } else {
-            $(target).find(".indicator").text( "collapse" );
-            $(div).show();
-        }
+    // Called by the deleteme.php window on a successful delete.
+    function imdeleted() {
+        top.restoreSession();
+        location.href = 'transaction/transactions.php';
     }
-
-    $(document).ready(function(){
-
-        $("#transactions_view").click( function() {
-            toggle( $(this), "#transactions_div" );
-        });
-
-        // load transaction divs
-        $("#transactions_div").load("transactions_full.php");
-    });
 </script>
 </head>
 
@@ -48,11 +34,71 @@ include_once("$srcdir/transactions.inc");
     </div>
 
     <div class='text'>
-    <?php if ($result = getTransByPid($pid)) { ?>
-        <div id='transactions_div'></div>
-    <?php } else { ?>
-        <span class="text"><?php echo xlt('There are no transactions on file for this patient.'); ?></span>
-    <?php } ?>
+        <?php if ($result = getTransByPid($pid)) { ?>
+
+            <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>&nbsp;</th>
+                    <th><?php echo xlt('Type'); ?></th>
+                    <th><?php echo xlt('Date'); ?></th>
+                    <th><?php echo xlt('User'); ?></th>
+                    <th><?php echo xlt('Details'); ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($result as $item) {
+                    if (!isset($item['body'])) {
+                        $item['body'] = '';
+                    }
+                    if (getdate() == strtotime($item['date'])) {
+                        $date = "Today, " . date('D F ds', strtotime($item['date']));
+                    } else {
+                        $date = date('D F ds', strtotime($item['date']));
+                    }
+                    $date = oeFormatShortDate($item['refer_date']);
+                    $id = $item['id'];
+                    $edit = xl('Edit');
+                    $view = xl('View');
+                    $delete = xl('Delete');
+                    $title = xl($item['title']);
+                    ?>
+                    <tr>
+                        <td>
+                            <div class="btn-group pull-left">
+                                <?php if ($item['title'] == 'LBTref') { ?>
+                                    <a href='print_referral.php?transid=<?php echo attr($id); ?>' onclick='top.restoreSession();'
+                                        class='btn btn-view btn-default'>
+                                        <?php echo text($view); ?>
+                                    </a>
+                                <?php } ?>
+                                <a href='add_transaction.php?transid=<?php echo attr($id); ?>&title=<?php echo attr($title); ?>&inmode=edit'
+                                    onclick='top.restoreSession()'
+                                    class='btn btn-default btn-edit'>
+                                    <?php echo text($edit); ?>
+                                </a>
+                                <?php if (acl_check('admin', 'super')) { ?>
+                                    <a href='../deleter.php?transaction=<?php echo attr($id); ?>'
+                                        onclick='top.restoreSession()'
+                                        class='btn btn-default btn-delete'>
+                                        <?php echo text($delete); ?>
+                                    </a>
+                                <?php } ?>
+                            </div>
+                        </td>
+                        <td><?php echo generate_display_field(['data_type' => 1, 'list_id' => 'transactions'], $item['title']); ?></td>
+                        <td><?php echo text($date); ?></td>
+                        <td><?php echo text($item['user']); ?></td>
+                        <td><?php echo text($item['body']); ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+            </table>
+
+        <?php } else { ?>
+            <span class="text"><?php echo xlt('There are no transactions on file for this patient.'); ?></span>
+        <?php } ?>
     </div>
 </body>
 </html>
