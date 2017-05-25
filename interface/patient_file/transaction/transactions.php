b5969900b2fcc27f3539ee1dcdfd52da1c133a77
Robert Down <robertdown@live.com>
2017-05-24 23:29:04 -0400

    Some documentation

--- interface/patient_file/transaction/transactions.php
+++ interface/patient_file/transaction/transactions.php
@@ -12,6 +12,17 @@ require_once "$srcdir/options.inc.php";
 
 $include_standard_style_js = ['common.js'];
 
+/*
+ * This code kind of makes me nervous - it forces in the output of the standard
+ * header into a variable so we can pass it to the twig twmplate.
+ *
+ * I'm nervous because I don't have a lot of experience with ob_* functions.
+ * Ideally, once we get the assets helper class up and running we will use that
+ * instead. See https://stackoverflow.com/questions/2830366/require-once-to-variable
+ * for info on this method - RD
+ *
+ * @TODO migrate to asset helper class 2017-05-24 RD
+ */
 ob_start();
 require_once "{$GLOBALS['srcdir']}/templates/standard_header_template.php";
 $header = ob_get_contents();
