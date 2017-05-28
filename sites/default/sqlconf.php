40cecd443c3ad7b3c4539b1987d2e7e124ec5939
aalmangour <amanqour@gmail.com>
2017-05-29 02:16:25 +0300

    fixing incorrect message that says provider not available while creating provider schedule

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -1,16 +1,17 @@
 <?php
 //  OpenEMR
 //  MySQL Config
-//  Referenced from /library/sqlconf.php.
 
+$host	= '127.0.0.1';
+$port	= '3306';
+$login	= 'openemr_dev';
+$pass	= 'xz3rox123';
+$dbase	= 'openemr_dev';
+
+//Added ability to disable
+//utf8 encoding - bm 05-2009
 global $disable_utf8_flag;
 $disable_utf8_flag = false;
-
-$host	= 'localhost';
-$port	= '3306';
-$login	= 'openemr';
-$pass	= 'openemr';
-$dbase	= 'openemr';
 
 $sqlconf = array();
 global $sqlconf;
@@ -19,12 +20,11 @@ $sqlconf["port"] = $port;
 $sqlconf["login"] = $login;
 $sqlconf["pass"] = $pass;
 $sqlconf["dbase"] = $dbase;
-
 //////////////////////////
 //////////////////////////
 //////////////////////////
 //////DO NOT TOUCH THIS///
-$config = 0; /////////////
+$config = 1; /////////////
 //////////////////////////
 //////////////////////////
 //////////////////////////
