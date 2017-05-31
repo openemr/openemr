08d4c38715fd848d28ceea9c8ab22f9c72a500a5
Sherwin Gaddis <sherwingaddis@gmail.com>
2017-05-31 17:27:55 -0400

    Removed double null

--- sql/5_0_0-to-5_0_1_upgrade.sql
+++ sql/5_0_0-to-5_0_1_upgrade.sql
@@ -132,19 +132,19 @@ INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `notes`, `ac
 -- 1. users
 --
 #IfMissingColumn users weno_prov_id
-ALTER TABLE `users` ADD `weno_prov_id` VARCHAR(15) NULL DEFAULT NULL;
+ALTER TABLE `users` ADD `weno_prov_id` VARCHAR(15) DEFAULT NULL;
 #Endif
 
 -- 2. prescriptions 
 --
 #IfMissingColumn prescriptions ntx 
-ALTER TABLE `prescriptions` ADD `ntx` INT(2) NULL DEFAULT NULL;
+ALTER TABLE `prescriptions` ADD `ntx` INT(2) DEFAULT NULL;
 #Endif
 
 -- 3. prescriptions 
 --
 #IfMissingColumn prescriptions rtx 
-ALTER TABLE `prescriptions` ADD `rtx` INT(2) NULL DEFAULT NULL;
+ALTER TABLE `prescriptions` ADD `rtx` INT(2) DEFAULT NULL;
 #Endif
 
 -- 4. prescriptions 
