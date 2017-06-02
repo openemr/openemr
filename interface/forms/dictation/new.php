7d103bd12536a94d37102b0170dcf2a9bc2557d8
Sherwin Gaddis <sherwingaddis@gmail.com>
2017-06-02 08:05:01 -0400

    After the first review I went ahead and added the other supporting files

--- interface/forms/dictation/new.php
+++ interface/forms/dictation/new.php
@@ -1,21 +1,13 @@
-<!-- Form generated from formsWiz -->
 <?php
-/** Copyright (C) 2016 Sherwin Gaddis <sherwingaddis@gmail.com>
- *
- * LICENSE: This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
- *
+/** 
+ *  Dictation Form
+ * 
  * @package OpenEMR
- * @author  Sherwin Gaddis <sherwingaddis@gmail.com>
  * @link    http://www.open-emr.org
+ * @author  Sherwin Gaddis <sherwingaddis@gmail.com>
+ * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ * @copyright Copyright (c) 2017 Sherwin Gaddis <sherwingaddis@gmail.com>
+ * 
  */
 
 use OpenEMR\Core\Header;
@@ -27,30 +19,32 @@ $returnurl = 'encounter_top.php';
 ?>
 <html>
 <head>
-<?php Header::setupHeader(); ?>
-	<script type="text/javascript" src="<?php echo $webroot."/library/custom_template/ckeditor/ckeditor.js" ?>"</script>
-	<script src="<?php echo $webroot."/library/custom_template/ckeditor/_samples/sample.js" ?>" type="text/javascript"></script>
-	<link href="<?php echo $webroot."/library/custom_template/ckeditor/_samples.css"; ?>" rel="stylesheet" type="text/css" />
+   <?php Header::setupHeader(); ?>
+   <script type="text/javascript" src="<?php echo $webroot."/library/custom_template/ckeditor/ckeditor.js" ?>"</script>
+   <script src="<?php echo $webroot."/library/custom_template/ckeditor/_samples/sample.js" ?>" type="text/javascript"></script>
+   <link href="<?php echo $webroot."/library/custom_template/ckeditor/_samples.css"; ?>" rel="stylesheet" type="text/css" />
 </head>
 <body class="body_top">
-<h1><?php echo xlt('Speech Dictation'); ?></h1><br><br>
-
 <div class="container">
-<form method=post action="<?php echo $rootdir;?>/forms/dictation/save.php?mode=new" name="my_form">
-<div class="form-group">
+   <div class="page-header">
+      <h1><?php echo xlt('Speech Dictation'); ?></h1><br><br>
+   </div>
 
-<label class="text"><?php echo xlt('Dictation: '); ?></label><br><textarea class="form-control ckeditor" cols=80 rows=24 wrap="virtual" name="dictation" ></textarea>
-</div>
-<div class="form-group">
-<label class="text"><?php echo xlt('Additional Notes:'); ?> </label><br><textarea class="form-control ckeditor" cols=80 rows=8 wrap="virtual" name="additional_notes" ></textarea>
+  <form method=post action="<?php echo $rootdir;?>/forms/dictation/save.php?mode=new" name="my_form">
+
+     <div class="form-group">
+       <label for="dictation"><?php echo xlt('Dictation: '); ?></label><br><textarea class="form-control ckeditor" cols=80 rows=24 wrap="virtual" name="dictation" ></textarea>
+    </div>
+    <div class="form-group">
+       <label for="additional-notes"><?php echo xlt('Additional Notes:'); ?> </label><br><textarea class="form-control ckeditor" cols=80 rows=8 wrap="virtual" name="additional_notes" ></textarea>
+    </div>
+
+     <a href="javascript:top.restoreSession();document.my_form.submit();" class="btn btn-default"><?php echo xlt('Save'); ?></a>
+
+     <a href="<?php echo "$rootdir/patient_file/encounter/$returnurl";?>" class="btn btn-default" onclick="top.restoreSession()"><?php echo xlt('Don\'t Save'); ?></a>
+  </form>
 </div>
 
-<a href="javascript:top.restoreSession();document.my_form.submit();" class="link_submit"><button><?php echo xlt('Save'); ?></button></a>
-
-<a href="<?php echo "$rootdir/patient_file/encounter/$returnurl";?>" class="link"
- onclick="top.restoreSession()"><button><?php echo xlt('Don\'t Save'); ?></button></a>
-</form>
-</div>
 <?php
 formFooter();
 ?>
--- interface/forms/dictation/view.php
+++ interface/forms/dictation/view.php
@@ -1,31 +1,49 @@
-<!-- Forms generated from formsWiz -->
 <?php
+/** 
+ *  Dictation Form Review Saved Data
+ * 
+ * @package OpenEMR
+ * @link    http://www.open-emr.org
+ * @author  Sherwin Gaddis <sherwingaddis@gmail.com>
+ * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ * @copyright Copyright (c) 2017 Sherwin Gaddis <sherwingaddis@gmail.com>
+ * 
+ */
 
-
-
-
+use OpenEMR\Core\Header;
 include_once("../../globals.php");
 $returnurl = 'encounter_top.php';
 ?>
-<html><head>
-<?php html_header_show();?>
-<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
+<html>
+<head>
+   <?php Header::setupHeader(); ?>
+   <script type="text/javascript" src="<?php echo $webroot."/library/custom_template/ckeditor/ckeditor.js" ?>"</script>
+   <script src="<?php echo $webroot."/library/custom_template/ckeditor/_samples/sample.js" ?>" type="text/javascript"></script>
+   <link href="<?php echo $webroot."/library/custom_template/ckeditor/_samples.css"; ?>" rel="stylesheet" type="text/css" />
 </head>
 <body class="body_top">
+<div class="container">
 <?php
 include_once("$srcdir/api.inc");
 $obj = formFetch("form_dictation", $_GET["id"]);
 ?>
-<form method=post action="<?php echo $rootdir?>/forms/dictation/save.php?mode=update&id=<?php echo attr($_GET["id"]);?>" name="my_form">
-<span class="title"><?php echo xlt('Speech Dictation'); ?></span><Br><br>
-<span class=text><?php echo xlt('Dictation: '); ?></span><br><textarea cols=80 rows=24 wrap=virtual name="dictation" ><?php echo text($obj{"dictation"});?></textarea><br>
-<span class=text><?php echo xlt('Additional Notes: '); ?></span><br><textarea cols=80 rows=8 wrap=virtual name="additional_notes" ><?php echo text($obj{"additional_notes"});?></textarea><br>
-<br>
-<a href="javascript:top.restoreSession();document.my_form.submit();" class="link_submit">[<?php echo xlt('Save'); ?>]</a>
-<br>
-<a href="<?php echo "$rootdir/patient_file/encounter/$returnurl";?>" class="link"
- onclick="top.restoreSession()">[<?php echo xlt('Don\'t Save Changes'); ?>]</a>
-</form>
+   <form method=post action="<?php echo $rootdir?>/forms/dictation/save.php?mode=update&id=<?php echo attr($_GET["id"]);?>" name="my_form">
+     <div class="page-header">
+      <h1><?php echo xlt('Speech Dictation Review/Edit'); ?></h1>
+     </div>
+     <div class="form-group">
+       <label for="dictation"><?php echo xlt('Dictation: '); ?></label><br><textarea class="form-control ckeditor" cols=80 rows=24 wrap="virtual" name="dictation" ><?php echo text($obj{"dictation"});?></textarea>
+     </div>
+     <div class="form-group">
+       <label for="additional_notes"><?php echo xlt('Additional Notes: '); ?></span><br><textarea class="form-control ckeditor"cols=80 rows=8 wrap="virtual" name="additional_notes" ><?php echo text($obj{"additional_notes"});?></textarea>
+     </div>
+
+       <button type="submit" class="btn btn-default btn-save"><?php echo xlt('Update'); ?></button>
+
+       <a href="<?php echo "$rootdir/patient_file/encounter/$returnurl";?>" class="btn btn-cancel btn-link" onclick="top.restoreSession()"><?php echo xlt('Cancel'); ?></a>
+     </form>
+</div>
+
 <?php
 formFooter();
 ?>
