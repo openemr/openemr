3e2d009ad8acadca058ff2fce57a21a0cbd18a26
Sherwin Gaddis <sherwingaddis@gmail.com>
2017-06-03 06:30:52 -0400

    XSS prevention with html_entity_decode()

--- interface/forms/dictation/report.php
+++ interface/forms/dictation/report.php
@@ -40,7 +40,7 @@ function dictation_report( $pid, $encounter, $cols, $id) {
       }
       $key=ucwords(str_replace("_"," ",$key));
       print "<td><span class='bold'>" . xlt($key) . ": </span><span class='text'>" .
-        nl2br($value) . "</span></td>";
+        html_entity_decode($value) . "</span></td>";
       $count++;
       if ($count == $cols) {
         $count = 0;
