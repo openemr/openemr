45a17a40cff4bdd309d2cf7557811bd23f97b605
Ranganath Pathak <pathak01@hotmail.com>
2017-06-07 02:14:45 -0700

    bootstrapped version of the following encounter forms with namespace reference
    Encounters > Administrative
    	Fee Sheet - Search Button row needs minor work
    	Misc Billing Options HCFA - minor problems in Light css
    	New Encounter Form - Good
    	Procedure Order - Good
    
    Encounters > Clinical
    	Care Plan - Good
    	Clinical Instructions - Good
    	Observation - Good
    	Review Of Systems - Good
    	Review Of Systems Checks - Good
    	SOAP - Good after removing  class form-horizontal
    	Speech Dictation - Good
    	Vitals - Semi styled

--- interface/forms/dictation/view.php
+++ interface/forms/dictation/view.php
@@ -3,29 +3,82 @@
 
 
 
-
+use OpenEMR\Core\Header;
 include_once("../../globals.php");
 $returnurl = 'encounter_top.php';
 ?>
 <html><head>
-<?php html_header_show();?>
-<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
+<?php Header::setupHeader('bootstrap');?>
+
+<style type="text/css" title="mystyles" media="all">
+ 
+	.form-group{
+		margin-bottom: 5px;
+		
+	}
+	legend{
+		border-bottom: 2px solid #E5E5E5;
+		background:#E5E5E5;
+		padding-left:10px;
+	}
+	.form-horizontal .control-label {
+		padding-top: 2px;
+    }
+	fieldset{
+		background-color: #F2F2F2;
+		margin-bottom:10px;
+		padding-bottom:15px;
+	}
+</style>
 </head>
 <body class="body_top">
 <?php
 include_once("$srcdir/api.inc");
 $obj = formFetch("form_dictation", $_GET["id"]);
 ?>
-<form method=post action="<?php echo $rootdir?>/forms/dictation/save.php?mode=update&id=<?php echo attr($_GET["id"]);?>" name="my_form">
-<span class="title"><?php echo xlt('Speech Dictation'); ?></span><Br><br>
-<span class=text><?php echo xlt('Dictation: '); ?></span><br><textarea cols=80 rows=24 wrap=virtual name="dictation" ><?php echo text($obj{"dictation"});?></textarea><br>
-<span class=text><?php echo xlt('Additional Notes: '); ?></span><br><textarea cols=80 rows=8 wrap=virtual name="additional_notes" ><?php echo text($obj{"additional_notes"});?></textarea><br>
-<br>
-<a href="javascript:top.restoreSession();document.my_form.submit();" class="link_submit">[<?php echo xlt('Save'); ?>]</a>
-<br>
-<a href="<?php echo "$rootdir/patient_file/encounter/$returnurl";?>" class="link"
- onclick="top.restoreSession()">[<?php echo xlt('Don\'t Save Changes'); ?>]</a>
-</form>
+<div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="page-header">
+                    <h2><?php echo xlt("Speech Dictation"); ?></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+
+				<form method=post action="<?php echo $rootdir?>/forms/dictation/save.php?mode=update&id=<?php echo attr($_GET["id"]);?>" name="my_form">
+				
+				<fieldset>
+						<legend class=""><?php echo xlt('Dictation')?></legend>
+							<div class="form-group">
+								<div class="col-sm-10 col-sm-offset-1">
+									<textarea name="dictation"	class="form-control" cols="80" rows="15" ><?php echo text($obj{"dictation"});?></textarea>
+								</div>
+							</div>
+				</fieldset>
+				<fieldset>
+						<legend class=""><?php echo xlt('Additional Notes'); ?></legend>
+							<div class="form-group">
+								<div class="col-sm-10 col-sm-offset-1">
+									<textarea name="additional_notes"	class="form-control" cols="80" rows="5" ><?php echo text($obj{"additional_notes"});?></textarea>
+								</div>
+							</div>
+				</fieldset>
+				
+                    <div class="form-group">
+                        <div class="col-sm-12 text-center">
+                            <div class="btn-group" role="group">
+                                <a href="javascript:top.restoreSession();document.my_form.submit();" class="btn btn-default btn-save"><?php echo xlt('Save'); ?></a>
+								<a href="<?php echo "$rootdir/patient_file/encounter/$returnurl";?>" class="btn btn-default btn-cancel" onclick="top.restoreSession()"><?php echo xlt('Don\'t Save'); ?></a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 <?php
 formFooter();
 ?>
