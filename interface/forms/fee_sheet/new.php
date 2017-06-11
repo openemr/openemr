3ff36394cbe352e383407183cce895805bcee83c
shachar058 <shaharzi@matrix.co.il>
2017-06-11 11:33:49 +0100

    Permission fixes in fee sheet (#839)

--- interface/forms/fee_sheet/new.php
+++ interface/forms/fee_sheet/new.php
@@ -28,6 +28,14 @@ require_once("../../globals.php");
 require_once("$srcdir/FeeSheetHtml.class.php");
 require_once("codes.php");
 
+//acl check
+$can_view = acl_check('admin', 'superbill', false, 'view');
+$can_write = acl_check('admin', 'superbill', false, 'write');
+if (!$can_view && !$can_write)
+{
+  formJump();
+}
+
 // Some table cells will not be displayed unless insurance billing is used.
 $usbillstyle = $GLOBALS['ippf_specific'] ? " style='display:none'" : "";
 $justifystyle = justifiers_are_used() ? "" : " style='display:none'";
