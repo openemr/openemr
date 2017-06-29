95307557d27115f5fbceb2a9c6ab842672ef4f30
Brady Miller <brady.g.miller@gmail.com>
2017-06-28 18:17:21 -0700

    security fix

--- library/sanitize.inc.php
+++ library/sanitize.inc.php
@@ -1,31 +1,24 @@
 <?php
 /**
-* Function to check and/or sanitize things for security such as
-* directories names, file names, etc.
-*
-* Copyright (C) 2012 by following Brady Miller <brady@sparmy.com>
-*
-* LICENSE: This program is free software; you can redistribute it and/or 
-* modify it under the terms of the GNU General Public License 
-* as published by the Free Software Foundation; either version 3 
-* of the License, or (at your option) any later version. 
-* This program is distributed in the hope that it will be useful, 
-* but WITHOUT ANY WARRANTY; without even the implied warranty of 
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-* GNU General Public License for more details. 
-* You should have received a copy of the GNU General Public License 
-* along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
-*
-* @package OpenEMR
-* @author Brady Miller <brady@sparmy.com>
-* @author Roberto Vasquez <robertogagliotta@gmail.com>
-* @author Shachar Zilbershlag <shaharzi@matrix.co.il>
-* @link http://www.open-emr.org
-*/
+ * Function to check and/or sanitize things for security such as
+ * directories names, file names, etc.
+ *
+ * @package OpenEMR
+ * @link    http://www.open-emr.org
+ * @author  Brady Miller <brady.g.miller@gmail.com>
+ * @author  Roberto Vasquez <robertogagliotta@gmail.com>
+ * @author  Shachar Zilbershlag <shaharzi@matrix.co.il>
+ * @copyright Copyright (c) 2012-2017 Brady Miller <brady.g.miller@gmail.com>
+ * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+
 // If the label contains any illegal characters, then the script will die.
 function check_file_dir_name($label) {
-  if (empty($label) || preg_match('/[^A-Za-z0-9_.-]/', $label))
+  if (empty($label) || preg_match('/[^A-Za-z0-9_.-]/', $label)) {
+    error_log("ERROR: The following variable contains invalid characters:" . $label);
     die(xlt("ERROR: The following variable contains invalid characters").": ". attr($label));
+  }
 }
 
 // Convert all illegal characters to _
