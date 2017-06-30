77e77331a68249e6e3955b4d0c3881606a990eaa
epsdky <sequex@hushmail.com>
2017-06-30 00:11:50 -0700

    calendar slot fix

--- version.php
+++ version.php
@@ -47,7 +47,7 @@ if (getenv('OPENEMR__ENVIRONMENT') === 'dev') {
     $v_js_includes = md5(microtime());
 } else {
     // Change this number when bumping
-    $v_js_includes = 38;
+    $v_js_includes = 39;
 }
 // Do note modify below
 $GLOBALS['v_js_includes'] = $v_js_includes;
