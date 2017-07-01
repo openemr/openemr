0396fad055865ca2682a0c9521209367c32694cf
MD Support <mdsupport@users.sourceforge.net>
2017-07-01 14:30:11 -0400

    POC: Simplified datetimepicker (Rev 1)
    Incorporate translations and minor settings.  Brings in mapping for html5 input date types.
    Including batchcom modifications to illustrate the minimal approach.

--- library/asset_datetimepicker.inc.php
+++ library/asset_datetimepicker.inc.php
@@ -14,36 +14,80 @@
  * "oemr_time" : only time
  * Additional settings should be specified with data-dttm-* attributes of the field.
 **/
-$oemr_dttm_dir = $GLOBALS['assets_static_relative']."/jquery-datetimepicker-2-5-4/build";
+
+// Performance considerations -
+// 1. Add an option that dictates if standard english should be translated (default: no)
+// 2. Move this logic to login and store json string in $_SESSION
+//
+$mm = array('January','February','March','April','May','June','July ', 
+		'August', 'September', 'October', 'November', 'December');
+$dttm_ix = count($mm);
+while ($dttm_ix-- > 0) {
+	$mm[$dttm_ix] = xla($mm[$dttm_ix]);
+}
+$dws = array();
+$dd = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+$dttm_ix = count($dd);
+while ($dttm_ix-- > 0) {
+	array_unshift($dws, xla(substr($dd[$dttm_ix], 0, 3)));
+	$dd[$dttm_ix] = xla($dd[$dttm_ix]);
+}
+// Ready to construct json translatable structure
+$dttm_wrk = array('months' => $mm, 'dayOfWeekShort' => $dws, 'dayOfWeek' => $dd);
+// Initialize default settings needed for dttm object
+$dttm_settings = array('i18n'=>array('en'=>$dttm_wrk), 
+		'rtl' => ($_SESSION['language_direction'] == 'rtl') ? 'true' : 'false'
+);
+// Add additional values from application logic below (e.g. user/session settings)
+
+// -- Bring in components if needed --
+//if (class_exists('OpenEMR\Core\Header')) {
+	// Datepicker won't work unless added to the Header::setupHeader list
+//} else {
+	// This will fail if base script has not brought in jQ
+	$oemr_dttm_dir = $GLOBALS['assets_static_relative']."/jquery-datetimepicker-2-5-4/build";
+	printf('<link rel="stylesheet" type="text/css" href="%s/jquery.datetimepicker.min.css">
+			<script type="text/javascript" src="%s/jquery.datetimepicker.full.min.js"></script>',
+			$oemr_dttm_dir, $oemr_dttm_dir);
+//}
 ?>
 
-<link rel="stylesheet" type="text/css" href="<?php echo $oemr_dttm_dir ?>/jquery.datetimepicker.min.css" type="text/css">
-<script type="text/javascript" src="<?php echo $oemr_dttm_dir ?>/jquery.datetimepicker.full.min.js"></script>
-
 <script type="text/javascript">
-jQuery('.oemr_date, .oemr_datetime, .oemr_time').each(function() {
-	var ctl = jQuery(this);
-	var ctl_opts = {};
-	if (ctl.hasClass('oemr_date')) {
-		ctl_opts['timepicker'] = false;
-		ctl_opts['format'] = 'Y-m-d';
+var dttm_sel_map = {
+	"cls": {
+		"oemr_date": {'timepicker':false, 'format': 'Y-m-d'},
+		"oemr_time": {'datepicker':false, 'format': 'H:i'},
+		"oemr_datetime": {'format': 'Y-m-d H:i'},
+	},
+	"inp": {
+		"date": {'timepicker':false, 'format': 'Y-m-d'},
+		"time": {'datepicker':false, 'format': 'H:i'},
+		"datetime-local": {'format': 'Y-m-d H:i'},
 	}
-	if (ctl.hasClass('oemr_time')) {
-		ctl_opts['datepicker'] = false;
-		ctl_opts['format'] = 'H:i';
-	}
-	<?php // TBD: Set inherited values from application logic (e.g. user/session settings) ?>
-	
-	<?php // Other properties (without validation) ?>
-	var ctl_data = ctl.data();
-	for (var ctl_prop in ctl_data) {
-		if (ctl_prop.toString().substr(0,5) == "dttm-") {
-			var ctl_prop_val = ctl_data[ctl_prop];
-			if (ctl_prop_val.toString() == 'true') {ctl_prop_val=true;}
-			if (ctl_prop_val.toString() == 'false') {ctl_prop_val=false;}
-			ctl_opts[ctl_prop.toString().slice(5)] = ctl_prop_val;
-		}
-	}
-	ctl.datetimepicker(ctl_opts);
+};
+var jq_sel = [];
+Object.keys(dttm_sel_map.cls).forEach(function(dttm_cls) {
+	jq_sel.push("."+dttm_cls);
+});
+Object.keys(dttm_sel_map.inp).forEach(function(dttm_type) {
+	jq_sel.push("input[type="+dttm_type+"]");
+});
+jQuery(jq_sel.join(",")).each(function() {
+    var ctl = jQuery(this);
+    var ctl_opts = <?php echo json_encode($dttm_settings); ?>;
+    Object.keys(dttm_sel_map.cls).forEach(function(dttm_cls) {
+        if (ctl.hasClass(dttm_cls)) {Object.assign(ctl_opts, dttm_sel_map.cls[dttm_cls]);}
+    });
+    Object.keys(dttm_sel_map.inp).forEach(function(dttm_typ) {
+        if (ctl.attr("type") == dttm_typ) {Object.assign(ctl_opts, dttm_sel_map.inp[dttm_typ]);}
+    });
+    <?php // Other properties (without validation) ?>
+    var ctl_data = ctl.data();
+    for (var ctl_prop in ctl_data) {
+        if (ctl_prop.toString().substr(0,5) == "dttm-") {
+            ctl_opts[ctl_prop.toString().slice(5)] = ctl_data[ctl_prop];
+        }
+    }
+    ctl.datetimepicker(ctl_opts);
 });
 </script>
