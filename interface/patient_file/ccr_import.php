434f11b9ca5e5dab6a32f1143f259a3305a22dc8
Brady Miller <brady.g.miller@gmail.com>
2017-07-02 18:10:04 -0700

    minor adjustments to prior commit

--- interface/patient_file/ccr_import.php
+++ interface/patient_file/ccr_import.php
@@ -4,19 +4,6 @@
  *
  * Functions to upload the CCR XML and to parse and insert it into audit tables.
  *
- * Copyright (C) 2013 Z&H Consultancy Services Private Limited <sam@zhservices.com>
- *
- * LICENSE: This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
- *
  * @package OpenEMR
  * @link    http://www.open-emr.org
  * @author  Eldho Chacko <eldho@zhservices.com>
@@ -44,8 +31,7 @@ use OpenEMR\Core\Header;
 <body class="body_top" >
   <main class="container">
     <div class="row">
-      <div class="col-md-3"></div>
-      <div class="col-md-6">
+      <div class="col-md-6 col-md-offset-3">
         <h3><?php echo xlt("Steps for uploading CCR XML");?></h3>
         <ol class="list-group">
           <li class="list-group-item"><?php echo xlt('For an existing patient, go to Patient Summary->Documents. For a new patient, go to Miscellanous->New Documents').'.'; ?></li>
