d85bbb26abe3f88da27ad90b2ec91a983d0a816e
Jason 'Toolbox' Oettinger <jason@oettinger.email>
2017-07-15 14:44:31 -0400

    Adding lint_style_staged as an option, allowing pre-commit checks for PSR generalized sniffing to function in ci.sh

--- ci/ci.sh
+++ ci/ci.sh
@@ -8,41 +8,52 @@ if [ -z "$1" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
   exit 0
 fi
 
-case "$CI_JOB" in
 
-"lint_syntax")
-  if [ "$1" == "-d" ] || [ "$1" == "--dir" ] ; then
-    cd $2
-    find . -type d \( -path ./vendor \
-                      -o -path ./interface/main/calendar/modules \
-                      -o -path ./interface/reports \
-                      -o -path ./contrib/util \
-                      -o -path ./library/html2pdf/vendor/tecnickcom \
-                      -o -path ./library/classes/fpdf \
-                      -o -path ./library/html2pdf \
-                      -o -path ./gacl \
-                      -o -path ./library/edihistory \) -prune -o \
-          -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
-  fi
-  ;;
-"lint_style")
-  if [ "$1" == "-d" ] || [ "$1" == "--dir" ] ; then
-    cd $2
-    echo "extension = ldap.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+#takes list of files/folders to sniff as its only argument(s)
+function sniff {
+    INI=$HOME/.phpenv/versions/$(phpenv version-name 2>&1 >/dev/null)/etc/php.ini || true
+    if [ -f $INI ]; then
+        grep -q "extension = ldap.so" $INI || echo "extension = ldap.so" >> $INI
+    fi
     composer global require "squizlabs/php_codesniffer=3.0.*"
-    composer global exec -- cd $2 && phpcs -p -n --extensions=php,inc --standard=ci/phpcs.xml --report-width=120 --report=summary --report=source --report=info .
-  fi
-  ;;
-"lint_style_new_code")
-  if [ "$1" == "-d" ] || [ "$1" == "--dir" ] ; then
-    cd $2
-    echo "extension = ldap.so" >> $HOME/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-    composer global require "squizlabs/php_codesniffer=3.0.*"
-    MODIFIED_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD |  tr "\n" " ")
-    composer global exec -- cd $2 && phpcs -p -n --extensions=php,inc --standard=ci/phpcs_strict.xml --report-width=120 --report=summary --report=source --report=info $MODIFIED_FILES
-  fi
-  ;;
-*)
-  echo "Error: not a valid CI_JOB"
-  ;;
-esac
+
+    composer global exec -- cd $DIR && \
+        phpcs -p -n --extensions=php,inc --report-width=120 $@
+}
+
+if [ "$1" == "-d" ] || [ "$1" == "--dir" ] ; then
+
+    DIR=$2
+
+    case "$CI_JOB" in
+
+        "lint_syntax")
+            cd $2
+            find . -type d \( -path ./vendor \
+                            -o -path ./interface/main/calendar/modules \
+                            -o -path ./interface/reports \
+                            -o -path ./contrib/util \
+                            -o -path ./library/html2pdf/vendor/tecnickcom \
+                            -o -path ./library/classes/fpdf \
+                            -o -path ./library/html2pdf \
+                            -o -path ./gacl \
+                            -o -path ./library/edihistory \) -prune -o \
+                -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
+            ;;
+        "lint_style")
+            sniff . --standard=ci/phpcs.xml --report=summary --report=source --report=info
+            ;;
+        "lint_style_new_commit")
+            MODIFIED_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD | tr "\n" " ")
+            sniff "$MODIFIED_FILES" --standard=ci/phpcs_strict.xml --report=summary
+            ;;
+        "lint_style_staged")
+            MODIFIED_FILES=$(git diff --cached --name-only | tr "\n" " ")
+            sniff "$MODIFIED_FILES" --standard=ci/phpcs_strict.xml --report=summary
+            ;;
+        *)
+            echo "Error: not a valid CI_JOB"
+
+            ;;
+    esac
+fi
