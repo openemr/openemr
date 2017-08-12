67c94fbabf2523310e7f7de45bd394a005f55416
Brady Miller <brady.g.miller@gmail.com>
2017-08-12 10:35:09 -0700

    psr2 changes to prior commit

--- library/allow_cronjobs.php
+++ library/allow_cronjobs.php
@@ -10,22 +10,28 @@
  */
 
 // Check if running as a cronjob
-if (php_sapi_name() !== 'cli') return;
+if (php_sapi_name() !== 'cli') {
+    return;
+}
 
 /**
  * Cronjobs are expected to specify arguments as -
  * php -f full_script_name arg1=value1 arg2=value2
- * 
+ *
  * Code below translates $argv to $_REQUEST
- */ 
+ */
 foreach ($argv as $argk => $argval) {
-    if ($argk == 0) continue;
+    if ($argk == 0) {
+        continue;
+    }
     $pair = explode("=", $argval);
     $_REQUEST[trim($pair[0])] = (isset($pair[1]) ? trim($pair[1]) : null);
 }
 
 // Every job must have at least one argument specifing site
-if (!isset($_REQUEST['site'])) exit("site=?");
+if (!isset($_REQUEST['site'])) {
+    exit("site=?");
+}
 
 // Simulate $_GET and $_POST for use by scripts
 $_GET = $_REQUEST;
@@ -33,5 +39,3 @@ $_POST = $_REQUEST;
 
 // Ignore auth checks
 $ignoreAuth = true;
-
-?>
