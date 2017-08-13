0c7a67b8fb665323abe6ae02b658a7066a24c1f8
stee <stephen.waite@cmsvt.com>
2017-08-13 12:55:09 -0400

    fix slash

--- library/gen_x12_837.inc.php
+++ library/gen_x12_837.inc.php
@@ -11,7 +11,7 @@
  * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-use OpenEMR/Billing;
+use OpenEMR\Billing;
 
 require_once("Claim.class.php");
 
