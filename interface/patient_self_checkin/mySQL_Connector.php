08db5ae9fd9db007f0ef7aee1208f00e24f99c60
Alfie Carlisle <alfie@carlisles.co>
2017-08-14 12:25:31 +0100

    Link to OpenEMR db

--- interface/patient_self_checkin/mySQL_Connector.php
+++ interface/patient_self_checkin/mySQL_Connector.php
@@ -1,10 +1,13 @@
 <?php
 
+require_once("../globals.php");
+require_once($GLOBALS['webserver_root'] . '/sites/default/sqlconf.php');
+
 // Localhost testing server
-DEFINE ('DB_USER', 'alfie');
-DEFINE ('DB_PASSWORD', 'mysql');
-DEFINE ('DB_HOST','192.168.0.2:3307');
-DEFINE ('DB_NAME', 'emis');
+DEFINE ('DB_USER', $login);
+DEFINE ('DB_PASSWORD', $pass);
+DEFINE ('DB_HOST',$host .":". $port);
+DEFINE ('DB_NAME', $dbase);
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die();
