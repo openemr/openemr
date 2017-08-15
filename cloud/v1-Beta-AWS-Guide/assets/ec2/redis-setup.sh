c6f57bc074c16a69001009de4d3477c3f5e8f99e
Matthew Vita <matthewvita@live.com>
2017-08-14 21:09:36 -0400

    deleting beta cloud solution

--- cloud/v1-Beta-AWS-Guide/assets/ec2/redis-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update -y
-sudo apt-get install -y redis-server
-sudo sed -i 's/bind\ 127.0.0.1/\#bind\ 127.0.0.1/' /etc/redis/redis.conf
-sudo service redis-server restart
