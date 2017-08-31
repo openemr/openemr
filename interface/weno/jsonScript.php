966e5a1307b3708ca993bbb85c9b67d900030a5f
Sherwin Gaddis <sherwingaddis@gmail.com>
2017-08-31 13:24:57 -0400

    Updated the drug list and pharmacy list

--- interface/weno/jsonScript.php
+++ interface/weno/jsonScript.php
@@ -5,7 +5,7 @@
  * @package OpenEMR
  * @link    http://www.open-emr.org
  * @author  Sherwin Gaddis <sherwingaddis@gmail.com>
- * @author  Brady Miller 
+ * @author  Brady Miller
  * @copyright Copyright (c) 2016-2017 Sherwin Gaddis <sherwingaddis@gmail.com>
  * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
