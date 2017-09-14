059e6768fbf168930fd5712a355c69c07ab73541
Brady Miller <brady.g.miller@gmail.com>
2017-09-13 22:12:32 -0700

    Added stripe package

---
+++ vendor/stripe/stripe-php/lib/Event.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Stripe;
+
+/**
+ * Class Event
+ *
+ * @property string $id
+ * @property string $object
+ * @property string $api_version
+ * @property int $created
+ * @property mixed $data
+ * @property bool $livemode
+ * @property int $pending_webhooks
+ * @property string $request
+ * @property string $type
+ *
+ * @package Stripe
+ */
+class Event extends ApiResource
+{
+    /**
+     * @param array|string $id The ID of the event to retrieve, or an options
+     *     array containing an `id` key.
+     * @param array|string|null $opts
+     *
+     * @return Event
+     */
+    public static function retrieve($id, $opts = null)
+    {
+        return self::_retrieve($id, $opts);
+    }
+
+    /**
+     * @param array|null $params
+     * @param array|string|null $opts
+     *
+     * @return Collection of Events
+     */
+    public static function all($params = null, $opts = null)
+    {
+        return self::_all($params, $opts);
+    }
+}
