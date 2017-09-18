05d33832c50e2e29a2c2a9c2c36d332f412d3c92
shachar058 <shaharzi@matrix.co.il>
2017-09-18 08:32:41 +0100

    fix for saving encounters and forms (#1087)
    * fix for saving encounters and forms
    
    * commented out console log

--- interface/forms/newpatient/save.php
+++ interface/forms/newpatient/save.php
@@ -174,9 +174,23 @@ $result4 = sqlStatement("SELECT fe.encounter,fe.date,openemr_postcalendar_catego
      top.window.parent.left_nav.setPatientEncounter(EncounterIdArray,EncounterDateArray,CalendarCategoryArray);
  top.restoreSession();
 <?php if ($mode == 'new') { ?>
- parent.left_nav.setEncounter(<?php echo "'" . attr(oeFormatShortDate($date)) . "', '" . attr($encounter) . "', window.name"; ?>);
+    if(parent.left_nav) {
+        parent.left_nav.setEncounter(<?php echo "'" . attr(oeFormatShortDate($date)) . "', '" . attr($encounter) . "', window.name"; ?>);
+        //console.log('new - parent.left_nav is defined');
+    }
+    else {
+        parent.parent.frames["left_nav"].setEncounter(<?php echo "'" . attr(oeFormatShortDate($date)) . "', '" . attr($encounter) . "', window.name"; ?>);
+        //console.log('new - parent.left_nav is undefined');
+    }
 <?php } // end if new encounter ?>
- parent.left_nav.loadFrame('enc2', window.name, '<?php echo $nexturl; ?>');
+    if(parent.left_nav){
+        parent.left_nav.loadFrame('enc2', window.name, '<?php echo $nexturl; ?>');
+        //console.log('modify - parent.left_nav is defined');
+    }
+    else {
+        parent.parent.frames["left_nav"].loadFrame('enc2', parent.name, '<?php echo $nexturl; ?>');
+        //console.log('modify - parent.left_nav is undefined');
+    }
 </script>
 
 </body>
