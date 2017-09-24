3d16325aeb382118fd7c711fdc18d20f7664dbf0
Amiel Elboim <amielel@matrix.co.il>
2017-09-24 16:08:14 +0300

    fix code style

--- interface/patient_file/deleter.php
+++ interface/patient_file/deleter.php
@@ -32,7 +32,7 @@ require_once($GLOBALS['srcdir'].'/acl.inc');
 require_once($GLOBALS['srcdir'].'/sl_eob.inc.php');
 
  $patient     = $_REQUEST['patient'];
- $encounterid = filter_var($_REQUEST['encounterid'],FILTER_VALIDATE_INT);
+ $encounterid = filter_var($_REQUEST['encounterid'], FILTER_VALIDATE_INT);
  $formid      = $_REQUEST['formid'];
  $issue       = $_REQUEST['issue'];
  $document    = $_REQUEST['document'];
