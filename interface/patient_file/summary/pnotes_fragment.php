94df1459c593ad318b2f5883e8ba0afff2d8c7f6
shahar zi <shaharzi@matrix.co.il>
2017-11-15 11:39:03 +0200

    tabs shown if offsite portal enabled

--- interface/patient_file/summary/pnotes_fragment.php
+++ interface/patient_file/summary/pnotes_fragment.php
@@ -35,6 +35,12 @@
  }
 
 ?>
+<?php if($GLOBALS['portal_offsite_enable'] == 1){ ?>
+<ul class="tabNav">
+  <li class="current" ><a href="#"><?php echo htmlspecialchars(xl('Inbox'), ENT_NOQUOTES); ?></a></li>
+  <li><a href="#"><?php echo htmlspecialchars(xl('Sent Items'), ENT_NOQUOTES); ?></a></li>
+</ul>
+<?php } ?>
 <div class='tabContainer' >
   <div class='tab current' >
     <?php
@@ -137,6 +143,91 @@
         <br/><?php
     } ?>
     </div>
+    <?php if($GLOBALS['portal_offsite_enable'] == 1){ ?>
+        <div class='tab'>
+            <?php
+            //display all of the notes for the day, as well as others that are active from previous dates, up to a certain number, $N
+            $M = 3; ?>
+            <br/>
+            <?php
+            $has_sent_note = 0;
+            if (!$thisauth) {
+                echo "<p>(" . htmlspecialchars(xl('Notes not authorized'), ENT_NOQUOTES) . ")</p>\n";
+            } else { ?>
+                <table width='100%' border='0' cellspacing='1' cellpadding='1' style='border-collapse:collapse;' >
+                    <?php
+                    //retrieve all active notes
+                    $result_sent = getSentPnotesByDate(
+                        "",
+                        1,
+                        "id,date,body,user,title,assigned_to,pid",
+                        $pid,
+                        "$M",
+                        0,
+                        '',
+                        $docid
+                    );
+                    if ($result_sent != null) {
+                        $notes_sent_count = 0;//number of notes so far displayed
+                        echo "<tr class='text' style='border-bottom:2px solid #000;' >\n";
+                        echo "<td valign='top' class='text' ><b>". htmlspecialchars(xl('To'), ENT_NOQUOTES) ."</b></td>\n";
+                        echo "<td valign='top' class='text' ><b>". htmlspecialchars(xl('Date'), ENT_NOQUOTES) ."</b></td>\n";
+                        echo "<td valign='top' class='text' ><b>". htmlspecialchars(xl('Subject'), ENT_NOQUOTES) ."</b></td>\n";
+                        echo "<td valign='top' class='text' ><b>". htmlspecialchars(xl('Content'), ENT_NOQUOTES) ."</b></td>\n";
+                        echo "</tr>\n";
+                        foreach ($result_sent as $iter) {
+                            $has_sent_note = 1;
+                            $body = $iter['body'];
+                            if (preg_match('/^\d\d\d\d-\d\d-\d\d \d\d\:\d\d /', $body)) {
+                                $body = nl2br(htmlspecialchars(oeFormatPatientNote($body), ENT_NOQUOTES));
+                            } else {
+                                $body = htmlspecialchars(oeFormatSDFT(strtotime($iter['date'])) . date(' H:i', strtotime($iter['date'])) .
+                                        ' (' . $iter['user'] . ') ', ENT_NOQUOTES) .
+                                    nl2br(htmlspecialchars(oeFormatPatientNote($body), ENT_NOQUOTES));
+                            }
+
+                            $body = preg_replace('/(:\d{2}\s\()'.$iter['pid'].'(\sto\s)/', '${1}'.$patientname.'${2}', $body);
+                            $body = strlen($body) > 120 ? substr($body, 0, 120)."<b>.......</b>" : $body;
+                            echo " <tr class='text' id='".htmlspecialchars($iter['id'], ENT_QUOTES)."' style='border-bottom:1px dashed;height:30px;' >\n";
+                            // Modified 6/2009 by BM to incorporate the patient notes into the list_options listings
+                            echo "<td valign='top' class='text'>".htmlspecialchars($iter['assigned_to'], ENT_NOQUOTES)."</td>\n";
+                            echo "<td valign='top' class='text'>".htmlspecialchars($iter['date'], ENT_NOQUOTES)."</td>\n";
+                            echo "  <td valign='top' class='text'><b>";
+                            echo generate_display_field(array('data_type'=>'1','list_id'=>'note_type'), $iter['title']);
+                            echo "</b></td>\n";
+                            echo "  <td valign='top' class='text'>$body</td>\n";
+                            echo " </tr>\n";
+                            $notes_sent_count++;
+                        }
+                    } ?>
+                </table>
+                <?php
+                if ($has_sent_note < 1) { ?>
+                    <span class='text'>
+                    <?php
+                    echo xlt("There are no notes on file for this patient.");
+                    if (acl_check('patients', 'notes', '', array('write', 'addonly'))) {
+                        echo " ";
+                        echo "<a href='pnotes_full.php' onclick='top.restoreSession()'>";
+                        echo xlt("To add notes, please click here");
+                        echo "</a>.";
+                    }
+                    ?>
+                    </span><?php
+                } else { ?>
+                    <br/>
+                    <span class='text'>
+        <?php echo htmlspecialchars(xl('Displaying the following number of most recent notes'), ENT_NOQUOTES).":"; ?>
+                        <b><?php echo $M;?></b><br>
+        <a href='pnotes_full.php?s=1' onclick='top.restoreSession()'><?php echo htmlspecialchars(xl('Click here to view them all.'), ENT_NOQUOTES); ?></a>
+        </span>
+                    <?php
+                } ?>
+                <br/>
+                <br/><?php
+            } ?>
+        </div>
+    <?php } ?>
 </div>
 
 <script language="javascript">
