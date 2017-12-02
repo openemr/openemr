fc3f9175c6de07b637cea8006f802b02e9213913
Brady Miller <brady.g.miller@gmail.com>
2017-12-01 16:19:04 -0800

    bug fix when modifying external codes

--- interface/patient_file/encounter/superbill_custom_full.php
+++ interface/patient_file/encounter/superbill_custom_full.php
@@ -156,7 +156,7 @@ if (isset($mode)) {
   }
   // If codes history is enabled in the billing globals save data to codes history table
   if  ($GLOBALS['save_codes_history'] && $alertmsg=='' &&
-      ( $mode == "modify" || $mode == "add" || $mode == "modify_complete" || $mode == "delete" ) ){
+      ( $mode == "add" || $mode == "modify_complete" || $mode == "delete" ) ){
 
       $action_type= empty($_POST['code_id']) ? 'new' : $mode;
       $action_type= ($action_type=='add') ? 'update' : $action_type ;
