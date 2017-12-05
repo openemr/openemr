62f825f7f0e3a8ceea6a22a79b7534e8a2d818e0
ophthal <magauran@ophthal.org>
2017-12-05 15:40:21 -0500

    Bootstrap and Modals
    Signed-off-by: ophthal <magauran@ophthal.org>

--- library/MedEx/API.php
+++ library/MedEx/API.php
@@ -1928,7 +1928,7 @@ class Display extends base
                 </form>
             </div>
             <div class="row-fluid text-center">
-                <input class="btn" onclick="add_this_recall();" value="<?php echo xla('Add Recall'); ?>" id="add_new" name="add_new">
+                <input class="btn btn-info" onclick="add_this_recall();" value="<?php echo xla('Add Recall'); ?>" id="add_new" name="add_new">
                 <p>
                     <em class="small text-muted">* <?php echo xlt('N.B.{{Nota bene}}')." ".xlt('Demographic changes made here are recorded system-wide'); ?>.</em>
                 </p>
@@ -2372,8 +2372,9 @@ class Setup extends Base
                         </div>
                     </div>
                     <div class="text-center row showReminders">
-                        <input value="<?php echo xla('Sign-up'); ?>" onclick="goReminderRecall('setup&stage=2');" class="btn">
+                        <input value="<?php echo xla('Sign-up'); ?>" onclick="goReminderRecall('setup&stage=2');" class="btn btn-info">
                     </div>
+
                 </div>
             </div>
         </div>
@@ -2383,14 +2384,11 @@ class Setup extends Base
         ?>
         <div class="row">
             <form name="medex_start" id="medex_start">
-                <div class="col-sm-1"></div>
-                <div class="col-sm-10 text-center">
+                <div class="col-sm-10 col-sm-offset-1 text-center">
                     <div id="setup_1" class="showReminders borderShadow">
                         <div class="title row fa"><?php echo xlt('Register'); ?>: MedEx Bank</div>
                         <div class="row showReminders">
-                            <div class="col-sm-1">
-                            </div>
-                            <div class="fa col-sm-10 text-center">
+                            <div class="fa col-sm-10 col-sm-offset-1 text-center">
                                 <div class="divTable4 fa" id="answer" name="answer">
                                     <div class="divTableBody">
                                         <div class="divTableRow">
@@ -2411,8 +2409,7 @@ class Setup extends Base
                                             <div class="divTableCell"><i id="pwd_check" name="pwd_check" class="top_right_corner nodisplay red fa fa-check"></i>
                                                 <i class="fa top_right_corner fa-question" id="pwd_ico_help" aria-hidden="true" onclick="$('#pwd_help').toggleClass('nodisplay');"></i>
                                                 <input type="password" placeholder="<?php xla('Password'); ?>" id="new_password" name="new_password" class="form-control" required> 
-                                                <br />
-                                                <div id="pwd_help" class="nodisplay signup_help"><?php echo xlt('8-12 characters long, including at least one upper case letter, one lower case letter, one number and one special character'); ?></div>
+                                                <div id="pwd_help" class="nodisplay signup_help"><?php echo xlt('Secure Password Required').": ". xlt('8-12 characters long, including at least one upper case letter, one lower case letter, one number, one special character and no common strings'); ?>...</div>
                                             </div>
                                         </div>
                                         <div class="divTableRow">
@@ -2421,7 +2418,6 @@ class Setup extends Base
                                             </div>
                                             <div class="divTableCell"><i id="pwd_rcheck" name="pwd_rcheck" class="top_right_corner nodisplay red fa fa-check"></i>
                                                 <input type="password" placeholder="<?php echo xla('Repeat password'); ?>" id="new_rpassword" name="new_rpassword" class="form-control" required>
-                                                        <br />
                                                 <div id="pwd_rhelp" class="nodisplay signup_help" style=""><?php echo xlt('Passwords do not match.'); ?></div>
                                             </div>
                                         </div>
@@ -2429,23 +2425,52 @@ class Setup extends Base
                                 </div>
                                 <div id="ihvread" name="ihvread" class="fa text-left">
                                     <input type="checkbox" class="updated required" name="TERMS_yes" id="TERMS_yes" required>
-                                    <label for="TERMS_yes" class="input-helper input-helper--checkbox" title="<?php echo xla('Terms and Conditions'); ?>"><?php echo xlt('I have read and my practice agrees to the'); ?> 
+                                    <label for="TERMS_yes" class="input-helper input-helper--checkbox" title="Terms and Conditions"><?php echo xlt('I have read and my practice agrees to the'); ?> 
                                         <a href="#" onclick="cascwin('https://medexbank.com/cart/upload/index.php?route=information/information&information_id=5','TERMS',800, 600);">MedEx <?php echo xlt('Terms and Conditions'); ?></a></label><br />
                                     <input type="checkbox" class="updated required" name="BusAgree_yes" id="BusAgree_yes" required>
-                                    <label for="BusAgree_yes" class="input-helper input-helper--checkbox" title="<?php echo xla('HIPAA Agreement'); ?>"><?php echo xlt('I have read and accept the'); ?> 
+                                    <label for="BusAgree_yes" class="input-helper input-helper--checkbox" title="BAA"><?php echo xlt('I have read and accept the'); ?> 
                                     <a href="#" onclick="cascwin('https://medexbank.com/cart/upload/index.php?route=information/information&information_id=8','Bus Assoc Agree',800, 600);">MedEx <?php echo xlt('Business Associate Agreement'); ?></a></label>
                                     <br />
-                    <div class="align-center row showReminders">
-                                    <input value="Register" class="btn" onclick="signUp();" value="<?php echo xlt('Register'); ?>" />
-                    </div>
-                </div>
+                                    <div class="align-center row showReminders">
+                                        <input id="Register" class="btn btn-info" value="<?php echo xla('Register'); ?>" />
+                                    </div>
+
+                                    <div id="myModal" class="modal fade" role="dialog">
+                                      <div class="modal-dialog">
+
+                                        <!-- Modal content-->
+                                        <div class="modal-content">
+                                          <div class="modal-header"  style="background-color: #0d4867;color: #fff;font-weight: 700;">
+                                            <button type="button" class="close" data-dismiss="modal" style="color:#fff;opacity:1;box-shadow:unset !important;">&times;</button>
+                                            <h2 class="modal-title" style="font-weight:600;">Sign-Up Confirmation</h2>
+                                          </div>
+                                          <div class="modal-body" style="padding: 10px 45px;">
+                                            <p>You are opening a secure connection to MedExBank.com to create your account.<br />
+                                               By proceeding, you agree to the MedEx Terms and Conditions and formally execute <br />
+                                               the Business Associate Agreement with MedEx on behalf of your practice.  <br />
+                                               During this step your EHR will exchange Practice data with the MedEx servers.  <br />
+                                                <br />
+                                                You will them be directed to login at MedExBank.com to:
+                                                <ul style="text-align: left;width: 90%;margin: 0 auto;">
+                                                    <li> confirm your practice and providers' information</li>
+                                                    <li> choose your service options</li>
+                                                    <li> update and activate your message templates</li>
+                                                </ul>
+                                            </p>
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                            <button type="button" class="btn btn-default" onlick="actualSignUp();" id="actualSignUp">Proceed</button>
+                                          </div>
+                                        </div>
+
+                                      </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-1"></div>
                         </div>
                     </div>
-                    <div class="col-sm-1"></div>
                 </div>
-                <div class="col-sm-1"></div>
             </form>
         </div>
         <script>
@@ -2459,30 +2484,10 @@ class Setup extends Base
                 if (!$("#TERMS_yes").is(':checked')) return alert('<?php echo xlt('You must agree to the Terms & Conditions before signing up');?>... ');
                 if (!$("#BusAgree_yes").is(':checked')) return alert('<?php echo xlt('You must agree to the HIPAA Business Associate Agreement');?>... ');
                 //No translation for now - unless you can get it translated without breaking the Auto-Registration...
-                if (confirm(" CONFIRM: You are opening a secure connection to MedExBank.com to create your account.\n\nBefore your practice can send live messages, you will need to login to MedExBank.com to:\n     confirm your practice information\n     choose your service options\n     create your desired SMS/Voice and/or e-mail messages."))
-                    {
-                    var url = "save.php?MedEx=start";
-                    formData = $("form#medex_start").serialize();
-                    top.restoreSession();
-                    $.ajax({
-                        type   : 'POST',
-                        url    : url,
-                        data   : formData 
-                        })
-                    .done(function(result) {
-                            obj = JSON.parse(result);
-                            $("#answer").html(obj.show);
-                            $("#ihvread").addClass('nodisplay');
-                                
-                            if (obj.success) {
-                                $("#butme").html('<a href="messages.php?go=Preferences"><?php echo xlt('Preferences'); ?></a>');
-                                url="https://www.medexbank.com/cart/upload/";
-                                window.open(url, 'clinical', 'resizable=1,scrollbars=1');
-                refresh_me();
-                            }
-                    });
-                }
+                
+                $("#myModal").modal();
             }
+            
             function validateEmail(email) {
                 var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 return re.test(email);
@@ -2584,6 +2589,33 @@ class Setup extends Base
                 return true;
             }
             $(document).ready(function() {
+                $("#Register").click(function() {
+                     signUp();
+                });
+                $("#actualSignUp").click(function() {
+                    var url = "save.php?MedEx=start";
+                    var email = $("#new_email").val();
+                    $("#actualSignUp").html('<i class="fa fa-spinner fa-pulse fa-2x fa-fw"></i><span class="sr-only">Loading...</span>');
+                    formData = $("form#medex_start").serialize();
+                    top.restoreSession();
+                    $.ajax({
+                        type   : 'POST',
+                        url    : url,
+                        data   : formData 
+                        })
+                    .done(function(result) {
+                        obj = JSON.parse(result);
+                        $("#answer").html(obj.show);
+                        $("#ihvread").addClass('nodisplay');
+                        $('#myModal').modal('toggle');
+                        if (obj.success) {
+                            $("#butme").html('<a href="messages.php?go=Preferences"><?php echo xlt('Preferences'); ?></a>');
+                            url="https://www.medexbank.com/login/"+email;
+                            window.open(url, 'clinical', 'resizable=1,scrollbars=1');
+                        refresh_me();
+                                }
+                        });
+                });
                 $("#new_email").blur(function(e) {
                                     e.preventDefault();
                                     var email = $("#new_email").val();
