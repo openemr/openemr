55b1e82499ae905f7bc4a82c6ba475886f92585b
Vaibhav Gupta <vaibhav.gupta@krishitech.internal>
2017-12-06 15:57:25 +0530

    more indianisation plus setupscript

--- setupscript.sh
+++ setupscript.sh
@@ -2,6 +2,15 @@
 
 set -x
 
-sudo chmod 666 /var/www/html/sites/default/sqlconf.php
+sudo chmod 666 \
+'/var/www/html/sites/default/sqlconf.php' \
+'/var/www/html/interface/modules/zend_modules/config/application.config.php'
 
-sudo chmod 666 /var/www/html/interface/modules/zend_modules/config/application.config.php
+sudo chown -R www-data \
+'/var/www/html/sites/default/documents' \
+'/var/www/html/sites/default/edi' \
+'/var/www/html/sites/default/era' \
+'/var/www/html/sites/default/letter_templates' \
+'/var/www/html/gacl/admin/templates_c' \
+'/var/www/html/interface/main/calendar/modules/PostCalendar/pntemplates/compiled' \
+'/var/www/html/interface/main/calendar/modules/PostCalendar/pntemplates/cache'
