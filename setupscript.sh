05ba0efeefbb96e4ca4266d0555b590c9f6c6607
vaibhavgupta3110 <vaibhav.gupta@me.com>
2017-12-13 14:14:51 +0530

    FINALISE SETUPSCRIPT

--- setupscript.sh
+++ setupscript.sh
@@ -3,14 +3,14 @@
 set -x
 
 sudo chmod 666 \
-'/var/www/html/sites/default/sqlconf.php' \
+'/var/www/html/sites/*/sqlconf.php' \
 '/var/www/html/interface/modules/zend_modules/config/application.config.php'
 
 sudo chown -R www-data \
-'/var/www/html/sites/default/documents' \
-'/var/www/html/sites/default/edi' \
-'/var/www/html/sites/default/era' \
-'/var/www/html/sites/default/letter_templates' \
+'/var/www/html/sites/*/documents' \
+'/var/www/html/sites/*/edi' \
+'/var/www/html/sites/*/era' \
+'/var/www/html/sites/*/letter_templates' \
 '/var/www/html/gacl/admin/templates_c' \
 '/var/www/html/interface/main/calendar/modules/PostCalendar/pntemplates/compiled' \
 '/var/www/html/interface/main/calendar/modules/PostCalendar/pntemplates/cache'
