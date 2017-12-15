5ae39fcc6af3aa878c92cdc028517cb0be2dffa4
Brady Miller <brady.g.miller@gmail.com>
2017-12-15 02:54:47 -0800

    minor change to prior commit for 5.0.1-dev

--- portal/patient/fwk/libs/verysimple/DB/DataDriver/MySQLi.php
+++ portal/patient/fwk/libs/verysimple/DB/DataDriver/MySQLi.php
@@ -99,10 +99,10 @@ class DataDriverMySQLi implements IDataDriver
                 );
             }
         }
-        if (!defined(MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)) {
-            define(MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT, 64);
-        }
         if ($mysqlSsl) {
+            if (!defined(MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)) {
+                define(MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT, 64);
+            }
             $ok = mysqli_real_connect(
                 $connection,
                 $host,
