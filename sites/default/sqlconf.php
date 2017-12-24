81d36347846018f9cdee36ee462142e541bad460
eyalvo-matrix <eyalvo.matrix@gmail.com>
2017-12-24 10:02:35 +0200

    Update sqlconf.php

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -6,7 +6,7 @@ $host	= 'localhost';
 $port	= '3306';
 $login	= 'openemr';
 $pass	= 'openemr';
-$dbase	= 'fixfixgoroups';
+$dbase	= 'openemr';
 
 //Added ability to disable
 //utf8 encoding - bm 05-2009
