d3bffc87fd11107c6fe38683fb87f3df23357392
vaibhavgupta3110 <vaibhav.gupta@me.com>
2018-01-29 07:39:28 +0530

    updare

--- setupscript.sh
+++ setupscript.sh
@@ -1,6 +1,19 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
 set -x
+
+sudo apt-get update && sudo apt-get -y upgrade \
+sudo apt-get -y autoremove && sudo apt-get -y update
+
+sudo apt-get install \ build-essential apache2 git tar curl \
+python openssl git libffi-dev py-pip python-dev build-base openssl-dev \
+dcron vim nano bash bash-doc bash-completion tree curl \ apache2
+mariadb-server libapache2-mod-php libtiff-tools php php-mysql php-cli \
+php-gd php-xsl php-curl php=mcrypt php-soap php-json php-gettext imagemagick \
+php-mbstring php-zip php-ldap php-xml
+
+
+sudo git clone https://github.com/vaibhavgupta3110/openemr.git -b test /var/www/html
 
 # openemr's setup requireemnts
 
