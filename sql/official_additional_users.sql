78e6971ac60aa67af378fc443af8d9d16f4676b1
vaibhavgupta3110 <vaibhav.gupta@me.com>
2018-01-30 22:19:31 +0530

    vg update
    hello moto

--- sql/official_additional_users.sql
+++ sql/official_additional_users.sql
@@ -1,14 +1,3 @@
 -- DO NOT TOUCH
-
 INSERT INTO `users` (username,password,lname,authorized,active) VALUES ('phimail-service','NoLogin','phiMail Gateway',0,0);
 INSERT INTO `users` (username,password,lname,authorized,active) VALUES ('portal-user','NoLogin','Patient Portal User',0,0);
-
--- INSERT YOUR VALUES HERE
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
