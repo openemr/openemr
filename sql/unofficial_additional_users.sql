fa879d9c591d324f2ed9f4b35008178f042a7dcd
kk-47 <34308328+kk-47@users.noreply.github.com>
2018-02-09 08:18:53 +0530

    Update unofficial_additional_users.sql

--- sql/unofficial_additional_users.sql
+++ sql/unofficial_additional_users.sql
@@ -1,5 +1,5 @@
 -- INSERT YOUR VALUES BETWEEN THE ''
-INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
+INSERT INTO 'Dr. Prajwal rao' (username,password,lname,authorized,active) VALUES ('prao','prao123','Rao',1,1);
 INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
 INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
 INSERT INTO 'users' (username,password,lname,authorized,active) VALUES ('','','',1,1);
