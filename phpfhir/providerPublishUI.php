2ef5396bf607b41af19ea98d3237f41415bcc557
Brady Miller <brady.g.miller@gmail.com>
2018-02-14 22:49:27 -0800

    minor fhir code updates

--- phpfhir/providerPublishUI.php
+++ phpfhir/providerPublishUI.php
@@ -1,6 +1,15 @@
 <?php
-// Temporary rest test interface until add a model
-//$ignoreAuth = true;
+/**
+ * Provider publish fhir UI
+ * (Temporary rest test interface until add a model)
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Jerry Padgett <sjpadgett@gmail.com>
+ * @copyright Copyright (c) 2018 Jerry Padgett <sjpadgett@gmail.com>
+ * @license   https://www.gnu.org/licenses/agpl-3.0.en.html GNU Affero General Public License 3
+ */
+
 require_once("../interface/globals.php");
 require_once("./libs/controller/ClientAppController.php");
 require 'vendor/autoload.php';
@@ -10,7 +19,7 @@ use OpenEMR\Core\Header;
 // kick off app endpoints controller
 $clientApp = new clientController();
 
-echo "<script>var pid='" . $pid . "'</script>";
+echo "<script>var pid='" . attr($pid) . "'</script>";
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,41 +49,41 @@ echo "<script>var pid='" . $pid . "'</script>";
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                     data-target="#nav-header-collapse">
-                <span class="sr-only">Toggle</span>
+                <span class="sr-only"><?php echo xlt('Toggle'); ?></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="#">
-                oeFHIR
+                <?php echo xlt('oeFHIR'); ?>
             </a>
         </div>
         <div class="collapse navbar-collapse" id="nav-header-collapse">
             <form class="navbar-form navbar-left" method="GET" role="search">
                 <div class="form-group">
-                    <input type="text" name="q" class="form-control" placeholder="Search">
+                    <input type="text" name="q" class="form-control" placeholder="<?php echo xla('Search'); ?>">
                 </div>
                 <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
             </form>
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown ">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                        Activity
+                        <?php echo xlt('Activity'); ?>
                         <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li class="dropdown-header">Profiles</li>
-                        <li class=""><a href="#">CCD</a></li>
-                        <li class=""><a href="#">Care Plan</a></li>
-                        <li class=""><a href="#">Episode</a></li>
+                        <li class="dropdown-header"><?php echo xlt('Profiles'); ?></li>
+                        <li class=""><a href="#"><?php echo xlt('CCD'); ?></a></li>
+                        <li class=""><a href="#"><?php echo xlt('Care Plan'); ?></a></li>
+                        <li class=""><a href="#"><?php echo xlt('Episode'); ?></a></li>
                         <li class="divider"></li>
-                        <li class="dropdown-header">Resources</li>
-                        <li class=""><a href="#">Patient</a></li>
-                        <li class=""><a href="#">Organization</a></li>
+                        <li class="dropdown-header"><?php echo xlt('Resources'); ?></li>
+                        <li class=""><a href="#"><?php echo xlt('Patient'); ?></a></li>
+                        <li class=""><a href="#"><?php echo xlt('Organization'); ?></a></li>
                         <li class="divider"></li>
-                        <li><a href="#">Server Login</a></li>
+                        <li><a href="#"><?php echo xlt('Server Login'); ?></a></li>
                     </ul>
                 </li>
-                <li><a href="https://fhirtest.uhn.ca" target="_blank">Visit Test Server</a></li>
+                <li><a href="https://fhirtest.uhn.ca" target="_blank"><?php echo xlt('Visit Test Server'); ?></a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
@@ -82,18 +91,18 @@ echo "<script>var pid='" . $pid . "'</script>";
 <div class="container-fluid main-container">
     <div class="col-md-2 sidebar">
         <ul class="nav nav-pills nav-stacked">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a onclick="doPublish(event, 'create')" href="#">Publish</a></li>
-            <li><a onclick="doPublish(event, 'read')" href="#">Read</a></li>
-            <li><a onclick="doPublish(event, 'history')" href="#">Get History</a></li>
-            <li><a onclick="alert('Not Implemented');return false;" href="#">Search</a></li>
+            <li class="active"><a href="#"><?php echo xlt('Home'); ?></a></li>
+            <li><a onclick="doPublish(event, 'create')" href="#"><?php echo xlt('Publish'); ?></a></li>
+            <li><a onclick="doPublish(event, 'read')" href="#"><?php echo xlt('Read'); ?></a></li>
+            <li><a onclick="doPublish(event, 'history')" href="#"><?php echo xlt('Get History'); ?></a></li>
+            <li><a onclick="alert('Not Implemented');return false;" href="#"><?php echo xlt('Search'); ?></a></li>
             <li><a href="#"></a></li>
         </ul>
     </div>
     <div class="col-md-10 content">
         <div class="panel panel-default">
             <div class="panel-heading">
-                Dashboard
+                <?php echo xlt('Dashboard'); ?>
             </div>
             <div id="dashboard" class="panel-body">
             </div>
