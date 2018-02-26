00ccafd662f14d483915a5231d252733c48219a7
Amiel Elboim <amielel@matrix.co.il>
2018-02-26 10:32:17 +0200

    make comstat_id in the postcalendar_categories table unique

--- sql/5_0_0-to-5_0_1_upgrade.sql
+++ sql/5_0_0-to-5_0_1_upgrade.sql
@@ -671,6 +671,10 @@ ALTER TABLE `openemr_postcalendar_categories` ADD `pc_constant_id` VARCHAR (255)
 UPDATE `openemr_postcalendar_categories` SET pc_constant_id = LOWER(REPLACE (pc_catname,' ', '_'));
 #EndIf
 
+#IfNotIndex openemr_postcalendar_categories pc_constant_id
+ALTER TABLE openemr_postcalendar_categories ADD UNIQUE (`pc_constant_id`);
+#EndIf
+
 #IfMissingColumn facility facility_taxonomy
 ALTER TABLE facility ADD facility_taxonomy VARCHAR(15) DEFAULT NULL;
 #EndIf
