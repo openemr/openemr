cb06c4b31a6530ccfbfcbf58a49dcc77cb966fd5
Amiel Elboim (Matrix) <amielel@matrix.co.il>
2018-03-26 10:25:21 +0300

    New ACL for documents deleting (#1520)

--- version.php
+++ version.php
@@ -33,7 +33,7 @@ $v_database = 250;
 // controls is (subsequently the acl_upgrade.php script then is used to
 // upgrade and track this value)
 //
-$v_acl = 7;
+$v_acl = 8;
 
 //Offsite Portal SOAP functions version, which are at myportal directory.
 $v_offsite_portal='1.47';
