cd5f5c60b6b4cfabcc866045487929e05f5457ca
Jerry Padgett <sjpadgett@gmail.com>
2018-04-19 22:46:59 -0400

    CCM and CCDA local service updates (#1564)
    * Removed background service ssmanager and add on-demand ccda service start.
    Added/fixed cda templates in document generator.
    Security fixes in dependency libraries to support document generator.
    Easier install
    
    * Fix encounter ccda not importing. Add local ccda service start up.

--- ccdaservice/README.md
+++ ccdaservice/README.md
@@ -1,61 +1,39 @@
 # Prepare
 
-* Import to database, table background_services: ccdaservice/support/background_services.sql - Now included in database.sql and upgrade 5.0.1
 * If not already completed, you must install the CCM features using the Modules->Manage Modules click Unregistered tab.
 * The Carecoordination module setting for Mirth IP must be set to localhost. Menu item: Modules->Manage Modules click in Carecoordination Config, the settings tab.
 * Ignore any errors that do not throw you back to command prompt when using npm install as some libs need to be compiled and is verbose about it.
 
-## Ubuntu
-
+## Ubuntu Setup
+- cd ~
 - apt-get remove --purge nodejs npm // Ensures clean install and will allow chance to cleanup.
-
-- curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-
+- curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 - sudo apt-get install -y nodejs
 
 Navigate to: openemr/ccdaservice and run the following to install requires dependencies.
 
-- sudo npm install libxmljs -g
-
-- sudo npm link libxmljs
-
 - sudo npm install --production
 
-- sudo npm i node-linux // Not in use at moment - ssmanager handles service process.
 
-* If the service is not installed in last step the ssmanager, started by backgroud services, will still start and maintain ccda service.
-* For portal this is not necessarily true.
+## Windows Setup
 
-## Windows
-
-* Download and install node v6.9.5 or 6.1 for your windows version.
+* Download and install nodejs v9.4 for your windows version.
 * Ensure system variable NODE_PATH is set i.e %USERPROFILE%\AppData\Roaming\npm\node_modules
 
-####  - Important to start Node.js cmd prompt as adminisrator.
-
 Navigate to: openemr/ccdaservice and run the following:
-
-- npm install libxmljs -g
-
-- npm install node-windows -g
-
-- npm link libxmljs
-
--npm link node-windows
-
+- npm install --global --production windows-build-tools
 - npm install --production
-
-* The service will automatically be installed when OpenEMR is log into the first time and with both installs, ccda service is monitored by ssmanager started by background services.
+## Use
+* CCDA service must be turned on in OpenEMR menu Globals->Connectors.
+* The service will automatically start on demand by application.
 
 ## Developing
 * Note that these scripts run in strict mode so javascript will hold you very much accountable with how objects and variables are handled.
-* Important to note that during developement error checking is relaxed, so after CCM sends xml, it waits to receive back document and if document generation errors for some reason either CCM waits till php timeout or you need to restart apache to clear that socket. 99% of the time this is not an issue however, currently CCM doesn't do any supervision on socket communication thus sends and waits. I hate timers but may be solution here.
-* For now node modules are run local to service directory so all support dependecies are installed there (except libxmljs & node-windows).
+
+* For now node modules are run local to service directory so all support dependecies are installed there.
 
 ## Tools
-* node winservice is service install/start both Windows and Ubuntu (not currently used in Ubuntu)
-* node unservice is service uninstall
-
+* The nodejs ccda service now starts on demand.
 ## License
-   		Copyright 2017 sjpadgett@gmail.com
+   		Copyright 2018 sjpadgett@gmail.com
 		GNU GPL
--- ccdaservice/ccda_gateway.php
+++ ccdaservice/ccda_gateway.php
@@ -32,9 +32,9 @@ if (isset($_SESSION['pid']) && isset($_SESSION['patient_portal_onsite_two'])) {
     session_destroy();
     $ignoreAuth = false;
     require_once(dirname(__FILE__) . "/../interface/globals.php");
-    if (! isset($_SESSION['authUserID'])) {
+    if (!isset($_SESSION['authUserID'])) {
         $landingpage = "index.php";
-        header('Location: '.$landingpage);
+        header('Location: ' . $landingpage);
         exit;
     }
 
@@ -44,10 +44,9 @@ if (isset($_SESSION['pid']) && isset($_SESSION['patient_portal_onsite_two'])) {
 
 // give me something to do.
 $dowhat = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
-if ($dowhat && $GLOBALS['ccda_alt_service_enable'] >= 1) { // do I need this?
-    require_once("./../ccdaservice/ssmanager.php");
-    if (!runCheck()) { // woops, try again
-        if (!runCheck()) {
+if ($dowhat && $GLOBALS['ccda_alt_service_enable'] > 0) {
+    if (!checkService()) { // woops, try again
+        if (!checkService()) { // thats 10 seconds of wasted time.
             die("Document service start failed. Click back to return home."); // nuts! give up
         }
     }
@@ -62,43 +61,40 @@ $parameterArray ['encounter'];
 $parameterArray ['combination'] = $pid;
 $parameterArray ['components']; // = 'progress_note|consultation_note|continuity_care_document|diagnostic_image_reporting|discharge_summary|history_physical_note|operative_note|procedure_note|unstructured_document';
 $parameterArray ['sections']; // = 'allergies|medications|problems|immunizations|procedures|results|plan_of_care|vitals|social_history|encounters|functional_status|referral|instructions';
-$parameterArray ['downloadccda']=1;
+$parameterArray ['downloadccda'] = 1;
 $parameterArray ['sent_by'];
 $parameterArray ['send'];
 $parameterArray ['view'] = 1;
 $parameterArray ['recipients'] = 'patient'; // emr_direct or hie else if not set $_SESSION['authUserID']
-$parameterArray [0] [6] = $_SESSION ['portal_username']; // set to an onsite portal user
+$parameterArray [0][6] = $_SESSION ['portal_username']; // set to an onsite portal user
 
-if (!isset($_SESSION ['site_id'])) {
-    $_SESSION ['site_id'] = 'default'; // do believe globals does this but I go rogue at times.
+if (!isset($_SESSION['site_id'])) {
+    $_SESSION ['site_id'] = 'default';
 }
 
-$server_url = 'http://localhost'. $GLOBALS['webroot'];  // I alias into openemr directory on my sights causing webroot to be empty.
-                                                                                            //I've have actually seen this return 'default' due to apache config'ed with localhost alias on more than one virtual host?? Watch
-//global $server_url; // can't find where this is defined!
+$server_url = $_SERVER['HTTP_HOST'] . $GLOBALS['webroot'];
 // CCM returns entire cda with service doing templates
 $ccdaxml = portalccdafetching($pid, $server_url, $parameterArray);
 // disposal decisions will be here.
-$h='';
+$h = '';
 if (!$parameterArray ['view']) {
     header('Content-Type: application/xml');
 } else {
-    $h='<a href="./../portal/home.php" </a><button style="color: red; background: white;" >' . xlt("Return Home") .'</button><br>';
+    $h = '<a href="./../portal/home.php" </a><button style="color: red; background: white;" >' . xlt("Return Home") . '</button><br>';
 }
 
-print_r($h.$ccdaxml.$h);
+print_r($h . $ccdaxml . $h);
 //service_shutdown(1); //In ssmanager  0= terminate and disable 1 = soft=terminate but still active w/no restart, > 1 just restart based on B.S timer
 exit;
 
 function portalccdafetching($pid, $server_url, $parameterArray)
 {
-    
     session_write_close();
-    $site_id = $_SESSION ['site_id'];
+    $site_id = $_SESSION['site_id'];
     $parameters = http_build_query($parameterArray); // future use
     try {
         $ch = curl_init();
-        $url = $server_url . "/interface/modules/zend_modules/public/encounterccdadispatch/index?site=$site_id&me=".session_id()."&param=1&view=1&combination=$pid&recipient=patient";
+        $url = $server_url . "/interface/modules/zend_modules/public/encounterccdadispatch/index?site=$site_id&me=" . session_id() . "&param=1&view=1&combination=$pid&recipient=patient";
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 0); // set true for look see
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
@@ -108,7 +104,7 @@ function portalccdafetching($pid, $server_url, $parameterArray)
         //curl_setopt ($ch, CURLOPT_COOKIE, 'XDEBUG_SESSION=1'); // break on first line in public/index.php - uncomment and start any xdebug session and fetch a ccda in app.
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            
+
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         $result = curl_exec($ch) or die(curl_error($ch));
@@ -117,6 +113,36 @@ function portalccdafetching($pid, $server_url, $parameterArray)
         return false;
     }
 
-        return $result;
+    return $result;
 }
+
+function checkService($ip = "localhost", $port = '6661')
+{
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    if ($socket === false) {
+        throw new Exception("Socket Creation Failed");
+    }
+
+    // Connect to the node server.
+    $result = socket_connect($socket, $ip, $port);
+    if ($result === false) {
+        $path = $GLOBALS['fileroot'] . "/ccdaservice";
+        if (IS_WINDOWS) {
+            $cmd = "node " . $path . "/serveccda.js";
+            pclose(popen("start /B " . $cmd, "r"));
+        } else {
+            $cmd = "nodejs " . $path . "/serveccda.js";
+            exec($cmd . " > /dev/null &");
+        }
+        sleep(2); // give cpu a rest
+        $result = socket_connect($socket, $ip, $port);
+        if ($result === false) { // hmm something is amist with service.
+            throw new Exception("Connection Failed");
+        }
+    }
+    socket_close($socket);
+    unset($socket);
+    return true;
+}
+
 return 0;
