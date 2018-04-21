d9a07e25ddfe51a2a2cee8f3ed48e824cec80010
Jerry Padgett <sjpadgett@gmail.com>
2018-04-21 15:42:44 -0400

    DICOM viewer mime detection update (#1570)

--- library/dicom_frame.php
+++ library/dicom_frame.php
@@ -60,21 +60,57 @@ $web_path .= '&retrieve&patient_id=' . attr($patid) . '&document_id=' . attr($do
         color: rgb(255, 0, 0);
         font-size: 1.5em;
     }
+
     .ui-autocomplete {
         position: absolute;
         top: 0;
         left: 0;
-        min-width:200px;
+        min-width: 200px;
         cursor: default;
     }
-    .ui-menu-item{
-        min-width:200px;
+
+    .ui-menu-item {
+        min-width: 200px;
     }
-    .fixed-height{
-        min-width:200px;
+
+    .fixed-height {
+        min-width: 200px;
         padding: 1px;
         max-height: 35%;
         overflow: auto;
+    }
+
+    .loader {
+        position: absolute;
+        left: 25%;
+        top: 15%;
+        z-index: 1;
+        border: 12px solid #f3f3f3;
+        border-radius: 50%;
+        border-top: 12px solid #3498db;
+        width: 60px;
+        height: 60px;
+        -webkit-animation: spin 2s linear infinite; /* Safari */
+        animation: spin 2s linear infinite;
+    }
+
+    /* Safari */
+    @-webkit-keyframes spin {
+        0% {
+            -webkit-transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
     }
 </style>
 <body>
@@ -82,14 +118,27 @@ $web_path .= '&retrieve&patient_id=' . attr($patid) . '&document_id=' . attr($do
 <div id="dwv" src='<?php echo $web_path ?>'>
     <!-- Toolbar -->
     <div class="toolbar"></div>
-    <div class="warn_diagnostic"><?php echo xlt('Not For Diagnostic Use') ?></div>
-    <!-- Layer Container -->
-    <div class="layerContainer">
-        <canvas class="imageLayer"><?php echo xlt('Only for HTML5 compatible browsers.') ?></canvas>
-    </div><!-- /layerContainer -->
-</div><!-- /dwv -->
-<!-- Main -->
-<script type="text/javascript" src="<?php echo $GLOBALS['web_root'] ?>/library/js/dwv/dicom_gui.js"></script>
-<script type="text/javascript" src="<?php echo $GLOBALS['web_root'] ?>/library/js/dwv/dicom_launcher.js"></script>
+    <div class="warn_diagnostic"><?php echo xlt('Not For Diagnostic Use') ?>
+        <!-- Layer Container -->
+        <div class="layerContainer">
+            <span class="loader"></span>
+            <canvas id="dwvimg" class="imageLayer"><?php echo xlt('Only for HTML5 compatible browsers.') ?></canvas>
+        </div>
+        <!-- /layerContainer -->
+    </div><!-- /dwv -->
+    <!-- Main -->
+    <script type="text/javascript" src="<?php echo $GLOBALS['web_root'] ?>/library/js/dwv/dicom_gui.js"></script>
+    <script type="text/javascript" src="<?php echo $GLOBALS['web_root'] ?>/library/js/dwv/dicom_launcher.js"></script>
+    <script>
+        var msg = '<?php echo xlt("Still Loading...") ?>';
+        var canvas = document.getElementById("dwvimg");
+        var ctx = canvas.getContext("2d");
+        ctx.font = "22px Arial";
+        ctx.fillStyle = "red";
+        $(window).on("load", function () {
+            ctx.fillText(msg, 10, 100);
+            $('.loader').toggle();
+        });
+    </script>
 </body>
 </html>
