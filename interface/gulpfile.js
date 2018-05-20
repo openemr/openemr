2b5b8f7a155627cfc41f346f99d0cead6a6ed01d
Daniel Sandoval <daniel@discuss.io>
2018-05-20 13:59:38 -0700

    some syntax cleanups in the themes, gulp build work, removing .out directory

--- interface/gulpfile.js
+++ interface/gulpfile.js
@@ -3,6 +3,7 @@
 // modules
 var browserSync = require('browser-sync');
 var csso = require('gulp-csso');
+var del = require('del');
 var gap = require('gulp-append-prepend');
 var gulp = require('gulp');
 var argv = require('minimist')(process.argv.slice(2));
@@ -10,6 +11,7 @@ var gulpif = require('gulp-if');
 var prefix = require('gulp-autoprefixer');
 var reload = browserSync.reload;
 var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
@@ -21,21 +23,25 @@ var config = {
         styles: {
             style_uni: 'themes/style_*.scss',
             style_color: 'themes/colors/*.scss',
-            all: 'themes/**/*style_*.css'
+            all: 'themes/**/style_*.css',
+            all_rtl: 'themes/**/*style_*.css',
         }
+    },
+    dist: {
+        storybook: '.out/'
     },
     dest: {
         themes: 'themes'
     }
 };
 
-// clean - will use once css files are no longer committed
-// gulp.task('clean', function (cb) {
-//     del([config.dest], cb);
-// });
+// clean - will make even stricter once css files are no longer committed
+gulp.task('clean', function () {
+    del.sync([config.dist.storybook]);
+});
 
 gulp.task('ingest', function() {
-    if (typeof config.dev !== "boolean") {
+    if (config.dev && typeof config.dev !== "boolean") {
         // allows for custom proxy to be passed into script
         config.proxy = config.dev;
         config.dev = true;
@@ -49,7 +55,15 @@ gulp.task('sync', ['styles'], function() {
         });
     }
 
-    gulp.watch('themes/**/*.scss', ['styles'])
+    if (config.dev) {
+        gulp.watch('themes/**/*.scss', ['styles']);
+    } else {
+        // hack to get font awesome files into the .out directory
+        gulp.src([
+            '../public/assets/font-awesome-4-6-3/fonts/**/*.{ttf,woff,eof,svg}'
+            ], {base: '../'})
+            .pipe(gulp.dest(config.dist.storybook));
+    }
 });
 
 // styles
@@ -61,6 +75,7 @@ gulp.task('styles:style_uni', function () {
         .pipe(gulpif(!config.dev, csso()))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.dest.themes))
+        .pipe(gulpif(!config.dev, gulp.dest(config.dist.storybook + config.dest.themes)))
         .pipe(gulpif(config.dev, reload({stream:true})));
 });
 
@@ -72,6 +87,7 @@ gulp.task('styles:style_color', function () {
         .pipe(gulpif(!config.dev, csso()))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.dest.themes))
+        .pipe(gulpif(!config.dev, gulp.dest(config.dist.storybook + config.dest.themes)))
         .pipe(gulpif(config.dev, reload({stream:true})));
 });
 
@@ -88,15 +104,23 @@ gulp.task('styles:rtl', function () {
             prefix:"rtl_"
         }))
         .pipe(gulp.dest(config.dest.themes))
+        .pipe(gulpif(!config.dev, gulp.dest(config.dist.storybook + config.dest.themes)))
         .pipe(gulpif(config.dev, reload({stream:true})));
 });
 
 gulp.task('styles:style_list', function () {
-    gulp.src(config.src.styles.all)
+    gulp.src(config.src.styles.all_rtl)
         .pipe(require('gulp-filelist')('themeOptions.json', {flatten: true, removeExtensions: true}))
         .pipe(gulp.dest('.storybook'));
 });
 
 gulp.task('styles', ['styles:style_uni', 'styles:style_color', 'styles:rtl', 'styles:style_list']);
 
-gulp.task('default', [ 'ingest', 'sync' ]);
+if (config.dev) {
+    gulp.task('default', [ 'ingest', 'sync' ]);
+} else {
+    gulp.task('default', function (callback) {
+        runSequence('clean', ['sync'], callback)
+    });
+}
+
