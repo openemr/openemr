94c38a73926916ac3ec289d60ee8eb12cbe6e5ab
Brady Miller <brady.g.miller@gmail.com>
2018-05-24 19:22:01 -0700

    some session fixes (#1659)

--- library/ESign/js/jquery.esign.js
+++ library/ESign/js/jquery.esign.js
@@ -1,7 +1,7 @@
 /**
  * jQuery plugin to enable esign functionality on a DOM object.
- * Pass in a selector to enable esign on the objects that match    
- * 
+ * Pass in a selector to enable esign on the objects that match
+ *
  * Copyright (C) 2013 OEMR 501c3 www.oemr.org
  *
  * LICENSE: This program is free software; you can redistribute it and/or
@@ -22,12 +22,12 @@
  **/
 
 (function( $ ) {
-	
+
 	$.fn.esign = function( customSettings, customEvents ) {
-		
+
 		// Initialize settings
 		var settings = $.extend({
-			
+
 			// These are the defaults.
 			module : "default",
 			baseUrl : "/interface/esign/index.php",
@@ -35,11 +35,12 @@
 			formViewAction : "/interface/esign/index.php?method=esign_form_view",
 			formSubmitAction : "/interface/esign/index.php?method=esign_form_submit"
 		}, customSettings );
-			
+
 		var events = $.extend({
-			
+
 			afterFormSuccess : function( response ) {
 				var logId = "esign-signature-log-"+response.formDir+"-"+response.formId;
+                top.restoreSession();
 				$.post( settings.logViewAction, response, function( html ) {
 					$("#"+logId).replaceWith( html );
 				});
@@ -47,58 +48,59 @@
 				$("#"+editButtonId).replaceWith( response.editButtonHtml );
 			}
 		}, customEvents );
-		
+
 		// Set up the page with the masking div and form container
 		$('body').append( "<div id='esign-mask-content' class='window rounded'></div>" );
 		$('body').append( "<div id='esign-mask'></div>" );
-		
+
 		// Initialize mask event handlers
 		$(document).on( 'click', '#esign-mask', function() {
 			$(this).hide();
 			$('.window').hide();
 			return false;
-		});			
+		});
 
 		$(window).resize( function() {
-			
+
 	 		var box = $('.window');
-	 
+
 	        //Get the screen height and width
 	        var maskHeight = $(document).height();
 	        var maskWidth = $(window).width();
-	      
+
 	        //Set height and width to mask to fill up the whole screen
 	        $('#esign-mask').css({'width':maskWidth,'height':maskHeight});
-	        
+
 	        //Get the window height and width
 			var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
 
 	        //Set the popup window to center
 	        box.css('top',  y/2 - box.height()/2);
 	        box.css('left', x/2 - box.width()/2);
-		 
+
 	        return false;
 		});
-		
+
 		// Initialize the signature form event handlers
 	    $(document).on( 'click', '#esign-back-button', function( e ) {
-	    	
+
 	    	e.preventDefault();
 	    	$(".window").hide();
 	        $("#esign-mask").hide();
-	        
+
 	        return false;
 	    });
-	
+
 	    $(document).on( 'click', '#esign-sign-button-'+settings.module, function( e ) {
-	    	
+
 	        e.preventDefault();
 	        var formData = $('#esign-signature-form').serialize();
-	        $.post( 
+            top.restoreSession();
+	        $.post(
 	        	settings.formSubmitAction,
 	        	formData,
 	        	function( response ) {
-	        		if ( response.status != 'success' ) { 
+	        		if ( response.status != 'success' ) {
 	        			$("#esign-form-error-container").remove();
 	        			$("#esign-mask-content").find("form").append("<div id='esign-form-error-container' class='error'>"+response.message+"</div>");
 	        		} else {
@@ -110,15 +112,15 @@
 	        	},
 	        	'json'
 	        );
-	    	
+
 	    	return false;
 	    });
 
 		function initElement( element ) {
-			
+
 			// Override the anchor
 			element.attr( 'href', '#esign-mask-content' );
-			
+
 			element.click( function( e ) {
 
 				e.preventDefault();
@@ -126,40 +128,41 @@
 				//Get the screen height and width
 				var maskHeight = $(document).height();
 				var maskWidth = $(window).width();
-			
+
 				// Set heigth and width to mask to fill up the whole screen
 				$('#esign-mask').css({'width':maskWidth,'height':maskHeight});
-				
-				//transition effect		
-				$('#esign-mask').fadeIn(200);	
-				$('#esign-mask').fadeTo("fast",0.5);	
-			
+
+				//transition effect
+				$('#esign-mask').fadeIn(200);
+				$('#esign-mask').fadeTo("fast",0.5);
+
 				// Get the window height (y) and width (x)
 				var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
-						                  
+
 				//Set the popup window to center
 				$('#esign-mask-content').css( 'top', Math.min( y/4-$('#esign-mask-content').height()/4, 100 ) );
 				$('#esign-mask-content').css( 'left', x/2-$('#esign-mask-content').width()/2 );
-				
+
 				// Get a list of all the data-* attributes
-				var params = element.data(); 
-				
+				var params = element.data();
+
 				// Load the form
+                top.restoreSession();
 				$.post( settings.formViewAction, params, function( response ) {
 					$('#esign-mask-content').html( response );
 				});
-			
+
 				//transition effect
-				$('#esign-mask-content').fadeIn(200); 
+				$('#esign-mask-content').fadeIn(200);
 		    });
-			
+
 			return false;
 		}
-		
+
 		return this.each( function() {
 			var element = $(this);
         	initElement( element );
         });
 	};
-	
+
 }( jQuery ));
