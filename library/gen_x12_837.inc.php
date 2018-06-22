f87b00af119e86701a5390d3aea862347f899544
stephen waite <stephen.waite@cmsvt.com>
2018-06-22 02:08:54 -0400

    remove else that was logging (#1721)

--- library/gen_x12_837.inc.php
+++ library/gen_x12_837.inc.php
@@ -787,9 +787,7 @@ function gen_x12_837($pid, $encounter, &$log, $encounter_claim = false)
             "*" . $claim->supervisorNumber() .
             "~\n";
         }
-    } else {
-        $log .= "*** Supervising provider has invalid last name.\n";
-    }
+    } 
 
     // Segments NM1*PW, N3, N4 (Ambulance Pick-Up Location) omitted.
     // Segments NM1*45, N3, N4 (Ambulance Drop-Off Location) omitted.
