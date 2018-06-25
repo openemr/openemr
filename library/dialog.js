fad948087a26cb72aaa06958f00d9c8a45e3b59d
JP-DEV\sjpad <sjpadgett@gmail.com>
2018-06-25 12:01:56 -0400

    rename modal classes to be unique to dlgopen

--- library/dialog.js
+++ library/dialog.js
@@ -176,19 +176,37 @@ function inDom(dependency, type, remove) {
 
 // Test if supporting dialog callbacks and close dependencies are in scope.
 // This is useful when opening and closing the dialog is in the same scope. Still use include_opener.js
-// in script that will close a dialog that is not in the same scope dlgopen was used.
+// in script that will close a dialog that is not in the same scope dlgopen was used
+// or use parent.dlgclose() if known decendent.
+// dlgopen() will always have a name whether assigned by dev or created by function.
 // Callback, onClosed and button clicks are still available either way.
 // For a callback on close use: dlgclose(functionName, farg1, farg2 ...) which becomes: functionName(farg1,farg2, etc)
 //
 if (typeof dlgclose !== "function") {
     if (!opener) {
-        opener = window.name;
+        if (!top.tab_mode) {
+            opener = top.get_opener(window.name) ? top.get_opener(window.name) : window;
+        } else {
+            opener = window;
+        }
     }
 
     var dlgclose =
         function (call, args) {
             var frameName = window.name;
             var wframe = opener;
+            if (frameName === '') {
+                // try to find dialog. dialogModal is embedded dialog class
+                // It has to be here somewhere.
+                frameName = $(".dialogModal").attr('id');
+                if (!frameName) {
+                    frameName = parent.$(".dialogModal").attr('id');
+                    if (!frameName) {
+                        console.log("Unable to find dialog.");
+                        return false;
+                    }
+                }
+            }
             if (!top.tab_mode) {
                 for (; wframe.name !== 'RTop' && wframe.name !== 'RBot'; wframe = wframe.parent) {
                     if (wframe.parent === wframe) {
@@ -202,6 +220,12 @@ if (typeof dlgclose !== "function") {
                     }
                 }
                 dialogModal = wframe.$('div#' + frameName);
+                if (dialogModal.length === 0) {
+                    // Never give up...
+                    frameName = $(".dialogModal").attr('id');
+                    dialogModal = wframe.$('div#' + frameName);
+                    console.log("Frame: used local find dialog");
+                }
             } else {
                 var dialogModal = top.$('div#' + frameName);
                 wframe = top;
@@ -229,7 +253,7 @@ if (typeof dlgclose !== "function") {
 * @param {url} string Content location.
 * @param {String} winname If set becomes modal id and/or iframes name. Or, one is created/assigned(iframes).
 * @param {Number| String} width|modalSize(modal-xlg) For sizing: an number will be converted to a percentage of view port width.
-* @param {Number} height Initial height. For iframe auto resize starts here.
+* @param {Number} height Initial minimum height. For iframe auto resize starts at this height.
 * @param {boolean} forceNewWindow Force using a native window.
 * @param {String} title If exist then header with title is created otherwise no header and content only.
 * @param {Object} opts Dialogs options.
@@ -347,25 +371,26 @@ function dlgopen(url, winname, width, height, forceNewWindow, title, opts) {
     if (Math.abs(width) > 0) {
         width = Math.abs(width);
         mWidth = (width / where.innerWidth * 100).toFixed(4) + '%';
-        msSize = '<style>.modal-custom' + winname + ' {width:' + mWidth + ';}</style>';
+        msSize = '<style>.modal-custom-' + winname + ' {width:' + mWidth + ';}</style>';
         mSize = 'modal-custom' + winname;
     } else if (jQuery.inArray(width, sizeChoices) !== -1) {
         mSize = width; // is a modal class
     } else {
-        msSize = 'default'; // standard B.S. modal default (modal-md)
+        msSize = '<style>.modal-custom-' + winname + ' {width:35%;}</style>'; // standard B.S. modal default (modal-md)
     }
-
+    // leave below for legacy
     if (mSize === 'modal-sm') {
-        msSize = '<style>.modal-sm {width:25%;}</style>';
+        msSize = '<style>.modal-custom-' + winname + ' {width:25%;}</style>';
     } else if (mSize === 'modal-md') {
-        msSize = '<style>.modal-md {width:40%;}</style>';
+        msSize = '<style>.modal-custom-' + winname + ' {width:40%;}</style>';
     } else if (mSize === 'modal-mlg') {
-        msSize = '<style>.modal-mlg {width:55%;}</style>';
+        msSize = '<style>.modal-custom-' + winname + ' {width:55%;}</style>';
     } else if (mSize === 'modal-lg') {
-        msSize = '<style>.modal-lg {width:75%;}</style>';
+        msSize = '<style>.modal-custom-' + winname + ' {width:75%;}</style>';
     } else if (mSize === 'modal-xl') {
-        msSize = '<style>.modal-xl {width:96%;}</style>';
+        msSize = '<style>.modal-custom-' + winname + ' {width:96%;}</style>';
     }
+    mSize = 'modal-custom-' + winname;
 
     // Initial responsive height.
     var vpht = where.innerHeight;
@@ -409,7 +434,7 @@ function dlgopen(url, winname, width, height, forceNewWindow, title, opts) {
             '<div class="modal-body %embedded%" %bodyStyles%>' +
             '%body%' + '</div></div></div></div>')
             .replace('%id%', winname)
-            .replace('%sStyle%', msSize !== "default" ? msSize : '')
+            .replace('%sStyle%', msSize ? msSize : '')
             .replace('%dialogId%', opts.dialogId ? ('id=' + opts.dialogId + '"') : '')
             .replace('%szClass%', mSize ? mSize : '')
             .replace('%head%', mTitle !== '' ? headerhtml : '')
