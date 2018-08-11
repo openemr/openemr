2b44121a1f5cca58d7bf591a5651c520177b126a
Brady Miller <brady.g.miller@gmail.com>
2018-08-11 00:31:58 -0700

    some bug fixes (#1794)

--- interface/patient_file/summary/pnotes_fragment.php
+++ interface/patient_file/summary/pnotes_fragment.php
@@ -106,7 +106,7 @@ if (isset($_GET['docUpdateId'])) {
                 echo generate_display_field(array('data_type'=>'1','list_id'=>'note_type'), $iter['title']);
                 echo "</b></td>\n";
 
-                echo "  <td valign='top' class='text'>$body</td>\n";
+                echo "  <td valign='top' class='text'>" . text($body) . "</td>\n";
                 echo "<td valign='top' class='text'><button data-id='" . attr($iter['id']) . "' class='complete_btn'>" . xlt('Completed') . "</button></td>\n";
                 echo " </tr>\n";
 
@@ -195,7 +195,7 @@ if (isset($_GET['docUpdateId'])) {
                             echo "  <td valign='top' class='text'><b>";
                             echo generate_display_field(array('data_type'=>'1','list_id'=>'note_type'), $iter['title']);
                             echo "</b></td>\n";
-                            echo "  <td valign='top' class='text'>$body</td>\n";
+                            echo "  <td valign='top' class='text'>" . text($body) . "</td>\n";
                             echo " </tr>\n";
                             $notes_sent_count++;
                         }
