31893330bc819a85826fe7ee7a8b700402f60daa
Amiel Elboim <amielel@matrix.co.il>
2018-08-21 09:33:04 +0300

    Travis notes

--- interface/patient_tracker/patient_tracker.php
+++ interface/patient_tracker/patient_tracker.php
@@ -943,8 +943,7 @@ if (!$_REQUEST['flb_table']) { ?>
 
 exit;
 
-                function myLocalJS()
-                {
+function myLocalJS() {
                     ?>
                     <script type="text/javascript">
                         var auto_refresh = null;
@@ -956,7 +955,7 @@ exit;
                              $(this).attr('allowFullscreen', 'true');
                         }
                         <?php
-                        if ($_REQUEST['kiosk'] == '1') { ?>
+        if ($_REQUEST['kiosk'] == '1') { ?>
                         $("[name='kiosk_hide']").hide();
                         $("[name='kiosk_show']").show();
         <?php } else { ?>
