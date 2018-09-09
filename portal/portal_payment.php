6f8f1bab162cd3eeb5c3e1234af32334d7a86790
Brady Miller <brady.g.miller@gmail.com>
2018-09-09 14:26:48 -0700

    portal fixes

--- portal/portal_payment.php
+++ portal/portal_payment.php
@@ -343,7 +343,7 @@ if ($_POST['form_save'] || $_REQUEST['receipt']) {
 
 <title><?php echo xlt('Receipt for Payment'); ?></title>
 
-<script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/jquery-1-11-3/jquery.js"></script>
+<script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/jquery-1-11-3/dist/jquery.js"></script>
 <script type="text/javascript">
 $( document ).ready();
 function goHome(){
