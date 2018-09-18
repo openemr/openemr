c343cc565581a9242c2d4b1a6efa872ab0a469d1
Rod Roark <rod@sunsetsystems.com>
2018-09-18 13:51:26 +1000

    Implementing U2F authentication.

--- sql/5_0_1-to-5_0_2_upgrade.sql
+++ sql/5_0_1-to-5_0_2_upgrade.sql
@@ -602,10 +602,29 @@ ALTER TABLE `lists` ADD `list_option_id` VARCHAR (100) DEFAULT NULL COMMENT 'Ref
 INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `notes`, `activity`) VALUES ('page_validation', 'messages#new_note','/interface/main/messages/messages.php',150, '{form_datetime:{futureDate:{message: "Must be future date"}}, reply_to:{presence: {message: "Please choose a patient"}}}', 1);
 #EndIf
 
-
 #IfNotRow4D supported_external_dataloads load_type ICD10 load_source CMS load_release_date 2018-10-01 load_filename 2019-ICD-10-CM-Code-Descriptions.zip
 INSERT INTO `supported_external_dataloads` (`load_type`, `load_source`, `load_release_date`, `load_filename`, `load_checksum`) VALUES ('ICD10', 'CMS', '2018-10-01', '2019-ICD-10-CM-Code-Descriptions.zip', 'b23e0128eb2dce0cb007c31638a8dc00');
 #EndIf
 #IfNotRow4D supported_external_dataloads load_type ICD10 load_source CMS load_release_date 2018-10-01 load_filename 2019-ICD-10-PCS-Order-File.zip 
 INSERT INTO `supported_external_dataloads` (`load_type`, `load_source`, `load_release_date`, `load_filename`, `load_checksum`) VALUES ('ICD10', 'CMS', '2018-10-01', '2019-ICD-10-PCS-Order-File.zip', 'eb545fe61ada9efad0ad97a669f8671f');
 #Endif
+
+#IfNotTable login_mfa_registrations
+CREATE TABLE `login_mfa_registrations` (
+  `user_id`         bigint(20)     NOT NULL,
+  `name`            varchar(256)   NOT NULL,
+  `last_challenge`  datetime       DEFAULT NULL,
+  `method`          varchar(31)    NOT NULL COMMENT 'Q&A, U2F, TOTP etc.',
+  `var1`            text           COMMENT 'Question, U2F registration etc.',
+  `var2`            varchar(256)   NOT NULL DEFAULT '' COMMENT 'Answer, last U2F count etc.',
+  PRIMARY KEY (`user_id`, `name`)
+) ENGINE=InnoDB;
+#EndIf
+
+#IfMissingColumn users_secure last_challenge_response
+ALTER TABLE `users_secure` ADD COLUMN `last_challenge_response` datetime DEFAULT NULL;
+#EndIf
+
+#IfMissingColumn users_secure login_work_area
+ALTER TABLE `users_secure` ADD COLUMN `login_work_area` text;
+#EndIf
