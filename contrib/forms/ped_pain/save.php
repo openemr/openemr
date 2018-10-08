47bccdfcb931bbb0013253b643a29044cb32ebfe
Danora24 <ec2-user@ip-172-31-15-221.ec2.internal>
2018-10-08 15:00:14 +0000

    quick fix

--- contrib/forms/ped_pain/save.php
+++ contrib/forms/ped_pain/save.php
@@ -40,12 +40,10 @@ if ($_GET["mode"] == "new") {
 	siblings_affected = ?,
 	immunization_up_to_date = ?,
 	notes = ?,
-	WHERE id=$id");
-	
-	array($_SESSION["pid"], $_SESSION["authProvider"], $_SESSION["authUser"], $userauthorized, $_POST["location"], $_POST["duration"],
+	WHERE id= ?", array($_SESSION["pid"], $_SESSION["authProvider"], $_SESSION["authUser"], $userauthorized, $_POST["location"], $_POST["duration"],
 	$_POST["severity"], $_POST["fever"], $_POST["lethargy"], $_POST["vomiting"], $_POST["oral_hydration_capable"], $_POST["urine_output_last_6_hours"], 
 	$_POST["pain_with_urination"], $_POST["cough_or_breathing_difficulty"], $_POST["able_to_sleep"], $_POST["nasal_discharge"], $_POST["previous_hospitalization"],
-	$_POST["siblings_affected"], $_POST["immunization_up_to_date"], $_POST["notes"], $id);
+	$_POST["siblings_affected"], $_POST["immunization_up_to_date"], $_POST["notes"], $id));
 }
 
 $_SESSION["encounter"] = $encounter;
