0ae05fc3863243394e452475cb495c3679650336
Matthew Vita <matthewvita48@gmail.com>
2018-10-13 13:40:03 -0400

    Breaking out into RestControllers

--- rest_router.php
+++ rest_router.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * rest_controller
+ * rest_router
  *
  * LICENSE: This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -26,54 +26,37 @@
 
 require_once("interface/globals.php");
 
-use OpenEMR\Services\FacilityService;
-use OpenEMR\Services\VersionService;
-use OpenEMR\Services\ProductRegistrationService;
-use OpenEMR\Services\PatientService;
 use OpenEMR\Common\Http\HttpRestRouteHandler;
+use OpenEMR\RestControllers\FacilityRestController;
+use OpenEMR\RestControllers\VersionRestController;
+use OpenEMR\RestControllers\ProductRegistrationRestController;
+use OpenEMR\RestControllers\PatientRestController;
 
+use OpenEMR\Services\PatientService;
 
 // TODO: Need to handle auth and tokens here
 
 $routes = array(
     'GET /facility' => function() {
-        return (new FacilityService())->getAll();
+        return (new FacilityRestController())->getAll();
     },
-    'GET /facility/:id' => function($id) {
-        return (new FacilityService())->getById($id);
+    'GET /facility/:fid' => function($fid) {
+        return (new FacilityRestController())->getOne($fid);
     },
     'POST /facility' => function() {
         $data = (array)(json_decode(file_get_contents('php://input')));
-        $facilityService = new FacilityService();
-        $validationResult = $facilityService->validate($data);
-
-        if (!$validationResult->isValid()) {
-            http_response_code(400);
-            return $validationResult->getMessages();
-        }
-
-        return $facilityService->insert($data);
+        return (new FacilityRestController())->post($data);
     },
-    'PUT /facility/:id' => function($id) {
+    'PUT /facility/:id' => function($fid) {
         $data = (array)(json_decode(file_get_contents('php://input')));
-        $data['fid'] = $id;
-        $facilityService = new FacilityService();
-        $validationResult = $facilityService->validate($data);
-
-        if (!$validationResult->isValid()) {
-            http_response_code(400);
-            return $validationResult->getMessages();
-        }
-
-        return $facilityService->update($data);
+        $data['fid'] = $fid;
+        return (new FacilityRestController())->put($data);
     },
     'GET /patient' => function() {
-        return (new PatientService())->getAll();
+        return (new PatientRestController(null))->getAll();
     },
     'GET /patient/:pid' => function($pid) {
-        $service = new PatientService();
-        $service->setPid($pid);
-        return $service->getOne();
+        return (new PatientRestController($pid))->getOne();
     },
     'GET /patient/:pid/encounter' => function($pid) {
         $service = new PatientService();
@@ -86,10 +69,10 @@ $routes = array(
         return $service->getEncounter($eid);
     },
     'GET /version' => function() {
-        return (new VersionService())->fetch()->toSerializedObject();
+        return (new VersionRestController())->getOne();
     },
     'GET /product' => function() {
-        return array("status" => (new ProductRegistrationService())->getProductStatus()->getStatusAsString());
+        return (new ProductRegistrationRestController())->getOne();
     }
 );
 
