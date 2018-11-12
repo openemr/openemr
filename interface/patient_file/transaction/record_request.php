97b76c80d003798b1f9bfcef0a4830f060b68db8
Brady Miller <brady.g.miller@gmail.com>
2018-11-11 20:36:44 -0800

    more bug fixes

--- interface/patient_file/transaction/record_request.php
+++ interface/patient_file/transaction/record_request.php
@@ -5,12 +5,13 @@
  * @package OpenEMR
  * @link    http://www.open-emr.org
  * @author  Brady Miller <brady.g.miller@gmail.com>
- * @copyright Copyright (c) 2010-2017 Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2010-2018 Brady Miller <brady.g.miller@gmail.com>
  * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
 
 require_once("../../globals.php");
+
 use OpenEMR\Core\Header;
 
 ?>
