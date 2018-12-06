a0a2edd2474f116fdda51b85fa655e1f870a73a9
Brady Miller <brady.g.miller@gmail.com>
2018-12-06 01:17:28 -0800

    minor changes to prior commit

--- interface/main/about_page.php
+++ interface/main/about_page.php
@@ -108,11 +108,11 @@ $version = $versionService->fetch();
                 <h4><?php  echo xlt('Version Number'); ?>: <?php echo "v".text($openemr_version) ?></h4>
                 <span class="text product-registration"><span class="email"></span> <span class="id"></span></span><br>
                 <?php if (!empty($GLOBALS['support_phone_number'])) { ?>
-                    <span class="text"><?php  echo xlt('Support Phone Number'); ?>: <?php echo $GLOBALS['support_phone_number'] ?></span><br>
+                    <span class="text"><?php  echo xlt('Support Phone Number'); ?>: <?php echo text($GLOBALS['support_phone_number']); ?></span><br>
                 <?php } ?>
-                <a href="<?php echo "http://open-emr.org/wiki/index.php/OpenEMR_".attr($version->getMajor()).".".attr($version->getMinor()).".".attr($version->getPatch())."_Users_Guide"; ?>" target="_blank" class="btn btn-block btn-default"><i class="fa fa-fw fa-book"></i>&nbsp;<?php echo xlt('User Manual'); ?></a>
+                <a href="<?php echo "http://open-emr.org/wiki/index.php/OpenEMR_" . attr($version->getMajor()) . "." . attr($version->getMinor()) . "." . attr($version->getPatch()) . "_Users_Guide"; ?>" target="_blank" class="btn btn-block btn-default"><i class="fa fa-fw fa-book"></i>&nbsp;<?php echo xlt('User Manual'); ?></a>
                 <?php if (!empty($GLOBALS['online_support_link'])) { ?>
-                    <a href='<?php echo $GLOBALS["online_support_link"]; ?>' target="_blank" class="btn btn-default btn-block"><i class="fa fa-fw fa-question-circle"></i>&nbsp;<?php echo xlt('Online Support'); ?></a>
+                    <a href='<?php echo attr($GLOBALS["online_support_link"]); ?>' target="_blank" class="btn btn-default btn-block"><i class="fa fa-fw fa-question-circle"></i>&nbsp;<?php echo xlt('Online Support'); ?></a>
                 <?php } ?>
                 <a href="../../acknowledge_license_cert.html" target="_blank" class="btn btn-default btn-block"><i class="fa fa-fw fa-info-circle"></i><?php echo xlt('Acknowledgments, Licensing and Certification'); ?></a>
                 <div class="donations-needed">
@@ -120,7 +120,7 @@ $version = $versionService->fetch();
                     <a href="http://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=V6EVVTYYK264C" target="_blank" class="btn btn-lg btn-block"><i class="fa fa-2x fa-heart"></i><br/><?php echo xlt("DONATE NOW!"); ?></a>
                 </div>
                 <div class="review">
-                    <a href="https://www.softwareadvice.com/medical/openemr-review/?step=1" title="Voice your opinion" target="_blank"><img src="../../images/review-logo.png"></a>
+                    <a href="https://www.softwareadvice.com/medical/openemr-review/?step=1" title="<?php echo xla("Voice your opinion"); ?>" target="_blank"><img src="<?php echo $GLOBALS['images_static_relative']; ?>/review-logo.png"></a>
                 </div>
 
             </div>
