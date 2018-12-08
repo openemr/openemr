1598d05781a614cf6dbc8418c4d97f4600177394
Brady Miller <brady.g.miller@gmail.com>
2018-12-08 15:42:56 -0800

    some bug fixes

--- contrib/util/dupecheck/mergerecords.php
+++ contrib/util/dupecheck/mergerecords.php
@@ -1,11 +1,42 @@
 <?php
+/**
+ * dupecheck mergerecords.php
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2018 Brady Miller <brady.g.miller@gmail.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+
 require_once("../../../interface/globals.php");
 require_once("../../../library/pnotes.inc");
 require_once("../../../library/log.inc");
 require_once("./Utils.php");
 
-$parameters = GetParameters();
-$oemrdb = $GLOBALS['dbh'];
+if (!empty($_POST)) {
+    if (!verifyCsrfToken($_POST["csrf_token_form"])) {
+        csrfNotVerified();
+    }
+    foreach ($_POST as $key => $value) {
+        $parameters[$key] = $value;
+    }
+}
+
+if (!empty($_GET)) {
+    if (!verifyCsrfToken($_GET["csrf_token_form"])) {
+        csrfNotVerified();
+    }
+    foreach ($_GET as $key => $value) {
+        $parameters[$key] = $value;
+    }
+}
+
+if (!acl_check('admin', 'super')) {
+    die(xlt("Not Authorized"));
+}
+
 ?>
 
 <html>
@@ -24,8 +55,8 @@ if (! isset($parameters['otherid'])) {
 }
 
 // get the PID matching the masterid
-$sqlstmt = "select pid from patient_data where id='".$parameters['masterid']."'";
-$qResults = sqlStatement($sqlstmt);
+$sqlstmt = "select pid from patient_data where id=?";
+$qResults = sqlStatement($sqlstmt, array($parameters['masterid']));
 if (! $qResults) {
     echo "Error fetching master PID.";
     exit;
@@ -42,8 +73,8 @@ if ($parameters['confirm'] == 'yes') {
 // loop over the other IDs and alter their database records
 foreach ($parameters['otherid'] as $otherID) {
     // get info about the "otherID"
-    $sqlstmt = "select lname, pid from patient_data where id='".$otherID."'";
-    $qResults = sqlStatement($sqlstmt);
+    $sqlstmt = "select lname, pid from patient_data where id=?";
+    $qResults = sqlStatement($sqlstmt, array($otherID));
     if (! $qResults) {
         echo "Error fetching master PID.";
         exit;
@@ -51,8 +82,8 @@ foreach ($parameters['otherid'] as $otherID) {
 
     $orow = sqlFetchArray($qResults);
     $otherPID = $orow['pid'];
-    
-    echo "Merging PID ".$otherPID." into the master PID ".$masterPID."<br>";
+
+    echo "Merging PID " . text($otherPID) . " into the master PID " . text($masterPID) . "<br>";
 
     UpdateTable("batchcom", "patient_id", $otherPID, $masterPID);
     UpdateTable("immunizations", "patient_id", $otherPID, $masterPID);
@@ -71,14 +102,14 @@ foreach ($parameters['otherid'] as $otherID) {
     UpdateTable("chart_tracker", "ct_pid", $otherPID, $masterPID);
     UpdateTable("openemr_postcalendar_events", "pc_pid", $otherPID, $masterPID);
     UpdateTable("documents", "foreign_id", $otherPID, $masterPID);
-   
+
     // update all the forms* tables
     $sqlstmt = "show tables like 'form%'";
     $qResults = sqlStatement($sqlstmt);
     while ($row = sqlFetchArray($qResults)) {
         UpdateTable($row['Tables_in_'.$sqlconf["dbase"].' (form%)'], "pid", $otherPID, $masterPID);
     }
-    
+
     // How to handle the data that should be unique to each patient:
     //  Demographics, Employment, Insurance, and History ??
     //
@@ -89,22 +120,22 @@ foreach ($parameters['otherid'] as $otherID) {
 
     // alter the patient's last name to indicate they have been merged into another record
     $newlname = "~~~MERGED~~~".$orow['lname'];
-    $sqlstmt = "update patient_data set lname='".$newlname."' where pid='".$otherPID."'";
+    $sqlstmt = "update patient_data set lname=? where pid=?";
     if ($commitchanges == true) {
-        $qResults = sqlStatement($sqlstmt);
+        $qResults = sqlStatement($sqlstmt, array($newlname, $otherPID));
     }
 
-    echo "<li>Altered last name of PID ".$otherPID." to '".$newlname."'</li>";
+    echo "<li>Altered last name of PID " . text($otherPID) . " to '" . text($newlname) . "'</li>";
 
     // add patient notes regarding the merged data
     $notetext = "All related patient data has been merged into patient record PID# ".$masterPID;
-    echo "<li>Added note about the merge to the PID ".$otherPID."</li>";
+    echo "<li>Added note about the merge to the PID " . text($otherPID) . "</li>";
     if ($commitchanges == true) {
         addPnote($otherPID, $notetext);
     }
 
     $notetext = "All related patient data has been merged from patient record PID# ".$otherPID;
-    echo "<li>Added note about the merge to the Master PID ".$masterPID."</li>";
+    echo "<li>Added note about the merge to the Master PID " . text($masterPID) . "</li>";
     if ($commitchanges == true) {
         addPnote($masterPID, $notetext);
     }
@@ -121,7 +152,7 @@ foreach ($parameters['otherid'] as $otherID) {
 
 function UpdateTable($tablename, $pid_col, $oldvalue, $newvalue)
 {
-    global $commitchanges, $oemrdb;
+    global $commitchanges;
 
     $sqlstmt = "select count(*) as numrows from ".$tablename." where ".$pid_col."='".$oldvalue."'";
     $qResults = sqlStatement($sqlstmt);
@@ -129,14 +160,14 @@ function UpdateTable($tablename, $pid_col, $oldvalue, $newvalue)
     if ($qResults) {
         $row = sqlFetchArray($qResults);
         if ($row['numrows'] > 0) {
-            $sqlstmt = "update ".$tablename." set ".$pid_col."='".$newvalue."' where ".$pid_col."='".$oldvalue."'";
+            $sqlstmt = "update " . escape_table_name($tablename) . " set " . escape_sql_column_name($pid_col, array()) . "=? where " . escape_sql_column_name($pid_col, array()) . "=?";
             if ($commitchanges == true) {
-                $qResults = sqlStatement($sqlstmt);
+                $qResults = sqlStatement($sqlstmt, array($newvalue, $oldvalue));
             }
 
             $rowsupdated = generic_sql_affected_rows();
             echo "<li>";
-            echo "".$tablename.": ".$rowsupdated." row(s) updated<br>";
+            echo "" . text($tablename) . ": " . text($rowsupdated) . " row(s) updated<br>";
             echo "</li>";
         }
     }
@@ -148,11 +179,12 @@ function UpdateTable($tablename, $pid_col, $oldvalue, $newvalue)
 Nothing has been changed yet. What you see above are the changes that will be made if you choose to commit them.<br>
 Do you wish to commit these changes to the database?
 <form method="post" action="mergerecords.php">
-<input type="hidden" name="masterid" value="<?php echo $parameters['masterid']; ?>">
-<input type="hidden" name="dupecount" value="<?php echo $parameters['dupecount']; ?>">
+<input type="hidden" name="csrf_token_form" value="<?php echo attr(collectCsrfToken()); ?>" />
+<input type="hidden" name="masterid" value="<?php echo attr($parameters['masterid']); ?>">
+<input type="hidden" name="dupecount" value="<?php echo attr($parameters['dupecount']); ?>">
 <?php
 foreach ($parameters['otherid'] as $otherID) {
-    echo "<input type='hidden' name='otherid[]' value='$otherID'>";
+    echo "<input type='hidden' name='otherid[]' value='<?php echo attr($otherID); ?>'>";
 }
 ?>
 <input type="submit" name="confirm" value="yes">
@@ -165,7 +197,7 @@ foreach ($parameters['otherid'] as $otherID) {
 </body>
 <?php if ($commitchanges == true) : ?>
 <script language="javascript">
-window.opener.removedupe(<?php echo $parameters['dupecount']; ?>);
+window.opener.removedupe(<?php echo js_escape($parameters['dupecount']); ?>);
 window.close();
 </script>
 <?php endif; ?>
