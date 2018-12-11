63c6e3aa707db56c35100513767d382144bbf246
Sherwin Gaddis <sherwingaddis@gmail.com>
2018-12-10 23:26:29 -0500

    Here is the updated integration of the DDI

--- controllers/C_Prescription.class.php
+++ controllers/C_Prescription.class.php
@@ -40,6 +40,54 @@ class C_Prescription extends Controller
         $this->pconfig = $GLOBALS['oer_config']['prescriptions'];
         $this->RxList = new RxList();
 
+        $interaction = "";
+
+        /*
+        *   check to see if RxNorm installed
+        */
+        $rxn = sqlQuery("SELECT table_name FROM information_schema.tables WHERE table_name = 'RXNCONSO' OR table_name = 'rxnconso'");
+        if ($rxn == false) {
+           $interaction = "Could not find RxNorm Table! Please install.";
+        } elseif ($rxn == true) {
+           /*
+            *   Grab medication list from prescriptions list
+            *   load into array
+            */
+           $pid = $GLOBALS['pid'];
+            $medList = sqlStatement("SELECT drug FROM prescriptions WHERE active = 1 AND patient_id = ?", array($pid));
+            $nameList = array();
+            while ($name = sqlFetchArray($medList)) {
+                $drug = explode(" ", $name['drug']);
+                $rXn = sqlQuery("SELECT `rxcui` FROM `" . mitigateSqlTableUpperCase('RXNCONSO') . "` WHERE `str` LIKE ?", array("%" . $drug[0] . "%"));
+                $nameList[] = $rXn['rxcui'];
+            }
+            if (count($nameList) < 2) {
+                $interaction = "Need more than one drug.";
+
+            } else {
+                /*
+                *  If there are drugs to compare, collect the data
+                */
+                $rxcui_list = implode(", ", $nameList);
+                $data = file_get_contents("https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=".$rxcui_list);
+                $json = json_decode($data, true);
+
+                if (!empty($json['fullInteractionTypeGroup'][0]['fullInteractionType'])) {
+                    foreach ($json['fullInteractionTypeGroup'][0]['fullInteractionType'] as $item) {
+                        $interaction = xlt('Comment').":".text($item['comment'])."</br>";
+                        $interaction .= xlt('Drug1 Name{{Drug1 Interaction}}').":".text($item['minConcept'][0]['name'])."</br>";
+                        $interaction .= xlt('Drug2 Name{{Drug2 Interaction}}').":".text($item['minConcept'][1]['name'])."</br>";
+                        $interaction .= xlt('Severity').":". text($item['interactionPair'][0]['severity'])."</br>";
+                        $interaction .= xlt('Description').":". text($item['interactionPair'][0]['description']);
+                    }
+                } else {
+                    $interaction = 'No interactions found';
+                }
+            }
+
+        }
+
+        $this->assign("INTERACTION", $interaction);
         if ($GLOBALS['inhouse_pharmacy']) {
             // Make an array of drug IDs and selectors for the template.
             $drug_array_values = array(0);
