248254ce45100c6e9828dd5358ef6bfb482e4061
Brady Miller <brady.g.miller@gmail.com>
2018-12-12 17:46:08 -0800

    jquery updates

--- interface/super/rules/www/js/custom.js
+++ interface/super/rules/www/js/custom.js
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  */
 var custom = function( args ) {
 
@@ -31,7 +31,7 @@ var custom = function( args ) {
     }
 
     var fn_wire_events = function() {
-        $('#fld_table').change( function() {
+        $('#fld_table').on("change", function() {
             fn_work();
         });
     }
