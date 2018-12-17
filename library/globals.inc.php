3893a563ddc693f393b77e1c02aea9e4712890c8
Brady Miller <brady.g.miller@gmail.com>
2018-12-16 23:08:44 -0800

    minor changes to prior commit

--- library/globals.inc.php
+++ library/globals.inc.php
@@ -207,7 +207,7 @@ $GLOBALS_METADATA = array(
         'css_header' => array(
             xl('General Theme (need to logout/login after change this setting)'),
             'css',
-            'style_dune.css',
+            'style_light.css',
             xl('Pick a general theme (need to logout/login after change this setting).')
         ),
 
