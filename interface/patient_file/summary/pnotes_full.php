1a222a1e2192cd8d727ef82005a33d38606cd210
Amiel Elboim <amielel@matrix.co.il>
2018-12-30 09:05:12 +0200

    fix

--- interface/patient_file/summary/pnotes_full.php
+++ interface/patient_file/summary/pnotes_full.php
@@ -459,6 +459,7 @@ if ($result != "") {
         }
 
         $body = $iter['body'];
+        $body = preg_replace('/(\sto\s)-patient-(\))/', '${1}'.$patientname.'${2}', $body);
         $body = preg_replace('/(\d{4}-\d{2}-\d{2} \d{2}:\d{2}\s\([^)(]+\s)(to)(\s[^)(]+\))/', '${1}' . xl('to') . '${3}', $body);
         if (preg_match('/^\d\d\d\d-\d\d-\d\d \d\d\:\d\d /', $body)) {
             $body = nl2br(text(oeFormatPatientNote($body)));
@@ -467,7 +468,6 @@ if ($result != "") {
             ' (' . text($iter['user']) . ') ' . nl2br(text(oeFormatPatientNote($body)));
         }
 
-        $body = preg_replace('/(\sto\s)-patient-(\))/', '${1}'.$patientname.'${2}', $body);
         if (($iter{"activity"}) && ($iter['message_status'] != "Done")) {
             $checked = "checked";
         } else {
