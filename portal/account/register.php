93fe9be98deade254eef15f9e97b41ce4b32d3fb
Brady Miller <brady.g.miller@gmail.com>
2019-01-01 00:16:04 -0800

    updated emodal (#2085)

--- portal/account/register.php
+++ portal/account/register.php
@@ -89,7 +89,7 @@ if ($GLOBALS['language_menu_login']) {
 
 <script src="<?php echo $GLOBALS['assets_static_relative']; ?>/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/jquery-datetimepicker/build/jquery.datetimepicker.full.min.js"></script>
-<script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/modified/emodal-1-2-65/dist/eModal.js"></script>
+<script type="text/javascript" src="<?php echo $GLOBALS['assets_static_relative']; ?>/emodal/dist/eModal.min.js"></script>
 
 <script>
 var newPid = 0;
