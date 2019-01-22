932b5257a20feefb252c1b25681e8c345397cf8d
Rod Roark <rod@sunsetsystems.com>
2019-01-22 17:42:03 +1100

    Added layout edit options J, K, M and m. (#2153)
    * Added layout edit options J, K, M and m.
    
    J is to start this field on a new row, regardless of whether the current row is full.
    K is to insert a blank row before this field, and also forces a new row.
    M and m work with radio button fields. A field with M is a "master" field which, when changed,
    will cause the following m "member" fields to receive the same change. You can have multiple
    such groups with each M field starting a new group.
    
    * Fixes for previous commit.

--- library/options.js.php
+++ library/options.js.php
@@ -290,4 +290,26 @@ function lbfCanvasGetData(canid) {
   return lbfCanvases[canid].getImage().toDataURL();
 }
 
+// This is invoked when a field with edit option M is changed.
+// Its purpose is to make the corresponding change to the member fields (edit option m).
+//
+function checkGroupMembers(elem, groupnumber) {
+  var i = elem.id.indexOf('[');
+  if (i < 0) {
+    alert(<?php echo xlj('Field not suitable for edit option M') ?> + ': ' + elem.name);
+    return;
+  }
+  var suffix = elem.id.substring(i);
+  var members = document.getElementsByClassName('lbf_memgroup_' + groupnumber);
+  if (members.length == 0) {
+    alert(<?php echo xlj('No member fields found for') ?> + ': ' + elem.name);
+    return;
+  }
+  for (var i = 0; i < members.length; ++i) {
+    if (members[i].id.indexOf(suffix) > 1) {
+      members[i].checked = true;
+    }
+  }
+}
+
 </script>
