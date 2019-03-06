3c2206347983a214a91bfa6ad9bce4cb384285e5
Brady Miller <brady.g.miller@gmail.com>
2019-03-05 18:22:57 -0800

    replace file_get_contents with oeHttp

--- controllers/C_Prescription.class.php
+++ controllers/C_Prescription.class.php
@@ -158,8 +158,8 @@ class C_Prescription extends Controller
                     // If there are drugs to compare, collect the data
                     // (array_filter removes empty items)
                     $rxcui_list = implode("+", array_filter($nameList));
-                    // Do not urlencode the $rxcui_list, since this breaks the + items
-                    $data = file_get_contents("https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=" . $rxcui_list);
+                    // Unable to urlencode the $rxcui, since this breaks the + items on call to rxnav.nlm.nih.gov; so need to include it in the path
+                    $data = oeHttp::bodyFormat('body')->get('https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=' . $rxcui_list);
                     $json = json_decode($data, true);
                     if (!empty($json['fullInteractionTypeGroup'][0]['fullInteractionType'])) {
                         foreach ($json['fullInteractionTypeGroup'][0]['fullInteractionType'] as $item) {
--- library/classes/RXList.class.php
+++ library/classes/RXList.class.php
@@ -1,39 +1,60 @@
 <?php
- // $Id$
- // $Author$
- //
- // $Log$
+/**
+ * RXList.class.php
+ *
+ * Revision 1.6  2019/02/18  avanss llc
+ * replaced rxlist API with rxnav.nlm.nih.gov
+ *
+ * Revision 1.5  2016/02/016   sherwin gaddis
+ * fix broken url
+ *
+ * Revision 1.4  2008/05/09 20:10:28  cfapress
+ * Changes to handle to HTML returned by rxlist.com
+ *
+ * Revision 1.3  2007/06/19 02:34:45  sunsetsystems
+ * drug lookup fix from Sam Rajan
+ *
+ * Revision 1.2  2005/12/27 00:45:19  sunsetsystems
+ * fix broken url
+ *
+ * Revision 1.1.1.3  2005/06/23 05:25:49  drbowen
+ * Initial import.
+ *
+ * Revision 1.1.1.2  2005/03/28 00:44:54  drbowen
+ * Initial import.
+ *
+ * Revision 1.1.1.1  2005/03/09 19:59:43  wpennington
+ * First import of OpenEMR
+ *
+ * Revision 1.2  2002/08/06 13:49:08  rufustfirefly
+ * updated rxlist class for changes in RxList.com
+ *
+ * Revision 1.1  2002/07/08 14:42:25  rufustfirefly
+ * RxList.com prescription "module" for formulary
+ *
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    rufustfirefly
+ * @author    wpennington
+ * @author    drbowen
+ * @author    sunsetsystems
+ * @author    cfapress
+ * @author    sherwin gaddis
+ * @author    avanss llc
+ * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @author    Jerry Padgett <sjpadgett@gmail.com>
+ * @author    Copyright (c) 2002 rufustfirefly
+ * @author    Copyright (c) 2005 wpennington
+ * @author    Copyright (c) 2005 drbowen
+ * @author    Copyright (c) 2005-2007 sunsetsystems
+ * @author    Copyright (c) 2008 cfapress
+ * @author    Copyright (c) 2016 sherwin gaddis
+ * @author    Copyright (c) 2019 avanss llc
+ * @author    Copyright (c) 2019 Brady Miller <brady.g.miller@gmail.com>
+ * @author    Copyright (c) 2019 Jerry Padgett <sjpadgett@gmail.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
 
- // Revision 1.6  2019/02/18  avanss llc
- // replaced rxlist API with rxnav.nlm.nih.gov
-
- // Revision 1.5  2016/02/016   sherwin gaddis
- // fix broken url
-
- // Revision 1.4  2008/05/09 20:10:28  cfapress
- // Changes to handle to HTML returned by rxlist.com
- //
- // Revision 1.3  2007/06/19 02:34:45  sunsetsystems
- // drug lookup fix from Sam Rajan
- //
- // Revision 1.2  2005/12/27 00:45:19  sunsetsystems
- // fix broken url
- //
- // Revision 1.1.1.3  2005/06/23 05:25:49  drbowen
- // Initial import.
- //
- // Revision 1.1.1.2  2005/03/28 00:44:54  drbowen
- // Initial import.
- //
- // Revision 1.1.1.1  2005/03/09 19:59:43  wpennington
- // First import of OpenEMR
- //
- // Revision 1.2  2002/08/06 13:49:08  rufustfirefly
- // updated rxlist class for changes in RxList.com
- //
- // Revision 1.1  2002/07/08 14:42:25  rufustfirefly
- // RxList.com prescription "module" for formulary
- //
 
 if (!defined('__CLASS_RXLIST_PHP__')) {
     define('__CLASS_RXLIST_PHP__', true);
@@ -43,21 +64,10 @@ if (!defined('__CLASS_RXLIST_PHP__')) {
 
         function getPage($query)
         {
-            $url = "https://rxnav.nlm.nih.gov/REST/Prescribe/drugs?name=".urlencode($query);
-
-            if (!($fp = fopen($url, "r"))) {
-                // If we fail to get the page, return false
-                return false;
-            } else {
-                // Get the page
-                $buffer = 0;
-                while (!feof($fp)) {
-                    $buffer .= fgets($fp, 4096);
-                }
-
-                fclose($fp);
-                return $buffer;
-            } // end checking for successful open
+            $url = "https://rxnav.nlm.nih.gov/REST/Prescribe/drugs";
+            $response = oeHttp::get($url, ['name'=>$query]);
+            $buffer = $response->body();
+            return $buffer ? $buffer : false;
         } // end function RxList::getPage
 
         function get_list($query)
