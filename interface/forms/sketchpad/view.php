866185da0bd4f45cbcd6b39c58d948b2f97504c3
Mouse55 <drhantke@drhantke.com>
2019-03-16 08:39:46 -0700

    using OpenEMR\Core\Header

--- interface/forms/sketchpad/view.php
+++ interface/forms/sketchpad/view.php
@@ -23,6 +23,8 @@
 require_once("../../globals.php");
 require_once($GLOBALS['srcdir'].'/api.inc');
 
+use OpenEMR\Core\Header;
+
 $returnurl = $GLOBALS['concurrent_layout'] ? 'encounter_top.php' : 'patient_encounter.php';
 
 $form_name = xlt('Sketchpad');
@@ -43,6 +45,7 @@ $bg = $GLOBALS['webroot'] . '/interface/forms/' . $folder_name . '/images/' . ba
 <html>
 <head>
   <title><?php echo $form_name; ?></title>
+  <?php Header::setupHeader(); ?>
   <style>
     #canvas {
       background: url("<?php echo $bg; ?>");
@@ -60,7 +63,6 @@ $bg = $GLOBALS['webroot'] . '/interface/forms/' . $folder_name . '/images/' . ba
   </style>
   <link rel="stylesheet" href="../../forms/<?php echo $folder_name; ?>/style.css" type="text/css"/>
   <link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
-  <script src="../../../public/assets/jquery/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
   <script src="../../../library/js/canvas.js" type="text/javascript" charset="utf-8"></script>
 </head>
 <body>
