c23b3427d97eb955dc01f174d3068a239b9422d9
Claire McCrea <claire.n.mccrea@gmail.com>
2019-03-28 12:16:35 -0600

    Reverted  variable reference

--- custom/download_qrda.php
+++ custom/download_qrda.php
@@ -150,10 +150,10 @@ $type_report = (($type_report == "amc") || ($type_report == "amc_2011") || ($typ
         <th scope="col" class="multiDownload">
             <input type="checkbox" name="checkAll" id="checkAll"/>
             <div style="display:none" id=downloadAll>
-                <img class="downloadIcon" src="<?php echo $GLOBALS['images_static_relative'];?>/downbtn.gif" onclick="downloadAllXML();" />
+                <img class="downloadIcon" src="<?php echo $GLOBALS['webroot'];?>/downbtn.gif" onclick="downloadAllXML();" />
             </div>
             <div style="display:none" id=spinAll>
-                <img src="<?php echo $GLOBALS['images_static_relative'];?>/interface/pic/ajax-loader.gif"/>
+                <img src="<?php echo $GLOBALS['webroot'];?>/interface/pic/ajax-loader.gif"/>
             </div>
         </th>
         <th scope="col">
@@ -205,10 +205,10 @@ $type_report = (($type_report == "amc") || ($type_report == "amc_2011") || ($typ
                 echo "</td>";
                 echo "<td align=center>";
                 echo "<div id=download" . attr($counter) . ">";
-                echo "<img class='downloadIcon' src='" . $GLOBALS['images_static_relative'] . "/downbtn.gif' onclick=downloadXML(" . attr_js($counter) . ",1); />";
+                echo "<img class='downloadIcon' src='" . $GLOBALS['images_static_relative'] . "/downbtn.gif' onclick=downloadXML()" . attr_js($counter) . ",1); />";
                 echo "</div>";
                 echo "<div style='display:none' id=spin" . attr($counter) . ">";
-                echo "<img src='" . $GLOBALS['images_static_relative'] . "/interface/pic/ajax-loader.gif'/>";
+                echo "<img src='" . $GLOBALS['webroot'] . "/interface/pic/ajax-loader.gif'/>";
                 echo "</div>";
                 echo "</td>";
                 echo "<td>";
