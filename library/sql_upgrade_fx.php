144fe04c1313d02c8838878cd8c7a6929e87cb34
Sherwin Gaddis <sherwingaddis@gmail.com>
2019-04-17 12:17:45 -0400

    located missing brace

--- library/sql_upgrade_fx.php
+++ library/sql_upgrade_fx.php
@@ -830,6 +830,7 @@ function upgradeFromSqlFile($filename)
             if ($skipping) {
                 echo "<font color='green'>Skipping section $line</font><br />\n";
             }
+      }
           else if (preg_match('/^#IfNotWenoRx/', $line) && !tableHasRow('erx_weno_drugs', "drug_id", '1008') == true){
             if (tableHasRow('erx_weno_drugs', "drug_id", '1008') == true) {
                 $skipping = true;
