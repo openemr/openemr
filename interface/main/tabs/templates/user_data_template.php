77fb70f66ac4a562ebf981708c969da3c2425c85
Ranganath Pathak <pathak01@hotmail.com>
2019-04-21 00:58:13 -0700

    Restyling MFA - Multi Factor Authorization
    Highlighted the feature in the setup.php script and indicated that this is set for only the init$
    Modified setup help file accordingly
    Restyled the MFA pages to integrate with styling of rest of the application using OemrUI class
    Added help file to facilitate easy implementation of the feature
    Emphasized that this feature has to be enabled on a per user basis
    As this is a user specific feature added it as menu item under User Name
    Restyled the Change Password page to match styling using OemrUI class:)

--- interface/main/tabs/templates/user_data_template.php
+++ interface/main/tabs/templates/user_data_template.php
@@ -6,8 +6,10 @@
  * @link      http://www.open-emr.org
  * @author    Kevin Yeh <kevin.y@integralemr.com>
  * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @author    Ranganath Pathak <pathak@scrs1.org>
  * @copyright Copyright (c) 2016 Kevin Yeh <kevin.y@integralemr.com>
  * @copyright Copyright (c) 2016 Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2019 Ranganath Pathak <pathak@scrs1.org>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 ?>
@@ -23,6 +25,7 @@
                 <ul class="userfunctions menuEntries">
                     <li class="menuLabel" data-bind="click: editSettings"><?php echo xlt("Settings");?></li>
                     <li class="menuLabel" data-bind="click: changePassword"><?php echo xlt("Change Password");?></li>
+                    <li class="menuLabel" data-bind="click: changeMFA"><?php echo xlt("MFA Management");?></li>
                     <li class="menuLabel" data-bind="click: logout"><?php echo xlt("Logout");?></li>
                 </ul>
             </div>
