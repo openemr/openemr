4980a41030a176970e7e001c70a783390f18b046
Sharon Cohen <sharonco@matrix.co.il>
2019-04-23 12:10:13 +0300

    REmoved files

--- interface/patient_file/summary/dashboard_react/index.php
+++ interface/patient_file/summary/dashboard_react/index.php
@@ -1,16 +1,8 @@
-<?php
-$user = (object) [
-    'name' => 'Jane Doe',
-    'email' => 'janedoe@gmail.com',
-    'logged' => true
-];
-?>
 <!doctype html>
 <html lang="en">
     <head>
-        <title>React Dashboard</title>
+        <title>Patient Dashboard</title>
         <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="./app/assets/css/app.css" type="text/css">
         <link rel="stylesheet" href="../../../../public/assets/bootstrap/dist/css/bootstrap.min.css?v=43" type="text/css">
@@ -20,7 +12,8 @@ $user = (object) [
     </script>
     <body>
 
-        <div id="app"></div>
+        <div id="app">
+        </div>
 
         <script type="text/javascript" src="./app/assets/bundle/main.bundle.js" ></script>
 
