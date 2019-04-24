4258f1332a1b50c17cfad81450243a60c1e1c862
Sherwin Gaddis <sherwingaddis@gmail.com>
2019-04-24 14:12:19 -0400

    Clear Log and Tab Log feature add
    changed styling and translated and ecaped the text.
    
    Strong Tag Added

---
+++ interface/billing/clear_log.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * interface/billing/customize_log.php - starting point for customization of billing log
+ *
+ * @package OpenEMR
+ * @author Stephen Waite <stephen.waite@cmsvt.com>
+ * @link http://www.open-emr.org
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Stephen Waite <stephen.waite@cmsvt.com>
+ * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2014 Stephen Waite <stephen.waite@cmsvt.com>
+ * @copyright Copyright (c) 2019 Brady Miller <brady.g.miller@gmail.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+*/
+
+
+
+     
+require_once("../globals.php");
+
+$filename = $GLOBALS['OE_SITE_DIR'] . '/documents/edi/process_bills.log';
+$date = date("Y-m-d");
+
+$newlog = $GLOBALS['OE_SITE_DIR'] . '/documents/edi/' . $date . '_process_bills.log';
+
+rename($filename, $newlog);
+
+file_put_contents($filename, " ");
+
+echo "Log is clear, please close window";
+/*
+$fh = fopen($filename, 'r');
+
+while ($line = fgets($fh)) {
+    echo(text($line));
+    echo("<br />");
+}
+
+    fclose($fh);
+*/
