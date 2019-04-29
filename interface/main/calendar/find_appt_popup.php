b445d8406ac3705cbc4d4c3ec66c06bf1ab5b370
stephen waite <cmswest1@gmail.com>
2019-04-29 15:11:44 -0400

    handle other globals date displays

--- interface/main/calendar/find_appt_popup.php
+++ interface/main/calendar/find_appt_popup.php
@@ -109,22 +109,50 @@ if ($_REQUEST['searchdays']) {
     $searchdays = $_REQUEST['searchdays'];
 }
 
- // Get a start date.
+// Get a start date and end date - actually the date after the end date.
 if ($_REQUEST['startdate']) {
-    if ($GLOBALS['date_display_format'] == 1) {
-        $sdate = ($_REQUEST['startdate']);
+    $sdate = ($_REQUEST['startdate']);
+    if ($GLOBALS['date_display_format'] == 0) {
+        // $matches[0] is YYYY, $matches[1] is MM, $matches[2] is DD
+        $matches = explode('-', $sdate);
+        $sdate = date(
+            "Y-m-d",
+            mktime(0,0,0, $matches[1], $matches[2], $matches[0])
+        );
+        $edate = date(
+            "Y-m-d",
+            mktime(0, 0, 0, $matches[1], $matches[2] + $searchdays, $matches[0])
+        );
+
+    } else if ($GLOBALS['date_display_format'] == 1) {
         // $matches[0] is MM, $matches[1] is DD, $matches[2] is YYYY
         $matches = explode('/', $sdate);
+        // $sdate is $_REQUEST['startdate'] and is already in proper format
+        $edate = date(
+            "Y-m-d",
+            mktime(0, 0, 0, $matches[0], $matches[1] + $searchdays, $matches[2])
+        );
+    } else if ($GLOBALS['date_display_format'] == 2) {
+        // $matches[0] is DD, $matches[1] is MM, $matches[2] is YYYY
+        $matches = explode('/', $sdate);
+        $sdate = date(
+                "Y-m-d",
+                mktime(0,0,0, $matches[1], $matches[0], $matches[2])
+        );
+        $edate = date(
+            "Y-m-d",
+            mktime(0, 0, 0, $matches[1], $matches[0] + $searchdays, $matches[2])
+        );
     }
 } else {
     $sdate = date("Y-m-d");
+    // $matches[0] is MM, $matches[1] is DD, $matches[2] is YYYY
+    $matches = explode('-', $sdate);
+    $edate = date(
+        "Y-m-d",
+        mktime(0, 0, 0, $matches[0], $matches[1] + $searchdays, $matches[2])
+    );
 }
-
-// Get an end date - actually the date after the end date.
-$edate = date(
-    "Y-m-d",
-    mktime(0, 0, 0, $matches[0], $matches[1] + $searchdays, $matches[2])
-);
 
 // compute starting time slot number and number of slots.
 $slotstime = strtotime("$sdate 00:00:00");
