343b4176ce0f5d2d033d09fdf3bca74d1a67b2ef
Yuriy Gershem <yuriyge@matrix.co.il>
2019-05-02 14:13:51 +0300

    Fix FontAwesome for medical problem

--- interface/patient_file/summary/dashboard_react/app/_devapp/components/MedicalProblems.js
+++ interface/patient_file/summary/dashboard_react/app/_devapp/components/MedicalProblems.js
@@ -9,10 +9,10 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import ToggleButton from "./global/ToggleButton";
 
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faPlus,faMinus,faEdit } from '@fortawesome/free-solid-svg-icons'
-// library.add( faPlus,faMinus,faEdit )
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+library.add( faEdit )
 
 
 
---
+++ interface/patient_file/summary/dashboard_react/app/_devapp/utils/agent.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+const requests = {
+
+}
