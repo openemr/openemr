671130c2a2ed94a7955e0670fbc0b92209178bbd
stephen waite <cmswest1@gmail.com>
2019-05-15 13:32:50 -0400

    typo, thank you jit and sherwin

--- library/billing/src/X12_5010_837P.php
+++ library/billing/src/X12_5010_837P.php
@@ -7,7 +7,7 @@
  * @author Rod Roark <rod@sunsetsystems.com>
  * @author Stephen Waite <stephen.waite@cmsvt.com>
  * @copyright Copyright (c) 2009 Rod Roark <rod@sunsetsystems.com>
- * @copyright Copyright (c) 2018 Stephen Waite <stephen.waite@cmsvt.com>
+ * @copyright Copyright (c) 2018-2019 Stephen Waite <stephen.waite@cmsvt.com>
  * @link https://github.com/openemr/openemr/tree/master
  * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
@@ -785,7 +785,7 @@ class X12_5010_837P
             "*" . "1" .
             "*";
             if ($claim->referrerLastName()) {
-                $out .= $claim->referrerLastName;
+                $out .= $claim->referrerLastName();
             } else {
                 $log .= "*** Missing referrer last name.\n";
             }
