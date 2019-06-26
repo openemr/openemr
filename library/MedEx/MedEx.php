da853261af1045a443da7b85bf629d4ce8e91d53
Ray Magauran <ophthal@users.noreply.github.com>
2019-06-25 20:16:12 -0400

    5.0.2 medex updates (#2516)

--- library/MedEx/MedEx.php
+++ library/MedEx/MedEx.php
@@ -27,21 +27,22 @@
      * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
      */
 
-$ignoreAuth=true;
-$_SERVER['HTTP_HOST']   = 'default'; //change for multi-site
+    $ignoreAuth=true;
+    $_SERVER['HTTP_HOST']   = 'default'; //change for multi-site
 
-require_once(dirname(__FILE__)."/../../interface/globals.php");
-require_once(dirname(__FILE__)."/../patient.inc");
-require_once(dirname(__FILE__)."/API.php");
-
-
-if (!empty($_POST['callback_key'])) {
-    $MedEx = new MedExApi\MedEx('MedExBank.com');
-    $response = $MedEx->login('1');
-    header('Content-type: application/json');
-    echo json_encode($response);
-    exit;
-}
+    require_once(dirname(__FILE__)."/../../interface/globals.php");
+    require_once(dirname(__FILE__)."/../patient.inc");
+    require_once(dirname(__FILE__)."/../log.inc");
+    require_once(dirname(__FILE__)."/API.php");
+    
+    
+    if (empty($_POST['callback_key'])) {
+        $MedEx = new MedExApi\MedEx('MedExBank.com');
+        $response = $MedEx->login('1');
+        header('Content-type: application/json');
+        echo json_encode($response);
+        exit;
+    }
     echo "Not logged in: ";
     echo $MedEx->getLastError();
     exit;
