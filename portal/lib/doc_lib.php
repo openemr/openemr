35ae07cff8e7d1ac800a444eecf2be431be7466f
Brady Miller <brady.g.miller@gmail.com>
2019-07-07 12:04:42 -0700

    minor fixes

--- portal/lib/doc_lib.php
+++ portal/lib/doc_lib.php
@@ -10,7 +10,7 @@
  * @copyright Copyright (c) 2019 Brady Miller <brady.g.miller@gmail.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
-$ignoreAuth = true;
+
 
 // Will start the (patient) portal OpenEMR session/cookie.
 require_once(dirname(__FILE__) . "/../src/Common/Session/SessionUtil.php");
