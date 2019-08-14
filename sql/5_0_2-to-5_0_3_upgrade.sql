21e01b253d42d30606b2a6f1e0ea2577d491b2e4
JP-DEV\sjpad <sjpadgett@gmail.com>
2019-08-14 03:24:43 -0400

    merge conflict

--- sql/5_0_2-to-5_0_3_upgrade.sql
+++ sql/5_0_2-to-5_0_3_upgrade.sql
@@ -85,6 +85,10 @@
 --    arguments: none
 --    behavior: can take a long time.
 
+#IfMissingColumn facility iban
+ALTER TABLE `facility` ADD `iban` varchar(50) default NULL;
+#EndIf
+
 #IfNotRow2D list_options list_id apps option_id oeSignerRemote
 INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`) VALUES
 ('apps', 'oeSignerRemote', './../portal/sign/assets/signit.php', 30, 0, 0, '', '', '', 0, 0, 0, '', 1);
