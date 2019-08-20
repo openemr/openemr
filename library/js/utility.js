6d4b24aa2ce0ebbde8c69ee2bc4d24d6e8a5288d
Brady Miller <brady.g.miller@gmail.com>
2019-08-20 14:24:55 -0700

    several more updates per Jerry

--- library/js/utility.js
+++ library/js/utility.js
@@ -1,14 +1,15 @@
 /**
- * Javascript translate function.
- *  This calls the i18next.t function that has been set up in main.php
+ * Javascript utility functions for openemr
  *
- * @package OpenEMR
- * @link    http://www.open-emr.org
- * @author  Brady Miller <brady.g.miller@gmail.com>
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Brady Miller <brady.g.miller@gmail.com>
  * @copyright Copyright (c) 2019 Brady Miller <brady.g.miller@gmail.com>
- * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
+// Translation function
+//  This calls the i18next.t function that has been set up in main.php
 function xl(string) {
     if (typeof top.i18next.t == 'function') {
         return top.i18next.t(string);
