a76e4897536ad148d74a068f8ded545f1bb476f8
JP-DEV\sjpad <sjpadgett@gmail.com>
2019-09-04 22:17:44 -0400

    forgotten namespaces

--- modules/oeFax/contact.php
+++ modules/oeFax/contact.php
@@ -8,7 +8,6 @@
  * @copyright Copyright (c) 2018-2019 Jerry Padgett <sjpadgett@gmail.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
-namespace Modules\oeFax\Controller;
 
 require_once(__DIR__ . "/../../interface/globals.php");
 
--- modules/oeFax/setup.php
+++ modules/oeFax/setup.php
@@ -9,8 +9,6 @@
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-namespace Modules\oeFax\Controller;
-
 require_once(__DIR__ . "/../../interface/globals.php");
 
 use OpenEMR\Core\Header;
