4db88c8de9c07a29bdce137c757835575f694b5f
Sherwin Gaddis <sherwingaddis@gmail.com>
2019-10-23 05:27:33 -0400

    Move extension to the Financial Reports folder

--- src/FinancialReports/HeaderExtension.php
+++ src/FinancialReports/HeaderExtension.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Header Extension.
- * This file goes in the twig/twig/src folder
+ *
  * @package OpenEMR
  * @link    http://www.open-emr.org
  * @author  Sherwin Gaddis <sherwingaddis@gmail.com>
@@ -9,7 +9,7 @@
  * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-namespace Twig;
+namespace OpenEMR\FinancialReports;
 
 use Twig\Extension\AbstractExtension;
 
