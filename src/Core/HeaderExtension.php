9caccfe9b1845ef343f5c81810993624a288fe1f
Sherwin Gaddis <sherwingaddis@gmail.com>
2019-10-24 06:23:47 -0400

    Moving this to the core since this can live anywhere.

--- src/Core/HeaderExtension.php
+++ src/Core/HeaderExtension.php
@@ -9,7 +9,7 @@
  * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-namespace OpenEMR\FinancialReports;
+namespace OpenEMR\Core;
 
 use Twig\Extension\AbstractExtension;
 
