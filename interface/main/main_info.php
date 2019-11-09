8e2b1ae952c3ea23dd2d5746faead9623280d8da
JP-DEV\sjpad <sjpadgett@gmail.com>
2019-11-09 10:29:55 -0500

    Calendar Restyle

--- interface/main/main_info.php
+++ interface/main/main_info.php
@@ -11,36 +11,6 @@
 
 
 require_once("../globals.php");
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<HTML>
-<HEAD>
-<TITLE><?php echo xlt('Calendar'); ?></TITLE>
-<!-- (CHEMED) -->
-<!-- The DOCTYPE is set above to XHTML to put IE into Sttrict Mode so we can get a viewport width -->
-<script type='text/javascript' language='JavaScript'>
-function GetInnerX () {
-    var x;
-    if (self.innerHeight) // all except Explorer
-    {
-        x = self.innerWidth;
-    }
-    else if (document.documentElement && document.documentElement.clientHeight)
-        // Explorer 6 Strict Mode
-    {
-        x = document.documentElement.clientWidth;
-    }
-    else if (document.body) // other Explorers
-    {
-        x = document.body.clientWidth;
-    }
-    return x;
-}
-
-var x = GetInnerX();
-var framesrc = '<frame ';
-
-<?php
 
 // this allows us to keep our viewtype between screens -- JRM calendar_view_type
 $viewtype = $GLOBALS['calendar_view_type'];
@@ -80,26 +50,34 @@ if ($_SESSION['userauthorized'] && $GLOBALS['docs_see_entire_calendar']) {
 }
 ?>
 
-framesrc += ' src="<?php echo $framesrc; ?>';
-framesrc += '&framewidth=' + encodeURIComponent(x) + '" name="Calendar" scrolling="auto" frameborder="YES">';
-
-</script>
-<!-- END (CHEMED) -->
-</HEAD>
-
-<!-- (CHEMED) -->
-<script type='text/javascript' language='JavaScript'>
-    document.write('<frameset rows="*" cols="*" name="Main" frameborder="NO" border="0" framespacing="0" >');
-    document.write(framesrc);
-    document.write('</frameset>');
-    document.close();
-
-</script>
-<!-- END (CHEMED) -->
-
-
-<noframes><body bgcolor="#FFFFFF">
-<?php echo xlt('Frame support required'); ?>
-</body></noframes>
-
-</HTML>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo xlt('Calendar'); ?></title>
+<style>
+.calFrame {
+    border: none;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    object-fit: contain;
+    object-position: left top;
+}
+.calMain {
+    position: fixed;
+    vertical-align: top;
+    margin: 0px;
+}
+body {
+    margin: 0px;
+}
+</style>
+</head>
+<body>
+    <div class="calMain" name="Main">
+    <iframe class="calFrame" name="Calendar" src="<?php echo $framesrc; ?>"></iframe>
+    </div>
+</body>
+</html>
