b6189d8a28261bd88d5f430f4f2da6ca1188e6f7
Brady Miller <brady.g.miller@gmail.com>
2019-11-19 10:16:35 -0800

    some more work

--- interface/themes/themeBuilder.php
+++ interface/themes/themeBuilder.php
@@ -9,7 +9,10 @@
  * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
+// need to skip auth to allow use of this script from the login script
+// this script is safe to ignore auth (takes no user input)
 $ignoreAuth=true;
+
 require_once __DIR__ . '/../../interface/globals.php';
 
 $cssFile = file_get_contents('themeBuilder.css');
@@ -18,6 +21,11 @@ $cssFile = file_get_contents('themeBuilder.css');
 $variables = [
     'font-family',
     'font-size',
+];
+
+// Allowed other replacements
+$otherVariables = [
+    'fonts_dir'
 ];
 
 // Build string-replacement array
@@ -29,8 +37,9 @@ foreach ($matches as $match) {
     $rawString = $match[0];
     $key = $match[1];
     if (!array_key_exists($key, $GLOBALS)) {
-        $msg = sprintf("The key '%s' was not found in the list of global variables", $key);
+        $msg = sprintf("OpenEMR ERROR: The key '%s' was not found in the list of global variables in themeBuilder", $key);
         error_log(errorLogEscape($msg));
+        $cssFile = str_replace($rawString, '', $cssFile);
         continue;
     }
 
@@ -42,11 +51,23 @@ foreach ($matches as $match) {
     if (in_array($key, $variables)) {
         // This is a CSS variable, not a $GLOBAL value replacement
         $globalVal = $GLOBALS["{$key}"];
+        // Ensure only contains valid characters
+        if (preg_match('/[^A-Za-z0-9,"\-\s]/', $globalVal)) {
+            $msg = sprintf("OpenEMR ERROR: The value '%s' contains illegal character in themeBuilder", $globalVal);
+            error_log(errorLogEscape($msg));
+            $cssFile = str_replace($rawString, '', $cssFile);
+            continue;
+        }
         $attribString = "{$key}: {$globalVal} !important;";
         $cssFile = str_replace($rawString, $attribString, $cssFile);
-    } else {
+    } else if (in_array($key, $otherVariables)) {
         // $GLOBAL value replacement
         $cssFile = str_replace($rawString, $GLOBALS["{$key}"], $cssFile);
+    } else {
+        $msg = sprintf("OpenEMR ERROR: The key '%s' was not allowed in themeBuilder", $key);
+        error_log(errorLogEscape($msg));
+        $cssFile = str_replace($rawString, $attribString, $cssFile);
+        continue;
     }
 }
 
