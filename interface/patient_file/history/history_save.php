0cfc88bfa04534312683943455d23ca0ab94b50d
visolveemr <services@visolve.com>
2019-12-16 15:46:58 -0500

    update copyright

--- interface/patient_file/history/history_save.php
+++ interface/patient_file/history/history_save.php
@@ -5,7 +5,7 @@
  * @package   OpenEMR
  * @link      http://www.open-emr.org
  * @author    Brady Miller <brady.g.miller@gmail.com>
- * @copyright Copyright (c) 2018 Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2019 Brady Miller <brady.g.miller@gmail.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
