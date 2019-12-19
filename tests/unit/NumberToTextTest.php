d2cddc5398afa295c1614f9e051d13aee1dd86ba
JP-DEV\sjpad <sjpadgett@gmail.com>
2019-12-19 11:15:34 -0500

    Revert "issue #2846"
    This reverts commit a24b7ef90fff55411103c0cf231e04be9002551a.

--- tests/unit/NumberToTextTest.php
+++ tests/unit/NumberToTextTest.php
@@ -1,15 +1,14 @@
 <?php
 /* Copyright © 2010 by Andrew Moore */
 /* Licensing information appears at the end of this file. */
-namespace OpenEMR\Tests\Unit;
 
-require_once dirname(__FILE__) . '/../../vendor/autoload.php';
+error_reporting(E_ALL);
+require_once 'PHPUnit/Framework.php';
+require_once dirname(__FILE__) . '/../../library/classes/NumberToText.class.php';
 
-use NumberToText;
-use PHPUnit\Framework\TestCase;
-
-class NumberToTextTest extends TestCase
+class NumberToTextTest extends PHPUnit_Framework_TestCase
 {
+
   /**
    * @dataProvider cases
    */
@@ -18,8 +17,10 @@ class NumberToTextTest extends TestCase
         $ntt = new NumberToText($numeral);
         $this->assertEquals($text, $ntt->convert(), "'$numeral' converts to '$text'");
     }
+
     public static function cases()
     {
+
         return array( array( 0,     'zero'),
                   array( 1,     'one'),
                   array( 14000, 'fourteen thousand'),
@@ -31,13 +32,16 @@ class NumberToTextTest extends TestCase
                   );
     }
 }
+
 /*
 This file is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as publish by the Free Software
 Foundation.
+
 This file is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 PARTICULAR PURPOSE.  See the GNU Gneral Public License for more details.
+
 You should have received a copy of the GNU General Public Licence along with
 this file.  If not see <http://www.gnu.org/licenses/>.
 */
