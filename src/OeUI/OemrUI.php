0744173870331a7daa9d9175c35f0ba13925e4a0
Brady Miller <brady.g.miller@gmail.com>
2020-02-26 19:48:20 -0800

    focus fix

--- src/OeUI/OemrUI.php
+++ src/OeUI/OemrUI.php
@@ -3,16 +3,19 @@
  * OemrUI class.
  *
  * @package   OpenEMR
- * @link      https://www.open-emr.org
- * @author    Ranganath Pathak <pathak@scrs1.org>
- * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @link      http://www.open-emr.org
+ * @author Ranganath Pathak <pathak@scrs1.org>
  * @copyright Copyright (c) 2018 Ranganath Pathak <pathak@scrs1.org>
- * @copyright Copyright (c) 2020 Brady Miller <brady.g.miller@gmail.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 namespace OpenEMR\OeUI;
 
 use OpenEMR\Common\Csrf\CsrfUtils;
+use OpenEMR\Core\Header;
+
+// Special case where not setting up the header for a script, so using setupAssets function,
+//  which does not autoload anything. The actual header is set up in another script.
+Header::setupAssets(['jquery-ui', 'jquery-ui-base']);
 
 class OemrUI
 {
