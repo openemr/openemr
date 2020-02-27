6620cdebbab31bf703d91418afa51cd2dd9e134c
Cliff Su <stu01509@yahoo.com.tw>
2020-02-28 06:58:48 +0800

    fix: add closing p tag.

--- Documentation/help_files/openemr_multisite_admin_help.php
+++ Documentation/help_files/openemr_multisite_admin_help.php
@@ -36,40 +36,40 @@
             </div>
             <div class= "row">
                 <div class="col-sm-12">
-                    <p><?php echo ("This is the central location to manage multisite intallations");?>.
+                    <p><?php echo ("This is the central location to manage multisite intallations");?>.</p>
 
-                    <p><?php echo ("It serves three functions");?>:
+                    <p><?php echo ("It serves three functions");?>:</p>
                         <ul>
                             <li><?php echo ("Tabulates all sites that have been installed using the multisite module with ability to login to each site"); ?></li>
                             <li><?php echo ("Keeps track of the version and status of the site's database, access control list tables and patch status"); ?></li>
                             <li><?php echo ("Lets the user add a new site using the multisite module"); ?></li>
                         </ul>
 
-                    <p><?php echo ("Initially it will have only the 'default' site installed");?>.
+                    <p><?php echo ("Initially it will have only the 'default' site installed");?>.</p>
 
-                    <p><?php echo ("The displayed table will have six columns - 'Site ID', 'DB Name', 'Site Name', 'Version', 'Is Current', and 'Log In'");?>.
+                    <p><?php echo ("The displayed table will have six columns - 'Site ID', 'DB Name', 'Site Name', 'Version', 'Is Current', and 'Log In'");?>.</p>
 
-                    <p><?php echo ("<strong>Site ID</strong>  - unique ID of the site, should be one word, preferably lower case");?>.
+                    <p><?php echo ("<strong>Site ID</strong>  - unique ID of the site, should be one word, preferably lower case");?>.</p>
 
-                    <p><?php echo ("This will be used to identify which site to login to");?>.
+                    <p><?php echo ("This will be used to identify which site to login to");?>.</p>
 
-                    <p><?php echo ("The site-specific non-database patient data will be stored in a sub-directory bearing the site ID in the 'sites' directory");?>.
+                    <p><?php echo ("The site-specific non-database patient data will be stored in a sub-directory bearing the site ID in the 'sites' directory");?>.</p>
 
-                    <p><?php echo ("<strong>DB Name</strong> - the name of the database containing site-specific data");?>.
+                    <p><?php echo ("<strong>DB Name</strong> - the name of the database containing site-specific data");?>.</p>
 
-                    <p><?php echo ("<strong>Site Name</strong> - by default it will be OpenEMR, once the site is setup this can be changed for that instance by going to Administration > Appearance > Application Title");?>.
+                    <p><?php echo ("<strong>Site Name</strong> - by default it will be OpenEMR, once the site is setup this can be changed for that instance by going to Administration > Appearance > Application Title");?>.</p>
 
-                    <p><?php echo ("<strong>Version</strong> - the version of the current installation");?>.
+                    <p><?php echo ("<strong>Version</strong> - the version of the current installation");?>.</p>
 
-                    <p><?php echo ("As the script files are common to all sites, it would be imperative that all sites have the same version number");?>.
+                    <p><?php echo ("As the script files are common to all sites, it would be imperative that all sites have the same version number");?>.</p>
 
-                    <p><?php echo ("<strong>Is Current</strong> - Whether on not the the site's installed database, access control list version and patch status is current i.e. the OpenEMR scripts will work with the installed database, the latest access control lists are available and that the required patches have been applied and is up to date ");?>.
+                    <p><?php echo ("<strong>Is Current</strong> - Whether on not the the site's installed database, access control list version and patch status is current i.e. the OpenEMR scripts will work with the installed database, the latest access control lists are available and that the required patches have been applied and is up to date ");?>.</p>
 
-                    <p><?php echo ("<strong>Log In</strong> - That will let you login to the particular site");?>.
+                    <p><?php echo ("<strong>Log In</strong> - That will let you login to the particular site");?>.</p>
 
-                    <p><?php echo ("Clicking the 'Add a New Site' button will take you to 'Optional Site ID Selection' page that will begin the process of adding a new site using the setup script");?>.
+                    <p><?php echo ("Clicking the 'Add a New Site' button will take you to 'Optional Site ID Selection' page that will begin the process of adding a new site using the setup script");?>.</p>
 
-                    <p><?php echo ("More information on how to use the multisite module is available by clicking the help icon on the 'Optional Site ID Selection' page");?>.
+                    <p><?php echo ("More information on how to use the multisite module is available by clicking the help icon on the 'Optional Site ID Selection' page");?>.</p>
 
                 </div>
             </div>
