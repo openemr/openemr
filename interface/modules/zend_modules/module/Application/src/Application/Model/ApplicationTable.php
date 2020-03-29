94ff635ad5652b1fce1a1ce46a6d6a41817a75f9
Yash Raj Bothra <47683556+yashrajbothra@users.noreply.github.com>
2020-03-29 13:14:59 +0530

    Fixed : Deprecated Warnings in zend code (#3183)

--- interface/modules/zend_modules/module/Application/src/Application/Model/ApplicationTable.php
+++ interface/modules/zend_modules/module/Application/src/Application/Model/ApplicationTable.php
@@ -353,7 +353,7 @@ class ApplicationTable extends AbstractTableGateway
     * @param    Date format set in GLOBALS
     * @return   Date format in PHP
     **/
-    public function dateFormat($format = null)
+    public static function dateFormat($format = null)
     {
         if ($format == "0") {
             $date_format = 'yyyy/mm/dd';
