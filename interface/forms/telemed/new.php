f5100e8d2f2360ca074275bbbba1a689637f80f3
ophthal <magauran@ophthal.org>
2020-04-03 16:55:25 -0400

    TeleHealth Encounter Form

---
+++ interface/forms/telemed/new.php
@@ -0,0 +1,84 @@
+<?php
+    /**
+     * forms/TeleVisit/new.php
+     *
+     *
+     * @package   OpenEMR
+     * @link      https://www.open-emr.org
+     * @author    Ray Magauran <rmagauran@gmail.com>
+     * @copyright Copyright (c) 2020 Raymond Magauran <rmagauran@gmail.com>
+     * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+     */
+    
+    
+    include_once("../../globals.php");
+    include_once("$srcdir/api.inc");
+    include_once("$srcdir/forms.inc");
+    
+    $form_name = "TeleHealth Visit";
+    $table_name = "form_telemed";
+    $form_folder = "telemed";
+    formHeader("Form: ".$form_name);
+    $returnurl = 'encounter_top.php';
+    
+    $pid = $_REQUEST['pid'];
+    
+    if (!$pid) {
+        $pid = $_SESSION['pid'];
+    } else {
+        $_SESSION['pid'] = $pid;
+    }
+    
+    if (!$user) {
+        $user = $_SESSION['authUser'];
+    }
+    
+    if (!$group) {
+        $group = $_SESSION['authProvider'];
+    }
+    
+    if (!$_SESSION['encounter']) {
+        $encounter = date("Ymd");
+    } else {
+        $encounter=$_SESSION['encounter'];
+    }
+//    var_dump($_SESSION);die();
+    $query = "select * from form_encounter where pid =? and encounter= ?";
+    $encounter_data = sqlQuery($query, array($pid,$encounter));
+    $encounter_date = $encounter_data['date'];
+
+    $query = "SELECT * " .
+        "FROM form_encounter AS fe, forms AS f WHERE " .
+        "fe.pid = ? AND fe.date = ? AND " .
+        "f.formdir = ? AND f.encounter = fe.encounter AND f.encounter=? AND f.deleted = 0";
+    $erow = sqlQuery($query, array($pid, $encounter_date, $form_folder, $encounter));
+ 
+    if ($erow['form_id'] > '0') {
+        formHeader("Redirecting....");
+        formJump('./view_form.php?formname=telemed&id='.attr($erow['form_id']).'&pid='.attr($pid));
+        formFooter();
+        exit;
+    }
+    $id = '1';
+    $provider_id = $encounter_data['provider_id'];
+    if ($provider_id < '1') {
+        //find the default provider_id from the calendar
+        $query = "select * from openemr_postcalendar_events where pc_pid=? and pc_eventDate=?";
+        $appt = sqlQuery($query, array($pid, $encounter_date));
+        $new_provider_id = $appt['pc_aid'];
+        if (($new_provider_id < '1') || (!$new_provider_id)) {
+            $new_provider_id = $_SESSION['authId'];
+        }
+        $provider_id = $new_provider_id;
+    }
+    $values = [];
+    $values['encounter'] = $encounter_data['encounter'];
+    $values['provider_id'] = $provider_id;
+    $newid = formSubmit($table_name, $values, $id, $provider_id);
+    addForm($encounter, "TeleHealth Visit", $newid, "telemed", $pid, $userauthorized);
+    
+     // echo "formJump('./view_form.php?formname=$form_folder&id='.attr($newid).'&pid='.attr($pid));";die();
+    formHeader("Redirecting....");
+    formJump('./view_form.php?formname='.$form_folder.'&id='.attr($newid).'&pid='.attr($pid));
+    formFooter();
+    exit;
---
+++ interface/forms/telemed/save.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * transfer summary form.
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Naina Mohamed <naina@capminds.com>
+ * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2012-2013 Naina Mohamed <naina@capminds.com> CapMinds Technologies
+ * @copyright Copyright (c) 2019 Brady Miller <brady.g.miller@gmail.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+
+require_once("../../globals.php");
+require_once("$srcdir/api.inc");
+require_once("$srcdir/forms.inc");
+
+use OpenEMR\Common\Csrf\CsrfUtils;
+
+if (!CsrfUtils::verifyCsrfToken($_POST["csrf_token_form"])) {
+    CsrfUtils::csrfNotVerified();
+}
+
+if (!$encounter) { // comes from globals.php
+    die(xlt("Internal error: we do not seem to be in an encounter!"));
+}
+
+$id = 0 + (isset($_GET['id']) ? $_GET['id'] : '');
+
+$sets = "date = CURDATE(),
+    pid = ?,
+    encounter = ? ,
+    user = ?,
+    groupname = ?,
+    authorized = ?,
+    activity = 1,
+    tm_duration = ?,
+    tm_subj = ?,
+    tm_obj = ?,
+    tm_imp = ?,
+    tm_plan = ?";
+/*
+echo "<pre>".$userauthorized;
+var_dump($_SESSION);
+var_dump($_REQUEST);
+die();
+*/
+if (empty($id)) {
+    $newid = sqlInsert(
+        "INSERT INTO form_telemed SET $sets",
+        [
+            $_SESSION["pid"],
+            $encounter,
+            $userauthorized,
+            $_SESSION["authGroup"],
+            $_SESSION["authUserID"],
+            $_POST["tm_duration"],
+            $_POST["tm_subj"],
+            $_POST["tm_obj"],
+            $_POST["tm_imp"],
+            $_POST["tm_plan"]
+        ]
+    );
+    addForm($encounter, "telemed", $newid, "telemed", $pid, $userauthorized);
+} else {
+    $sets = "date = CURDATE(),
+    pid = ?,
+    user = ?,
+    groupname = ?,
+    authorized = ?,
+    activity = 1,
+    tm_duration = ?,
+    tm_subj = ?,
+    tm_obj = ?,
+    tm_imp = ?,
+    tm_plan = ?";
+    sqlStatement(
+        "UPDATE form_telemed SET $sets WHERE id = ?",
+        [
+            $_SESSION["pid"],
+            $userauthorized,
+            $_SESSION["authGroup"],
+            $_SESSION["authUserID"],
+            $_POST["tm_duration"],
+            $_POST["tm_subj"],
+            $_POST["tm_obj"],
+            $_POST["tm_imp"],
+            $_POST["tm_plan"],
+            $id
+        ]
+    );
+}
+
+$_SESSION["encounter"] = $encounter;
+formHeader("Redirecting....");
+formJump();
+formFooter();
