c6d7a50785222ac7551b7f84fdf00f67e9f674c1
Tyler Wrenn <wrenntd@gmail.com>
2020-04-17 13:37:09 -0400

    First Phase of Portal RTL Support Fixes

--- portal/get_patient_info.php
+++ portal/get_patient_info.php
@@ -182,14 +182,14 @@ if (
     if (empty($userData)) {
         $logit->portalLog('login attempt', '', ($_POST['uname'] . ':not active patient'), '', '0');
         OpenEMR\Common\Session\SessionUtil::portalSessionCookieDestroy();
-        header('Location: ' . $landingpage . '&w');
+        header('Location: ' . $landingpage . '&w=inactive');
         exit();
     }
 
     if ($userData['email'] != $_POST['passaddon'] && $GLOBALS['enforce_signin_email']) {
         $logit->portalLog('login attempt', '', ($_POST['uname'] . ':invalid email'), '', '0');
         OpenEMR\Common\Session\SessionUtil::portalSessionCookieDestroy();
-        header('Location: ' . $landingpage . '&w');
+        header('Location: ' . $landingpage . '&w=bademail');
         exit();
     }
 
@@ -197,7 +197,7 @@ if (
         // Patient has not authorized portal, so escape
         $logit->portalLog('login attempt', '', ($_POST['uname'] . ':allow portal turned off'), '', '0');
         OpenEMR\Common\Session\SessionUtil::portalSessionCookieDestroy();
-        header('Location: ' . $landingpage . '&w');
+        header('Location: ' . $landingpage . '&w=noportal');
         exit();
     }
 
@@ -273,7 +273,7 @@ if (
     } else {
         $logit->portalLog('login', '', ($_POST['uname'] . ':not authorized'), '', '0');
         OpenEMR\Common\Session\SessionUtil::portalSessionCookieDestroy();
-        header('Location: ' . $landingpage . '&w');
+        header('Location: ' . $landingpage . '&w=unauth');
         exit();
     }
 } else { // problem with query
