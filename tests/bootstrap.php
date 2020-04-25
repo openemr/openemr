5e4bb73db496758d6c28a51878cf305a464f1a57
Dixon Whitmire <dixon.whitmire@ibm.com>
2020-04-25 13:39:02 -0400

    incorporating Brady's travis changes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,14 +2,18 @@
 
 declare(strict_types=1);
 
-# loads the composer class loader
-require_once(__DIR__ . "/../vendor/autoload.php");
-
-# configure session/globals
-@session_start();
-$_SESSION["site_id"] = "default";
-$backpic = "";
+$_GET['site'] = 'default';
 $ignoreAuth = true;
-
-# load globals
 require_once(__DIR__ . "/../interface/globals.php");
+
+# loads the composer class loader
+// require_once(__DIR__ . "/../vendor/autoload.php");
+
+// # configure session/globals
+// @session_start();
+// $_SESSION["site_id"] = "default";
+// $backpic = "";
+// $ignoreAuth = true;
+
+// # load globals
+// require_once(__DIR__ . "/../interface/globals.php");
