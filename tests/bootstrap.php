42e292e34c8c5a344485333de8eb3424ace19076
Brady Miller <brady.g.miller@gmail.com>
2020-04-24 21:47:42 -0700

    travis hail mary

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,14 +2,6 @@
 
 declare(strict_types=1);
 
-# loads the composer class loader
-require_once(__DIR__ . "/../vendor/autoload.php");
-
-# configure session/globals
-@session_start();
-$_SESSION["site_id"] = "default";
-$backpic = "";
+$_GET['site'] = 'default';
 $ignoreAuth = true;
-
-# load globals
 require_once(__DIR__ . "/../interface/globals.php");
