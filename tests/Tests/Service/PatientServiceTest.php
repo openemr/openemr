f220162fc33a5f94888c8b9faad39b020689da3b
Dixon Whitmire <dixonwh@gmail.com>
2020-04-26 15:29:39 -0400

    Adding Test Fixtures For Unit/Api Tests (#3415)
    * adding test fixtures for unit/api tests
    
    * addressing linting issues
    
    * implementing PR feedback and improvements
    
    * minimal config for local tests to pass
    
    * incorporating Brady's travis changes
    
    * reducing travis build for testing
    
    * testing configurable e2e base url
    
    * correct test fixture setup signature
    
    * restoring Travis CI configuration. Add E2E_BASE_URL to nginx compose configurations
    
    Co-authored-by: Dixon Whitmire <dixon.whitmire@ibm.com>

---
+++ tests/Tests/Service/PatientServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OpenEMR\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+use OpenEMR\Tests\Fixture\FixtureLoader;
+
+class PatientServiceTest extends TestCase
+{
+
+    public function testIt()
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,6 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+$_GET['site'] = 'default';
+$ignoreAuth = true;
+require_once(__DIR__ . "/../interface/globals.php");
