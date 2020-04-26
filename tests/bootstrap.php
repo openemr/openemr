ab7cc21d55b886c22e777bc0226ad3e1d8a84928
Dixon Whitmire <dixon.whitmire@ibm.com>
2020-04-25 23:16:08 -0400

    reducing travis build for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,15 +5,3 @@ declare(strict_types=1);
 $_GET['site'] = 'default';
 $ignoreAuth = true;
 require_once(__DIR__ . "/../interface/globals.php");
-
-# loads the composer class loader
-// require_once(__DIR__ . "/../vendor/autoload.php");
-
-// # configure session/globals
-// @session_start();
-// $_SESSION["site_id"] = "default";
-// $backpic = "";
-// $ignoreAuth = true;
-
-// # load globals
-// require_once(__DIR__ . "/../interface/globals.php");
