8210eb8f69904e84402dd863c8976cddd809787e
Sherwin Gaddis <sherwingaddis@gmail.com>
2020-05-15 08:23:28 -0400

    Adding the ability to delete a drug from patient chart

---
+++ library/deleteDrug.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *  @package   OpenEMR
+ *  @link      http://www.open-emr.org
+ *  @author    Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  @copyright Copyright (c )2020. Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ *
+ *
+ */
+
+require_once "../interface/globals.php";
+
+if ($_POST) {
+    $id = filter_input(INPUT_POST, 'drugId', FILTER_VALIDATE_INT);
+
+    $sql = "delete from prescriptions where id = ?";
+
+    sqlQuery($sql, [$id]);
+
+    echo "Done";
+}
