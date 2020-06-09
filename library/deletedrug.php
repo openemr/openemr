f81208638e7807f63541daa2519abec4edd4d4cf
Sherwin Gaddis <sherwingaddis@gmail.com>
2020-06-08 21:23:23 -0400

    Adding the ability to delete a drug from patient chart (#3520)
    * Adding the ability to delete a drug from patient chart
    
    * I forgot the csrf
    
    * adding translation and PSR fix
    
    * changed to isPromise and csrf token
    
    * Made the suggested changes
    
    * updates added
    
    * psr-4 updates
    
    * reverted ajax
    
    * Latest revisions
    
    * ACL correction
    
    Co-authored-by: Sherwin Gaddis <customercare@juggernautmarketplaces.com>

---
+++ library/deletedrug.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *  @package   OpenEMR
+ *  @link      http://www.open-emr.org
+ *  @author    Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  @copyright Copyright (c )2020. Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ *
+ *
+ */
+
+require_once "../interface/globals.php";
+
+use OpenEMR\Common\Csrf\CsrfUtils;
+use OpenEMR\Common\Acl\AclMain;
+
+//ensure user has proper access
+if (!AclMain::aclCheckCore('patient', 'rx', '', 'write')) {
+    echo xlt('ACL Administration Not Authorized');
+    exit;
+}
+
+$id = filter_input(INPUT_POST, 'drugId', FILTER_VALIDATE_INT);
+$id = trim($id);
+if (isset($id)) {
+    if (!CsrfUtils::verifyCsrfToken($_POST["csrf_token_form"])) {
+        CsrfUtils::csrfNotVerified();
+    }
+    try {
+        $sql = "delete from prescriptions where id = ?";
+        sqlQuery($sql, [$id]);
+        echo xlt("Done");
+    } catch (Exception $e) {
+        echo 'Error Message: ' . text($e->getMessage());
+    }
+}
