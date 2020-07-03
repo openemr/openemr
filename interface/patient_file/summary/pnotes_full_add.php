e12591923eb008c5807311d67d827edee988a08b
Cliff Su <stu01509@yahoo.com.tw>
2020-07-04 07:03:27 +0800

    refactor: refine the patient messages and add patient messages pages.
    Remove outdated code.
    Adjust the bootstrap grid system and table style.
    Adjust the style to follow the coding style.

--- interface/patient_file/summary/pnotes_full_add.php
+++ interface/patient_file/summary/pnotes_full_add.php
@@ -184,185 +184,172 @@ function submitform(attr) {
 }
 </script>
 </head>
-<body class="body_top">
-<div id="pnotes"> <!-- large outer DIV -->
-<?php
-$title_docname = "";
-if ($docid) {
-    $title_docname .= " " . xl("linked to document") . " ";
-    $d = new Document($docid);
-    $title_docname .= $d->get_url_file();
-}
+<body>
+    <div class="container"> <!-- large outer DIV -->
+        <div id="pnotes">
+            <?php
+            $title_docname = "";
+            if ($docid) {
+                $title_docname .= " " . xl("linked to document") . " ";
+                $d = new Document($docid);
+                $title_docname .= $d->get_url_file();
+            }
 
-if ($orderid) {
-    $title_docname .= " " . xl("linked to procedure order") . " $orderid";
-}
+            if ($orderid) {
+                $title_docname .= " " . xl("linked to procedure order") . " $orderid";
+            }
 
-$urlparms = "docid=" . attr_url($docid) . "&orderid= " . attr_url($orderid);
-?>
+            $urlparms = "docid=" . attr_url($docid) . "&orderid= " . attr_url($orderid);
+            ?>
 
-<form class='border-0' method='post' name='new_note' id="new_note" action='pnotes_full.php?<?php echo $urlparms; ?>'>
-<input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
-<div>
-    <div id="pnotes_title">
-        <span class="title"><?php echo xlt('Patient Message') . text($title_docname); ?></span>
-    </div>
-    <div>
-        <?php if ($noteid) { ?>
-            <!-- existing note -->
-            <a href="#" class="btn btn-primary" id="printnote"><span><?php echo xlt('View Printable Version'); ?></span></a>
-        <?php } ?>
-        <a class="btn btn-primary btn-lg" id='cancel' href='javascript:;'><?php echo xlt('Cancel'); ?></a>
-    </div>
-</div>
-<br/>
+            <form class='border-0' method='post' name='new_note' id="new_note" action='pnotes_full.php?<?php echo $urlparms; ?>'>
+                <input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="title"><?php echo xlt('Patient Message') . text($title_docname); ?></h2>
+                    </div>
+                </div>
+                <div class="btn-group">
+                    <?php if ($noteid) { ?>
+                        <!-- existing note -->
+                        <a href="#" class="btn btn-primary btn-print" id="printnote"><?php echo xlt('View Printable Version'); ?></a>
+                    <?php } ?>
+                    <a class="btn btn-secondary btn-cancel" id='cancel' href='#'><?php echo xlt('Cancel'); ?></a>
+                </div>
 
-<input type='hidden' name='mode' id="mode" value="new" />
-<input type='hidden' name='trigger' id="trigger" value="add" />
-<input type='hidden' name='offset' id="offset" value="<?php echo attr($offset); ?>" />
-<input type='hidden' name='form_active' id="form_active" value="<?php echo attr($form_active); ?>" />
-<input type='hidden' name='form_inactive' id="form_inactive" value="<?php echo attr($form_inactive); ?>" />
-<input type='hidden' name='noteid' id="noteid" value="<?php echo attr($noteid); ?>" />
-<input type='hidden' name='form_doc_only' id="form_doc_only" value="<?php echo attr($form_doc_only); ?>" />
-<table class='border-0' cellspacing='8'>
- <tr>
-  <td class='text'>
-    <?php
-    if ($noteid) {
-       // Modified 6/2009 by BM to incorporate the patient notes into the list_options listings
-        echo xlt('Amend Existing Message') .
-        "<b> &quot;" . generate_display_field(array('data_type' => '1','list_id' => 'note_type'), $title) . "&quot;</b>\n";
-    } else {
-        echo xlt('Add New Message') . "\n";
-    }
-    ?>
-  </td>
- </tr>
- <tr>
-  <td class='text'>
-    <br/>
-    <div class="form-group">
-   <label for='note_type' class='font-weight-bold'><?php echo xlt('Type'); ?>:</label>
-    <?php
-   // Added 6/2009 by BM to incorporate the patient notes into the list_options listings
-    generate_form_field(array('data_type' => 1,'field_id' => 'note_type','list_id' => 'note_type','empty_title' => 'SKIP'), $title);
-    ?>
-    </div>
-    <div class="form-group">
-   <label for='assigned_to' class='font-weight-bold'><?php echo xlt('To{{Destination}}'); ?>:</label>
-   <select name='assigned_to' id='assigned_to' class='form-control'>
-<?php
-while ($urow = sqlFetchArray($ures)) {
-    echo "    <option value='" . attr($urow['username']) . "'";
-    if ($urow['username'] == $assigned_to) {
-        echo " selected";
-    }
+                <br/>
 
-    echo ">" . text($urow['lname']);
-    if ($urow['fname']) {
-        echo text(", " . $urow['fname']);
-    }
+                <input type='hidden' name='mode' id="mode" value="new" />
+                <input type='hidden' name='trigger' id="trigger" value="add" />
+                <input type='hidden' name='offset' id="offset" value="<?php echo attr($offset); ?>" />
+                <input type='hidden' name='form_active' id="form_active" value="<?php echo attr($form_active); ?>" />
+                <input type='hidden' name='form_inactive' id="form_inactive" value="<?php echo attr($form_inactive); ?>" />
+                <input type='hidden' name='noteid' id="noteid" value="<?php echo attr($noteid); ?>" />
+                <input type='hidden' name='form_doc_only' id="form_doc_only" value="<?php echo attr($form_doc_only); ?>" />
 
-    echo "</option>\n";
-}
-?>
-   <option value=''><?php echo xlt('Mark Message as Completed'); ?></option>
-   </select>
-    </div>
-  </td>
- </tr>
-<?php if ($GLOBALS['messages_due_date']) { ?>
- <tr>
-     <td>
-         <div class="form-group">
-         <label for='datetime' class='font-weight-bold'><?php echo xlt('Due date'); ?>:</label>
-        <?php
-        generate_form_field(array('data_type' => 4, 'field_id' => 'datetime', 'edit_options' => 'F'), empty($datetime) ? date('Y-m-d H:i') : $datetime);
-        ?>
+                <div class="form-group mt-3">
+                    <label>
+                    <?php
+                    if ($noteid) {
+                    // Modified 6/2009 by BM to incorporate the patient notes into the list_options listings
+                        echo xlt('Amend Existing Message') .
+                        "<span class='font-weight-bold'> &quot;" . generate_display_field(array('data_type' => '1','list_id' => 'note_type'), $title) . "&quot;</span>\n";
+                    } else {
+                        echo xlt('Add New Message') . "\n";
+                    }
+                    ?>
+                    </label>
+                </div>
+
+                <div class="form-group mt-3">
+                    <label for='note_type' class='font-weight-bold'><?php echo xlt('Type'); ?>:</label>
+                    <?php
+                    // Added 6/2009 by BM to incorporate the patient notes into the list_options listings
+                    generate_form_field(array('data_type' => 1,'field_id' => 'note_type','list_id' => 'note_type','empty_title' => 'SKIP'), $title);
+                    ?>
+                </div>
+
+                <div class="form-group mt-3">
+                    <label for='assigned_to' class='font-weight-bold'><?php echo xlt('To{{Destination}}'); ?>:</label>
+                    <select name='assigned_to' id='assigned_to' class='form-control'>
+                        <?php
+                        while ($urow = sqlFetchArray($ures)) {
+                            echo "    <option value='" . attr($urow['username']) . "'";
+                            if ($urow['username'] == $assigned_to) {
+                                echo " selected";
+                            }
+
+                            echo ">" . text($urow['lname']);
+                            if ($urow['fname']) {
+                                echo text(", " . $urow['fname']);
+                            }
+
+                            echo "</option>\n";
+                        }
+                        ?>
+                        <option value=''><?php echo xlt('Mark Message as Completed'); ?></option>
+                    </select>
+                </div>
+
+                <?php if ($GLOBALS['messages_due_date']) { ?>
+                    <div class="form-group mt-3">
+                        <label for='datetime' class='font-weight-bold'><?php echo xlt('Due date'); ?>:</label>
+                        <?php
+                            generate_form_field(array('data_type' => 4, 'field_id' => 'datetime', 'edit_options' => 'F'), empty($datetime) ? date('Y-m-d H:i') : $datetime);
+                        ?>
+                    </div>
+                <?php } ?>
+
+                <div class="form-group mt-3">
+                    <?php
+                    if ($noteid) {
+                        $body = $prow['body'];
+                        $body = preg_replace(array('/(\sto\s)-patient-(\))/', '/(:\d{2}\s\()' . $patient_id . '(\sto\s)/'), '${1}' . $patientname . '${2}', $body);
+                        $body = nl2br(text(oeFormatPatientNote($body)));
+                        echo "<div class='text'>" . $body . "</div>";
+                    }
+                    ?>
+                </div>
+
+                <div class="form-group mt-3">
+                    <textarea name='note' id='note' class='form-control' rows='4' cols='58'></textarea>
+                </div>
+
+                <div class="btn-group mt-1">
+                    <?php if ($noteid) { ?>
+                        <!-- existing note -->
+                        <a href="#" class="btn btn-primary btn-save" id="newnote" title="<?php echo xla('Add as a new message'); ?>" ><?php echo xlt('Save as new message'); ?></a>
+                        <a href="#" class="btn btn-secondary" id="appendnote" title="<?php echo xla('Append to the existing message'); ?>"><?php echo xlt('Append this message'); ?></a>
+                    <?php } else { ?>
+                        <a href="#" class="btn btn-primary btn-save" id="newnote" title="<?php echo xla('Add as a new message'); ?>" ><?php echo xlt('Save as new message'); ?></a>
+                    <?php } ?>
+                </div>
+            </form>
+
+            <form class='border-0' method='post' name='update_activity' id='update_activity'
+                action="pnotes_full.php?<?php echo $urlparms; ?>">
+                <input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
+
+                <!-- start of previous notes DIV -->
+                <div class="pat_notes">
+                    <input type='hidden' name='mode' value="update" />
+                    <input type='hidden' name='offset' id='noteid' value="<?php echo attr($offset); ?>" />
+                    <input type='hidden' name='noteid' id='noteid' value="0" />
+                </div>
+            </form>
+
+            <table class='table table-borderless'>
+                <tr>
+                    <td>
+                        <?php
+                        if ($offset > ($N - 1)) {
+                            echo "   <a class='link' href='pnotes_full.php" .
+                            "?$urlparms" .
+                            "&form_active=" . attr_url($form_active) .
+                            "&form_inactive=" . attr_url($form_inactive) .
+                            "&form_doc_only=" . attr_url($form_doc_only) .
+                            "&offset=" . attr_url($offset - $N) . "' onclick='top.restoreSession()'>[" .
+                            xlt('Previous') . "]</a>\n";
+                        }
+                        ?>
+                    </td>
+                    <td class="text-right">
+                        <?php
+                        if ($result_count == $N) {
+                            echo "   <a class='link' href='pnotes_full.php" .
+                            "?$urlparms" .
+                            "&form_active=" . attr_url($form_active) .
+                            "&form_inactive=" . attr_url($form_inactive) .
+                            "&form_doc_only=" . attr_url($form_doc_only) .
+                            "&offset=" . attr_url($offset + $N) . "' onclick='top.restoreSession()'>[" .
+                            xlt('Next') . "]</a>\n";
+                        }
+                        ?>
+                    </td>
+                </tr>
+            </table>
         </div>
-     </td>
- </tr>
-    <?php
-}
-?>
-<tr>
-    <td>
-<?php
-if ($noteid) {
-    $body = $prow['body'];
-    $body = preg_replace(array('/(\sto\s)-patient-(\))/', '/(:\d{2}\s\()' . $patient_id . '(\sto\s)/'), '${1}' . $patientname . '${2}', $body);
-    $body = nl2br(text(oeFormatPatientNote($body)));
-    echo "<div class='text'>" . $body . "</div>";
-}
-?>
-    </td>
-</tr>
-<tr>
-    <td>
-        <textarea name='note' id='note' class='form-control' rows='4' cols='58'></textarea>
-    </td>
-</tr>
-<tr>
-    <td>
-        <?php if ($noteid) { ?>
-            <!-- existing note -->
-            <a href="#" class="btn btn-primary" id="newnote" title="<?php echo xla('Add as a new message'); ?>" ><?php echo xlt('Save as new message'); ?></a>
-            <a href="#" class="btn btn-rprimary" id="appendnote" title="<?php echo xla('Append to the existing message'); ?>"><?php echo xlt('Append this message'); ?></a>
-        <?php } else { ?>
-            <a href="#" class="btn btn-primary" id="newnote" title="<?php echo xla('Add as a new message'); ?>" ><?php echo xlt('Save as new message'); ?></a>
-        <?php } ?>
-    </td>
-</tr>
-
-</table>
-<br />
-<br />
-</form>
-<form class='border-0' method='post' name='update_activity' id='update_activity'
- action="pnotes_full.php?<?php echo $urlparms; ?>">
-<input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
-
-<!-- start of previous notes DIV -->
-<div class="pat_notes">
-
-
-<input type='hidden' name='mode' value="update" />
-<input type='hidden' name='offset' id='noteid' value="<?php echo attr($offset); ?>" />
-<input type='hidden' name='noteid' id='noteid' value="0" />
-</form>
-
-<table class='border-0' width='400px' cellpadding='0' cellspacing='0'>
- <tr>
-  <td>
-<?php
-if ($offset > ($N - 1)) {
-    echo "   <a class='link' href='pnotes_full.php" .
-    "?$urlparms" .
-    "&form_active=" . attr_url($form_active) .
-    "&form_inactive=" . attr_url($form_inactive) .
-    "&form_doc_only=" . attr_url($form_doc_only) .
-    "&offset=" . attr_url($offset - $N) . "' onclick='top.restoreSession()'>[" .
-    xlt('Previous') . "]</a>\n";
-}
-?>
-  </td>
-  <td align='right'>
-<?php
-if ($result_count == $N) {
-    echo "   <a class='link' href='pnotes_full.php" .
-    "?$urlparms" .
-    "&form_active=" . attr_url($form_active) .
-    "&form_inactive=" . attr_url($form_inactive) .
-    "&form_doc_only=" . attr_url($form_doc_only) .
-    "&offset=" . attr_url($offset + $N) . "' onclick='top.restoreSession()'>[" .
-    xlt('Next') . "]</a>\n";
-}
-?>
-  </td>
- </tr>
-</table>
-
-</div> <!-- close the previous-notes DIV -->
+    </div> <!-- close the previous-notes DIV -->
 
 <script>
 
@@ -396,7 +383,6 @@ if ($noteid /* && $title == 'New Document' */) {
 <script>
 
 // jQuery stuff to make the page a little easier to use
-
 $(function () {
     $("#appendnote").click(function() { AppendNote(); });
     $("#newnote").click(function() { NewNote(); });
@@ -472,7 +458,7 @@ $(function () {
         <?php $datetimepicker_formatInput = true; ?>
         <?php require($GLOBALS['srcdir'] . '/js/xl/jquery-datetimepicker-2-5-4.js.php'); ?>
         ,minDate : 0 //only future
-    })
+    });
 
 });
 </script>
