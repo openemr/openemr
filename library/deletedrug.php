52f162c6cd3a66bb1c4e6706456b5b8e847af0c2
Sherwin Gaddis <sherwingaddis@gmail.com>
2020-07-29 04:20:15 -0400

    Need to remove drug from the patient medication list also. (#3745)
    * Need to remove drug from the patient medication list also.
    
    * Added the type field of medication
    
    * Updated logging and binding. Used sqlStatment.
    
    * PSR updates
    
    * Changes to the event logger dropping logstin

--- library/deletedrug.php
+++ library/deletedrug.php
@@ -14,6 +14,7 @@ require_once "../interface/globals.php";
 
 use OpenEMR\Common\Acl\AclMain;
 use OpenEMR\Common\Csrf\CsrfUtils;
+use OpenEMR\Common\Logging\EventAuditLogger;
 
 //ensure user has proper access
 if (!AclMain::aclCheckCore('patient', 'rx', '', 'write')) {
@@ -27,11 +28,49 @@ if (isset($id)) {
     if (!CsrfUtils::verifyCsrfToken($_POST["csrf_token_form"])) {
         CsrfUtils::csrfNotVerified();
     }
+
+    /**
+     * find the drug name in the prescription table
+     */
+    try {
+        $drug_name = "SELECT patient_id, drug FROM prescriptions WHERE id = ?";
+        $dn = sqlQuery($drug_name, [$id]);
+    } catch (Exception $e) {
+        echo 'Caught exception ', text($e->getMessage()), "\n";
+        if ($e->getMessage()) {
+            exit;
+        }
+    }
+
+    /**
+     * remove drug from the medication list if exist
+     */
+    try {
+        $pid = $dn['patient_id'];
+        $drugname = $dn['drug'];
+        if (!empty($drugname)) {
+            $medicationlist = "DELETE FROM lists WHERE pid = ? AND type = 'medication' AND title = ?";
+            sqlStatement($medicationlist, [$pid, $drugname]);
+            EventAuditLogger::instance()->newEvent("delete", $_SESSION['authUser'], $_SESSION['authProvider'], 1, $drugname, $pid);
+        }
+    } catch (Exception $e) {
+        echo 'Caught exception ', text($e->getMessage()), "\n";
+        if ($e->getMessage()) {
+            exit;
+        }
+    }
+
+    /**
+     * remove drug from the prescription
+     */
     try {
         $sql = "delete from prescriptions where id = ?";
         sqlQuery($sql, [$id]);
-        echo xlt("Done");
     } catch (Exception $e) {
-        echo 'Error Message: ' . text($e->getMessage());
+        echo 'Caught exception ', text($e->getMessage()), "\n";
+        if ($e->getMessage()) {
+            exit;
+        }
     }
 }
+echo xlt("Finished Deleting");
