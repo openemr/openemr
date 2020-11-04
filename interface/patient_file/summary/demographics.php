8158cd320a17bf70f76a730e6f8617ae4f0f1c80
stephen waite <stephen.waite@cmsvt.com>
2020-11-04 13:41:35 -0500

    typo

--- interface/patient_file/summary/demographics.php
+++ interface/patient_file/summary/demographics.php
@@ -1453,7 +1453,7 @@ while ($gfrow = sqlFetchArray($gfres)) {
                                 "INNER JOIN categories_to_documents " .
                                 "ON categories_to_documents.document_id=documents.id " .
                                 "WHERE categories_to_documents.category_id=? " .
-                                "AND documents.foreign_id=? AND documents.deleted = 0" .
+                                "AND documents.foreign_id=? AND documents.deleted = 0 " .
                                 "ORDER BY documents.date DESC";
                                 $resNew2 = sqlStatement($query, array($categoryId, $pid));
                                 $limitCounter = 0; // limit to one entry per category
