7b9cde287faf869c73bc2a191a05d41a1517941d
Brady Miller <brady.g.miller@gmail.com>
2020-11-29 02:42:50 -0800

    getting even more, more, more desperate

--- tests/Tests/E2e/Pages/UsersTab.php
+++ tests/Tests/E2e/Pages/UsersTab.php
@@ -33,10 +33,12 @@ class UsersTab
     {
         // need to switch to the iframe
         $crawler = $this->switchToIFrame(WebDriverBy::xpath(UsersTab::ADMIN_IFRAME));
+        $this->client->waitFor(UsersTab::ADD_USER_BUTTON);
         $crawler->filterXPath(UsersTab::ADD_USER_BUTTON)->click();
 
         $this->client->switchTo()->defaultContent();
         $crawler = $this->switchToIFrame(WebDriverBy::xpath(UsersTab::NEW_USER_IFRAME));
+        $this->client->waitFor(UsersTab::NEW_USER_BUTTON);
         $newUser = $crawler->filterXPath(UsersTab::NEW_USER_BUTTON)->form();
 
         $newUser['rumple'] = $username;
@@ -49,7 +51,7 @@ class UsersTab
         $crawler->filterXPath(UsersTab::CREATE_USER_BUTTON)->click();
 
         $this->client->switchTo()->defaultContent();
-        $this->switchToIFrame(WebDriverBy::xpath(UsersTab::ADMIN_IFRAME));
+        $crawler = $this->switchToIFrame(WebDriverBy::xpath(UsersTab::ADMIN_IFRAME));
 
         $this->client->waitFor("//table//a[text()='$username']");
 
