6fa971b861ce0fbad1a51aba60e709b3a6e39864
Brady Miller <brady.g.miller@gmail.com>
2020-11-28 23:03:42 -0800

    retrial

--- tests/Tests/E2e/Pages/UsersTab.php
+++ tests/Tests/E2e/Pages/UsersTab.php
@@ -50,11 +50,11 @@ class UsersTab
         $this->client->switchTo()->defaultContent();
         $crawler = $this->switchToIFrame(WebDriverBy::xpath(UsersTab::ADMIN_IFRAME));
 
-        $this->client->takeScreenshot('../artifacts/screen.png');
+        $this->client->takeScreenshot('artifacts/screen.png');
 
         $this->client->waitFor("//table//a[text()='$username']");
 
-        $this->client->takeScreenshot('../artifacts/screen2.png');
+        $this->client->takeScreenshot('artifacts/screen2.png');
 
         $this->client->switchTo()->defaultContent();
     }
