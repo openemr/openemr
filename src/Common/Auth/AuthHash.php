18b77d044b955f23b0f5b9d3a03fc953ccfcadc1
Diogo Vidigal <dcvidiga@gmail.com>
2020-12-18 16:34:47 +0000

    Php Styling --------------------- After megration hashed salted passwords from 4.2.0 couldn't login due to the drop of the salt. This will fix that migration and it should happen only one time. After the first successful login, the hash password will be updated

--- src/Common/Auth/AuthHash.php
+++ src/Common/Auth/AuthHash.php
@@ -192,12 +192,11 @@ class AuthHash
         if (!empty(preg_match('/^\$6\$rounds=/', $hash))) {
             // Process SHA512HASH algo separately, since uses crypt
             $valid = hash_equals($hash, crypt($password, $hash));
-        } else if(preg_match('/^\$2a\$05\$/', $hash)){ 
-            $salt = explode("$",$hash);
-            $salt = "$2a$05$".substr($salt[3],0, 21)."$";
+        } else if (preg_match('/^\$2a\$05\$/', $hash)) {
+            $salt = explode("$", $hash);
+            $salt = "$2a$05$".substr($salt[3],0, 21) . "$";
             $valid = hash_equals($hash, crypt($password, $salt));
-        }
-        else {
+        } else {
             // Process algos supported by standard password_verify
             $valid = password_verify($password, $hash);
         }
