6b526c024c02156c3b02db79c2fe9a88c233676c
Brady Miller <brady.g.miller@gmail.com>
2020-12-19 12:55:48 -0800

    remove mu3 settings since 6.0.0 will not have mu3

--- library/globals.inc.php
+++ library/globals.inc.php
@@ -2580,7 +2580,7 @@ $GLOBALS_METADATA = array(
         'audit_events_query' => array(
             xl('Audit Logging SELECT Query'),
             'bool',                           // data type
-            '1',                              // default
+            '0',                              // default
             xl('Enable logging of all SQL SELECT queries.') . ' (' . xl('Note that Audit Logging needs to be enabled above') . ')'
         ),
 
@@ -2668,7 +2668,7 @@ $GLOBALS_METADATA = array(
                 '1' => xl('Hide print feature'),
                 '2' => xl('Log entire document'),
             ),
-            '2',                               // default
+            '0',                               // default
             xl('Individual pages can override 2nd and 3rd options by implementing a log message.')
         ),
 
