b432f497e810aac53ea42356ce674efb8d966d77
Stephen Nielson <snielson@discoverandchange.com>
2020-12-22 19:18:51 -0500

    Missing copyright header

--- tests/Tests/Unit/FHIR/SMART/SMARTLaunchTokenTest.php
+++ tests/Tests/Unit/FHIR/SMART/SMARTLaunchTokenTest.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * SMARTLaunchTokenTest Unit tests the SmartLaunchTokenTest for serialization & deserialization of the token.
+ * @package openemr
+ * @link      http://www.open-emr.org
+ * @author    Stephen Nielson <stephen@nielson.org>
+ * @copyright Copyright (c) 2020 Stephen Nielson <stephen@nielson.org>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
 
 namespace OpenEMR\Tests\Unit\FHIR\SMART;
 
