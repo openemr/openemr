22e55c923150f9314c7fcb58504d1261f4a99ad3
Brady Miller <brady.g.miller@gmail.com>
2021-01-05 12:49:53 -0800

    preparing for 6.0.0 release

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 $v_major = '6';
 $v_minor = '0';
 $v_patch = '0';
-$v_tag   = '-dev'; // minor revision number, should be empty for production releases
+$v_tag   = ''; // minor revision number, should be empty for production releases
 
 // A real patch identifier. This is incremented when we release a patch for a
 // production release. Note the above $v_patch variable is a misnomer and actually
