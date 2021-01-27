a620252c561e0ab505944ab220451d276216b680
Jerry Padgett <sjpadgett@gmail.com>
2021-01-27 16:14:38 -0500

    Updated procedures (#4194)
    * Updated procedures
    - updated filters
    - fetch results all or by each lab provider
    - labcorp requisition and custom HL7
    - results parse handles more labs
    - specimen labels
    
    * - ci fixes
    
    * fixups
    
    * database fix
    
    * saved mpdf output to variable to avoid the temp file
    
    * -  fix req document save
    
    Co-authored-by: Brady Miller <brady.g.miller@gmail.com>

--- interface/procedure_tools/gen_universal_hl7/gen_hl7_order.inc.php
+++ interface/procedure_tools/gen_universal_hl7/gen_hl7_order.inc.php
@@ -18,7 +18,6 @@
 *
 * @package   OpenEMR
 * @author    Rod Roark <rod@sunsetsystems.com>
-* @author    Jerry Padgett <sjpadgett@gmail.com>
 */
 
 /*
@@ -113,11 +112,11 @@ function hl7Relation($s)
     $tmp = strtolower($s);
     if ($tmp == 'self' || $tmp == '') {
         return 'self';
-    } elseif ($tmp == 'spouse') {
+    } else if ($tmp == 'spouse') {
         return 'spouse';
-    } elseif ($tmp == 'child') {
+    } else if ($tmp == 'child') {
         return 'child';
-    } elseif ($tmp == 'other') {
+    } else if ($tmp == 'other') {
         return 'other';
     }
 
@@ -193,7 +192,7 @@ function gen_hl7_order($orderid, &$out)
 
     $porow = sqlQuery(
         "SELECT " .
-        "po.date_collected, po.date_ordered, po.order_priority, " .
+        "po.*, " .
         "pp.*, " .
         "pd.pid, pd.pubpid, pd.fname, pd.lname, pd.mname, pd.DOB, pd.ss, " .
         "pd.phone_home, pd.phone_biz, pd.sex, pd.street, pd.city, pd.state, pd.postal_code, " .
@@ -215,16 +214,16 @@ function gen_hl7_order($orderid, &$out)
 
     $pcres = sqlStatement(
         "SELECT " .
-        "pc.procedure_code, pc.procedure_name, pc.procedure_order_seq, pc.diagnoses, pt.specimen " .
-        "FROM procedure_order_code AS pc, procedure_type as pt " .
+        "pc.procedure_code, pc.procedure_name, pc.procedure_order_seq, pc.diagnoses " .
+        "FROM procedure_order_code AS pc " .
         "WHERE " .
         "pc.procedure_order_id = ? AND " .
-        "pc.procedure_name = pt.name AND " .
         "pc.do_not_send = 0 " .
         "ORDER BY pc.procedure_order_seq",
         array($orderid)
     );
 
+    $padOrderId = trim($porow['send_fac_id']) . "-" . trim(str_pad((string)$orderid, 4, "0", STR_PAD_LEFT));
   // Message Header
     $out .= "MSH" .
     $d1 . "$d2~\\&" .               // Encoding Characters (delimiters)
@@ -235,7 +234,7 @@ function gen_hl7_order($orderid, &$out)
     $d1 . date('YmdHis', $today) .  // Date and time of this message
     $d1 .
     $d1 . 'ORM' . $d2 . 'O01' .     // Message Type
-    $d1 . str_pad((string)$orderid, 4, "0", STR_PAD_LEFT) .  // Unique Message Number
+    $d1 . $padOrderId .  // Unique Message Number
     $d1 . $porow['DorP'] .          // D=Debugging, P=Production
     $d1 . '2.3' .                   // HL7 Version ID
     $d0;
@@ -247,7 +246,7 @@ function gen_hl7_order($orderid, &$out)
     $d1 . $porow['pid'] .            // Patient ID (required)
     $d1 .                            // Alternate Patient ID (not required)
     $d1 . hl7Text($porow['lname']) .
-      $d2 . hl7Text($porow['fname']);
+    $d2 . hl7Text($porow['fname']);
     if ($porow['mname']) {
         $out .= $d2 . hl7Text($porow['mname']);
     }
@@ -258,10 +257,10 @@ function gen_hl7_order($orderid, &$out)
     $d1 . hl7Sex($porow['sex'])  .   // Sex: M, F or U
     $d1 . $d1 .
     $d1 . hl7Text($porow['street']) .
-      $d2 .
-      $d2 . hl7Text($porow['city']) .
-      $d2 . hl7Text($porow['state']) .
-      $d2 . hl7Zip($porow['postal_code']) .
+    $d2 .
+    $d2 . hl7Text($porow['city']) .
+    $d2 . hl7Text($porow['state']) .
+    $d2 . hl7Zip($porow['postal_code']) .
     $d1 .
     $d1 . hl7Phone($porow['phone_home']) .
     $d1 . hl7Phone($porow['phone_biz']) .
@@ -272,7 +271,6 @@ function gen_hl7_order($orderid, &$out)
     $d0;
 
     // NTE segment(s).
-    //$msql = sqlStatement("SELECT title FROM lists WHERE type='medication' AND pid='".$porow['pid']."'");
     $msql = sqlStatement("SELECT drug FROM prescriptions WHERE active=1 AND patient_id=?", [$porow['pid']]);
     $drugs = array();
     while ($mres = sqlFetchArray($msql)) {
@@ -283,7 +281,7 @@ function gen_hl7_order($orderid, &$out)
     $out .= "NTE" .
         $d1 . "1" .
         $d1 . "L" .
-        $d1 . "Medications:" . $med_list .
+        $d1 . "medications " . $med_list .
         $d0;
 
   // Patient Visit.
@@ -293,84 +291,94 @@ function gen_hl7_order($orderid, &$out)
     $d1 .                                 // Patient Location (for inpatient only?)
     $d1 . $d1 . $d1 .
     $d1 . hl7Text($porow['docnpi']) .     // Attending Doctor ID
-      $d2 . hl7Text($porow['doclname']) . // Last Name
-      $d2 . hl7Text($porow['docfname']) . // First Name
+    $d2 . hl7Text($porow['doclname']) . // Last Name
+    $d2 . hl7Text($porow['docfname']) . // First Name
     str_repeat($d1, 11) .                 // PV1 8 to 18 all empty
     $d1 . $porow['encounter'] .           // Encounter Number
     str_repeat($d1, 13) .                 // PV1 20 to 32 all empty
     $d0;
 
   // Insurance stuff.
+    $ins_type = trim($porow['billing_type']);
     $payers = loadPayerInfo($porow['pid'], $porow['date_ordered']);
     $setid = 0;
-    foreach ($payers as $payer) {
-        $payer_object = $payer['object'];
-        $payer_address = $payer_object->get_address();
-        $out .= "IN1" .
-        $d1 . ++$setid .                                // Set ID
-        $d1 .                                           // Insurance Plan Identifier ??
-        $d1 . hl7Text($payer['company']['id']) .        // Insurance Company ID
-        $d1 . hl7Text($payer['company']['name'])   .    // Insurance Company Name
-        $d1 . hl7Text($payer_address->get_line1()) .    // Street Address
-        $d2 .
-        $d2 . hl7Text($payer_address->get_city()) .   // City
-        $d2 . hl7Text($payer_address->get_state()) .  // State
-        $d2 . hl7Zip($payer_address->get_zip()) .     // Zip Code
-        $d1 .
-        $d1 . hl7Phone($payer_object->get_phone()) .    // Phone Number
-        $d1 . hl7Text($payer['data']['group_number']) . // Insurance Company Group Number
-        str_repeat($d1, 7) .                            // IN1 9-15 all empty
-        $d1 . hl7Text($payer['data']['subscriber_lname']) .   // Insured last name
-        $d2 . hl7Text($payer['data']['subscriber_fname']) . // Insured first name
-        $d2 . hl7Text($payer['data']['subscriber_mname']) . // Insured middle name
-        $d1 . hl7Relation($payer['data']['subscriber_relationship']) .
-        $d1 . hl7Date($payer['data']['subscriber_DOB']) .     // Insured DOB
-        $d1 . hl7Date($payer['data']['subscriber_street']) .  // Insured Street Address
-        $d2 .
-        $d2 . hl7Text($payer['data']['subscriber_city']) .  // City
-        $d2 . hl7Text($payer['data']['subscriber_state']) . // State
-        $d2 . hl7Zip($payer['data']['subscriber_postal_code']) . // Zip
-        $d1 .
-        $d1 .
-        $d1 . $setid .                                  // 1=Primary, 2=Secondary, 3=Tertiary
-        str_repeat($d1, 13) .                           // IN1-23 to 35 all empty
-        $d1 . hl7Text($payer['data']['policy_number']) . // Policy Number
-        str_repeat($d1, 12) .                           // IN1-37 to 48 all empty
-        $d0;
-
-        // IN2 segment omitted.
+    if ($ins_type == 'T') {
+        // only send primary and secondary insurance
+        foreach ($payers as $payer) {
+            $payer_object = $payer['object'];
+            $payer_address = $payer_object->get_address();
+            $out .= "IN1" .
+                $d1 . ++$setid .                                // Set ID
+                $d1 .                                           // Insurance Plan Identifier ??
+                $d1 . hl7Text($payer['company']['ins_comp_id']) .  // Insurance Company ID
+                $d1 . hl7Text($payer['company']['name']) .    // Insurance Company Name
+                $d1 . hl7Text($payer_address->get_line1()) .    // Street Address
+                $d2 .
+                $d2 . hl7Text($payer_address->get_city()) .   // City
+                $d2 . hl7Text($payer_address->get_state()) .  // State
+                $d2 . hl7Zip($payer_address->get_zip()) .     // Zip Code
+                $d1 .
+                $d1 . hl7Phone($payer_object->get_phone()) .    // Phone Number
+                $d1 . hl7Text($payer['data']['group_number']) . // Insurance Company Group Number
+                str_repeat($d1, 7) .                            // IN1 9-15 all empty
+                $d1 . hl7Text($payer['data']['subscriber_lname']) .   // Insured last name
+                $d2 . hl7Text($payer['data']['subscriber_fname']) . // Insured first name
+                $d2 . hl7Text($payer['data']['subscriber_mname']) . // Insured middle name
+                $d1 . hl7Relation($payer['data']['subscriber_relationship']) .
+                $d1 . hl7Date($payer['data']['subscriber_DOB']) .     // Insured DOB
+                $d1 . hl7Date($payer['data']['subscriber_street']) .  // Insured Street Address
+                $d2 .
+                $d2 . hl7Text($payer['data']['subscriber_city']) .  // City
+                $d2 . hl7Text($payer['data']['subscriber_state']) . // State
+                $d2 . hl7Zip($payer['data']['subscriber_postal_code']) . // Zip
+                $d1 .
+                $d1 .
+                $d1 . $setid .                                  // 1=Primary, 2=Secondary, 3=Tertiary
+                str_repeat($d1, 13) .                           // IN1-23 to 35 all empty
+                $d1 . hl7Text($payer['data']['policy_number']) . // Policy Number
+                str_repeat($d1, 12) .                           // IN1-37 to 48 all empty
+                $d0;
+            if ($setid === 2) {
+                break;
+            }
+        }
+        if ($setid === 0) {
+            return "\nInsurance is being billed but patient does not have any payers on record!";
+        }
+    } else { // no insurance record
+        ++$setid;
+        $out .= "IN1|$setid||||||||||||||||||||||||||||||||||||||||||||||$ins_type" . $d0;
     }
+    if ($ins_type != 'C') {
+        $out .= "GT1" .
+            $d1 . "1" .                      // Set ID (always just 1 of these)
+            $d1 .
+            $d1 . hl7Text($porow['lname']) .
+            $d2 . hl7Text($porow['fname']);
+        if ($porow['mname']) {
+            $out .= $d2 . hl7Text($porow['mname']);
+        }
 
-  // Guarantor. OpenEMR doesn't have these so use the patient.
-    $out .= "GT1" .
-    $d1 . "1" .                      // Set ID (always just 1 of these)
-    $d1 .
-    $d1 . hl7Text($porow['lname']) .
-    $d2 . hl7Text($porow['fname']);
-    if ($porow['mname']) {
-        $out .= $d2 . hl7Text($porow['mname']);
+        $out .=
+            $d1 .
+            $d1 . hl7Text($porow['street']) .
+            $d2 .
+            $d2 . hl7Text($porow['city']) .
+            $d2 . hl7Text($porow['state']) .
+            $d2 . hl7Zip($porow['postal_code']) .
+            $d1 . hl7Phone($porow['phone_home']) .
+            $d1 . hl7Phone($porow['phone_biz']) .
+            $d1 . hl7Date($porow['DOB']) .   // DOB
+            $d1 . hl7Sex($porow['sex']) .   // Sex: M, F or U
+            $d1 .
+            $d1 . 'self' .                   // Relationship
+            $d1 . hl7SSN($porow['ss']) .
+            $d0;
     }
-
-    $out .=
-    $d1 .
-    $d1 . hl7Text($porow['street']) .
-    $d2 .
-    $d2 . hl7Text($porow['city']) .
-    $d2 . hl7Text($porow['state']) .
-    $d2 . hl7Zip($porow['postal_code']) .
-    $d1 . hl7Phone($porow['phone_home']) .
-    $d1 . hl7Phone($porow['phone_biz']) .
-    $d1 . hl7Date($porow['DOB']) .   // DOB
-    $d1 . hl7Sex($porow['sex'])  .   // Sex: M, F or U
-    $d1 .
-    $d1 . 'self' .                   // Relationship
-    $d1 . hl7SSN($porow['ss']) .
-    $d0;
-
   // Common Order.
     $out .= "ORC" .
     $d1 . "NW" .                     // New Order
-    $d1 . str_pad((string)$orderid, 4, "0", STR_PAD_LEFT) . // Placer Order Number
+    $d1 . $padOrderId . // Placer Order Number
     str_repeat($d1, 6) .             // ORC 3-8 not used
     $d1 . date('YmdHis') .           // Transaction date/time
     $d1 . $d1 .
@@ -386,15 +394,14 @@ function gen_hl7_order($orderid, &$out)
         // Observation Request.
         $out .= "OBR" .
         $d1 . ++$setid .                              // Set ID
-        $d1 . str_pad((string)$orderid, 4, "0", STR_PAD_LEFT) . // Placer Order Number
+        $d1 . $padOrderId . // Placer Order Number
         $d1 .
         $d1 . hl7Text($pcrow['procedure_code']) .
         $d2 . hl7Text($pcrow['procedure_name']) .
         $d1 . hl7Priority($porow['order_priority']) . // S=Stat, R=Routine
         $d1 .
         $d1 . hl7Time($porow['date_collected']) .     // Observation Date/Time
-        str_repeat($d1, 8) .                          // OBR 8-15 not used
-        $dl . hl7Text($pcrow['specimen']) .           // Specimen source aka OBR-15
+        str_repeat($d1, 8) .                  // OBR 8-15 not used
         $d1 . hl7Text($porow['docnpi']) .             // Physician ID
         $d2 . hl7Text($porow['doclname']) .         // Last Name
         $d2 . hl7Text($porow['docfname']) .         // First Name
@@ -404,11 +411,15 @@ function gen_hl7_order($orderid, &$out)
         $d1 . '0' .                                   // ?
         $d0;
 
-        // Diagnoses.  Currently hard-coded for ICD9 and we'll surely want to make
+        // Diagnoses.  Currently hard-coded for ICD10 and we'll surely want to make
         // this more flexible (probably when some lab needs another diagnosis type).
+        $test_diagnosis = $pcrow['diagnoses'];
+        if (empty($test_diagnosis)) { // add default primary dianosis
+            $test_diagnosis = $porow['order_diagnosis'];
+        }
         $setid2 = 0;
-        if (!empty($pcrow['diagnoses'])) {
-            $relcodes = explode(';', $pcrow['diagnoses']);
+        if (!empty($test_diagnosis)) {
+            $relcodes = explode(';', $test_diagnosis);
             foreach ($relcodes as $codestring) {
                 if ($codestring === '') {
                     continue;
@@ -421,9 +432,9 @@ function gen_hl7_order($orderid, &$out)
 
                 $desc = lookup_code_descriptions($codestring);
                 $out .= "DG1" .
-                $d1 . ++$setid2 .                         // Set ID
-                $d1 .                                     // Diagnosis Coding Method
-                $d1 . $code .                             // Diagnosis Code
+                    $d1 . ++$setid2 .                         // Set ID
+                    $d1 .                                     // Diagnosis Coding Method
+                    $d1 . $code .                             // Diagnosis Code
                     $d2 . hl7Text($desc) .                    // Diagnosis Description
                     $d2 . "I10" .                             // Diagnosis Type
                     $d1 . $d0;
@@ -454,9 +465,9 @@ function gen_hl7_order($orderid, &$out)
               $datatype = 'ST';
             if ($fldtype == 'N') {
                 $datatype = "NM";
-            } elseif ($fldtype == 'D') {
+            } else if ($fldtype == 'D') {
                   $answer = hl7Date($answer);
-            } elseif ($fldtype == 'G') {
+            } else if ($fldtype == 'G') {
                   $weeks = intval($answer / 7);
                   $days = $answer % 7;
                   $answer = $weeks . 'wks ' . $days . 'days';
@@ -513,7 +524,7 @@ function send_hl7_order($ppid, $out)
         header("Content-Description: File Transfer");
         echo $out;
         exit;
-    } elseif ($protocol == 'SFTP') {
+    } else if ($protocol == 'SFTP') {
         // Compute the target path/file name.
         $filename = $msgid . '.txt';
         if ($pprow['orders_path']) {
@@ -529,7 +540,7 @@ function send_hl7_order($ppid, $out)
         if (!$sftp->put($filename, $out)) {
             return xl('Creating this file on remote host failed') . ": '$filename'";
         }
-    } elseif ($protocol == 'FS') {
+    } else if ($protocol == 'FS') {
         // Compute the target path/file name.
         $filename = $msgid . '.txt';
         if ($pprow['orders_path']) {
@@ -543,7 +554,7 @@ function send_hl7_order($ppid, $out)
         } else {
             return xl('Cannot create file') . ' "' . "$filename" . '"';
         }
-    } else { // TBD: Insert "else if ($protocol == '???') {...}" to support other protocols.
+    } else {// TBD: Insert "else if ($protocol == '???') {...}" to support other protocols.
         return xl('This protocol is not implemented') . ": '$protocol'";
     }
 
