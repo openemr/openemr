0b9a4e38250280979743944056fdc1a989b61985
Jerry Padgett <sjpadgett@gmail.com>
2021-02-06 10:50:23 -0500

    Some portal cleanup and fixes (#4221)
    * Some portal cleanup and fixes
    - add portal session destroy in session utils to prevent logging to system log.
    - remove unneeded demographics
    - fix layout excludes!!!
    - default providers to Admin
    - only allow/list providers that have portal auth in their users permissions
    - fix register again!
    
    * - only allow auth portal providers in mail.
    
    * - add CurrentDate and CurrentTime tags to both template downloads
    - fix portal secure messaging
    - minor nitpick of utility escaping function vars
    
    * - style compromise between FF, Chrome!
    
    (cherry picked from commit 0685a723831f5116a759524a798dfe2c2a062d63)

--- src/Common/Session/SessionUtil.php
+++ src/Common/Session/SessionUtil.php
@@ -147,7 +147,23 @@ class SessionUtil
 
     public static function portalSessionCookieDestroy(): void
     {
-        self::standardSessionCookieDestroy();
+        // Destroy the cookie
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            [
+                'expires' => time() - 42000,
+                'path' => $params["path"],
+                'domain' => $params["domain"],
+                'secure' => $params["secure"],
+                'httponly' => $params["httponly"],
+                'samesite' => $params["samesite"]
+            ]
+        );
+
+        // Destroy the session.
+        session_destroy();
     }
 
     public static function apiSessionStart($web_root): void
