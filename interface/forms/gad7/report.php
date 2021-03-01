d2899bc862e834b4efdd10be9158a991f7cc7aa2
ruth <ruth@muswell.me.uk>
2021-03-01 12:40:58 +0000

    corrections to style

--- interface/forms/gad7/report.php
+++ interface/forms/gad7/report.php
@@ -20,9 +20,9 @@ function gad7_report($pid, $encounter, $cols, $id)
     $count = 0;
     $gad7_total = 0;
 
-    $str_values = [0=>'Not at all (0)',1 => 'Several days (1)',2 => 'More than half of days (2)',3 => 'Nearly every day (3)'];
+    $str_values = [0 => 'Not at all (0)',1 => 'Several days (1)',2 => 'More than half of days (2)',3 => 'Nearly every day (3)'];
 
-    $str_difficulty_values = [0=>'Not at all (0)',1=>"Somewhat difficult (1)", 2=>"Very difficult (2)", 3=>"Extremely difficult (3)", 'undef'=> 'not answered'];
+    $str_difficulty_values = [0 => 'Not at all (0)',1=>"Somewhat difficult (1)", 2 => "Very difficult (2)", 3 => "Extremely difficult (3)", 'undef'=> 'not answered'];
 
     $str_issues = ["nervous_score" => "Feeling nervous","control_worry_score" => "Not controlling worry","worry_score" => "Worrying","relax_score" => "Trouble relaxing","restless_score" => "Being restless","irritable_score" => "Being irritable","fear_score" => "Feeling afraid", "difficulty" => "Difficulty working etc.","total" => "Total GAD-7 score"];
 
@@ -34,13 +34,12 @@ function gad7_report($pid, $encounter, $cols, $id)
         print "<table><tr>";
         foreach ($data as $key => $value) {
 // include scores_array and total for backward compatibility
-            if ($key == "id" || $key == "pid" || $key == "user" || $key == "groupname" || $key == "authorized" || $key ==  "activity" || $key == "date" || $value == "" || $key == "scores_array" || $key == "total" || $value == "0000-00-00 00:00:00")  {
+            if ($key == "id" || $key == "pid" || $key == "user" || $key == "groupname" || $key == "authorized" || $key ==  "activity" || $key == "date" || $value == "" || $key == "scores_array" || $key == "total" || $value == "0000-00-00 00:00:00") {
                 continue;
             }
-            if ($key == "difficulty"){
+            if ($key == "difficulty") {
                 print "<td><span class=bold>" . xlt($str_issues[$key]) . ": </span><span class=text>" . text($str_difficulty_values [$value]) . "</span></td>";
-            }
-            else {
+            } else {
                 print "<td><span class=bold>" . xlt($str_issues[$key]) . ": </span><span class=text>" . text($str_values [$value]) . "</span></td>";
                 $gad7_total += $value;
             }
@@ -51,20 +50,19 @@ function gad7_report($pid, $encounter, $cols, $id)
             }
         }
         // print the total
-        switch(intdiv($gad7_total,5)){
-       case 0:
-            $exp = $str_score_analysis [0];
-            break;
-       case 1:
-       case 2:
-            $exp = $str_score_analysis [5];
-            break;
-
-       default:
-              $exp = $str_score_analysis [15];
+        switch (intdiv($gad7_total, 5)) {
+            case 0:
+                $exp = $str_score_analysis [0];
+                break;
+            case 1:
+            case 2:
+                $exp = $str_score_analysis [5];
+                break;
+            default:
+                $exp = $str_score_analysis [15];
         }
 
-          print "<td><span class=bold>" . xlt($str_issues["total"]) . ": </span><span class=text>" . text($gad7_total) . " - ".$exp."</span></td>";
+          print "<td><span class=bold>" . xlt($str_issues["total"]) . ": </span><span class=text>" . text($gad7_total) . " - ".$exp . "</span></td>";
     }
 
     print "</tr></table>";
