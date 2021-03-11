890bfe177fdf53b2d0eb7a85c513627299204636
Brady Miller <brady.g.miller@gmail.com>
2021-03-10 22:37:46 -0800

    changing tabNotchosen to tabsNoHover

--- interface/main/tabs/templates/tabs_template.php
+++ interface/main/tabs/templates/tabs_template.php
@@ -18,7 +18,7 @@
             <i class="fa fa-caret-up menu_arrow" id="patient_caret" title="<?php echo xla('Toggle the Patient Panel'); ?>" aria-hidden="true"></i>
         </div>
         <!-- ko  foreach: tabsList -->
-            <div class="tabSpan bgcolor2" data-bind="click: tabClicked, css: {tabNotchosen: !visible()}">
+            <div class="tabSpan bgcolor2" data-bind="click: tabClicked, css: {tabsNoHover: !visible()}">
                 <span class="tabTitle" data-bind="text: title, click: tabClicked, css: {tabHidden: !visible()}"></span>
                 <span class="fa fa-fw fa-sync" data-bind="click: tabRefresh, class: spinner"></span>
                 <!--ko if:!locked() -->
