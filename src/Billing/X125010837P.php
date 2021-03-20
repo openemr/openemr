62588db8785474e47cdcb9c3bfca1eb6c82afe10
stephen waite <stephen.waite@cmsvt.com>
2021-03-20 07:44:58 -0400

    remove null test

--- src/Billing/X125010837P.php
+++ src/Billing/X125010837P.php
@@ -699,7 +699,7 @@ class X125010837P
         }
 
         // Segment REF*F8 Payer Claim Control Number for claim re-submission.icn_resubmission_number
-        if (strlen(trim($claim->billing_options['icn_resubmission_number'] ?? null)) > 3) {
+        if (strlen(trim($claim->billing_options['icn_resubmission_number'])) > 3) {
             ++$edicount;
             error_log("Method 1: " . errorLogEscape($claim->billing_options['icn_resubmission_number']), 0);
             $out .= "REF" .
@@ -2187,7 +2187,7 @@ class X125010837P
         }
 
         // Segment REF*F8 Payer Claim Control Number for claim re-submission.icn_resubmission_number
-        if (strlen(trim($claim->billing_options['icn_resubmission_number'] ?? null)) > 3) {
+        if (strlen(trim($claim->billing_options['icn_resubmission_number'])) > 3) {
             ++$edicount;
             error_log("Method 1: " . errorLogEscape($claim->billing_options['icn_resubmission_number']), 0);
             $out .= "REF" .
