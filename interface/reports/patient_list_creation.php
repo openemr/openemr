159771749ee6db47edebf325b52c3eb219f74355
stephen waite <stephen.waite@cmsvt.com>
2021-04-25 16:03:05 -0400

    typo

--- interface/reports/patient_list_creation.php
+++ interface/reports/patient_list_creation.php
@@ -10,7 +10,7 @@
  * @author    Stephen Waite <stephen.waite@cmsvt.com>
  * @copyright Copyright (c) 2014 Ensoftek, Inc
  * @copyright Copyright (c) 2017-2018 Brady Miller <brady.g.miller@gmail.com>
- * @copyright Copyright (c) 2019 Stephen Waite <stephen.waite@cmsvt.com>
+ * @copyright Copyright (c) 2021 Stephen Waite <stephen.waite@cmsvt.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
