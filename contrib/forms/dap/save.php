161f1e66874f93b46de1400865f84bd3f499c9b8
Sherwin <sherwingaddis@gmail.com>
2021-05-07 10:21:33 -0400

    Corrections

--- contrib/forms/dap/save.php
+++ contrib/forms/dap/save.php
@@ -11,6 +11,12 @@
 require_once("../../globals.php");
 require_once("$srcdir/api.inc");
 
+use OpenEMR\Common\Csrf\CsrfUtils;
+
+if (!CsrfUtils::verifyCsrfToken($_POST["csrf_token_form"])) {
+    CsrfUtils::csrfNotVerified();
+}
+
 require("C_FormDAP.class.php");
 $c = new C_FormDAP();
 echo $c->default_action_process($_POST);
