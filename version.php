fc909e4789f7174f190396782eeac16d7617e3bf
Brady Miller <brady.g.miller@gmail.com>
2021-05-23 13:21:24 -0700

    added suppport for birth name and care team status

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ $v_realpatch = '0';
 // is a database change in the course of development.  It is used
 // internally to determine when a database upgrade is needed.
 //
-$v_database = 391;
+$v_database = 392;
 
 // Access control version identifier, this is to be incremented whenever there
 // is a access control change in the course of development.  It is used
