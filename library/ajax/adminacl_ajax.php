a2e20d22b43ed2e1fdee83ab8e1be23c99b6e9d4
stephen waite <stephen.waite@cmsvt.com>
2021-05-30 07:37:33 -0400

    typo

--- library/ajax/adminacl_ajax.php
+++ library/ajax/adminacl_ajax.php
@@ -90,7 +90,7 @@ if ($_POST["control"] == "membership") {
 
         // check if user is protected. If so, then state message unable to remove from admin group.
         $userService = new UserService();
-        $user = $userService->getIdByUsername($_POST["name");
+        $user = $userService->getIdByUsername($_POST["name"]);
         $userNametoID = $user['id'];
         if (checkUserSetting("gacl_protect", "1", $userNametoID) || ($_POST["name"] == "admin")) {
              $gacl_protect = true;
