d6c580f280d836ec59661f433797f9b3103c6faf
Stephen Nielson <snielson@discoverandchange.com>
2021-06-03 10:39:20 -0400

    Improved uuid Unit Tests to account for caching
    Made the uuid unit test account for caching so we run the tests 5 times
    which gets rid of the jitter that happened on a single record call.
    Before the performance was 23 ms compared with the original of 5ms.
    Running it 5 times to look at caching implementations we get about
    12seconds on an AMD Ryzen 7 4750U.

--- tests/Tests/Unit/GenerateUUIDTest.php
+++ tests/Tests/Unit/GenerateUUIDTest.php
@@ -18,6 +18,57 @@ class GenerateUUIDTest extends TestCase
 {
     const SEED_COUNT = 0;
 
+    private function createTable()
+    {
+        $this->dropTable();
+        $sql = "CREATE TABLE `facility_test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` binary(16) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(30) DEFAULT NULL,
+  `fax` varchar(30) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `postal_code` varchar(11) DEFAULT NULL,
+  `country_code` varchar(30) NOT NULL DEFAULT '',
+  `federal_ein` varchar(15) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `service_location` tinyint(1) NOT NULL DEFAULT 1,
+  `billing_location` tinyint(1) NOT NULL DEFAULT 1,
+  `accepts_assignment` tinyint(1) NOT NULL DEFAULT 1,
+  `pos_code` tinyint(4) DEFAULT NULL,
+  `x12_sender_id` varchar(25) DEFAULT NULL,
+  `attn` varchar(65) DEFAULT NULL,
+  `domain_identifier` varchar(60) DEFAULT NULL,
+  `facility_npi` varchar(15) DEFAULT NULL,
+  `facility_taxonomy` varchar(15) DEFAULT NULL,
+  `tax_id_type` varchar(31) NOT NULL DEFAULT '',
+  `color` varchar(7) NOT NULL DEFAULT '',
+  `primary_business_entity` int(10) NOT NULL DEFAULT 1 COMMENT '0-Not Set as business entity 1-Set as business entity',
+  `facility_code` varchar(31) DEFAULT NULL,
+  `extra_validation` tinyint(1) NOT NULL DEFAULT 1,
+  `mail_street` varchar(30) DEFAULT NULL,
+  `mail_street2` varchar(30) DEFAULT NULL,
+  `mail_city` varchar(50) DEFAULT NULL,
+  `mail_state` varchar(3) DEFAULT NULL,
+  `mail_zip` varchar(10) DEFAULT NULL,
+  `oid` varchar(255) NOT NULL DEFAULT '' COMMENT 'HIEs CCDA and FHIR an OID is required/wanted',
+  `iban` varchar(50) DEFAULT NULL,
+  `info` text DEFAULT NULL,
+  `weno_id` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8";
+        sqlStatementNoLog($sql);
+    }
+
+    private function dropTable()
+    {
+        sqlStatementNoLog("DROP TABLE IF EXISTS `facility_test`");
+    }
+
     public function generate_facility_test($prefix, $number)
     {
         // nothing to do here.
@@ -43,17 +94,33 @@ class GenerateUUIDTest extends TestCase
 
     public function testGenerateUUID()
     {
-        $seedCount = [0, 1, 100, 500, 1000];
-        $origResults = [];
-        $newResults = [];
+        $this->createTable();
+        $seedCount = [0, 1, 2, 3, 4, 5, 10, 50, 100, 200];
+//        $seedCount = [0, 1, 2,3,4,5,6,7,8,9,10];//, 20, 50, 100];
+        $results = ['new' => [], 'orig' => []];
+        $iterations = 5;
         foreach ($seedCount as $seed)
         {
-            $registry = (new UuidRegistry(['table_name' => 'facility_test']));
-            $newResults[] = $this->runTestWithFunction($registry, 'createMissingUuidsImproved', $seed);
+            $iteration = ['new' => [], 'orig' => []];
+            for ($runs = 0; $runs < $iterations; $runs++) {
 
-            $registry = (new UuidRegistry(['table_name' => 'facility_test']));
-            $origResults[] = $this->runTestWithFunction($registry, 'createMissingUuids', $seed);
+                $registry = (new UuidRegistry(['table_name' => 'facility_test']));
+                $iteration['new'][] = $this->runTestWithFunction($registry, 'createMissingUuidsImproved', $seed);
+
+                $registry = (new UuidRegistry(['table_name' => 'facility_test']));
+                $iteration['orig'][] = $this->runTestWithFunction($registry, 'createMissingUuids', $seed);
+            }
+            $sum = array_reduce($iteration['new'], function($carry, $val) {
+                return $carry + $val['run'];
+            }, 0);
+            $results['new'][] = round($sum / $iterations, 4);
+            $sum = array_reduce($iteration['orig'], function($carry, $val) {
+                return $carry + $val['run'];
+            }, 0);
+            $results['orig'][] = round($sum / $iterations, 4);
         }
+
+        $this->dropTable();
 
         echo "\n\nRun Results\n";
         for ($i = 0; $i < count($seedCount); $i++)
@@ -61,10 +128,12 @@ class GenerateUUIDTest extends TestCase
             echo str_pad("Orig", 15, " ")
                 . str_pad("New", 15, " ")
                 . str_pad("Records", 15, " ")
+                . str_pad("Iterations", 15, " ")
                 . "\n";
-            echo str_pad($origResults[$i]['run'], 15, " ")
-                . str_pad($newResults[$i]['run'], 15, " ")
+            echo str_pad($results['orig'][$i], 15, " ")
+                . str_pad($results['new'][$i], 15, " ")
                 . str_pad($seedCount[$i], 15, " ")
+                . str_pad($iterations, 15, " ")
                 . "\n";
 //            echo "!Vert\t\t" . $origResults[$i]['run'] . "\t\t" . $newResults[$i]['run'] . "\t\t" . $seedCount[$i] . "\n";
         }
