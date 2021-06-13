8182414f592579d53b176b4ec34f077906814bc2
Jerry Padgett <sjpadgett@gmail.com>
2021-06-13 18:38:06 -0400

    add to care plan

--- sql/6_0_0-to-6_1_0_upgrade.sql
+++ sql/6_0_0-to-6_1_0_upgrade.sql
@@ -681,6 +681,6 @@ ALTER TABLE `form_vitals` ADD `oxygen_flow_rate` FLOAT(5,2) NULL DEFAULT '0.00';
 #EndIf
 
 #IfMissingColumn form_clinical_notes note_related_to
-ALTER TABLE `form_clinical_notes` ADD `note_related_to` TEXT NULL COMMENT 'Reference to lists id for note relationships(json)';
-ALTER TABLE `form_care_plan` ADD `note_related_to` TEXT NULL COMMENT 'Reference to lists id for note relationships(json)';
+ALTER TABLE `form_clinical_notes` ADD `note_related_to` TEXT COMMENT 'Reference to lists id for note relationships(json)';
+ALTER TABLE `form_care_plan` ADD `note_related_to` TEXT COMMENT 'Reference to lists id for note relationships(json)';
 #EndIf
