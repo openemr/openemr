d58a58a3e52f337168be35dd4d30176f13c39e87
Jerry Padgett <sjpadgett@gmail.com>
2021-07-19 14:47:18 -0400

    Convert ccda_gateway to use new CDADocumentService New CDADocumentService

--- ccdaservice/ccda_gateway.php
+++ ccdaservice/ccda_gateway.php
@@ -12,11 +12,11 @@
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-//authenticate for portal or main- never know where it gets used
-
-// Will start the (patient) portal OpenEMR session/cookie.
-
 use OpenEMR\Common\Csrf\CsrfUtils;
+use OpenEMR\Services\CDADocumentService;
+
+// authenticate for portal or main- never know where it gets used
+// Will start the (patient) portal OpenEMR session/cookie.
 
 require_once(__DIR__ . "/../src/Common/Session/SessionUtil.php");
 OpenEMR\Common\Session\SessionUtil::portalSessionStart();
@@ -45,7 +45,6 @@ if (!CsrfUtils::verifyCsrfToken($_GET["csrf_token_form"])) {
     CsrfUtils::csrfNotVerified();
 }
 
-$dowhat = $_REQUEST['action'] ?? '';
 if ((!$GLOBALS['ccda_alt_service_enable']) > 0) {
     die("Cda generation service turned off: Verify in Administration->Globals! Click back to return home."); // Die an honorable death!!
 }
@@ -56,73 +55,27 @@ if (!isset($_SESSION['site_id'])) {
 
 session_write_close();
 
-$parameterArray = array();
-//$parameterArray['encounter'];
-$parameterArray['combination'] = $pid;
-$parameterArray['components'] = 'allergies|medications|problems|immunizations|procedures|results|plan_of_care|vitals|social_history|encounters|functional_status|referral|instructions|medical_devices|goals';
-//$parameterArray['sections'];
-$parameterArray['downloadccda'] = "download_ccda";
-$parameterArray['latestccda'] = '0';
-$parameterArray['send_to'] = 'download_all';
-$parameterArray['sent_by_app'] = 'portal';
-$parameterArray['downloadformat'] = 'ccda';
-$parameterArray['ccda_pid'][] = $pid;
-//$parameterArray['me'] = urlencode(session_id());
-$parameterArray['view'] = 0;
-$parameterArray['recipient'] = 'patient'; // emr_direct or hie
-$parameterArray['site'] = $_SESSION ['site_id']; // set to an onsite portal user
+$cdaService = new CDADocumentService();
 
-
-$server_url = $GLOBALS['qualified_site_addr'];
-// CCM returns viewable CCD html file or
-// zip containing a CCDA.xml, CCDA.html and cda.xsl
-$ccdaxml = portalccdafetching($pid, $server_url, $parameterArray, $dowhat);
-
-if ($dowhat === 'dl') {
+if ($_REQUEST['action'] === 'dl') {
+    $ccda_xml = $cdaService->portalGenerateCCDZip($pid);
+    // download zip containing CCDA.xml, CCDA.html and cda.xsl files
     header("Cache-Control: public");
     header("Content-Description: File Transfer");
     header("Content-Disposition: attachment; filename=SummaryofCare.zip");
-    header("Content-Type: application/download");
+    header("Content-Type: application/zip");
     header("Content-Transfer-Encoding: binary");
-    echo $ccdaxml;
+    echo $ccda_xml;
+
     exit;
 }
-// display to new tab opened in home
-echo($ccdaxml);
+if ($_REQUEST['action'] === 'view') {
+    $ccda_xml = $cdaService->portalGenerateCCD($pid);
+    // CCM returns viewable CCD html file
+    // that displays to new tab opened from home
+    echo $ccda_xml;
 
-exit;
-
-function portalccdafetching($pid, $server_url, $parameterArray = [], $action = 'view')
-{
-    $parameters = '';
-    $site_id = $_SESSION['site_id'];
-    $url = $server_url . "/interface/modules/zend_modules/public/encounterccdadispatch/index?site=" .
-        urlencode($site_id) . "&me=" . urlencode(session_id()) .
-        "&param=1&view=1&combination=" . urlencode($pid) . "&recipient=patient";
-    if ($action === 'dl') {
-        $parameters = http_build_query($parameterArray);
-        $url = $server_url . "/interface/modules/zend_modules/public/encounterccdadispatch/index?me=" . urlencode(session_id());
-    }
-    try {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters);
-        curl_setopt($ch, CURLOPT_HEADER, 0); // set true for look see
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_COOKIESESSION, true);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie");
-        curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        //curl_setopt($ch, CURLOPT_COOKIE, 'XDEBUG_SESSION=1'); // debug break on first line in public/index.php
-
-        $result = curl_exec($ch) or die(curl_error($ch));
-        curl_close($ch);
-    } catch (Exception $e) {
-        die($e->getMessage());
-    }
-
-    return $result;
+    exit;
 }
+
+die(xlt("Error. Nothing to do."));
