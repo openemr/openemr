094c0ca174f53106ceb501edfbf262fb28f5cdc4
Stephen Nielson <stephen@nielson.org>
2021-08-19 15:48:59 -0400

    Patient previous,suffix,careteam,ethnicity.
    Fixed a number of patient issues.  I added support for the previous name
    and suffix in FHIR.  Updated the database (should really only effect developers)
    so that the history type key is set for careteams to leverage what Jerry
    did for previous name.
    
    Also fixed a problem with the ethnicity flags so that any unknown value
    would exclude the FHIR ombCategory for ethnicity.
    
    I had to change up the PatientService to override the search method.  I
    did this to grab the previous name.  Standard api will now have a
    previous name property.  Not sure I like giving the raw database values
    back and those values are ignored on patient save right now.  I didn't
    want to override what @sjpadgett did with patient name history so I left
    that alone.

--- src/Services/PatientService.php
+++ src/Services/PatientService.php
@@ -22,6 +22,7 @@ use OpenEMR\Events\Patient\BeforePatientCreatedEvent;
 use OpenEMR\Events\Patient\BeforePatientUpdatedEvent;
 use OpenEMR\Events\Patient\PatientCreatedEvent;
 use OpenEMR\Events\Patient\PatientUpdatedEvent;
+use OpenEMR\Services\Search\FhirSearchWhereClauseBuilder;
 use OpenEMR\Services\Search\ISearchField;
 use OpenEMR\Services\Search\TokenSearchField;
 use OpenEMR\Services\Search\SearchModifier;
@@ -323,6 +324,84 @@ class PatientService extends BaseService
             }
         }
         return $this->search($querySearch, $isAndCondition);
+    }
+
+    public function search($search, $isAndCondition = true)
+    {
+        $sql = "SELECT 
+                    patient_data.*
+                    ,patient_history_type_key
+                    ,previous_name_first
+                    ,previous_name_prefix
+                    ,previous_name_first
+                    ,previous_name_middle
+                    ,previous_name_last
+                    ,previous_name_suffix
+                    ,previous_name_enddate
+                FROM patient_data
+                LEFT JOIN (
+                    SELECT 
+                    pid AS patient_history_pid
+                    ,history_type_key AS patient_history_type_key
+                    ,previous_name_prefix
+                    ,previous_name_first
+                    ,previous_name_middle
+                    ,previous_name_last
+                    ,previous_name_suffix
+                    ,previous_name_enddate
+                    ,`date` AS previous_creation_date
+                    ,uuid AS patient_history_uuid
+                    FROM patient_history
+                ) patient_history ON patient_data.pid = patient_history.patient_history_pid";
+        $whereClause = FhirSearchWhereClauseBuilder::build($search, $isAndCondition);
+
+        $sql .= $whereClause->getFragment();
+        $sqlBindArray = $whereClause->getBoundValues();
+        $statementResults =  QueryUtils::sqlStatementThrowException($sql, $sqlBindArray);
+
+        $processingResult = $this->hydrateSearchResultsFromQueryResource($statementResults);
+        return $processingResult;
+    }
+
+    private function hydrateSearchResultsFromQueryResource($queryResource)
+    {
+        $processingResult = new ProcessingResult();
+        $patientsByUuid = [];
+        $patientFields = array_combine($this->getFields(), $this->getFields());
+        $previousNameColumns = ['previous_name_prefix', 'previous_name_first', 'previous_name_middle'
+            , 'previous_name_last', 'previous_name_suffix', 'previous_name_enddate'];
+        $previousNamesFields = array_combine($previousNameColumns, $previousNameColumns);
+        $patientOrderedList = [];
+        while ($row = sqlFetchArray($queryResource)) {
+            $record = $this->createResultRecordFromDatabaseResult($row);
+            $patientUuid = $record['patient_uuid'];
+            if (!isset($patientsByUuid[$patientUuid])) {
+                $patient = array_intersect_key($record, $patientFields);
+                $patient['previous_names'] = [];
+                $patientOrderedList[] = $patientUuid;
+            }
+            else {
+                $patient = $patientsByUuid[$patientUuid];
+            }
+            if (!empty($record['patient_history_type_key']))
+            {
+                if ($record['patient_history_type_key'] == 'name_history')
+                {
+                    $previousName = array_intersect_key($record, $previousNamesFields);
+                    $previousName['formatted_name'] = $this->formatPreviousName($previousName);
+                    $patient['previous_names'][] = $previousName;
+                }
+            }
+
+            // now let's grab our history
+            $patientsByUuid[$patientUuid] = $patient;
+        }
+        foreach ($patientOrderedList as $uuid)
+        {
+            $patient = $patientsByUuid[$uuid];
+            $processingResult->addData($patient);
+        }
+        return $processingResult;
     }
 
     /**
