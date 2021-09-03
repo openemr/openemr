a0b9feb3bf6d276b42773a820af4bd9fd4bf22ff
stephen waite <stephen.waite@cmsvt.com>
2021-09-03 16:00:10 -0400

    replace deprecated each with foreach #2296

--- gacl/Cache_Lite/Lite.php
+++ gacl/Cache_Lite/Lite.php
@@ -366,7 +366,7 @@ class Cache_Lite
             $motif = ($group) ? 'cache_'.$group.'_' : 'cache_';
         }
         if ($this->_memoryCaching) {
-            while (list($key, $value) = each($this->_memoryCachingArray)) {
+            foreach ($this->_memoryCachingArray as $key => $value) {
                 if (strpos($key, $motif, 0)) {
                     unset($this->_memoryCachingArray[$key]);
                     $this->_memoryCachingCounter = $this->_memoryCachingCounter - 1;
@@ -490,8 +490,9 @@ class Cache_Lite
     {
         $this->_memoryCachingArray[$this->_file] = $data;
         if ($this->_memoryCachingCounter >= $this->_memoryCachingLimit) {
-            list($key, $value) = each($this->_memoryCachingArray);
-            unset($this->_memoryCachingArray[$key]);
+            foreach ($this->_memoryCachingArray as $key => $value) {
+                unset($this->_memoryCachingArray[$key]);
+            }
         } else {
             $this->_memoryCachingCounter = $this->_memoryCachingCounter + 1;
         }
