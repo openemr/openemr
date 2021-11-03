9079090bfa13d5e12cbbf86723b23268dd41fd1e
Sherwin <sherwingaddis@gmail.com>
2021-11-02 21:53:29 -0400

    Adding edi_sequence table for billing

--- sql/6_0_0-to-6_1_0_upgrade.sql
+++ sql/6_0_0-to-6_1_0_upgrade.sql
@@ -1261,3 +1261,11 @@ UPDATE `layout_options` SET `codes` = 'SNOMED-CT:67822003' WHERE `form_id` = 'HI
 #IfNotRow3D layout_options form_id DEM field_id pubpid max_length 255
 UPDATE `layout_options` SET `max_length` = '255' WHERE `form_id` = 'DEM' AND `field_id` = 'pubpid';
 #EndIf
+
+#IfNotTable edi_sequence
+CREATE TABLE `edi_sequences` (
+    `id` int(11) unsigned NOT NULL default '0'
+) ENGINE=InnoDB;
+
+#IfNotRow edi_sequence id 1
+INSERT INTO `edi_sequences` VALUES (1);
--- sql/database.sql
+++ sql/database.sql
@@ -8377,12 +8377,28 @@ INSERT INTO `rule_target` ( `id`, `group_id`, `include_flag`, `required_flag`, `
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `edi_sequences`
+--
+
+DROP TABLE IF EXISTS `edi_sequences`;
+CREATE TABLE `edi_sequences` (
+  `id` int(11) unsigned NOT NULL default '0'
+) ENGINE=InnoDB;
+
+--
+-- Inserting data for table `edi_sequences`
+--
+
+INSERT INTO `edi_sequences` VALUES (1);
+
+-- --------------------------------------------------------
+--
 -- Table structure for table `sequences`
 --
 
 DROP TABLE IF EXISTS `sequences`;
 CREATE TABLE `sequences` (
-  `id` int(11) unsigned NOT NULL default '0'
+    `id` int(11) unsigned NOT NULL default '0'
 ) ENGINE=InnoDB;
 
 --
