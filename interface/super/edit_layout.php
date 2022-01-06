4fe981d76f9d62a48e0a0caefbc0f98655be4df8
Robert Down RN <robertdown@live.com>
2022-01-06 14:14:00 -0800

    Rolling back #4767 (#4814)

--- interface/super/edit_layout.php
+++ interface/super/edit_layout.php
@@ -399,20 +399,6 @@ natsort($sorted_datatypes);
 // The layout ID identifies the layout to be edited.
 $layout_id = empty($_REQUEST['layout_id']) ? '' : $_REQUEST['layout_id'];
 $layout_tbl = !empty($layout_id) ? tableNameFromLayout($layout_id) : '';
-// Setup alert for table layouts
-$layout_alert = "";
-if (($layout_id !== "") && ($layout_tbl !== "")) {
-    $layout_alert = sprintf(
-        '<div class="alert alert-warning alert-dismissible fade show my-0 py-0" role="alert">
-            <strong>%s</strong> %s
-            <button type="button" class="close my-0 py-0" data-dismiss="alert">
-                <span>&times;</span>
-            </button>
-        </div>',
-        xlt('Do not delete standard fields!'),
-        xlt('Select "UOR" option "Unused" to hide the field.')
-    );
-}
 
 // Tag style for stuff to hide if not an LBF layout. Currently just for the Source column.
 $lbfonly = substr($layout_id, 0, 3) == 'LBF' ? "" : "style='display:none;'";
