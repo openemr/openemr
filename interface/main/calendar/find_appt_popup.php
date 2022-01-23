920577e6a894a4535dc4cf17ecc5ec306ba627ff
stephen waite <stephen.waite@cmsvt.com>
2022-01-23 17:47:29 -0500

    test for global and fix logic

--- interface/main/calendar/find_appt_popup.php
+++ interface/main/calendar/find_appt_popup.php
@@ -405,8 +405,9 @@ if (isset($_REQUEST['cktime'])) {
                 }
 
                 $ampmFlag = $ampm;
+                $hour_format_leading_zeros = ($GLOBALS['time_display_format'] == 0) ? 'h' : 'H';
 
-                $atitle = "Choose " . date("H:i a", $utime);
+                $atitle = "Choose " . date($hour_format_leading_zeros . ":i a", $utime);
                 $adate = getdate($utime);
                 $anchor = "<a href='' class='text-decoration-none' onclick='return setappt(" .
                 attr_js($adate['year']) . "," .
@@ -416,7 +417,7 @@ if (isset($_REQUEST['cktime'])) {
                 attr_js($adate['minutes']) . ")'" .
                 " title='" . attr($atitle) . "' alt='" . attr($atitle) . "'" .
                 ">";
-                $hour_format = $GLOBALS['time_display_format'] ? 'G' : 'g';
+                $hour_format = ($GLOBALS['time_display_format'] == 0) ? 'G' : 'g';
                 echo (strlen(date($hour_format, $utime)) < 2 ? "<span class='invisible'>0</span>" : "") .
                 $anchor . date($hour_format . ":i", $utime) . "</a> ";
 
