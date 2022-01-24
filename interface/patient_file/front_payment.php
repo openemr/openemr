1160db174b30e94e25085ec86d07ed3e1f3875d1
Brady Miller <brady.g.miller@gmail.com>
2022-01-23 23:57:56 -0800

    payment processing fix in rel_600

--- interface/patient_file/front_payment.php
+++ interface/patient_file/front_payment.php
@@ -196,7 +196,7 @@ if (!empty($_POST['form_save'])) {
 
     if ($_POST['form_upay'] && $_REQUEST['radio_type_of_payment'] != 'pre_payment') {
         foreach ($_POST['form_upay'] as $enc => $payment) {
-            if ($amount = 0 + $payment) {
+            if ($amount = 0 + (int)$payment) {
                  $zero_enc = $enc;
                 if ($_REQUEST['radio_type_of_payment'] == 'invoice_balance') {
                     if (!$enc) {
