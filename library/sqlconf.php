6329d07113f1055b2ba1e1b48a5f14d320b2ebc9
Robert Down <robertdown@live.com>
2022-02-10 13:21:19 -0800

    Condition away potential fatal errors during auto install

--- library/sqlconf.php
+++ library/sqlconf.php
@@ -11,29 +11,30 @@
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
+use OpenEMR\Core\Kernel;
 use OpenEMR\Events\Core\SqlConfigEvent;
-
-$eventDispatcher = $GLOBALS['kernel']->getEventDispatcher();
-$sqlConfigEvent = new SqlConfigEvent();
 
 require_once $GLOBALS['OE_SITE_DIR'] . "/sqlconf.php";
 
-if ($eventDispatcher->hasListeners(SqlConfigEvent::EVENT_NAME)) {
+if (array_key_exists('kernel', $GLOBALS) && $GLOBALS['kernel'] instanceof Kernel) {
+    $eventDispatcher = $GLOBALS['kernel']->getEventDispatcher();
+    $sqlConfigEvent = new SqlConfigEvent();
 
-    /**
-     * @var SqlConfigEvent
-     */
-    $configEvent = $eventDispatcher->dispatch(new SqlConfigEvent(), SqlConfigEvent::EVENT_NAME);
+    if ($eventDispatcher->hasListeners(SqlConfigEvent::EVENT_NAME)) {
+        /**
+         * @var SqlConfigEvent
+         */
+        $configEvent = $eventDispatcher->dispatch(new SqlConfigEvent(), SqlConfigEvent::EVENT_NAME);
+        $configEntity = $configEvent->getConfig();
 
-    $configEntity = $configEvent->getConfig();
-
-    // Override the variables set in sites/<site_id>/sqlconf.php file that was required above.
-    $host = $configEntity->getHost();
-    $port = $configEntity->getPort();
-    $login = $configEntity->getUser();
-    $pass = $configEntity->getPass();
-    $dbase = $configEntity->getDatabaseName();
-    $db_encoding = $configEntity->getEncoding();
-    $disable_utf8_flag = $configEntity->getDisableUTF8();
-    $config = $configEntity->getConfig();
+        // Override the variables set in sites/<site_id>/sqlconf.php file that was required above.
+        $host = $configEntity->getHost();
+        $port = $configEntity->getPort();
+        $login = $configEntity->getUser();
+        $pass = $configEntity->getPass();
+        $dbase = $configEntity->getDatabaseName();
+        $db_encoding = $configEntity->getEncoding();
+        $disable_utf8_flag = $configEntity->getDisableUTF8();
+        $config = $configEntity->getConfig();
+    }
 }
