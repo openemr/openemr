beb3853d00a04b23bc3568e4945cbaf9f69f4625
Ken Chapple <ken@mi-squared.com>
2022-03-18 11:20:25 -0400

    ported a bunch of Ruby code to support Cat III to PHP

--- src/Services/Qrda/AggregateCount.php
+++ src/Services/Qrda/AggregateCount.php
@@ -38,37 +38,92 @@ class AggregateCount
 
             $population = $this->create_population_from_population_set($pop_code, $population_set, $cache_entry);
             if ($cache_entry->pop_set_hash['stratification_id']) {
-                // TODO
                  // strat_id = population_set.stratifications.where(stratification_id: cache_entry.pop_set_hash[:stratification_id]).first&.hqmf_id
                  // observation = cache_entry['observations'][pop_code] if cache_entry['observations'] && cache_entry['observations'][pop_code]
                  // population.add_stratification(strat_id,cache_entry[pop_code], observation)
+                $strat_id = null;
+                foreach ($population_set->stratifications as $stratification) {
+                    if ($cache_entry->pop_set_hash['stratification_id']) {
+                        $strat_id = $stratification->hqmf_id;
+                    }
+                }
+                $observation = null;
+                if ($cache_entry['observations'] && $cache_entry['observations'][$pop_code]) {
+                    $observation = $cache_entry['observations'][$pop_code];
+                }
+                $population->add_stratification($strat_id, $cache_entry[$pop_code], $observation);
 
             } else {
-                // TODO
                 // population.value = cache_entry[pop_code]
                 // population.observation = cache_entry['observations'][pop_code] if cache_entry['observations'] && cache_entry['observations'][pop_code]
                 // population.supplemental_data = cache_entry.supplemental_data[pop_code]
+                $population->value = $cache_entry[$pop_code];
+                if ($cache_entry['observations'] && $cache_entry['observations'][$pop_code]) {
+                    $population->observation = $cache_entry['observations'][$pop_code];
+                }
+                $population->supplemental_data = $cache_entry->supplemental_data[$pop_code];
             }
 
             $entry_populations[] = $population;
         }
 
-        // TODO
+        // See if we already have this population
+        // Ruby:
         // return if population_groups.find {|pg| pg.populations.collect(&:id).compact.sort == entry_populations.collect(&:id).compact.sort }
+        $population_group = null;
+        foreach ($this->population_groups as $pg) {
+            $diff = array_diff($pg->populations, $entry_populations);
+            if (count($diff) === 0) {
+                $population_group = $pg;
+                break;
+            }
+        }
 
-        $population_group = new PopulationGroup();
-        $population_group->populations = $entry_populations;
-        $this->population_groups[] = $population_group;
+        if ($population_group === null) {
+            $population_group = new PopulationGroup();
+            $population_group->populations = $entry_populations;
+            $this->population_groups[] = $population_group;
+        } else {
+            return $population_group;
+        }
     }
 
     public function create_population_from_population_set($pop_code, $population_set, $cache_entry)
     {
-        // TODO Do we need the caching stuff?
+        // population = populations.find { |pop| pop.id == population_set.populations[pop_code]&.hqmf_id } if pop_code != 'STRAT'
+        // return population unless population.nil? && !cache_entry.pop_set_hash[:stratification_id]
+        $population = null;
+        foreach ($this->populations as $pop) {
+            if (
+                $pop_code != 'STRAT' &&
+                $pop->id == $population_set->populations[$pop_code]->hqmf_id
+            ) {
+                $population = $pop;
+                break;
+            }
+        }
+
+        if (
+            $population !== null ||
+            !empty($cache_entry->pop_set_hash['stratification_id'])
+        ) {
+            return $population;
+        }
 
         $population = new Population();
         $population->type = $pop_code;
         $population->id = $population_set->populations[$pop_code]->hqmf_id;
         $this->populations[] = $population;
         return $population;
+    }
+
+    public function is_cv()
+    {
+        foreach ($this->populations as $population) {
+            if ($population->type == 'MSRPOPL') {
+                return true;
+            }
+        }
+        return false;
     }
 }
