478e3fd785b90f0f9dffccd3c51eeeed87b1cb16
stephen waite <stephen.waite@cmsvt.com>
2022-04-03 14:14:19 -0400

    psr12

--- src/USPS/USPSAddress.php
+++ src/USPS/USPSAddress.php
@@ -22,90 +22,101 @@ namespace OpenEMR\USPS;
  * @since 1.0
  * @author Vincent Gabriel
  */
-class USPSAddress {
+class USPSAddress
+{
   /**
    * @var array list of all key=>value pairs we added so far for the current address
    */
-  protected $addressInfo = array();
+    protected $addressInfo = array();
   /**
    * Set the address2 property
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setAddress($value) {
-    return $this->setField('Address2', $value);
-  }
+    public function setAddress($value)
+    {
+        return $this->setField('Address2', $value);
+    }
   /**
    * Set the address1 property usually the apt or suit number
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setApt($value) {
-    return $this->setField('Address1', $value);
-  }
+    public function setApt($value)
+    {
+        return $this->setField('Address1', $value);
+    }
   /**
    * Set the city property
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setCity($value) {
-    return $this->setField('City', $value);
-  }
+    public function setCity($value)
+    {
+        return $this->setField('City', $value);
+    }
   /**
    * Set the state property
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setState($value) {
-    return $this->setField('State', $value);
-  }
+    public function setState($value)
+    {
+        return $this->setField('State', $value);
+    }
   /**
    * Set the zip4 property - zip code value represented by 4 integers
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setZip4($value) {
-    return $this->setField('Zip4', $value);
-  }
+    public function setZip4($value)
+    {
+        return $this->setField('Zip4', $value);
+    }
   /**
    * Set the zip5 property - zip code value represented by 5 integers
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setZip5($value) {
-    return $this->setField('Zip5', $value);
-  }
+    public function setZip5($value)
+    {
+        return $this->setField('Zip5', $value);
+    }
   /**
    * Set the firmname property
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setFirmName($value) {
-    return $this->setField('FirmName', $value);
-  }
+    public function setFirmName($value)
+    {
+        return $this->setField('FirmName', $value);
+    }
    /**
    * Set the revision property
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setRevision($value) {
-    return $this->setField('Revision', $value);
-  }
+    public function setRevision($value)
+    {
+        return $this->setField('Revision', $value);
+    }
   /**
    * Add an element to the stack
    * @param string|int $key
    * @param string|int $value
    * @return object USPSAddress
    */
-  public function setField($key, $value) {
-    $this->addressInfo[ ucwords($key) ] = $value;
-    return $this;
-  }
+    public function setField($key, $value)
+    {
+        $this->addressInfo[ ucwords($key) ] = $value;
+        return $this;
+    }
   /**
    * Returns a list of all the info we gathered so far in the current address object
    * @return array
    */
-  public function getAddressInfo() {
-    return $this->addressInfo;
-  }
+    public function getAddressInfo()
+    {
+        return $this->addressInfo;
+    }
 }
--- src/USPS/USPSAddressVerify.php
+++ src/USPS/USPSAddressVerify.php
@@ -25,37 +25,41 @@ use OpenEMR\USPS\USPSAddress;
  * @since 1.0
  * @author Vincent Gabriel
  */
-class USPSAddressVerify extends USPSBase {
+class USPSAddressVerify extends USPSBase
+{
   /**
    * @var string - the api version used for this type of call
    */
-  protected $apiVersion = 'Verify';
+    protected $apiVersion = 'Verify';
   /**
    * @var array - list of all addresses added so far
    */
-  protected $addresses = array();
+    protected $addresses = array();
   /**
    * Perform the API call to verify the address
    * @return string
    */
-  public function verify() {
-    return $this->doRequest();
-  }
+    public function verify()
+    {
+        return $this->doRequest();
+    }
   /**
    * returns array of all addresses added so far
    * @return array
    */
-  public function getPostFields() {
-    return $this->addresses;
-  }
+    public function getPostFields()
+    {
+        return $this->addresses;
+    }
   /**
    * Add Address to the stack
    * @param USPSAddress object $data
    * @param string $id the address unique id
    * @return void
    */
-  public function addAddress(USPSAddress $data, $id=null) {
-    $packageId = $id !== null ? $id : ((count($this->addresses)+1));
-    $this->addresses['Address'][] = array_merge(array('@attributes' => array('ID' => $packageId)), $data->getAddressInfo());
-  }
+    public function addAddress(USPSAddress $data, $id = null)
+    {
+        $packageId = $id !== null ? $id : ((count($this->addresses) + 1));
+        $this->addresses['Address'][] = array_merge(array('@attributes' => array('ID' => $packageId)), $data->getAddressInfo());
+    }
 }
