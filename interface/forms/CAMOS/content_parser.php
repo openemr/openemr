c28caceca1ddc59193dd890900a4f703a1414925
Brady Miller <brady.g.miller@gmail.com>
2022-04-11 22:51:28 -0700

    remove unneeded globals.php require from content_parser.php library

--- interface/forms/CAMOS/content_parser.php
+++ interface/forms/CAMOS/content_parser.php
@@ -12,7 +12,6 @@
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-require_once(dirname(__FILE__) . "/../../globals.php");
 require_once(dirname(__FILE__) . "/../../../library/api.inc");
 require_once(dirname(__FILE__) . "/../../forms/vitals/C_FormVitals.class.php");
 
