1af6636b021703bc408c69ac4503baa9ffc0155d
ruth <ruth@muswell.me.uk>
2022-05-09 13:35:47 +0100

    initial version of phq9 form

---
+++ interface/forms/phq9/view.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * phq-9 form using forms api     view.php    create a new form
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Ruth Moulton <moulton ruth@muswell.me.uk>
+ * @author    Jerry Padgett <sjpadgett@gmail.com>
+ * @copyright Copyright (c) 2021 ruth moulton <ruth@muswell.me.uk>
+ *
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+require_once(__DIR__ . "/../../globals.php");
+
+$viewmode = 'update';
+require_once("common.php");
