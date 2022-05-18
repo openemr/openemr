663d0fb565e9f079a2821d942f082856ac345b5e
Stephen Nielson <snielson@discoverandchange.com>
2022-05-18 14:09:52 -0600

    CQM cat3 report fixes #5353 (#5354)
    * CQM cat3 report fixes
    
    Make it so we include partial pids of patients.
    
    Fix escaping for duplicate merge patients on qrda.
    
    Fix the directory mkdir to work on linux for the cat3_export.
    
    * Fix js escaping, export pids

--- src/Cqm/QrdaControllers/QrdaReportController.php
+++ src/Cqm/QrdaControllers/QrdaReportController.php
@@ -228,28 +228,17 @@ class QrdaReportController
 
         $directory = $GLOBALS['OE_SITE_DIR'] . DIRECTORY_SEPARATOR . 'documents' . DIRECTORY_SEPARATOR . 'cat3_reports';
         if (!is_dir($directory)) {
-            if (!mkdir($directory, 775, true) && !is_dir($directory)) {
+            if (!mkdir($directory, 0775, true) && !is_dir($directory)) {
                 throw new \RuntimeException(sprintf('Directory "%s" was not created', $directory));
             }
         }
 
-        if (is_array($pids)) {
-            if (count($pids) === 1) {
-                $pids = $pids[0];
-            } else {
-                $pids = '';
-            }
-        }
         foreach ($measures as $measure) {
             if (is_array($measure)) {
                 $measure = $measure['measure_id'];
             }
             $xml = $this->getCategoryIIIReport($pids, $measure, $options);
             $filename = $measure . "_all_patients.xml";
-            if (!empty($pids)) {
-                $meta = sqlQuery("Select `fname`, `lname`, `pid` From `patient_data` Where `pid` = ?", [$pids]);
-                $filename = $measure . '_' . $pids . '_' . $meta['fname'] . '_' . $meta['lname'] . ".xml";
-            }
             $file = $directory . DIRECTORY_SEPARATOR . $filename;
             file_put_contents($file, $xml);
             unset($content);
