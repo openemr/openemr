475887e6fba45214e18096a992236b4b9413c2e1
stephen waite <stephen.waite@cmsvt.com>
2022-05-19 13:09:14 -0400

    fixing thanks to Ken and Jerry

--- src/Services/Qdm/Services/AssessmentService.php
+++ src/Services/Qdm/Services/AssessmentService.php
@@ -4,9 +4,7 @@
  * @package   OpenEMR
  * @link      http://www.open-emr.org
  * @author    Ken Chapple <ken@mi-squared.com>
- * @author    Stephen Waite <stephen.waite@cmsvt.com>
  * @copyright Copyright (c) 2021 Ken Chapple <ken@mi-squared.com>
- * @copyright Copyright (c) 2022 Stephen Waite <stephen.waite@cmsvt.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU GeneralPublic License 3
  */
 
@@ -30,52 +28,5 @@ class AssessmentService extends AbstractObservationService implements QdmService
     public function getModelClass()
     {
         return AssessmentPerformed::class;
-    }
-
-    /**
-     * @param array $record
-     * @return mixed
-     * @throws \Exception
-     *
-     * Map an OpenEMR record into a QDM model
-     */
-    public function makeQdmModel(QdmRecord $recordObj)
-    {
-        $record = $recordObj->getData();
-        $modelClass = $this->getModelClass();
-        $id = parent::convertToObjectIdBSONFormat($recordObj->getEntityCount());
-        $qdmModel = new $modelClass([
-            '_id' => $id,
-            'id' => $id,
-            'relevantPeriod' => new Interval([
-                'low' => new DateTime([
-                    'date' => $record['date']
-                ]),
-                'high' => null, // TODO We don't have an end-date for assessment?,
-                'lowClosed' => $record['date'] ? true : false,
-                'highClosed' => false
-            ]),
-            'authorDatetime' => new DateTime([
-                'date' => $record['date']
-            ]),
-        ]);
-
-        $qdmModel->result = $this->makeResult($record);
-
-        // If the reason status is "negated" then add the code to negation rationale, otherwise add to reason
-        if (!empty($record['ob_reason_code'])) {
-            if ($record['ob_reason_status'] == parent::NEGATED) {
-                $qdmModel->negationRationale = $this->makeQdmCode($record['ob_reason_code']);
-            } else {
-                $qdmModel->reason = $this->makeQdmCode($record['ob_reason_code']);
-            }
-        }
-
-        $codes = $this->explodeAndMakeCodeArray($record['code']);
-        foreach ($codes as $code) {
-            $qdmModel->addCode($code);
-        }
-
-        return $qdmModel;
     }
 }
