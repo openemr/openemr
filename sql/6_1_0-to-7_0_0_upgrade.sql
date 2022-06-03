7b49ea6e98d11debcfa22f3d2a71af8730f92865
Samuel Suede <suedes@oregonstate.edu>
2022-06-03 18:22:41 -0400

    Add database changes to upgrade script

--- sql/6_1_0-to-7_0_0_upgrade.sql
+++ sql/6_1_0-to-7_0_0_upgrade.sql
@@ -718,3 +718,11 @@ UPDATE list_options SET seq=10 WHERE list_id="immunization_refusal_reason" AND o
 #IfMissingColumn procedure_result date_end
 ALTER TABLE `procedure_result` ADD `date_end` datetime DEFAULT NULL COMMENT 'lab-provided end date specific to this result';
 #EndIf
+
+#IfNotRow3D layout_options form_id DEM field_id title title Title
+UPDATE `layout_options` SET `title` = 'Title' WHERE `layout_options`.`form_id` = 'DEM' AND `layout_options`.`field_id` = 'title';
+#EndIf
+
+#IfNotRow3D layout_options form_id DEM field_id fname title Name
+UPDATE `layout_options` SET `title` = 'Name', `titlecols` = '1', `datacols` = '3' WHERE `layout_options`.`form_id` = 'DEM' AND `layout_options`.`field_id` = 'fname';
+#EndIf
