eaf09010ba7e934664595daf5af7f5ca743f03a8
Robert Down <robertdown@live.com>
2022-06-05 07:36:16 -0700

    Configure New Encounter Form
    These settings only change the display setting of the various
    fields, the actual input element is still rendered on the page.

---
+++ src/OeUI/RenderHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OpenEMR\OeUi;
+
+class RenderHelper
+{
+
+    const SHOW_ON_NEW_ONLY = 'show_new';
+
+    const SHOW_ON_EDIT_ONLY = 'show_edit';
+
+    const SHOW_ALL = 'show_both';
+
+    const HIDE_ALL = 'hide_both';
+
+    static public function displayField($option, $mode) {
+        if ($option === self::HIDE_ALL) {
+            return false;
+        }
+
+        if ($option === self::SHOW_ALL) {
+            return true;
+        }
+
+        if ($mode === 'new' && $option === self::SHOW_ON_NEW_ONLY) {
+            return true;
+        }
+
+        if ($mode === 'edit' && $option === self::SHOW_ON_EDIT_ONLY) {
+            return true;
+        }
+
+        return false;
+    }
+}
