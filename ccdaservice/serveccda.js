a01ecbde9fd4bc73c3443a59ef2c9445e20182ce
Stephen Nielson <snielson@discoverandchange.com>
2022-06-29 16:47:56 -0600

    Add Provenance to Social History Observations
    Added the provenance information to social history observations for
    smoking and gender

--- ccdaservice/serveccda.js
+++ ccdaservice/serveccda.js
@@ -2745,7 +2745,45 @@ function populateSocialHistory(pd) {
         },
         "element": pd.element,
         "value": pd.description,
-        "gender": all.patient.gender
+        "gender": all.patient.gender,
+        "author": {
+            "code": {
+                "name": all.author.physician_type || '',
+                "code": all.author.physician_type_code || '',
+                "code_system_name": "SNOMED CT"
+            },
+            "date_time": {
+                "point": {
+                    "date": authorDateTime,
+                    "precision": "tz"
+                }
+            },
+            "identifiers": [
+                {
+                    "identifier": all.author.npi ? "2.16.840.1.113883.4.6" : all.author.id,
+                    "extension": all.author.npi ? all.author.npi : ''
+                }
+            ],
+            "name": [
+                {
+                    "last": all.author.lname,
+                    "first": all.author.fname
+                }
+            ],
+            "organization": [
+                {
+                    "identity": [
+                        {
+                            "root": oidFacility || "2.16.840.1.113883.4.6",
+                            "extension": npiFacility || ""
+                        }
+                    ],
+                    "name": [
+                        all.encounter_provider.facility_name
+                    ]
+                }
+            ]
+        }
     };
 }
 
