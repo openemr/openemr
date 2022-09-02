6184afc43480adb909a3f5c89b6768ef5dfdac4d
Brady Miller <brady.g.miller@gmail.com>
2022-09-01 23:53:23 -0700

    minor adjustment to prior commit (#5720)

--- sql/7_0_0-to-7_0_1_upgrade.sql
+++ sql/7_0_0-to-7_0_1_upgrade.sql
@@ -194,6 +194,6 @@ ALTER TABLE `openemr_postcalendar_events` ADD `uuid` binary(16) DEFAULT NULL;
 CREATE UNIQUE INDEX `uuid` ON `openemr_postcalendar_events` (`uuid`);
 #EndIf
 
-#IfNotRow2D list_options list_id drug_route option_id "bymouth"
+#IfNotRow2D list_options list_id drug_route option_id bymouth
 INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `notes`, `codes`) VALUES ('drug_route', 'bymouth', 'By Mouth', 1, 0, 'PO', 'NCI-CONCEPT-ID:C38288');
 #EndIf
