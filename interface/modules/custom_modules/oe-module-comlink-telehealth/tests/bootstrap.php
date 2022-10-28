6c9fd74b81c503424f0dd4d01ffe2a09259d81ea
Stephen Nielson <snielson@discoverandchange.com>
2022-10-27 20:03:02 -0400

    Telehealth fix unit tests, security
    Make it so the unit tests can only run from the command line.  Fix
    extraneous parameter in unit test.

--- interface/modules/custom_modules/oe-module-comlink-telehealth/tests/bootstrap.php
+++ interface/modules/custom_modules/oe-module-comlink-telehealth/tests/bootstrap.php
@@ -12,6 +12,11 @@
 
 declare(strict_types=1);
 
+// make sure this can only be run on the command line.
+if (php_sapi_name() !== 'cli') {
+    exit;
+}
+
 $_GET['site'] = 'default';
 $ignoreAuth = true;
 require_once(__DIR__ . "/../../../../globals.php");
