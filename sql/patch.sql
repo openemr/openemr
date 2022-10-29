4a6a7eed351786298ad19eb80933f9e0bd0fbfd5
JERRYS-DEV\sjpad <sjpadgett@gmail.com>
2022-10-28 20:47:44 -0400

    Add a disable validation to config ccda in connectors update patch database changes

--- sql/patch.sql
+++ sql/patch.sql
@@ -182,3 +182,7 @@ ALTER TABLE `layout_group_properties` ADD `grp_unchecked` tinyint(1) NOT NULL DE
 #IfNotColumnType audit_details field_value LONGTEXT
 ALTER TABLE `audit_details` CHANGE `field_value` `field_value` LONGTEXT COMMENT 'openemr table field value';
 #EndIf
+
+#IfMissingColumn audit_master is_unstructured_document
+ALTER TABLE `audit_master` ADD `is_unstructured_document` BOOLEAN NULL DEFAULT FALSE;
+#EndIf
