92cac7aeb1cb816d39a87ecb591463c759addcdc
Brady Miller <brady.g.miller@gmail.com>
2022-10-31 20:21:01 -0700

    qrda_category1.inc inc to php migration

--- custom/ajax_download.php
+++ custom/ajax_download.php
@@ -17,7 +17,7 @@ require_once("../interface/globals.php");
 require_once("$srcdir/report_database.inc");
 require_once("../ccr/uuid.php");
 require_once("qrda_category1_functions.php");
-require_once("qrda_category1.inc");
+require_once("qrda_category1.inc.php");
 require_once("qrda_functions.php");
 
 use OpenEMR\Common\Csrf\CsrfUtils;
