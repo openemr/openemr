7875a9362d8fb628ea14866781aed270c0c0f470
stephen waite <stephen.waite@cmsvt.com>
2022-11-04 17:06:38 -0400

    fixes #5310, add ZZ to provider number arrays

--- library/classes/InsuranceNumbers.class.php
+++ library/classes/InsuranceNumbers.class.php
@@ -3,6 +3,15 @@
 /**
  * class InsuranceNumbers
  *
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    tekknogenius
+ * @author    Brady Miller <brady.g.miller@gmail.com>
+ * @author    Stephen Waite <stephen.waite@cmsvt.com>
+ * @copyright Copyright (c) 2005 tekknogenius
+ * @copyright Copyright (c) 2016-2021 Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2019-2022 Stephen Waite <stephen.waite@cmsvt.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
 use OpenEMR\Common\ORDataObject\ORDataObject;
@@ -17,16 +26,48 @@ class InsuranceNumbers extends ORDataObject
         var $rendering_provider_number;
         var $group_number;
         var $provider_number_type;
-        var $provider_number_type_array = array ("" => "Unspecified", "0B" => "State License Number", "1A" => "Blue Cross Provider Number","1B" => "Blue Shield Provider Number","1C" => "Medicare Provider Number",
-                                                        "1D" => "Medicaid Provider Number","1G" => "Provider UPIN Number","1H" => "Champus Identification Number","1J" => "Facility ID Number",
-                                                        "B3" => "Preferred Provider Organization Number","BQ" => "Health Maintenance Organization Code Number","E1" => "Employer's Identification Number",
-                                                        "FH" => "Clinic Number", "G2" => "Provider Commercial Number", "G5" => "Provider Site Number", "LU" => "Location Number", "SY" => "Social Security Number",
-                                                        "U3" => "Unique Supplier Identification Number (USIN)", "X5" => "State Industrial Accident Provider Number");
+        var $provider_number_type_array = array
+        (
+            ""   => "Unspecified",
+            "0B" => "State License Number",
+            "1A" => "Blue Cross Provider Number",
+            "1B" => "Blue Shield Provider Number",
+            "1C" => "Medicare Provider Number",
+            "1D" => "Medicaid Provider Number",
+            "1G" => "Provider UPIN Number",
+            "1H" => "Champus Identification Number",
+            "1J" => "Facility ID Number",
+            "B3" => "Preferred Provider Organization Number",
+            "BQ" => "Health Maintenance Organization Code Number",
+            "E1" => "Employer's Identification Number",
+            "FH" => "Clinic Number",
+            "G2" => "Provider Commercial Number",
+            "G5" => "Provider Site Number",
+            "LU" => "Location Number",
+            "SY" => "Social Security Number",
+            "U3" => "Unique Supplier Identification Number (USIN)",
+            "X5" => "State Industrial Accident Provider Number",
+            "ZZ" => "Mutually Defined/Taxonomy"
+        );
         var $rendering_provider_number_type;
-        var $rendering_provider_number_type_array = array ("" => "Unspecified", "0B" => "State License Number", "1A" => "Blue Cross Provider Number","1B" => "Blue Shield Provider Number","1C" => "Medicare Provider Number",
-                                                        "1D" => "Medicaid Provider Number","1G" => "Provider UPIN Number","1H" => "Champus Identification Number","G2" => "Provider Commercial Number",
-                                                        "LU" => "Location Number", "N5" => "Provider Plan Network Identification Number",
-                                                        "TJ" => "Federal Taxpayer's Identification Number", "X4" => "Clinical Laboratory Improvement Amendment Number", "X5" => "State Industrial Accident Provider Number");
+        var $rendering_provider_number_type_array = array
+        (
+            ""   => "Unspecified",
+            "0B" => "State License Number",
+            "1A" => "Blue Cross Provider Number",
+            "1B" => "Blue Shield Provider Number",
+            "1C" => "Medicare Provider Number",
+            "1D" => "Medicaid Provider Number",
+            "1G" => "Provider UPIN Number",
+            "1H" => "Champus Identification Number",
+            "G2" => "Provider Commercial Number",
+            "LU" => "Location Number",
+            "N5" => "Provider Plan Network Identification Number",
+            "TJ" => "Federal Taxpayer's Identification Number",
+            "X4" => "Clinical Laboratory Improvement Amendment Number",
+            "X5" => "State Industrial Accident Provider Number",
+            "ZZ" => "Mutually Defined/Taxonomy"
+        );
 
         /**
          * Constructor sets all Insurance attributes to their default value
@@ -52,8 +93,9 @@ class InsuranceNumbers extends ORDataObject
         function insurance_numbers_factory($provider_id)
         {
             $ins = array();
-            $sql = "SELECT id FROM "  . escape_table_name($this->_table) . " WHERE provider_id = ? ORDER BY insurance_company_id";
-            $results = sqlQ($sql, array($provider_id));
+            $sql = "SELECT id FROM "  . escape_table_name($this->_table) .
+                " WHERE provider_id = ? ORDER BY insurance_company_id";
+            $results = sqlStatementNoLog($sql, array($provider_id));
 
             while ($row = sqlFetchArray($results)) {
                     $ins[] = new InsuranceNumbers($row['id']);
