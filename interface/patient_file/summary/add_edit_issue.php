ede9a08b7116346edb44bc2316d15df4e0b1b331
Machine-OpenEMR <59622821+Machine-OpenEMR@users.noreply.github.com>
2022-11-30 23:15:54 -0800

    fix enter in medical device gui (#5959)

--- interface/patient_file/summary/add_edit_issue.php
+++ interface/patient_file/summary/add_edit_issue.php
@@ -662,6 +662,16 @@ function getCodeText($code)
             document.forms[0].rem_selected_code.disabled = document.forms[0].form_selected_codes.selectedIndex == -1
         }
 
+        function processUdiEnter(event) {
+            if (event.key == 'Enter') {
+                event.preventDefault();
+                processUdi(document.getElementById('udi_process_button'));
+                return false;
+            } {
+                return true;
+            }
+        }
+
         function processUdi(param) {
             let udi = document.getElementById("form_udi").value;
             if (!udi) {
@@ -835,8 +845,8 @@ function getCodeText($code)
                         <?php if ($thistype == 'medical_device' || (!empty($irow['type']) && $irow['type'] == 'medical_device')) { ?>
                             <div class="form-group col-12">
                                 <label class="col-form-label" for="form_udi"><?php echo xlt('UDI{{Unique Device Identifier}}'); ?>:</label>
-                                <input type='text' class="form-control" name='form_udi' id='form_udi' value='<?php echo attr($irow['udi'] ?? '') ?>' />
-                                <button type="button" class="btn btn-primary btn-sm" style="margin-right:5px;" onclick='processUdi(this)'><?php echo (!empty($irow['udi_data'])) ? xlt('Re-Process UDI') : xlt('Process UDI'); ?></button>
+                                <input type='text' class="form-control" name='form_udi' id='form_udi' onkeydown="processUdiEnter(event)" value='<?php echo attr($irow['udi'] ?? '') ?>' />
+                                <button type="button" class="btn btn-primary btn-sm" id='udi_process_button' style="margin-right:5px;" onclick='processUdi(this)'><?php echo (!empty($irow['udi_data'])) ? xlt('Re-Process UDI') : xlt('Process UDI'); ?></button>
                                 <div id="udi_display" class="shadow p-3 mb-5 rounded">
                                     <?php if (!empty($irow['udi_data'])) { ?>
                                         <?php echo (new MedicalDevice($irow['udi_data']))->fullOutputHtml(false); ?>
