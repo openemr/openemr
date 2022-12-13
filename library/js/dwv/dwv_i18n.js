409f1bf1c3ba1c789788a5503ab778b0acf58bad
Brady Miller <brady.g.miller@gmail.com>
2022-12-12 21:55:50 -0800

    interim

--- library/js/dwv/dwv_i18n.js
+++ library/js/dwv/dwv_i18n.js
@@ -1,10 +1,27 @@
-// original file available at https://github.com/ivmartel/dwv/blob/e6ea132ba2bac98546a1b27476e7ebe27bfeb02d/src/utils/i18n.js
+// original file available at https://github.com/ivmartel/dwv/blob/v0.30.8/src/utils/i18n.js
 
 // namespaces
 var dwv = dwv || {};
-// external
+/**
+ * The i18next namespace.
+ *
+ * @external i18next
+ * @see https://www.i18next.com
+ */
 var i18next = i18next || {};
-var i18nextXHRBackend = i18nextXHRBackend || {};
+/**
+ * The i18nextHttpBackend namespace.
+ *
+ * @external i18nextHttpBackend
+ * @see https://github.com/i18next/i18next-http-backend
+ */
+var i18nextHttpBackend = i18nextHttpBackend || {};
+/**
+ * The i18nextBrowserLanguageDetector namespace.
+ *
+ * @external i18nextBrowserLanguageDetector
+ * @see https://github.com/i18next/i18next-browser-languageDetector
+ */
 var i18nextBrowserLanguageDetector = i18nextBrowserLanguageDetector || {};
 
 // This is mainly a wrapper around the i18next object.
@@ -15,157 +32,157 @@ dwv.i18nLocalesPath = null;
 
 /**
  * Initialise i18n.
- * @param {String} language The language to translate to. Defaults to 'auto' and
+ *
+ * @param {string} language The language to translate to. Defaults to 'auto' and
  *   gets the language from the browser.
- * @param {String} localesPath Path to the locales directory.
- * @external i18next
- * @external i18nextXHRBackend
- * @external i18nextBrowserLanguageDetector
+ * @param {string} localesPath Path to the locales directory.
  */
-dwv.i18nInitialise = function (language, localesPath)
-{
-    var lng = (typeof language === "undefined") ? "auto" : language;
-    var lpath = (typeof localesPath === "undefined") ? "./../public/assets/dwv" : localesPath;
-    // store as global
-    dwv.i18nLocalesPath = lpath;
-    // i18n options: default 'en' language and
-    //  only load language, not specialised (for ex en-GB)
-    var options = {
-        fallbackLng: "en",
-        load: "languageOnly",
-        backend: { loadPath: lpath + "/locales/{{lng}}/{{ns}}.json" }
-    };
-    // use the XHR backend to get translation files
-    var i18n = i18next.use(i18nextXHRBackend);
-    // use browser language or the specified one
-    if (lng === "auto") {
-        i18n.use(i18nextBrowserLanguageDetector);
-    }
-    else {
-        options.lng = lng;
-    }
-    // init i18n: will be ready when the 'loaded' event is fired
-    i18n.init(options);
+dwv.i18nInitialise = function (language, localesPath) {
+  var lng = (typeof language === 'undefined') ? 'auto' : language;
+  var lpath = (typeof localesPath === 'undefined') ? './../public/assets/dwv' : localesPath;
+  // store as global
+  dwv.i18nLocalesPath = lpath;
+  // i18n options: default 'en' language and
+  //  only load language, not specialised (for ex en-GB)
+  var options = {
+    fallbackLng: 'en',
+    load: 'languageOnly',
+    backend: {loadPath: lpath + '/locales/{{lng}}/{{ns}}.json'}
+  };
+    // use the HTTP backend to get translation files
+  var i18n = i18next.use(i18nextHttpBackend);
+  // use browser language or the specified one
+  if (lng === 'auto') {
+    i18n.use(i18nextBrowserLanguageDetector);
+  } else {
+    options.lng = lng;
+  }
+  // init i18n: will be ready when the 'loaded' event is fired
+  i18n.init(options);
 };
 
 /**
  * Initialise i18n with recources as input.
- * @param {String} language The language to translate to. Defaults to 'auto' and
+ *
+ * @param {string} language The language to translate to. Defaults to 'auto' and
  *   gets the language from the browser.
- * @param {Object} resources Languages provided as object.
- * @external i18next
- * @external i18nextBrowserLanguageDetector
+ * @param {object} resources Languages provided as object.
  */
-dwv.i18nInitialiseWithResources = function (language, resources)
-{
-    var lng = (typeof language === "undefined") ? "auto" : language;
-    // i18n options: default 'en' language and
-    //  only load language, not specialised (for ex en-GB)
-    var options = {
-        fallbackLng: "en",
-        load: "languageOnly",
-        resources: resources
-    };
+dwv.i18nInitialiseWithResources = function (language, resources) {
+  var lng = (typeof language === 'undefined') ? 'auto' : language;
+  // i18n options: default 'en' language and
+  //  only load language, not specialised (for ex en-GB)
+  var options = {
+    fallbackLng: 'en',
+    load: 'languageOnly',
+    resources: resources
+  };
     // use browser language or the specified one
     // init i18n: will be ready when the 'loaded' event is fired
-    if (lng === "auto") {
-        var i18n = i18next.use(i18nextBrowserLanguageDetector);
-        i18n.init(options);
-    }
-    else {
-        options.lng = lng;
-        i18next.init(options);
-    }
+  if (lng === 'auto') {
+    var i18n = i18next.use(i18nextBrowserLanguageDetector);
+    i18n.init(options);
+  } else {
+    options.lng = lng;
+    i18next.init(options);
+  }
 };
 
 /**
  * Handle i18n 'initialized' event.
- * @param {Object} callback The callback function to call when i18n is initialised.
- *  It can take one argument that will be replaced with the i18n options.
- * @external i18next
+ *
+ * @param {object} callback The callback function to call when i18n
+ *   is initialised.
+ * It can take one argument that will be replaced with the i18n options.
  */
 dwv.i18nOnInitialised = function (callback) {
-    i18next.on('initialized', callback);
+  i18next.on('initialized', callback);
 };
 
 /**
  * Stop handling i18n load event.
- * @external i18next
  */
 dwv.i18nOffInitialised = function () {
-    i18next.off('initialized');
+  i18next.off('initialized');
 };
 
 /**
  * Handle i18n failed load event.
- * @param {Object} callback The callback function to call when i18n is loaded.
+ *
+ * @param {object} callback The callback function to call when i18n is loaded.
  *  It can take three arguments: lng, ns and msg.
- * @external i18next
  */
 dwv.i18nOnFailedLoad = function (callback) {
-    i18next.on('failedLoading', callback);
+  i18next.on('failedLoading', callback);
 };
 
 /**
  * Stop handling i18n failed load event.
- * @external i18next
  */
 dwv.i18nOffFailedLoad = function () {
-    i18next.off('failedLoading');
+  i18next.off('failedLoading');
 };
 
 /**
  * Get the translated text.
- * @param {String} key The key to the text entry.
- * @param {Object} options The translation options such as plural, context...
- * @external i18next
+ *
+ * @param {string} key The key to the text entry.
+ * @param {object} options The translation options such as plural, context...
+ * @returns {string} The translated text.
  */
 dwv.i18n = function (key, options) {
-    return i18next.t(key, options);
+  return i18next.t(key, options);
 };
 
 /**
  * Check the existence of a translation.
- * @param {String} key The key to the text entry.
- * @param {Object} options The translation options such as plural, context...
- * @external i18next
+ *
+ * @param {string} key The key to the text entry.
+ * @param {object} options The translation options such as plural, context...
+ * @returns {boolean} True if the key has a translation.
  */
 dwv.i18nExists = function (key, options) {
-    return i18next.exists(key, options);
+  return i18next.exists(key, options);
 };
 
 /**
- * Translate all data-i18n tags in the current html page. If an html tag defines the
- * data-i18n attribute, its value will be used as key to find its corresponding text
- * and will replace the content of the html tag.
+ * Translate all data-i18n tags in the current html page. If an html tag defines
+ * the data-i18n attribute, its value will be used as key to find its
+ * corresponding text and will replace the content of the html tag.
  */
 dwv.i18nPage = function () {
-    // get all elements
-    var elements = document.getElementsByTagName("*");
-    // if the element defines data-i18n, replace its content with the tranlation
-    for (var i = 0; i < elements.length; ++i) {
-        if (typeof elements[i].dataset.i18n !== "undefined") {
-            elements[i].innerHTML = dwv.i18n(elements[i].dataset.i18n);
-        }
+  // get all elements
+  var elements = document.getElementsByTagName('*');
+  // if the element defines data-i18n, replace its content with the tranlation
+  for (var i = 0; i < elements.length; ++i) {
+    if (typeof elements[i].dataset.i18n !== 'undefined') {
+      elements[i].innerHTML = dwv.i18n(elements[i].dataset.i18n);
     }
+  }
 };
 
 /**
  * Get the current locale resource path.
  * Warning: to be used once i18next is initialised.
- * @return {String} The path to the locale resource.
+ *
+ * @param {string} filename The file to locate.
+ * @returns {string} The path to the locale resource.
  */
 dwv.i18nGetLocalePath = function (filename) {
-    var lng = i18next.language.substr(0, 2);
-    return dwv.i18nLocalesPath + "/locales/" + lng + "/" + filename;
+  var lng = i18next.language.substr(0, 2);
+  return dwv.i18nLocalesPath +
+        '/locales/' + lng + '/' + filename;
 };
 
 /**
  * Get the current locale resource path.
  * Warning: to be used once i18next is initialised.
- * @return {String} The path to the locale resource.
+ *
+ * @param {string} filename The file to locate.
+ * @returns {string} The path to the locale resource.
  */
 dwv.i18nGetFallbackLocalePath = function (filename) {
-    var lng = i18next.languages[i18next.languages.length-1].substr(0, 2);
-    return dwv.i18nLocalesPath + "/locales/" + lng + "/" + filename;
+  var lng = i18next.languages[i18next.languages.length - 1].substr(0, 2);
+  return dwv.i18nLocalesPath +
+        '/locales/' + lng + '/' + filename;
 };
