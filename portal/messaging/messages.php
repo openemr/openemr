d3b3cdb4d5bf078dc340c876160d50e6ca79a28c
Jerry Padgett <sjpadgett@gmail.com>
2023-01-22 21:19:45 -0500

    * fix reply select correct name from dropdown on modal open

--- portal/messaging/messages.php
+++ portal/messaging/messages.php
@@ -75,7 +75,7 @@ function getAuthPortalUsers()
             $resultusers[] = $row;
         }
 
-        $authpatients = sqlStatement("SELECT LOWER(CONCAT(patient_data.fname, patient_data.id)) as userid,
+        $authpatients = sqlStatement("SELECT (CONCAT(patient_data.fname, patient_data.id)) as userid,
  CONCAT(patient_data.fname,' ',patient_data.lname) as username,'p' as type,patient_data.pid as pid FROM patient_data WHERE allow_patient_portal = 'YES'");
         while ($row = sqlFetchArray($authpatients)) {
             $resultpatients[] = $row;
