941ab464e9a5429640f1d32e94b7e448d374c6d7
Robert Down RN <robertdown@live.com>
2023-02-22 16:57:44 -0500

    fix: Use data-attributes and proper nav semantics in External Data (#6213)

--- interface/reports/external_data.php
+++ interface/reports/external_data.php
@@ -33,7 +33,7 @@ $records2 = array();
         $arrOeUiSettings = array(
             'heading_title' => xl('External Data'),
             'include_patient_name' => true,
-            'expandable' => false,
+            'expandable' => true,
             'expandable_files' => array("external_data_patient_xpd", "stats_full_patient_xpd", "patient_ledger_patient_xpd"),//all file names need suffix _xpd
             'action' => "",//conceal, reveal, search, reset, link or back
             'action_title' => "",
@@ -61,113 +61,102 @@ $records2 = array();
             ?>
             <div class="row mt-3">
                 <div class="col-sm-12">
-                    <ul class="nav nav-pills">
-                        <li class="nav-item" id="btnEncounters">
-                            <a href="#" class="nav-link font-weight-bold active"><?php echo xlt('Encounters'); ?></a>
+                    <ul class="nav nav-pills" id="pill-list" role="tablist">
+                        <li class="nav-item" role="presention">
+                            <a href="#encounters" id="pills-encounters-tab" class="nav-link active" data-toggle="pill" type="button" role="tab" aria-controls="pills-encounters-tab" aria-selected="true"><?php echo xlt('Encounters'); ?></a>
                         </li>
-                        <li class="nav-item" id="btnProcedures">
-                            <a href="#" class="nav-link font-weight-bold"><?php echo xlt('Procedures'); ?></a>
+                        <li class="nav-item" role="presentation">
+                            <a href="#procedures" id="pills-procedures-tab" class="nav-link" data-toggle="pill" type="button" role="tab" aria-controls="pills-procedures-tab" aria-selected="false"><?php echo xlt('Procedures'); ?></a>
                         </li>
                     </ul>
                 </div>
                 <div class="col-sm-12 mt-3">
-                    <div class="table-responsive jumbotron py-4" id="encounters">
-                        <table class="table">
-                            <tr>
-                                <td><?php echo xlt('Date'); ?></td>
-                                <td><?php echo xlt('Diagnosis'); ?></td>
-                                <td><?php echo xlt('Provider'); ?></td>
-                                <td><?php echo xlt('Facility'); ?></td>
-                            </tr>
-                            <?php
-                            $query1 = "SELECT ee.*,CONCAT_WS(' ',u1.lname, u1.fname) AS provider,u2.organization AS facility
-                         FROM external_encounters AS ee
-                         LEFT JOIN users AS u1 ON u1.id = ee.ee_provider_id
-                         LEFT JOIN users AS u2 ON u2.id = ee.ee_facility_id
-                         WHERE ee.ee_pid = ?";
-                            $res1 = sqlStatement($query1, array($pid));
-                            while ($row1 = sqlFetchArray($res1)) {
-                                $records1[] = $row1;
-                            }
-
-                            foreach ($records1 as $value1) {
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane active" id="encounters" role="tabpanel" aria-labelledby="encounters-tab">
+                            <div class="table-responsive">
+                                <?php
+                                $query1 = "SELECT ee.*,CONCAT_WS(' ',u1.lname, u1.fname) AS provider,u2.organization AS facility
+                                    FROM external_encounters AS ee
+                                    LEFT JOIN users AS u1 ON u1.id = ee.ee_provider_id
+                                    LEFT JOIN users AS u2 ON u2.id = ee.ee_facility_id
+                                    WHERE ee.ee_pid = ?";
+                                $res1 = sqlStatement($query1, array($pid));
+                                while ($row1 = sqlFetchArray($res1)) {
+                                    $records1[] = $row1;
+                                }
                                 ?>
-                                <tr>
-                                    <td><?php echo text(oeFormatShortDate($value1['ee_date'])); ?></td>
-                                    <td><?php echo text($value1['ee_encounter_diagnosis']); ?></td>
-                                    <td><?php echo text($value1['provider']); ?></td>
-                                    <td><?php echo text($value1['facility']); ?></td>
-                                </tr>
-                                <?php
-                            } ?>
-                        </table>
-                        <?php if (empty($records1)) { ?>
-                            <p class="text-center">
-                                <?php echo xlt('Nothing to display'); ?>
-                            </p>
-                        <?php } ?>
-                    </div>
-                    <div class="table-responsive jumbotron py-4" id="procedures">
-                        <table class="table">
-                            <tr>
-                                <td><?php echo xlt('Date'); ?></td>
-                                <td><?php echo xlt('Code'); ?></td>
-                                <td><?php echo xlt('Code Text'); ?></td>
-                                <td><?php echo xlt('Facility'); ?></td>
-                            </tr>
-                            <?php
-                            $query2 = "SELECT ep.*,u.organization AS facility
-                         FROM external_procedures AS ep
-                         LEFT JOIN users AS u ON u.id = ep.ep_facility_id
-                         WHERE ep.ep_pid = ?";
-                            $res2 = sqlStatement($query2, array($pid));
-                            while ($row2 = sqlFetchArray($res2)) {
-                                $records2[] = $row2;
-                            }
 
-                            foreach ($records2 as $value2) { ?>
-                                <tr>
-                                    <td><?php echo text(oeFormatShortDate($value2['ep_date'])); ?></td>
-                                    <td><?php echo text($value2['ep_code_type'] . ':' . $value2['ep_code']); ?></td>
-                                    <td><?php echo text($value2['ep_code_text']); ?></td>
-                                    <td><?php echo text($value2['facility']); ?></td>
-                                </tr>
+                                <?php if (count($records1) > 0) : ?>
+                                    <table class="table table-sm">
+                                        <thead>
+                                            <tr>
+                                                <th><?php echo xlt('Date'); ?></th>
+                                                <th><?php echo xlt('Diagnosis'); ?></th>
+                                                <th><?php echo xlt('Provider'); ?></th>
+                                                <th><?php echo xlt('Facility'); ?></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            foreach ($records1 as $value1) : ?>
+                                                <tr>
+                                                    <td><?php echo text(oeFormatShortDate($value1['ee_date'])); ?></td>
+                                                    <td><?php echo text($value1['ee_encounter_diagnosis']); ?></td>
+                                                    <td><?php echo text($value1['provider']); ?></td>
+                                                    <td><?php echo text($value1['facility']); ?></td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                <?php else : ?>
+                                    <p class="text-center">
+                                        <?php echo xlt('No External Encounters'); ?>
+                                    </p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="procedures" role="tabpanel" aria-labelledby="proccedures-tab">
+                            <div class="table-responsive">
                                 <?php
-                            } ?>
-                        </table>
-                        <?php if (empty($records2)) { ?>
-                        <p class="text-center">
-                            <?php echo xlt('Nothing to display'); ?>
-                        </p>
-                        <?php } ?>
+                                $query2 = "SELECT ep.*,u.organization AS facility FROM external_procedures AS ep LEFT JOIN users AS u ON u.id = ep.ep_facility_id WHERE ep.ep_pid = ?";
+                                $res2 = sqlStatement($query2, array($pid));
+                                while ($row2 = sqlFetchArray($res2)) {
+                                    $records2[] = $row2;
+                                }
+                                ?>
+                                <?php if (count($records2) > 0) : ?>
+                                    <table class="table table-sm">
+                                        <thead>
+                                            <tr>
+                                                <th><?php echo xlt('Date'); ?></th>
+                                                <th><?php echo xlt('Code'); ?></th>
+                                                <th><?php echo xlt('Code Text'); ?></th>
+                                                <th><?php echo xlt('Facility'); ?></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            foreach ($records2 as $value2) : ?>
+                                                <tr>
+                                                    <td><?php echo text(oeFormatShortDate($value2['ep_date'])); ?></td>
+                                                    <td><?php echo text($value2['ep_code_type'] . ':' . $value2['ep_code']); ?></td>
+                                                    <td><?php echo text($value2['ep_code_text']); ?></td>
+                                                    <td><?php echo text($value2['facility']); ?></td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                <?php else : ?>
+                                    <p class="text-center">
+                                        <?php echo xlt('No External Procedures'); ?>
+                                    </p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div><!--end of container div-->
         <?php $oemr_ui->oeBelowContainerDiv();?>
-    <script>
-        var listId = '#' + <?php echo js_escape($list_id); ?>;
-        $(function () {
-            $(listId).addClass("active");
-        });
-
-        $(function() {
-            $('.nav-link').click(function() {
-                $('.nav-link').removeClass("active");
-                $(this).addClass("active");
-            });
-
-            $('#btnEncounters').click(function() {
-                $('#encounters').show();
-                $('#procedures').hide();
-            });
-
-            $('#btnProcedures').click(function() {
-                $('#procedures').show();
-                $('#encounters').hide();
-            });
-        });
-        $('#encounters').hide();
-    </script>
     </body>
 </html>
