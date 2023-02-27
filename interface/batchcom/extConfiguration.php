2186b5d02357de31bab1122c8a379379e4593459
hKhatri <hardik.khatri110@gmail.com>
2023-02-27 21:02:24 +0530

    Added Add/Edit Action/Reminder configuration page.

---
+++ interface/batchcom/extConfiguration.php
@@ -0,0 +1,315 @@
+<?php
+/**
+ * REminder Tool for selecting/communicating with subsets of patients
+ */
+
+//INCLUDES, DO ANY ACTIONS, THEN GET OUR DATA
+require_once("../globals.php");
+require_once("$srcdir/registry.inc");
+require_once("batchcom.inc.php");
+
+use OpenEMR\Core\Header;
+use OpenEMR\Common\Acl\AclMain;
+use OpenEMR\OemrAd\Reminder;
+
+// Option lists
+$notification_tpl_list = new wmt\Options('Notification_Template');
+
+// Option lists
+function getEmailTpl() {
+	ob_start();
+	$tpl_list = new wmt\Options('Reminder_Email_Messages');
+	$tpl_list->showOptions('free_text');
+	$msgtpl = ob_get_clean();
+	$htmlStr = preg_replace( "/\r|\n/", "", $msgtpl );
+	return $htmlStr;
+}
+
+function getSmsTpl() {
+	ob_start();
+	$tpl_list = new wmt\Options('Reminder_SMS_Messages');
+	$tpl_list->showOptions('free_text');
+	$msgtpl = ob_get_clean();
+	$htmlStr = preg_replace( "/\r|\n/", "", $msgtpl );
+	return $htmlStr;
+}
+
+function getFaxTpl() {
+	ob_start();
+	$tpl_list = new wmt\Options('Reminder_Fax_Messages');
+	$tpl_list->showOptions('free_text');
+	$msgtpl = ob_get_clean();
+	$htmlStr = preg_replace( "/\r|\n/", "", $msgtpl );
+	return $htmlStr;
+}
+
+function getPostalTpl() {
+	ob_start();
+	$tpl_list = new wmt\Options('Reminder_Postal_Letters');
+	$tpl_list->showOptions('free_text');
+	$msgtpl = ob_get_clean();
+	$htmlStr = preg_replace( "/\r|\n/", "", $msgtpl );
+	return $htmlStr;
+}
+
+function getInternalMsgTpl() {
+	ob_start();
+	$tpl_list = new wmt\Options('Reminder_Internal_Messages');
+	$tpl_list->showOptions('free_text');
+	$msgtpl = ob_get_clean();
+	$htmlStr = preg_replace( "/\r|\n/", "", $msgtpl );
+	return $htmlStr;
+}
+
+function getDefaultTpl() {
+	$htmlStr = "<option value=''>Select Template</option>";
+	return $htmlStr;
+}
+
+// If we are saving, then save.
+if (isset($_POST['formaction']) && $_POST['formaction'] == 'save') {
+	$opt = $_POST['opt'];
+
+	$isDeleted = Reminder::deleteNotificationConfiguration();
+
+	if($isDeleted) {
+		foreach ($opt as $key => $optItem) {
+			$savedata = array();
+			$savedata['id'] = isset($optItem['id']) ? $optItem['id'] : "";
+			$savedata['seq'] = $key;
+			$savedata['communication_type'] = isset($optItem['communication_type']) ? $optItem['communication_type'] : "";
+			$savedata['notification_template'] = isset($optItem['notification_template']) ? $optItem['notification_template'] : "";
+			$savedata['data_set'] = isset($optItem['data_set']) ? $optItem['data_set'] : "";
+			$savedata['trigger_type'] = isset($optItem['trigger_type']) ? $optItem['trigger_type'] : "";
+			$savedata['time_trigger_data'] = isset($optItem['time_trigger_data']) ? $optItem['time_trigger_data'] : "";
+			$savedata['event_trigger'] = isset($optItem['event_trigger']) ? $optItem['event_trigger'] : "";
+
+			$isConfigurationExist = Reminder::isConfigurationExist($savedata['id']);
+
+			if($isConfigurationExist === false) {
+				//Insert Data
+				Reminder::saveNotificationConfiguration($savedata);
+			}
+		}
+	}
+}
+
+//Fetch configuration
+$configurationList = Reminder::getNotificationConfiguration();
+
+function writeOptionLine(
+	$id = '',
+	$communication_type = '',
+	$notification_template = '',
+	$data_set = '',
+	$trigger_type = '',
+	$time_trigger = '',
+	$event_trigger = ''
+) {
+	global $opt_line_no, $notification_tpl_list;
+	++$opt_line_no;
+
+	$timeData = isset($time_trigger) ? json_decode($time_trigger, true) : array();
+	$timeTitle = isset($timeData['title']) ? $timeData['title'] : "";
+
+	$communication_type_List = array(
+		'' => 'Select',
+		'email' => 'Email',
+		'sms' => 'SMS',
+		'fax' => 'Fax',
+		'postalmethod' => 'Postal Method',
+		'internalmessage' => 'Internal Message'
+	);
+
+	$notification_type_List = array(
+		'' => 'Select',
+		'notification' => 'Notification',
+		'reminder' => 'Reminder'
+	);
+
+	$addditional_info_List = array(
+		'' => 'Select',
+		'yes' => 'Yes',
+		'no' => 'No'
+	);
+
+	$trigger_type_List = array(
+		'' => 'Select',
+		'time' => 'Time',
+		'event' => 'Event'
+	);
+
+	?>
+	<tr>
+		<td width="150">
+			<input type="text" name="opt[<?php echo $opt_line_no; ?>][id]" value="<?php echo htmlspecialchars($id, ENT_QUOTES); ?>" class="optin" style="width: 100%;" />
+		</td>
+		<td width="100">
+			<select id="communication_type_<?php echo $opt_line_no; ?>" name="opt[<?php echo $opt_line_no; ?>][communication_type]" class="optin communication_type" data-index="<?php echo $opt_line_no; ?>" style="min-width: 100%; height: 30px; max-width: 120px; margin: 3px;">
+				<?php
+				foreach ($communication_type_List as $key => $desc) {
+		            ?>
+		            <option value="<?php echo $key ?>" <?php echo ($key == $communication_type) ? "selected" : ""; ?> ><?php echo htmlspecialchars($desc) ?></option>
+		            <?php
+		        }
+				?>
+			</select>
+		</td>
+		<td width="80">
+			<select name="opt[<?php echo $opt_line_no; ?>][notification_template]" id="notification_template_<?php echo $opt_line_no; ?>" class="optin notification_template" style="min-width: 100%; max-width: 150px; height: 30px; margin: 3px;">
+				<?php echo getDefaultTpl() ?>
+			</select>
+			<script type="text/javascript">
+				$(document).ready(function(){
+					setOptionVal('<?php echo $communication_type; ?>', '<?php echo $opt_line_no; ?>', '<?php echo $notification_template; ?>');
+				});
+			</script>
+		</td>
+		<td width="250">
+			<textarea name="opt[<?php echo $opt_line_no; ?>][data_set]" style="min-width: 100%; height: 30px; min-height: 30px; margin: 3px; resize: vertical;"><?php echo $data_set; ?></textarea>
+		</td>
+		<td>
+			<select name="opt[<?php echo $opt_line_no; ?>][trigger_type]" class="optin" style=" max-width: 100px; width: 100%; height: 30px; margin: 3px;">
+				<?php
+				foreach ($trigger_type_List as $key => $desc) {
+		            ?>
+		            <option value="<?php echo $key ?>" <?php echo ($key == $trigger_type) ? "selected" : ""; ?> ><?php echo htmlspecialchars($desc) ?></option>
+		            <?php
+		        }
+				?>
+			</select>
+		</td>
+		<td width="30">
+			<input type="text" id="<?php echo "time_trigger_".$opt_line_no; ?>" name="opt[<?php echo $opt_line_no; ?>][time_trigger]" value="<?php echo htmlspecialchars($timeTitle, ENT_QUOTES); ?>" class="optin time_trigger" style="width: 100%;" onClick="select_time('<?php echo "time_trigger_".$opt_line_no; ?>')" readonly />
+			<textarea id="<?php echo "time_trigger_".$opt_line_no."_raw"; ?>" name="opt[<?php echo $opt_line_no; ?>][time_trigger_data]" style="display: none;"><?php echo $time_trigger; ?></textarea>
+		</td>
+		<td width="30">
+			<input type="text" name="opt[<?php echo $opt_line_no; ?>][event_trigger]" value="<?php echo htmlspecialchars($event_trigger, ENT_QUOTES); ?>" class="optin" style="width: 100%;" />
+		</td>
+	</tr>
+	<?php
+}
+
+?>
+<html>
+<head>
+<title><?php echo xlt('Action Events'); ?></title>
+<?php Header::setupHeader(['datetime-picker']); ?>
+</head>
+<body class="body_top container">
+<header class="row">
+	<nav>
+	    <ul class="nav nav-tabs nav-justified">
+	        <li role="presentation" title="<?php echo xla('Action Events'); ?>">
+	            <a href="<?php echo $GLOBALS['rootdir']; ?>/batchcom/extsettings.php">
+	                <?php echo xlt('Action Events'); ?>
+	            </a>
+	        </li>
+	        <li role="presentation" title="<?php echo xla('Configurations'); ?>">
+	            <a href="<?php echo $GLOBALS['rootdir']; ?>/batchcom/extConfiguration.php">
+	                <?php echo xlt('Configurations'); ?>
+	            </a>
+	        </li>
+	    </ul>
+	</nav>
+    <h1 class="col-md-12 text-left">
+        <?php echo xlt('Action Events')?>
+    </h1>
+</header>
+<main>
+	<form method='post' name='theform' id='theform' action='extsettings.php'>
+		<table class="table table-striped table-condensed" style="margin-top:15px;">
+			<thead>
+    			<tr>
+    				<th><b><?php xl('ID', 'e'); ?></b></th>
+    				<th><b><?php xl('Type', 'e'); ?></b></th>
+    				<th><b><?php xl('Template', 'e'); ?></b></th>
+    				<th><b><?php xl('Data Set', 'e'); ?></b></th>
+    				<th><b><?php xl('Trigger Type ', 'e'); ?></b></th>
+    				<th><b><?php xl('Time Trigger', 'e'); ?></b></th>
+    				<th><b><?php xl('Event Trigger', 'e'); ?></b></th>
+    			</tr>
+    		</thead>
+    		<tbody>
+    			<?php
+    			$opt_line_no = 0;
+
+    			foreach ($configurationList as $key => $item) {
+    				$itemid = isset($item['id']) ? $item['id'] : "";
+					$itemcommunication_type = isset($item['communication_type']) ? $item['communication_type'] : "";
+					$itemnotification_template = isset($item['notification_template']) ? $item['notification_template'] : "";
+					$itemdata_set = isset($item['data_set']) ? $item['data_set'] : "";
+					$itemtrigger_type = isset($item['trigger_type']) ? $item['trigger_type'] : "";
+					$itemtime_trigger = isset($item['time_trigger_data']) ? $item['time_trigger_data'] : "";
+					$itemevent_trigger = isset($item['event_trigger']) ? $item['event_trigger'] : "";
+
+					writeOptionLine($itemid, $itemcommunication_type, $itemnotification_template, $itemdata_set, $itemtrigger_type, $itemtime_trigger, $itemevent_trigger);
+    			}
+
+    			for ($i = 0; $i < 3; ++$i) {
+	                writeOptionLine('', '', '', '', '', '', '');
+	            }
+    			?>
+    		</tbody>
+		</table>
+		<input type="hidden" name="formaction" value="save">
+		<p>
+		    <button type="submit" name="form_save" id="form_save" class="btn btn-default btn-save">Save</button>
+		</p>
+	</form>
+</main>
+
+<script type="text/javascript">	
+	// This invokes the select-time popup.
+	function select_time(id = '') {
+		var timeData = $('#'+id+"_raw").text();
+
+		var url = '<?php echo $GLOBALS['webroot']."/library/OemrAD/interface/batchcom/select_time.php?pid=". $pid; ?>&id='+id+'&data_set='+encodeURI(timeData);
+	  	let title = '<?php echo xlt('Time'); ?>';
+	  	dlgopen(url, 'timeSelection', 900, 400, '', title);
+	}
+
+	// This is for callback by the time popup.
+	function setTime(id, data) {
+		$('#'+id).val(data['title']);
+		$('#'+id+'_raw').text(JSON.stringify(data));
+	}
+
+	$(document).ready(function(){
+		$('.communication_type').change(function(){
+			var selectedVal = $(this).val();
+			var eleIndex = $(this).data('index');
+			var id = 'notification_template_'+eleIndex;
+			setOptionVal(selectedVal, eleIndex);
+		});
+	});
+
+	//Set Option
+	function setOptions(val, id) {
+		if(val == "email") {
+			$('#'+id).html("<?php echo getEmailTpl(); ?>");
+		} else if(val == "sms") {
+			$('#'+id).html("<?php echo getSmsTpl(); ?>");
+		} else if(val == "fax") {
+			$('#'+id).html("<?php echo getFaxTpl(); ?>");
+		} else if(val == "postalmethod") {
+			$('#'+id).html("<?php echo getPostalTpl(); ?>");
+		} else if(val == "internalmessage") {
+			$('#'+id).html("<?php echo getInternalMsgTpl(); ?>");
+		} else {
+			$('#'+id).html("<?php echo getDefaultTpl(); ?>");
+		}
+	}
+
+	//Init
+	function setOptionVal(val, index, selectVal = '') {
+		var id = 'notification_template_'+index;
+		setOptions(val, id);
+		if(selectVal != "") {
+			$('#'+id).val(selectVal);
+		}
+	}
+</script>
+
+</body>
+</html>
---
+++ interface/batchcom/extsettings.php
@@ -0,0 +1,267 @@
+<?php
+/**
+ * REminder Tool for selecting/communicating with subsets of patients
+ */
+
+//INCLUDES, DO ANY ACTIONS, THEN GET OUR DATA
+require_once("../globals.php");
+require_once("$srcdir/registry.inc");
+require_once("$srcdir/wmt-v3/wmt.globals.php");
+require_once("$srcdir/OemrAD/oemrad.globals.php");
+
+use OpenEMR\Core\Header;
+use OpenEMR\Common\Acl\AclMain;
+use OpenEMR\OemrAd\Reminder;
+
+// Option lists
+$notification_tpl_list = new wmt\Options('Notification_Template');
+
+// If we are saving, then save.
+if (isset($_POST['formaction']) && $_REQUEST['formaction'] == 'save') {
+	$opt = $_POST['opt'];
+
+	$isDeleted = Reminder::deleteActionEventConfiguration();
+
+	if($isDeleted) {
+		foreach ($opt as $key => $optItem) {
+			$savedata = array();
+			$savedata['id'] = isset($optItem['id']) ? $optItem['id'] : "";
+			$savedata['seq'] = $key;
+			$savedata['trigger_type'] = isset($optItem['trigger_type']) ? $optItem['trigger_type'] : "";
+			$savedata['action_type'] = isset($optItem['action_type']) ? $optItem['action_type'] : "";
+			$savedata['configuration_id'] = isset($optItem['configuration_id']) ? $optItem['configuration_id'] : "";
+			$savedata['active'] = isset($optItem['active']) ? $optItem['active'] : "0";
+
+			$isConfigurationExist = Reminder::isActionEventConfigurationExist($savedata['id']);
+
+			if($isConfigurationExist === false) {
+				//Insert Data
+				Reminder::saveActionEventConfiguration($savedata);
+			}
+		}
+	}
+} else if(isset($_REQUEST['formaction']) && isset($_REQUEST['id']) && $_REQUEST['formaction'] == 'inactive') {
+	$actionId = $_REQUEST['id'];
+	Reminder::updateActionEventConfiguration($actionId, array("active" => "1"));
+} else if(isset($_REQUEST['formaction']) && isset($_REQUEST['id']) && $_REQUEST['formaction'] == 'active') {
+	$actionId = $_REQUEST['id'];
+	Reminder::updateActionEventConfiguration($actionId, array("active" => "0"));
+} else if(isset($_REQUEST['formaction']) && isset($_REQUEST['id']) && $_REQUEST['formaction'] == 'delete') {
+	$actionId = $_REQUEST['id'];
+	Reminder::deleteActionEventConfiguration($actionId);
+}
+
+//Fetch configuration
+$configurationList = Reminder::getActionEventConfiguration();
+
+function writeOptionLine(
+	$id = '',
+	$trigger_type = '',
+	$action_type = '',
+	$configuration_id = '',
+	$active = ''
+) {
+
+	global $opt_line_no, $notification_tpl_list;
+	++$opt_line_no;
+
+	$trigger_type_List = array(
+		'' => 'Select',
+		'time' => 'Time',
+		'event' => 'Trigger'
+	);
+
+	$action_type_List = array(
+		'' => 'Select',
+		'messaging' => 'Messaging',
+		'internal_messaging' => 'Internal Messaging',
+		'api' => 'API',
+		'idempiere_webservice' => 'Idempiere Webservice',
+		'hubspot_sync' => 'Hubspot Sync',
+		'action_reminder' => 'Action/Reminder'
+	);
+
+	?>
+	<tr>
+		<td width="280">
+			<input type="text" class="form-control" name="opt[<?php echo $opt_line_no; ?>][id]" value="<?php echo htmlspecialchars($id, ENT_QUOTES); ?>" class="optin" />
+		</td>
+		<td>
+			<select id="<?php echo "trigger_type_".$opt_line_no; ?>" data-id="<?php echo $opt_line_no; ?>" name="opt[<?php echo $opt_line_no; ?>][trigger_type]" class="optin trigger_type form-control">
+				<?php
+				foreach ($trigger_type_List as $key => $desc) {
+		            ?>
+		            <option value="<?php echo $key ?>" <?php echo ($key == $trigger_type) ? "selected" : ""; ?> ><?php echo htmlspecialchars($desc) ?></option>
+		            <?php
+		        }
+				?>
+			</select>
+		</td>
+		<td>
+			<select id="<?php echo "action_type_".$opt_line_no; ?>" name="opt[<?php echo $opt_line_no; ?>][action_type]" class="optin form-control">
+				<?php
+				foreach ($action_type_List as $key => $desc) {
+		            ?>
+		            <option value="<?php echo $key ?>" <?php echo ($key == $action_type) ? "selected" : ""; ?> ><?php echo htmlspecialchars($desc) ?></option>
+		            <?php
+		        }
+				?>
+			</select>
+		</td>
+		<td style="width: 80px!important;">
+			<button type="button" class="btn btn-primary" onClick="time_configuration_list('<?php echo htmlspecialchars($configuration_id, ENT_QUOTES); ?>','<?php echo "configuration_id_".$opt_line_no; ?>', '<?php echo $opt_line_no; ?>', '<?php echo $id; ?>')">Edit</button>
+			<!-- <input type="hidden" id="<?php //echo "configuration_id_".$opt_line_no; ?>" name="opt[<?php //echo $opt_line_no; ?>][configuration_id]" value="<?php //echo htmlspecialchars($configuration_id, ENT_QUOTES); ?>" class="optin" style="width: 100%;" onClick="time_configuration_list('<?php //echo htmlspecialchars($configuration_id, ENT_QUOTES); ?>','<?php //echo "configuration_id_".$opt_line_no; ?>', '<?php //echo $opt_line_no; ?>')" readonly /> -->
+			<textarea class="optin" id="<?php echo "configuration_id_".$opt_line_no; ?>" name="opt[<?php echo $opt_line_no; ?>][configuration_id]" style="display: none;" ><?php echo htmlspecialchars($configuration_id, ENT_QUOTES); ?></textarea>
+			<input type="hidden" name="opt[<?php echo $opt_line_no; ?>][active]" value="<?php echo $active; ?>" />
+		</td>
+		<td width="400" valign="middle">
+			<?php if(isset($id) && !empty($id)) { ?>
+			<?php 
+				if($active == "0") {
+					$inactiveUrl = $GLOBALS['webroot']."/interface/batchcom/extsettings.php?formaction=inactive&id=".$id;
+					$btnTitle = "Inactive";
+				} else if($active == "1") {
+					$inactiveUrl = $GLOBALS['webroot']."/interface/batchcom/extsettings.php?formaction=active&id=".$id;
+					$btnTitle = "Active";
+				} 
+			?>
+			<button type="button" class="btn btn-primary" onClick="onAction('<?php echo $inactiveUrl; ?>')"><?php echo $btnTitle; ?></button>
+
+			<?php $deleteUrl = $GLOBALS['webroot']."/interface/batchcom/extsettings.php?formaction=delete&id=".$id; ?>
+			<button type="button" class="btn btn-primary" onClick="onAction('<?php echo $deleteUrl; ?>')">Delete</button>
+			
+			<button type="button" class="btn btn-primary" onClick="prepareConfigPopup('<?php echo htmlspecialchars($id, ENT_QUOTES); ?>')">Prepare/Action</button>
+
+			<?php } ?>
+			<?php if($active == "0") { ?>
+				<span class="statuslabel">Active</span>
+			<?php } else if($active == "1") { ?>
+				<span class="statuslabel">Inactive</span>
+			<?php } ?>
+		</td>
+	</tr>
+	<?php
+}
+
+?>
+<html>
+<head>
+<title><?php echo xlt('Action Events'); ?></title>
+<?php Header::setupHeader(['datetime-picker']); ?>
+
+<style type="text/css">
+	.statuslabel {
+		font-size: 12px;
+		margin-left: 10px;
+		padding-top: 8px;
+	    height: 30px;
+	    display: inline-block;
+	    vertical-align: middle;
+	}
+</style>
+
+</head>
+<body class="body_top container">
+<header class="row">
+    <h1 class="col-md-12 text-left">
+        <?php echo xlt('Action Events')?>
+    </h1>
+</header>
+<div>
+	<form method='post' name='theform' id='theform' action='extsettings.php'>
+		<table class="table table-striped table-condensed" style="margin-top:15px; width: 100%!important; max-width: 1100px;">
+			<thead>
+    			<tr>
+    				<th><b><?php xl('Event Name', 'e'); ?></b></th>
+    				<th><b><?php xl('Trigger Type', 'e'); ?></b></th>
+    				<th><b><?php xl('Action Type', 'e'); ?></b></th>
+    				<th style="width: 80px!important;"><b><?php xl('Configuration', 'e'); ?></b></th>
+    				<th></th>
+    			</tr>
+    		</thead>
+    		<tbody>
+    			<?php
+    			$opt_line_no = 0;
+
+    			foreach ($configurationList as $key => $item) {
+    				$itemid = isset($item['id']) ? $item['id'] : "";
+					$itemtrigger_type = isset($item['trigger_type']) ? $item['trigger_type'] : "";
+					$itemaction_type = isset($item['action_type']) ? $item['action_type'] : "";
+					$itemconfiguration_id = isset($item['configuration_id']) ? $item['configuration_id'] : "";
+					$itemactive = isset($item['active']) ? $item['active'] : "";
+
+					writeOptionLine($itemid, $itemtrigger_type, $itemaction_type, $itemconfiguration_id, $itemactive);
+    			}
+
+    			for ($i = 0; $i < 3; ++$i) {
+	                writeOptionLine('', '', '', '', '');
+	            }
+    			?>
+    		</tbody>
+    	</table>
+    	<input type="hidden" name="formaction" value="save">
+		<p>
+		    <button type="submit" name="form_save" id="form_save" class="btn btn-primary btn-save">Save</button>
+		</p>
+    </form>
+</div>
+
+<script type="text/javascript">	
+	var ele_id = '';
+
+	// This invokes the select-time-configuration popup.
+	function time_configuration_list(cValue = '', id = '', inx = 0, event_id = '') {
+		var config_val = $('#configuration_id_'+inx).val();
+		var timeData = $('#'+id).text();
+		var triggerType = $('#trigger_type_'+inx).val();
+		var actionType = $('#action_type_'+inx).val();
+		ele_id = id;
+
+		if(triggerType == "time") {
+			timeConfiguration(config_val, triggerType, actionType, event_id);
+		} else if(triggerType == "event") {
+			eventConfiguration(config_val, triggerType, actionType, event_id);
+		}
+	}
+
+	function timeConfiguration(cValue = '', triggerType, actionType, event_id) {
+		var url = encodeURI('<?php echo $GLOBALS['webroot']."/library/OemrAD/interface/batchcom/time_configuration_list.php?pid=". $pid ."&trigger_type="; ?>'+triggerType+'&action_type='+actionType+'&selectedItem='+cValue+'&eId='+event_id);
+	  	let title = '<?php echo xlt('Time Configuration'); ?>';
+	  	dlgopen(url, 'timeConfigurationSelection', 1200, 400, '', title);
+	}
+
+	function eventConfiguration(cValue = '', triggerType, actionType, event_id) {
+		var url = encodeURI('<?php echo $GLOBALS['webroot']."/library/OemrAD/interface/batchcom/time_configuration_list.php?pid=". $pid ."&trigger_type="; ?>'+triggerType+'&action_type='+actionType+'&selectedItem='+cValue+'&eId='+event_id);
+	  	let title = '<?php echo xlt('Trigger Configuration'); ?>';
+	  	dlgopen(url, 'eventConfigurationSelection', 1200, 400, '', title);
+	}
+
+	function prepareConfigPopup(event_id = '') {
+		if(event_id != '') {
+			var url = encodeURI('<?php echo $GLOBALS['webroot']."/library/OemrAD/interface/batchcom/action_event_log.php?pid=". $pid ; ?>&event_id='+event_id);
+		  	let title = '<?php echo xlt('Prepare/Action'); ?>';
+		  	dlgopen(url, 'prepareConfigPopup', 900, 400, '', title);
+	  	}
+	}
+
+	// This is for callback by the config id popup.
+	function setConfigId(id) {
+		$('#'+ele_id).val(JSON.stringify(id));
+		ele_id = "";
+	}
+
+	function onAction(url) {
+		window.location=url;
+	}
+
+	$(document).ready(function(){
+		$('.trigger_type').change(function(){
+			var ordId = $(this).data('id');
+			var eleid = "configuration_id_"+ordId;
+			$('#'+eleid).val('');
+		});
+	});
+</script>
+
+</body>
+</html>
