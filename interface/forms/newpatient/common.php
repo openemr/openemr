fcc8d42919ee1dd3418d9aea042a7c49a3a97e4b
stephen waite <stephen.waite@cmsvt.com>
2023-03-03 16:03:57 -0500

    fix: set viewmode to true if incoming form id for encounter form editing (#6257)
    * fix: set viewmode to true if incoming form id for encounter form editing
    
    * remove debug
    
    * remove global

--- interface/forms/newpatient/common.php
+++ interface/forms/newpatient/common.php
@@ -42,6 +42,9 @@ $years = array($thisyear - 1, $thisyear, $thisyear + 1, $thisyear + 2);
 $mode = (!empty($_GET['mode'])) ? $_GET['mode'] : null;
 
 $viewmode = false;
+if (!empty($_GET['id'])) {
+    $viewmode = true;
+}
 
 // "followup" mode is relevant when enable follow up encounters global is enabled
 // it allows the user to duplicate past encounter and connect between the two
@@ -91,7 +94,6 @@ function getDefinedFacility()
     global $viewmode;
     global $default_fac_override;
     global $care_team_facility;
-    global $GLOBALS;
     global $user_facility;
     $care_team_facility = null;
     if (!empty($GLOBALS['set_service_facility_encounter'])) {
