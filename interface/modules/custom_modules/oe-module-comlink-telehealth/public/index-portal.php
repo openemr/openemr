b3bf2b757267b978e3fa9d915c411b8169c563d9
Stephen Nielson <snielson@discoverandchange.com>
2023-03-04 16:38:55 -0500

    Skip showing error message for index-portal access
    Since we are sending users directly to the index-portal page to launch
    their telehealth session we don't want to display this as an error.

--- interface/modules/custom_modules/oe-module-comlink-telehealth/public/index-portal.php
+++ interface/modules/custom_modules/oe-module-comlink-telehealth/public/index-portal.php
@@ -23,6 +23,7 @@ if (!empty($query)) {
 }
 // need to retain the webroot if we have one
 $landingpage = $basePath . "portal/index.php?site=" . urlencode($_GET['site_id'] ?? '') . "&redirect=" . urlencode($redirect);
+$skipLandingPageError = true;
 
 // since we are working inside the portal we have to use the portal session verification logic here...
 require_once "../../../../../portal/verify_session.php";
