d7cdfe4049b55d5a84a6ee07ca1900894d4ddcbd
luisuriarte <luisuriarte@gmail.com>
2023-03-14 13:44:35 -0300

    Fixed

--- interface/code_systems/list_staged.php
+++ interface/code_systems/list_staged.php
@@ -205,9 +205,9 @@ if (is_dir($mainPATH)) {
                     array_push($revisions, $temp_date);
                     $supported_file = 1;
                 } elseif (preg_match("/SnomedCT_SpanishRelease-es_PRODUCTION_([0-9]{8})[0-9a-zA-Z]{8}.zip/", $file, $matches)) {
-                    // Hard code the version SNOMED feed to be International:Spanish
+                    // Hard code the version SNOMED feed to be International:Spanishlang
                     //
-                    $version = "International:Spanish";
+                    $version = "International:Spanishlang";
                     $rf2 = true;
                     $date_release = substr($matches[1], 0, 4) . "-" . substr($matches[1], 4, -2) . "-" . substr($matches[1], 6);
                     $temp_date = array('date' => $date_release, 'version' => $version, 'path' => $mainPATH . "/" . $matches[0]);
