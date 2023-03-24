bca4811c8bdf2def3c5ce3fa82d914dbf7edc8c3
GuateSuper <info@guatesuper.com>
2023-03-24 09:47:42 -0600

    improve vitals record when language is not English Standard

--- interface/forms/vitals/vitals.js
+++ interface/forms/vitals/vitals.js
@@ -137,7 +137,7 @@ function vitalsGetPrecision(node, defaultValue) {
 function convUnit(system, unit, value)
 {
     console.log("unit = ",unit)
-    if (unit == 'kg' || unit == 'lbs' || unit = 'libra') //when measuring in spanish, the weight measure unit is libras
+    if (unit == 'kg' || unit == 'lbs') //|| unit = 'libra') when measuring in spanish, the weight measure unit is libras
     {
         if (system == 'metric')
         {
@@ -149,7 +149,7 @@ function convUnit(system, unit, value)
         }
     }
 
-    if (unit == 'in' || unit = 'en' || unit == 'cm') //when measuring in spanish, the weight measure unit is en
+    if (unit == 'in' || unit == 'cm') //|| unit = 'en' when measuring in spanish, the weight measure unit is en
     {
         if (system == 'metric')
         {
