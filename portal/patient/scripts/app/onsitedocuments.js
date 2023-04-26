5e98186cd5c6b1ee1200b178db414af716b913d4
Jerry Padgett <sjpadgett@gmail.com>
2023-04-25 23:40:41 -0400

    Some portal fixes and changes (#6412)
    - style nav bar logo to be responsive
    - change Top Nav menu toggle to indicate menu state in button.
    - fix profile save alert to show in correct alert box.
    - add Dismiss Form button to top and bottom of editor
    - change edited document in select list to blue from red to indicated form is active.
    - change My Documents to Document Actions to be clearer with instructions.

--- portal/patient/scripts/app/onsitedocuments.js
+++ portal/patient/scripts/app/onsitedocuments.js
@@ -206,7 +206,7 @@ var page = {
                 $("#submitTemplate").hide();
                 $("#sendTemplate").hide();
                 $("#downloadTemplate").hide();
-                isModule ? $("#dismissOnsiteDocumentButton").show() : $("#dismissOnsiteDocumentButton").hide();
+                isModule ? $(".dismissOnsiteDocumentButton").show() : $(".dismissOnsiteDocumentButton").hide();
                 ((isModule || page.isFrameForm) && !page.isLocked) ? $("#saveTemplate").show() : $("#saveTemplate").hide();
                 isModule ? $("#homeTemplate").show() : $("#homeTemplate").hide();
                 (page.encounterFormName === 'HIS' && !page.isLocked) ? $("#chartHistory").show() : $("#chartHistory").hide();
@@ -616,15 +616,22 @@ var page = {
         let currentNameStyled = currentName.substr(0, currentName.lastIndexOf('.')) || currentName;
         currentNameStyled = currentNameStyled.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, ' ');
         if (currentName === 'Help') {
-            $("#dismissOnsiteDocumentButton").addClass("d-none");
+            $(".dismissOnsiteDocumentButton").addClass("d-none");
         } else {
-            $("#dismissOnsiteDocumentButton").removeClass("d-none");
+            $(".dismissOnsiteDocumentButton").removeClass("d-none");
         }
         page.isFrameForm = 0;
         page.encounterFormId = 0;
         page.encounterFormName = '';
         if (docid !== 'Help') {
             $("#topnav").hide();
+            let menuMsg;
+            if($(parent.document.getElementById('topNav')).is('.collapse:not(.show)')) {
+                menuMsg = xl("Show Top Menu");
+            } else {
+                menuMsg = xl("Hide Top Menu");
+            }
+            document.getElementById("showNav").innerHTML = menuMsg;
         }
         if (currentName === templateName && currentName && !page.isNewDoc) {
             // update form for any submits.(downloads and prints)
@@ -685,11 +692,10 @@ var page = {
                             $('#patientSignature').css('cursor', 'default').off();
                             $('#witnessSignature').css('cursor', 'default').off();
                         }
-                        bindFetch();
-
-                        if (page.isFrameForm) {
-                            //$("#editorContainer").removeClass('w-100').addClass('w-auto');
+                        if (typeof bindFetch == 'function') {
+                            bindFetch();
                         }
+
                         // new encounter form
                         // lbf has own signer instance. no binding here.
                         // page.encounterFormName & page.isFrameForm is set from template directive
