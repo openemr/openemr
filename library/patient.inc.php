f532f3a02e01635b0e34853661fb2eb43dd4337b
stephen waite <stephen.waite@cmsvt.com>
2023-06-09 17:11:17 -0400

    formatting

--- library/patient.inc.php
+++ library/patient.inc.php
@@ -399,7 +399,8 @@ function getInsuranceDataByDate(
    */
     $sql = "select $given from insurance_data as insd " .
     "left join insurance_companies as ic on ic.id = provider " .
-    "where pid = ? and (date_format(date,'%Y-%m-%d') <= ? OR date IS NULL) and (date_format(date_end,'%Y-%m-%d') >= ? OR date_end IS NULL) and " .
+    "where pid = ? and (date_format(date,'%Y-%m-%d') <= ? OR date IS NULL) and " .
+    "(date_format(date_end,'%Y-%m-%d') >= ? OR date_end IS NULL) and " .
     "type=? order by date DESC limit 1";
     return sqlQuery($sql, array($pid,$date,$date,$type));
 }
