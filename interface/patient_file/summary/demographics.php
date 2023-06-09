74a162fdc6d21c534b1a2a7cd5e0e98002e65be9
stephen waite <stephen.waite@cmsvt.com>
2023-06-09 17:10:00 -0400

    update comment

--- interface/patient_file/summary/demographics.php
+++ interface/patient_file/summary/demographics.php
@@ -1128,7 +1128,7 @@ $oemr_ui = new OemrUI($arrOeUiSettings);
                         while ($row = sqlFetchArray($res)) {
                             if ($row['provider']) {
                                 // since the query is sorted by DATE DESC can use prior ins type to identify
-                                // until insurance date_end is handled for upgraders must use old logic
+                                // unless insurance date_end is applicable
                                 $row['isOld'] = (!empty($row['date_end']) && $row['date_end'] <= date("Y-m-d")) ? true : false;
                                 if (!$row['isOld']) {
                                     $row['isOld'] = (strcmp($row['type'], $prior_ins_type) == 0) ? true : false;
