6b654e8e877c9cff337ddb5166207dd77c9af69b
Jerry Padgett <sjpadgett@gmail.com>
2023-06-09 01:07:41 -0400

    - temove unnecessary config - parse portal to core messaging theme

--- portal/messaging/messages.php
+++ portal/messaging/messages.php
@@ -106,7 +106,13 @@ function getAuthPortalUsers()
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <?php Header::setupHeader(['no_main-theme',  'portal-theme', 'ckeditor', 'angular', 'angular-sanitize', 'checklist-model']); ?>
+    <?php
+    if (IS_PORTAL) {
+        Header::setupHeader(['no_main-theme',  'portal-theme', 'ckeditor', 'angular', 'angular-sanitize', 'checklist-model']);
+    } else {
+        Header::setupHeader(['ckeditor', 'angular', 'angular-sanitize', 'checklist-model']);
+    }
+    ?>
     <title><?php echo xlt("Secure Messaging"); ?></title>
     <meta name="description" content="Mail Application" />
 </head>
