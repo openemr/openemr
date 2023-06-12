40516cc2c7598e7da9e35fdf87c1b44ad64e384d
stephen waite <stephen.waite@cmsvt.com>
2023-06-12 14:48:00 -0400

    clean up pid by payer list.php

--- contrib/util/pid_by_payer_id.php
+++ contrib/util/pid_by_payer_id.php
@@ -1,8 +1,24 @@
 <?php
 
-$_GET['site'] = 'default';
+/**
+@package   OpenEMR
+* @link      https://www.open-emr.org
+* @author    Brady Miller <brady.g.miller@gmail.com>
+* @copyright Copyright (c) 2021 Brady Miller <brady.g.miller@gmail.com>
+* @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+*/
+
+// comment this out when using this script (and then uncomment it again when done using script)
+exit;
+
+if (php_sapi_name() !== 'cli') {
+    echo "Only php cli can execute command\n";
+    echo "example use: php default 2022-01-01 2022-12-31\n";
+    die;
+}
+
+$_GET['site'] = $argv[1];
 $ignoreAuth = true;
-require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . "/../../interface/globals.php";
 
 use OpenEMR\Services\{
@@ -12,58 +28,34 @@ use OpenEMR\Services\{
     EncounterService
 };
 
+// get date range of encounters from command line args
+$startDate = $argv[2];
+$endDate = $argv[3];
 
-// grabs primary type from insurance_companies table
+// get insurance_companies by payer id, example 87726 for uhc
 $incos_by_payer_id = (new InsuranceCompanyService())->getAllByPayerID('87726');
-//var_dump($incos_by_payer_id);
-//echo count($incos_by_payer_id) . "\n";
-//exit;
 
 // grab pids with that insurance payer id
-//$pids_by_payer_id = [];
 foreach ($incos_by_payer_id as $key => $insco) {
-    //var_dump($insco);
-    //echo $insco['id'];
     $pids_by_payer_id_array[] = (new InsuranceService())->getPidsForPayerByEffectiveDate(
-        $insco['id'], 
-        $type = 'primary', 
-        $startDate = '2020-12-31',
-        $endDate = '2022-01-01'
+        $insco['id'],
+        $type = 'primary',
+        $startDate,
+        $endDate
     );
 
-    $pids_by_payer_id = array_merge(...$pids_by_payer_id_array);        
-
+    $pids_by_payer_id = array_merge(...$pids_by_payer_id_array);
 }
-
-//var_dump($pids_by_payer_id);
-//echo "\n" . count($pids_by_payer_id) . "\n";
-//print_r(array_column($pids_by_payer_id, 'pid'));
-// exit;
 
 // grab encounters by dos
-$start_date = '2021-01-01';
-$end_date = '2021-12-31';
-
-echo $start_date . " " . $end_date;
-$encs_by_date_range = (new EncounterService())->getEncountersByDateRange($start_date, $end_date);
-
-//echo count($encs_by_date_range_array) . "/n";
-//$pids = array_column($encs_by_date_range_array, 'pid');
-//print_r($pids);
-//print_r(array_column($encs_by_date_range_array, 'pid'));
-//exit;
-
-// var_dump($encs_by_date_range_array);
-
+$encs_by_date_range = (new EncounterService())->getEncountersByDateRange($startDate, $endDate);
 $encs_result = array_intersect(array_column($pids_by_payer_id, 'pid'), array_column($encs_by_date_range, 'pid'));
-//print_r($encs_result);
 asort($encs_result);
+
+$output = $output ?? '';
 foreach ($encs_result as $key => $value) {
-    $output .= "$value, ";
+    $output .= ($value ?? '') . ", ";
 }
+echo "pids list \n";
 echo $output;
 echo "\n" . count($encs_result) . "\n";
-
-//exit;
-
-
