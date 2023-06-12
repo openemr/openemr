f6d2b9abe740ad311e3ef2616bc2b47b1fbf1756
stephen waite <stephen.waite@cmsvt.com>
2023-06-12 17:29:22 -0400

    fix: implements insurance date_end for billing (#6533) (#6541)
    * use insurance date end on encounters page
    
    * check insurance end date first for demographics old insurance display
    
    * check insurance end date in billing report
    
    * update comment
    
    * formatting
    
    * formatting
    
    * formatting

--- library/patient.inc.php
+++ library/patient.inc.php
@@ -391,15 +391,18 @@ function getInsuranceDataByDate(
     $type,
     $given = "insd.*, DATE_FORMAT(subscriber_DOB,'%m/%d/%Y') as subscriber_DOB, ic.name as provider_name"
 ) {
- // this must take the date in the following manner: YYYY-MM-DD
-  // this function recalls the insurance value that was most recently enterred from the
-  // given date. it will call up most recent records up to and on the date given,
-  // but not records enterred after the given date
+  /*
+   This must take the date in the following manner: YYYY-MM-DD.
+   This function recalls the insurance value that was most recently entered from the
+   given date and before the insurance end date. It will call up most recent records up to and on the date given,
+   but not records entered after the given date.
+   */
     $sql = "select $given from insurance_data as insd " .
     "left join insurance_companies as ic on ic.id = provider " .
     "where pid = ? and (date_format(date,'%Y-%m-%d') <= ? OR date IS NULL) and " .
-    "type=? order by date DESC limit 1";
-    return sqlQuery($sql, array($pid,$date,$type));
+    "(date_format(date_end,'%Y-%m-%d') >= ? OR date_end IS NULL) and " .
+    "type = ? order by date DESC limit 1";
+    return sqlQuery($sql, array($pid, $date, $date, $type));
 }
 
 function get_unallocated_patient_balance($pid)
