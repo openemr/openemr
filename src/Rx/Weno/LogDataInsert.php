390b83d1337fe40eb3124aaa0c1e522c628db80e
omegasystemsgroup <112465339+omegasystemsgroup@users.noreply.github.com>
2023-07-05 12:29:43 +0000

    Added truncate table before importing weno pharmacies LogDataInsert.php

--- src/Rx/Weno/LogDataInsert.php
+++ src/Rx/Weno/LogDataInsert.php
@@ -4,11 +4,15 @@
  *  @package OpenEMR
  *  @link    http://www.open-emr.org
  *  @author  Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  @author  Kofi Appiah <kkappiah@medsov.com>
  *  @copyright Copyright (c) 2020 Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  @copyright Copyright (c) 2023 Omega Systems Group International <info@omegasystemsgroup.com>
  *  @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
 namespace OpenEMR\Rx\Weno;
+
+use Exception;
 
 class LogDataInsert
 {
@@ -17,8 +21,15 @@ class LogDataInsert
     }
     public function insertPrescriptions($insertdata)
     {
+        //Weno would want a full database replacement on a weekly basis
+        // hence we truncate the database first before inserting
+        $db_exist = sqlQuery("SELECT 1 FROM weno_pharmacy LIMIT 1");
+
+        if(!empty($db_exist)) {
+            $this->removeWenoPharmacies();
+        }
+
         $sql = "INSERT INTO prescriptions SET "
-            . "id = ?, "
             . "active = ?, "
             . "date_added = ?, "
             . "patient_id = ?, "
@@ -31,11 +42,10 @@ class LogDataInsert
             . "rxnorm_drugcode = ?, "
             . "external_id = ?, "
             . "indication = ?, "
-            . "txDate = ? ";
+            . "start_date = ? ";
 
         try {
             sqlInsert($sql, [
-                $insertdata['id'],
                 $insertdata['active'],
                 $insertdata['date_added'],
                 $insertdata['patient_id'],
@@ -48,11 +58,100 @@ class LogDataInsert
                 $insertdata['rxnorm_drugcode'],
                 $insertdata['provider_id'],
                 $insertdata['prescriptionguid'],
-                $insertdata['txDate']
+                $insertdata['start_date']
             ]);
-            echo "inserted data! <br>";
         } catch (Exception $e) {
             return $e->getMessage();
         }
+    }
+
+    public function insertPharmacies($insertdata)
+    {
+        $sql = "INSERT INTO weno_pharmacy SET "
+            . "ncpdp = ?, "
+            . "npi = ?, "
+            . "business_name = ?, "
+            . "address_line_1 = ?, "
+            . "address_line_2 = ?, "
+            . "city = ?, "
+            . "state = ?, "
+            . "zipcode = ?,"
+            . "country_code = ?, "
+            . "international = ?, "
+            . "pharmacy_phone = ?, "
+            . "on_weno = ?, "
+            . "test_pharmacy = ?, "
+            . "state_wide_mail_order = ?, "
+            . "24hr = ? ";
+
+        try {
+            sqlInsert($sql, [
+                $insertdata['ncpdp'],
+                $insertdata['npi'],
+                $insertdata['business_name'],
+                $insertdata['address_line_1'],
+                $insertdata['address_line_2'],
+                $insertdata['city'],
+                $insertdata['state'],
+                $insertdata['zipcode'],
+                $insertdata['country'],
+                $insertdata['international'],
+                $insertdata['pharmacy_phone'],
+                $insertdata['on_weno'],
+                $insertdata['test_pharmacy'],
+                $insertdata['state_wide_mail'],
+                $insertdata['fullDay'],
+            ]);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function updatePharmacies($insertdata)
+    {
+        $sql = "UPDATE weno_pharmacy SET "
+        . "npi = ?, "
+        . "business_name = ?, "
+        . "address_line_1 = ?, "
+        . "address_line_2 = ?, "
+        . "city = ?, "
+        . "state = ?, "
+        . "zipcode = ?,"
+        . "country_code = ?, "
+        . "international = ?, "
+        . "pharmacy_phone = ?, "
+       // . "on_weno = ?, "
+        . "test_pharmacy = ?, "
+        . "state_wide_mail_order = ?, "
+        . "24hr = ? "
+        . "WHERE ncpdp = ?";
+
+        try {
+            sqlStatement($sql, [
+                $insertdata['npi'],
+                $insertdata['business_name'],
+                $insertdata['address_line_1'],
+                $insertdata['address_line_2'],
+                $insertdata['city'],
+                $insertdata['state'],
+                $insertdata['zipcode'],
+                $insertdata['country'],
+                $insertdata['international'],
+                $insertdata['pharmacy_phone'],
+                //$insertdata['on_weno'], //pharmacy lite directory dooesnt contain on_weno field
+                $insertdata['test_pharmacy'],
+                $insertdata['state_wide_mail'],
+                $insertdata['fullDay'],
+                $insertdata['ncpdp']
+            ]);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    function removeWenoPharmacies()
+    {
+        $sql = "TRUNCATE TABLE weno_pharmacy";
+        sqlStatement($sql);
     }
 }
