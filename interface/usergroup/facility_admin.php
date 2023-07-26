cfed6f4049204f8f441d3340d0f46de0c0ebe2b9
Robert Down <robertdown@live.com>
2023-07-26 14:09:30 -0400

    Tidy up the controller portions

--- interface/usergroup/facility_admin.php
+++ interface/usergroup/facility_admin.php
@@ -26,22 +26,23 @@ require_once($GLOBALS['srcdir'] . "/validation/validation_script.js.php");
 //Gets validation rules from Page Validation list.
 //Note that for technical reasons, we are bypassing the standard validateUsingPageRules() call.
 $rules = collectValidationPageRules("/interface/usergroup/facilities_add.php");
-$args['collectThis'] = (empty($rules)) ? "undefined" : json_sanitize($rules["facility-add"]["rules"]);
-$args['forceClose'] = (isset($_POST["mode"]) && $_POST["mode"] == "facility") ? true : false;
-$args['erxEnabled'] = $GLOBALS['erx_enable'];
 
-$my_fid = isset($_GET['fid']) ? $_GET['fid'] : '';
-$args['fid'] = $my_fid;
-$args['facility'] = $facilityService->getById($my_fid);
-
+$my_fid = $_GET['fid'] ?? '';
 $pc = new POSRef();
-$args['pos_code'] = $pc->get_pos_ref();
-
 $resPBE = $facilityService->getPrimaryBusinessEntity(array("excludedId" => ($my_fid ?? null)));
 $disabled = (!empty($resPBE) && sizeof($resPBE) > 0) ? 'disabled' : '';
-$args['disablePBE'] = $disabled;
 
-$args['alertMsg'] = trim($alertmsg) ? true : false;
+$args = [
+    'collectThis' => (empty($rules)) ? "undefined" : json_sanitize($rules["facility-add"]["rules"]),
+    'forceClose' => (isset($_POST["mode"]) && $_POST["mode"] == "facility") ? true : false,
+    'erxEnabled' => $GLOBALS['erx_enable'],
+    'alertMsg' => trim($alertmsg) ? true : false,
+    'disablePBE' => $disabled,
+    'pos_code' => $pc->get_pos_ref(),
+    'mode' => 'edit',
+    'my_fid' => $my_fid,
+    'facility' => $facilityService->getById($my_fid),
+];
 
 $twig = new TwigContainer(null, $GLOBALS["kernel"]);
 $t = $twig->getTwig();
