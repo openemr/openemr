dc8324579e406e006b4dcc7d94a485f487e6a796
stephen waite <stephen.waite@cmsvt.com>
2023-08-30 13:02:53 -0400

    add valedictory to encounter form and reports

--- interface/forms/newpatient/report.php
+++ interface/forms/newpatient/report.php
@@ -33,7 +33,8 @@ function newpatient_report($pid, $encounter, $cols, $id)
         $rawRefProvider = $userService->getUser($result["referring_provider_id"]);
         $calendar_category = (new AppointmentService())->getOneCalendarCategory($result['pc_catid']);
         $reason = (!$hasAccess) ? false : $result['reason'];
-        $provider = (!$hasAccess) ? false : $rawProvider['fname'] . " " . $rawProvider['lname'];
+        $provider = (!$hasAccess) ? false : $rawProvider['fname'] . " " . $rawProvider['lname']
+            . ", " . $rawProvider['valedictory'];
         $referringProvider = (!$hasAccess || !$rawRefProvider) ? false : $rawRefProvider['fname'] . " " . $rawRefProvider['lname'];
         $posCode = (!$hasAccess) ? false : sprintf('%02d', trim($result['pos_code'] ?? false));
         $posCode = ($posCode && $posCode != '00') ? $posCode : false;
