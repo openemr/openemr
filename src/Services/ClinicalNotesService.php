43695f7a9b1d5a382f69127152ebe25a83302b68
Brady Miller <brady.g.miller@gmail.com>
2023-08-29 18:49:23 -0700

    fix: several clinical_notes form fixes

--- src/Services/ClinicalNotesService.php
+++ src/Services/ClinicalNotesService.php
@@ -315,8 +315,8 @@ class ClinicalNotesService extends BaseService
 
         $sql = "SELECT fcn.*, lo_category.title AS category_title, lo_category.notes AS category_code
                 FROM `form_clinical_notes` fcn
-                LEFT JOIN list_options lo_category ON lo_category.list_id = `Clinical_Notes_Category` AND lo_category.option_id = fcn.clinical_notes_category
-                LEFT JOIN list_options lo_type ON lo_type.list_id = `Clinical_Notes_Type` AND lo_type.option_id = fcn.clinical_notes_type
+                LEFT JOIN list_options lo_category ON lo_category.list_id = 'Clinical_Note_Category' AND lo_category.option_id = fcn.clinical_notes_category
+                LEFT JOIN list_options lo_type ON lo_type.list_id = 'Clinical_Note_Type' AND lo_type.option_id = fcn.clinical_notes_type
                 WHERE fcn.`form_id`=? AND fcn.`pid` = ? AND fcn.`encounter` = ?";
         return QueryUtils::fetchRecords($sql, array($formid, $pid, $encounter));
     }
@@ -366,7 +366,7 @@ class ClinicalNotesService extends BaseService
 
         $selectList = [];
         foreach ($optionsList as $option) {
-            $selectList[] = ['value' => $option['option_id'], 'code' => $option['notes'], 'title' => $option['title']];
+            $selectList[] = ['value' => $option['option_id'], 'code' => $option['notes'], 'title' => xl_list_label($option['title'])];
         }
         return $selectList;
     }
