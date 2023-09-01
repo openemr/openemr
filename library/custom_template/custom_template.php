5246f41a00d1c9238219c5090c0e8aa0ac15c062
Robert Down <robertdown@live.com>
2023-09-01 01:17:55 -0400

    Reduce the number of items submitted when reloading based on context to prevent HTTP 415 errors

--- library/custom_template/custom_template.php
+++ library/custom_template/custom_template.php
@@ -188,108 +188,109 @@ if (empty($isNN) && empty($rowContext)) {
             <input type="hidden" name="type" id="type" value="<?php echo  attr($type); ?>" />
             <input type="hidden" name="ccFlag" id="type" value="<?php echo  attr($cc_flag); ?>" />
             <input type="hidden" name="contextName" id="contextName" value="<?php echo attr($contextName); ?>" />
-            <div class="row">
-              <div class="col-md-12">
-                <?php if (!$isNN) { ?>
-                <div id="searchCriteria">
-                    <div class="select-box form-inline mb-1">
-                        <label for="contextId"><?php echo xlt('Context') . ':'; ?></label>
-                        <select id="contextSearch" name="contextId" class="form-control form-control-sm w-50">
-                            <option value=""></option>
-                        </select>
-                    </div>
+        </form>
+        <div class="row">
+            <div class="col-md-12">
+            <?php if (!$isNN) { ?>
+            <div id="searchCriteria">
+                <div class="select-box form-inline mb-1">
+                    <label for="contextId"><?php echo xlt('Context') . ':'; ?></label>
+                    <select id="contextSearch" name="contextId" class="form-control form-control-sm w-50">
+                        <option value=""></option>
+                    </select>
                 </div>
+            </div>
+            <?php } ?>
+            </div>
+            <div class="col-md-4 text mb-2" id="templateDD">
+            <select class="form-control form-control-sm" name="template" id="template" onchange="TemplateSentence(this.value)">
+                <option value=""><?php echo htmlspecialchars(xl('Select category'), ENT_QUOTES); ?></option>
+                <?php
+                $resTemplates = sqlStatement("SELECT * FROM template_users AS tu LEFT OUTER JOIN customlists AS c ON tu.tu_template_id=c.cl_list_slno WHERE tu.tu_user_id=? AND c.cl_list_type=3 AND cl_list_id=? AND cl_deleted=0 ORDER BY c.cl_list_item_long", array($_SESSION['authUserID'], ($rowContext['cl_list_id'] ?? null)));
+                while ($rowTemplates = sqlFetchArray($resTemplates)) {
+                    echo "<option value='" . htmlspecialchars($rowTemplates['cl_list_slno'], ENT_QUOTES) . "'>" . htmlspecialchars(xl($rowTemplates['cl_list_item_long']), ENT_QUOTES) . "</option>";
+                }
+                ?>
+            </select>
+            </div>
+            <div class="col-md-8 text mb-1">
+            <div id="share" style="display:none"></div>
+                <div class="btn-group" role="group">
+                    <a href="#" class="btn btn-secondary btn-sm" id="enter" onclick="top.restoreSession();ascii_write('13','textarea1');" title="<?php echo xla("Enter Key"); ?>"><i class="fas fa-sign-in-alt">&nbsp;</i></a>
+                    <a href="#" class="btn btn-secondary btn-sm" id="quest" onclick="top.restoreSession();CKEDITOR.instances.textarea1.insertText('? ');" title="<?php echo xla("Question Mark"); ?>"><i class="fas fa-question-circle"></i></a>
+                    <a href="#" class="btn btn-secondary btn-sm" id="para" onclick="top.restoreSession();ascii_write('para','textarea1');" title="<?php echo xla("New Paragraph"); ?>"><i class="fas fa-paragraph"></i></a>
+                    <a href="#" class="btn btn-secondary btn-sm" id="space" onclick="top.restoreSession();ascii_write('32','textarea1');" title="<?php echo xla("Space"); ?>"><?php echo xlt("Space"); ?></a>
+                </div>
+                <?php
+                $res = sqlStatement("SELECT * FROM template_users AS tu LEFT OUTER JOIN customlists AS cl ON cl.cl_list_slno = tu.tu_template_id WHERE tu.tu_user_id = ? AND cl.cl_list_type = 6 AND cl.cl_deleted = 0 ORDER BY cl.cl_order", array($_SESSION['authUserID']));
+                while ($row = sqlFetchArray($res)) { ?>
+                    <a href="#" onclick="top.restoreSession();CKEDITOR.instances.textarea1.insertText('<?php echo $row['cl_list_item_short']; ?>');" class="btn btn-primary" title="<?php echo htmlspecialchars(xl($row['cl_list_item_long']), ENT_QUOTES); ?>"><?php echo ucfirst(htmlspecialchars(xl($row['cl_list_item_long']), ENT_QUOTES)); ?></a>
                 <?php } ?>
-              </div>
-              <div class="col-md-4 text mb-2" id="templateDD">
-                <select class="form-control form-control-sm" name="template" id="template" onchange="TemplateSentence(this.value)">
-                    <option value=""><?php echo htmlspecialchars(xl('Select category'), ENT_QUOTES); ?></option>
-                    <?php
-                    $resTemplates = sqlStatement("SELECT * FROM template_users AS tu LEFT OUTER JOIN customlists AS c ON tu.tu_template_id=c.cl_list_slno WHERE tu.tu_user_id=? AND c.cl_list_type=3 AND cl_list_id=? AND cl_deleted=0 ORDER BY c.cl_list_item_long", array($_SESSION['authUserID'], ($rowContext['cl_list_id'] ?? null)));
-                    while ($rowTemplates = sqlFetchArray($resTemplates)) {
-                        echo "<option value='" . htmlspecialchars($rowTemplates['cl_list_slno'], ENT_QUOTES) . "'>" . htmlspecialchars(xl($rowTemplates['cl_list_item_long']), ENT_QUOTES) . "</option>";
-                    }
-                    ?>
-                </select>
-              </div>
-              <div class="col-md-8 text mb-1">
-                <div id="share" style="display:none"></div>
-                    <div class="btn-group" role="group">
-                        <a href="#" class="btn btn-secondary btn-sm" id="enter" onclick="top.restoreSession();ascii_write('13','textarea1');" title="<?php echo xla("Enter Key"); ?>"><i class="fas fa-sign-in-alt">&nbsp;</i></a>
-                        <a href="#" class="btn btn-secondary btn-sm" id="quest" onclick="top.restoreSession();CKEDITOR.instances.textarea1.insertText('? ');" title="<?php echo xla("Question Mark"); ?>"><i class="fas fa-question-circle"></i></a>
-                        <a href="#" class="btn btn-secondary btn-sm" id="para" onclick="top.restoreSession();ascii_write('para','textarea1');" title="<?php echo xla("New Paragraph"); ?>"><i class="fas fa-paragraph"></i></a>
-                        <a href="#" class="btn btn-secondary btn-sm" id="space" onclick="top.restoreSession();ascii_write('32','textarea1');" title="<?php echo xla("Space"); ?>"><?php echo xlt("Space"); ?></a>
-                    </div>
-                    <?php
-                    $res = sqlStatement("SELECT * FROM template_users AS tu LEFT OUTER JOIN customlists AS cl ON cl.cl_list_slno = tu.tu_template_id WHERE tu.tu_user_id = ? AND cl.cl_list_type = 6 AND cl.cl_deleted = 0 ORDER BY cl.cl_order", array($_SESSION['authUserID']));
-                    while ($row = sqlFetchArray($res)) { ?>
-                        <a href="#" onclick="top.restoreSession();CKEDITOR.instances.textarea1.insertText('<?php echo $row['cl_list_item_short']; ?>');" class="btn btn-primary" title="<?php echo htmlspecialchars(xl($row['cl_list_item_long']), ENT_QUOTES); ?>"><?php echo ucfirst(htmlspecialchars(xl($row['cl_list_item_long']), ENT_QUOTES)); ?></a>
-                    <?php } ?>
-                    <a class="btn btn-primary btn-sm btn-transmit float-right" href="#" onclick="return SelectToSave(<?php echo attr_js($type); ?>, <?php echo attr_js($cc_flag); ?>)"><?php echo xlt('Insert in Form'); ?></a>
-                </div>
-              <div class="col-md-4">
-                <div class=shadow-lgt">
-                    <div style="overflow-y: scroll; overflow-x: hidden; height: 500px">
-                        <ul id="menu5" class="example_menu w-100">
+                <a class="btn btn-primary btn-sm btn-transmit float-right" href="#" onclick="return SelectToSave(<?php echo attr_js($type); ?>, <?php echo attr_js($cc_flag); ?>)"><?php echo xlt('Insert in Form'); ?></a>
+            </div>
+            <div class="col-md-4">
+            <div class=shadow-lgt">
+                <div style="overflow-y: scroll; overflow-x: hidden; height: 500px">
+                    <ul id="menu5" class="example_menu w-100">
+                        <li>
+                            <a class="expanded"><?php echo htmlspecialchars(xl('Components'), ENT_QUOTES); ?></a>
+                            <ul id="template_sentence"></ul>
+                        </li>
+                        <?php
+                        if ($pid != '') :
+                            $row = sqlQuery("SELECT p.*, IF(ISNULL(p.providerID), NULL, CONCAT(u.lname,',',u.fname)) pcp " .
+                                "FROM patient_data p LEFT OUTER JOIN users u " .
+                                "ON u.id=p.providerID WHERE pid=?", array($pid));
+                            ?>
                             <li>
-                                <a class="expanded"><?php echo htmlspecialchars(xl('Components'), ENT_QUOTES); ?></a>
-                                <ul id="template_sentence"></ul>
+                                <a class="collapsed"><?php echo htmlspecialchars(xl('Patient Details'), ENT_QUOTES); ?></a>
+                                <ul>
+                                    <?php
+                                    $mname = ($row['mname'] != '') ? ' ' . $row['mname'] . ' ' : ' ';
+                                    $fullName = $row['fname'] . $mname . $row['lname'];
+                                    listitemCode(xl('Full name'), $fullName);
+                                    listitemCode(xl('First name'), $row['fname']);
+                                    listitemCode(xl('Last name'), $row['lname']);
+                                    listitemCode(xl('Phone'), $row['phone_home']);
+                                    listitemCode(xl('SSN'), $row['ss']);
+                                    listitemCode(xl('Date Of Birth'), $row['DOB']);
+                                    listitemCode(xl('PCP'), $row['pcp']);
+                                    ?>
+                                </ul>
                             </li>
                             <?php
-                            if ($pid != '') :
-                                $row = sqlQuery("SELECT p.*, IF(ISNULL(p.providerID), NULL, CONCAT(u.lname,',',u.fname)) pcp " .
-                                    "FROM patient_data p LEFT OUTER JOIN users u " .
-                                    "ON u.id=p.providerID WHERE pid=?", array($pid));
-                                ?>
+                            foreach ($ISSUE_TYPES as $issType => $issTypeDesc) :
+                                $res = sqlStatement('SELECT title, id, IF(diagnosis="","",CONCAT(" [",diagnosis,"]")) codes FROM lists WHERE pid=? AND type=? AND enddate IS NULL ORDER BY title', array($pid, $issType));
+                                if (sqlNumRows($res)) : ?>
                                 <li>
-                                    <a class="collapsed"><?php echo htmlspecialchars(xl('Patient Details'), ENT_QUOTES); ?></a>
+                                    <a class="collapsed"><?php echo htmlspecialchars(xl($issTypeDesc[0]), ENT_QUOTES); ?></a>
                                     <ul>
                                         <?php
-                                        $mname = ($row['mname'] != '') ? ' ' . $row['mname'] . ' ' : ' ';
-                                        $fullName = $row['fname'] . $mname . $row['lname'];
-                                        listitemCode(xl('Full name'), $fullName);
-                                        listitemCode(xl('First name'), $row['fname']);
-                                        listitemCode(xl('Last name'), $row['lname']);
-                                        listitemCode(xl('Phone'), $row['phone_home']);
-                                        listitemCode(xl('SSN'), $row['ss']);
-                                        listitemCode(xl('Date Of Birth'), $row['DOB']);
-                                        listitemCode(xl('PCP'), $row['pcp']);
+
+                                        while ($row = sqlFetchArray($res)) {
+                                            if (!empty($isNN)) {
+                                                $row['id'] = "";
+                                            }
+                                            listitemCode((strlen($row['title']) > 20) ? (substr($row['title'], 0, 18) . '..') : $row['title'], ($row['title'] . $row['codes']), $row['id']);
+                                        }
                                         ?>
                                     </ul>
                                 </li>
-                                <?php
-                                foreach ($ISSUE_TYPES as $issType => $issTypeDesc) :
-                                    $res = sqlStatement('SELECT title, id, IF(diagnosis="","",CONCAT(" [",diagnosis,"]")) codes FROM lists WHERE pid=? AND type=? AND enddate IS NULL ORDER BY title', array($pid, $issType));
-                                    if (sqlNumRows($res)) : ?>
-                                    <li>
-                                        <a class="collapsed"><?php echo htmlspecialchars(xl($issTypeDesc[0]), ENT_QUOTES); ?></a>
-                                        <ul>
-                                            <?php
-
-                                            while ($row = sqlFetchArray($res)) {
-                                                if (!empty($isNN)) {
-                                                    $row['id'] = "";
-                                                }
-                                                listitemCode((strlen($row['title']) > 20) ? (substr($row['title'], 0, 18) . '..') : $row['title'], ($row['title'] . $row['codes']), $row['id']);
-                                            }
-                                            ?>
-                                        </ul>
-                                    </li>
-                                    <?php endif;
-                                endforeach;
-                            endif; ?>
-                        </ul>
-                    </div>
+                                <?php endif;
+                            endforeach;
+                        endif; ?>
+                    </ul>
                 </div>
-                <a href="personalize.php?list_id=<?php echo $rowContext['cl_list_id'] ?? ''; ?>" id="personalize_link" class="iframe_medium btn btn-secondary btn-sm"><?php echo xlt("Personalize"); ?></a>
-                <a href="add_custombutton.php" id="custombutton" class="iframe_medium btn btn-secondary btn-sm" title="<?php echo htmlspecialchars(xl('Add Buttons for Special Chars,Texts to be Displayed on Top of the Editor for inclusion to the text on a Click'), ENT_QUOTES); ?>"><?php echo htmlspecialchars(xl('Add Buttons'), ENT_QUOTES); ?></a>
-              </div>
-              <div class="col-md-8">
-                <textarea class="ckeditor" cols="130" rows="360" id="textarea1" name="textarea1"></textarea>
-                <span class="float-right my-1"><a href="#" onclick="return SelectToSave(<?php echo attr_js($type); ?>, <?php echo attr_js($cc_flag); ?>)" class="btn btn-primary btn-sm btn-save float-right"><?php echo xlt('Insert in Form'); ?></a></span>
-              </div>
             </div>
-        </form>
+            <a href="personalize.php?list_id=<?php echo $rowContext['cl_list_id'] ?? ''; ?>" id="personalize_link" class="iframe_medium btn btn-secondary btn-sm"><?php echo xlt("Personalize"); ?></a>
+            <a href="add_custombutton.php" id="custombutton" class="iframe_medium btn btn-secondary btn-sm" title="<?php echo htmlspecialchars(xl('Add Buttons for Special Chars,Texts to be Displayed on Top of the Editor for inclusion to the text on a Click'), ENT_QUOTES); ?>"><?php echo htmlspecialchars(xl('Add Buttons'), ENT_QUOTES); ?></a>
+            </div>
+            <div class="col-md-8">
+            <textarea class="ckeditor" cols="130" rows="360" id="textarea1" name="textarea1"></textarea>
+            <span class="float-right my-1"><a href="#" onclick="return SelectToSave(<?php echo attr_js($type); ?>, <?php echo attr_js($cc_flag); ?>)" class="btn btn-primary btn-sm btn-save float-right"><?php echo xlt('Insert in Form'); ?></a></span>
+            </div>
+        </div>
+
     </div>
         <?php
   } else {
