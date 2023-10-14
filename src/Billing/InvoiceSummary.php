0f583af31efee61d741a03d533e00ac726e61028
stephen waite <stephen.waite@cmsvt.com>
2023-10-14 14:01:07 -0400

    revert invoice summary, first refactor bucks

--- src/Billing/InvoiceSummary.php
+++ src/Billing/InvoiceSummary.php
@@ -35,10 +35,8 @@
 namespace OpenEMR\Billing;
 
 require_once(dirname(__FILE__) . "/../../custom/code_types.inc.php");
-require_once(dirname(__FILE__) . "/../../library/payment.inc.php");
 
 use OpenEMR\Billing\SLEOB;
-use OpenEMR\Common\Utils\FormatMoney;
 
 // for Integrated A/R.
 //
@@ -258,128 +256,4 @@ class InvoiceSummary
 
         return -1;
     }
-
-    public static function arGetInvoiceSummaryText($codes, $allow_delete = false, $firstProcCodeIndex = -1, $encount = 0) {
-
-        foreach ($codes as $code => $cdata) {
-            ++$encount;
-            $dispcode = $code;
-
-            // remember the index of the first entry whose code is not "CO-PAY", i.e. it's a legitimate proc code
-            if ($firstProcCodeIndex == -1 && strcmp($code, "CO-PAY") != 0) {
-                $firstProcCodeIndex = $encount;
-            }
-
-            // this sorts the details more or less chronologically:
-            ksort($cdata['dtl']);
-            foreach ($cdata['dtl'] as $dkey => $ddata) {
-                $ddate = substr($dkey, 0, 10);
-                if (preg_match('/^(\d\d\d\d)(\d\d)(\d\d)\s*$/', $ddate, $matches)) {
-                    $ddate = $matches[1] . '-' . $matches[2] . '-' . $matches[3];
-                }
-                $tmpchg = "";
-                $tmpadj = "";
-                if (!empty($ddata['chg']) && ($ddata['chg'] != 0)) {
-                    if (isset($ddata['rsn'])) {
-                        $tmpadj = 0 - $ddata['chg'];
-                    } else {
-                        $tmpchg = $ddata['chg'];
-                    }
-                }
-                ?>
-                <tr>
-                    <td class="detail" style="background:<?php echo $dispcode ? 'lightyellow' : ''; ?>"><?php echo text($dispcode); $dispcode = "" ?></td>
-                    <td class="detail"><?php echo text(FormatMoney::getFormattedMoney($tmpchg)); ?></td>
-                    <td class="detail">&nbsp;</td>
-                    <td class="detail">
-                        <?php
-                        if (isset($ddata['plv'])) {
-                            if (!$ddata['plv']) {
-                                echo 'Pt/';
-                            } else {
-                                echo 'Ins' . text($ddata['plv']) . '/';
-                            }
-                        }
-                        echo text($ddata['src'] ?? '');
-                        ?>
-                    </td>
-                    <td class="detail"><?php echo text($ddate); ?></td>
-                    <td class="detail"><?php echo text(FormatMoney::getFormattedMoney($ddata['pmt'] ?? '')); ?></td>
-                    <td class="detail"><?php echo text(FormatMoney::getFormattedMoney($tmpadj)); ?></td>
-                    <td class="detail">&nbsp;</td>
-                    <td class="detail"><?php echo text($ddata['rsn'] ?? ''); ?></td>
-                    <?php
-                    if ($allow_delete) { ?>
-                        <td class="detail">
-                            <?php
-                            if (!empty($ddata['arseq'])) { ?>
-                                <input name="form_del[<?php echo attr($ddata['arseq']); ?>]"
-                                    type="checkbox" />
-                                <?php
-                            } else {
-                                ?> &nbsp;
-                                <?php
-                            } ?>
-                        </td>
-                    <?php } ?>
-                </tr>
-            <?php } // end of prior detail line ?>
-            <tr>
-                <td class="last_detail"><?php echo text($dispcode);
-                    $dispcode = "" ?>
-                </td>
-                <td class="last_detail">&nbsp;</td>
-                <td class="last_detail">
-                    <input name="form_line[<?php echo attr($code); ?>][bal]" type="hidden"
-                        value="<?php echo attr(FormatMoney::getFormattedMoney($cdata['bal'])); ?>" />
-                    <input name="form_line[<?php echo attr($code); ?>][ins]" type="hidden"
-                        value="<?php echo attr($cdata['ins'] ?? ''); ?>" />
-                    <input name="form_line[<?php echo attr($code); ?>][code_type]" type="hidden"
-                        value="<?php echo attr($cdata['code_type'] ?? ''); ?>" /> <?php echo text(sprintf("%.2f", $cdata['bal'])); ?>
-                    &nbsp;
-                </td>
-                <td class="last_detail"></td>
-                <td class="last_detail"></td>
-                <td class="last_detail">
-                    <input name="form_line[<?php echo attr($code); ?>][pay]"
-                        onkeyup="updateFields(document.forms[0]['form_line[<?php echo attr($code); ?>][pay]'], document.forms[0]['form_line[<?php echo attr($code); ?>][adj]'], document.forms[0]['form_line[<?php echo attr($code); ?>][bal]'], document.forms[0]['form_line[CO-PAY][bal]'], <?php echo ($firstProcCodeIndex == $encount) ? 1 : 0 ?>)"
-                        onfocus="this.select()" autofocus size="10" type="text" class="form-control"
-                        value="0.00" />
-                </td>
-                <td class="last_detail">
-                    <input name="form_line[<?php echo attr($code); ?>][adj]" size="10" type="text"
-                        class="form-control"
-                        value='<?php echo attr((!empty($totalAdjAmount)) ? $totalAdjAmount : '0.00'); ?>'
-                        onclick="this.select()" />
-                </td>
-                <td class="last_detail text-center">
-                    <a href="#" class="text-decoration-none" onclick="return writeoff(<?php echo attr_js($code); ?>)">WO</a>
-                </td>
-                <td class="last_detail">
-                    <select class="form-control" name="form_line[<?php echo attr($code); ?>][reason]">
-                        <?php
-                        // Adjustment reasons are now taken from the list_options table.
-                        echo "    <option value=''></option>\n";
-                        $ores = sqlStatement("SELECT option_id, title, is_default FROM list_options " .
-                            "WHERE list_id = 'adjreason' AND activity = 1 ORDER BY seq, title");
-                        while ($orow = sqlFetchArray($ores)) {
-                            echo "    <option value='" . attr($orow['option_id']) . "'";
-                            if ($orow['is_default']) {
-                                echo " selected";
-                            }
-                            echo ">" . text($orow['title']) . "</option>\n";
-                        }
-                        ?>
-                    </select>
-                    <?php
-                    // TBD: Maybe a comment field would be good here, for appending
-                    // to the reason.
-                    ?>
-                </td>
-                <?php if ($allow_delete) { ?>
-                    <td class="last_detail">&nbsp;</td>
-                <?php } ?>
-            </tr>
-        <?php } // end of code ?>
-<?php } 
 }
