d2eff85aff72418a9e435bc3511b6a9be6c567f9
dataengr <98280242+dataengr@users.noreply.github.com>
2023-10-25 06:09:28 +0000

    remove vehr items

--- interface/modules/custom_modules/oe-module-visualehr/api/color.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function getColor()
-{
-    $colors = ["#eb5ea8", "#9f4ed3", "#99b1fc", "#7e83d0", "#fb4e4e", "#32a852", "#fb4e4e"];
-    $randColorIndex = random_int(0, count($colors) - 1);
-    $generatedColor = $colors[$randColorIndex];
-    return $generatedColor;
-}
