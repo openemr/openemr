8332700877f90f7885b628de87074842295382f5
Ken Chapple <ken@mi-squared.com>
2023-10-27 14:38:20 -0400

    only push active diag to newcrop

--- interface/eRxStore.php
+++ interface/eRxStore.php
@@ -11,7 +11,7 @@
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-require_once(__DIR__ . "/../library/api.inc.php");
+require_once(__DIR__ . "/../library/api.inc");
 
 class eRxStore
 {
