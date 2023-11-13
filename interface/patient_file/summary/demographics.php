2df01603682b316be3cd4b0db0865e1acc332549
Jerry Padgett <sjpadgett@gmail.com>
2023-11-13 13:27:51 -0500

    Persist summary screen container state (#7017)
    * Persist summary screen container state
    - Just needed a value for utility
    
    * - Undefined variable $count2 in C:\xampp\htdocs\openemr\interface\patient_file\summary\demographics.php on line 1933
    - Undefined array key "" in C:\xampp\htdocs\openemr\library\classes\Prescription.class.php on line 413
    - Undefined array key "" in C:\xampp\htdocs\openemr\library\classes\Prescription.class.php on line 324
    - Trying to access array offset on value of type bool in C:\xampp\htdocs\openemr\library\forms.inc.php on line 162

--- interface/patient_file/summary/demographics.php
+++ interface/patient_file/summary/demographics.php
@@ -335,7 +335,7 @@ $arrOeUiSettings = array(
     'heading_title' => xl('Medical Record Dashboard'),
     'include_patient_name' => true,
     'expandable' => true,
-    'expandable_files' => array(), //all file names need suffix _xpd
+    'expandable_files' => array('demographics_xpd'), //all file names need suffix _xpd
     'action' => "", //conceal, reveal, search, reset, link or back
     'action_title' => "",
     'action_href' => "", //only for actions - reset, link or back
@@ -1923,6 +1923,7 @@ $oemr_ui = new OemrUI($arrOeUiSettings);
                             // Show Recall if one exists
                             $query = sqlStatement("SELECT * FROM `medex_recalls` WHERE `r_pid` = ?", [(int)$pid]);
                             $recallArr = [];
+                            $count2 = 0;
                             while ($result2 = sqlFetchArray($query)) {
                                 //tabYourIt('recall', 'main/messages/messages.php?go=' + choice);
                                 //parent.left_nav.loadFrame('1', tabNAME, url);
