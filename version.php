b9106da71c7066b4565b4f28ee839fe49aca6879
Brady Miller <brady.g.miller@gmail.com>
2023-11-14 23:55:43 -0800

    chore: increment v_js_includes

--- version.php
+++ version.php
@@ -47,7 +47,7 @@ if (!empty($_ENV['OPENEMR__ENVIRONMENT']) && ($_ENV['OPENEMR__ENVIRONMENT'] === 
     $v_js_includes = md5(microtime());
 } else {
     // Change this number when bumping
-    $v_js_includes = 75;
+    $v_js_includes = 76;
 }
 
 // Do not modify below
