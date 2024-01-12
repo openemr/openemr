38df906906bb2c2261d0f504ba5bb7a87ddec30a
Hank Rappaport <hanksterr7@aol.com>
2024-01-12 02:37:21 -0500

    fix: clinical rules - fix the optional/required setting (#7154)

--- sql/database.sql
+++ sql/database.sql
@@ -8053,7 +8053,7 @@ DROP TABLE IF EXISTS `rule_filter`;
 CREATE TABLE `rule_filter` (
   `id` varchar(31) NOT NULL DEFAULT '' COMMENT 'Maps to the id column in the clinical_rules table',
   `include_flag` tinyint(1) NOT NULL default 0 COMMENT '0 is exclude and 1 is include',
-  `required_flag` tinyint(1) NOT NULL default 0 COMMENT '0 is required and 1 is optional',
+  `required_flag` tinyint(1) NOT NULL default 0 COMMENT '0 is optional and 1 is required',
   `method` varchar(31) NOT NULL DEFAULT '' COMMENT 'Maps to list_options list rule_filters',
   `method_detail` varchar(31) NOT NULL DEFAULT '' COMMENT 'Maps to list_options lists rule__intervals',
   `value` varchar(255) NOT NULL DEFAULT '',
