1c5a1c83c8ac1028bf705d5a3d7f2459e8a9820b
Jerry Padgett <sjpadgett@gmail.com>
2024-02-02 14:38:12 -0500

    - no clue

--- interface/modules/custom_modules/oe-module-weno/src/Services/WenoPharmaciesImport.php
+++ interface/modules/custom_modules/oe-module-weno/src/Services/WenoPharmaciesImport.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace OpenEMR\Modules\WenoModule\Services;
+
 /**
  *  @package OpenEMR
  *  @link    http://www.open-emr.org
@@ -8,11 +10,7 @@
  *  @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-namespace OpenEMR\Modules\WenoModule\Services;
-
-
 use Exception;
-use RuntimeException;
 
 class WenoPharmaciesImport
 {
