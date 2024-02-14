ba69cb06c13966a3b3a6f9ae0dabdbb7eb1542bd
stephen waite <stephen.waite@cmsvt.com>
2024-02-13 21:57:17 -0500

    bug: fix bug

--- interface/usergroup/facility_admin.php
+++ interface/usergroup/facility_admin.php
@@ -18,6 +18,12 @@ use OpenEMR\Common\Twig\TwigContainer;
 use OpenEMR\Core\Header;
 use OpenEMR\Services\FacilityService;
 
+// Ensure authorized
+if (!AclMain::aclCheckCore('admin', 'users')) {
+    echo (new TwigContainer(null, $GLOBALS['kernel']))->getTwig()->render('core/unauthorized.html.twig', ['pageTitle' => xl("Facility Admin")]);
+    exit;
+}
+
 $facilityService = new FacilityService();
 
 $alertmsg = '';
