889e6b9bb0a3e57bcc54ac19acaefc0f6698b8bf
hKhatri <hardik.khatri110@gmail.com>
2024-03-20 18:33:57 +0530

    Fixed PHP Styling issues

--- interface/forms/LBF/php/find_encounter_form_popup.php
+++ interface/forms/LBF/php/find_encounter_form_popup.php
@@ -6,14 +6,6 @@
  *
  * @package   OpenEMR
  * @link      https://www.open-emr.org
- * @author    Rod Roark <rod@sunsetsystems.com>
- * @author    Ian Jardine ( github.com/epsdky )
- * @author    Roberto Vasquez <robertogagliotta@gmail.com>
- * @author    Brady Miller <brady.g.miller@gmail.com>
- * @author    Stephen Waite <stephen.waite@cmsvt.com>
- * @copyright Copyright (c) 2005-2013 Rod Roark <rod@sunsetsystems.com>
- * @copyright Copyright (c) 2017-2019 Brady Miller <brady.g.miller@gmail.com>
- * @copyright Copyright (c) 2019 Stephen Waite <stephen.waite@cmsvt.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
 */
 
@@ -30,7 +22,8 @@ $pid = isset($_REQUEST['pid']) ? $_REQUEST['pid'] : "";
 $formname = isset($_REQUEST['formname']) ? $_REQUEST['formname'] : "";
 $encounter = isset($_REQUEST['encounter']) ? $_REQUEST['encounter'] : "";
 
-function fetch_appt_signatures_data_byId($eid) {
+function fetch_appt_signatures_data_byId($eid)
+{
     if (!empty($eid)) {
         $eSql = "SELECT FE.encounter, E.id, E.tid, E.table, E.uid, U.fname, U.lname, E.datetime, E.is_lock, E.amendment, E.hash, E.signature_hash
                 FROM form_encounter FE
@@ -48,92 +41,85 @@ $result4 = sqlStatement("SELECT fe.encounter,fe.date,openemr_postcalendar_catego
 
 $enounterList = array();
 while ($rowresult4 = sqlFetchArray($result4)) {
-	$encounter = isset($rowresult4['encounter']) ? $rowresult4['encounter'] : '';
-	$id = '';
+    $encounter = isset($rowresult4['encounter']) ? $rowresult4['encounter'] : '';
+    $id = '';
 
-	if (!empty($encounter)) {
-		$sql= "SELECT * FROM forms WHERE deleted=0 AND pid=? AND encounter=? AND formdir=?";
-		$parms = array($pid, $encounter, $formname);
-		$frow = sqlQuery($sql, $parms);
-		if($frow['form_id']) {
-			$id= $frow['form_id'];
-		}
-	}
+    if (!empty($encounter)) {
+        $sql = "SELECT * FROM forms WHERE deleted=0 AND pid=? AND encounter=? AND formdir=?";
+        $parms = array($pid, $encounter, $formname);
+        $frow = sqlQuery($sql, $parms);
+        if ($frow['form_id']) {
+            $id = $frow['form_id'];
+        }
+    }
 
-	if (!empty($id)) {
-		$rowresult4['form_id'] = $id;
-		$enounterList[] = $rowresult4;
-	}
+    if (!empty($id)) {
+        $rowresult4['form_id'] = $id;
+        $enounterList[] = $rowresult4;
+    }
 }
-
 ?>
 
 <html>
 <head>
-	<title><?php echo htmlspecialchars( xl('Select Encounter'), ENT_NOQUOTES); ?></title>
-	<link rel="stylesheet" href='<?php echo $css_header ?>' type='text/css'>
-	<?php Header::setupHeader(['opener', 'dialog', 'jquery', 'jquery-ui', 'jquery-ui-base', 'fontawesome', 'main-theme']); ?>
+    <title><?php echo htmlspecialchars(xl('Select Encounter'), ENT_NOQUOTES); ?></title>
+    <link rel="stylesheet" href='<?php echo $css_header ?>' type='text/css'>
+    <?php Header::setupHeader(['opener', 'dialog', 'jquery', 'jquery-ui', 'jquery-ui-base', 'fontawesome', 'main-theme']); ?>
 
-	<style type="text/css">
-		
-		.encounter-container {
-			padding-top: 20px;
-			font-size: 16px;
-		}
+    <style type="text/css">
+        .encounter-container {
+            padding-top: 20px;
+            font-size: 16px;
+        }
 
-		.encounter-container ul li {
-			line-height: 25px;
-		}
+        .encounter-container ul li {
+            line-height: 25px;
+        }
+    </style>
 
-	</style>
+    <script type="text/javascript">
+        function selectEncounter(encounter_id, form_id, pid) {
+            return selEncounterForm(encounter_id, form_id, pid);
+        }
 
-	<script type="text/javascript">
-		
-		function selectEncounter(encounter_id, form_id, pid) {
-			return selEncounterForm(encounter_id, form_id, pid);
-		}
-
-		function selEncounterForm(encounter_id, form_id, pid) {
-			if (opener.closed || ! opener.setEncounterForm)
-			alert("<?php echo htmlspecialchars( xl('The destination form was closed; I cannot act on your selection.'), ENT_QUOTES); ?>");
-			else
-			opener.setEncounterForm(encounter_id, form_id, pid);
-			window.close();
-			return false;
-		 }
-
-	</script>
+        function selEncounterForm(encounter_id, form_id, pid) {
+            if (opener.closed || ! opener.setEncounterForm)
+            alert("<?php echo htmlspecialchars(xl('The destination form was closed; I cannot act on your selection.'), ENT_QUOTES); ?>");
+            else
+            opener.setEncounterForm(encounter_id, form_id, pid);
+            window.close();
+            return false;
+        }
+    </script>
 </head>
 <body>
-	<div class="encounter-container">
-		<ul>
-			<?php
-				foreach ($enounterList as $i => $item) {
-					$edate = isset($item['date']) ? date($dateFormat, strtotime($item['date'])) : '';
-					$cCat = isset($item['pc_catname']) ? $item['pc_catname'] : '';
-					$pName = trim($item['fname'].' '.$item['mname'].' '.$item['lname']);
-					if(!empty($pName)) {
-						$pName = ' - '.$pName;
-					}
+    <div class="encounter-container">
+        <ul>
+            <?php
+            foreach ($enounterList as $i => $item) {
+                $edate = isset($item['date']) ? date($dateFormat, strtotime($item['date'])) : '';
+                $cCat = isset($item['pc_catname']) ? $item['pc_catname'] : '';
+                $pName = trim($item['fname'] . ' ' . $item['mname'] . ' ' . $item['lname']);
+                if (!empty($pName)) {
+                    $pName = ' - ' . $pName;
+                }
 
-					$signed = $item['signed'] === true ? 'Signed' : 'Unsigned';
-					if(!empty($signed)) {
-						$signed = ' - <i>'.$signed.'</i>';
-					}
+                $signed = $item['signed'] === true ? 'Signed' : 'Unsigned';
+                if (!empty($signed)) {
+                    $signed = ' - <i>' . $signed . '</i>';
+                }
 
-					$titleLink = trim($edate .' '. $cCat.$pName.$signed);
+                $titleLink = trim($edate . ' ' . $cCat . $pName . $signed);
 
-					$encounter_id = isset($item['encounter']) ? $item['encounter'] : '';
-					$form_id = isset($item['form_id']) ? $item['form_id'] : '';
+                $encounter_id = isset($item['encounter']) ? $item['encounter'] : '';
+                $form_id = isset($item['form_id']) ? $item['form_id'] : '';
 
-					?>
-					<li>
-						<a href="javascript: void(0)" onClick="selectEncounter('<?php echo $encounter_id; ?>', '<?php echo $form_id; ?>', '<?php echo $pid; ?>')"><?php echo $titleLink; ?></a>
-					</li>
-					<?php
-				}
-			?>
-		</ul>
-	</div>
+                ?>
+                <li>
+                   <a href="javascript: void(0)" onClick="selectEncounter('<?php echo $encounter_id; ?>', '<?php echo $form_id; ?>', '<?php echo $pid; ?>')"><?php echo $titleLink; ?></a>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
 </body>
 </html>
