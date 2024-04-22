f71adfef61cd0c2d4b599e1bd52ee3798dd4db47
ruth <ruth@muswell.me.uk>
2024-04-22 17:31:03 +0100

    display correct count values for ALL - issue #7385

--- interface/patient_file/history/encounters.php
+++ interface/patient_file/history/encounters.php
@@ -437,7 +437,9 @@ window.onload = function() {
             if (($pagesize > 0) && ($pagestart > 0)) {
                 generatePageElement($pagestart - $pagesize, $pagesize, $billing_view, $issue, "&lArr;" . htmlspecialchars(xl("Prev"), ENT_NOQUOTES) . " ");
             }
-            echo ($pagestart + 1) . "-" . $upper . " " . htmlspecialchars(xl('of'), ENT_NOQUOTES) . " " . $numRes;
+
+            echo (($pagesize > 0) ? ($pagestart + 1) : "1") . "-" . $upper . " " . htmlspecialchars(xl('of'), ENT_NOQUOTES) . " " . $numRes;
+
             if (($pagesize > 0) && ($pagestart + $pagesize <= $numRes)) {
                 generatePageElement($pagestart + $pagesize, $pagesize, $billing_view, $issue, " " . htmlspecialchars(xl("Next"), ENT_NOQUOTES) . "&rArr;");
             }
