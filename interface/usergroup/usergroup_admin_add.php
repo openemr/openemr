0ed60e5b12060f70380418835626b41ef79d062c
mabeshark <38800918+mabeshark@users.noreply.github.com>
2024-04-22 22:21:51 -0400

    Fix: Input sanitation on google signin email (#7356)
    * Fix input sanitation on google signin email
    
    * ValidationUtils::isValidEmail to validate email
    
    * Refactor email validation into js function
    
    * Syntax fixes to js
    
    * Revert backend changes
    
    * Fix PHP syntax issues
    
    * Fix PHP syntax issues

--- interface/usergroup/usergroup_admin_add.php
+++ interface/usergroup/usergroup_admin_add.php
@@ -117,6 +117,12 @@ function submitform() {
         }
     } //secure_pwd if ends here
 
+    // Valiate Google email (if provided)
+    if(document.new_user.google_signin_email.value != "" && !isValidEmail(document.new_user.google_signin_email.value)) {
+        alert(<?php echo xlj('Google email provided is invalid/not properly formatted (e.g. first.last@gmail.com)') ?>);
+        return false;
+    }
+
     <?php if ($GLOBALS['erx_enable']) { ?>
    alertMsg='';
    f=document.forms[0];
