fc285b4e3d3c0d77286a11fb0042264bbb80a6e5
Jerry Padgett <sjpadgett@gmail.com>
2024-06-20 01:03:26 -0400

    change weno_pharmacy structure

--- interface/modules/custom_modules/oe-module-weno/sql/1_0_0-to-2_0_0_upgrade.sql
+++ interface/modules/custom_modules/oe-module-weno/sql/1_0_0-to-2_0_0_upgrade.sql
@@ -27,7 +27,7 @@ CREATE TABLE `weno_pharmacy` (
      `State_Wide_Mail_Order` varchar(15) NOT NULL,
      `Mail_Order_US_State_Serviced` varchar(255) DEFAULT NULL,
      `Mail_Order_ US_Territories_Serviced` varchar(255) DEFAULT NULL,
-     `On_WENO` tinyint DEFAULT NULL,
+     `On_WENO` varchar(10) DEFAULT NULL,
      `24HR` varchar(3) DEFAULT NULL,
      PRIMARY KEY (`id`),
      UNIQUE KEY `ncpdp` (`NCPDP_safe`)
