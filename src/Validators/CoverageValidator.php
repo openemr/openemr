896e0510cce224da95ceed3e3bfcd4c9993be4d1
Stephen Nielson <snielson@discoverandchange.com>
2024-06-28 10:41:09 -0600

    ins subscriber name validation fixes see #7477 (#7478)
    * ins subscriber name validation fixes see #7477
    
    This puts in multibyte case insensitive validation on the subscriber
    previous name fields.  It does not fully fix the issue for all unicode
    names as apparently some unicode character sets can have multiple
    uppercase characters that correspond to a single lowercase and viceversa
    with many lowercase to a single uppercase.  Some research shows we need
    to normalize the characters and then do a comparison but need to do more
    research to see if this is an actual valid assumption or not before
    implementing.
    
    This will almost fix #7477 for nearly all issues, but the edge cases
    still need to be addressed.
    
    * Implement multi-byte normalization
    
    Did a bunch of research on the unicode normalization process to make
    sure we were using the correct normalization form.  Implemented a unit
    test to verify various test cases.  Everything appears to be working
    correctly.
    
    * Remove unused namespace again...
    
    * Remove old TODO comments

--- src/Validators/CoverageValidator.php
+++ src/Validators/CoverageValidator.php
@@ -112,7 +112,13 @@ class CoverageValidator extends BaseValidator
                                     $previousNames = $patient['previous_names'];
                                     $found = false;
                                     foreach ($previousNames as $previousName) {
-                                        if ($previousName['previous_name_first'] == $values['subscriber_fname'] && $previousName['previous_name_last'] == $values['subscriber_lname']) {
+                                        // do a strict equality and then we can do multibyte comparison for localizations
+                                        // note if we want to handle more comprehensive multibytes
+                                        // we need to do some normalizations as per this stackoverflow post: https://stackoverflow.com/a/38855868
+                                        if (
+                                            mb_is_string_equal_ci($previousName['previous_name_first'], $values['subscriber_fname'])
+                                            && mb_is_string_equal_ci($previousName['previous_name_last'], $values['subscriber_lname'])
+                                        ) {
                                             $found = true;
                                             break;
                                         }
