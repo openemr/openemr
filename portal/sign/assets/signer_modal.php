21cfddb12911e8b662235ef8f85539dde48107c1
Stephen Nielson <snielson@discoverandchange.com>
2024-08-01 14:17:16 -0600

    Fix styles

--- portal/sign/assets/signer_modal.php
+++ portal/sign/assets/signer_modal.php
@@ -33,8 +33,8 @@ if (empty($is_portal)) {
 
 require_once(__DIR__ . '/../../../interface/globals.php');
 
-use \OpenEMR\Common\Logging\SystemLogger;
-use \OpenEMR\Common\Twig\TwigContainer;
+use OpenEMR\Common\Logging\SystemLogger;
+use OpenEMR\Common\Twig\TwigContainer;
 
 $aud = "admin-signature";
 $cuser = attr($_SESSION['authUserID'] ?? "-patient-");
