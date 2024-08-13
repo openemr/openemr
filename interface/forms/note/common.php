542834f2dc272cee1c1e9caa11480fdd6170cbc2
juggernautsei <sherwingaddis@gmail.com>
2024-08-13 18:05:37 -0400

    refactor form

--- interface/forms/note/common.php
+++ interface/forms/note/common.php
@@ -35,14 +35,14 @@ if ($_GET['id'] != "") {
 ?>
 <html>
 <head>
-<title><?php echo xlt('School/Work Note'); ?></title>
-<?php Header::setupHeader('datetime-picker, common'); ?>
+    <title><?php echo xlt('School/Work Note'); ?></title>
+    <?php Header::setupHeader('datetime-picker'); ?>
 
-<script>
-// required for textbox date verification
-var mypcc = <?php echo js_escape($GLOBALS['phone_country_code']); ?>;
+    <script>
+        // required for textbox date verification
+        var mypcc = <?php echo js_escape($GLOBALS['phone_country_code']); ?>;
 
-$(function () {
+        $(function () {
             $('.datepicker').datetimepicker({
                 <?php $datetimepicker_timepicker = false; ?>
                 <?php $datetimepicker_showseconds = false; ?>
@@ -52,11 +52,11 @@ $(function () {
             });
         });
 
-function PrintForm() {
-    newwin = window.open(<?php echo js_escape($rootdir . "/forms/" . $form_name . "/print.php?id=" . urlencode($_GET["id"])); ?>,"mywin");
-}
+        function PrintForm() {
+            newwin = window.open(<?php echo js_escape($rootdir . "/forms/" . $form_name . "/print.php?id=" . urlencode($_GET["id"])); ?>,"mywin");
+        }
 
-</script>
+    </script>
 </head>
 <body>
 <div class="container">
@@ -68,55 +68,59 @@ function PrintForm() {
     </div>
     <form method=post action="<?php echo $rootdir . "/forms/" . $form_name . "/save.php?mode=update&id=" . attr_url($_GET["id"]);?>" name="my_form" id="my_form">
         <input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
-
-        <select name="note_type" class="form-control">
+        <div class="m-3">
+            <select name="note_type" class="form-control">
                 option value="WORK NOTE" <?php if ($obj['note_type'] == "WORK NOTE") {
                     echo " SELECTED";
                 } ?>><?php echo xlt('WORK NOTE'); ?></option>
                 <option value="SCHOOL NOTE" <?php if ($obj['note_type'] == "SCHOOL NOTE") {
                     echo " SELECTED";
                 } ?>><?php echo xlt('SCHOOL NOTE'); ?></option><
-        </select>
-        <br />
-        <b><?php echo xlt('MESSAGE:'); ?></b>
-        <br />
-        <textarea class="form-control" name="message" id="message" cols ="67" rows="4"><?php echo text($obj["message"]);?></textarea>
-        <br /> <br />
-
-        <table>
-        <tr><td>
-        <span class="mr-1"><?php echo xlt('Doctor:'); ?> </span><input class="form-control mr-2" type="text" name="doctor" value="<?php echo attr($obj["doctor"]);?>">
-        </td><td>
-        <span class="ml-3"><?php echo xlt('Date'); ?></span>
-           <input type='text' size='10' class='form-control datepicker ml-3' name='date_of_signature' id='date_of_signature'
-            value='<?php echo attr(oeFormatShortDate($obj['date_of_signature'])); ?>'
-            title='<?php echo xla('Date of Signature'); ?>' />
-        </td></tr>
-        </table>
+            </select>
+        </div>
+        <div class="m-4">
+            <b><?php echo xlt('MESSAGE:'); ?></b>
+            <textarea class="form-control" name="message" id="message" cols ="67" rows="4"><?php echo text($obj["message"]);?></textarea>
+        </div>
+        <div class="mt-4">
+            <b><?php echo xlt('Signature:'); ?>:</b>
+        </div>
+        <div class="m-3">
+            <table>
+                <tr><td>
+                        <span class="mr-1"><?php echo xlt('Doctor:'); ?> </span><input class="form-control mr-2" type="text" name="doctor" value="<?php echo attr($obj["doctor"]);?>">
+                    </td><td>
+                        <span class="ml-3"><?php echo xlt('Date'); ?></span>
+                        <input type='text' size='10' class='datepicker form-control ml-3' name='date_of_signature' id='date_of_signature'
+                               value='<?php echo attr(oeFormatShortDate($obj['date_of_signature'])); ?>'
+                               title='<?php echo xla('Date of Signature'); ?>' />
+                    </td></tr>
+            </table>
+        </div>
         <div style="margin: 10px;">
             <input type="button" class="btn btn-primary mr-1" value="    <?php echo xla('Save'); ?>    ">
             <input type="button" class="btn btn-warning mr-1" value="<?php echo xla('Don\'t Save'); ?>">
-            <input type="button" class="btn btn-success" value="<?php echo xla('View Printable Version'); ?>">
+            <input type="button" class="printform btn btn-success" value="<?php echo xla('View Printable Version'); ?>">
         </div>
     </form>
 </div>
 </body>
 <script>
 
-// jQuery stuff to make the page a little easier to use
+    // jQuery stuff to make the page a little easier to use
 
-$(function () {
-    $(".save").click(function() { top.restoreSession(); $("#my_form").submit(); });
-    $(".dontsave").click(function() { parent.closeTab(window.name, false); });
-    $(".printform").click(function() { PrintForm(); });
+    $(function () {
+        $(".save").click(function() { top.restoreSession(); $("#my_form").submit(); });
+        $(".dontsave").click(function() { parent.closeTab(window.name, false); });
+        $(".printform").click(function() { PrintForm(); });
 
-    // disable the Print ability if the form has changed
-    // this forces the user to save their changes prior to printing
-    $("#img_date_of_signature").click(function() { $(".printform").attr("disabled","disabled"); });
-    $("input").keydown(function() { $(".printform").attr("disabled","disabled"); });
-    $("select").change(function() { $(".printform").attr("disabled","disabled"); });
-    $("textarea").keydown(function() { $(".printform").attr("disabled","disabled"); });
-});
+        // disable the Print ability if the form has changed
+        // this forces the user to save their changes prior to printing
+        $("#img_date_of_signature").click(function() { $(".printform").attr("disabled","disabled"); });
+        $("input").keydown(function() { $(".printform").attr("disabled","disabled"); });
+        $("select").change(function() { $(".printform").attr("disabled","disabled"); });
+        $("textarea").keydown(function() { $(".printform").attr("disabled","disabled"); });
+    });
 
 </script>
 
