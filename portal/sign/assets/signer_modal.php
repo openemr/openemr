5095ac4d2408bb3916c250ba5b56203d1d5d7aea
Stephen Nielson <snielson@discoverandchange.com>
2024-09-12 10:17:24 -0400

    Feat fixes #7658 signature page twigify (#7659)
    * Implement twig version of portal signature modal.
    
    Allow module writers to be able to reconfigure the portal signing modal
    by overriding the twig files.
    
    * Fix styles
    
    * Had bug in prior commit with twig files
    
    Renamed signer content twig file to be inner content in order to avoid
    confusion.  Fixed bug where js file had the twig includes and instead I
    needed the _signer_modal.html.twig file to be the main wrapper
    container.
    
    * Fix translation error
    
    (cherry picked from commit 0390adb8d07859048dfedd6211749fc421ab1485)

--- portal/sign/assets/signer_modal.php
+++ portal/sign/assets/signer_modal.php
@@ -33,65 +33,28 @@ if (empty($is_portal)) {
 
 require_once(__DIR__ . '/../../../interface/globals.php');
 
+use OpenEMR\Common\Logging\SystemLogger;
+use OpenEMR\Common\Twig\TwigContainer;
+
 $aud = "admin-signature";
 $cuser = attr($_SESSION['authUserID'] ?? "-patient-");
 $cpid = attr($_SESSION['pid'] ?? "0");
 $api_id = $_SESSION['api_csrf_token'] ?? ''; // portal doesn't do remote
 
-$msg1 = xlt('Use Current');
-$msg2 = xlt('Cancel');
-$msg3 = xlt('Is Authorizing Signature');
-$msg4 = xlt('Sign Above');
-$msg5 = xlt('Clear Canvas');
-$msg6 = xlt('Sign and Save');
-$msg7 = xlt('Signatory');
-$msg8 = xlt('Signatory did not sign or Signer was closed.');
-$msg9 = xlt('Waiting for Signature on Signer Pad');
-$msg10 = xlt('Please provide a signature first');
-$msg11 = xlt('Signer Pad Checked In and Available');
-$msg12 = xlt('Transaction Failed');
-// why tempt fate 13 a no no....
-$msg14 = xlt("A Remote Signing Device is not answering.") . "<br /><h4>" . xlt("Using this device until remote becomes available.") . "</h4>";
-$msg15 = xlt("Remote is Currently Busy");
-// module translations
-$vars = "<script>const msgSignator='" . $msg7 . "';const msgNoSign='" . $msg8 . "';const msgWaiting='" . $msg9 . "';const msgBusy='" . $msg15 . "';</script>\n";
-$vars .= "<script>const msgNeedSign='" . $msg10 . "';const msgCheckIn='" . $msg11 . "';const msgFail='" . $msg12 . "';const msgAnswering='" . $msg14 . "';</script>\n";
-$vars .= "<script>var apiToken=" . js_escape($api_id) . ";</script>\n";
-// override templates or source to ensure these are set correctly for signer api.
-// you'll always have two signatures, portal(not witnessed, that's coming) and clinic.
-if ($is_portal) {
-    $aud = "patient-signature";
-    $vars .= "<script>var isPortal=" . js_escape($is_portal) . ";var cuser=" . js_escape($cuser) . ";var cpid=" . js_escape($cpid) . ";</script>\n";
+$twigVars = [
+    'is_portal' => $is_portal
+    ,'cuser' => $cuser
+    ,'cpid' => $cpid
+    ,'aud' => $is_portal ? $aud = 'patient_signature' : $aud
+];
+$twigContainer = (new TwigContainer(null, $GLOBALS['kernel']))->getTwig();
+try {
+    $modal = $twigContainer->render("portal/partial/_signer_modal.html.twig", $twigVars);
+} catch (Exception $exception) {
+    (new SystemLogger())->errorLogCaller($exception->getMessage(), ['trace' => $exception->getTraceAsString()]);
+    // we want the json to fail
+    die(json_encode(['error' => 'Server died']));
 }
-// short & sweet dynamic modal
-$modal = <<<MODAL
-$vars
-<div id='openSignModal' class='modal fade' role='dialog' tabindex='-1'>
-    <div class='modal-dialog modal-xl'>
-        <div class='modal-content signature-pad'>
-            <div class='modal-body signature-pad-body'><span class='sigNav'><label style='display: none;'>
-                <input style='display:none;' type='checkbox' id='isAdmin' name='isAdmin' />$msg3</label></span>
-                <div class='row signature-pad-content'>
-                    <div class='embed-responsive embed-responsive-21by7 border border-dark'>
-                        <canvas class="canvas embed-responsive-item"></canvas>
-                    </div>
-                    <div class='signature-pad-footer text-dark'>
-                        <div class='description'>$msg4</div>
-                        <div class='btn-group signature-pad-actions bg-light'>
-                                <button type='button' class='btn btn-secondary btn-sm clear' data-action='clear'>$msg5</button>
-                                <button type='button' class='btn btn-secondary btn-sm' data-action='place' data-type='$aud' id='signatureModal'>$msg1</button>
-                                <button type='button' class='btn btn-danger btn-sm' data-dismiss='modal'>$msg2</button>
-                                <button type='button' class='btn btn-success btn-sm save' data-action='save_signature'>$msg6</button>
-                                <span><h6 id='labelName'></h6></span>
-                            <input type='hidden' id='name' /><input type='hidden' id='user' value='$cuser' /><input type='hidden' id='pid' value='$cpid' />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-MODAL;
 
 echo js_escape($modal);
 exit();
