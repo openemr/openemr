fdc6b89303903a2ca6d6b94df3868a426b4eddfb
Jerry Padgett <sjpadgett@gmail.com>
2025-02-08 12:39:13 -0500

    Refactor and improve Manage Duplicates (#7970)
    * Refactor and improve Manage Duplicates
    adjust data set weights
    
    * add function to recalculate scores on refresh and form entry.
    cleanup

--- interface/patient_file/manage_dup_patients.php
+++ interface/patient_file/manage_dup_patients.php
@@ -6,7 +6,9 @@
  * @package   OpenEMR
  * @link      http://www.open-emr.org
  * @author    Rod Roark <rod@sunsetsystems.com>
+ * @author    Jerry Padgett <sjpadgett@gmail.com>
  * @copyright Copyright (c) 2017-2021 Rod Roark <rod@sunsetsystems.com>
+ * @copyright Copyright (c) 2025 Jerry Padgett <sjpadgett@gmail.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
@@ -20,15 +22,16 @@ use OpenEMR\Common\Twig\TwigContainer;
 use OpenEMR\Core\Header;
 use OpenEMR\Services\FacilityService;
 
-$firsttime = true;
+$first_time = true;
 
-function displayRow($row, $pid = '')
+/**
+ * @param $row
+ * @param $pid
+ * @return void
+ */
+function displayRow($row, $pid = ''): void
 {
-    global $firsttime;
-
-    $bgcolor = '#ffdddd';
-    $myscore = '';
-    $options = '';
+    global $first_time;
 
     if (empty($pid)) {
         $pid = $row['pid'];
@@ -37,82 +40,126 @@ function displayRow($row, $pid = '')
     if (isset($row['myscore'])) {
         $myscore = $row['myscore'];
         $options = "<option value=''></option>" .
-        "<option value='MK'>" . xlt('Merge and Keep') . "</option>" .
-        "<option value='MD'>" . xlt('Merge and Discard') . "</option>";
+            "<option value='MK'>" . xlt('Merge and Keep') . "</option>" .
+            "<option value='MD'>" . xlt('Merge and Discard') . "</option>";
     } else {
         $myscore = $row['dupscore'];
         $options = "<option value=''></option>" .
-        "<option value='U'>" . xlt('Mark as Unique') . "</option>" .
-        "<option value='R'>" . xlt('Recompute Score') . "</option>";
-        if (!$firsttime) {
-            echo " <tr bgcolor='#dddddd'><td class='detail' colspan='12'>&nbsp;</td></tr>\n";
+            "<option value='U'>" . xlt('Mark as Unique') . "</option>" .
+            "<option value='R'>" . xlt('Recompute Score') . "</option>";
+        if (!$first_time) {
+            echo " <tr><td class='detail' colspan='12'>&nbsp;</td></tr>\n";
         }
     }
 
-    $firsttime = false;
+    $first_time = false;
     $ptname = $row['lname'] . ', ' . $row['fname'] . ' ' . $row['mname'];
     $phones = array();
     if (trim($row['phone_home'])) {
         $phones[] = trim($row['phone_home']);
     }
-    if (trim($row['phone_biz' ])) {
-        $phones[] = trim($row['phone_biz' ]);
+    if (trim($row['phone_biz'])) {
+        $phones[] = trim($row['phone_biz']);
     }
     if (trim($row['phone_cell'])) {
         $phones[] = trim($row['phone_cell']);
     }
     $phones = implode(', ', $phones);
-
-    $facname = '';
+    $fac_name = '';
     if ($row['home_facility']) {
         $facrow = getFacility($row['home_facility']);
         if (!empty($facrow['name'])) {
-            $facname = $facrow['name'];
+            $fac_name = $facrow['name'];
         }
     }
+    $highlight_text = $row['dupscore'] > 17 ? xlt('Merge From') : '';
+    $highlight_class = $row['dupscore'] > 17 ? 'highlight' : '';
+    if (!empty($row['myscore']) && $row['myscore'] > 17) {
+        $highlight_class = 'highlight-master';
+        $highlight_text = xlt('Merge To');
+    }
+    echo "<tr class='$highlight_class'>";
     ?>
- <tr bgcolor='<?php echo $bgcolor; ?>'>
-  <td class="detail" bgcolor="#dddddd">
-   <select onchange='selchange(this, <?php echo attr_js($pid); ?>, <?php echo attr_js($row['pid']); ?>)' style='width:100%'>
-    <?php echo $options; // this is html and already escaped as required ?>
-   </select>
-  </td>
-  <td class="detail" align="right">
-    <?php echo text($myscore); ?>
-  </td>
-  <td class="detail" align="right" onclick="openNewTopWindow(<?php echo attr_js($row['pid']); ?>)"
-    title="<?php echo xla('Click to open in a new window or tab'); ?>" style="color:blue;cursor:pointer">
-    <?php echo text($row['pid']); ?>
-  </td>
-  <td class="detail">
-    <?php echo text($row['pubpid']); ?>
-  </td>
-  <td class="detail">
-    <?php echo text($ptname); ?>
-  </td>
-  <td class="detail">
-    <?php echo text(oeFormatShortDate($row['DOB'])); ?>
-  </td>
-  <td class="detail">
-    <?php echo text($row['ss']); ?>
-  </td>
-  <td class="detail">
-    <?php echo text($row['email']); ?>
-  </td>
-  <td class="detail">
-    <?php echo text($phones); ?>
-  </td>
-  <td class="detail">
-    <?php echo text(oeFormatShortDate($row['regdate'])); ?>
-  </td>
-  <td class="detail">
-    <?php echo text($facname); ?>
-  </td>
-  <td class="detail">
-    <?php echo text($row['street']); ?>
-  </td>
- </tr>
+    <td>
+        <select onchange='selectChange(this, <?php echo attr_js($pid); ?>, <?php echo attr_js($row['pid']); ?>)' style='width:100%'>
+            <?php echo $options; // this is html and already escaped as required
+            ?>
+        </select>
+    </td>
+    <td>
+        <?php echo text($myscore); ?>
+    </td>
+    <td class="text-warning" onclick="openNewTopWindow(<?php echo attr_js($row['pid']); ?>)"
+        title="<?php echo xla('Click to open in a new window or tab'); ?>" style="cursor:pointer">
+        <?php echo text($row['pid']); ?>
+    </td>
+    <td>
+        <?php echo text($row['pubpid']); ?>
+    </td>
+    <td>
+        <?php echo $highlight_text; ?>
+    </td>
+    <td>
+        <?php echo text($ptname); ?>
+    </td>
+    <td>
+        <?php echo text(oeFormatShortDate($row['DOB'])); ?>
+    </td>
+    <td>
+        <?php echo text($row['sex']); ?>
+    </td>
+    <td>
+        <?php echo text($row['email']); ?>
+    </td>
+    <td>
+        <?php echo text($phones); ?>
+    </td>
+    <td>
+        <?php echo text(oeFormatShortDate($row['regdate'])); ?>
+    </td>
+    <td>
+        <?php echo text($row['street']); ?>
+    </td>
+    </tr>
     <?php
+}
+
+/**
+ * @return int
+ */
+function calculateScores(): int
+{
+    sqlStatementNoLog("UPDATE patient_data SET dupscore = -9 WHERE dupscore != -1");
+
+    $query_limit = 5000;
+    $endtime = time() + 365 * 24 * 60 * 60; // a year from now
+    $endtime = time() + 240 * 60;
+    $count = 0;
+    $finished = false;
+
+    while (!$finished && time() < $endtime) {
+        $scores = array();
+        $query = "SELECT p1.pid, MAX(" . getDupScoreSQL() . ") AS dupscore" .
+            " FROM patient_data AS p1, patient_data AS p2" .
+            " WHERE p1.dupscore = -9 AND p2.pid < p1.pid" .
+            " GROUP BY p1.pid ORDER BY p1.pid LIMIT " . escape_limit($query_limit);
+        $results = sqlStatementNoLog($query);
+        while ($row1 = sqlFetchArray($results)) {
+            $scores[$row1['pid']] = $row1['dupscore'];
+        }
+        foreach ($scores as $pid => $score) {
+            sqlStatementNoLog(
+                "UPDATE patient_data SET dupscore = ? WHERE pid = ?",
+                array($score, $pid)
+            );
+            ++$count;
+        }
+        if (count($scores) < $query_limit) {
+            $finished = true;
+        }
+    }
+
+    return $count;
 }
 
 if (!empty($_POST)) {
@@ -126,174 +173,154 @@ if (!AclMain::aclCheckCore('admin', 'super')) {
     exit;
 }
 
-$scorecalc = getDupScoreSQL();
+$calc_count = calculateScores();
+$score_calculate = getDupScoreSQL();
+
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-<title><?php echo xlt('Duplicate Patient Management') ?></title>
-
+    <title><?php echo xlt('Duplicate Patient Management') ?></title>
     <?php Header::setupHeader(['report-helper']); ?>
 
-<style type="text/css">
+    <style>
+      .table th, .table td {
+        text-align: center;
+        vertical-align: middle;
+      }
 
- .dehead { color:#000000; font-family:sans-serif; font-size:10pt; font-weight:bold }
- .detail { color:#000000; font-family:sans-serif; font-size:10pt; font-weight:normal }
- .delink { color:#0000cc; font-family:sans-serif; font-size:10pt; font-weight:normal; cursor:pointer }
+      .table tr.highlight {
+        background-color: #ffc10733 !important;
+      }
 
-table.mymaintable, table.mymaintable td {
- border: 1px solid #aaaaaa;
- border-collapse: collapse;
-}
-table.mymaintable td {
- padding: 1pt 4pt 1pt 4pt;
-}
+      .table tr.highlight-master {
+        background-color: #ff000733 !important;
+      }
+    </style>
+    <script>
+        $(function () {
+            // Enable fixed headers when scrolling the report.
+            if (window.oeFixedHeaderSetup) {
+                oeFixedHeaderSetup(document.getElementById('mymaintable'));
+            }
+        });
 
-</style>
+        function openNewTopWindow(pid) {
+            document.fnew.patientID.value = pid;
+            top.restoreSession();
+            document.fnew.submit();
+        }
 
-<script>
-
-$(function () {
-    // Enable fixed headers when scrolling the report.
-    if (window.oeFixedHeaderSetup) {
-        oeFixedHeaderSetup(document.getElementById('mymaintable'));
-    }
-});
-
-function openNewTopWindow(pid) {
- document.fnew.patientID.value = pid;
- top.restoreSession();
- document.fnew.submit();
-}
-
-function selchange(sel, toppid, rowpid) {
-  var f = document.forms[0];
-  if (sel.value == '') return;
-  top.restoreSession();
-  if (sel.value == 'MK') {
-    window.location = 'merge_patients.php?pid1=' + encodeURIComponent(rowpid) + '&pid2=' + encodeURIComponent(toppid);
-  }
-  else if (sel.value == 'MD') {
-    window.location = 'merge_patients.php?pid1=' + encodeURIComponent(toppid) + '&pid2=' + encodeURIComponent(rowpid);
-  }
-  else {
-    // Currently 'U' and 'R' actions are supported and rowpid is meaningless.
-    f.form_action.value = sel.value;
-    f.form_toppid.value = toppid;
-    f.form_rowpid.value = rowpid;
-    f.submit();
-  }
-}
-
-</script>
-
+        function selectChange(select, toppid, rowpid) {
+            let form = document.forms[0];
+            if (select.value == '') {
+                return;
+            }
+            top.restoreSession();
+            if (select.value == 'MK') {
+                window.location = 'merge_patients.php?pid1=' + encodeURIComponent(rowpid) + '&pid2=' + encodeURIComponent(toppid);
+            } else if (select.value == 'MD') {
+                window.location = 'merge_patients.php?pid1=' + encodeURIComponent(toppid) + '&pid2=' + encodeURIComponent(rowpid);
+            } else {
+                // Currently 'U' and 'R' actions are supported and rowpid is meaningless.
+                form.form_action.value = select.value;
+                form.form_toppid.value = toppid;
+                form.form_rowpid.value = rowpid;
+                form.submit();
+            }
+        }
+    </script>
 </head>
+<body class="container-fluid bg-light text-dark">
+    <div class="text-center mx-2">
+        <div class="text-center mt-1 w-100">
+            <h2><?php echo xlt('Duplicate Patient Management') ?></h2>
+        </div>
+        <form class="form" method='post' action='manage_dup_patients.php'>
+            <input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
+            <div class="btn-sm-group mb-1 text-center">
+                <button class="btn btn-sm btn-primary btn-refresh" type='submit' name='form_refresh' value="Refresh"><?php echo xla('ReCalculate Scores') ?></button>
+                <button class="btn btn-sm btn-primary btn-print" type='button' value='Print' onclick='window.print()'><?php echo xla('Print'); ?></button>
+            </div>
+            <table id='mymaintable' class='table table-sm table-bordered table-hover w-100 table-light'>
+                <thead class="thead-dark text-center">
+                <tr>
+                    <th>
+                        <?php echo xlt('Actions'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Score'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Pid'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Public'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Scope'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Name'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('DOB'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Gender'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Email'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Telephone'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Registered'); ?>
+                    </th>
+                    <th>
+                        <?php echo xlt('Address'); ?>
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="text-center">
+                <?php
+                $form_action = $_POST['form_action'] ?? '';
+                if ($form_action == 'U') {
+                    sqlStatement(
+                        "UPDATE patient_data SET dupscore = -1 WHERE pid = ?",
+                        array($_POST['form_toppid'])
+                    );
+                } elseif ($form_action == 'R') {
+                    updateDupScore($_POST['form_toppid']);
+                }
 
-<body style='margin: 2em; background-color: #dddddd' >
-<center>
-
-<h2><?php echo xlt('Duplicate Patient Management')?></h2>
-
-<form method='post' action='manage_dup_patients.php'>
-<input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
-
-<table border='0' cellpadding='3'>
- <tr>
-  <td align='center'>
-   <input type='submit' name='form_refresh' value="<?php echo xla('Refresh') ?>">
-   &nbsp;
-   <input type='button' value='<?php echo xla('Print'); ?>' onclick='window.print()' />
-  </td>
- </tr>
- <tr>
-  <td height="1">
-  </td>
- </tr>
-</table>
-
-<table id='mymaintable' class='mymaintable'>
- <thead>
-  <tr bgcolor="#dddddd">
-   <td class="dehead">
-    <?php echo xlt('Actions'); ?>
-   </td>
-   <td class="dehead" align="right">
-    <?php echo xlt('Score'); ?>
-   </td>
-   <td class="dehead" align="right">
-    <?php echo xlt('Pid'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('ID'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('Name'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('DOB'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('SSN'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('Email'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('Telephone'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('Registered'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('Home Facility'); ?>
-   </td>
-   <td class="dehead">
-    <?php echo xlt('Address'); ?>
-   </td>
-  </tr>
- </thead>
- <tbody>
-<?php
-
-$form_action = $_POST['form_action'] ?? '';
-
-if ($form_action == 'U') {
-    sqlStatement(
-        "UPDATE patient_data SET dupscore = -1 WHERE pid = ?",
-        array($_POST['form_toppid'])
-    );
-} else if ($form_action == 'R') {
-    updateDupScore($_POST['form_toppid']);
-}
-
-$query = "SELECT * FROM patient_data WHERE dupscore > 7 " .
-    "ORDER BY dupscore DESC, pid DESC LIMIT 100";
-$res1 = sqlStatement($query);
-while ($row1 = sqlFetchArray($res1)) {
-    displayRow($row1);
-    $query = "SELECT p2.*, ($scorecalc) AS myscore " .
-    "FROM patient_data AS p1, patient_data AS p2 WHERE " .
-    "p1.pid = ? AND p2.pid < p1.pid AND ($scorecalc) > 7 " .
-    "ORDER BY myscore DESC, p2.pid DESC";
-    $res2 = sqlStatement($query, array($row1['pid']));
-    while ($row2 = sqlFetchArray($res2)) {
-        displayRow($row2, $row1['pid']);
-    }
-}
-?>
-</tbody>
-</table>
-<input type='hidden' name='form_action' value='' />
-<input type='hidden' name='form_toppid' value='0' />
-<input type='hidden' name='form_rowpid' value='0' />
-</form>
-</center>
-
-<!-- form used to open a new top level window when a patient row is clicked -->
-<form name='fnew' method='post' target='_blank'
- action='../main/main_screen.php?auth=login&site=<?php echo attr_url($_SESSION['site_id']); ?>'>
-<input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
-<input type='hidden' name='patientID' value='0' />
-</form>
-
+                $query = "SELECT * FROM patient_data WHERE dupscore > 12 " . "ORDER BY dupscore DESC, pid DESC LIMIT 100";
+                $res1 = sqlStatement($query);
+                while ($row1 = sqlFetchArray($res1)) {
+                    displayRow($row1);
+                    $query = "SELECT p2.*, ($score_calculate) AS myscore " .
+                        "FROM patient_data AS p1, patient_data AS p2 WHERE " .
+                        "p1.pid = ? AND p2.pid < p1.pid AND ($score_calculate) > 12 " .
+                        "ORDER BY myscore DESC, p2.pid DESC";
+                    $res2 = sqlStatement($query, array($row1['pid']));
+                    while ($row2 = sqlFetchArray($res2)) {
+                        displayRow($row2, $row1['pid']);
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+            <input type='hidden' name='form_action' value='' />
+            <input type='hidden' name='form_toppid' value='0' />
+            <input type='hidden' name='form_rowpid' value='0' />
+        </form>
+    </div>
+    <!-- form used to open a new top level window when a patient row is clicked -->
+    <form name='fnew' method='post' target='_blank'
+        action='../main/main_screen.php?auth=login&site=<?php echo attr_url($_SESSION['site_id']); ?>'>
+        <input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
+        <input type='hidden' name='patientID' value='0' />
+    </form>
 </body>
 </html>
