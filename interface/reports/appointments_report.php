a6dcab581885c4fcc77c4935b70f7cbf4945ca60
ruth <ruth@muswell.me.uk>
2025-02-11 11:01:21 +0000

    start to code csv header row

--- interface/reports/appointments_report.php
+++ interface/reports/appointments_report.php
@@ -464,8 +464,19 @@ if (!empty($_POST['form_refresh']) || !empty($_POST['form_orderby'])) {
     $appointments = sortAppointments($appointments, $form_orderby);
     if (!empty($_POST['form_csvexport'])) {
         // include provider as well
-          $fields = ['ufname','ulname','pc_eventDate', 'pc_startTime', 'fname', 'lname', 'DOB'];
-           $spreadsheet = new SpreadSheetService($appointments, $fields, 'appts');
+        //  $fields = ['ufname','ulname','pc_eventDate', 'pc_startTime', 'fname', 'lname', 'DOB'];
+        // RM generate csv file with same column headers row as used in the report itself
+           $fields = ['Provider','Date', 'Time', 'Patient', 'DOB'];
+
+              for ($i = 0; $i < count($appointments); ++$i) {
+              $appointments[$i]["Provider"] = $appointments[$i]["ulname"] . ',' . $appointments[$i]["ufname"] . ' ' .  $appointments[$i]["umname"] ;
+              $csvfields[$i]["Provider"] = $appointments[$i]["Provider"] ;
+              $csvfields[$i]["Date"] = $appointments[$i]["pc_eventDate"] ;
+              $csvfields[$i]["Time"] = $appointments[$i]["pc_startTime"] ;
+              $csvfields[$i]["Patient"] = $appointments[$i]["fname"] . " " .  $appointments[$i]["lname"] ;
+              $csvfields[$i]["DOB"] = $appointments[$i]["DOB"] ;
+           }
+           $spreadsheet = new SpreadSheetService($csvfields, $fields, 'appts');
         if (!empty($spreadsheet->buildSpreadsheet())) {
             $spreadsheet->downloadSpreadsheet('Csv');
         }
