b203b35507a34bdfe59d60ae4d043f3188657d23
stephen waite <stephen.waite@cmsvt.com>
2025-02-11 20:36:36 -0500

    fix: compare integers

--- interface/billing/billing_report.php
+++ interface/billing/billing_report.php
@@ -12,7 +12,7 @@
  * @author    Stephen Waite <stephen.waite@cmsvt.com>
  * @copyright Copyright (c) 2016 Terry Hill <terry@lillysystems.com>
  * @copyright Copyright (c) 2017-2020 Brady Miller <brady.g.miller@gmail.com>
- * @copyright Copyright (c) 2018-2020 Jerry Padgett <sjpadgett@gmail.com>
+ * @copyright Copyright (c) 2018-2025 Jerry Padgett <sjpadgett@gmail.com>
  * @copyright Copyright (c) 2019-2020 Sherwin Gaddis <sherwingaddis@gmail.com>
  * @copyright Copyright (c) 2021-2025 Stephen Waite <stephen.waite@cmsvt.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
@@ -352,9 +352,9 @@ $partners = $x->_utility_array($x->x12_partner_factory());
             pid = parseInt(pid, 10);
             enc = parseInt(enc, 10);
             // Restore the session (getSessionValue calls restoreSession()) and load the encounter
-            let curpid = await top.getSessionValue('pid');
+            let curpid = parseInt(await top.getSessionValue('pid'));
             try {
-                if (curpid != pid) { // set patient
+                if (curpid !== pid) { // set patient
                     if (curpid != 0) {
                         top.clearPatient(false);
                     }
