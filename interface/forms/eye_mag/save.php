c4e8c3a428f58858f582481922975d881224973c
ophthal <60580695+ophthal@users.noreply.github.com>
2025-03-09 21:30:20 -0400

    Update save.php

--- interface/forms/eye_mag/save.php
+++ interface/forms/eye_mag/save.php
@@ -893,8 +893,8 @@ if (($_REQUEST["mode"]  ?? '') == "new") {
         $_POST['DIL_RISKS'] = '0';
     }
 
-    if ($_POST['DIL_MEDS']) {
-        $_POST['IOPPOSTTIME'] =  $_POST['DIL_MEDS'];
+    if ($_POST['DIL_MEDS']) { 
+        $_POST['IOPPOSTTIME'] =  date('H:i:s', strtotime($_POST['DIL_MEDS']));
     }
 
     if (!($_POST['ATROPINE'] ?? '')) {
