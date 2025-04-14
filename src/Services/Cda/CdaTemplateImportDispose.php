59802d6abd7bf1ed95230b2d3686310c0b20c74a
Jerry Padgett <sjpadgett@gmail.com>
2025-04-14 10:29:48 -0400

    CCDA Fixes (#8224)
    * CCDA Fixes
    remove all forms of br in xml
    
    * ci
    
    * minor fix to Advance Directive URL query string that caused a mangled query for the directive document fetch.
    
    * Pass userAuth as options for command classes so session can be set to allow ccda document security
    commands were processing methods twice! i.e.   $GLOBALS['modules_application']->getServiceManager()->build(CarecoordinationTable::class)->insert_patient($input->getOption('am_id'), $input->getOption('document_id'));
    Some refactor of import_ccda.php
    Developed a webpage as wrapper for command line tool. Useful to run tool or use it to create the command line to copy/paste.
    Big problem with web page is for hundreds of imports there isn't a way to break out of the run. Command line can be with cntrl-c
    When run in audit mode the imports slows by generally additional 50% otherwise.
    
    * fix some verbiage
    
    * switch to proc_open() to manage cmd exec.
    disable Start Import button while processing import session.
    add a reload button.
    
    * move unlink log to be in globals scope
    
    * rename ProgreeNoteParser class to better explain purpose
    
    * refactor to track encounter ids in memory for lookup/reference by other components.
    fix clinical notes being set to correct encounter.
    
    * tell notes insert what encounter
    
    * remove webpage
    address Brady's concerns for command line uitility
    
    * Parse encounter translation code if primary code is missing.
    redo find closest encounter for better prediction of inclusion items.
    
    * expand the clinical note parser to trap narrative text not in table format as eMD presents.

--- src/Services/Cda/CdaTemplateImportDispose.php
+++ src/Services/Cda/CdaTemplateImportDispose.php
@@ -34,6 +34,7 @@ class CdaTemplateImportDispose
     protected $codeService;
     protected $userauthorized;
     private int $currentEncounter;
+    private array $encounterList = [];
 
     public function __construct()
     {
@@ -333,10 +334,12 @@ class CdaTemplateImportDispose
         }
         foreach ($clinical_note_array as $key => $value) {
             $plan_date_value = $value['date'] ? date("Y-m-d", $this->str_to_time($value['date'])) : null;
-            // encounters already created or should be created
+            // encounters already created or should have been created
+            // all newly created encounters get stored to a static array
+            $encounter_for_forms = $this->encounterList[$value['encounter_extension']] ?? null;
             if (empty($encounter_for_forms)) {
                 $encounter_for_forms = $this->findClosestEncounterWithForm(trim($plan_date_value), $pid, 'form_clinical_notes');
-                if (empty($encounter_for_forms && empty($this->currentEncounter))) {
+                if (empty($encounter_for_forms && empty($static_encounter))) {
                     $query_sel_enc = "SELECT encounter FROM form_encounter WHERE date=? AND pid=?";
                     $res_query_sel_enc = $appTable->zQuery($query_sel_enc, array(date('Y-m-d H:i:s'), $pid));
                     if ($res_query_sel_enc->count() == 0) {
@@ -344,10 +347,7 @@ class CdaTemplateImportDispose
                         $res_enc_cur = $res_enc->current();
                         $encounter_for_forms = $res_enc_cur['encounter'];
                     } else {
-                        $encounter_for_forms = $this->currentEncounter;
-                        /*foreach ($res_query_sel_enc as $value2) {
-                            $encounter_for_forms = $value2['encounter'];
-                        }*/
+                        $encounter_for_forms = $encounter_for_forms;
                     }
                 }
             }
@@ -680,6 +680,9 @@ class CdaTemplateImportDispose
                 $catname = substr($catname, 0, 92);
                 if (empty($reason)) {
                     $reason = trim($cat[1] ?? '');
+                    if (empty($reason)) {
+                        $reason = $catname;
+                    }
                 }
                 $pc_catid = sqlQuery("SELECT pc_catid FROM `openemr_postcalendar_categories` Where `pc_catname` = ?", array($catname))['pc_catid'] ?? '';
             }
@@ -807,6 +810,8 @@ class CdaTemplateImportDispose
                     }
                 }
             }
+
+            $this->encounterList[$value["extension"]] = $encounter_id;
             //to external_encounters
             $insertEX = "INSERT INTO external_encounters(ee_date,ee_pid,ee_provider_id,ee_facility_id,ee_encounter_diagnosis,ee_external_id) VALUES (?,?,?,?,?,?)";
             $appTable->zQuery($insertEX, array($encounter_date_value, $pid, $provider_id, $facility_id, ($value['encounter_diagnosis_issue'] ?? null), ($value['extension'] ?? null)));
@@ -2231,24 +2236,71 @@ class CdaTemplateImportDispose
     }
 
     /**
-     * @param $item_date
-     * @param $item_pid
-     * @return int
+     * Find the closest encounter (by date) for a given item date and patient ID.
+     * The original code was refactored by ChatGPT to improve readability and maintainability.
+     *  The refactor request was to expand the encounter search to separate both before and after the item date.
+     * @param string $item_date The form item's date (any format parsable by strtotime).
+     * @param int    $item_pid  The patient ID.
+     * @return int              The encounter id (or 0 if none found).
      */
     public function findClosestEncounter($item_date, $item_pid): int
     {
-        $item_date = !empty($item_date) ? date('Y-m-d 23:59:59', strtotime($item_date)) : null;
-        $sql = "SELECT
-            fe.encounter,
-            fe.date,
-            fe.pid
-            FROM `form_encounter` AS fe
-            WHERE fe.date <= ? AND fe.pid = ?
-            ORDER BY fe.encounter DESC, fe.date DESC Limit 1";
-        $rtn = sqlQuery($sql, array($item_date, $item_pid));
+        if (empty($item_date)) {
+            return 0;
+        }
 
-        return (int)($rtn['encounter'] ?? '') ?: 0;
+        $item_ts = strtotime($item_date);
+        if ($item_ts === false) {
+            return 0;
+        }
+
+        $item_date_end = date('Y-m-d 23:59:59', $item_ts);
+        $item_date_start = date('Y-m-d 00:00:00', $item_ts);
+
+        $sqlBefore = "SELECT encounter, date
+                  FROM form_encounter
+                  WHERE date <= ? AND pid = ?
+                  ORDER BY date DESC
+                  LIMIT 1";
+        $resultBefore = sqlQuery($sqlBefore, array($item_date_end, $item_pid));
+
+        $sqlAfter = "SELECT encounter, date
+                 FROM form_encounter
+                 WHERE date >= ? AND pid = ?
+                 ORDER BY date ASC
+                 LIMIT 1";
+        $resultAfter = sqlQuery($sqlAfter, array($item_date_start, $item_pid));
+
+        $encounterBefore = null;
+        $encounterAfter = null;
+        $diffBefore = PHP_INT_MAX;
+        $diffAfter = PHP_INT_MAX;
+
+        if ($resultBefore && !empty($resultBefore['date'])) {
+            $tsBefore = strtotime($resultBefore['date']);
+            $diffBefore = abs($item_ts - $tsBefore);
+            $encounterBefore = (int)$resultBefore['encounter'];
+        }
+
+        if ($resultAfter && !empty($resultAfter['date'])) {
+            $tsAfter = strtotime($resultAfter['date']);
+            $diffAfter = abs($tsAfter - $item_ts);
+            $encounterAfter = (int)$resultAfter['encounter'];
+        }
+
+        // If only one side is available, return that one.
+        if ($encounterBefore === null && $encounterAfter !== null) {
+            return $encounterAfter;
+        } elseif ($encounterAfter === null && $encounterBefore !== null) {
+            return $encounterBefore;
+        } elseif ($encounterBefore !== null && $encounterAfter !== null) {
+            // Return the one with the smaller difference.
+            return ($diffBefore <= $diffAfter) ? $encounterBefore : $encounterAfter;
+        }
+
+        return 0;
     }
+
 
     /**
      * @param $item_date
