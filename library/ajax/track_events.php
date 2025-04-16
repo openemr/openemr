a94bf86c4933e969fadd723a56482bbd21a5a408
Jerry Padgett <sjpadgett@gmail.com>
2025-04-16 19:48:44 -0400

    syntax styling

--- library/ajax/track_events.php
+++ library/ajax/track_events.php
@@ -10,7 +10,6 @@
  * @license        https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-
 require_once("../../interface/globals.php");
 
 use OpenEMR\Common\Csrf\CsrfUtils;
