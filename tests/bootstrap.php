55f1abbbc52119a14d36429f13670aba2ce4ae00
Jerry Padgett <sjpadgett@gmail.com>
2025-04-20 19:33:40 -0400

    set environment doesn't work

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,3 @@ if (php_sapi_name() !== 'cli') {
 $_GET['site'] = 'default';
 $ignoreAuth = true;
 require_once(__DIR__ . "/../interface/globals.php");
-
-if (!defined('APP_UNDER_TEST')) {
-    define('APP_UNDER_TEST', true);
-}
