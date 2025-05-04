ae994e5e8483ef6ac3dd7466acc0c8c7597fb032
Jerry Padgett <sjpadgett@gmail.com>
2025-05-04 02:46:17 -0400

    Telemetry portal tracking events (#8328)
    * Telemetry portal tracking events
    add portal dashboard card click events for tracking
    fix allowTelemetry logic
    
    * style fix
    
    * Added new recordApiTrackEvent to dispatch for successfull requests.
    
    * refactor service to not handle calls as http request!
    Should fix tests
    
    * for API type events, instead of tracking by number of times an endpoint is used we insert all to track scope differences.
    
    * move scopes to label to test for unique on save.

---
+++ portal/lib/track_portal_events.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * track_portal_events.php
+ *
+ * @package        OpenEMR
+ * @link           https://www.open-emr.org
+ * @author         Jerry Padgett <sjpadgett@gmail.com>
+ * @copyright      Copyright (c) 2025 <sjpadgett@gmail.com>
+ * @license        https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+require_once(__DIR__ . '/../../src/Common/Session/SessionUtil.php');
+OpenEMR\Common\Session\SessionUtil::portalSessionStart();
+
+$sessionAllowWrite = true;
+if (isset($_SESSION['pid']) && isset($_SESSION['patient_portal_onsite_two'])) {
+    $pid = $_SESSION['pid'];
+    $ignoreAuth_onsite_portal = true;
+    require_once(__DIR__ . '/../../interface/globals.php');
+} else {
+    OpenEMR\Common\Session\SessionUtil::portalSessionCookieDestroy();
+    $ignoreAuth = false;
+    require_once(__DIR__ . '/../../interface/globals.php');
+    if (!isset($_SESSION['authUserID'])) {
+        $landingpage = 'index.php';
+        header('Location: ' . $landingpage);
+        exit;
+    }
+}
+
+
+use OpenEMR\Common\Csrf\CsrfUtils;
+use OpenEMR\Services\VersionService;
+use OpenEMR\Telemetry\TelemetryRepository;
+use OpenEMR\Telemetry\TelemetryService;
+
+header("Content-Type: application/json");
+
+/**
+ * Main request handler that reads input, verifies the CSRF token, and delegates
+ * to the appropriate telemetry service method.
+ */
+function handleRequest(): void
+{
+    // Read JSON payload.
+    $input_json = file_get_contents('php://input');
+    $data = json_decode($input_json, true);
+
+    // Verify CSRF token.
+    if (!isset($data["csrf_token_form"]) || !CsrfUtils::verifyCsrfToken($data["csrf_token_form"])) {
+        CsrfUtils::csrfNotVerified();
+    }
+
+    $telemetryRepo = new TelemetryRepository();
+    $versionService = new VersionService();
+    $telemetryService = new TelemetryService($telemetryRepo, $versionService);
+
+    $action = $data['action'] ?? '';
+    switch ($action) {
+        case 'portalCardClickData':
+            $telemetryService->reportClickEvent($data);
+            break;
+        case 'allUsageData':
+            $result = $telemetryService->reportUsageData();
+            echo json_encode($result);
+            break;
+        default:
+            http_response_code(400);
+            echo json_encode(["error" => "Invalid action"]);
+            break;
+    }
+}
+
+handleRequest();
