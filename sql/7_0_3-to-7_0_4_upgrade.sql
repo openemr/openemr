08ed79b7bc8f60f7dec52fa9fb9d93240a5c2a39
Jerry Padgett <sjpadgett@gmail.com>
2025-05-14 10:05:45 -0400

    Fix sql error track events add additional col event_url to unique key. key is now event_label, event_url

--- sql/7_0_3-to-7_0_4_upgrade.sql
+++ sql/7_0_3-to-7_0_4_upgrade.sql
@@ -136,7 +136,7 @@ CREATE TABLE `track_events` (
     `last_event`     DATETIME NULL,
     `label_count`    INT UNSIGNED NOT NULL DEFAULT 1,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `unique_event_label` (`event_label`)
+    UNIQUE KEY `unique_event_label` (`event_label`, `event_url`(255))
 ) ENGINE = InnoDB COMMENT = 'Telemetry Event Data';
 #EndIf
 
@@ -158,4 +158,8 @@ ALTER TABLE `product_registration` ADD `last_ask_version` TINYTEXT;
 
 #IfMissingColumn product_registration options
 ALTER TABLE `product_registration` ADD `options` TEXT COMMENT 'JSON array of scope options';
+#EndIf
+
+#IfTable track_events
+ALTER TABLE `track_events` DROP INDEX `unique_event_label`, ADD UNIQUE `unique_event_label` (`event_label`, `event_url`(255));
 #EndIf
