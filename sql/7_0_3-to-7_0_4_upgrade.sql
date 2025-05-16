d13c254d8f07f53208a2124801b34aa7e46d5779
Jerry Padgett <sjpadgett@gmail.com>
2025-05-16 15:01:15 -0400

    Reindex, Remove scopes, Add encounter forms (#8366)
    * Reindex Remove scopes Add encounter forms
    add user role in place of scopes for api tracking
    
    * Add encounter form click events to tracking
    
    * adjust indexes

--- sql/7_0_3-to-7_0_4_upgrade.sql
+++ sql/7_0_3-to-7_0_4_upgrade.sql
@@ -136,7 +136,7 @@ CREATE TABLE `track_events` (
     `last_event`     DATETIME NULL,
     `label_count`    INT UNSIGNED NOT NULL DEFAULT 1,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `unique_event_label_url` (`event_label`, `event_url`(255))
+    UNIQUE KEY `unique_event_label_target` (`event_label`, `event_url`(255), `event_target`(255))
 ) ENGINE = InnoDB COMMENT = 'Telemetry Event Data';
 #EndIf
 
@@ -160,6 +160,14 @@ ALTER TABLE `product_registration` ADD `last_ask_version` TINYTEXT;
 ALTER TABLE `product_registration` ADD `options` TEXT COMMENT 'JSON array of scope options';
 #EndIf
 
-#IfNotIndex track_events unique_event_label_url
-ALTER TABLE `track_events` DROP INDEX `unique_event_label`, ADD UNIQUE `unique_event_label_url` (`event_label`, `event_url`(255));
+#IfIndex track_events unique_event_label
+ALTER TABLE `track_events` DROP INDEX `unique_event_label`;
+#EndIf
+
+#IfIndex track_events unique_event_label_url
+ALTER TABLE `track_events` DROP INDEX `unique_event_label_url`;
+#EndIf
+
+#IfNotIndex track_events unique_event_label_target
+ALTER TABLE `track_events` ADD UNIQUE `unique_event_label_target` (`event_label`, `event_url`(255), `event_target`(255));
 #EndIf
