b9530368e80a76a4eee2a2349b71f56dc94b81f3
Michael A. Smith <michael@smith-li.com>
2025-06-24 17:16:51 -0400

    fix(shell): address SC2006 (#8496)
    Reduces the error count by 30.

--- contrib/util/backup_oemr.sh
+++ contrib/util/backup_oemr.sh
@@ -15,7 +15,7 @@ newerthan='2005-01-01'
 #re-edit this file and change the date to the last succesfull cd copy
 
 # a name for the file that is unique of that day
-file=`date +%F`
+file=$(date +%F)
 
 #get the content from the mysql database
 mysqldump -u ${dbuser} -p${dbpass} --databases ${db} > ${workdir}${file}.sql.bak
@@ -34,7 +34,7 @@ bzip2 ${workdir}${file}postgres.bak
 tar -N ${newerthan} -jcf ${workdir}${file}openemr.tar.bzip2 ${installdir}
 
 # create a disk image
-label=`date +%y%m%d`
+label=$(date +%y%m%d)
 mkisofs -J -R -V ${label} -o ${tempdir}image.iso ${workdir}
 
 #burn it
