8bdf1d8eef37741565ef269630dc67b9be69b4ac
Michael A. Smith <michael@smith-li.com>
2025-07-14 05:28:33 -0400

    refactor(rector): set rector level to 2

--- rector.php
+++ rector.php
@@ -31,9 +31,9 @@ return RectorConfig::configure()
     // uncomment to reach your current PHP version
     // ->withPhpSets()
     ->withPhpVersion(PhpVersion::PHP_81)
-    ->withTypeCoverageLevel(1)
-    ->withDeadCodeLevel(1)
-    ->withCodeQualityLevel(1)
+    ->withTypeCoverageLevel(2)
+    ->withDeadCodeLevel(2)
+    ->withCodeQualityLevel(2)
     ->withCache(
         // ensure file system caching is used instead of in-memory
         cacheClass: FileCacheStorage::class,
