c9fd6150f25b610a7e877a66de3fe547d6fce3df
Michael A. Smith <michael@smith-li.com>
2025-08-02 06:50:32 -0400

    fix(print_daysheet): redundant assignment (#8717)

--- interface/billing/print_daysheet_report_num2.php
+++ interface/billing/print_daysheet_report_num2.php
@@ -197,7 +197,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
     foreach ($all4 as $iter) {
         // Case statment to tally information by user
         switch ($iter['user']) {
-            case $iter['user'] = $final_list[0]:
+            case $final_list[0]:
                 $us0_user = $iter['user'];
                 $us0_fee += $iter['fee'];
                 $us0_inspay += $iter['ins_code'];
@@ -205,7 +205,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us0_patadj += $iter['pat_adjust_dollar'];
                 $us0_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[1]:
+            case $final_list[1]:
                 $us1_user = $iter['user'];
                 $us1_fee += $iter['fee'];
                 $us1_inspay += $iter['ins_code'];
@@ -213,7 +213,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us1_patadj += $iter['pat_adjust_dollar'];
                 $us1_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[2]:
+            case $final_list[2]:
                 $us2_user = $iter['user'];
                 $us2_fee += $iter['fee'];
                 $us2_inspay += $iter['ins_code'];
@@ -221,7 +221,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us2_patadj += $iter['pat_adjust_dollar'];
                 $us2_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[3]:
+            case $final_list[3]:
                 $us3_user = $iter['user'];
                 $us3_fee += $iter['fee'];
                 $us3_inspay += $iter['ins_code'];
@@ -229,7 +229,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us3_patadj += $iter['pat_adjust_dollar'];
                 $us3_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[4]:
+            case $final_list[4]:
                 $us4_user = $iter['user'];
                 $us4_fee += $iter['fee'];
                 $us4_inspay += $iter['ins_code'];
@@ -237,7 +237,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us4_patadj += $iter['pat_adjust_dollar'];
                 $us4_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[5]:
+            case $final_list[5]:
                 $us5_user = $iter['user'];
                 $us5_fee += $iter['fee'];
                 $us5_inspay += $iter['ins_code'];
@@ -245,7 +245,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us5_patadj += $iter['pat_adjust_dollar'];
                 $us5_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[6]:
+            case $final_list[6]:
                 $us6_user = $iter['user'];
                 $us6_fee += $iter['fee'];
                 $us6_inspay += $iter['ins_code'];
@@ -253,7 +253,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us6_patadj += $iter['pat_adjust_dollar'];
                 $us6_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[7]:
+            case $final_list[7]:
                 $us7_user = $iter['user'];
                 $us7_fee += $iter['fee'];
                 $us7_inspay += $iter['ins_code'];
@@ -261,7 +261,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us7_patadj += $iter['pat_adjust_dollar'];
                 $us7_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[8]:
+            case $final_list[8]:
                 $us8_user = $iter['user'];
                 $us8_fee += $iter['fee'];
                 $us8_inspay += $iter['ins_code'];
@@ -269,7 +269,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us8_patadj += $iter['pat_adjust_dollar'];
                 $us8_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[9]:
+            case $final_list[9]:
                 $us9_user = $iter['user'];
                 $us9_fee += $iter['fee'];
                 $us9_inspay += $iter['ins_code'];
@@ -277,7 +277,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us9_patadj += $iter['pat_adjust_dollar'];
                 $us9_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[10]:
+            case $final_list[10]:
                 $us10_user = $iter['user'];
                 $us10_fee += $iter['fee'];
                 $us10_inspay += $iter['ins_code'];
@@ -285,7 +285,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us10_patadj += $iter['pat_adjust_dollar'];
                 $us10_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[11]:
+            case $final_list[11]:
                 $us11_user = $iter['user'];
                 $us11_fee += $iter['fee'];
                 $us11_inspay += $iter['ins_code'];
@@ -293,7 +293,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us11_patadj += $iter['pat_adjust_dollar'];
                 $us11_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[12]:
+            case $final_list[12]:
                 $us12_user = $iter['user'];
                 $us12_fee += $iter['fee'];
                 $us12_inspay += $iter['ins_code'];
@@ -301,7 +301,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us12_patadj += $iter['pat_adjust_dollar'];
                 $us12_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[13]:
+            case $final_list[13]:
                 $us13_user = $iter['user'];
                 $us13_fee += $iter['fee'];
                 $us13_inspay += $iter['ins_code'];
@@ -309,7 +309,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us13_patadj += $iter['pat_adjust_dollar'];
                 $us13_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[14]:
+            case $final_list[14]:
                 $us14_user = $iter['user'];
                 $us14_fee += $iter['fee'];
                 $us14_inspay += $iter['ins_code'];
@@ -317,7 +317,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us14_patadj += $iter['pat_adjust_dollar'];
                 $us14_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[15]:
+            case $final_list[15]:
                 $us15_user = $iter['user'];
                 $us15_fee += $iter['fee'];
                 $us15_inspay += $iter['ins_code'];
@@ -325,7 +325,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us15_patadj += $iter['pat_adjust_dollar'];
                 $us15_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[16]:
+            case $final_list[16]:
                 $us16_user = $iter['user'];
                 $us16_fee += $iter['fee'];
                 $us16_inspay += $iter['ins_code'];
@@ -333,7 +333,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us16_patadj += $iter['pat_adjust_dollar'];
                 $us16_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[17]:
+            case $final_list[17]:
                 $us17_user = $iter['user'];
                 $us17_fee += $iter['fee'];
                 $us17_inspay += $iter['ins_code'];
@@ -341,7 +341,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us17_patadj += $iter['pat_adjust_dollar'];
                 $us17_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[18]:
+            case $final_list[18]:
                 $us18_user = $iter['user'];
                 $us18_fee += $iter['fee'];
                 $us18_inspay += $iter['ins_code'];
@@ -349,7 +349,7 @@ if ($ret = getBillsBetweendayReport($code_type)) {
                 $us18_patadj += $iter['pat_adjust_dollar'];
                 $us18_patpay += $iter['pat_code'];
                 break;
-            case $iter['user'] = $final_list[19]:
+            case $final_list[19]:
                 $us19_user = $iter['user'];
                 $us19_fee += $iter['fee'];
                 $us19_inspay += $iter['ins_code'];
