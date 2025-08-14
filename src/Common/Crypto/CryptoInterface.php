1bc1087f38d6135f623bca25cf8c0831737c5955
Michael A. Smith <michael@smith-li.com>
2025-08-14 08:44:09 -0400

    chore(copyright): fix OpenCoreEMR headers

--- src/Common/Crypto/CryptoInterface.php
+++ src/Common/Crypto/CryptoInterface.php
@@ -6,7 +6,7 @@
  * @package   OpenEMR
  * @link      https://www.open-emr.org
  * @author    Michael A. Smith <michael@opencoreemr.com>
- * @copyright Copyright (c) 2025 OpenCoreEMR
+ * @copyright Copyright (c) 2025 OpenCoreEMR Inc.
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
--- tests/Tests/Common/Logging/EventAuditLoggerBreakglassTest.php
+++ tests/Tests/Common/Logging/EventAuditLoggerBreakglassTest.php
@@ -5,7 +5,8 @@
  *
  * @package   OpenEMR\Tests\Common\Logging
  * @link      http://www.open-emr.org
- * @copyright Copyright (c) 2025 OpenCoreEMR
+ * @author    Michael A. Smith <michael@opencoreemr.com>
+ * @copyright Copyright (c) 2025 OpenCoreEMR Inc.
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
