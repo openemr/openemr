93701997d519df28a3b4d290ed05c76fff77595c
Michael A. Smith <michael@opencoreemr.com>
2025-10-01 22:15:36 -0400

    chore(rector): set ReturnNeverTypeRector

--- rector.php
+++ rector.php
@@ -40,6 +40,7 @@ use Rector\Php80\Rector\NotIdentical\StrContainsRector;
 use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Php82\Rector\FuncCall\Utf8DecodeEncodeToMbConvertEncodingRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
@@ -102,6 +103,7 @@ return RectorConfig::configure()
         RandomFunctionRector::class, // one of the withPhpSets rules
         ReadOnlyPropertyRector::class, // one of the withPhpSets rules
         ReplaceHttpServerVarsByServerRector::class, // one of the withPhpSets rules
+        ReturnNeverTypeRector::class, // one of the withPhpSets rules
         SetCookieRector::class, // one of the withPhpSets rules
         StrContainsRector::class, // one of the withPhpSets rules
         StrEndsWithRector::class, // one of the withPhpSets rules
