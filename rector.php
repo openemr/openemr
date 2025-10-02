f0c87296f01267c272e7670e1caf099528b9e82f
Michael A. Smith <michael@opencoreemr.com>
2025-10-01 22:15:29 -0400

    chore(rector): set NullToStrictStringFuncCallArgRector

--- rector.php
+++ rector.php
@@ -9,8 +9,9 @@ use Rector\Config\RectorConfig;
 use Rector\Php55\Rector\Class_\ClassConstantToSelfClassRector;
 use Rector\Php71\Rector\Assign\AssignArrayToStringRector;
 use Rector\Php73\Rector\FuncCall\ArrayKeyFirstLastRector;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\Php74\Rector\FuncCall\ArrayKeyExistsOnPropertyRector;
+use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
@@ -51,6 +52,7 @@ return RectorConfig::configure()
         AssignArrayToStringRector::class, // one of the withPhpSets rules
         ChangeSwitchToMatchRector::class, // one of the withPhpSets rules
         ClassConstantToSelfClassRector::class, // one of the withPhpSets rules
+        NullToStrictStringFuncCallArgRector::class, // one of the withPhpSets rules
     ])
     ->withSkip([
         __DIR__ . '/sites/default/documents/smarty'
