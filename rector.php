75145b7d16d52f99663eb9f8c097fe5ab6630372
Michael A. Smith <michael@opencoreemr.com>
2025-10-01 22:15:26 -0400

    chore(rector): set ListToArrayDestructRector

--- rector.php
+++ rector.php
@@ -8,9 +8,10 @@ use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
 use Rector\Php55\Rector\Class_\ClassConstantToSelfClassRector;
 use Rector\Php71\Rector\Assign\AssignArrayToStringRector;
+use Rector\Php71\Rector\List_\ListToArrayDestructRector;
 use Rector\Php73\Rector\FuncCall\ArrayKeyFirstLastRector;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\Php74\Rector\FuncCall\ArrayKeyExistsOnPropertyRector;
+use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
@@ -51,6 +52,7 @@ return RectorConfig::configure()
         AssignArrayToStringRector::class, // one of the withPhpSets rules
         ChangeSwitchToMatchRector::class, // one of the withPhpSets rules
         ClassConstantToSelfClassRector::class, // one of the withPhpSets rules
+        ListToArrayDestructRector::class, // one of the withPhpSets rules
     ])
     ->withSkip([
         __DIR__ . '/sites/default/documents/smarty'
