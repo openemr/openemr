a94a0be82892ab9ee3f8b4661971d7f8fa3d1713
Michael A. Smith <michael@opencoreemr.com>
2025-10-05 14:50:03 -0400

    refactor(php): replace while each with foreach

--- gacl/profiler.inc.php
+++ gacl/profiler.inc.php
@@ -132,7 +132,7 @@ class Profiler {
             echo"============================================================================\n";
             print( "Calls                    Time  Routine\n");
             echo"-----------------------------------------------------------------------------\n";
-            while ([$key, $val] = each ($this->description)) {
+            foreach ($this->description as $key => $val) {
                 $t = $this->elapsedTime($key);
                 $total = $this->running[$key];
                 $count = $this->count[$key];
