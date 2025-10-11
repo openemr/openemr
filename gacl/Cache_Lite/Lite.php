374078ae64f9126be36939465a0844102e1df6b7
Igor Mukhin <igor.mukhin@gmail.com>
2025-10-11 05:51:16 +0300

    Require checker | Removed no longer existing set_magic_quotes_runtime calls

--- gacl/Cache_Lite/Lite.php
+++ gacl/Cache_Lite/Lite.php
@@ -530,14 +530,11 @@ class Cache_Lite
         if ($fp) {
             clearstatcache(); // because the filesize can be cached by PHP itself...
             $length = @filesize($this->_file);
-            $mqr = get_magic_quotes_runtime();
-            set_magic_quotes_runtime(0);
             if ($this->_readControl) {
                 $hashControl = @fread($fp, 32);
                 $length -= 32;
             }
             $data = @fread($fp, $length);
-            set_magic_quotes_runtime($mqr);
             if ($this->_fileLocking) @flock($fp, LOCK_UN);
             @fclose($fp);
             if ($this->_readControl) {
