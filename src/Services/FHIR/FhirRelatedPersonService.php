8736f53c59fe5f38d04b4e112a4acc96bc806868
Stephen Nielson <snielson@discoverandchange.com>
2025-10-25 19:18:41 -0600

    WIP FHIR RelatedPersons Fixes #9248
    Currently this is a WIP as we are passing on the inferno test suite but
    that is using test data in the FhirRelatedPersonService. Once @psaos has
    finished his changes next week we will fully link up the service and be
    able to mark this as complete.
    
    Fixes #9248

--- src/Services/FHIR/FhirRelatedPersonService.php
+++ src/Services/FHIR/FhirRelatedPersonService.php
@@ -2,19 +2,24 @@
 
 namespace OpenEMR\Services\FHIR;
 
+use Mi2\Framework\ListOptions;
 use OpenEMR\FHIR\R4\FHIRDomainResource\FHIRRelatedPerson;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRId;
 use OpenEMR\FHIR\R4\FHIRElement\FHIRMeta;
 use OpenEMR\Services\CodeTypesService;
 use OpenEMR\Services\FHIR\Traits\BulkExportSupportAllOperationsTrait;
 use OpenEMR\Services\FHIR\Traits\FhirBulkExportDomainResourceTrait;
 use OpenEMR\Services\FHIR\Traits\FhirServiceBaseEmptyTrait;
 use OpenEMR\Services\FHIR\Traits\VersionedProfileTrait;
+use OpenEMR\Services\ListService;
 use OpenEMR\Services\Search\FhirSearchParameterDefinition;
 use OpenEMR\Services\Search\ISearchField;
 use OpenEMR\Services\Search\SearchFieldType;
 use OpenEMR\Services\Search\ServiceField;
 use OpenEMR\Services\Search\TokenSearchField;
 use OpenEMR\Services\Search\TokenSearchValue;
+use OpenEMR\Tests\Certification\HIT1\G10_Certification\SinglePatient700APITest;
+use OpenEMR\Tests\Certification\HIT1\G10_Certification\Trait\G10ApiTestTrait;
 use OpenEMR\Validators\ProcessingResult;
 use Ramsey\Uuid\Rfc4122\UuidV4;
 
@@ -48,6 +53,7 @@ class FhirRelatedPersonService extends FhirServiceBase implements IResourceUSCIG
     public function parseOpenEMRRecord($dataRecord = [], $encode = false)
     {
         $fhirRelatedPerson = new FHIRRelatedPerson();
+        $this->populateId($fhirRelatedPerson, $dataRecord);
         $this->populateMeta($fhirRelatedPerson, $dataRecord);
         $this->populateActive($fhirRelatedPerson, $dataRecord);
         $this->populatePatient($fhirRelatedPerson, $dataRecord);
@@ -58,7 +64,19 @@ class FhirRelatedPersonService extends FhirServiceBase implements IResourceUSCIG
         return $fhirRelatedPerson;
     }
 
-    public function populateActive(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void {
+    public function populateId(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void
+    {
+        if (empty($dataRecord['uuid'])) {
+            // this should never happen
+            throw new \InvalidArgumentException('UUID cannot be empty.');
+        }
+        $fhirId = new FHIRId();
+        $fhirId->setValue($dataRecord['uuid']);
+        $fhirRelatedPerson->setId($fhirId);
+    }
+
+    public function populateActive(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void
+    {
         if ('1' === $dataRecord['active']) {
             $fhirRelatedPerson->setActive(true);
         } else {
@@ -66,7 +84,8 @@ class FhirRelatedPersonService extends FhirServiceBase implements IResourceUSCIG
         }
     }
 
-    public function populateMeta(FhirRelatedPerson $fhirRelatedPerson, array $dataRecord): void {
+    public function populateMeta(FhirRelatedPerson $fhirRelatedPerson, array $dataRecord): void
+    {
         $meta = new FHIRMeta();
         $meta->setVersionId('1');
         if (!empty($dataRecord['last_updated'])) {
@@ -80,41 +99,58 @@ class FhirRelatedPersonService extends FhirServiceBase implements IResourceUSCIG
         $fhirRelatedPerson->setMeta($meta);
     }
 
-    public function populatePatient(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void {
+    public function populatePatient(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void
+    {
         if (!empty($dataRecord['puuid'])) {
             $fhirRelatedPerson->setPatient(UtilsService::createRelativeReference('Patient', $dataRecord['puuid']));
         }
     }
 
-    public function populateRelationship(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void {
+    public function populateRelationship(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void
+    {
         $relationshipCode = 'U'; // unknown
-        $codeTypesService = new CodeTypesService();
+        $description = 'Unknown';
 
-        if (!empty($dataRecord['relationship'])) {
-            $concepts = $codeTypesService->parseCodesIntoCodeableConcepts($dataRecord['relationship']);
-            $fhirRelatedPerson->addRelationship(UtilsService::createCodeableConcept($concepts));
+
+        if (!empty($dataRecord['relationship_code'])) {
+            $relationshipCode = $dataRecord['relationship_code'];
+            $listOptionsService = new ListService();
+            $option = $listOptionsService->getListOption('personal_relationship', $relationshipCode);
+            $description = $option['title'] ?? $description;
         }
+        $concept = UtilsService::createCodeableConcept(
+            [$relationshipCode => [
+            'code' => $relationshipCode,
+            'system' => FhirCodeSystemConstants::HL7_ROLE_CODE,
+            'description' => $description
+            ]
+            ]
+        );
+        $fhirRelatedPerson->addRelationship($concept);
     }
 
-    public function populateName(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void {
+    public function populateName(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void
+    {
         $humanName = UtilsService::createHumanNameFromRecord($dataRecord);
         $fhirRelatedPerson->addName($humanName);
     }
 
-    public function populateTelecom(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void {
+    public function populateTelecom(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void
+    {
         $fhirRelatedPerson->addTelecom(UtilsService::createContactPoint($dataRecord['phone_work'], 'phone', 'work'));
         $fhirRelatedPerson->addTelecom(UtilsService::createContactPoint($dataRecord['phone'], 'phone', 'home'));
         $fhirRelatedPerson->addTelecom(UtilsService::createContactPoint($dataRecord['email'], 'email', 'home'));
     }
 
-    public function populateAddress(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void {
+    public function populateAddress(FHIRRelatedPerson $fhirRelatedPerson, $dataRecord): void
+    {
         foreach ($dataRecord['addresses'] as $address) {
             $fhirRelatedPerson->addAddress(UtilsService::createAddressFromRecord($address));
         }
     }
 
     /**
-     * @param ISearchField[] $openEMRSearchParameters
+     * @param  ISearchField[] $openEMRSearchParameters
      * @return ProcessingResult
      */
     protected function searchForOpenEMRRecords($openEMRSearchParameters): ProcessingResult
@@ -123,33 +159,42 @@ class FhirRelatedPersonService extends FhirServiceBase implements IResourceUSCIG
         $uuid = null;
         $patientUuid = null;
         // going to fake it
-        if ($openEMRSearchParameters['_id']) {
+        if ($openEMRSearchParameters['uuid']) {
             /**
              * @var TokenSearchField $id
              */
-            $id = $openEMRSearchParameters['_id'];
+            $id = $openEMRSearchParameters['uuid'];
             /**
              * @var TokenSearchValue[] $values
              */
             $values = $id->getValues();
             $uuid = $values[0]->getHumanReadableCode();
         }
-        if ($openEMRSearchParameters['patient']) {
+        if ($openEMRSearchParameters['puuid']) {
             /**
              * @var TokenSearchField $patient
              */
-            $patient = $openEMRSearchParameters['patient'];
+            $patient = $openEMRSearchParameters['puuid'];
             /**
              * @var TokenSearchValue[] $values
              */
             $values = $patient->getValues();
             $patientUuid = $values[0]->getHumanReadableCode();
         }
+        $record = $this->getSampleRelatedPerson($patientUuid, $uuid);
+
+        $processingResult = new ProcessingResult();
+        $processingResult->addData($record);
+        return $processingResult;
+    }
+
+    public function getSampleRelatedPerson(?string $patientUuid, ?string $uuid)
+    {
         $record = [
-            'puuid' => $patientUuid ?? UuidV4::uuid4()->toString(),
+            'puuid' => $patientUuid ?? SinglePatient700APITest::PATIENT_ID_PRIMARY,
             'uuid' => $uuid ?? UuidV4::uuid4()->toString(),
             'active' => '1',
-            'relationship_code' => CodeTypesService::CODE_TYPE_HL7_ROLE_CODE . ':FAMMEMB',
+            'relationship_code' => 'FAMMEMB',
             'fname' => 'John',
             'lname' => 'Doe',
             'email' => 'example@open-emr.org',
@@ -166,9 +211,7 @@ class FhirRelatedPersonService extends FhirServiceBase implements IResourceUSCIG
                 ]
             ]
         ];
-        $processingResult = new ProcessingResult();
-        $processingResult->addData($record);
-        return $processingResult;
+        return $record;
     }
 
     public function getPatientContextSearchField(): FhirSearchParameterDefinition
@@ -186,7 +229,8 @@ class FhirRelatedPersonService extends FhirServiceBase implements IResourceUSCIG
         return $this->getProfileForVersions(self::USCGI_PROFILE_URI, $this->getSupportedVersions());
     }
 
-    public function getLastModifiedSearchField(): FhirSearchParameterDefinition {
+    public function getLastModifiedSearchField(): FhirSearchParameterDefinition
+    {
         return new FhirSearchParameterDefinition('_lastUpdated', SearchFieldType::DATETIME, ['date']);
     }
 }
