f4f752cbaed30c5ee04b1ba66fc2790c231da3b2
Igor Mukhin <igor.mukhin@gmail.com>
2025-10-29 00:05:01 +0200

    DB | Added Database exceptions

---
+++ src/Common/Database/Exception/DatabaseQueryException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package   OpenEMR
+ *
+ * @link      http://www.open-emr.org
+ *
+ * @author    Igor Mukhin <igor.mukhin@gmail.com>
+ * @copyright Copyright (c) 2025 OpenCoreEMR Inc
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+namespace OpenEMR\Common\Database\Exception;
+
+class DatabaseQueryException extends StatementAwareDatabaseException
+{
+}
