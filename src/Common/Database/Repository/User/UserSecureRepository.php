ccfb7e93bad251cda888cdeaf7074d4d0d18e184
Igor Mukhin <igor.mukhin@gmail.com>
2025-10-29 04:01:42 +0200

    Repositories | Added UserSecureRepository

---
+++ src/Common/Database/Repository/User/UserSecureRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @package   OpenEMR
+ *
+ * @link      http://www.open-emr.org
+ *
+ * @author    Igor Mukhin <igor.mukhin@gmail.com>
+ * @copyright Copyright (c) 2025 OpenCoreEMR Inc
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+namespace OpenEMR\Common\Database\Repository\User;
+
+use OpenEMR\Common\Database\Repository\IdAwareAbstractRepository;
+
+/**
+ * Usage:
+ *   $userSecureRepository = RepositoryFactory::createRepository(UserSecureRepository::class);
+ *   $affected = $userRepository->remove($id);
+ *   $affected = $userRepository->removeBy(['username' => $username]);
+ *
+ * @phpstan-type TUserSecure = array{
+ *     id: int,
+ *     username: string,
+ *     password: string,
+ *     password_history1: ?string,
+ *     password_history2: ?string,
+ *     password_history3: ?string,
+ *     password_history4: ?string,
+ *     last_challenge_response: ?string,
+ *     login_work_area: ?string,
+ *     last_update_password: string,
+ *     last_update: string,
+ *     total_login_fail_counter: int,
+ *     login_fail_counter: int,
+ *     last_login_fail: ?int,
+ *     auto_block_emailed: int,
+ * }
+ *
+ * @extends IdAwareAbstractRepository<TUserSecure>
+ */
+class UserSecureRepository extends IdAwareAbstractRepository
+{
+    public function __construct()
+    {
+        parent::__construct('users_secure');
+    }
+}
