79ee609d3ec7c45d6f21c1d056e9e5b56774dbf9
zmilan <zivkovic.milan@gmail.com>
2025-11-11 20:41:23 +0100

    fix(sessionWrapper): refactor globals.php to use session wrapper and fix small issues (WiP)

--- src/Common/Session/PHPSessionWrapper.php
+++ src/Common/Session/PHPSessionWrapper.php
@@ -36,7 +36,10 @@ class PHPSessionWrapper implements SessionWrapperInterface
 
     public function migrate(bool $destroy = false): bool
     {
-        return session_regenerate_id($destroy);
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            return session_regenerate_id($destroy);
+        }
+        return true;
     }
 
     public function getSymfonySession(): ?Session
@@ -52,5 +55,10 @@ class PHPSessionWrapper implements SessionWrapperInterface
     public function save(): void
     {
         session_write_close();
+    }
+
+    public function clear(): void
+    {
+        session_unset();
     }
 }
