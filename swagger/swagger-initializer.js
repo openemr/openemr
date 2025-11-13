8d1957eb50ee1c43dc68b20c3c12a1952f5a17c5
Eric Gearman <EGearman@ibss.net>
2025-11-13 22:35:36 +0000

    Update to swagger to have the document-post action function

--- swagger/swagger-initializer.js
+++ swagger/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
     const ui = SwaggerUIBundle({
     url: "openemr-api.yaml",
     dom_id: '#swagger-ui',
-    oauth2RedirectUrl: `${window.location.protocol}//${window.location.host}/openemr${window.location.pathname.replace(/[^/]*$/, '')}oauth2-redirect.html`,
+    oauth2RedirectUrl: `${window.location.protocol}//${window.location.host}${window.location.pathname.replace(/[^/]*$/, '')}oauth2-redirect.html`,
     deepLinking: true,
     presets: [
         SwaggerUIBundle.presets.apis
