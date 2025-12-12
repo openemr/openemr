b4d5f99c49b15b36fd17dfbcd4e0437ccde700b1
Eric Stern <eric@ericstern.com>
2025-12-12 14:17:50 -0800

    Tidy up a little further, improve money validation slightly

--- portal/portal_payment.rainforest.php
+++ portal/portal_payment.rainforest.php
@@ -15,16 +15,20 @@ use OpenEMR\PaymentProcessing\Rainforest;
 use OpenEMR\Common\Session\SessionUtil;
 use OpenEMR\Core\OEGlobalsBag;
 
-// Aped from other AJAX endpoints; this isn't an ideal way to read config.
+// Aped from other AJAX endpoints; this isn't an ideal way to read config but
+// it's how most of the current app does it.
 SessionUtil::portalSessionStart();
 $ignoreAuth_onsite_portal = true;
 require_once __DIR__ . '/../interface/globals.php';
 
-// Future scope: proper JSON API
+// Future scope: proper JSON API (e.g. PSR-7 resources)
 $dollars = $_POST['dollars'] ?? '0.00';
 assert(is_string($dollars));
 // TODO: validate this properly
 $cents = intval(100 * floatval($dollars));
+if ($cents <= 0) {
+    throw new UnexpectedValueException('Payment amount must be positive');
+}
 $money = Money::USD($cents);
 
 $gb = OEGlobalsBag::getInstance();
@@ -33,5 +37,3 @@ $rf = Rainforest::makeFromGlobals($gb);
 $params = $rf->getPaymentComponentParameters($money);
 header('Content-type: application/json');
 echo json_encode($params);
-error_log("New Path");
-exit;
