d0885eefd6796aa2583f245b1c79bf511e666a66
Igor Mukhin <igor.mukhin@gmail.com>
2025-12-17 17:11:56 +0200

    Settings API | Added GlobalSettingsFixture & UserSettingsFixture

---
+++ tests/Tests/Fixtures/Settings/UserSettingsFixture.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @package   OpenEMR
+ *
+ * @link      http://www.open-emr.org
+ *
+ * @author    Igor Mukhin <igor.mukhin@gmail.com>
+ * @copyright Copyright (c) 2025 OpenCoreEMR Inc
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+namespace OpenEMR\Tests\Fixtures\Settings;
+
+use OpenEMR\Common\Database\Database;
+use OpenEMR\Common\Database\DatabaseTables;
+use OpenEMR\Tests\Fixtures\AbstractFixture;
+
+class UserSettingsFixture extends AbstractFixture
+{
+    private string $table = DatabaseTables::TABLE_USER_SETTINGS;
+
+    public function __construct(
+        private readonly Database $db
+    ) {
+    }
+
+    public function load(): void
+    {
+        $this->loadFromFile(sprintf('%s/../data/settings/user_settings.json', __DIR__));
+    }
+
+    protected function loadRecord(array $record): array
+    {
+        $this->db->insert($this->table, $record);
+
+        return $record;
+    }
+}
