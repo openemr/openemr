1ba680e8522a5957e2944db76af596ab6c05b3ba
Igor Mukhin <igor.mukhin@gmail.com>
2025-12-17 17:16:31 +0200

    Fixtures | Added RemovableFixtureInterface

---
+++ tests/Tests/Fixtures/RemovableFixtureInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package   OpenEMR
+ *
+ * @link      http://www.open-emr.org
+ *
+ * @author    Igor Mukhin <igor.mukhin@gmail.com>
+ * @copyright Copyright (c) 2025 OpenCoreEMR Inc
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+namespace OpenEMR\Tests\Fixtures;
+
+interface RemovableFixtureInterface extends FixtureInterface
+{
+    public function removeFixtureRecords(): void;
+}
