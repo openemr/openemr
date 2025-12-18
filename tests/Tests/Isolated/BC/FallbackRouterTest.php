ab0f5a87716ecb268b9873f3e90fd139f04c43ba
Eric Stern <eric@ericstern.com>
2025-12-17 16:53:29 -0800

    Test placeholder

---
+++ tests/Tests/Isolated/BC/FallbackRouterTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEMR\Tests\Isolated\BC;
+
+use OpenEMR\BC\FallbackRouter;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(FallbackRouter::class)]
+#[Small]
+class FallbackRouterTest extends TestCase
+{
+    // TODO: add tests :)
+}
