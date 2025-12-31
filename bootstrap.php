32c8497a488df79886869ad081211959b6ba85cc
Eric Stern <eric@ericstern.com>
2025-12-31 13:52:13 -0800

    Note on error_log

--- bootstrap.php
+++ bootstrap.php
@@ -10,7 +10,8 @@ chdir(__DIR__);
 date_default_timezone_set('UTC');
 ini_set('display_errors', '0');
 ini_set('display_startup_errors', '0');
-ini_set('error_log', '/dev/stdout'); // Docker wants logs written to stdout
+// Docker wants logs written to stdout. This may need to vary by SAPI.
+ini_set('error_log', '/dev/stdout');
 ini_set('log_errors', '1');
 
 error_reporting(E_ALL);
