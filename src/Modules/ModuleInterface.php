170a30bbe8794505c78b27d32ece6f02d90508ee
Eric Stern <eric@ericstern.com>
2025-12-31 16:15:54 -0800

    More details on interface

--- src/Modules/ModuleInterface.php
+++ src/Modules/ModuleInterface.php
@@ -6,5 +6,18 @@ namespace OpenEMR\Modules;
 
 interface ModuleInterface
 {
+    /**
+     * The `type` needed in a module's `composer.json`
+     *
+     * @internal
+     */
     final public const COMPOSER_TYPE = 'openemr-module';
+
+    /**
+     * The module's `extra` in `composer.json` needs the fully-qualified name of
+     * the class that implements `ModuleInterface`.
+     *
+     * @internal
+     */
+    final public const ENTRYPOINT_KEY = 'openemr-module-entrypoint';
 }
