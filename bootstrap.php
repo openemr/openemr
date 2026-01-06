c7376894dec85be767311f3d426233d63f873c7d
Eric Stern <eric@ericstern.com>
2026-01-05 17:26:16 -0800

    Remove some outdated stuff

--- bootstrap.php
+++ bootstrap.php
@@ -15,25 +15,5 @@ ini_set('error_log', '/dev/stdout');
 ini_set('log_errors', '1');
 
 error_reporting(E_ALL);
-error_reporting(E_ALL & ~E_USER_DEPRECATED);
 
 require_once 'vendor/autoload.php';
-
-// This probably needs to initialize the module manager in such a way that
-// enabled modules can be determined before the main DI container is
-// initialized. Doing so will allow modules to vend autowirable services into
-// the DI system.
-//
-// Loosely:
-// $moduleManager = ... // early uinit
-// foreach ($moduleManager->getEnabledModules() as $modileInfo) {
-//   foreach ($moduleInfo->getAutowiredClasses() as $fqcn) {
-//     $container->autowire($fqcn)
-//   }
-// }
-//
-// After this happens, the application should be able to access the
-// fully-prepared DI container to do whatever needs doing.
-//
-// While the manager should be available within the continer, it might be
-// necessary to do some early magic.
