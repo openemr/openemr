7da27ea62970d1e5de578c20e746e1271f47b527
Eric Stern <eric@ericstern.com>
2026-01-13 14:35:54 -0800

    doccomment for how the processors should work

--- src/PaymentProcessing/Rainforest/Webhooks/ProcessorInterface.php
+++ src/PaymentProcessing/Rainforest/Webhooks/ProcessorInterface.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace OpenEMR\PaymentProcessing\Rainforest\Webhooks;
 
+/**
+ * Webhook event processor
+ *
+ * A processor can handle one or more events, and there can be zero or more
+ * processors that are registered for a given event.
+ *
+ * Each processor must be idempotent, and must not rely on running in
+ * a specific order.
+ */
 interface ProcessorInterface
 {
     public function handle(Webhook $webhook): void;
