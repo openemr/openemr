506907c138f3df7d19268e1deb6151a1523a21e5
Eric Stern <eric@ericstern.com>
2026-01-12 17:27:59 -0800

    Notes for next steps

--- verify.php
+++ verify.php
@@ -9,13 +9,26 @@ use Http\Discovery\Psr17Factory;
 
 \Dotenv\Dotenv::createUnsafeImmutable('.')->load();
 
+$mid = getenv('RAINFOREST_MERCHANT_ID');
+if ($mid === false) {
+    throw new InvalidArgumentException('RAINFOREST_MERCHANT_ID envvar is missing.');
+}
+
 $whv = new WebhookVerifier(getenv('RAINFOREST_WEBHOOK_SECRET'));
 
 $req = (new Psr17Factory())->createServerRequestFromGlobals();
 
 try {
     $wh = $whv->verify($req);
+    if ($wh->getMerchantId() !== $mid) {
+        error_log('Webhook for other merchant, ignoring');
+        header('HTTP/1.1 204 No Content');
+        exit;
+    }
     print_r($wh);
+    // TODO: figure out the processing rules here.
+    // More or less, when payin.authorized (or .succeeded?), look up the ar_ data
+    // and update it so it shows the payment has cleared.
 } catch (Throwable $e) {
     error_log((string)$e);
     header('HTTP/1.1 400 Bad Request');
