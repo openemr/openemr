0943e9dc31d4eb441ce77af8919092385299cb8f
Eric Stern <eric@ericstern.com>
2026-01-15 14:29:04 -0800

    Rough-in the insert

--- src/PaymentProcessing/Recorder.php
+++ src/PaymentProcessing/Recorder.php
@@ -10,9 +10,36 @@ class Recorder
 {
     private function recordActivity(string $patientId, string $encounterId): void
     {
+        $query = <<<'SQL'
+            INSERT INTO `ar_activity`
+            SET
+                `pid` = ?,
+                `encounter` = ?,
+                `sequence_no` = ?,
+                `code_type` = ?,
+                `code` = ?,
+                `modifier` = ?,
+                `payer_type` = ?,
+                `post_time` = ?,
+                `post_user` = ?,
+                `session_id` = ?,
+                `modified_time` = ?,
+                `pay_amount` = ?,
+                `adj_amount` = ?,
+                `memo` = ?,
+                `account_code` = ?
+            SQL;
+        QueryUtils::inTransaction(function () {
+            $next = $this->getNextSequenceNumber();
+            sqlStatement($query, $params);
+        });
     }
 
     // Note: values are stored as `int`s
+    //
+    // Note: even in a default-configured DB transaction, this still has
+    // a potential race condition. It should either be done as a subquery in
+    // the insert, or using a locking read (SELECT...FOR UPDATE may work?)
     private function getNextSequenceNumber(string $patientId, string $encounterId): string
     {
         $result = QueryUtils::querySingleRow(<<<'SQL'
