d21b79f1f73a6a6bda78024c2cb2b02c63e7279e
zmilan <zivkovic.milan@gmail.com>
2026-01-17 12:11:19 +0100

    feat(session): handling more of portal-related files

--- src/Core/Header.php
+++ src/Core/Header.php
@@ -9,6 +9,7 @@
 namespace OpenEMR\Core;
 
 use OpenEMR\Common\Logging\SystemLogger;
+use OpenEMR\Common\Session\SessionWrapperFactory;
 use OpenEMR\Events\Core\ScriptFilterEvent;
 use OpenEMR\Events\Core\StyleFilterEvent;
 use OpenEMR\Services\LogoService;
@@ -215,6 +216,9 @@ class Header
         $foundAssets = [];
         $excludedCount = 0;
 
+        $session = SessionWrapperFactory::getInstance()->getActiveSession();
+        $language_direction = $session->get('language_direction');
+
         foreach ($map as $k => $opts) {
             $autoload = $opts['autoload'] ?? false;
             $alreadyBuilt = $opts['alreadyBuilt'] ?? false;
@@ -247,7 +251,7 @@ class Header
                     }
                 }
 
-                if ($rtl && !empty($_SESSION['language_direction']) && $_SESSION['language_direction'] == 'rtl') {
+                if ($rtl && !empty($language_direction) && $language_direction == 'rtl') {
                     $tmpRtl = self::buildAsset($rtl, $alreadyBuilt);
                     foreach ($tmpRtl['scripts'] as $s) {
                         self::$scripts[] = $s;
