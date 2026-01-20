b56943ac2dc5791ee00c00162bfc5320747f3d12
Eric Stern <eric@ericstern.com>
2026-01-20 09:16:52 -0800

    format

--- tests/Tests/BC/DatabaseTest.php
+++ tests/Tests/BC/DatabaseTest.php
@@ -6,7 +6,6 @@
  * @author    Eric Stern <erics@opencoreemr.com>
  * @copyright Copyright (c) 2026 OpenCoreEMR
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
- *
  */
 
 declare(strict_types=1);
