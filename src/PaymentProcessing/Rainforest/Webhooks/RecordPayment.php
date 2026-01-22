039c42c605a07f6dc994267fcebfe32cff1ea41a
Eric Stern <eric@ericstern.com>
2026-01-22 14:49:06 -0800

    Build sessions

--- src/PaymentProcessing/Rainforest/Webhooks/RecordPayment.php
+++ src/PaymentProcessing/Rainforest/Webhooks/RecordPayment.php
@@ -6,8 +6,11 @@ namespace OpenEMR\PaymentProcessing\Rainforest\Webhooks;
 
 use Money\{
     Currencies\ISOCurrencies,
+    Currency,
     Formatter\DecimalMoneyFormatter,
+    Money,
 };
+use OpenEMR\Common\Database\QueryUtils;
 use OpenEMR\PaymentProcessing\Rainforest\Metadata;
 use OpenEMR\PaymentProcessing\Recorder;
 
@@ -27,40 +30,69 @@ class RecordPayment implements ProcessorInterface
     {
         // FIXME: this must be idempotent
         assert($webhook->eventType === 'payment.something');
-
-        $data = $webhook->data;
-
-        $metadata = Metadata::fromParsedJson($data['metadata']);
-        $patientId = $metadata->patientId;
-
-        $memo = sprintf('Rainforest transaction id %s', $data['payin_id']);
-        $dmf = new DecimalMoneyFormatter(new ISOCurrencies());
-
-        // See format $payinPayload in PaymentProcessing/Rainforest
-        $activities = array_map(function ($enc) use ($dmf, $patientId, $memo): array {
-            return [
-                'patientId' => $patientId,
-                'encounterId' => $enc->id,
-                'codeType' => $enc->codeType,
-                'code' => $enc->code,
-                'modifier' => '',
-                'payerType' => '0',
-                'postUser' => '', // does this need to be filled?
-                'sessionId' => '????', // ar_sessions
-                'payAmount' => $dmf->format($enc->amount),
-                'adjustmentAmount' => '0.00',
-                'memo' => $memo,
-            ];
-        }, $metadata->encounters);
-
+        // This transaction should be done in a general recording service, but
+        // PaymentProcessing/Recorder isn't sophisticaed enough yet.
         //
-        // FROM WHAT I CAN TELL recording the payment is enough but ar_session
-        // seems relevant
-        $r = new Recorder();
-        // In txn?
-        foreach ($activities as $activity) {
-            $r->recordActivity($activity);
-        }
+        // Also, most of the data munging should be done prior to DB
+        // interactions, but the use() would be bonkers.
+        QueryUtils::inTransaction(function () use ($webhook) {
+
+            /**
+             * @var array{
+             *   amount: int,
+             *   currency_code: string,
+             *   metadata: array<string, mixed>,
+             *   payin_id: string,
+             * }
+             */
+            $data = $webhook->data;
+
+            $metadata = Metadata::fromParsedJson($data['metadata']);
+            $patientId = $metadata->patientId;
+
+            $memo = sprintf('Rainforest transaction id %s', $data['payin_id']);
+            $dmf = new DecimalMoneyFormatter(new ISOCurrencies());
+
+            $r = new Recorder();
+            // (txn should start here)
+            $sessionId = $r->createSession([
+                'payerId' => '',
+                'userId' => '',
+                'reference' => $data['payin_id'],
+                'payTotal' => new Money((string)$data['amount'], new Currency($data['currency_code'])),
+                'paymentType' => Recorder::PAYMENT_TYPE_PATIENT,
+                'description' => '',
+                'adjustmentCode' => Recorder::ADJUSTMENT_CODE_PATIENT_PAYMENT,
+                'patientId' => $patientId,
+                'paymentMethod' => Recorder::PAYMENT_METHOD_CREDIT_CARD,
+            ]);
+
+            // See format $payinPayload in PaymentProcessing/Rainforest
+            $activities = array_map(function ($enc) use ($dmf, $patientId, $memo, $sessionId): array {
+                return [
+                    'patientId' => $patientId,
+                    'encounterId' => $enc->id,
+                    'codeType' => $enc->codeType,
+                    'code' => $enc->code,
+                    'modifier' => '',
+                    'payerType' => '0',
+                    'postUser' => '', // does this need to be filled?
+                    'sessionId' => $sessionId,
+                    'payAmount' => $dmf->format($enc->amount),
+                    'adjustmentAmount' => '0.00',
+                    'memo' => $memo,
+                    // need account code?
+                ];
+            }, $metadata->encounters);
+
+            //
+            // FROM WHAT I CAN TELL recording the payment is enough but ar_session
+            // seems relevant
+            // In txn?
+            foreach ($activities as $activity) {
+                $r->recordActivity($activity);
+            }
+        });
         // insert into payments ?
         // insert into ar_session
         // update onsite_portal_activity
