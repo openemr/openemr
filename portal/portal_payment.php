00bf34a832ffe53bf51f0a597fa1ab5b79079784
Eric Stern <eric@ericstern.com>
2026-01-22 13:43:56 -0800

    Prod mode switching

--- portal/portal_payment.php
+++ portal/portal_payment.php
@@ -1281,7 +1281,7 @@ error_log(print_r($value, true));
     }
     if ($globalsBag->get('payment_gateway') === 'Rainforest' && $session->has('patient_portal_onsite_two')) {
         if (!!$GLOBALS['gateway_mode_production']) {
-            // todo: prod mode
+            echo '<script type="module" src="https://static.rainforestpay.com/payment.js"></script>';
         } else {
             echo '<script type="module" src="https://static.rainforestpay.com/sandbox.payment.js"></script>';
         }
