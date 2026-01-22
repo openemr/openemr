18c62a6987d26e7e469995eb79f24214dfef57f1
Eric Stern <eric@ericstern.com>
2026-01-22 13:39:26 -0800

    Get the data saving

--- src/PaymentProcessing/Rainforest/Webhooks/RecordPayment.php
+++ src/PaymentProcessing/Rainforest/Webhooks/RecordPayment.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace OpenEMR\PaymentProcessing\Rainforest\Webhooks;
 
+use Money\{
+    Currencies\ISOCurrencies,
+    Formatter\DecimalMoneyFormatter,
+};
+use OpenEMR\PaymentProcessing\Rainforest\Metadata;
 use OpenEMR\PaymentProcessing\Recorder;
 
 class RecordPayment implements ProcessorInterface
@@ -17,32 +22,48 @@ class RecordPayment implements ProcessorInterface
 
     public function handle(Webhook $webhook): void
     {
+        // FIXME: this must be idempotent
         assert($webhook->eventType === 'payment.something');
 
         $data = $webhook->data;
+        $metadata = Metadata::fromParsedJson($data['metadata']);
 
-        // This SHOULD delegate to a payment service, but creating that too is
-        // probably excessive scope creep.
-        // TODO:
+        // error_log(print_r($metadata, true));
+
+        $patientId = $metadata->patientId;
+
+        $memo = sprintf('Rainforest transaction id %s', $data['payin_id']);
+        $dmf = new DecimalMoneyFormatter(new ISOCurrencies());
+
+        // See format $payinPayload in PaymentProcessing/Rainforest
+        $activities = array_map(function ($enc) use ($dmf, $patientId, $memo): array {
+            return [
+                'patientId' => $patientId,
+                'encounterId' => $enc->id,
+                'codeType' => $enc->codeType,
+                'code' => $enc->code,
+                'modifier' => '',
+                'payerType' => '0',
+                'postUser' => '????',
+                'sessionId' => '????', // ar_sessions
+                'payAmount' => $dmf->format($enc->amount),
+                'adjustmentAmount' => '0.00',
+                'memo' => $memo,
+            ];
+        }, $metadata->encounters);
+
         // insert into ar_session
         // insert into ar_activity
         // update onsite_portal_activity
         // more?
         //
-        // FROM WHAT I CAN TELL recording the payment is enough
+        // FROM WHAT I CAN TELL recording the payment is enough but ar_session
+        // seems relevant
         $r = new Recorder();
-        $r->recordActivity([
-            'patientId' => '', // Need to send in and retrieve from pmt metadata
-            'encounterId' => '', // ^^
-            'codeType' => 'PCP',
-            'code' => '99205', // WHERE DOES THIS COME FROM
-            'modifier' => '',
-            'payerType' => '0',
-            'postUser' => '????',
-            'sessionId' => '????',
-            'payAmount' => 'dollar-format-from-wh', // data.amount
-            'adjustmentAmount' => '0.00',
-            'memo' => 'Rainforest transaction id XXXX', // data.payin_id
-        ]);
+        // In txn?
+        foreach ($activities as $activity) {
+            $r->recordActivity($activity);
+        }
+        // insert into payments ?
     }
 }
