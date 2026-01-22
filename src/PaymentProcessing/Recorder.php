72bc0e6522a88ee3ecdb4e775191342aa7f076bf
Eric Stern <eric@ericstern.com>
2026-01-22 14:36:28 -0800

    Session creator

--- src/PaymentProcessing/Recorder.php
+++ src/PaymentProcessing/Recorder.php
@@ -4,10 +4,84 @@ declare(strict_types=1);
 
 namespace OpenEMR\PaymentProcessing;
 
+use Money\{
+    Currencies\ISOCurrencies,
+    Formatter\DecimalMoneyFormatter,
+    Money,
+};
 use OpenEMR\Common\Database\QueryUtils;
 
 class Recorder
 {
+    // Future (all blocks): convert to enums
+    // Future (all blocks): add other supported types
+
+    public const ADJUSTMENT_CODE_PATIENT_PAYMENT = 'patient_payment';
+
+    public const PAYMENT_METHOD_CREDIT_CARD = 'credit_card';
+
+    public const PAYMENT_TYPE_PATIENT = 'patient';
+
+    // COPAY/PCP
+    /**
+     * @param array{
+     *   payerId: string,
+     *   userId: string,
+     *   reference: string,
+     *   payTotal: Money,
+     *   paymentType: self::PAYMENT_TYPE_*,
+     *   description: string,
+     *   adjustmentCode: self::ADJUSTMENT_CODE_*,
+     *   patientId: string,
+     *   paymentMethod: self::PAYMENT_METHOD_*,
+     * } $data
+     * @return string the session identifier
+     */
+    public function createSession(array $data): string
+    {
+        $now = date('Y-m-d H:i:s');
+        $date = date('Y-m-d'); // Future: param?
+        $query = <<<'SQL'
+        INSERT INTO `ar_session` (
+            `payer_id`,
+            `user_id`,
+            `closed`,
+            `reference`,
+            `check_date`,
+            `deposit_date`,
+            `pay_total`,
+            `created_time`,
+            `modified_time`,
+            `global_amount`,
+            `payment_type`,
+            `description`,
+            `adjustment_code`,
+            `post_to_date`,
+            `patient_id`,
+            `payment_method`
+        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+        SQL;
+        $params = [
+            $data['payerId'], // `payer_id`
+            $data['userId'], // `user_id`
+            0, // `closed`
+            $data['reference'], // `reference`
+            $date, // `check_date`
+            $date, // `deposit_date`
+            self::formatMoney($data['payTotal']), // `pay_total`
+            $now, // `created_time`
+            $now, // `modified_time`
+            '0.00', // `global_amount`
+            $data['paymentType'], // `payment_type`
+            $data['description'], // `description`
+            $data['adjustmentCode'], // `adjustment_code`
+            $date, // `post_to_date`
+            $data['patientId'], // `patient_id`
+            $data['paymentMethod'], // `payment_method
+        ];
+        return (string) QueryUtils::sqlInsert($query, $params);
+    }
+
     /**
      * Future scope:
      * - Amounts to Money type
@@ -108,5 +182,17 @@ class Recorder
             WHERE pid = ? AND encounter = ?
         SQL, [$patientId, $encounterId]);
         return $result['increment'];
+    }
+
+    /**
+     * Converts well-formed Money objects to a decimal string
+     *
+     * e.g. Money::USD(123) => '1.23'
+     */
+    private static function formatMoney(Money $money): string
+    {
+        // Note: this should be done in a DBAL layer, not here.
+        $dmf = new DecimalMoneyFormatter(new ISOCurrencies());
+        return $dmf->format($money);
     }
 }
