c91ef5b8a007e84a72710b96956a87363b7edb37
Eric Stern <eric@ericstern.com>
2026-01-23 12:23:56 -0800

    add new wrapper

--- library/global_functions.inc.php
+++ library/global_functions.inc.php
@@ -4,3 +4,11 @@ declare(strict_types=1);
 
 // This file is intentionally empty for now; see PHPStan rule
 // ForbiddenGlobalNamespaceRule
+
+/**
+ * Reads $_POST and trims the value. New code should NOT use this function.
+ */
+function trimPost(string $key): string
+{
+    return \trim($_POST[$key] ?? '');
+}
