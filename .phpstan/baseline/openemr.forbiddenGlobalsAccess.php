d2c340bb9ce9d5d4f1d99774593a1c4ade6a614c
Eric Stern <eric@ericstern.com>
2026-02-02 15:04:19 -0800

    Update baseline (mostly trades)

--- .phpstan/baseline/openemr.forbiddenGlobalsAccess.php
+++ .phpstan/baseline/openemr.forbiddenGlobalsAccess.php
@@ -3023,7 +3023,7 @@ $ignoreErrors[] = [
 ];
 $ignoreErrors[] = [
     'message' => '#^Direct access to \\$GLOBALS is forbidden\\. Use OEGlobalsBag\\:\\:getInstance\\(\\)\\-\\>get\\(\\) instead\\.$#',
-    'count' => 81,
+    'count' => 72,
     'path' => __DIR__ . '/../../library/sql.inc.php',
 ];
 $ignoreErrors[] = [
@@ -3323,7 +3323,7 @@ $ignoreErrors[] = [
 ];
 $ignoreErrors[] = [
     'message' => '#^Direct access to \\$GLOBALS is forbidden\\. Use OEGlobalsBag\\:\\:getInstance\\(\\)\\-\\>get\\(\\) instead\\.$#',
-    'count' => 5,
+    'count' => 15,
     'path' => __DIR__ . '/../../src/Common/Database/QueryUtils.php',
 ];
 $ignoreErrors[] = [
