1e63cbab34558bca029533f87cdb6efb1ff32c75
Michael A. Smith <michael@opencoreemr.com>
2026-02-02 00:26:15 -0500

    Merge commit from fork
    * fix(security): add ACL and CSRF checks to procedure type endpoints
    
    types_edit.php and types_ajax.php had no access control checks,
    allowing any authenticated user to view, add, edit, or delete
    procedure type configuration by accessing these endpoints directly,
    bypassing the ACL checks in the parent types.php page.
    
    Add admin/super ACL check to types_edit.php with CSRF token
    protection on form submission. Add admin/super or patients/lab
    ACL check to types_ajax.php to match the parent page's popup
    mode permissions.
    
    Ref: GHSA-6h2m-4ppf-ph4j
    
    * fixup! fix(security): add ACL and CSRF checks to procedure type endpoints
    
    fixup! fix(security): add ACL and CSRF checks to procedure type endpoints
    
    Use OEGlobalsBag instead of $GLOBALS['kernel'] in types_edit.php ACL
    check to avoid increasing PHPStan baseline error count.

--- interface/orders/types_edit.php
+++ interface/orders/types_edit.php
@@ -8,15 +8,26 @@
  * @author    Rod Roark <rod@sunsetsystems.com>
  * @author    Brady Miller <brady.g.miller@gmail.com>
  * @author    Jerry Padgett <sjpadgett@gmail.com>
+ * @author    Michael A. Smith <michael@opencoreemr.com>
  * @copyright Copyright (c) 2010-2017 Rod Roark <rod@sunsetsystems.com>
  * @copyright Copyright (c) 2019 Brady Miller <brady.g.miller@gmail.com>
+ * @copyright Copyright (c) 2026 OpenCoreEMR Inc <https://opencoreemr.com/>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
 require_once("../globals.php");
 require_once("$srcdir/options.inc.php");
 
+use OpenEMR\Common\Acl\AclMain;
+use OpenEMR\Common\Csrf\CsrfUtils;
+use OpenEMR\Common\Twig\TwigContainer;
 use OpenEMR\Core\Header;
+use OpenEMR\Core\OEGlobalsBag;
+
+if (!AclMain::aclCheckCore('admin', 'super')) {
+    echo (new TwigContainer(null, OEGlobalsBag::getInstance()->get('kernel')))->getTwig()->render('core/unauthorized.html.twig', ['pageTitle' => xl("Configure Orders and Results")]);
+    exit;
+}
 
 $typeid = ($_REQUEST['typeid'] ?? '') + 0;
 $parent = ($_REQUEST['parent'] ?? '') + 0;
@@ -269,6 +280,12 @@ function recursiveDelete($typeid): void
         <?php
         // If we are saving, then save and close the window.
         //
+        if (!empty($_POST['form_save']) || !empty($_POST['form_delete'])) {
+            if (!CsrfUtils::verifyCsrfToken($_POST["csrf_token_form"])) {
+                CsrfUtils::csrfNotVerified();
+            }
+        }
+
         if (!empty($_POST['form_save'])) {
             $p_procedure_code = invalue('form_procedure_code');
 
@@ -336,8 +353,9 @@ function recursiveDelete($typeid): void
         <div class="row">
             <div class="col-sm-12">
                 <form method='post' name='theform' class="form-horizontal"
-                    action='types_edit.php?typeid=<?php echo attr_url($typeid); ?>&parent=<?php echo attr_url($parent); ?>'>
-                    <!-- no restoreSession() on submit because session data are not relevant -->
+                    action='types_edit.php?typeid=<?php echo attr_url($typeid); ?>&parent=<?php echo attr_url($parent); ?>'
+                    onsubmit='return top.restoreSession()'>
+                    <input type="hidden" name="csrf_token_form" value="<?php echo attr(CsrfUtils::collectCsrfToken()); ?>" />
                     <fieldset>
                         <legend name="form_legend" id="form_legend"><?php echo xlt('Enter Details'); ?> <i id='enter_details' class='fa fa-info-circle oe-text-black oe-superscript enter-details-tooltip' aria-hidden='true'></i></legend>
                         <div class="row">
