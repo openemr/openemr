4a163d9732f9a4906afa69d30efbb4e0db1049a3
Eric Stern <eric@ericstern.com>
2026-02-10 15:58:19 -0800

    Update to imported name

--- interface/globals.php
+++ interface/globals.php
@@ -776,9 +776,9 @@ if (!empty($checkModulesTableExists)) {
 $encounter = empty($session->get('encounter')) ? 0 : $session->get('encounter');
 
 if (!empty($_GET['pid']) && empty($session->get('pid'))) {
-    OpenEMR\Common\Session\SessionUtil::setSession('pid', $_GET['pid']);
+    SessionUtil::setSession('pid', $_GET['pid']);
 } elseif (!empty($_POST['pid']) && empty($session->get('pid'))) {
-    OpenEMR\Common\Session\SessionUtil::setSession('pid', $_POST['pid']);
+    SessionUtil::setSession('pid', $_POST['pid']);
 }
 
 $pid = empty($session->get('pid')) ? 0 : $session->get('pid');
