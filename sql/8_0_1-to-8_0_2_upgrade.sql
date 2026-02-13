8362f878123dafc589d9c50fdc614bacaddaf01c
phantom930 <phantom93930@gmail.com>
2026-02-13 09:26:46 +0100

    fix: update patient_data.race

--- sql/8_0_1-to-8_0_2_upgrade.sql
+++ sql/8_0_1-to-8_0_2_upgrade.sql
@@ -1,7 +1,20 @@
 --
--- Rename misspelled list_options option_id declne_to_specfy -> decline_to_specify
+-- Rename the misspelled list_options option_id from 'declne_to_specfy' to 'decline_to_specify',
+-- and update any patient_data.race records that reference the old value.
 --
 
+#IfColumn patient_data race
+UPDATE `patient_data` SET `race` = 'decline_to_specify' WHERE `race` = 'declne_to_specfy';
+#EndIf
+
+#IfRow2D list_options list_id race option_id declne_to_specfy
+UPDATE `list_options` SET `option_id` = 'decline_to_specify' WHERE `list_id` = 'race' AND `option_id` = 'declne_to_specfy';
+#EndIf
+
 #IfRow2D list_options list_id language option_id declne_to_specfy
-UPDATE `list_options` SET `option_id` = 'decline_to_specify' WHERE `option_id` = 'declne_to_specfy';
+UPDATE `list_options` SET `option_id` = 'decline_to_specify' WHERE `list_id` = 'language' AND `option_id` = 'declne_to_specfy';
+#EndIf
+
+#IfRow2D list_options list_id ethrace option_id declne_to_specfy
+UPDATE `list_options` SET `option_id` = 'decline_to_specify' WHERE `list_id` = 'ethrace' AND `option_id` = 'declne_to_specfy';
 #EndIf
