166e80856a3212b88cdcb4096702989fbbb2a68d
Eric Stern <eric@ericstern.com>
2026-02-13 10:44:54 -0800

    missed a header

--- config/cli-config.php
+++ config/cli-config.php
@@ -7,6 +7,12 @@
  * this file MUST live at this exact path or in the repository root. A future
  * integration w/ CLI tooling may end up in a different location.
  *
+ * @package   openemr
+ * @link      https://www.open-emr.org
+ * @author    Eric Stern <erics@opencoreemr.com>
+ * @copyright Copyright (c) 2026 OpenCoreEMR
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ *
  * @link https://www.doctrine-project.org/projects/doctrine-migrations/en/3.9/reference/configuration.html#advanced
  * @link https://www.doctrine-project.org/projects/doctrine-migrations/en/3.9/reference/custom-integration.html#custom-integration
  */
