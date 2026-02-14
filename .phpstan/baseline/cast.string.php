a595f294e67602dc4d69aa39fc4a33818806ecdf
Eric Stern <eric@ericstern.com>
2026-02-13 18:47:00 -0800

    trade some more errors

--- .phpstan/baseline/cast.string.php
+++ .phpstan/baseline/cast.string.php
@@ -673,6 +673,11 @@ $ignoreErrors[] = [
 ];
 $ignoreErrors[] = [
     'message' => '#^Cannot cast mixed to string\\.$#',
+    'count' => 2,
+    'path' => __DIR__ . '/../../interface/main/calendar/modules/PostCalendar/pnadminapi.php',
+];
+$ignoreErrors[] = [
+    'message' => '#^Cannot cast mixed to string\\.$#',
     'count' => 14,
     'path' => __DIR__ . '/../../interface/main/calendar/modules/PostCalendar/pnincludes/Date/Calc.php',
 ];
