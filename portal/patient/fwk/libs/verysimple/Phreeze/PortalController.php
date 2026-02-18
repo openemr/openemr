3041cab206e983de5e863c161aca823d5b44f11f
zmilan <zivkovic.milan@gmail.com>
2026-02-18 20:09:50 +0100

    10244|fix(session): fix small issue

--- portal/patient/fwk/libs/verysimple/Phreeze/PortalController.php
+++ portal/patient/fwk/libs/verysimple/Phreeze/PortalController.php
@@ -505,7 +505,7 @@ abstract class PortalController
                         $val = serialize($val);
                     }
 
-                    $val = VerySimpleStringUtil::EncodeSpecialCharacters($val, true, true);
+                    $val = VerySimpleStringUtil::EncodeSpecialCharacters((string) $val, true, true);
 
                     $xml .= "<" . htmlspecialchars($var) . ">" . $val . "</" . htmlspecialchars($var) . ">\r\n";
                 }
