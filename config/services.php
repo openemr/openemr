c613fb0c883dea8128ce9a87b7e9c7a033673057
Eric Stern <eric@ericstern.com>
2026-03-05 19:51:08 -0800

    missed a sort

--- config/services.php
+++ config/services.php
@@ -7,10 +7,10 @@
 declare(strict_types=1);
 
 use Firehed\Container\TypedContainerInterface as TC;
-use Monolog\Level;
-use OpenEMR\Common\Logging\SystemLogger;
 use Lcobucci\Clock\SystemClock;
+use Monolog\Level;
 use OpenEMR\Common\Http\Psr17Factory;
+use OpenEMR\Common\Logging\SystemLogger;
 
 return [
     Level::class => fn (TC $c) => Level::fromName($c->get('LOG_LEVEL')),
