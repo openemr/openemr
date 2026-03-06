2b5fd783c2293367da21e2a4322baaaaaa99bd5a
Eric Stern <eric@ericstern.com>
2026-03-05 19:27:52 -0800

    Wire up other PSR entries

--- config/services.php
+++ config/services.php
@@ -9,10 +9,16 @@ declare(strict_types=1);
 use Firehed\Container\TypedContainerInterface as TC;
 use Monolog\Level;
 use OpenEMR\Common\Logging\SystemLogger;
+use Lcobucci\Clock\SystemClock;
+use OpenEMR\Common\Http\Psr17Factory;
 
 return [
     SystemLogger::class => fn (TC $c) => new SystemLogger($c->get(Level::class)),
 
     Level::class => fn (TC $c) => Level::fromName($c->get('LOG_LEVEL')),
+
+    Psr17Factory::class,
+
+    SystemClock::class => fn () => SystemClock::fromSystemTimezone(),
 ];
 
