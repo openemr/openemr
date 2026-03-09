668a77ce6980b5c12e132d7d4671cd83aeef9a52
Eric Stern <eric@ericstern.com>
2026-03-09 13:27:26 -0700

    Add file headers

--- config/database.php
+++ config/database.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Database and Doctrine configuration
+ *
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    Eric Stern <erics@opencoreemr.com>
+ * @copyright Copyright (c) 2026 Eric Stern
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
 declare(strict_types=1);
 
 use Doctrine\DBAL\{
