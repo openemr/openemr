67f134c35c7114642e0b7897d1cf277ed9ec9826
Eric Stern <eric@ericstern.com>
2026-03-11 16:58:01 -0700

    File headers

--- src/Common/Logging/Audit/Event.php
+++ src/Common/Logging/Audit/Event.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Data transfer object for auditable events
+ *
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    Eric Stern <erics@opencoreemr.com>
+ * @copyright Copyright (c) 2026 Eric Stern
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
 declare(strict_types=1);
 
 namespace OpenEMR\Common\Logging\Audit;
--- src/Common/Logging/Audit/LogTablesSink.php
+++ src/Common/Logging/Audit/LogTablesSink.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Audit event sink that writes to log tables:
+ * - log
+ * - log_comment_encrypt
+ * - api_log
+ *
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    Eric Stern <erics@opencoreemr.com>
+ * @copyright Copyright (c) 2026 Eric Stern
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
 declare(strict_types=1);
 
 namespace OpenEMR\Common\Logging\Audit;
