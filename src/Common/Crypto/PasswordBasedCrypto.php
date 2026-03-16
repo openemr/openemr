81b0607aeac3e1da7f1996a7f87df348d4c64966
Eric Stern <eric@ericstern.com>
2026-03-16 14:22:36 -0700

    Get fairly close

--- src/Common/Crypto/PasswordBasedCrypto.php
+++ src/Common/Crypto/PasswordBasedCrypto.php
@@ -27,11 +27,44 @@ class PasswordBasedCrypto
         $hmac = hash_hmac('sha384', $iv . $encrypted, $hmacKey, true);
 
         $output = $hmac . $iv . $encrypted;
+        // prefix
         return base64_encode($output);
     }
 
     public function decrypt(string $ciphertext, string $password): string
     {
-        return '';
+        // trim prefix and get key version
+
+        $input = base64_decode($ciphertext);
+
+        // (hmac)
+        $salt = mb_substr($input, 0, 32, '8bit');
+        $rest = mb_substr($input, 32, null, '8bit');
+
+        $preKey = hash_pbkdf2('sha384', $password, $salt, 100_000, 32, true);
+        $secretKey = hash_hkdf('sha384', $preKey, 32, 'aes-256-encryption', $salt);
+        $hmacKey = hash_hkdf('sha384', $preKey, 32, 'sha-384-authentication', $salt);
+
+        $hashHmac = mb_substr($rest, 0, 48, '8bit');
+        $ivLen = openssl_cipher_iv_length('aes-256-cbc');
+        $iv = mb_substr($rest, 48, $ivLen, '8bit');
+        $encrypted = mb_substr($rest, ($ivLen + 48), null, '8bit');
+
+        $expectedHmac = hash_hmac('sha384', $iv . $encrypted, $hmacKey, true);
+        if (!hash_equals(known_string: $expectedHmac, user_string: $hashHmac)) {
+            throw new \Exception('Invalid HMAC');
+        }
+
+        $output = openssl_decrypt(
+            $encrypted,
+            'aes-256-cbc',
+            $secretKey,
+            OPENSSL_RAW_DATA,
+            $iv,
+        );
+        if ($output === false) {
+            throw new \Exception('Could not decrypt');
+        }
+        return $output;
     }
 }
