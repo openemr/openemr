1bf2a058689ab3ee1ea2390abb9527feb5ddc7a4
Eric Stern <eric@ericstern.com>
2026-03-16 11:47:09 -0700

    Now we can just use loggertrait

--- src/Common/Logging/SystemLogger.php
+++ src/Common/Logging/SystemLogger.php
@@ -7,6 +7,8 @@ use Monolog\Logger;
 use Monolog\Processor\PsrLogMessageProcessor;
 use OpenEMR\Core\OEGlobalsBag;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
+use Stringable;
 
 /**
  * Class SystemLogger logs information out to the syslog and is a compatible PSR3 logger.
@@ -26,12 +28,12 @@ use Psr\Log\LoggerInterface;
  */
 class SystemLogger implements LoggerInterface
 {
-    /**
-     * @var LoggerInterface;
-     */
-    private $logger;
+    use LoggerTrait;
+
+    private LoggerInterface $logger;
 
     const LOG_LEVEL_DEBUG = "DEBUG";
+
     public function __construct($logLevel = null)
     {
         /**
@@ -61,146 +63,14 @@ class SystemLogger implements LoggerInterface
     }
 
     /**
-     * System is unusable.
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public function emergency($message, array $context = []): void
-    {
-        $this->logger->emergency($message, $context);
-    }
-
-    /**
-     * Action must be taken immediately.
-     *
-     * Example: Entire website down, database unavailable, etc. This should
-     * trigger the SMS alerts and wake you up.
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public function alert($message, array $context = []): void
-    {
-        $this->logger->alert($message, $context);
-    }
-
-    /**
-     * Critical conditions.
-     *
-     * Example: Application component unavailable, unexpected exception.
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public function critical($message, array $context = []): void
-    {
-        $this->logger->critical($message, $context);
-    }
-
-    /**
-     * Runtime errors that do not require immediate action but should typically
-     * be logged and monitored.
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public function error($message, array $context = []): void
-    {
-        $this->logger->error($message, $context);
-    }
-
-    /**
-     * Runtime errors that do not require immediate action but should typically
-     * be logged and monitored.  This function automatically logs the class and function method that invoked the
-     * error log.
-     * @param $message
-     * @param array $context
-     */
-    public function errorLogCaller($message, array $context = []): void
-    {
-        // we skip over arguments and go 2 stack traces to get the current call and the caller function into this one.
-        $dbt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
-        $callerContext = $dbt[1] ?? [];
-        $callerClass = $callerContext['class'] ?? "";
-        $callerType = $callerContext['type'] ?? "";
-        $callerFunction = $callerContext['function'] ?? "";
-        $caller = $callerClass . $callerType . $callerFunction;
-        if ($caller != "") {
-            // make it look like a method signature
-            $caller .= "() ";
-        }
-        $this->error($caller . $message, $context);
-    }
-
-    /**
-     * Exceptional occurrences that are not errors.
-     *
-     * Example: Use of deprecated APIs, poor use of an API, undesirable things
-     * that are not necessarily wrong.
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public function warning($message, array $context = []): void
-    {
-        $this->logger->warning($message, $context);
-    }
-
-    /**
-     * Normal but significant events.
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public function notice($message, array $context = []): void
-    {
-        $this->logger->notice($message, $context);
-    }
-
-    /**
-     * Interesting events.
-     *
-     * Example: User logs in, SQL logs.
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public function info($message, array $context = []): void
-    {
-        $this->logger->info($message, $context);
-    }
-
-    /**
-     * Detailed debug information.
-     *
-     * @param string $message
-     * @param array $context
-     * @return void
-     */
-    public function debug($message, array $context = []): void
-    {
-        $this->logger->debug($message, $context);
-    }
-
-    /**
      * Logs with an arbitrary level.
      *
      * @param mixed $level
-     * @param string $message
+     * @param string|Stringable $message
      * @param array $context
-     * @return void
      */
     public function log($level, $message, array $context = []): void
     {
         $this->logger->log($level, $message, $context);
     }
-
 }
