1103f4409018a1207b27707d2c9a71281f7a48b2
Eric Stern <eric@ericstern.com>
2026-03-16 14:01:35 -0700

    Stub implementation

---
+++ src/Common/Crypto/PasswordBasedCrypto.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEMR\Common\Crypto;
+
+class PasswordBasedCrypto
+{
+    public function encrypt(string $plaintext, string $password): string
+    {
+        return '';
+    }
+
+    public function decrypt(string $ciphertext, string $password): string
+    {
+        return '';
+    }
+}
