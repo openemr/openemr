553de12e4492bd28124ee97c7656c7375b1de9e9
Eric Stern <eric@ericstern.com>
2026-03-17 16:42:53 -0700

    DB reader

---
+++ src/Common/Crypto/Keys/PlaintextKeyInDbKeysTable.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEMR\Common\Crypto\Keys;
+
+use Doctrine\DBAL\Connection;
+
+class PlaintextKeyInDbKeysTable implements KeyManagerInterface
+{
+    public function __construct(
+        private Connection $conn,
+    ) {
+    }
+
+    public function getKey(string $identifier): KeyMaterial
+    {
+        $result = $this->conn->fetchOne(
+            'SELECT value FROM keys WHERE name = ?',
+            [$identifier],
+        );
+
+        if (!is_string($result)) {
+            throw new \Exception('No key found');
+        }
+
+        $key = base64_decode($result, strict: true);
+        if ($key === false) {
+            throw new \Exception('Malformed key');
+        }
+
+        return new KeyMaterial($key);
+    }
+}
