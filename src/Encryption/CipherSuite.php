1d51b4a37b3f03f4eb82e1b940f49f63eb0b1df7
Eric Stern <eric@ericstern.com>
2026-03-20 16:06:00 -0700

    Wire in cyphersuite now

--- src/Encryption/CipherSuite.php
+++ src/Encryption/CipherSuite.php
@@ -4,13 +4,25 @@ declare(strict_types=1);
 
 namespace OpenEMR\Encryption;
 
-class CipherSuite
+use OpenEMR\Encryption\Keys\KeyResolverInterface;
+use SensitiveParameter;
+
+readonly class CipherSuite
 {
-    public function encrypt(Plaintext $plaintext): Message
+    public function __construct(
+        private KeyResolverInterface $keyResolver,
+    ) {
+    }
+
+    public function encrypt(#[SensitiveParameter] Plaintext $plaintext): Message
     {
+        // mostly inverse of decrypt but this isn't supported yet
+        throw new \BadMethodCallException('Encryption not yet supported');
     }
 
     public function decrypt(Message $message): Plaintext
     {
+        $cipher = $this->keyResolver->resolve($message->keyId);
+        return $cipher->decrypt($message->ciphertext);
     }
 }
