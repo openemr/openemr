ba0ab5196d93bcdfcea4ad800e6e70115f5594c1
Eric Stern <eric@ericstern.com>
2026-03-20 15:13:12 -0700

    More plumbing I think

---
+++ src/Encryption/CipherSuite.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEMR\Encryption;
+
+class CipherSuite
+{
+    public function encrypt(Plaintext $plaintext): Message
+    {
+    }
+
+    public function decrypt(Message $message): Plaintext
+    {
+    }
+}
