5757ef318293c7bcde486f7a7640b5f16d310687
Eric Stern <eric@ericstern.com>
2026-03-27 17:15:14 -0700

    Add the parser-detector thing into MessageFormat

--- src/Encryption/MessageFormat.php
+++ src/Encryption/MessageFormat.php
@@ -12,12 +12,31 @@ declare(strict_types=1);
 
 namespace OpenEMR\Encryption;
 
+use UnexpectedValueException;
+use ValueError;
+
 enum MessageFormat
 {
     // Existing format as of March 2026: the "format" id is also a key id
     case ImplicitKey;
     // Future: ExplicitKey
     // Maybe future: Plaintext
+    case UnusedCaseToSupportConditionals;
 
     const LATEST = self::ImplicitKey;
+
+    // This is effectively BackedEnum's `::from`, rejiggered in
+    // a backwards-compatible way
+    public static function detect(string $message): MessageFormat
+    {
+        if (strlen($message) < 3) {
+            throw new UnexpectedValueException('Message is missing expected prefix');
+        }
+        $prefix = substr($message, 0, 3);
+        return match ($prefix) {
+            '001', '002', '003', '004', '005', '006', '007' => self::ImplicitKey,
+            default => throw new ValueError('Unknown message format'),
+            // default: plaintext?
+        };
+    }
 }
