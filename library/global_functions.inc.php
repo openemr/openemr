b1f651c5c93e5d44ea00129a68a8c149881a2005
Eric Stern <eric@ericstern.com>
2026-04-02 11:37:37 -0700

    refactor: move PrintEncHeader to alphabetical position

--- library/global_functions.inc.php
+++ library/global_functions.inc.php
@@ -623,6 +623,31 @@ function parse_note($note)
 }
 
 /**
+ * Print an encounter header row for patient ledger reports.
+ *
+ * @param string $dt The encounter date
+ * @param string $rsn The reason for visit
+ * @param int|string $dr The provider ID
+ * @return void
+ * @global string $bgcolor The current background color
+ * @global int $orow The row counter
+ */
+function PrintEncHeader($dt, $rsn, $dr): void
+{
+    global $bgcolor, $orow;
+    $bgcolor = (($bgcolor == "#FFFFDD") ? "#FFDDDD" : "#FFFFDD");
+    echo "<tr class='bg-white'>";
+    if (strlen((string) $rsn) > 50) {
+        $rsn = substr((string) $rsn, 0, 50) . '...';
+    }
+
+    echo "<td colspan='4'><span class='font-weight-bold'>" . xlt('Encounter Dt / Rsn') . ": </span><span class='detail'>" . text(substr((string) $dt, 0, 10)) . " / " . text($rsn) . "</span></td>";
+    echo "<td colspan='5'><span class='font-weight-bold'>" . xlt('Provider') . ": </span><span class='detail'>" . text(User_Id_Look($dr)) . "</span></td>";
+    echo "</tr>\n";
+    $orow++;
+}
+
+/**
  * Reads $_POST and trims the value. New code should NOT use this function.
  */
 function trimPost(string $key): string
@@ -650,31 +675,6 @@ function User_Id_Look($thisField)
     }
 
     return $ret;
-}
-
-/**
- * Print an encounter header row for patient ledger reports.
- *
- * @param string $dt The encounter date
- * @param string $rsn The reason for visit
- * @param int|string $dr The provider ID
- * @return void
- * @global string $bgcolor The current background color
- * @global int $orow The row counter
- */
-function PrintEncHeader($dt, $rsn, $dr): void
-{
-    global $bgcolor, $orow;
-    $bgcolor = (($bgcolor == "#FFFFDD") ? "#FFDDDD" : "#FFFFDD");
-    echo "<tr class='bg-white'>";
-    if (strlen((string) $rsn) > 50) {
-        $rsn = substr((string) $rsn, 0, 50) . '...';
-    }
-
-    echo "<td colspan='4'><span class='font-weight-bold'>" . xlt('Encounter Dt / Rsn') . ": </span><span class='detail'>" . text(substr((string) $dt, 0, 10)) . " / " . text($rsn) . "</span></td>";
-    echo "<td colspan='5'><span class='font-weight-bold'>" . xlt('Provider') . ": </span><span class='detail'>" . text(User_Id_Look($dr)) . "</span></td>";
-    echo "</tr>\n";
-    $orow++;
 }
 
 /**
