91ae08d8ef4347cc8357f99f69ff567611e6bde8
Eric Stern <eric@ericstern.com>
2026-04-03 09:22:37 -0700

    Fix wacky headers

--- tests/Tests/Isolated/Core/ErrorHandlerTest.php
+++ tests/Tests/Isolated/Core/ErrorHandlerTest.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * @package   OpenEMR
- *
+ * @package   openemr
  * @link      https://www.open-emr.org
- *
- * @author    Eric Stern <eric@lwt.io>
- * @copyright Copyright (c) 2026 Eric Stern
+ * @author    Eric Stern <erics@opencoreemr.com>
+ * @copyright Copyright (c) 2026 OpenCoreEMR
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
+
+declare(strict_types=1);
 
 namespace OpenEMR\Tests\Isolated\Core;
 
