7022a7f7ab8dcfdf7ba02303952e309fb4d3e400
Eric Stern <eric@ericstern.com>
2026-04-07 09:45:57 -0700

    Add headers, support passing Plaintext object

--- src/Encryption/CipherSuiteInterface.php
+++ src/Encryption/CipherSuiteInterface.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    Eric Stern <erics@opencoreemr.com>
+ * @copyright Copyright (c) 2026 OpenCoreEMR <https://opencoreemr.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
 declare(strict_types=1);
 
 namespace OpenEMR\Encryption;
@@ -10,7 +18,8 @@ use SensitiveParameter;
  * Cryptography implementations.
  *
  * Unlike the lower-level cryptographic tools, this does not require wrapping
- * and unwrapping `Plaintext` structures.
+ * and unwrapping `Plaintext` structures (though you _may_ pass a `Plaintext`
+ * to the `encrypt()` method).
  *
  * Note: This is the replacement for `CryptoInterface` (and `CryptoGen`), and
  * should be preferred whenever possible.
@@ -29,5 +38,5 @@ interface CipherSuiteInterface
      * encrypted data in a format that includes enough metadata to later
      * decrypt it using the `decrypt()` method.
      */
-    public function encrypt(#[SensitiveParameter] string $plaintext): string;
+    public function encrypt(#[SensitiveParameter] Plaintext|string $plaintext): string;
 }
