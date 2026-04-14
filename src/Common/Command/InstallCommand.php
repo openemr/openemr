72d4f05b99f4a1dfa1eb0440c2bffd123fa71cce
Eric Stern <eric@ericstern.com>
2026-04-14 16:44:45 -0700

    Prepare logger

--- src/Common/Command/InstallCommand.php
+++ src/Common/Command/InstallCommand.php
@@ -8,11 +8,14 @@ use Installer;
 use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'install')]
 class InstallCommand extends Command
 {
     public function __invoke(
+        OutputInterface $output,
         #[Option] string $dbServer = '127.0.0.1',
         #[Option] int $dbPort = 3306,
         #[Option] string $dbUser = '',
@@ -21,6 +24,8 @@ class InstallCommand extends Command
         #[Option] string $dbRootUser = 'root',
         #[Option] string $dbRootPassword = '',
     ): int {
+        $logger = new ConsoleLogger($output);
+
         // login -> dbuser
         // pass -> dbPassowrd
         // dbname => dbName
