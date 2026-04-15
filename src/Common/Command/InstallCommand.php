c72636fbd33ef45686577fc5ddb0311ed2a5d4f0
Eric Stern <eric@ericstern.com>
2026-04-14 17:20:46 -0700

    Confirm before continuing

--- src/Common/Command/InstallCommand.php
+++ src/Common/Command/InstallCommand.php
@@ -8,13 +8,16 @@ use Installer;
 use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(name: 'install')]
 class InstallCommand extends Command
 {
     public function __invoke(
+        InputInterface $input,
         OutputInterface $output,
         #[Option] string $dbHost = '127.0.0.1',
         #[Option] int $dbPort = 3306,
@@ -24,6 +27,11 @@ class InstallCommand extends Command
         #[Option] string $dbRootUser = 'root',
         #[Option] string $dbRootPassword = '',
     ): int {
+        $io = new SymfonyStyle($input, $output);
+
+        if ($input->isInteractive() && !$io->confirm('This command is experimental. Continue? (--no-interaction will auto-apply "yes")', default: false)) {
+            return Command::FAILURE;
+        }
 
         // login -> dbuser
         // pass -> dbPassowrd
