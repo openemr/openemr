bec88bb685f61d248e59acfc598aa86172b33f4b
stephen waite <stephen.waite@cmsvt.com>
2026-04-23 19:45:03 -0400

    assisted by claude.ai:

--- library/classes/Controller.class.php
+++ library/classes/Controller.class.php
@@ -3,11 +3,11 @@
 /**
  * Controller Class.
  *
- * @package OpenEMR
- * @link    https://www.open-emr.org
- * @author Stephen Waite <stephen.waite@open-emr.org>
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    Stephen Waite <stephen.waite@open-emr.org>
  * @copyright Copyright (c) 2026 Stephen Waite <stephen.waite@cmsvt.com>
- * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
 use OpenEMR\Common\Acl\AccessDeniedHelper;
