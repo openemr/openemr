c84949b1959e6e3f3372eac1c7934766393587f5
Eric Stern <eric@ericstern.com>
2026-05-04 09:58:29 -0700

    drop count (assuming count not sum)

--- .phpstan/fatal-baseline-caps.php
+++ .phpstan/fatal-baseline-caps.php
@@ -35,7 +35,7 @@ declare(strict_types=1);
 
 return [
     'all' => [
-        'class.notFound.php' => 182,
+        'class.notFound.php' => 181,
         'classConstant.notFound.php' => 0,
         'constant.notFound.php' => 0,
         'function.notFound.php' => 0,
