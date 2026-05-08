9d31e3df32487890fa849f5fa4075b335ad53066
Eric Stern <eric@ericstern.com>
2026-05-08 16:40:25 -0700

    Notes for other column decryption

--- src/Console/Command/Encryption/SyncCommand.php
+++ src/Console/Command/Encryption/SyncCommand.php
@@ -20,17 +20,21 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'encryption:sync', description: 'sync encryption')]
 class SyncCommand extends Command
 {
-    private array $database = [
-        'login_mfa_registrations' => ['var1'],
-        'log' => ['comments'],
+    private array $encryptedDatabaseColumns = [
         'api_log' => ['request_url', 'request_body', 'response'],
-        'x12_partners' => ['x12_sftp_pass'],
-        'onsite_portal_activity' => ['checksum'],
-        'oauth_clients' => ['client_secret'],
-        'keys' => ['value'],
-        'payment_processing_audit' => ['audit_data'],
-        'module_faxsms_credentials' => ['credentials'],
         'comlink_telehealth_auth' => ['auth_token'],
+        // Keys needs special handling! Encrypting the encryption keys will
+        // destroy the installation; only the OAuth keys need touching.
+        // 'keys' => ['value'],
+        // Logs also needs special handling - comments is either encrypted or
+        // base64'd
+        // 'log' => ['comments'],
+        'login_mfa_registrations' => ['var1'], // pk=(user_id,name)
+        'module_faxsms_credentials' => ['credentials'],
+        'oauth_clients' => ['client_secret'], // pk=client_id
+        'onsite_portal_activity' => ['checksum'],
+        'payment_processing_audit' => ['audit_data'], // pk=uuid
+        'x12_partners' => ['x12_sftp_pass'],
     ];
 
     // TODO: can this be pulled straight from GLOBALS_METADATA?
@@ -92,6 +96,13 @@ class SyncCommand extends Command
         OutputInterface $output,
         #[Option(description: 'Do not write changes')] bool $dryRun = false,
     ): int {
+        foreach ($this->encryptedDatabaseColumns as $table => $columns) {
+            foreach ($columns as $column) {
+                // $this->syncDataInColumn($table, $column); // need to read+pass PK
+            }
+        }
+
+
         // iterate over table, figure out target data based on config state and
         // if needed key version
         //
@@ -141,5 +152,15 @@ class SyncCommand extends Command
         // files: again, its own thing
 
         return 0;
+    }
+
+    private function syncDataInColumn(string $table, string $column, string $pkColumn): void
+    {
+        // Depending on installation size, this may need to be done in chunks.
+        // This also needs to check if the table even exists, some are for
+        // dormant modules.
+
+        // one table as a multi-column PK. It's probably best to just fix that
+        // at the source rather than trying to patch around it here.
     }
 }
