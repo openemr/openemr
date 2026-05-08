5e6946cfb54a5005beaf9650dc4a86a107e983bd
Eric Stern <eric@ericstern.com>
2026-05-08 16:36:42 -0700

    Get crypto working

--- config/hacks.php
+++ config/hacks.php
@@ -12,14 +12,38 @@
 
 declare(strict_types=1);
 
+use Doctrine\DBAL\Connection;
+use Psr\Log\LoggerInterface;
 use Firehed\Container\TypedContainerInterface as TC;
-use OpenEMR\Common\Crypto;
+use OpenEMR\BC\Crypto;
+use OpenEMR\Common\Crypto\CryptoInterface;
+use OpenEMR\Encryption;
 
 return [
-    Crypto\CryptoInterface::class => Crypto\CryptoGen::class,
-    Crypto\CryptoGen::class => function (TC $c) {
-        $GLOBALS['OE_SITE_DIR'] = 'sites/' . $c->getString('OPENEMR_SITE');
-        require_once 'library/sql.inc.php';
-        return new Crypto\CryptoGen();
+    CryptoInterface::class => Crypto\Crypto::class,
+    Crypto\Crypto::class => function (TC $c) {
+        // TODO: these need to be sourced from app config
+        $shouldEncryptForDatabase = true;
+        $shouldEncryptForFilesystem = true;
+        return new Crypto\Crypto(
+            $c->get(Encryption\Keys\KeychainInterface::class),
+            $c->get(LoggerInterface::class),
+            shouldEncryptForDatabase: $shouldEncryptForDatabase,
+            shouldEncryptForFilesystem: $shouldEncryptForFilesystem,
+        );
+    },
+
+    Encryption\Keys\KeychainInterface::class => function (TC $c) {
+        $keyDirectory = sprintf(
+            '%s/documents/logs_and_misc/methods',
+            $c->getString('legacySiteDirectory'),
+        );
+        $fs = new Encryption\Storage\PlaintextKeyOnDisk($keyDirectory);
+        $db = new Encryption\Storage\PlaintextKeyInDbKeysTable($c->get(Connection::class));
+        return Crypto\LegacyKeychainLoader::loadWithEngines(
+            filesystemStorage: $fs,
+            databaseStorage: $db,
+            storageDir: $keyDirectory,
+        );
     },
 ];
