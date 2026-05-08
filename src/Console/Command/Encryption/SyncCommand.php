294995a4ced4dccb33a8348ecd8c136fa1238fd8
Eric Stern <eric@ericstern.com>
2026-05-08 13:08:34 -0700

    Stub db interaction concept

--- src/Console/Command/Encryption/SyncCommand.php
+++ src/Console/Command/Encryption/SyncCommand.php
@@ -17,9 +17,32 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 #[AsCommand(name: 'encryption:sync', description: 'sync encryption')]
 class SyncCommand extends Command
 {
+    private array $database = [
+        'login_mfa_registrations' => ['var1'],
+        'log' => ['comments'],
+        'api_log' => ['request_url', 'request_body', 'response'],
+        'x12_partners' => ['x12_sftp_pass'],
+        'onsite_portal_activity' => ['checksum'],
+        'oauth_clients' => ['client_secret'],
+        'keys' => ['value'],
+        'payment_processing_audit' => ['audit_data'],
+        'module_faxsms_credentials' => ['credentials'],
+        'comlink_telehealth_auth' => ['auth_token'],
+    ];
+
     public function __invoke(
         OutputInterface $output,
+        #[Option(description: 'Do not write changes')] bool $dryRun = false,
     ): int {
+        // iterate over table, figure out target data based on config state and
+        // if needed key version
+        //
+        // loop over data not in target state
+        // - decryptFromDatabase
+        // - encryptForDatabase
+        // - UPDATE
+        //
+        //
         return 0;
     }
 }
