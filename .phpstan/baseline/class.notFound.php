d7e7c728ed6b55e061647496efdfc2b26a16fac1
Jerry Padgett <sjpadgett@gmail.com>
2026-06-11 16:04:53 -0400

    chore: regenerate phpstan baseline

--- .phpstan/baseline/class.notFound.php
+++ .phpstan/baseline/class.notFound.php
@@ -37,6 +37,16 @@ $ignoreErrors[] = [
     'path' => __DIR__ . '/../../interface/modules/custom_modules/oe-module-dorn/src/Bootstrap.php',
 ];
 $ignoreErrors[] = [
+    'message' => '#^Call to static method makeFromGlobals\\(\\) on an unknown class OpenEMR\\\\Modules\\\\ClaimRevConnector\\\\ClaimRevApi\\.$#',
+    'count' => 1,
+    'path' => __DIR__ . '/../../interface/modules/custom_modules/oe-module-dorn/src/ConnectorApi.php',
+];
+$ignoreErrors[] = [
+    'message' => '#^Caught class OpenEMR\\\\Modules\\\\ClaimRevConnector\\\\ClaimRevAuthenticationException not found\\.$#',
+    'count' => 1,
+    'path' => __DIR__ . '/../../interface/modules/custom_modules/oe-module-dorn/src/ConnectorApi.php',
+];
+$ignoreErrors[] = [
     'message' => '#^Property OpenEMR\\\\Modules\\\\Dorn\\\\models\\\\OrderStatusViewModel\\:\\:\\$createdDateTimeUtc has unknown class OpenEMR\\\\Modules\\\\Dorn\\\\models\\\\DateTime as its type\\.$#',
     'count' => 1,
     'path' => __DIR__ . '/../../interface/modules/custom_modules/oe-module-dorn/src/models/OrderStatusViewModel.php',
