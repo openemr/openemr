2ae4f40d971b562c58b10af9b127a8190c899265
Eric Stern <eric@ericstern.com>
2026-06-22 15:16:39 -0700

    Remove vestigial inputs

--- src/Console/Command/Codes/UpdateMappingsCommand.php
+++ src/Console/Command/Codes/UpdateMappingsCommand.php
@@ -15,8 +15,6 @@ namespace OpenEMR\Console\Command\Codes;
 use OpenEMR\Services\CodeTypes\CodeTypeMappingUpdater;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: 'codes:update-mappings',
@@ -29,10 +27,8 @@ class UpdateMappingsCommand extends Command
         parent::__construct();
     }
 
-    public function __invoke(
-        InputInterface $input,
-        OutputInterface $output,
-    ): int {
+    public function __invoke(): int
+    {
         $this->updater->updateActivatedMappings();
 
         return Command::SUCCESS;
