cf2d9c5bfa42e3f67681127ddcae42b1dabf7aeb
Eric Stern <eric@ericstern.com>
2026-06-24 10:03:51 -0700

    Define CLI module interface

---
+++ src/Modules/CliModuleInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEMR\Modules;
+
+interface CliModuleInterface extends ModuleInterface
+{
+    /**
+     * Provide a list of `doctrine/console` commands that the module exposes.
+     * This must return the class names of the commands, not instantiated
+     * objects.
+     *
+     * Commands MUST be registered in the DI container
+     *
+     * Invokble commands (which do not need to extend `Command` nor have an
+     * `execute()` method) are preferred.
+     *
+     * @return class-string[]
+     */
+    public static function getCommandClasses(): array;
+}
