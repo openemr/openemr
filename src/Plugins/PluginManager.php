940b51a98646c7b907011c12299d707bf9223043
Eric Stern <eric@ericstern.com>
2026-06-24 16:45:08 -0700

    Plugin manager needs to do terrible things

--- src/Plugins/PluginManager.php
+++ src/Plugins/PluginManager.php
@@ -4,44 +4,69 @@ declare(strict_types=1);
 
 namespace OpenEMR\Plugins;
 
+use Firehed\Container\AutoDetect;
+use OpenEMR\Plugin\CliPluginInterface;
+use OpenEMR\PluginInstaller\GeneratedInstalledPlugins;
+use OpenEMR\PluginInstaller\Plugin;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
 
 class PluginManager
 {
+    public static function fromConfig(): self
+    {
+        // This might also need to be a singleton?
+        $builder = AutoDetect::getBuilder(compiledOutputPath: 'vendor/compiledModuleContainer.php');
+        $builder->addDirectory(__DIR__ . '/config');
+        $container = $builder->build();
+        return $container->get(self::class);
+    }
+
     public function __construct(
         private Activation\StateProvider $activationState,
     ) {
     }
 
     /**
-     * @template T of PluginInterface
-     * @param class-string<T> $filter
-     * @return iterable<class-string<T>>
+     * @return iterable<Plugin>
      */
-    private function getActivePlugins(string $filter = PluginInterface::class): iterable
+    private function getActivePlugins(): iterable
     {
         // TODO: cache/memoize this data?
-        $pluginData = \OpenEMR\PluginInstaller\GeneratedInstalledPlugins::PLUGIN_DATA;
-        foreach ($pluginData as $package => $data) {
-            if (!$this->activationState->isActive($package)) {
-                continue;
+        foreach (GeneratedInstalledPlugins::getPlugins() as $plugin) {
+            if ($this->activationState->isActive($plugin->name)) {
+                yield $plugin;
             }
-            foreach ($data['bootstraps'] as $bootstrap) {
-                if (is_a($bootstrap, $filter, allow_string: true)) {
-                    yield $bootstrap;
+        }
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function getConfigFiles(): array
+    {
+        $configFiles = [];
+        foreach ($this->getActivePlugins() as $plugin) {
+            foreach ($plugin->bootstrapClasses as $bootstrap) {
+                foreach ($bootstrap::getConfigFiles() as $relativePath) {
+                    $configFiles[] = $plugin->relativeInstallDirectory . '/' . $relativePath;
                 }
             }
         }
+        return $configFiles;
     }
 
     public function addCommands(Application $cli, ContainerInterface $container): void
     {
         // TODO: Figure out how to make these lazy-loaded
-        foreach ($this->getActivePlugins(CliPluginInterface::class) as $bootstrap) {
-            $commands = $bootstrap::getCommandClasses();
-            foreach ($commands as $command) {
-                $cli->addCommand($container->get($command));
+        foreach ($this->getActivePlugins() as $plugin) {
+            foreach ($plugin->bootstrapClasses as $bootstrap) {
+                if (!is_a($bootstrap, CliPluginInterface::class, allow_string: true)) {
+                    continue;
+                }
+                foreach ($bootstrap::getCommandClasses() as $command) {
+                    $cli->addCommand($container->get($command));
+                }
             }
         }
     }
