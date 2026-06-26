b89da50b95c0ec0bea383602034b18bb8ba11f3b
Eric Stern <eric@ericstern.com>
2026-06-26 13:50:14 -0700

    Prepare BreakglassChecker(Interface) for wiring

---
+++ config/audit.php
@@ -0,0 +1,20 @@
+<?php
+
+use Firehed\Container\TypedContainerInterface as TC;
+use OpenEMR\Common\Database\{
+    ConnectionManager,
+    ConnectionType,
+};
+use OpenEMR\Common\Logging\{
+    BreakglassChecker,
+    BreakglassCheckerInterface,
+};
+
+return [
+    BreakglassCheckerInterface::class => BreakglassChecker::class,
+    // See notes in BreakglassChecker's constructor: it must use the
+    // non-audited connection in order to avoid an infinte loop w/ SQL logging
+    BreakglassChecker::class => fn (TC $c) => new BreakglassChecker(
+        $c->get(ConnectionManager::class)->get(ConnectionType::NonAudited),
+    ),
+];
