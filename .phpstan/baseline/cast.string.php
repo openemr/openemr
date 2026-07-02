ffc13a048a5d4c44e4a8625ecab2c2e0ab87d9a2
Eric Stern <eric@ericstern.com>
2026-07-02 13:26:19 -0700

    baseline

--- .phpstan/baseline/cast.string.php
+++ .phpstan/baseline/cast.string.php
@@ -353,7 +353,7 @@ $ignoreErrors[] = [
 ];
 $ignoreErrors[] = [
     'message' => '#^Cannot cast mixed to string\\.$#',
-    'count' => 91,
+    'count' => 90,
     'path' => __DIR__ . '/../../interface/forms/eye_mag/php/eye_mag_functions.php',
 ];
 $ignoreErrors[] = [
