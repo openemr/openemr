d503fe8710e5f0cf5f9e6494f57b840b698c6061
Michael A. Smith <michael@opencoreemr.com>
2026-07-15 09:27:11 -0400

    fix(painmap): echo nothing (#13008)

--- .phpstan/baseline/echo.nonString.php
+++ .phpstan/baseline/echo.nonString.php
@@ -179,11 +179,6 @@ $ignoreErrors[] = [
 $ignoreErrors[] = [
     'message' => '#^Parameter \\#1 \\(mixed\\) of echo cannot be converted to string\\.$#',
     'count' => 1,
-    'path' => __DIR__ . '/../../interface/forms/painmap/save.php',
-];
-$ignoreErrors[] = [
-    'message' => '#^Parameter \\#1 \\(mixed\\) of echo cannot be converted to string\\.$#',
-    'count' => 1,
     'path' => __DIR__ . '/../../interface/forms/phq9/common.php',
 ];
 $ignoreErrors[] = [
