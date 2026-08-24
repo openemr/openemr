439e8e30ee1704fafe4ffc3ea621888359fc00ee
Simon Quigley <squigley@altispeed.com>
2026-08-24 11:47:00 -0500

    feat(lbf-statements): add Module Manager listener and configure page
    Wire register/enable/disable/unregister and a Configure link that points at the rules screen.

---
+++ interface/modules/custom_modules/oe-module-lbf-statements/moduleConfig.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Modules → Configure entry. Rules are edited from Form statements.
+ *
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    Simon Quigley <squigley@altispeed.com>
+ * @copyright Copyright (c) 2026 Simon Quigley <squigley@altispeed.com>
+ * @license   GNU General Public License 3
+ */
+
+declare(strict_types=1);
+
+require_once dirname(__DIR__, 4) . "/globals.php";
+
+use OpenEMR\Common\Acl\AclMain;
+use OpenEMR\Core\OEGlobalsBag;
+
+$module_config = 1;
+
+if (!AclMain::aclCheckCore('admin', 'super')) {
+    echo xlt('Not authorized');
+    exit;
+}
+
+$url = OEGlobalsBag::getInstance()->getWebRoot()
+    . '/interface/modules/custom_modules/oe-module-lbf-statements/public/admin.php';
+?>
+<div class="container-fluid p-3">
+    <p><?php echo xlt('This module has no extra site settings. Rules and the paragraph field are edited from Modules → Form statements.'); ?></p>
+    <p><a href="<?php echo attr($url); ?>"><?php echo xlt('Open Form statement rules'); ?></a></p>
+</div>
