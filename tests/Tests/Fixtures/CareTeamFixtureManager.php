fbce3bf0ecf875627e5e052aa80fb69ade79b0d7
Craig Allen <craig@interconnected.au>
2026-09-03 21:53:51 +0200

    test: enable strict types for care-team coverage

--- tests/Tests/Fixtures/CareTeamFixtureManager.php
+++ tests/Tests/Fixtures/CareTeamFixtureManager.php
@@ -14,6 +14,8 @@
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
+declare(strict_types=1);
+
 namespace OpenEMR\Tests\Fixtures;
 
 use OpenEMR\BC\ServiceContainer;
