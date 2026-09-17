b90de4f53154ed446787aab0a100e9ecf298673a
Simon Quigley <squigley@altispeed.com>
2026-09-17 16:41:08 -0500

    refactor(statements): move envelope globals UI to Twig and a JS file
    Stephen Waite asked that the custom-measurement callback follow
    TelehealthGlobalConfig: a class under src/ renders Twig, and the
    script is a separate asset instead of inline markup in globals.inc.php.

---
+++ src/Billing/StatementEnvelopeGlobalConfig.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Globals UI for custom statement envelope measurements.
+ *
+ * @package   OpenEMR
+ * @link      https://www.open-emr.org
+ * @author    Simon Quigley <squigley@altispeed.com>
+ * @copyright Copyright (c) 2026 Simon Quigley <squigley@altispeed.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+declare(strict_types=1);
+
+namespace OpenEMR\Billing;
+
+use OpenEMR\Common\Twig\TwigContainer;
+
+class StatementEnvelopeGlobalConfig
+{
+    /**
+     * HTML display section for Administration > Billing > Statement envelope.
+     *
+     * @param mixed $fldid
+     * @param mixed $fldarray
+     */
+    public static function renderCustomFields($fldid, $fldarray): string
+    {
+        $twig = (new TwigContainer())->getTwig();
+        return $twig->render('billing/statement_envelope_custom.html.twig', [
+            'fldid' => $fldid,
+            'fldarray' => $fldarray,
+        ]);
+    }
+}
